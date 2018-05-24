3f22eb20b437c115436badde7e989c8c5a60d19c
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2018-05-24 14:27:38 +0100

    Stop isort complaining

--- src/tlo/__init__.py
+++ src/tlo/__init__.py
@@ -1,15 +1,11 @@
 __version__ = '0.1.0'
 
-# We use this as our main date type (even though it has nanosecond resolution...)
+# We import our key classes so they're available in the main namespace.
+# Pandas' Timestamp is used as our main date type (even though it has nanosecond resolution...)
+
 from pandas import Timestamp as Date  # noqa
 from pandas.tseries.offsets import DateOffset  # noqa
 
-# Import our key classes so they're available in the main namespace
-from .core import (  # noqa
-    Module,
-    Parameter,
-    Property,
-    Types,
-)
+from .core import Module, Parameter, Property, Types  # noqa
 from .population import Person, Population  # noqa
 from .simulation import Simulation  # noqa
