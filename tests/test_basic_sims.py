fd7a50e40c270e5fcfd01260893209c38a0840c7
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2018-05-28 19:22:01 +0100

    Add a second test case with birth

--- tests/test_basic_sims.py
+++ tests/test_basic_sims.py
@@ -1,8 +1,8 @@
 
 import pandas as pd
 
-from tlo import Date, Person, Simulation
-from tlo.test import random_death
+from tlo import Date, DateOffset, Person, Simulation
+from tlo.test import random_birth, random_death
 
 
 def test_individual_death():
@@ -53,3 +53,56 @@ def test_individual_death():
     pd.testing.assert_series_equal(
         pd.Series([Date(2010, 3, 1), Date(2010, 4, 1)], name='date_of_death'),
         sim.population.props['date_of_death'])
+
+
+def test_birth_and_death():
+    # This combines both population-scope and individual-scope events,
+    # with more complex logic.
+    # Create a new simulation to orchestrate matters
+    sim = Simulation(start_date=Date(1950, 1, 1))
+
+    # Register modules
+    rb = random_birth.RandomBirth(name='rb')
+    rb.pregnancy_probability = 0.5
+    rd = random_death.RandomDeath(name='rd')
+    rd.death_probability = 0.01
+    sim.register(rb, rd)
+    assert sim.modules['rb'] is rb
+    assert sim.modules['rd'] is rd
+
+    # Seed the random number generators
+    sim.seed_rngs(2)
+
+    # Create a population of 10 individuals
+    sim.make_initial_population(n=10)
+    assert len(sim.population) == 10
+
+    # Test iteration and individual-based property access
+    for i, person in enumerate(sim.population):
+        assert isinstance(person, Person)
+        assert person.index == i
+        assert person.is_alive
+        assert not person.is_pregnant
+
+    # Simulate for 5 years
+    assert sim.date == Date(1950, 1, 1)
+    sim.simulate(end_date=Date(1955, 1, 1))
+    assert sim.date == Date(1955, 1, 1)
+
+    # Check birth stats match reference data
+    assert len(sim.population) == 35
+
+    # Check no-one gave birth after dying
+    for person in sim.population:
+        if not person.is_alive:
+            for child in person.children:
+                assert child.date_of_birth < person.date_of_death
+
+    # Check people can't become pregnant while already pregnant
+    for person in sim.population:
+        # Iterate over pairs of adjacent children
+        for child1, child2 in zip(person.children[:-1], person.children[1:]):
+            # Children earlier in the list are born earlier
+            assert child1.date_of_birth < child2.date_of_birth
+            # Birth dates need to be at least 9 months apart
+            assert child1.date_of_birth + DateOffset(months=9) <= child2.date_of_birth
