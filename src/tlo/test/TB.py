152cbab277f3a0213f09b4dead2c776253082fc8
Tara <t.mangal@imperial.ac.uk>
2018-08-20 14:47:29 +0100

    developing baseline population prevalence

--- src/tlo/test/TB.py
+++ src/tlo/test/TB.py
@@ -7,6 +7,11 @@ TB_prev2018 = 18091575 / sim_size
 
 inds = pd.read_csv('Q:/Thanzi la Onse/HIV/initial_pop_dataframe2018.csv')
 
+# add column to inds for TB status
+inds['TB status'] = 'U'
+
+# inds.head(20)
+
 incidence = pd.read_excel('Q:/Thanzi la Onse/TB/Method Template TB.xlsx', sheet_name='TB_incidence', header=0)
 
 
@@ -17,15 +22,42 @@ mu = 0.15  # TB mortality rate
 
 current_time = 2018
 
+# baseline population
 # assign infected status using WHO prevalence 2016 by 2 x age-groups
 def prevalenceTB_inds(inds, current_time):
 
     # sample from uninfected population using WHO incidence
-    # age 0-14
+    # male age 0-14
     tmp = np.random.choice(inds.index[(inds.age < 15) & (inds.sex == 'M')],
                             size=int((incidence['Incident cases'][(incidence.Year == 2016) &
                                                                   (incidence.Sex == 'M') &
                                                              (incidence.Age == '0_14')]) / sim_size),
                            replace=False)
+    inds.loc[tmp, 'TB status'] = 'I'  # change status to infected
+
+    # female age 0-14
+    tmp2 = np.random.choice(inds.index[(inds.age < 15) & (inds.sex == 'F')],
+                            size=int((incidence['Incident cases'][(incidence.Year == 2016) &
+                                                                  (incidence.Sex == 'F') &
+                                                             (incidence.Age == '0_14')]) / sim_size),
+                           replace=False)
+    inds.loc[tmp2, 'TB status'] = 'I'  # change status to infected
+
+    # male age >=15
+    tmp3 = np.random.choice(inds.index[(inds.age >= 15) & (inds.sex == 'M')],
+                           size=int((incidence['Incident cases'][(incidence.Year == 2016) &
+                                                                 (incidence.Sex == 'M') &
+                                                                 (incidence.Age == '15_80')]) / sim_size),
+                           replace=False)
+    inds.loc[tmp3, 'TB status'] = 'I'  # change status to infected
+
+    # female age >=15
+    tmp4 = np.random.choice(inds.index[(inds.age >= 15) & (inds.sex == 'F')],
+                            size=int((incidence['Incident cases'][(incidence.Year == 2016) &
+                                                                  (incidence.Sex == 'F') &
+                                                                  (incidence.Age == '15_80')]) / sim_size),
+                            replace=False)
+    inds.loc[tmp4, 'TB status'] = 'I'  # change status to infected
+
 
 
