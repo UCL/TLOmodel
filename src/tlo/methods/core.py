5eafffcdedbf48550ec2e320a7cdaace214b44b0
Asif Tamuri <tamuri@gmail.com>
2018-10-03 21:22:10 +0100

    the start of core module handling dob and sex. currently filled with dummy values.

---
+++ src/tlo/methods/core.py
@@ -0,0 +1,91 @@
+"""
+A skeleton template for disease methods.
+"""
+import numpy as np
+import pandas as pd
+
+from tlo import Module, Parameter, Property, Types
+
+
+class Core(Module):
+    """
+    One line summary goes here...
+
+    All disease modules need to be implemented as a class inheriting from Module.
+    They need to provide several methods which will be called by the simulation
+    framework:
+    * `read_parameters(data_folder)`
+    * `initialise_population(population)`
+    * `initialise_simulation(sim)`
+    * `on_birth(mother, child)`
+    """
+
+    # Here we declare parameters for this module. Each parameter has a name, data type,
+    # and longer description.
+    PARAMETERS = {
+        'prop_male': Parameter(Types.REAL, 'Proportion of population that is male'),
+    }
+
+    # Next we declare the properties of individuals that this module provides.
+    # Again each has a name, type and description. In addition, properties may be marked
+    # as optional if they can be undefined for a given individual.
+    PROPERTIES = {
+        'date_of_birth': Property(Types.DATE, 'Date of birth of this individual'),
+        'sex': Property(Types.CATEGORICAL, 'Male or female', categories=['M', 'F']),
+        'mother_id': Property(Types.INT, 'Unique identifier of mother of this individual'),
+        'is_alive': Property(Types.BOOL, 'Whether this individual is alive'),
+    }
+
+    def read_parameters(self, data_folder):
+        """Read parameter values from file, if required.
+
+        Here we do nothing.
+
+        :param data_folder: path of a folder supplied to the Simulation containing data files.
+          Typically modules would read a particular file within here.
+        """
+        self.parameters['prop_male'] = 0.505
+
+    def initialise_population(self, population):
+        """Set our property values for the initial population.
+
+        This method is called by the simulation when creating the initial population, and is
+        responsible for assigning initial values, for every individual, of those properties
+        'owned' by this module, i.e. those declared in the PROPERTIES dictionary above.
+
+        :param population: the population of individuals
+        """
+        df = population.props
+        df.mother_id = None
+        df.is_alive = True
+
+        # date_of_birth is uniform sample from ages 0 to 100
+        df.date_of_birth = pd.NaT
+        age = pd.to_timedelta(np.random.sample(size=len(df)) * 100, unit='Y')
+        df.date_of_birth = pd.Timestamp.now() - age
+
+        # sex
+        df.sex = np.nan  # this is how to initialise a Categorical property
+        prop_male = self.parameters['prop_male']
+        prop_female = 1 - prop_male
+        df.sex = np.random.choice(['M', 'F'], size=len(df), p=[prop_male, prop_female])
+
+    def initialise_simulation(self, sim):
+        """Get ready for simulation start.
+
+        This method is called just before the main simulation loop begins, and after all
+        modules have read their parameters and the initial population has been created.
+        It is a good place to add initial events to the event queue.
+        """
+        pass
+
+    def on_birth(self, mother, child):
+        """Initialise our properties for a newborn individual.
+
+        This is called by the simulation whenever a new person is born.
+
+        :param mother: the mother for this child
+        :param child: the new child
+        """
+        pass
+
