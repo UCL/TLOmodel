085f835f6b6298b37c751517ed5cba5e3727bfa2
Tara Mangal <t.mangal@imperial.ac.uk>
2018-10-05 18:12:15 +0100

    test runs complete

--- src/tlo/test/TB.py
+++ src/tlo/test/TB.py
@@ -10,7 +10,8 @@ from tlo.events import PopulationScopeEventMixin, RegularEvent
 
 file_path = '/Users/Tara/Desktop/TLO/TB/Method_TB.xlsx'
 tb_data = pd.read_excel(file_path, sheet_name=None, header=0)
-Active_tb_prop, Latent_tb_prop = tb_data['Active_tb_prop'], tb_data['Latent_tb_prop']
+Active_tb_prop, Latent_tb_prop = tb_data['Active_TB_prob'], tb_data['Latent_TB_prob']
+
 
 class tb_baseline(Module):
     """ Set up the baseline population with TB prevalence
@@ -55,22 +56,24 @@ class tb_baseline(Module):
         'rr_tb_mortality_hiv': Parameter(
             Types.REAL,
             'relative risk of mortality from tb in hiv+ compared with hiv-'),
-        'force_of_infection': Parameter(
+        'transmission_rate': Parameter(
             Types.REAL,
-            'force of infection for new latent infections applied across whole pop')  # dummy value
+            'mean rate of transmission per TB case')  # dummy value
     }
 
     # Next we declare the properties of individuals that this module provides.
     # Again each has a name, type and description. In addition, properties may be marked
     # as optional if they can be undefined for a given individual.
     PROPERTIES = {
-        'has_tb': Property(Types.CATEGORICAL, 'tb status: Uninfected, Latent, Active'),
-        'date_active_tb': Property(Types.DATE, 'Date active tb infection started'),
+        'has_tb': Property(Types.CATEGORICAL, categories=['Uninfected', 'Latent', 'Active'], description='tb status'),
+        'date_active_tb': Property(Types.DATE, 'Date active tb started'),
         'date_latent_tb': Property(Types.DATE, 'Date acquired tb infection (latent stage)'),
         'date_tb_death': Property(Types.DATE, 'Projected time of tb death if untreated'),
-        'on_treatment': Property(Types.BOOL, 'Currently on treatment for tb'),
-    }
 
+        # should be handled by other modules TODO: remove from here
+        'sex': Property(Types.CATEGORICAL, categories=['M', 'F'], description='Male or female'),
+        'date_of_birth': Property(Types.DATE, 'Date of birth'),
+    }
 
     def read_parameters(self, data_folder):
         """Read parameter values from file, if required.
@@ -95,14 +98,13 @@ class tb_baseline(Module):
 
         params['rr_infectiousness_hiv'] = 0.52
         params['prob_self_cure'] = 0.15
-        params['self_cure'] = 2
+        params['self_cure'] = 0.33  # tiemersma plos one 2011, self-cure/death in 3 yrs
         params['tb_mortality_rate'] = 0.15
         params['rr_tb_mortality_HIV'] = 17.1
-        params['force_of_infection'] = 0.0015  # dummy value
+        params['transmission_rate'] = 7.2  # dummy value
 
     def get_age(self, date_of_birth):
         return (self.sim.date - date_of_birth).dt.days / 365.25
-
 
     def initialise_population(self, population):
         """Set our property values for the initial population.
@@ -114,12 +116,26 @@ class tb_baseline(Module):
         :param population: the population of individuals
 
         initial pop is 2014
-        then 2015-2018 needs to run with beta in the FOI
+        then 2015-2018+ needs to run with beta in the FOI
 
         """
         p = population
 
         now = self.sim.date
+
+        # TODO: this should be moved to core demography module
+        initial_pop_data = pd.read_csv(
+            '/Users/Tara/Desktop/TLO/initial_pop_dataframe2018.csv')
+        initial_sex_age: pd.DataFrame = initial_pop_data.groupby(['sex', 'age']).size().reset_index(name='counts')
+        initial_sex_age['proportion'] = initial_sex_age.counts / len(initial_pop_data)
+
+        sampled_sex_age = initial_sex_age.sample(n=len(p), weights='proportion', random_state=self.rng,
+                                                 replace=True)
+        sampled_sex_age = sampled_sex_age.reset_index(drop=True)
+
+        p.sex = sampled_sex_age['sex']
+        p.date_of_birth = sampled_sex_age['age'].apply(lambda x: self.sim.date - DateOffset(years=x))
+
         age = self.get_age(p.date_of_birth)
 
         # set-up 2014 population
@@ -137,7 +153,7 @@ class tb_baseline(Module):
             if idx.any():
                 # sample from uninfected population using WHO prevalence
                 fraction_latent_tb = Latent_tb_prop.loc[
-                    (Latent_tb_prop.sex == 'M') & (Latent_tb_prop.age == i), 'prop_latent_tb']
+                    (Latent_tb_prop.sex == 'M') & (Latent_tb_prop.age == i), 'prob_latent_tb']
                 male_latent_tb = p[idx].sample(frac=fraction_latent_tb).index
                 p[male_latent_tb, 'has_tb'] = 'Latent'
                 p[male_latent_tb, 'date_latent_tb'] = now
@@ -145,7 +161,7 @@ class tb_baseline(Module):
                 idx_uninfected = (age == i) & (p.sex == 'M') & (p.has_tb == 'Uninfected')
 
                 fraction_active_tb = Active_tb_prop.loc[
-                    (Active_tb_prop.sex == 'M') & (Active_tb_prop.age == i), 'prop_active_tb']
+                    (Active_tb_prop.sex == 'M') & (Active_tb_prop.age == i), 'prob_active_tb']
                 male_active_tb = p[idx_uninfected].sample(frac=fraction_active_tb).index
                 p[male_active_tb, 'has_tb'] = 'Active'
                 p[male_active_tb, 'date_active_tb'] = now
@@ -156,7 +172,7 @@ class tb_baseline(Module):
             if idx.any():
                 # sample from uninfected population using WHO prevalence
                 fraction_latent_tb = Latent_tb_prop.loc[
-                    (Latent_tb_prop.sex == 'F') & (Latent_tb_prop.age == i), 'prop_latent_tb']
+                    (Latent_tb_prop.sex == 'F') & (Latent_tb_prop.age == i), 'prob_latent_tb']
                 female_latent_tb = p[idx].sample(frac=fraction_latent_tb).index
                 p[female_latent_tb, 'has_tb'] = 'Latent'
                 p[female_latent_tb, 'date_latent_tb'] = now
@@ -164,11 +180,10 @@ class tb_baseline(Module):
                 idx_uninfected = (age == i) & (p.sex == 'F') & (p.has_tb == 'Uninfected')
 
                 fraction_active_tb = Active_tb_prop.loc[
-                    (Active_tb_prop.sex == 'F') & (Active_tb_prop.age == i), 'prop_active_tb']
+                    (Active_tb_prop.sex == 'F') & (Active_tb_prop.age == i), 'prob_active_tb']
                 female_active_tb = p[idx_uninfected].sample(frac=fraction_active_tb).index
                 p[female_active_tb, 'has_tb'] = 'Active'
                 p[female_active_tb, 'date_active_tb'] = now
-
 
     def initialise_simulation(self, sim):
         """Get ready for simulation start.
@@ -181,7 +196,6 @@ class tb_baseline(Module):
         TB_poll = TB_Event(self)
         # first TB_event happens in 12 months, i.e. 2015
         sim.schedule_event(TB_poll, sim.date + DateOffset(months=12))
-
 
     def on_birth(self, mother, child):
         """Initialise our properties for a newborn individual.
@@ -228,30 +242,36 @@ class TB_Event(RegularEvent, PopulationScopeEventMixin):
 
         # apply a force of infection to produce new latent cases
         # no age distribution for FOI but the relative risks would affect distribution of active infection
-        # remember event is occurring each month so scale rates accordingly
-        prob_TB_new = pd.Series(params['force_of_infection'], index=p[p.has_TB == 'Uninfected'].index)
-        is_newly_infected = prob_TB_new > rng.rand(len(prob_TB_new))
+        # remember event is occurring annually so scale rates accordingly
+
+        active_total = len(p[p.has_tb == 'Active'])
+        uninfected_total = len(p[p.has_tb == 'Uninfected'])
+        total_population = len(p)
+
+        force_of_infection = (params['transmission_rate'] * active_total * uninfected_total) / total_population
+
+        prob_tb_new = pd.Series(force_of_infection, index=p[p.has_tb == 'Uninfected'].index)
+        is_newly_infected = prob_tb_new > rng.rand(len(prob_tb_new))
         new_case = is_newly_infected[is_newly_infected].index
-        p[new_case, 'has_TB'] = 'Latent'
-        p[new_case, 'date_active_tb'] = now
+        p[new_case, 'has_tb'] = 'Latent'
+        p[new_case, 'date_latent_tb'] = now
 
         # 14% of newly infected latent cases become active directly
-        fast_progression = p.loc[(p.has_tb == 'Latent') & (p.date_active_tb == now)].sample(
+        fast_progression = p[(p.has_tb == 'Latent') & (p.date_latent_tb == now)].sample(
             frac=params['prop_fast_progressor']).index
         p[fast_progression, 'has_tb'] = 'Active'
         p[fast_progression, 'date_active_tb'] = now
 
-
         # slow progressors with latent TB become active
         # needs to be a random sample with weights for RR of active disease
         eff_prob_active_tb = pd.Series(params['progression_to_active_rate'], index=p[p.has_tb == 'Latent'].index)
-        # eff_prob_active_tb.loc[p.has_hiv] *= params['rr_tb_hiv']
+        # eff_prob_active_tb.loc[p.has_hiv] *= params['rr_tb_hiv'] # this will be further staged
         # eff_prob_active_tb.loc[p.on_art] *= params['rr_tb_art']
-        # eff_prob_active_tb.loc[p.on_art] *= params['rr_tb_malnourished']
-        # eff_prob_active_tb.loc[p.on_art] *= params['rr_tb_diabetes1']
-        # eff_prob_active_tb.loc[p.on_art] *= params['rr_tb_alcohol']
-        # eff_prob_active_tb.loc[p.on_art] *= params['rr_tb_smoking']
-        # eff_prob_active_tb.loc[p.on_art] *= params['rr_tb_pollution']
+        # eff_prob_active_tb.loc[p.is_malnourished] *= params['rr_tb_malnourished']
+        # eff_prob_active_tb.loc[p.has_diabetes1] *= params['rr_tb_diabetes1']
+        # eff_prob_active_tb.loc[p.high_alcohol] *= params['rr_tb_alcohol']
+        # eff_prob_active_tb.loc[p.is_smoker] *= params['rr_tb_smoking']
+        # eff_prob_active_tb.loc[p.high_pollution] *= params['rr_tb_pollution']
 
         prog_to_active = eff_prob_active_tb > rng.rand(len(eff_prob_active_tb))
         new_active_case = prog_to_active[prog_to_active].index
@@ -259,17 +279,5 @@ class TB_Event(RegularEvent, PopulationScopeEventMixin):
         p[new_active_case, 'date_active_tb'] = now
 
         # self-cure - move back from active to latent
-        self_cure_tb = p[p.has_tb == 'Active'].sample(frac=params['prob_self_cure']*params['self_cure']).index
+        self_cure_tb = p[p.has_tb == 'Active'].sample(frac=(params['prob_self_cure'] * params['self_cure'])).index
         p[self_cure_tb, 'has_tb'] = 'Latent'
-
-
-
-
-
-
-
-
-
-
-
-
--- tests/test_tb.py
+++ tests/test_tb.py
@@ -5,9 +5,9 @@ from tlo.test import TB
 
 
 def test_TB():
-    # Create a new simulation to orchestrate matters
-    sim = Simulation(start_date=Date(2018, 1, 1))
-    tb = TB.TB_baseline(name='tb')
+    # Create a new simulation
+    sim = Simulation(start_date=Date(2014, 1, 1))
+    tb = TB.tb_baseline(name='tb')
     sim.register(tb)
     assert sim.modules['tb'] is tb  # checks - this will cause error if false
 
@@ -15,25 +15,26 @@ def test_TB():
     sim.seed_rngs(1)
 
     # Create a population of 2 individuals
-    sim.make_initial_population(n=1000)
-    assert len(sim.population) == 1000
+    sim.make_initial_population(n=100000)
+    assert len(sim.population) == 100000
 
-    # Simulate for 24 months, but initialise_simulation has offset 12 months
-    assert sim.date == Date(2018, 1, 1)
-    sim.simulate(end_date=Date(2020, 1, 1))
+    # Decide how long to simulate - initialise_simulation has offset 12 months
+    assert sim.date == Date(2014, 1, 1)  # double check start date
+    sim.simulate(end_date=Date(2014, 12, 1))
 
     df = pd.DataFrame(sim.population.props)
     # df.to_csv('Q:/Thanzi la Onse/TB/test_dataframe.csv')
-    df.to_csv('/Users/Tara/Documents/test_dataframe.csv')
+    # df.to_csv('/Users/Tara/Documents/test_dataframe.csv')
 
-    print(df['has_TB'].value_counts())
+    print(df['has_tb'].value_counts())
 
 
 # check outputs in console
 
 outputs = pd.read_csv('/Users/Tara/Documents/test_dataframe.csv', header=0, sep=',')
 
+
 # outputs = pd.read_csv('Q:/Thanzi la Onse/TB/test_dataframe.csv', header=0, sep=',')
 #outputs.head(20)
 #outputs.describe(include='all')
-#outputs['has_TB'].value_counts()
+#outputs['has_tb'].value_counts()
