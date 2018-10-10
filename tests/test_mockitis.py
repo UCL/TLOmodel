031adaea9e50a27c2893fd7a475001a1082d4f4f
Tara Mangal <t.mangal@imperial.ac.uk>
2018-10-10 12:35:38 +0100

    tb methods modified

--- tests/test_mockitis.py
+++ tests/test_mockitis.py
@@ -1,34 +1,34 @@
+import pytest  # this is the library for testing
 
-import pytest
-from tlo import Date, DateOffset, Person, Simulation, Types
-from tlo.test import tb
+from tlo import Simulation, Date
 from tlo.methods import demography
+from tlo.test import mockitis
 
 path = '/Users/Tara/Desktop/TLO/Demography.xlsx'  # Edit this path so it points to your own copy of the Demography.xlsx file
 start_date = Date(2010, 1, 1)
-end_date = Date(2020, 1, 1)
+end_date = Date(2060, 1, 1)
 popsize = 1000
 
 
-@pytest.fixture
+@pytest.fixture  # this is a pytest feature
 def simulation():
     sim = Simulation(start_date=start_date)
-    core_module = demography.Demography(workbook_path=path)
-    tb_module = tb.tb_baseline()
-    sim.register(core_module)
-    sim.register(tb_module)
 
+    # create the modules
+    core_module = demography.Demography(workbook_path=path)
+    mockitis_module = mockitis.Mockitis()
+
+    # register the modules
+    sim.register(core_module)
+    sim.register(mockitis_module)
     return sim
 
 
-def test_tb_simulation(simulation):
+def test_mockitis_simulation(simulation):
     simulation.make_initial_population(n=popsize)
     simulation.simulate(end_date=end_date)
 
 
 if __name__ == '__main__':
     simulation = simulation()
-    test_tb_simulation(simulation)
-
-
-
+    test_mockitis_simulation(simulation)
