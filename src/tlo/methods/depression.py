3cc441a1040cf5f20b853a110fe8a1ae1283eeed
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2018-11-17 18:46:05 +0000

    as above

--- src/tlo/methods/depression.py
+++ src/tlo/methods/depression.py
@@ -17,6 +17,42 @@ class Depression(Module):
 
     # Module parameters
     PARAMETERS = {
+        'init_pr_depressed_m_age1519_no_chron_cond_wealth123': Parameter(
+            Types.REAL,
+            'initial probability of being depressed in male age1519 with no chronic condition with wealth level 123'),
+        'init_rp_depressed_f_not_rec_preg': Parameter(
+            Types.REAL,
+            'initial relative prevalence of being depressed in females not recently pregnant'),
+        'init_rp_depressed_f_rec_preg': Parameter(
+            Types.REAL,
+            'initial relative prevalence of being depressed in females recently pregnant'),
+        'init_rp_depressed_age2059': Parameter(
+            Types.REAL,
+            'initial relative prevalence of being depressed in 20-59 year olds'),
+        'init_rp_depressed_agege60': Parameter(
+            Types.REAL,
+            'initial relative prevalence of being depressed in 60 + year olds'),
+        'init_rp_depressed_chron_cond': Parameter(
+            Types.REAL,
+            'initial relative prevalence of being depressed in people with chroonic condition'),
+        'init_rp_depressed_wealth45': Parameter(
+            Types.REAL,
+            'initial relative prevalence of being depressed in people with wealth level 4 or 5'),
+        'init_rp_ever_depressed_per_year_older_m': Parameter(
+            Types.REAL,
+            'init_rp_ever_depressed_per_year_older_m if not currently depressed'),
+        'init_rp_ever_depressed_per_year_older_f': Parameter(
+            Types.REAL,
+            'init_rp_ever_depressed_per_year_older_f if nor currently depressed'),
+        'init_pr_on_antidepr_curr_depressed': Parameter(
+            Types.REAL,
+            'init_pr_on_antidepr_curr_depressed'),
+        'init_rp_never_depressed': Parameter(
+            Types.REAL,
+            'init_rp_never_depressed'),
+        'init_rp_ever_depressed_not_current': Parameter(
+            Types.REAL,
+            'init_rp_ever_depressed_not_current'),
         'base_3m_prob_depression': Parameter(
             Types.REAL,
             'Base incidence of depression over a 3 month period'),
@@ -91,6 +127,13 @@ class Depression(Module):
             Types.REAL, 'Base probability for recovering from this bout of depression (if relevant)'),
     }
 
+    def __init__(self):
+        super().__init__()
+# todo update this below
+        self.store = {'alive': []}
+        self.o_prop_urban = {'prop_urban': []}
+
+
     def read_parameters(self, data_folder):
         """Read parameter values from file, if required.
 
@@ -99,25 +142,37 @@ class Depression(Module):
         :param data_folder: path of a folder supplied to the Simulation containing data files.
           Typically modules would read a particular file within here.
         """
-        params = self.parameters  # To save typing!
-        params['base_3m_prob_depression'] = 0.0007
-        params['rr_depression_wealth45'] = 3
-        params['rr_depression_chron_cond'] = 1.25
-        params['rr_depression_pregnancy'] = 3
-        params['rr_depression_female'] = 1.5
-        params['rr_depression_prev_epis'] = 50
-        params['rr_depression_prev_epis_on_antidepr'] = 30
-        params['rr_depression_age_15_20'] = 1
-        params['rr_depression_age_60plus'] = 3
-        params['depression_resolution_rates'] = [0.2, 0.3, 0.5, 0.7, 0.95]
-        params['rr_resol_depress_chron_cond'] = 0.5
-        params['rr_resol_depress_on_antidepr'] = 1.5
-        params['rate_init_antidep'] = 0.03
-        params['rate_stop_antidepr'] = 0.70
-        params['rate_default_antidepr'] = 0.20
-        params['prob_3m_suicide_depression_m'] = 0.001
-        params['prob_3m_suicide_depression_f'] = 0.0005
-        params['prob_3m_selfharm_depression'] = 0.002
+
+        self.parameters['init_pr_depressed_m_age1519_no_chron_cond_wealth123'] = 0.1
+        self.parameters['init_rp_depressed_f_not_rec_preg'] = 1
+        self.parameters['init_rp_depressed_f_rec_preg'] = 1
+        self.parameters['init_rp_depressed_age2059'] = 1
+        self.parameters['init_rp_depressed_agege60'] = 1
+        self.parameters['init_rp_depressed_chron_cond'] = 1
+        self.parameters['init_rp_depressed_wealth45'] = 1
+        self.parameters['init_rp_ever_depressed_per_year_older_m'] = 0.007
+        self.parameters['init_rp_ever_depressed_per_year_older_f'] = 0.009
+        self.parameters['init_pr_on_antidepr_curr_depressed'] = 0.15
+        self.parameters['init_rp_never_depressed'] = 0
+        self.parameters['init_rp_ever_depressed_not_current'] = 1.5
+        self.parameters['base_3m_prob_depression'] = 0.0007
+        self.parameters['rr_depression_wealth45'] = 3
+        self.parameters['rr_depression_chron_cond'] = 1.25
+        self.parameters['rr_depression_pregnancy'] = 3
+        self.parameters['rr_depression_female'] = 1.5
+        self.parameters['rr_depression_prev_epis'] = 50
+        self.parameters['rr_depression_prev_epis_on_antidepr'] = 30
+        self.parameters['rr_depression_age_15_20'] = 1
+        self.parameters['rr_depression_age_60plus'] = 3
+        self.parameters['depression_resolution_rates'] = [0.2, 0.3, 0.5, 0.7, 0.95]
+        self.parameters['rr_resol_depress_chron_cond'] = 0.5
+        self.parameters['rr_resol_depress_on_antidepr'] = 1.5
+        self.parameters['rate_init_antidep'] = 0.03
+        self.parameters['rate_stop_antidepr'] = 0.70
+        self.parameters['rate_default_antidepr'] = 0.20
+        self.parameters['prob_3m_suicide_depression_m'] = 0.001
+        self.parameters['prob_3m_suicide_depression_f'] = 0.0005
+        self.parameters['prob_3m_selfharm_depression'] = 0.002
 
     def initialise_population(self, population):
         """Set our property values for the initial population.
@@ -128,10 +183,84 @@ class Depression(Module):
 
         :param population: the population of individuals
         """
-        # No-one is initially depressed (this is wrong, but we don't have data otherwise)
-        population.is_depressed = False
-        population.ever_depressed = False
-        # Other properties are left uninitialised, i.e. null/NaN/NaT as appropriate
+
+        df = population.props  # a shortcut to the data-frame storing data for individuals
+        df['de_depressed'] = False
+        df['de_date_init_last_depression'] = pd.NaT
+        df['de_date_depression_resolved'] = pd.NaT
+        df['de_non_fatal_self_harm_event'] = False
+        df['de_suicide'] = False
+        df['de_on_antidepr'] = False
+        df['de_ever_depressed'] = False
+        df['de_prob_3m_resol_depression'] = False
+
+        #  this below calls the age dataframe / call age.years to get age in years
+        age = population.age
+
+    # current depression ;
+    # dataframe sex  age group  de_chron_cond  recent_preg  wealth   pr_depressed
+    depressed_lookup = pd.DataFrame([('M', '15-19', 0.01),
+                               ('M', '20-24', 0.04),
+                               ('M', '25-29', 0.04),
+                               ('M', '30-34', 0.04),
+                               ('M', '35-39', 0.04),
+                               ('M', '40-44', 0.06),
+                               ('M', '45-49', 0.06),
+                               ('M', '50-54', 0.06),
+                               ('M', '55-59', 0.06),
+                               ('M', '60-64', 0.06),
+                               ('M', '65-69', 0.06),
+                               ('M', '70-74', 0.06),
+                               ('M', '75-79', 0.06),
+                               ('M', '80-84', 0.06),
+                               ('M', '85-89', 0.06),
+                               ('M', '90-94', 0.06),
+                               ('M', '95-99', 0.06),
+                               ('M', '100+', 0.06),
+
+                               ('F', '15-19', 0.002),
+                               ('F', '20-24', 0.002),
+                               ('F', '25-29', 0.002),
+                               ('F', '30-34', 0.002),
+                               ('F', '35-39', 0.002),
+                               ('F', '40-44', 0.002),
+                               ('F', '45-49', 0.002),
+                               ('F', '50-54', 0.002),
+                               ('F', '55-59', 0.002),
+                               ('F', '60-64', 0.002),
+                               ('F', '65-69', 0.002),
+                               ('F', '70-74', 0.002),
+                               ('F', '75-79', 0.002),
+                               ('F', '80-84', 0.002),
+                               ('F', '85-89', 0.002),
+                               ('F', '90-94', 0.002),
+                               ('F', '95-99', 0.002),
+                               ('F', '100+', 0.002)],
+                              columns=['sex', 'age_range', 'p_tob'])
+
+    # join the population dataframe with age information (we need them both together)
+    df_with_age = df.loc[gte_15, ['sex', 'li_wealth']].merge(age, left_index=True, right_index=True, how='inner')
+    assert len(df_with_age) == len(gte_15)  # check we have the same number of individuals after the merge
+
+    # join the population-with-age dataframe with the tobacco use lookup table (join on sex and age_range)
+    tob_probs = df_with_age.merge(tob_lookup, left_on=['sex', 'age_range'], right_on=['sex', 'age_range'],
+                                  how='left')
+
+    assert np.array_equal(tob_probs.years_exact, df_with_age.years_exact)
+    # check the order of individuals is the same by comparing exact ages
+    assert tob_probs.p_tob.isna().sum() == 0  # ensure we found a p_tob for every individual
+
+    # each individual has a baseline probability
+    # multiply this probability by the wealth level. wealth is a category, so convert to integer
+    tob_probs = tob_probs['li_wealth'].astype(int) * tob_probs['p_tob']
+
+    # we now have the probability of tobacco use for each individual where age >= 15
+    # draw a random number between 0 and 1 for all of them
+    random_draw = self.rng.random_sample(size=len(gte_15))
+
+    # decide on tobacco use based on the individual probability is greater than random draw
+    # this is a list of True/False. assign to li_tob
+    df.loc[gte_15, 'li_tob'] = (random_draw < tob_probs.values)
 
     def initialise_simulation(self, sim):
         """Get ready for simulation start.
