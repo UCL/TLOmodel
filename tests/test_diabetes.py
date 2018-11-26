d0611eb1d810ab64911690847f87dbd32622d32e
Mikaela Smit <mikaela.smit@gmail.com>
2018-11-26 12:24:11 +0000

    Small clean-ups in CVD model

--- tests/test_diabetes.py
+++ tests/test_diabetes.py
@@ -1,7 +1,7 @@
 import pytest
 
 from tlo import Simulation, Date
-from tlo.methods import demography#, cvd
+from tlo.methods import demography, CVD
 
 path = '/Users/mc1405/Dropbox/Projects - ongoing/Malawi Project/Model/Demography.xlsx'  # Edit this path so it points to your own copy of the Demography.xlsx file
 start_date = Date(2010, 1, 1)
@@ -13,9 +13,9 @@ popsize = 1000
 def simulation():
     sim = Simulation(start_date=start_date)
     core_module = demography.Demography(workbook_path=path)
-    #CVD_module = cvd.CVD()               # This will load method for high cholesterol
+    CVD_module = cvd.CVD()               # This will load method for high cholesterol
     sim.register(core_module)
-    #sim.register(cvd_module)                        # This will register method for high cholesterol
+    sim.register(cvd_module)                        # This will register method for high cholesterol
     return sim
 
 
