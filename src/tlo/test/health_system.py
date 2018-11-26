8bbdfcf3ba28ecf830e22531a7a75c1f3738e317
tdm32 <t.mangal@imperial.ac.uk>
2018-11-26 12:43:01 +0000

    HIV testing in health_system module

--- src/tlo/test/health_system.py
+++ src/tlo/test/health_system.py
@@ -3,55 +3,114 @@ A skeleton template for disease methods.
 """
 import pandas as pd
 
-from tlo import Module, Parameter, Types
+from tlo import DateOffset, Module, Parameter, Property, Types
+from tlo.events import PopulationScopeEventMixin, RegularEvent
 
 
 class health_system(Module):
-    """ determines availability of ART for HIV+ dependent on UNAIDS coverage estimates
+    """ routinely tests proportion of the population and
+    determines availability of ART for HIV+ dependent on UNAIDS coverage estimates
     """
 
     def __init__(self, name=None, workbook_path=None):
         super().__init__(name)
         self.workbook_path = workbook_path
-        self.store = {'Time': [], 'Number_treated': []}
+        self.store = {'Time': [], 'Number_tested': [], 'Number_treated': []}
 
     PARAMETERS = {
+        'testing_coverage': Parameter(Types.REAL, 'proportion of population tested'),
         'art_coverage': Parameter(Types.DATA_FRAME, 'estimated ART coverage')
     }
 
-    # no PROPERTIES
+    PROPERTIES = {
+        'ever_tested': Property(Types.BOOL, 'ever had a hiv test'),
+        'date_tested': Property(Types.DATE, 'date of hiv test'),
+        'hiv_diagnosed': Property(Types.BOOL, 'hiv+ and tested')
+    }
 
     def read_parameters(self, data_folder):
-        """Read parameter values from file, if required.
-
-        Here we do nothing.
-
-        :param data_folder: path of a folder supplied to the Simulation containing data files.
-          Typically modules would read a particular file within here.
-        """
+        params = self.parameters
+        params['testing_coverage'] = 0.2  # dummy value
 
         # TODO: check the sheet name
-        self.parameters['art_coverage'] = pd.read_excel(self.workbook_path,
-                                                        sheet_name='art_coverage')
+        # self.parameters['art_coverage'] = pd.read_excel(self.workbook_path,
+        #                                                 sheet_name='art_coverage')
 
     def initialise_population(self, population):
-        pass
+        """ set the default values for the new fields
+        """
+        df = population.props
+
+        df['ever_tested'] = False  # default: no individuals tested
+        df['date_tested'] = pd.NaT
+        df['hiv_diagnosed'] = False
+
 
     def initialise_simulation(self, sim):
-        pass
+
+        sim.schedule_event(TestingEvent(self), sim.date + DateOffset(months=12))
+
+        # add an event to log to screen
+        sim.schedule_event(HealthSystemLoggingEvent(self), sim.date + DateOffset(months=12))
+
 
     def on_birth(self, mother, child):
         pass
 
-    def treatment_available(self, population):
 
+class TestingEvent(RegularEvent, PopulationScopeEventMixin):
+    """A skeleton class for an event
+    Regular events automatically reschedule themselves at a fixed frequency,
+    and thus implement discrete timestep type behaviour. The frequency is
+    specified when calling the base class constructor in our __init__ method.
+    """
+
+    def __init__(self, module):
+        """One line summary here
+
+        """
+        super().__init__(module, frequency=DateOffset(months=12))  # every 12 months
+        # make sure any rates are annual if frequency of event is annual
+
+    def apply(self, population):
+        """Apply this event to the population.
+        :param population: the current population
+        """
+
+        params = self.module.parameters
         now = self.sim.date
         df = population.props
 
-        # select all HIV+
+        # get a list of random numbers between 0 and 1 for the whole population
+        random_draw = self.sim.rng.random_sample(size=len(df))
 
-        # extract probability of art by age, sex, year
+        # probability of HIV testing
+        testing_index = df.index[(random_draw < params['testing_coverage']) & ~df.ever_tested & df.is_alive]
+        testing_diagnosed_index = df.index[
+            (random_draw < params['testing_coverage']) & ~df.ever_tested & df.is_alive & df.has_hiv]
+        print('testing_index: ', testing_index)
+        print('diagnosed_index: ', testing_diagnosed_index)
 
-        # if just using probability then how to assign by longest time infected?
+        df.loc[testing_index, 'ever_tested'] = True
+        df.loc[testing_index, 'date_tested'] = now
+        df.loc[testing_diagnosed_index, 'hiv_diagnosed'] = True
 
-        pass
+
+class HealthSystemLoggingEvent(RegularEvent, PopulationScopeEventMixin):
+    def __init__(self, module):
+        """ produce some outputs to check
+        """
+        # run this event every 12 months (every year)
+        self.repeat = 12
+        super().__init__(module, frequency=DateOffset(months=self.repeat))
+
+    def apply(self, population):
+        # get some summary statistics
+        df = population.props
+
+        mask = (df['date_tested'] > self.sim.date - DateOffset(months=self.repeat))
+        recently_tested = mask.sum()
+
+        self.module.store['Time'].append(self.sim.date)
+        self.module.store['Number_tested'].append(recently_tested)
+
