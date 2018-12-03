6e31a2d697596988ba01f9953f2dde003d52c22d
tdm32 <t.mangal@imperial.ac.uk>
2018-12-03 16:05:13 +0000

    included relative infectiousness on art included relative susceptibility to hiv by intervention / risk group behaviour modification interventions working

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,7 +3,7 @@ import pytest  # this is the library for testing
 import matplotlib.pyplot as plt
 
 from tlo import Date, Simulation
-from tlo.methods import demography, antiretroviral_therapy, hiv_infection, health_system, health_system_tb, tb, male_circumcision
+from tlo.methods import demography, antiretroviral_therapy, hiv_infection, health_system, health_system_tb, tb, male_circumcision, hiv_behaviour_change
 
 
 
@@ -32,21 +32,26 @@ popsize = 50000
 @pytest.fixture
 def simulation():
     sim = Simulation(start_date=start_date)
+
+    #  call modules
     core_module = demography.Demography(workbook_path=path_dem)
     hiv_module = hiv_infection.hiv(workbook_path=path_hiv)
-    tb_module = tb.tb_baseline(workbook_path=path_tb)
-    hs_module = health_system.health_system(workbook_path=path_hs)
     art_module = antiretroviral_therapy.art(workbook_path=path_hs)
-    hs_tb_module = health_system_tb.health_system_tb()
+    hs_module = health_system.health_system(workbook_path=path_hs)
     circumcision_module = male_circumcision.male_circumcision()
+    behavioural_module = hiv_behaviour_change.BehaviourChange()
+    tb_module = tb.tb_baseline(workbook_path=path_tb)
+    hs_tb_module = health_system_tb.health_system_tb()
 
+    #  register modules
     sim.register(core_module)
     sim.register(hiv_module)
-    sim.register(tb_module)
-    sim.register(hs_module)
     sim.register(art_module)
-    sim.register(hs_tb_module)
+    sim.register(hs_module)
     sim.register(circumcision_module)
+    sim.register(behavioural_module)
+    sim.register(tb_module)
+    sim.register(hs_tb_module)
 
     return sim
 
@@ -75,6 +80,12 @@ testing_dates = simulation.modules['health_system'].store['Time']
 deaths_art = simulation.modules['art'].store['Number_dead_art']
 time_deaths_art = simulation.modules['art'].store['Time']
 
+time_circum = simulation.modules['male_circumcision'].store['Time']
+prop_circum = simulation.modules['male_circumcision'].store['proportion_circumcised']
+
+time_behav = simulation.modules['BehaviourChange'].store['Time']
+prop_counselled = simulation.modules['BehaviourChange'].store['Proportion_hiv_counselled']
+
 active_tb = simulation.modules['tb_baseline'].store['Total_active_tb']
 coinfected = simulation.modules['tb_baseline'].store['Total_co-infected']
 tb_deaths = simulation.modules['tb_baseline'].store['TB_deaths']
@@ -83,9 +94,6 @@ time2 = simulation.modules['tb_baseline'].store['Time']
 
 time_test_tb = simulation.modules['health_system_tb'].store['Time']
 tb_tests = simulation.modules['health_system_tb'].store['Number_tested_tb']
-
-time_circum = simulation.modules['male_circumcision'].store['Time']
-prop_circum = simulation.modules['male_circumcision'].store['proportion_circumcised']
 
 
 plt.figure(1)
@@ -116,10 +124,11 @@ plt.ylabel('Number of tests')
 
 plt.subplot(224)
 plt.plot(time_circum, prop_circum)
+plt.plot(time_behav, prop_counselled)
 plt.ylim(bottom=0)
-plt.legend(['circumcised'], loc='upper right')
+plt.legend(['circumcised', 'counselled'], loc='upper right')
 plt.xticks(rotation=45)
-plt.ylabel('Proportion circumcised')
+plt.ylabel('Proportion')
 
 plt.show()
 
