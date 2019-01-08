ee578383888c8840694fac9a74932bfad7412d49
Asif Tamuri <tamuri@gmail.com>
2019-01-08 10:04:20 +0000

    use a better name

--- src/tlo/analysis/utils.py
+++ src/tlo/analysis/utils.py
@@ -18,7 +18,7 @@ def parse_line(line):
     It returns the dictionary:
         { 'logger': <logger name>,
           'sim_date': <simulation date>,
-          'name': <the name/type of this log entry>,
+          'key': <the key of this log entry>,
           'object': <the logged python object>
         }
 
@@ -30,7 +30,7 @@ def parse_line(line):
     info = {
         'logger': parts[1],
         'sim_date': parts[2],
-        'name': parts[3],
+        'key': parts[3],
         'object': literal_eval(parts[4])
     }
     logger.debug('%s', info)
@@ -39,20 +39,30 @@ def parse_line(line):
 
 def parse_output(filepath):
     """
-    Parses logged output from a TLO run and create Pandas dataframes for analysis
+    Parses logged output from a TLO run and create Pandas dataframes for analysis.
 
-    The dictionary returned has the following format:
+    The input lines follow the format:
+    INFO|<logger name>|<simulation datestamp>|<log key>|<python list or dictionary>
+
+    e.g.
+
+    INFO|tlo.methods.demography|2010-11-02 23:00:59.111968|on_birth|{'mother': 17, 'child': 50}
+    INFO|tlo.methods.demography|2011-01-01 00:00:00|population|{'total': 51, 'male': 21, 'female': 30}
+    INFO|tlo.methods.demography|2011-01-01 00:00:00|age_range_m|[5, 4, 1, 1, 1, 2, 1, 2, 2, 1, 1, 0]
+    INFO|tlo.methods.demography|2011-01-01 00:00:00|age_range_f|[4, 7, 5, 1, 5, 1, 2, 0, 1, 2, 0, 1]
+
+    The dictionary returned has the format:
     {
         <logger 1 name>: {
-                           <log key 1 name>: <pandas dataframe>,
-                           <log key 2 name>: <pandas dataframe>,
-                           <log key 3 name>: <pandas dataframe>
+                           <log key 1>: <pandas dataframe>,
+                           <log key 2>: <pandas dataframe>,
+                           <log key 3>: <pandas dataframe>
                          },
 
         <logger 2 name>: {
-                           <log key 4 name>: <pandas dataframe>,
-                           <log key 5 name>: <pandas dataframe>,
-                           <log key 6 name>: <pandas dataframe>
+                           <log key 4>: <pandas dataframe>,
+                           <log key 5>: <pandas dataframe>,
+                           <log key 6>: <pandas dataframe>
                          },
         ...
     }
@@ -73,7 +83,7 @@ def parse_output(filepath):
                 if i['logger'] not in o:
                     o[i['logger']] = dict()
                 # add a dataframe for the name/key of this log entry, if required
-                if i['name'] not in o[i['logger']]:
+                if i['key'] not in o[i['logger']]:
                     # if the logged data is a list, it doesn't have column names
                     if isinstance(i['object'], list):
                         # create column names for each entry in the list
@@ -82,9 +92,9 @@ def parse_output(filepath):
                         # create column names from the keys of the dictionary
                         columns = list(i['object'].keys())
                     columns.insert(0, 'date')
-                    o[i['logger']][i['name']] = pd.DataFrame(columns=columns)
+                    o[i['logger']][i['key']] = pd.DataFrame(columns=columns)
 
-                df = o[i['logger']][i['name']]
+                df = o[i['logger']][i['key']]
 
                 # create a new row to append to the dataframe, add the simulation date
                 if isinstance(i['object'], dict):
@@ -92,12 +102,12 @@ def parse_output(filepath):
                     row['date'] = i['sim_date']
                 elif isinstance(i['object'], list):
                     if len(df.columns) - 1 != len(i['object']):
-                        logger.warning('List to dataframe %s, number of columns do not match', i['name'])
+                        logger.warning('List to dataframe %s, number of columns do not match', i['key'])
                     # add list to columns (skip first column, which is date)
                     row = dict(zip(df.columns[1:], i['object']))
                     row['date'] = i['sim_date']
                 else:
                     raise ValueError('Cannot handle log object of type %s' % type(i['object']))
                 # append the new row to the dataframe for this logger & log name
-                o[i['logger']][i['name']] = df.append(row, ignore_index=True)
+                o[i['logger']][i['key']] = df.append(row, ignore_index=True)
     return o
