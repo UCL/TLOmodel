dc219c5a29da3414d12005512f9944edbabda536
Asif Tamuri <tamuri@gmail.com>
2019-01-08 10:56:29 +0000

    move logging setup to simulation constructor

--- src/tlo/__init__.py
+++ src/tlo/__init__.py
@@ -5,9 +5,6 @@ We import our key classes so they're available in the main namespace.
 
 Pandas' Timestamp is used as our main date type (even though it has nanosecond resolution...)
 """
-import logging
-import sys
-
 from pandas import Timestamp as Date  # noqa
 from pandas.tseries.offsets import DateOffset  # noqa
 
@@ -16,11 +13,3 @@ from .population import Population  # noqa
 from .simulation import Simulation  # noqa
 
 __version__ = '0.1.0'
-
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(levelname)s|%(name)s|%(message)s')
-handler.setFormatter(formatter)
-logging.getLogger().addHandler(handler)
-
-logging.basicConfig(level=logging.DEBUG)
