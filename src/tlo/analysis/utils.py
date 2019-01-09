e0ae739fb8ffc5974b5eb0ad60b85b312a13288d
Asif Tamuri <tamuri@gmail.com>
2019-01-09 21:17:44 +0000

    Split apart file-reading and line handling

--- src/tlo/analysis/utils.py
+++ src/tlo/analysis/utils.py
@@ -37,19 +37,35 @@ def parse_line(line):
     return info
 
 
-def parse_output(filepath):
+def parse_log_file(filepath):
+    """
+    Parses logged output from a TLO run and create Pandas dataframes for analysis. See
+    parse_output() for details
+
+    :param filepath: file path to log file
+    :return: dictionary of parsed log data
+    """
+    with open(filepath) as log_file:
+        parse_output(log_file.readlines())
+
+
+def parse_output(list_of_log_lines):
     """
     Parses logged output from a TLO run and create Pandas dataframes for analysis.
 
-    The input lines follow the format:
+    Use parse_log_file() wrapper if required
+
+    Each input line follows the format:
     INFO|<logger name>|<simulation datestamp>|<log key>|<python list or dictionary>
 
     e.g.
 
-    INFO|tlo.methods.demography|2010-11-02 23:00:59.111968|on_birth|{'mother': 17, 'child': 50}
-    INFO|tlo.methods.demography|2011-01-01 00:00:00|population|{'total': 51, 'male': 21, 'female': 30}
-    INFO|tlo.methods.demography|2011-01-01 00:00:00|age_range_m|[5, 4, 1, 1, 1, 2, 1, 2, 2, 1, 1, 0]
-    INFO|tlo.methods.demography|2011-01-01 00:00:00|age_range_f|[4, 7, 5, 1, 5, 1, 2, 0, 1, 2, 0, 1]
+    [
+    'INFO|tlo.methods.demography|2010-11-02 23:00:59.111968|on_birth|{'mother': 17, 'child': 50}',
+    'INFO|tlo.methods.demography|2011-01-01 00:00:00|population|{'total': 51, 'male': 21, 'female': 30}',
+    'INFO|tlo.methods.demography|2011-01-01 00:00:00|age_range_m|[5, 4, 1, 1, 1, 2, 1, 2, 2, 1, 1, 0]',
+    'INFO|tlo.methods.demography|2011-01-01 00:00:00|age_range_f|[4, 7, 5, 1, 5, 1, 2, 0, 1, 2, 0, 1]',
+    ]
 
     The dictionary returned has the format:
     {
@@ -67,47 +83,45 @@ def parse_output(filepath):
         ...
     }
 
-    :param filepath: the full filepath to logged output file
+    :param list_of_log_lines: a list of log lines in the required format
     :return: a dictionary holding logged data as Python objects
     """
     o = dict()
 
-    # read logging lines from the file
-    with open(filepath) as log_file:
-        # for each logged line
-        for line in log_file:
-            # we only parse 'INFO' lines
-            if line.startswith('INFO'):
-                i = parse_line(line.strip())
-                # add a dictionary for the logger name, if required
-                if i['logger'] not in o:
-                    o[i['logger']] = dict()
-                # add a dataframe for the name/key of this log entry, if required
-                if i['key'] not in o[i['logger']]:
-                    # if the logged data is a list, it doesn't have column names
-                    if isinstance(i['object'], list):
-                        # create column names for each entry in the list
-                        columns = ['col_%d' % x for x in range(0, len(i['object']))]
-                    else:
-                        # create column names from the keys of the dictionary
-                        columns = list(i['object'].keys())
-                    columns.insert(0, 'date')
-                    o[i['logger']][i['key']] = pd.DataFrame(columns=columns)
-
-                df = o[i['logger']][i['key']]
-
-                # create a new row to append to the dataframe, add the simulation date
-                if isinstance(i['object'], dict):
-                    row = i['object']
-                    row['date'] = i['sim_date']
-                elif isinstance(i['object'], list):
-                    if len(df.columns) - 1 != len(i['object']):
-                        logger.warning('List to dataframe %s, number of columns do not match', i['key'])
-                    # add list to columns (skip first column, which is date)
-                    row = dict(zip(df.columns[1:], i['object']))
-                    row['date'] = i['sim_date']
+    # for each logged line
+    for line in list_of_log_lines:
+        # we only parse 'INFO' lines
+        if line.startswith('INFO'):
+            i = parse_line(line.strip())
+            # add a dictionary for the logger name, if required
+            if i['logger'] not in o:
+                o[i['logger']] = dict()
+            # add a dataframe for the name/key of this log entry, if required
+            if i['key'] not in o[i['logger']]:
+                # if the logged data is a list, it doesn't have column names
+                if isinstance(i['object'], list):
+                    # create column names for each entry in the list
+                    columns = ['col_%d' % x for x in range(0, len(i['object']))]
                 else:
-                    raise ValueError('Cannot handle log object of type %s' % type(i['object']))
-                # append the new row to the dataframe for this logger & log name
-                o[i['logger']][i['key']] = df.append(row, ignore_index=True)
+                    # create column names from the keys of the dictionary
+                    columns = list(i['object'].keys())
+                columns.insert(0, 'date')
+                o[i['logger']][i['key']] = pd.DataFrame(columns=columns)
+
+            df = o[i['logger']][i['key']]
+
+            # create a new row to append to the dataframe, add the simulation date
+            if isinstance(i['object'], dict):
+                row = i['object']
+                row['date'] = i['sim_date']
+            elif isinstance(i['object'], list):
+                if len(df.columns) - 1 != len(i['object']):
+                    logger.warning('List to dataframe %s, number of columns do not match', i['key'])
+                # add list to columns (skip first column, which is date)
+                row = dict(zip(df.columns[1:], i['object']))
+                row['date'] = i['sim_date']
+            else:
+                raise ValueError('Cannot handle log object of type %s' % type(i['object']))
+            # append the new row to the dataframe for this logger & log name
+            o[i['logger']][i['key']] = df.append(row, ignore_index=True)
     return o
