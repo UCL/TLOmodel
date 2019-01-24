455889a91f182e4a04e18e241dfe2457bfa87d95
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2019-01-24 18:50:15 +0000

    reduced code for setting of initial education level

--- tests/test_lifestyle.py
+++ tests/test_lifestyle.py
@@ -9,7 +9,7 @@ from tlo.test import random_birth
 path = 'C:/Users/Andrew Phillips/Dropbox/Thanzi la Onse/05 - Resources\Demographic data\Demography_WorkingFile_Complete.xlsx'
 # Edit this path so it points to your own copy of the Demography.xlsx file
 start_date = Date(2010, 1, 1)
-end_date = Date(2012, 1, 1)
+end_date = Date(2015, 4, 1)
 popsize = 1000
 
 
