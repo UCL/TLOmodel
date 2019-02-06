417b3188b109dcf7f52de734f9f2266e4f1b08c7
Asif Tamuri <tamuri@gmail.com>
2019-02-06 22:44:43 +0000

    use demog worksheet in resources, add check for column dtypes

--- tests/test_lifestyle.py
+++ tests/test_lifestyle.py
@@ -1,26 +1,29 @@
 import logging
+import os
 
 import pytest
 
 from tlo import Simulation, Date
 from tlo.methods import demography, lifestyle
-from tlo.test import random_birth
 
-path = 'C:/Users/Andrew Phillips/Dropbox/Thanzi la Onse/05 - Resources\Demographic data\Demography_WorkingFile_Complete.xlsx'
-# Edit this path so it points to your own copy of the Demography.xlsx file
+workbook_name = 'demography.xlsx'
+
 start_date = Date(2010, 1, 1)
 end_date = Date(2015, 4, 1)
 popsize = 1000
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def simulation():
+    demography_workbook = os.path.join(os.path.dirname(__file__),
+                                       'resources',
+                                       workbook_name)
     sim = Simulation(start_date=start_date)
-    core_module = demography.Demography(workbook_path=path)
-    lifestyle_module = lifestyle.Lifestyle()
-    sim.register(core_module)
-    sim.register(lifestyle_module)
+    sim.seed_rngs(1)
+    sim.register(demography.Demography(workbook_path=demography_workbook))
+    sim.register(lifestyle.Lifestyle())
 
+    # turn off demography module logging
     logging.getLogger('tlo.methods.demography').setLevel(logging.WARNING)
 
     return sim
@@ -29,6 +32,13 @@ def simulation():
 def test_lifestyle_simulation(simulation):
     simulation.make_initial_population(n=popsize)
     simulation.simulate(end_date=end_date)
+
+
+def test_dypes(simulation):
+    # check types of columns
+    df = simulation.population.props
+    orig = simulation.population.new_row
+    assert (df.dtypes == orig.dtypes).all()
 
 
 if __name__ == '__main__':
