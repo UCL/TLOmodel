53fe854fe91af50a43b70f4d9f83ac3ed0302039
Timothy Colbourn <timcolbourn@hotmail.com>
2019-02-13 21:27:30 +0000

    Have now got the baseline prevalences of each of the 11 contraceptive methods as separate columns with only one row for each age in years. I have also successfully merged these into the df (in an object called df_new) for the females1549 subset and isolated these as contraceptive_probs. I'm just stuck on using these contraceptive_probs to assign 'contraception' method. It hangs on line 103 of contraception.py with this error: ValueError: object too deep for desired array I don't know why as I think I am mapping the random probability of each of the 11 methods within 'contraception' to the 11 probability columns I've generated. Any help much appreciated!
    Former-commit-id: 24649c761f131b6130972d6e70db9bd5670b797f

---
+++ tests/test_contraception.py
@@ -0,0 +1,52 @@
+import os
+import time
+
+import pytest
+
+from tlo import Date, Simulation
+from tlo.methods import demography, contraception
+
+workbook_name = 'contraception.xlsx'
+workbook2_name = 'demography.xlsx'
+
+start_date = Date(2010, 1, 1)
+end_date = Date(2015, 1, 1)
+popsize = 50
+
+
+@pytest.fixture(scope='module')
+def simulation():
+    contraception_workbook = os.path.join(os.path.dirname(__file__),
+                                       'resources',
+                                       workbook_name)
+    demography_workbook = os.path.join(os.path.dirname(__file__),
+                                       'resources',
+                                       workbook2_name)
+
+    sim = Simulation(start_date=start_date)
+    demography_module = demography.Demography(workbook_path=demography_workbook)
+    sim.register(demography_module)
+    core_module = contraception.Contraception(workbook_path=contraception_workbook)
+    sim.register(core_module)
+    sim.seed_rngs(0)
+    return sim
+
+
+def test_run(simulation):
+    simulation.make_initial_population(n=popsize)
+    simulation.simulate(end_date=end_date)
+
+
+def test_dypes(simulation):
+    # check types of columns
+    df = simulation.population.props
+    orig = simulation.population.new_row
+    assert (df.dtypes == orig.dtypes).all()
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    simulation = simulation()
+    test_run(simulation)
+    t1 = time.time()
+    print('Time taken', t1 - t0)
