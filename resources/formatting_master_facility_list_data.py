b05aa0a35c62f739409924eb012ead0366def863
Tim Hallett <tbhallett@gmail.com>
2019-03-12 21:26:36 +0000

    working on master facility list data for mapping between villages and heath facilities
    Former-commit-id: 5feec57a1850d846087610f6bb08c1d5df873775

--- resources/formatting_master_facility_list_data.py
+++ resources/formatting_master_facility_list_data.py
@@ -13,23 +13,7 @@ wb=wb.drop(columns='Date')
 
 wb['Facility_ID']=np.arange(len(wb))
 
-# Save output file for information about facilities
-wb.to_csv(outputfile)
-
-#--------
-
-# Make the file that maps the connections between villages and the health facilities
-# This will have one row per village...
-#                               ... and columns of (Community Health Worker,Nearest Hospital,District Hospital,Referral Hospital)
-
-
-
-
-
-
-# Reduce the number of types of facilities
-
-# 1) Combine "Dispensaries" and "Health Centres"; Combine 'Health Post', 'Outreach', and 'Village Clinic' as "Community Health Worker" (= the Community Health Worker under a tree in each village)
+# Combine "Dispensaries" and "Health Centres"; Combine 'Health Post', 'Outreach', and 'Village Clinic' as "Community Health Worker" (= the Community Health Worker under a tree in each village)
 wb['Facility Type']=wb['Facility Type'].map({
             'Dispensary':'Health Centre',
             'Health Post': 'Community Health Worker',
@@ -39,123 +23,144 @@ wb['Facility Type']=wb['Facility Type'].map({
             'Health Centre': 'Health Centre'
         })
 
-# (now there are only three type: hospital, community health workers, health centre)
-wb.groupby(by='Facility Type').count()
-
-
-# 2) Label the referral Hospital as Refrral Hospitals:
+# Label the referral Hospital as Refrral Hospitals:
 wb.loc[wb['Facility Name']=='QUEEN ELIZABETH','Facility Type'] = 'Referral Hospital'
 wb.loc[wb['Facility Name']=='KAMUZU CENTRAL HOSPITAL','Facility Type'] = 'Referral Hospital'
 
-
 # 3) Label the district hopsitals
-
 wb.loc[wb['Facility Name'].str.contains(' DH'),'Facility Type']='District Hospital'
 
 # look at number of district hospitals per distirct (check it's 1 per district)
 wb.loc[wb['Facility Type']=='District Hospital',['District','Facility Type']].groupby(by=['District']).count()
 
+# Save output file for information about facilities
+wb.to_csv(outputfile)
 
+#--------
 
-
-
-
-# 3) Add in linkage to district hospitals; nearest (one) hospital only
+# Make the file that maps the connections between villages and the health facilities
+# When used we will .loc onto this to find (CHW (Community Health Worker, Near-Hospital (Nearest Hospital),District Hospital,Referral Hospital)
 
 villages=wb['Village'].unique()
-hospitals=wb.loc[wb['Facility Type']=='Hospital']
+villages=villages[~pd.isnull(villages)] # take out the nans
 
-# dataframe that will hold these additional linkages
-df=pd.DataFrame(data={'Village':villages},columns=wb.columns)
+# **** Get the listing of CHW per village
+df_CHW=wb.loc[wb['Facility Type']=='Community Health Worker',['Village','Facility Type','Facility_ID']]
+df_CHW.groupby(by='Village').count()
 
-v=villages[2] # for a particular village.....
+#TODO: Got this to this point
+# **** Attach the nearest hospital to each village (can be the distrct hospital but not the referral hospital)
+df_NearHospital=pd.DataFrame(columns={'Village','Facility Type','Facility_ID'})
 
-# take the average location of all facility types in village
-vill_x=np.mean(wb.loc[(wb['Village']==v) & (wb['Facility Type']!='Referral Hospital'),'Eastings'])
-vill_y=np.mean(wb.loc[(wb['Village']==v) & (wb['Facility Type']!='Referral Hospital'),'Northings'])
-
-hosp_x=hospitals['Eastings']
-hosp_y=hospitals['Northings']
-
-dist_sq= np.power(hosp_x-vill_x,2) + np.power(hosp_y-vill_y,2)
-
-record=hospitals.iloc[dist_sq.idxmin()]
-record.loc[record.index, 'Village'] = v
-
-df.loc[df['Village']==v]=record.values
-
-
-
-
-# 3) Attach Referral Hospitals to each village:
-
-# make a dataframe that will hold the new records
-villages=wb['Village'].unique()
-df=pd.DataFrame(data={'Village':villages},columns=wb.columns)
-
+hospitals=wb.loc[ (wb['Facility Type']=='Hospital') & (wb['Facility Type']!='Referral Hospital') ]
 
 for v in villages:
+    # take the average location of all facility types in village
+    vill_x=np.mean( wb.loc[wb['Village']==v,'Eastings'] )
+    vill_y=np.mean( wb.loc[wb['Village']==v,'Northings'] )
 
-    if not pd.isnull(v):
-        #new record for each village attaching to a referral hospital:
-        region = wb.loc[wb['Village'] == v, 'Region']
-        region = region.iloc[0].strip()
+    dist_sq= np.power(hospitals['Eastings']-vill_x,2) + np.power(hospitals['Northings']-vill_y,2)
 
-        if region=='South':
-            record=wb.loc[wb['Facility Name']=='QUEEN ELIZABETH']
-        else:
-            record = wb.loc[wb['Facility Name'] == 'KAMUZU CENTRAL HOSPITAL']
+    nearest_hosp_id = hospitals.loc[dist_sq.idxmin(),'Facility_ID']
 
-        record.loc[record.index, 'Village'] = v
-        df.loc[df['Village']==v]=record.values
+    df_NearHospital=df_NearHospital.append(  {'Village':v, 'Facility Type':'Near Hospital','Facility_ID': nearest_hosp_id }, ignore_index=True)
 
 
 
-wb=wb.append(df)
+# **** Add in linkage to district hospitals and the nearest hospital
+df_DistrictHospital=pd.DataFrame(columns={'Village','Facility Type','Facility_ID'})
+
+district_hosps=wb.loc[ (wb['Facility Type']=='District Hospital') ]
+districts=wb['District'].unique()
+
+for d in districts:
+    the_district_hospital_idx = wb.loc[(wb['District'] == d) & (wb['Facility Type'] == 'District Hospital')].Facility_ID.values[0]
+    print(the_district_hospital_idx)
 
 
 
-
-
-tas=wb.loc[wb['Facility Type']=='Hospital','Facility Name'].unique
-
-
-
-wb.loc[wb['Facility Type']=='Hospital','Facility Name'].unique
-
-# 4) Check to see how breakdown of facilities look by village;
-
-wb.loc[wb['Village']==villages[0]]
-
-
-# Clean out facilities which do not attach to a village
-wb[pd.isnull(wb['Village'])]
-
-
-# Clean out columns that are not useful to useful for the mapping
+# **** Add in linkage to referral hosital
 
 
 
+# **** Concatenate all the sets
 
 
 
+#TODO: Alter how the healthsystem read in the two files and uses them
 
 
-#-------------------------------------------------------------------------
-
-# Add that every village is also attaching to a Referral Hospital based on regio
-
-# Add that every village is also attching to all the hospitals in the district
-
-
-# Add it that there is a central hospital at the top of the hierarchy for all villages
-
-# experiment with the data:
-# reduce it to one villge
-
-
-myvillage='Iyela'
-
-df=wb.loc[wb['Village']==myvillage]
-
-
+#
+#
+# # 3) Attach Referral Hospitals to each village:
+#
+# # make a dataframe that will hold the new records
+# villages=wb['Village'].unique()
+# df=pd.DataFrame(data={'Village':villages},columns=wb.columns)
+#
+#
+# for v in villages:
+#
+#     if not pd.isnull(v):
+#         #new record for each village attaching to a referral hospital:
+#         region = wb.loc[wb['Village'] == v, 'Region']
+#         region = region.iloc[0].strip()
+#
+#         if region=='South':
+#             record=wb.loc[wb['Facility Name']=='QUEEN ELIZABETH']
+#         else:
+#             record = wb.loc[wb['Facility Name'] == 'KAMUZU CENTRAL HOSPITAL']
+#
+#         record.loc[record.index, 'Village'] = v
+#         df.loc[df['Village']==v]=record.values
+#
+#
+#
+# wb=wb.append(df)
+#
+#
+#
+#
+#
+# tas=wb.loc[wb['Facility Type']=='Hospital','Facility Name'].unique
+#
+#
+#
+# wb.loc[wb['Facility Type']=='Hospital','Facility Name'].unique
+#
+# # 4) Check to see how breakdown of facilities look by village;
+#
+# wb.loc[wb['Village']==villages[0]]
+#
+#
+# # Clean out facilities which do not attach to a village
+# wb[pd.isnull(wb['Village'])]
+#
+#
+# # Clean out columns that are not useful to useful for the mapping
+#
+#
+#
+#
+#
+#
+#
+#
+# #-------------------------------------------------------------------------
+#
+# # Add that every village is also attaching to a Referral Hospital based on regio
+#
+# # Add that every village is also attching to all the hospitals in the district
+#
+#
+# # Add it that there is a central hospital at the top of the hierarchy for all villages
+#
+# # experiment with the data:
+# # reduce it to one villge
+#
+#
+# myvillage='Iyela'
+#
+# df=wb.loc[wb['Village']==myvillage]
+#
+#
