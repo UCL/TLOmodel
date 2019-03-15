deda81e75fa887b1ed953029643521105ca23849
Asif Tamuri <tamuri@gmail.com>
2019-03-15 16:01:57 +0000

    clean up
    Former-commit-id: 4775700799dbf907d0c59f9a5986ba440c8496ed

--- src/tlo/methods/qaly.py
+++ src/tlo/methods/qaly.py
@@ -2,12 +2,12 @@
 This Module runs the counting of QALYS across all persons and logs it
 """
 
-from tlo import DateOffset, Module, Parameter, Property, Types
-from tlo.events import PopulationScopeEventMixin, RegularEvent
-import pandas as pd
 import logging
 
-from tlo.methods import healthsystem
+import pandas as pd
+
+from tlo import DateOffset, Module, Property, Types
+from tlo.events import PopulationScopeEventMixin, RegularEvent
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -20,18 +20,22 @@ class QALY(Module):
 
     def __init__(self, name=None, resourcefilepath=None):
         super().__init__(name)
-        self.resourcefilepath=resourcefilepath
-
+        self.resourcefilepath = resourcefilepath
 
     # Here we declare parameters for this module. Each parameter has a name, data type,
     # and longer description.
-    PARAMETERS = {'Weight_Database': Property(Types.DATA_FRAME,'Weight Database')} # This is a DALY database, so complement of the valyuest must be used.
-    PROPERTIES = {'qa_QALY': Property(Types.REAL, 'Summary of health state for previous year')}
+    # This is a DALY database, so complement of the values must be used.
+    PARAMETERS = {
+        'Weight_Database': Property(Types.DATA_FRAME, 'Weight Database')
+    }
 
+    PROPERTIES = {
+        'qa_QALY': Property(Types.REAL, 'Summary of health state for previous year')
+    }
 
     def read_parameters(self, data_folder):
-
-        self.parameters['Weight_Database'] = pd.read_csv(self.resourcefilepath+'ResourceFile_DALY_Weights.csv')
+        p = self.parameters
+        p['Weight_Database'] = pd.read_csv(self.resourcefilepath + 'ResourceFile_DALY_Weights.csv')
 
     def initialise_population(self, population):
         """Set our property values for the initial population.
@@ -45,13 +49,9 @@ class QALY(Module):
         pass
 
     def initialise_simulation(self, sim):
-
+        """ Launch the QALY Logger to run every year
         """
-        Launch the QALY Logger to run every year
-        """
-
-        sim.schedule_event(LogQALYs(self),sim.date)
-
+        sim.schedule_event(LogQALYs(self), sim.date)
 
     def on_birth(self, mother_id, child_id):
         """Initialise our properties for a newborn individual.
@@ -63,8 +63,7 @@ class QALY(Module):
         """
         pass
 
-
-    def get_qaly_weight(self,sequlae_code):
+    def get_qaly_weight(self, sequlae_code):
         """
         This can be used to look up the QALY code for a particular condition
 
@@ -95,25 +94,23 @@ class LogQALYs(RegularEvent, PopulationScopeEventMixin):
         """
         Running the QALY Logger
         """
-        print('The QALY Logger is occuring now!@@@@',self.sim.date)
+        logger.debug('The QALY Logger is occuring now!@@@@ %s', self.sim.date)
 
-        disease_specific_qaly_values= pd.DataFrame()
+        disease_specific_qaly_values = pd.DataFrame()
 
         # 1) Ask each disease module to log the QALYS
-        disease_modules=self.sim.modules['HealthSystem'].RegisteredDiseaseModules
+        disease_modules = self.sim.modules['HealthSystem'].registered_disease_modules
 
         for module in disease_modules.values():
-            out=module.report_qaly_values()
-            disease_specific_qaly_values = pd.concat([disease_specific_qaly_values , out],
-                                            axis=1)  # each column of this dataframe gives the reports from each module the HealthState
-
+            out = module.report_qaly_values()
+            # each column of this dataframe gives the reports from each module the HealthState
+            disease_specific_qaly_values = pd.concat([disease_specific_qaly_values, out], axis=1)
 
         # 2) Reconcile the QALY scores into a unifed score
-        df=self.sim.population.props
+        df = self.sim.population.props
 
-        df['qa_QALY'] = 0 # setting to zero ensures that all dead people are given zero values
-        df['qa_QALY']=disease_specific_qaly_values.prod(axis=1)
-
+        df['qa_QALY'] = 0  # setting to zero ensures that all dead people are given zero values
+        df['qa_QALY'] = disease_specific_qaly_values.prod(axis=1)
 
         # 4) Summarise the results and output these to the log
 
@@ -126,5 +123,3 @@ class LogQALYs(RegularEvent, PopulationScopeEventMixin):
 
         logger.info('%s|QALYS_F|%s', self.sim.date,
                     f_healthstate_sum.to_dict())
-
-
