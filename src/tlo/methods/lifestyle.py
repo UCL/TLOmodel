b0ddb052ec14b10ec8fa873af8065166ebb1c4f2
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2019-04-02 07:17:35 +0100

    health system interaction

--- src/tlo/methods/lifestyle.py
+++ src/tlo/methods/lifestyle.py
@@ -11,7 +11,7 @@ from tlo import DateOffset, Module, Parameter, Property, Types
 from tlo.events import PopulationScopeEventMixin, RegularEvent
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.CRITICAL)
 
 
 class Lifestyle(Module):
