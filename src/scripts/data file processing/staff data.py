0b2045474f87ce84b3b15f4177afc54dc4106ee2
Tim Hallett <tbhallett@gmail.com>
2019-04-06 11:51:15 +0100

    work on current staff resource file

--- src/scripts/data file processing/staff data.py
+++ src/scripts/data file processing/staff data.py
@@ -72,14 +72,28 @@ for d in pop_districts:
 for d in chai_districts:
     print('Chai file district, ', d , ' in resource file: ', (d in pop_districts))
 
-# Decide which type of count we want to use:
+# Decide which type of count we want to use: For simplicity, now use the funded positions:
 wb.loc[wb['Count']=='CURRENT TOTAL STAFF (NON-VACANT POSITIONS)','Nurse Officer'].sum()
 wb.loc[wb['Count']=='Funded TOTAL STAFF (NON-VACANT POSITIONS)','Nurse Officer'].sum()
 
-# Flip from wide to long format
-current_staff=pd.melt(wb, id_vars=['District','Count'], value_vars=officer_types,var_name='Officer', value_name='Number')
 
-current_staff.to_csv(outputfile_current_staff)
+# Flip from wide to long format
+current_staff_long=pd.melt(wb.loc[wb['Count']=='Funded TOTAL STAFF (NON-VACANT POSITIONS)'], id_vars=['District','Count'], value_vars=officer_types,var_name='Officer', value_name='Number')
+
+# Repeat rows so that it is one row per staff member
+current_staff_long['Number']=current_staff_long['Number'].astype(int)
+stafflist = current_staff_long.loc[current_staff_long.index.repeat(current_staff_long['Number'])]
+#reset insex
+stafflist = stafflist.reset_index(drop=True)
+stafflist=stafflist.drop(['Number','Count'],axis=1)
+
+len(stafflist)
+current_staff_long['Number'].sum()
+
+stafflist.loc[:,'Staff_ID']=pd.Series(stafflist.index)
+stafflist.to_csv(outputfile_current_staff)
+
+# Could identify the CHAM staff and sort those to particular facilities:
 
 
 #-----------------
@@ -92,8 +106,23 @@ sheet= pd.read_excel(workingfile,sheet_name='PFT',header=None)
 officers=sheet.iloc[1,np.arange(2,23)]
 hours=sheet.iloc[38,np.arange(2,23)]
 
-patient_facing_hours = pd.DataFrame({'Officer': officers,'Hours':hours})
+days_per_year_men=sheet.iloc[15,np.arange(2,23)]
+days_per_year_women=sheet.iloc[16,np.arange(2,23)]
+days_per_year_pregwomen=sheet.iloc[17,np.arange(2,23)]
+
+fr_men=sheet.iloc[53,np.arange(2,23)]
+fr_women=sheet.iloc[55,np.arange(2,23)] - sheet.iloc[57,np.arange(2,23)] # taking off pregnant women: this is for non-pregnant women
+fr_pregwomen= sheet.iloc[57,np.arange(2,23)]
+
+workingdays= ( fr_men*days_per_year_men ) + ( fr_women*days_per_year_women ) + ( fr_pregwomen*days_per_year_pregwomen )
+
+minutes_per_day = hours * 60
+
+minutes_per_year = minutes_per_day * workingdays
+
+patient_facing_hours = pd.DataFrame({'Officer': officers,'Working_Days_Per_Year':workingdays,'Hours_Per_Day':hours})
 patient_facing_hours.to_csv(outputfile_working_hours)
+
 
 
 # ---- check that the column name and types of officers are aligned
@@ -115,44 +144,81 @@ for n in names_staffsheet:
 # Create a mapping of how these different officer types will go to particular facility types
 # The level is important (as this determines how many other people are drawing on their time)
 
-array(['District Hospital', 'Community Health Worker', 'Health Centre',
-       'Hospital', 'Referral Hospital'])
-
 
 Facility_By_Officer=pd.DataFrame({'Officer': officer_types, 'Facility_Type':None})
 
 Facility_By_Officer.at[Facility_By_Officer['Officer']=='Medical Officer / Specialist','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Clinical Officer / Technician','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Med. Assistant','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Clinical Officer / Technician','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Med. Assistant','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
 Facility_By_Officer.at[Facility_By_Officer['Officer']=='Nurse Officer','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
 Facility_By_Officer.at[Facility_By_Officer['Officer']=='Nurse Midwife Technician','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Pharmacist','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Pharm Technician','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Pharm Assistant','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Lab Officer','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Lab Technician','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Lab Assistant','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='DCSA','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Dental Officer','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Dental Therapist','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Dental Assistant','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Mental Health Staff','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Nutrition Staff','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Radiographer','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Radiography Technician','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Sonographer','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
-Facility_By_Officer.at[Facility_By_Officer['Officer']=='Radiotherapy Technician','Facility_Type']={'District Hospital', 'Community Health Worker', 'Health Centre','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Pharmacist','Facility_Type']={'District Hospital', 'Health Centre','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Pharm Technician','Facility_Type']={'District Hospital', 'Health Centre','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Pharm Assistant','Facility_Type']={'District Hospital', 'Health Centre','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Lab Officer','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Lab Technician','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Lab Assistant','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='DCSA','Facility_Type']={'Community Health Worker'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Dental Officer','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Dental Therapist','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Dental Assistant','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Mental Health Staff','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Nutrition Staff','Facility_Type']={'District Hospital','Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Radiographer','Facility_Type']={'District Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Radiography Technician','Facility_Type']={'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Sonographer','Facility_Type']={'District Hospital', 'Referral Hospital'}
+Facility_By_Officer.at[Facility_By_Officer['Officer']=='Radiotherapy Technician','Facility_Type']={'Referral Hospital'}
 
 
+
+
+
+# Load the master facilities list:
 
 ResourceFile_MasterFacilitiesList = '/Users/tbh03/PycharmProjects/TLOmodel/resources/ResourceFile_MasterFacilitiesList.csv'
 
 fac=pd.read_csv(ResourceFile_MasterFacilitiesList)
 
-fac_in_Balaka=fac.loc[fac['District']=='Balaka'].copy()
+# Prepare dataframe for holding the assignments between Staff_ID and Facility_ID
+Facility_Assignment =pd.DataFrame(stafflist['Staff_ID'])
+Facility_Assignment.loc[:,'Facility_ID']=None
 
-staff_in_Balaka=current_staff.loc[current_staff['District']=='Balaka'].copy()
+# Loop through each staff member and allocate them to
 
-MedOff_in_Balaka=staff_in_Balaka.loc[staff_in_Balaka['Officer']==officer_types[0]]
+for staffmember in stafflist.index:
+
+    officer =stafflist.loc[staffmember].Officer
+
+    district= stafflist.loc[staffmember].District
+
+    fac_types_set= Facility_By_Officer.loc[Facility_By_Officer['Officer']==officer,'Facility_Type'][0]
+    fac_types_list = []
+
+    # convert the set of relevant facilities to a list
+    for x in fac_types_set:
+        fac_types_list.append(x)
+
+    # Get the facilities to which this staff member might be allocated
+    suitable_facilities=fac.loc[ ( fac['Facility Type'].isin(fac_types_list) ) & ( fac['District'] == district ) ]
+
+    # what is there are not any suitable facilities????
+
+    # choose one for this staff member:
+    assert len(suitable_facilities)>0
+
+    x=np.random.choice(len(suitable_facilities))
+
+    assigned_facility=suitable_facilities.iloc[x].Facility_ID
+
+    Facility_Assignment.at[staffmember,'Facility_ID']=assigned_facility
+
+
+
+
+
+
+
+
+
 
 
