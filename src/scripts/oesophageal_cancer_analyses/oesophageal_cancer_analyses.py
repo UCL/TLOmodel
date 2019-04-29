ab227ce109c3f66358e38b06fd488431f44c71e7
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2019-04-29 14:50:35 +0100

    ...

--- src/scripts/oesophageal_cancer_analyses/oesophageal_cancer_analyses.py
+++ src/scripts/oesophageal_cancer_analyses/oesophageal_cancer_analyses.py
@@ -18,8 +18,8 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 resourcefilepath = './resources/'
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2019, 7, 1)
-popsize = 100000
+end_date = Date(2015, 1, 1)
+popsize = 20
 
 # Establish the simulation object
 sim = Simulation(start_date=start_date)
