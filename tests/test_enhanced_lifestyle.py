e40d3b177b33c827fdb7b66907e6d5e183a26d86
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2019-05-03 12:28:07 +0100

    ...

--- tests/test_enhanced_lifestyle.py
+++ tests/test_enhanced_lifestyle.py
@@ -11,8 +11,8 @@ from tlo.methods import demography, enhanced_lifestyle
 workbook_name = 'demography.xlsx'
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2010, 2, 1)
-popsize = 1000
+end_date = Date(2012, 1, 1)
+popsize = 10000
 
 
 @pytest.fixture(autouse=True)
