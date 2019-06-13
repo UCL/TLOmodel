5ef023028573335728a36893f94260d1387031fd
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2019-06-13 14:39:00 +0100

    ...
    Former-commit-id: 673ed915e624916b182cbe7561179f18bb181238

--- src/scripts/oesophageal_cancer_analyses/oesophageal_cancer_analyses.py
+++ src/scripts/oesophageal_cancer_analyses/oesophageal_cancer_analyses.py
@@ -19,7 +19,7 @@ resourcefilepath = './resources/'
 
 start_date = Date(2010, 1, 1)
 end_date = Date(2015, 1, 1)
-popsize = 400000
+popsize = 100
 
 # Establish the simulation object
 sim = Simulation(start_date=start_date)
