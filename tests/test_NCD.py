ac7bfab218f2b6b2f771b9138cdee41c8bc7d40e
Mikaela Smit <mikaela.smit@gmail.com>
2019-06-13 17:03:17 +0100

    Code to read in HR - expand to all risk factors
    TO DO HT and Diabetes:
    1. QALY resolve
    2. Age groups for prevalence check
    3. Update incidence parameters
    4. Read risks from Excel
    5. Read prevalence from Excel?§

---
+++ tests/test_NCD.py
@@ -0,0 +1,50 @@
+import logging
+import os
+
+
+import pytest
+
+from tlo import Simulation, Date, Property
+from tlo.methods import demography, hypertension, highcholesterol, diabetes#, CVD
+
+start_date = Date(2010, 1, 1)
+end_date = Date(2030, 1, 1)
+popsize = 100
+
+
+@pytest.fixture
+def simulation():
+    sim = Simulation(start_date=start_date)
+
+    demography_workbook = os.path.join(os.path.dirname(__file__),
+                                       'resources',
+                                       'demography.xlsx')
+    core_module = demography.Demography(workbook_path=demography_workbook)
+    logging.getLogger('tlo.methods.demography').setLevel(logging.FATAL)
+
+    diabetes_module = diabetes.Diabetes()  # This will load method for diabetes
+    highcholesterol_module = highcholesterol.HC()         # This will load method for high cholesterol
+    hypertension_module = hypertension.HT()               # This will load method for hypertension
+
+
+
+    sim.register(core_module)
+    #sim.register(diabetes_module)  # This will register method for diabetes
+    #sim.register(highcholesterol_module)                  # This will register method for high cholesterol
+    sim.register(hypertension_module)                     # This will register method for hypertension
+
+
+
+    sim.seed_rngs(0)
+
+    return sim
+
+
+def test_NCD_simulation(simulation):
+    simulation.make_initial_population(n=popsize)
+    simulation.simulate(end_date=end_date)
+
+
+if __name__ == '__main__':
+    simulation = simulation()
+    test_NCD_simulation(simulation)
