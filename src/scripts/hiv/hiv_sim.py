76f0c545bc800bc00d68fdb2dadfe17d25a5017b
Tara Mangal <t.mangal@imperial.ac.uk>
2019-06-24 10:53:45 +0200

    analysis file updated

--- src/scripts/hiv/hiv_sim.py
+++ src/scripts/hiv/hiv_sim.py
@@ -4,22 +4,21 @@ import os
 
 from tlo import Date, Simulation
 from tlo.analysis.utils import parse_log_file
-from tlo.methods import chronicsyndrome, demography, healthburden, healthsystem, lifestyle, mockitis
-
-# [NB. Working directory must be set to the root of TLO: TLOmodel/]
+from tlo.methods import demography, healthsystem, lifestyle, healthburden, hiv, \
+    male_circumcision, hiv_behaviour_change, tb
 
 # Where will output go
-outputpath = ''
+outputpath = './src/scripts/outputLogs/'
 
 # date-stamp to label log files and any other outputs
 datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 
 # The resource files
-resourcefilepath = 'resources'
+resourcefilepath = './resources/'
 
-start_date = Date(year=2010, month=1, day=1)
-end_date = Date(year=2013, month=12, day=31)
-popsize = 50
+start_date = Date(2010, 1, 1)
+end_date = Date(2013, 1, 1)
+popsize = 2000
 
 # Establish the simulation object
 sim = Simulation(start_date=start_date)
@@ -34,28 +33,35 @@ fr = logging.Formatter("%(levelname)s|%(name)s|%(message)s")
 fh.setFormatter(fr)
 logging.getLogger().addHandler(fh)
 
-
 # ----- Control over the types of intervention that can occur -----
 # Make a list that contains the treatment_id that will be allowed. Empty list means nothing allowed.
 # '*' means everything. It will allow any treatment_id that begins with a stub (e.g. Mockitis*)
 service_availability = ['*']
 
-# -----------------------------------------------------------------
+logging.getLogger('tlo.methods.demography').setLevel(logging.WARNING)
+logging.getLogger('tlo.methods.lifestyle').setLevel(logging.WARNING)
+logging.getLogger('tlo.methods.qaly').setLevel(logging.WARNING)
+logging.getLogger('tlo.methods.hiv').setLevel(logging.WARNING)
+logging.getLogger('tlo.methods.tb').setLevel(logging.WARNING)
+logging.getLogger('tlo.methods.male_circumcision').setLevel(logging.WARNING)
 
 # Register the appropriate modules
 sim.register(demography.Demography(resourcefilepath=resourcefilepath))
-sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
-                                       service_availability=service_availability))
+sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath))
 sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
 sim.register(lifestyle.Lifestyle())
-sim.register(mockitis.Mockitis())
-sim.register(chronicsyndrome.ChronicSyndrome())
+sim.register(hiv.hiv(resourcefilepath=resourcefilepath))
+sim.register(tb.tb(resourcefilepath=resourcefilepath))
+sim.register(male_circumcision.male_circumcision(resourcefilepath=resourcefilepath))
+sim.register(hiv_behaviour_change.BehaviourChange())
 
 # Run the simulation and flush the logger
 sim.seed_rngs(0)
 sim.make_initial_population(n=popsize)
 sim.simulate(end_date=end_date)
 fh.flush()
+fh.close()
 
-# %% read the results
-output = parse_log_file(logfile)
+
+# # %% read the results
+# output = parse_log_file(logfile)
