03ffecd115e04043d820712313fb45d1a4b203b6
Tim Hallett <timothy.hallett@imperial.ac.uk>
2019-06-26 15:18:13 +0100

    updating plots

--- src/scripts/demography/analysis_demography.py
+++ src/scripts/demography/analysis_demography.py
@@ -11,6 +11,7 @@ import pandas as pd
 from tlo import Date, Simulation
 from tlo.analysis.utils import parse_log_file
 from tlo.methods import demography
+from tlo.methods.demography import make_age_range_lookup
 
 # Where will output go - by default, wherever this script is run
 outputpath = ''
@@ -20,13 +21,13 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 
 # The resource file for demography module
 # assume Python console is started in the top-leve TLOModel directory
-resourcefile_demography = Path('./resources')
+resourcefilepath = Path('./resources')
 
 
 # %% Run the Simulation
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2050, 1, 1)
+end_date = Date(2031, 1, 1)  # so last full year of simulation will be 2030
 popsize = 1000
 
 # add file handler for the purpose of logging
@@ -43,7 +44,7 @@ fh.setFormatter(fr)
 logging.getLogger().addHandler(fh)
 
 # run the simulation
-sim.register(demography.Demography(resourcefilepath=resourcefile_demography))
+sim.register(demography.Demography(resourcefilepath=resourcefilepath))
 sim.seed_rngs(1)
 sim.make_initial_population(n=popsize)
 sim.simulate(end_date=end_date)
@@ -64,7 +65,7 @@ Model_Pop_Normalised = 100 * np.asarray(Model_Pop) / np.asarray(
     Model_Pop[Model_Years == '2010-01-01'])
 
 # Load Data
-Data = pd.read_excel(resourcefile_demography, sheet_name='Interpolated Pop Structure')
+Data = pd.read_excel(Path(resourcefilepath) / 'ResourceFile_DemographicData.xlsx', sheet_name='Interpolated Pop Structure')
 Data_Pop = Data.groupby(by='year')['value'].sum()
 Data_Years = Data.groupby(by='year')['year'].mean()
 Data_Years = pd.to_datetime(Data_Years, format='%Y')
@@ -89,89 +90,87 @@ plt.show()
 pop_f_df = output['tlo.methods.demography']['age_range_f']
 pop_m_df = output['tlo.methods.demography']['age_range_m']
 
-# create mask for the 2015 and 2045 results:
-m2015 = ((pd.to_datetime(pop_f_df['date']) >= Date(2015, 1, 1)) &
-         (pd.to_datetime(pop_f_df['date']) < Date(2016, 1, 1)))
-m2045 = ((pd.to_datetime(pop_f_df['date']) >= Date(2045, 1, 1)) &
-         (pd.to_datetime(pop_f_df['date']) < Date(2046, 1, 1)))
+# create mask for the 2015 and 2030 results:
+m2015 = pd.to_datetime(pop_f_df['date']).dt.year == 2015
+m2030 = pd.to_datetime(pop_f_df['date']).dt.year == 2030
 
-# Extract the results for just 2015, and trim off the first two columns
-ModelOutput_Women_2015 = (np.asarray((pop_f_df.loc[m2015]).iloc[:, 1:22])).flatten()
-ModelOutput_Men_2015 = (np.asarray((pop_m_df.loc[m2015]).iloc[:, 1:22])).flatten()
-ModelOutput_Women_2045 = (np.asarray((pop_f_df.loc[m2045]).iloc[:, 1:22])).flatten()
-ModelOutput_Men_2045 = (np.asarray((pop_m_df.loc[m2045]).iloc[:, 1:22])).flatten()
+# Extract the results for men and women for these two years and combine
+model = pd.concat([
+                pd.concat([pd.melt(pop_f_df.loc[m2015, pop_f_df.columns[1:]]),
+                           pd.DataFrame(index=range(21), data={'Year': 2015, 'Sex': 'F'})], axis=1),    # Female, 2015
 
-# Trim off the last three columns (ages 90-94, 95-99, 100+) and replace the last column with the
-# sum of the last four columns in the original output
-ModelOutput_Women_2015_trimmed = ModelOutput_Women_2015[0:18]
-ModelOutput_Women_2015_trimmed[17] = sum(ModelOutput_Women_2015[18:21])
+                pd.concat([pd.melt(pop_f_df.loc[m2030, pop_f_df.columns[1:]]),
+                           pd.DataFrame(index=range(21), data={'Year': 2030, 'Sex': 'F'})], axis=1),    # Female, 2030
 
-ModelOutput_Men_2015_trimmed = ModelOutput_Men_2015[0:18]
-ModelOutput_Men_2015_trimmed[17] = sum(ModelOutput_Men_2015[18:21])
+                pd.concat([pd.melt(pop_m_df.loc[m2015, pop_m_df.columns[1:]]),
+                           pd.DataFrame(index=range(21), data={'Year': 2015, 'Sex': 'M'})], axis=1),    # Male, 2015
 
-ModelOutput_Women_2045_trimmed = ModelOutput_Women_2045[0:18]
-ModelOutput_Women_2045_trimmed[17] = sum(ModelOutput_Women_2045[18:21])
+                pd.concat([pd.melt(pop_m_df.loc[m2030, pop_m_df.columns[1:]]),
+                           pd.DataFrame(index=range(21), data={'Year': 2030, 'Sex': 'M'})], axis=1),    # Male, 2030
 
-ModelOutput_Men_2045_trimmed = ModelOutput_Men_2045[0:18]
-ModelOutput_Men_2045_trimmed[17] = sum(ModelOutput_Men_2045[18:21])
+            ],axis=0)
 
-# TODO: the age-group labels should be picked up in the log-file making process
-Age_Group_Labels = ["0-4",
-                    "5-9",
-                    "10-14",
-                    "15-19",
-                    "20-24",
-                    "25-29",
-                    "30-34",
-                    "35-39",
-                    "40-44",
-                    "45-49",
-                    "50-54",
-                    "55-59",
-                    "60-64",
-                    "65-69",
-                    "70-74",
-                    "75-79",
-                    "80-84",
-                    "85+"]
+model_pop_pyramind = model.rename(columns={'variable': 'agegrp', 'value': 'pop', 'Year':'year', 'Sex':'sex'})
 
-# Load population data to compare population pyramid
-Data = pd.read_excel(resourcefile_demography, sheet_name='Interpolated Pop Structure')
 
-df = Data[Data['year'] == 2015].copy()
-df['agegrp'] = np.floor(df.age_to / 5)
-df.loc[df.agegrp > 17, 'agegrp'] = 17
-tab = df.pivot_table(values="value", index=["gender", "agegrp"], aggfunc=np.sum)
-Data_Women_2015 = np.asarray(tab[tab.index.get_level_values('gender') == 'female']).flatten()
-Data_Men_2015 = np.asarray(tab[tab.index.get_level_values('gender') == 'male']).flatten()
+# Load population data to compare population pyramid: 2015
+Data = pd.read_excel(Path(resourcefilepath) / 'ResourceFile_DemographicData.xlsx', sheet_name='Interpolated Pop Structure')
+df = Data[(Data['year'] == 2015) | (Data['year'] == 2030)].copy()
 
-df = Data[Data['year'] == 2045].copy()
-df['agegrp'] = np.floor(df.age_to / 5)
-df.loc[df.agegrp > 17, 'agegrp'] = 17
-tab = df.pivot_table(values="value", index=["gender", "agegrp"], aggfunc=np.sum)
-Data_Women_2045 = np.asarray(tab[tab.index.get_level_values('gender') == 'female']).flatten()
-Data_Men_2045 = np.asarray(tab[tab.index.get_level_values('gender') == 'male']).flatten()
+# get look-up to group ages by same groups
+(__tmp__,age_grp_lookup) = make_age_range_lookup()
+df['agegrp'] = df['age_from'].map(age_grp_lookup)
+df= df.rename(columns={'gender':'sex', 'value':'pop'})
+df['sex'] = df['sex'].map({'female':'F', 'male':'M'})
+data_pop_pyramid= df.groupby(['year','agegrp','sex'])['pop'].sum().reset_index()
+
+
+# Join the model and the data together for ease of plotting
+combo = data_pop_pyramid.merge(model_pop_pyramind,on=['agegrp','year','sex'],how='inner',suffixes=('_data','_model'))
+combo = combo.sort_values(by = ['year','sex','agegrp']).reset_index(drop=True)
+
+# Add in a scaled population size (within a particular year) so that its percentage in that age/sex grp
+combo['pop_data_sc'] = combo['pop_data']
+combo.loc[combo['year']==2015,'pop_data_sc'] /=  combo.loc[combo['year']==2015,'pop_data'].sum()
+combo.loc[combo['year']==2030,'pop_data_sc'] /=  combo.loc[combo['year']==2030,'pop_data'].sum()
+
+combo['pop_model_sc'] = combo['pop_model']
+combo.loc[combo['year']==2015,'pop_model_sc'] /=  combo.loc[combo['year']==2015,'pop_model'].sum()
+combo.loc[combo['year']==2030,'pop_model_sc'] /=  combo.loc[combo['year']==2030,'pop_model'].sum()
+
+
+# Model Population Pyramid
+# TODO: new plots
+
 
 # Traditional Populaiton Pyramid: Model Only
 fig, axes = plt.subplots(ncols=2, nrows=2, sharey=True)
-y = np.arange(len(Age_Group_Labels))
-axes[0][0].barh(y, ModelOutput_Women_2015_trimmed / ModelOutput_Women_2015_trimmed.sum(),
+age_grp_labels = np.unique(list(age_grp_lookup.values()))
+axes[0][0].barh(combo.loc[(combo['year']==2015) & (combo['sex']=='F'),['agegrp']].values,
+                combo.loc[(combo['year']==2015) & (combo['sex']=='F'),['pop_model_sc']].values,
                 align='center', color='red', zorder=10)
 axes[0][0].set(title='Women, 2015')
-axes[0][1].barh(y, ModelOutput_Men_2015_trimmed / ModelOutput_Men_2015_trimmed.sum(),
+
+axes[0][1].barh(combo.loc[(combo['year']==2015) & (combo['sex']=='M'),['agegrp']].values,
+                combo.loc[(combo['year']==2015) & (combo['sex']=='M'),['pop_model_sc']].values,
                 align='center', color='blue', zorder=10)
 axes[0][1].set(title='Men, 2015')
-axes[1][0].barh(y, ModelOutput_Women_2045_trimmed / ModelOutput_Women_2045_trimmed.sum(),
+
+axes[1][0].barh(combo.loc[(combo['year']==2030) & (combo['sex']=='F'),['agegrp']].values,
+                combo.loc[(combo['year']==2030) & (combo['sex']=='F'),['pop_model_sc']].values,
                 align='center', color='red', zorder=10)
-axes[1][0].set(title='Women, 2045')
-axes[1][1].barh(y, ModelOutput_Men_2045_trimmed / ModelOutput_Men_2045_trimmed.sum(),
+axes[1][0].set(title='Women, 2030')
+
+axes[1][1].barh(combo.loc[(combo['year']==2030) & (combo['sex']=='M'),['agegrp']].values,
+                combo.loc[(combo['year']==2030) & (combo['sex']=='M'),['pop_model_sc']].values,
                 align='center', color='blue', zorder=10)
-axes[1][1].set(title='Men, 2045')
+axes[1][1].set(title='Men, 2030')
+
 axes[0][0].invert_xaxis()
-axes[0][0].set(yticks=y, yticklabels=Age_Group_Labels)
+axes[0][0].set(yticklabels=age_grp_labels)
 axes[0][0].yaxis.tick_right()
 axes[1][0].invert_xaxis()
-axes[1][0].set(yticks=y, yticklabels=Age_Group_Labels)
+axes[1][0].set(yticklabels=age_grp_labels)
 axes[1][0].yaxis.tick_right()
 
 for ax in axes.flat:
@@ -182,6 +181,7 @@ fig.tight_layout()
 fig.subplots_adjust(wspace=0.09)
 plt.savefig(outputpath + 'PopPyramidModelOnly' + datestamp + '.pdf')
 plt.show()
+
 
 # Model Vs Data Pop Pyramid
 
@@ -230,4 +230,19 @@ plt.plot_date(deaths_df['date'], deaths_df['age'])
 plt.xlabel('Year')
 plt.ylabel('Age at Death')
 plt.savefig(outputpath + 'Deaths' + datestamp + '.pdf')
+plt.show()
+
+# %% Plot deaths by cause:
+
+deaths_df = output['tlo.methods.demography']['death']
+deaths_df['date'] = pd.to_datetime(deaths_df['date'])
+deaths_df['year'] = deaths_df['date'].dt.year
+death_by_cause = deaths_df.groupby(['year','cause'])['person_id'].size()
+
+death_by_cause = death_by_cause.reset_index()
+death_by_cause.index = death_by_cause['year']
+death_by_cause.drop(columns='year', inplace=True)
+death_by_cause = death_by_cause.rename(columns={'person_id':'num_deaths'})
+
+death_by_cause.plot.bar(stacked=True)
 plt.show()
