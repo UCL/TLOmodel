7fd89646596c938d71ebf04d9e60756e5b5d7272
mikaelasmit <mikaelasmit@users.noreply.github.com>
2019-07-16 10:12:51 +0100

    Have written a test file
    To Do:
    1. Check prevalence
    2. Check incidence
    3. Add risk factors
    4. Run test file
    5. Log and plot
    6. Read in 95% CI for plot
    7. Trial outreach campaign
    8. HSI event not running
    9. Treatment with triage

--- tests/test_NCD.py
+++ tests/test_NCD.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import time
+from pathlib import Path
 
 
 import pytest
@@ -8,9 +10,13 @@ from tlo import Simulation, Date, Property
 from tlo.methods import demography, hypertension, t2dm#, CVD
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2030, 1, 1)
+end_date = Date(2015, 1, 1)
 popsize = 100
 
+
+@pytest.fixture(autouse=True)
+def disable_logging():
+    logging.disable(logging.INFO)
 
 @pytest.fixture
 def simulation():
@@ -22,17 +28,13 @@ def simulation():
     core_module = demography.Demography(workbook_path=demography_workbook)
     logging.getLogger('tlo.methods.demography').setLevel(logging.FATAL)
 
-    diabetes_module = t2dm.T2DM()                         # This will load method for diabetes
+    #diabetes_module = t2dm.T2DM()                         # This will load method for diabetes
     hypertension_module = hypertension.HT()               # This will load method for hypertension
-
-
 
     sim.register(core_module)
     #sim.register(diabetes_module)                          # This will register method for diabetes
     #sim.register(highcholesterol_module)                   # This will register method for high cholesterol
     sim.register(hypertension_module)                       # This will register method for hypertension
-
-
 
     sim.seed_rngs(0)
 
@@ -44,6 +46,31 @@ def test_NCD_simulation(simulation):
     simulation.simulate(end_date=end_date)
 
 
+def test_dtypes(simulation):
+    # check types of columns
+    df = simulation.population.props
+    orig = simulation.population.new_row
+    assert (df.dtypes == orig.dtypes).all()
+
+
 if __name__ == '__main__':
     simulation = simulation()
     test_NCD_simulation(simulation)
+
+
+
+def test_hypertension_adults(simulation):
+    # check all mothers are female
+    df = simulation.population.props
+    HTN = df.loc[df.current_status]
+    is_adult = HTN.apply(lambda age_years: df.at.age_years > 17)
+    assert is_adult.all()
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    simulation = simulation()
+    test_NCD_simulation(simulation)
+    t1 = time.time()
+    print('Time taken', t1 - t0)
+
