edbe98d52ff5d13e2e97844d04e88f7a5169f33e
Asif Tamuri <tamuri@gmail.com>
2019-07-26 18:37:55 +0100

    add test for epilepsy

--- tests/test_epilepsy.py
+++ tests/test_epilepsy.py
@@ -1,58 +1,57 @@
-import datetime
 import logging
 import os
+import time
 from pathlib import Path
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+import pytest
+
 from tlo import Date, Simulation
-from tlo.analysis.utils import parse_log_file
-from tlo.methods import demography, healthsystem, lifestyle, epilepsy, healthburden
-
-# Where will output go
-outputpath = './src/scripts/epilepsy_analyses/'
-
-# date-stamp to label log files and any other outputs
-datestamp = datetime.date.today().strftime("__%Y_%m_%d")
-
-# The resource files
-resourcefilepath = Path(os.path.dirname(__file__)) / '../resources'
+from tlo.methods import demography, epilepsy, healthburden, healthsystem, lifestyle
 
 start_date = Date(2010, 1, 1)
 end_date = Date(2015, 1, 1)
 popsize = 10000
 
-# Establish the simulation object
-sim = Simulation(start_date=start_date)
 
-# Establish the logger
-# logfile = outputpath + 'LogFile' + datestamp + '.log'
-
-# if os.path.exists(logfile):
-#    os.remove(logfile)
-# fh = logging.FileHandler(logfile)
-# fr = logging.Formatter("%(levelname)s|%(name)s|%(message)s")
-# fh.setFormatter(fr)
-# logging.getLogger().addHandler(fh)
-
-# logging.getLogger('tlo.methods.Depression').setLevel(logging.DEBUG)
+@pytest.fixture(autouse=True)
+def disable_logging():
+    logging.disable(logging.INFO)
 
 
-# Register the appropriate modules
-sim.register(demography.Demography(resourcefilepath=resourcefilepath))
-sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
-             ignore_appt_constraints=True,
-             ignore_cons_constraints=True
-             ))
-sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
-sim.register(lifestyle.Lifestyle())
-sim.register(epilepsy.Epilepsy(resourcefilepath=resourcefilepath))
+@pytest.fixture(scope='module')
+def simulation():
+    resourcefilepath = Path(os.path.dirname(__file__)) / '../resources'
+    sim = Simulation(start_date=start_date)
 
-# Run the simulation and flush the logger
-# sim.seed_rngs(0)
-sim.make_initial_population(n=popsize)
-sim.simulate(end_date=end_date)
-# fh.flush()
+    sim.register(demography.Demography(resourcefilepath=resourcefilepath))
+    sim.register(lifestyle.Lifestyle())
+
+    sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
+                                           ignore_appt_constraints=True,
+                                           ignore_cons_constraints=True))
+    sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
+
+    sim.register(epilepsy.Epilepsy(resourcefilepath=resourcefilepath))
+
+    sim.seed_rngs(0)
+    return sim
 
 
+def test_run(simulation):
+    simulation.make_initial_population(n=popsize)
+    simulation.simulate(end_date=end_date)
+
+
+def test_dtypes(simulation):
+    # check types of columns
+    df = simulation.population.props
+    orig = simulation.population.new_row
+    assert (df.dtypes == orig.dtypes).all()
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    simulation = simulation()
+    test_run(simulation)
+    t1 = time.time()
+    print('Time taken', t1 - t0)
