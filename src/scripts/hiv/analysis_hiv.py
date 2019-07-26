4899588d27626cd6f1ab7fc714b71d124e91eeda
Tara Mangal <t.mangal@imperial.ac.uk>
2019-07-26 16:24:04 +0100

    barplot with deaths by cause in analysis file

--- src/scripts/hiv/analysis_hiv.py
+++ src/scripts/hiv/analysis_hiv.py
@@ -23,7 +23,7 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 resourcefilepath = "./resources/"
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2012, 1, 1)
+end_date = Date(2018, 1, 1)
 popsize = 2000
 
 # Establish the simulation object
@@ -57,6 +57,7 @@ for name in logging.root.manager.loggerDict:
     if name.startswith("tlo"):
         logging.getLogger(name).setLevel(logging.WARNING)
 
+logging.getLogger("tlo.methods.demography").setLevel(logging.INFO)
 logging.getLogger('tlo.methods.hiv').setLevel(logging.INFO)
 logging.getLogger("tlo.methods.tb").setLevel(logging.INFO)
 logging.getLogger("tlo.methods.male_circumcision").setLevel(logging.INFO)
@@ -70,24 +71,58 @@ fh.flush()
 
 
 # %% read the results
-from tlo.analysis.utils import parse_log_file
-
 # out = sim.population.props
 # out.to_csv(r'C:\Users\Tara\Documents\TLO\outputs.csv', header=True)
-#
-# outputpath = './src/scripts/outputLogs/'
-#
-# # date-stamp to label log files and any other outputs
-# datestamp = datetime.date.today().strftime("__%Y_%m_%d")
-# logfile = outputpath + 'LogFile' + datestamp + '.log'
-#
-# output = parse_log_file(logfile)
-# #
-# deaths_df = output['tlo.methods.demography']['death']
-# deaths_df['date'] = pd.to_datetime(deaths_df['date'])
-# deaths_df['year'] = deaths_df['date'].dt.year
-# death_by_cause = deaths_df.groupby(['year','cause'])['person_id'].size()
-#
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from tlo.analysis.utils import parse_log_file
+
+outputpath = './src/scripts/hiv/'
+datestamp = datetime.date.today().strftime("__%Y_%m_%d")
+logfile = outputpath + 'LogFile' + datestamp + '.log'
+output = parse_log_file(logfile)
+
+# ------------------------------ PLOT DEATHS ----------------------------------------------------
+deaths_df = output['tlo.methods.demography']['death']
+deaths_df['date'] = pd.to_datetime(deaths_df['date'])
+deaths_df['year'] = deaths_df['date'].dt.year
+d_gp=deaths_df.groupby(['year', 'cause']).size().unstack().fillna(0)
+
+barWidth = 0.25
+# Set position of bar on X axis
+r1 = np.arange(len(d_gp.loc[:, 'Other']))
+r2 = [x + barWidth for x in r1]
+r3 = [x + barWidth for x in r2]
+
+# extract data for bars
+bars1 = d_gp.loc[:, 'Other']
+bars2 = d_gp.loc[:, 'hiv']
+bars3 = d_gp.loc[:, 'tb']
+
+colours = cm.inferno_r(np.linspace(.2,.8, 3))
+
+# Make the plot
+plt.bar(r1, bars1, color=colours[0], width=barWidth, edgecolor='white', label='Other')
+plt.bar(r2, bars2, color=colours[1], width=barWidth, edgecolor='white', label='hiv')
+plt.bar(r3, bars3, color=colours[2], width=barWidth, edgecolor='white', label='tb')
+
+
+# Add xticks on the middle of the group bars
+plt.xlabel('group', fontweight='bold')
+plt.xticks([r + barWidth for r in range(len(bars1))], ['2010', '2011', '2012', '2013', '2014', '2015', '2016',
+                                                       '2017'])
+plt.xlabel('Year')
+plt.ylabel('Number of deaths')
+plt.title('Number of deaths')
+
+# Create legend & Show graphic
+plt.legend(['Other','HIV', 'TB'], loc='upper left')
+plt.show()
+
+
 
 # TODO: Maybe add some graphs here to demonstrate the results? For example....
 # %% Demonstrate the HIV epidemic and it's impact on the population
