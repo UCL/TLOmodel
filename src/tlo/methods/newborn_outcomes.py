7de70d94818acb627a621906709678b8e84b85f4
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2019-07-31 13:57:52 +0100

    edits to HSIs

--- src/tlo/methods/newborn_outcomes.py
+++ src/tlo/methods/newborn_outcomes.py
@@ -69,6 +69,13 @@ class NewbornOutcomes(Module):
         params['cfr_neonatal_enceph'] = 0.15  # DUMMY
         params['cfr_ptb_comps'] = 0.3  # DUMMY
 
+        # Todo: Meet with TC to discuss as DALYS >150
+
+        # if 'HealthBurden' in self.sim.modules.keys():
+        #    params['daly_wt_mild_sneezing'] = self.sim.modules['HealthBurden'].get_daly_weight(50)
+        #    params['daly_wt_coughing'] = self.sim.modules['HealthBurden'].get_daly_weight(50)
+        #    params['daly_wt_advanced'] = self.sim.modules['HealthBurden'].get_daly_weight(589)
+
     def initialise_population(self, population):
 
         df = population.props  # a shortcut to the data-frame storing data for individuals
@@ -83,6 +90,9 @@ class NewbornOutcomes(Module):
         df['nb_ptb_comps'] = False
         df['nb_low_birth_weight'] = False
         df['nb_death_after_birth'] = False
+
+        # Register this disease module with the health system
+        self.sim.modules['HealthSystem'].register_disease_module(self)
 
     def initialise_simulation(self, sim):
 
@@ -105,10 +115,9 @@ class NewbornOutcomes(Module):
 
         # Newborns delivered at less than 37 weeks are allocated as either late or early preterm based on the
         # gestation at labour
-        #if df.at[mother_id, 'la_labour'] == 'early_preterm_labour':
         if mni[mother_id]['labour_state'] == 'EPTL':
             df.at[child_id, 'nb_early_preterm'] = True
-        #elif df.at[mother_id, 'la_labour'] == 'late_preterm_labour':
+
         elif mni[mother_id]['labour_state']== 'LPTL':
             df.at[child_id, 'nb_late_preterm'] = True
         else:
@@ -118,6 +127,35 @@ class NewbornOutcomes(Module):
         if df.at[child_id, 'is_alive'] & ~mni[mother_id]['stillbirth_in_labour']:
             self.sim.schedule_event(newborn_outcomes.NewbornOutcomeEvent(self.sim.modules['NewbornOutcomes'], child_id,
                                                             cause='newborn outcomes event'), self.sim.date)
+
+    def on_hsi_alert(self, person_id, treatment_id):
+        """
+        This is called whenever there is an HSI event commissioned by one of the other disease modules.
+        """
+
+        logger.debug('This is NewbornOutcomes, being alerted about a health system interaction '
+                     'person %d for: %s', person_id, treatment_id)
+
+ #   def report_daly_values(self):
+        # This must send back a pd.Series or pd.DataFrame that reports on the average daly-weights that have been
+        # experienced by persons in the previous month. Only rows for alive-persons must be returned.
+        # The names of the series of columns is taken to be the label of the cause of this disability.
+        # It will be recorded by the healthburden module as <ModuleName>_<Cause>.
+
+  #      logger.debug('This is NewbornOutcomes reporting my health values')
+
+   #     df = self.sim.population.props  # shortcut to population properties dataframe
+    #    p = self.parameters
+
+#        health_values = df.loc[df.is_alive, 'mi_specific_symptoms'].map({
+#            'none': 0,
+#            'mild sneezing': p['daly_wt_mild_sneezing'],
+#            'coughing and irritable': p['daly_wt_coughing'],
+#            'extreme emergency': p['daly_wt_advanced']
+#        })
+#        health_values.name = 'Mockitis Symptoms'    # label the cause of this disability
+
+#        return health_values.loc[df.is_alive]   # returns the series
 
 
 class NewbornOutcomeEvent(Event, IndividualScopeEventMixin):
@@ -130,10 +168,12 @@ class NewbornOutcomeEvent(Event, IndividualScopeEventMixin):
         df = self.sim.population.props
         params = self.module.parameters
         m = self
+        mni = self.sim.modules['Labour'].mother_and_newborn_info
 
     # ===================================== LOW BIRTH-WEIGHT STATUS ==================================================
-        # Here we determine if a newly born neonate will be of low birth weight
+        # todo: n.b. low birth weight and SGA may be handeled in conjuction with malnutrition?
 
+        # Here we determine if a newly born neonate will be of low birth weight
         # Birth order, mother weight, mother height, mother education and family wealth - Guassian coefficients-
         # not risks.
         rf1 = 1
@@ -195,14 +235,26 @@ class NewbornOutcomeEvent(Event, IndividualScopeEventMixin):
             if random < eff_prob_enchep:
                 df.at[individual_id, 'nb_neonatal_enchep'] = True
 
-        # Currently we schedule all newborns who have experienced any of the above complications to go through the
-        # NewbornDeathEvent
-        if df.at[individual_id, 'nb_neonatal_enchep'] or df.at[individual_id, 'nb_neonatal_sepsis'] or \
-            df.at[individual_id, 'nb_congenital_anomaly'] or df.at[individual_id, 'nb_ptb_comps']:
+            # If this neonate has been delivered in a facility
+
+        #   if mni[individual_id]['delivery_setting'] == 'FD': todo: how to link to mother ID
+            event = HSI_NewbornOutcomes_ReceivesCareFollowingDelivery(self.module, person_id=individual_id)
+            self.sim.modules['HealthSystem'].schedule_hsi_event(event,
+                                                                    priority=0,
+                                                                    topen=self.sim.date,
+                                                                    tclose=self.sim.date + DateOffset(days=1)
+                                                                    )
+            logger.info('This is NewbornOutcomesEvent scheduling HSI_NewbornOutcomes_ReceivesCareFollowingDeliveryd '
+                        'for person %d', individual_id)
+
+            # todo: will we consider care seeking for newborns from women who deliver in the community??
+
             self.sim.schedule_event(newborn_outcomes.NewbornDeathEvent(self.module, individual_id,
                                                                        cause='neonatal compilications')
                                                       ,self.sim.date)
-            #TODO is hte passing of 'clause' requied here? is any other cause ever used? The event itself already seems "cause specific"?
+            logger.info('This is NewbornOutcomesEvent scheduling NewbornDeathEvent for person %d', individual_id)
+
+
 
 class NewbornDeathEvent(Event, IndividualScopeEventMixin):
 
@@ -244,8 +296,47 @@ class NewbornDeathEvent(Event, IndividualScopeEventMixin):
         if df.at[individual_id, 'nb_death_after_birth']:
             self.sim.schedule_event(demography.InstantaneousDeath(self.module, individual_id,
                                                                   cause="neonatal complications"), self.sim.date)
+            logger.info('This is NewbornDeathEvent scheduling a death for person %d on date %s who died due to '
+                        ' complications following birth', individual_id, self.sim.date)
+
+            logger.info('%s|neonatal_death|%s', self.sim.date, #TODO: are we just recording all neonatal deaths?
+                        {'age': df.at[individual_id, 'age_years'],
+                         'person_id': individual_id})
+
 
         #TODO: just confirm that you want all the deaths to happen on the same of birth
+
+
+# ================================ Health System Interaction Events ================================================
+
+class HSI_NewbornOutcomes_ReceivesCareFollowingDelivery(Event, IndividualScopeEventMixin):
+    """
+    This is a Health System Interaction Event.
+    This is event manages care received by newborns following a facility delivery, including referall for additional
+     treatment in the case of an emergency
+    """
+
+    def __init__(self, module, person_id):
+        super().__init__(module, person_id=person_id)
+
+        # Get a blank footprint and then edit to define call on resources of this treatment event
+        the_appt_footprint = self.sim.modules['HealthSystem'].get_blank_appt_footprint()
+        the_appt_footprint['AccidentsandEmerg'] = 1  # Todo: review
+
+        # Define the necessary information for an HSI
+        self.TREATMENT_ID = 'NewbornOutcomes_ReceivesCareFollowingDelivery'
+        self.APPT_FOOTPRINT = the_appt_footprint
+        self.CONS_FOOTPRINT = self.sim.modules['HealthSystem'].get_blank_cons_footprint()
+        self.ACCEPTED_FACILITY_LEVELS = [1,2,3]     # This enforces that the apppointment must be run at that facility-level
+        self.ALERT_OTHER_DISEASES = []
+
+    def apply(self, person_id):
+
+        logger.debug('This is HSI_NewbornOutcomes_ReceivesCareFollowingDelivery,'
+                     '  person %d is receiving care from a skilled birth attendant following their birth',
+                     person_id)
+
+
 
 
 class NewbornOutcomesLoggingEvent(RegularEvent, PopulationScopeEventMixin):
