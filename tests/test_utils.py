09cfe2b02e92db5b0ae6484f879cd20016b692f2
Stefan Piatek <s.piatek@ucl.ac.uk>
2019-08-08 16:53:41 +0100

    Add utility function for state transitions
    Currently looping through each transition, is there a way to vectorise?

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -70,3 +70,50 @@ def test_show_changes_same_size(mock_sim):
     assert html.count('color:  black;') == 4  # unchanged
     assert html.count('color:  red;') == 4    # changed
     assert html.count('background-color:  yellow;') == 0
+
+
+class TestTransitionsStates:
+    def setup(self):
+
+        self.states = ['a', 'b', 'c', 'd']
+
+        self.df = pd.DataFrame({'state': self.states * 1000,
+                                'other_data_1': range(0, 4000),
+                                'is_alive': True})
+        self.prob_matrix = pd.DataFrame(columns=self.states, index=self.states)
+        # key is original state, values are probability for new states
+        #                        A    B     C    D
+        self.prob_matrix['a'] = [None, 0.1, None, None]
+        self.prob_matrix['b'] = [0.1, None, 0.6, None]
+        self.prob_matrix['c'] = [None, 0.2, None, 0.2]
+        self.prob_matrix['d'] = [None, None, 0.3, None]
+        # set seed for testing. Is there a better way do test this?
+        self.seed = 1234
+
+        # repeat each state by the list of integers
+        nested_states = [
+            list(state * repeat) for state, repeat in zip(self.states, [894, 404, 1606, 1096])
+        ]
+        self.expected = pd.DataFrame({'state': sum(nested_states, []),
+                                 'other_data_1': pd.Series(range(0, 4000)),
+                                 'is_alive': True})
+
+    def test_all_alive(self):
+        """Simple case, should change roughly to the probabilites given"""
+        expected = self.expected.copy()
+        output = tlo.util.transition_states(self.df, 'state', self.prob_matrix, seed=self.seed)
+        output_merged = self.df.copy()
+        output_merged['state'] = output
+        expected_size = expected.groupby('state').size()
+        output_size = output_merged.groupby('state').size()
+        assert (expected_size + output_size).equals(expected_size * 2)
+
+    def test_all_dead(self):
+        """No changes should be made if all individuals are dead"""
+        df = self.df.copy()
+        df['is_alive'] = False
+        output = tlo.util.transition_states(df, 'state', self.prob_matrix, seed=self.seed)
+        df['state'] = output
+        expected_size = self.df.groupby('state').size()
+        output_size = df.groupby('state').size()
+        assert (expected_size + output_size).equals(expected_size * 2)
