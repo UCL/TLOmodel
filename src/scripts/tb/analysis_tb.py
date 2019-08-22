9d1131e415661ba5216679f9247954bf958b8961
tdm32 <t.mangal@imperial.ac.uk>
2019-08-22 11:36:58 +0100

    tb mortality rates high and tb transmission rate high
    Former-commit-id: b983ebc7b34ed243a2c34479d61a984ce971df12

--- src/scripts/tb/analysis_tb.py
+++ src/scripts/tb/analysis_tb.py
@@ -1,6 +1,8 @@
 import datetime
 import logging
 import os
+import time
+
 
 from tlo import Date, Simulation
 from tlo.methods import (
@@ -23,7 +25,7 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 resourcefilepath = "./resources/"
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2012, 12, 1)
+end_date = Date(2012, 1, 1)
 popsize = 500
 
 # Establish the simulation object
@@ -44,6 +46,8 @@ logging.getLogger().addHandler(fh)
 # '*' means everything. It will allow any treatment_id that begins with a stub (e.g. Mockitis*)
 service_availability = ["*"]
 
+t0 = time.time()
+
 # Register the appropriate modules
 sim.register(demography.Demography(resourcefilepath=resourcefilepath))
 sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath))
@@ -57,8 +61,8 @@ for name in logging.root.manager.loggerDict:
     if name.startswith("tlo"):
         logging.getLogger(name).setLevel(logging.WARNING)
 
-logging.getLogger("tlo.methods.tb").setLevel(logging.DEBUG)
-# logging.getLogger("tlo.methods.hiv").setLevel(logging.INFO)
+logging.getLogger("tlo.methods.tb").setLevel(logging.INFO)
+logging.getLogger("tlo.methods.hiv").setLevel(logging.INFO)
 
 # Run the simulation and flush the logger
 sim.seed_rngs(0)
@@ -66,3 +70,29 @@ sim.make_initial_population(n=popsize)
 sim.simulate(end_date=end_date)
 fh.flush()
 # fh.close()
+
+t1 = time.time()
+print('Time taken, mins', (t1 - t0) / 60)
+
+# %% read the results
+# out = sim.population.props
+# out.to_csv(r'C:\Users\Tara\Documents\TLO\outputs.csv', header=True)
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import cm
+# from tlo.analysis.utils import parse_log_file
+#
+#
+# #
+# # outputpath = './src/scripts/tb/'
+# # datestamp = datetime.date.today().strftime("__%Y_%m_%d")
+# #
+# # logfile = outputpath + 'LogFile' + datestamp + '.log'
+# ##
+# logfile = './src/scripts/tb/LogFile__2019_08_22.log'
+#
+# output = parse_log_file(logfile)
+#
+# tb_df = output['tlo.methods.tb']['tb_prevalence']
