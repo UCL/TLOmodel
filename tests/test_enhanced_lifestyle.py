ac4d53f1066c3f4fbdcf102d26abd5d1223a83e3
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2019-09-02 16:41:52 +0100

    re-writing with or and with reading all parameter values (taking out from main body of program)

--- tests/test_enhanced_lifestyle.py
+++ tests/test_enhanced_lifestyle.py
@@ -9,8 +9,8 @@ from tlo.methods import demography, enhanced_lifestyle
 from pathlib import Path
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2012, 1, 1)
-popsize = 10000
+end_date = Date(2019, 10, 1)
+popsize = 30000
 
 
 @pytest.fixture(autouse=True)
