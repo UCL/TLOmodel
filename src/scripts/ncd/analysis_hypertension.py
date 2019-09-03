0921dd3fedfe7617655ca396e989431d0fd1853b
mikaelasmit <mikaelasmit@users.noreply.github.com>
2019-09-03 07:41:22 +0200

    Logger is working and so is plotting
    To Do:
    1. Read in original data with 95% CI (for plotting)
    2. Plot Age specific prevalence
    3. Trial outreach campaign
    4. Trial HSI event

--- src/scripts/ncd/analysis_hypertension.py
+++ src/scripts/ncd/analysis_hypertension.py
@@ -1,22 +1,30 @@
 import datetime
 import logging
 import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 from tlo import Date, Simulation
 from tlo.analysis.utils import parse_log_file
-from tlo.methods import demography, healthburden, healthsystem, lifestyle, t2dm, hypertension #, chronicsyndrome, mockitis,
+from tlo.methods import demography, healthburden, healthsystem, lifestyle, hypertension #, t2dm, chronicsyndrome, mockitis,
 
 # [NB. Working directory must be set to the root of TLO: TLOmodel/]
 # TODO: adapt to NCD analysis
 
 # Where will output go
-outputpath = ''
+outputpath = ""
 
 # date-stamp to label log files and any other outputs
 datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 
 # The resource files
-resourcefilepath = 'resources'
+resourcefilepath = Path("./resources")
+
+
+# %% Run the Simulation
 
 start_date = Date(year=2010, month=1, day=1)
 end_date = Date(year=2015, month=12, day=31)
@@ -36,30 +44,53 @@ fh.setFormatter(fr)
 logging.getLogger().addHandler(fh)
 
 
+# -----------------------------------------------------------------
 # ----- Control over the types of intervention that can occur -----
 # Make a list that contains the treatment_id that will be allowed. Empty list means nothing allowed.
 # '*' means everything. It will allow any treatment_id that begins with a stub (e.g. Mockitis*)
 service_availability = ['*']
 
-# -----------------------------------------------------------------
 
+# -----------------------------------------------------------------
 # Register the appropriate modules
 sim.register(demography.Demography(resourcefilepath=resourcefilepath))
 sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
                                        service_availability=service_availability))
 sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
 sim.register(lifestyle.Lifestyle())
-#sim.register(mockitis.Mockitis())
 sim.register(hypertension.HT(resourcefilepath=resourcefilepath))
-sim.register(t2dm.T2DM())
-#sim.register(chronicsyndrome.ChronicSyndrome())
+# sim.register(t2dm.T2DM(resourcefilepath=resourcefilepath))
 
+
+# -----------------------------------------------------------------
 # Run the simulation and flush the logger
 sim.seed_rngs(0)
 sim.make_initial_population(n=popsize)
 sim.simulate(end_date=end_date)
 fh.flush()
 
+
 # %% read the results
 output = parse_log_file(logfile)
+
+# -----------------------------------------------------------------
+# Plot output
+
+# Load Population and normalise
+pop_df = output["tlo.methods.demography"]["population"]
+Model_Years = pd.to_datetime(pop_df.date)
+Model_Pop = pop_df.total
+Model_Pop_Normalised = (
+     100 * np.asarray(Model_Pop) / np.asarray(Model_Pop[Model_Years == "2010-01-01"])
+)
+prev_df = output["tlo.methods.hypertension"]["prevalence"]
+Model_Prev_total = prev_df.total
+
+plt.plot(np.asarray(Model_Years), Model_Prev_total)
+
+plt.show()
+
+
+
+
 
