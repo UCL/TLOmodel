280d24ddb14a95b821bdbf280c2c21e4ee9baf9d
Asif Tamuri <tamuri@gmail.com>
2019-09-11 11:32:10 +0100

    fix formatting (mostly long lines)
    Former-commit-id: d7e9fbf0c065c223c7f6d40d502596d38d5ce57f

--- tests/test_contraception.py
+++ tests/test_contraception.py
@@ -5,10 +5,7 @@ from pathlib import Path
 import pytest
 
 from tlo import Date, Simulation
-from tlo.methods import demography, contraception
-
-workbook_name = 'ResourceFile_Contraception.xlsx'
-workbook2_name = 'ResourceFile_DemographicData.xlsx'
+from tlo.methods import contraception, demography
 
 start_date = Date(2010, 1, 1)
 end_date = Date(2013, 1, 1)
@@ -18,10 +15,6 @@ popsize = 200
 @pytest.fixture(scope='module')
 def simulation():
     resourcefilepath = Path(os.path.dirname(__file__)) / '../resources'
-
-    #contraception_workbook = Path(os.path.dirname(__file__)) / '../resources', workbook_name)
-    #demography_workbook = Path(os.path.dirname(__file__)) / '../resources', workbook2_name)
-
     sim = Simulation(start_date=start_date)
     demography_module = demography.Demography(resourcefilepath=resourcefilepath)
     sim.register(demography_module)
@@ -30,11 +23,12 @@ def simulation():
     sim.seed_rngs(0)
     return sim
 
+
 def __check_properties(df):
     assert not ((df.sex == 'M') & df.co_contraceptive != 'not_using').any()
 
+
 def test_run(simulation):
-    simulation.make_initial_population(n=popsize)
     simulation.simulate(end_date=end_date)
 
 
