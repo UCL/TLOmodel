75ac5417a9701df0bb617695a0317620d07b01a8
Asif Tamuri <tamuri@gmail.com>
2019-09-12 10:25:37 +0100

    arrange imports

--- src/scripts/contraception/analysis_contraception.py
+++ src/scripts/contraception/analysis_contraception.py
@@ -4,14 +4,14 @@ import logging
 import os
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 from tlo import Date, Simulation
 from tlo.analysis.utils import parse_log_file
-from tlo.methods import demography, contraception
+from tlo.methods import contraception, demography
 
 # Where will output go - by default, wherever this script is run
 outputpath = ''
