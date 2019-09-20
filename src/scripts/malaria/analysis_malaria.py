22bac4cd47fd25365a6ae46e0186994eacca3993
tdm32 <t.mangal@imperial.ac.uk>
2019-09-20 15:52:24 +0100

    incidence (asym/clinical/severe) included by age and district each month. interventio coverage from MAP - annual.

--- src/scripts/malaria/analysis_malaria.py
+++ src/scripts/malaria/analysis_malaria.py
@@ -23,7 +23,7 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 resourcefilepath = "./resources/"
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2015, 1, 1)
+end_date = Date(2012, 1, 1)
 popsize = 500
 
 # Establish the simulation object
