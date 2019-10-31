2c5fa17a1e615c11bcdc83b9f0b5124a67965b76
Stefan Piatek <s.piatek@ucl.ac.uk>
2019-10-31 12:37:32 +0000

    Revert healthsystem unit tests to master version
    Former-commit-id: 757e400a770c321e98f33b8c3d6e9da554c52b63

--- tests/test_healthburden.py
+++ tests/test_healthburden.py
@@ -43,9 +43,19 @@ def check_dtypes(simulation):
     assert (df.dtypes == orig.dtypes).all()
 
 
-def test_run_with_healthsystem_and_healthburden_no_intvs():
+def test_run_with_healthburden_with_dummy_diseases():
+    # There should be no events run or scheduled
+
+    # Get ready for temporary log-file
+    f = tempfile.NamedTemporaryFile(dir='.')
+    fh = logging.FileHandler(f.name)
+    fr = logging.Formatter("%(levelname)s|%(name)s|%(message)s")
+    fh.setFormatter(fr)
+    logging.getLogger().addHandler(fh)
+
     # Establish the simulation object
     sim = Simulation(start_date=start_date)
+    sim.seed_rngs(0)
 
     # Define the service availability as null
     service_availability = []
@@ -58,43 +68,12 @@ def test_run_with_healthsystem_and_healthburden_no_intvs():
                                            mode_appt_constraints=0))
     sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
     sim.register(enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath))
-
-    # Run the simulation and flush the logger
-    sim.make_initial_population(n=popsize)
-    sim.simulate(end_date=end_date)
-
-    check_dtypes(sim)
-
-
-def test_run_with_healthsystem_and_healthburden_intvs_on():
-    # Get ready for temporary log-file
-    f = tempfile.NamedTemporaryFile(dir='.')
-    fh = logging.FileHandler(f.name)
-    fr = logging.Formatter("%(levelname)s|%(name)s|%(message)s")
-    fh.setFormatter(fr)
-    logging.getLogger().addHandler(fh)
-    # Establish the simulation object
-    sim = Simulation(start_date=start_date)
-    sim.seed_rngs(0)
-
-    # Define the service availability
-    service_availability = list(['Mockitis*', 'ChronicSyndrome*'])
-
-    # Register the appropriate modules
-    sim.register(demography.Demography(resourcefilepath=resourcefilepath))
-    sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
-    sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
-                                           service_availability=service_availability,
-                                           capabilities_coefficient=0.0,
-                                           mode_appt_constraints=0))
-    sim.register(enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath))
     sim.register(mockitis.Mockitis())
     sim.register(chronicsyndrome.ChronicSyndrome())
 
     # Run the simulation and flush the logger
     sim.make_initial_population(n=popsize)
     sim.simulate(end_date=end_date)
-
     check_dtypes(sim)
 
     # read the results
@@ -103,7 +82,7 @@ def test_run_with_healthsystem_and_healthburden_intvs_on():
     f.close()
 
     # Do the checks
-    # correctly configured index (outputs on 31st decemnber in each year of simulation for each age/sex group)
+    # correctly configured index (outputs on 31st december in each year of simulation for each age/sex group)
     dalys = output['tlo.methods.healthburden']['DALYS']
     age_index = sim.modules['Demography'].AGE_RANGE_CATEGORIES
     sex_index = ['M', 'F']
