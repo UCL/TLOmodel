8d8dda3dd8f4f8e6d498f1dd164aad72057f79ba
Stefan Piatek <s.piatek@ucl.ac.uk>
2019-11-01 14:45:05 +0000

    Revert back to previous logging level for demography

--- src/tlo/methods/demography.py
+++ src/tlo/methods/demography.py
@@ -16,7 +16,7 @@ from tlo import DateOffset, Module, Parameter, Property, Types
 from tlo.events import Event, IndividualScopeEventMixin, PopulationScopeEventMixin, RegularEvent
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.CRITICAL)
+logger.setLevel(logging.INFO)
 
 # Limits for setting up age range categories
 MIN_AGE_FOR_RANGE = 0
