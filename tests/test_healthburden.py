c1f64775b1450aaaef4e4bd8045ad82760438462
Asif Tamuri <tamuri@gmail.com>
2019-11-01 15:40:47 +0000

    Up test popsize for healthburden/system tests
    Former-commit-id: 8da211e0425671956fc52a41c4e320a5f39afec9

--- tests/test_healthburden.py
+++ tests/test_healthburden.py
@@ -25,7 +25,7 @@ except NameError:
 
 start_date = Date(2010, 1, 1)
 end_date = Date(2012, 1, 1)
-popsize = 10
+popsize = 100
 
 
 # Simply test whether the system runs under multiple configurations of the healthsystem
