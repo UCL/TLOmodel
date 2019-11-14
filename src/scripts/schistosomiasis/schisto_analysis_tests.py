dd8944f00221f569d02343bd6a0d364640502bc6
ihawryluk <iwona.haa@gmail.com>
2019-11-14 16:12:36 +0000

    MDA coverage data alligned with the available data
    Former-commit-id: 73b369ed502ded34c515e382b576d991f0a0718f

--- src/scripts/schistosomiasis/schisto_analysis_tests.py
+++ src/scripts/schistosomiasis/schisto_analysis_tests.py
@@ -22,8 +22,8 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 # The resource files
 resourcefilepath = Path("./resources")
 # resourcefilepath = Path(os.path.dirname(__file__)) / '../../../resources'
-start_date = Date(2010, 1, 1)
-end_date = Date(2011, 1, 1)
+start_date = Date(2015, 1, 1)
+end_date = Date(2019, 1, 1)
 popsize = 1000
 
 # Establish the simulation object
