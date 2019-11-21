341e5f3c36b47f1e3b843ae51b2818e3014c4768
Ines Lin <ines.lin@ucl.ac.uk>
2019-11-21 11:50:02 +0000

    new pneumonia file
    Former-commit-id: 6d99df3b164ba65014285a2890b29587283e9b64

--- tests/test_childhood_pneumonia.py
+++ tests/test_childhood_pneumonia.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pytest
 from tlo import Date, Simulation
-from tlo.methods import demography, lifestyle, new_pneumonia, healthsystem
+from tlo.methods import demography, lifestyle, pneumonia2, healthsystem
 
 workbook_name = 'demography.xlsx'
 
@@ -25,7 +25,7 @@ def simulation():
     sim.register(demography.Demography(resourcefilepath=resourcefilepath))
     sim.register(lifestyle.Lifestyle())
     sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath))
-    sim.register(new_pneumonia.NewPneumonia(resourcefilepath=resourcefilepath))
+    sim.register(pneumonia2.NewPneumonia2(resourcefilepath=resourcefilepath))
     logging.getLogger('tlo.methods.lifestyle').setLevel(logging.CRITICAL)
 #   logging.getLogger('tlo.methods.lifestyle').setLevel(logging.WARNING)
     sim.seed_rngs(0)
