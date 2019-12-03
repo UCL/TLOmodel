784cd2ee0fb765a7dd723f0c192eb934d411dcf0
Tim Hallett <timothy.hallett@imperial.ac.uk>
2019-12-03 18:57:45 +0000

    basic version of the SymptomManager

--- src/tlo/methods/symptommanager.py
+++ src/tlo/methods/symptommanager.py
@@ -15,36 +15,17 @@ from tlo.population import logger
 
 class SymptomManager(Module):
     """
-    One line summary goes here...
-
-    All disease modules need to be implemented as a class inheriting from Module.
-    They need to provide several methods which will be called by the simulation
-    framework:
-    * `read_parameters(data_folder)`
-    * `initialise_population(population)`
-    * `initialise_simulation(sim)`
-    * `on_birth(mother, child)` [If this is disease module]
-    * `on_hsi_alert(person_id, treatment_id)` [If this is disease module]
-    *  `report_daly_values()` [If this is disease module]
-
+    This module is used to track the symptoms of persons. The addition and removal of symptoms is handled here.
     """
 
-    # Here we declare parameters for this module. Each parameter has a name, data type,
-    # and longer description.
-    PARAMETERS = {
-        'parameter_a': Parameter(
-            Types.REAL, 'Description of parameter a'),
-    }
+    list_of_symptoms = ['a', 'b', 'c']
 
-    # Next we declare the properties of individuals that this module provides.
-    # Again each has a name, type and description. In addition, properties may be marked
-    # as optional if they can be undefined for a given individual.
+    # These are properties of individual for the prescence/abscence of symptoms.
+    # A value > 0 implies the symptom is present.
 
-    # Note that all properties must have a two letter prefix that identifies them to this module.
-
-    PROPERTIES = {
-        'sk_property_a': Property(Types.BOOL, 'Description of property a'),
-    }
+    PROPERTIES = dict()
+    for symp in list_of_symptoms:
+        PROPERTIES['sy_' + symp] = Property(Types.INT, 'Prescence of symptom ' + symp)
 
     def __init__(self, name=None, resourcefilepath=None):
         # NB. Parameters passed to the module can be inserted in the __init__ definition.
@@ -59,168 +40,81 @@ class SymptomManager(Module):
         pass
 
     def initialise_population(self, population):
-        """Set our property values for the initial population.
-
-        This method is called by the simulation when creating the initial population, and is
-        responsible for assigning initial values, for every individual, of those properties
-        'owned' by this module, i.e. those declared in the PROPERTIES dictionary above.
-
-        :param population: the population of individuals
         """
-        raise NotImplementedError
+        Initialise all persons to have no symptoms at simulation initiation
+        """
+
+        # Get the variable names that are defined
+        symptom_var_names = [col for col in self.sim.population.props if col.startswith('sy_') ]
+        self.symptom_var_names = symptom_var_names
+
+        # Set all to zero
+        self.sim.population.props[symptom_var_names]= 0
+
+        # Get and save the list_of_symptoms
+        self.list_of_symptoms = [a.split('sy_')[1] for a in symptom_var_names]
+
 
     def initialise_simulation(self, sim):
-        """Get ready for simulation start.
-
-        This method is called just before the main simulation loop begins, and after all
-        modules have read their parameters and the initial population has been created.
-        It is a good place to add initial events to the event queue.
-
-        If this is a disease module, register this disease module with the healthsystem:
-        self.sim.modules['HealthSystem'].register_disease_module(self)
         """
-
-        raise NotImplementedError
+        Nothing to do before simulation starts
+        """
+        pass
 
     def on_birth(self, mother_id, child_id):
-        """Initialise our properties for a newborn individual.
-
-        This is called by the simulation whenever a new person is born.
-
-        :param mother_id: the mother for this child
-        :param child_id: the new child
         """
-        raise NotImplementedError
-
-    def report_daly_values(self):
-        # This must send back a pd.Series or pd.DataFrame that reports on the average daly-weights that have been
-        # experienced by persons in the previous month. Only rows for alive-persons must be returned.
-        # The names of the series of columns is taken to be the label of the cause of this disability.
-        # It will be recorded by the healthburden module as <ModuleName>_<Cause>.
-
-        # To return a value of 0.0 (fully health) for everyone, use:
-        # df = self.sim.popultion.props
-        # return pd.Series(index=df.index[df.is_alive],data=0.0)
-
-        raise NotImplementedError
-
-    def on_hsi_alert(self, person_id, treatment_id):
+        Set that the child will have no symptoms by default
         """
-        This is called whenever there is an HSI event commissioned by one of the other disease modules.
+        self.sim.population.props[child_id,self.symptom_var_names]=0
+
+    def chg_symptom(self, person_id, symptom_string, add_or_remove ,disease_module):
+        """
+        This is how disease module report that a person has developed a new symptom or an existing symptom has resolved.
+
+        :param person_id: The person_id (int or list of int) for whom the symptom changes
+        :param symptom_string: The string for the symptom (must be one of the list_of_symptoms)
+        :param add_or_remove: '+' to add the symptom or '-' to remove the symptom
+        :param disease_module: pointer to the disease module that is reporting this change in symptom
         """
 
-        raise NotImplementedError
+        # Check that the person_id is for an existing and alive person
+        alive_person_ids= list(self.sim.population.props.loc[self.sim.population.props.is_alive==True].index)
+        if type(person_id) is not list:
+            person_id= [person_id]
+        for p in person_id:
+            assert p in alive_person_ids
+
+        # Check that the symptom_string is legitimate
+        assert symptom_string in self.list_of_symptoms
+        symptom_var_name = 'sy_' + symptom_string
+        assert symptom_var_name in self.sim.population.props.columns
+
+        # Check that the add/remove signal is legitimate
+        assert add_or_remove in ['+','-']
+
+        # Check that the provided disease_module is a registered disease_module
+        assert disease_module in self.sim.modules['HealthSystem'].registered_disease_modules.values()
 
 
-# ---------------------------------------------------------------------------------------------------------
-#   DISEASE MODULE EVENTS
-#
-#   These are the events which drive the simulation of the disease. It may be a regular event that updates
-#   the status of all the population of subsections of it at one time. There may also be a set of events
-#   that represent disease events for particular persons.
-# ---------------------------------------------------------------------------------------------------------
+        # Make the operation:
+        if add_or_remove == '+':
+            # Add the symptom
+            self.sim.population.props.loc[person_id,symptom_var_name]+= 1
 
-class Skeleton_Event(RegularEvent, PopulationScopeEventMixin):
-    """A skeleton class for an event
+        else:
+            # Remove the symptom
+            assert (self.sim.population.props.loc[person_id, symptom_var_name]>0).all(),\
+                'Warning: Request to remove symptoms from individuals that do not have the symptom'
 
-    Regular events automatically reschedule themselves at a fixed frequency,
-    and thus implement discrete timestep type behaviour. The frequency is
-    specified when calling the base class constructor in our __init__ method.
-    """
-
-    def __init__(self, module):
-        """One line summary here
-
-        We need to pass the frequency at which we want to occur to the base class
-        constructor using super(). We also pass the module that created this event,
-        so that random number generators can be scoped per-module.
-
-        :param module: the module that created this event
-        """
-        super().__init__(module, frequency=DateOffset(months=1))
-        assert isinstance(module, Skeleton)
-
-    def apply(self, population):
-        """Apply this event to the population.
-
-        :param population: the current population
-        """
-        raise NotImplementedError
+            self.sim.population.props.loc[person_id, symptom_var_name] = \
+                (self.sim.population.props.loc[person_id, symptom_var_name] - 1).clip(lower=0)
 
 
-# ---------------------------------------------------------------------------------------------------------
-#   LOGGING EVENTS
-#
-#   Put the logging events here. There should be a regular logger outputting current states of the
-#   population. There may also be a loggig event that is driven by particular events.
-# ---------------------------------------------------------------------------------------------------------
-
-class Skeleton_LoggingEvent(RegularEvent, PopulationScopeEventMixin):
-    def __init__(self, module):
-        """Produce a summary of the numbers of people with respect to the action of this module.
-        This is a regular event that can output current states of people or cumulative events since last logging event.
-        """
-
-        # run this event every year
-        self.repeat = 12
-        super().__init__(module, frequency=DateOffset(months=self.repeat))
-        assert isinstance(module, Skeleton)
-
-    def apply(self, population):
-        # Make some summary statitics
-
-        dict_to_output = {
-            'Metric_One': 1.0,
-            'Metric_Two': 2.0
-        }
-
-        logger.info('%s|summary_12m|%s', self.sim.date, dict_to_output)
+        # Logging
 
 
-# ---------------------------------------------------------------------------------------------------------
-#   HEALTH SYSTEM INTERACTION EVENTS
-#
-#   Here are all the different Health System Interactions Events that this module will use.
-# ---------------------------------------------------------------------------------------------------------
+        # TODO: Chexk that this works for person_id lists of any lenth
 
-class HSI_Skeleton_Example_Interaction(HSI_Event, IndividualScopeEventMixin):
-    """This is a Health System Interaction Event. An interaction with the healthsystem are encapsulated in events
-    like this.
-    It must begin HSI_<Module_Name>_Description
-    """
 
-    def __init__(self, module, person_id):
-        super().__init__(module, person_id=person_id)
-        assert isinstance(module, Skeleton)
-
-        # Define the call on resources of this treatment event: Time of Officers (Appointments)
-        #   - get an 'empty' footprint:
-        the_appt_footprint = self.sim.modules['HealthSystem'].get_blank_appt_footprint()
-        #   - update to reflect the appointments that are required
-        the_appt_footprint['Over5OPD'] = 1  # This requires one out patient
-
-        # Define the facilities at which this event can occur (only one is allowed)
-        # Choose from: list(pd.unique(self.sim.modules['HealthSystem'].parameters['Facilities_For_Each_District']
-        #                            ['Facility_Level']))
-        the_accepted_facility_level = 0
-
-        # Define the necessary information for an HSI
-        self.TREATMENT_ID = 'Skeleton_Example_Interaction'  # This must begin with the module name
-        self.EXPECTED_APPT_FOOTPRINT = the_appt_footprint
-        self.ACCEPTED_FACILITY_LEVEL = the_accepted_facility_level
-        self.ALERT_OTHER_DISEASES = []
-
-    def apply(self, person_id, squeeze_factor):
-        """
-        Do the action that take place in this health system interaction, in light of squeeze_factor
-        Can reutrn an updated APPT_FOOTPRINT if this differs from the declaration in self.EXPECTED_APPT_FOOTPRINT
-        """
-        pass
-
-    def did_not_run(self):
-        """
-        Do any action that is neccessary when the health system interaction is not run.
-        This is called each day that the HSI is 'due' but not run due to insufficient health system capabilities
-
-        """
-        pass
+        # Check that all the symptom variables are in good condition
+        assert (self.sim.population.props[self.symptom_var_names]>0).all().all()
