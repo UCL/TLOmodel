fa7899f2379027d8a483216e4afaa4375d4ed9f3
Tara Mangal <t.mangal@imperial.ac.uk>
2019-12-09 09:38:59 +0000

    confidence intervals added to plot
    Former-commit-id: 3e038391eade3e8a1c051bcc98de04608af70b30

--- src/scripts/malaria/analysis_malaria.py
+++ src/scripts/malaria/analysis_malaria.py
@@ -91,7 +91,9 @@ from pathlib import Path
 
 outputpath = './src/scripts/malaria/'
 datestamp = datetime.date.today().strftime("__%Y_%m_%d")
-logfile = outputpath + 'LogFile' + datestamp + '.log'
+# logfile = outputpath + 'LogFile' + datestamp + '.log'
+
+logfile = outputpath + 'LogFile' + '__2019_12_08' + '.log'
 output = parse_log_file(logfile)
 #
 resourcefilepath = Path("./resources")
@@ -103,79 +105,87 @@ mort = output['tlo.methods.malaria']['ma_mortality']
 
 # get model output dates in correct format
 model_years = pd.to_datetime(inc.date)
-start_date = Date(2010, 1, 1)
-end_date = Date(2012, 12, 31)
-#
-# # import malaria data
-# inc_data = pd.read_excel(
-#     Path(resourcefilepath) / "ResourceFile_malaria.xlsx",
-#     sheet_name="inc1000py_MAPdata",
-# )
-# PfPR_data = pd.read_excel(
-#     Path(resourcefilepath) / "ResourceFile_malaria.xlsx",
-#     sheet_name="PfPR_MAPdata",
-# )
-# mort_data = pd.read_excel(
-#     Path(resourcefilepath) / "ResourceFile_malaria.xlsx",
-#     sheet_name="mortalityRate_MAPdata",
-# )
-# tx_data = pd.read_excel(
-#     Path(resourcefilepath) / "ResourceFile_malaria.xlsx",
-#     sheet_name="txCov_MAPdata",
-# )
-#
-# # check date format for year columns in each sheet
+model_years = model_years.dt.year
+start_date = 2010
+end_date = 2025
+
+# import malaria data
+inc_data = pd.read_excel(
+    Path(resourcefilepath) / "ResourceFile_malaria.xlsx",
+    sheet_name="inc1000py_MAPdata",
+)
+PfPR_data = pd.read_excel(
+    Path(resourcefilepath) / "ResourceFile_malaria.xlsx",
+    sheet_name="PfPR_MAPdata",
+)
+mort_data = pd.read_excel(
+    Path(resourcefilepath) / "ResourceFile_malaria.xlsx",
+    sheet_name="mortalityRate_MAPdata",
+)
+tx_data = pd.read_excel(
+    Path(resourcefilepath) / "ResourceFile_malaria.xlsx",
+    sheet_name="txCov_MAPdata",
+)
+
+# check date format for year columns in each sheet
 # inc_data_years = pd.to_datetime(inc_data.Year, format="%Y")
 # PfPR_data_years = pd.to_datetime(PfPR_data.Year, format="%Y")
 # mort_data_years = pd.to_datetime(mort_data.Year, format="%Y")
 # tx_data_years = pd.to_datetime(tx_data.Year, format="%Y")
 #
-# ## FIGURES
+# yr_num = inc_data_years.values
+
+## FIGURES
 plt.figure(1)
 
 # Malaria incidence per 1000py - all ages with MAP model estimates
-plt.subplot(221)  # numrows, numcols, fignum
-# plt.plot(inc_data_years, inc_data.inc_1000pyMean)  # MAP data
+ax = plt.subplot(221)  # numrows, numcols, fignum
+plt.plot(inc_data.Year, inc_data.inc_1000pyMean)  # MAP data
+plt.fill_between(inc_data.Year, inc_data.inc_1000py_Lower,
+                 inc_data.inc_1000pyUpper)  # ,color='red',alpha=.5)
+# plt.fill_between(inc_data_years, inc_data.inc_1000py_Lower, inc_data.inc_1000pyUpper)#,color='red',alpha=.5)
 plt.plot(model_years, inc.inc_clin_counter)  # model - using the clinical counter for multiple episodes per person
 plt.title("Malaria Inc / 1000py")
 plt.xlabel("Year")
 plt.ylabel("Incidence (/1000py)")
 plt.xticks(rotation=90)
-# plt.gca().set_xlim(start_date, end_date)
-# plt.legend(["Data", "Model"])
+plt.gca().set_xlim(start_date, end_date)
+plt.legend(["Data", "Model"])
 
 # Malaria parasite prevalence rate - 2-10 year olds with MAP model estimates
 # expect model estimates to be slightly higher as some will have
 # undetectable parasitaemia
-plt.subplot(222)  # numrows, numcols, fignum
-# plt.plot(PfPR_data_years, PfPR_data.PfPR_median)  # MAP data
+ax2 = plt.subplot(222)  # numrows, numcols, fignum
+plt.plot(PfPR_data.Year, PfPR_data.PfPR_median)  # MAP data
 plt.plot(model_years, pfpr.child2_10_prev)  # model
 plt.title("Malaria PfPR 2-10 yrs")
 plt.xlabel("Year")
 plt.xticks(rotation=90)
 plt.ylabel("PfPR (%)")
-# plt.gca().set_xlim(start_date, end_date)
-# plt.legend(["Data", "Model"])
+plt.gca().set_xlim(start_date, end_date)
+plt.legend(["Data", "Model"])
 
 # Malaria treatment coverage - all ages with MAP model estimates
-plt.subplot(223)  # numrows, numcols, fignum
-# plt.plot(tx_data_years, tx_data.ACT_coverage)  # MAP data
+ax3 = plt.subplot(223)  # numrows, numcols, fignum
+plt.plot(tx_data.Year, tx_data.ACT_coverage)  # MAP data
 plt.plot(model_years, tx.treatment_coverage)  # model
 plt.title("Malaria Treatment Coverage")
 plt.xlabel("Year")
 plt.xticks(rotation=90)
 plt.ylabel("Treatment coverage (%)")
-# plt.gca().set_xlim(start_date, end_date)
-# plt.legend(["Data", "Model"])
+plt.gca().set_xlim(start_date, end_date)
+plt.gca().set_ylim(0.0, 1.0)
+plt.legend(["Data", "Model"])
 
 # Malaria mortality rate - all ages with MAP model estimates
-plt.subplot(224)  # numrows, numcols, fignum
-# plt.plot(mort_data_years, mort_data.mortality_rate_median)  # MAP data
+ax4 = plt.subplot(224)  # numrows, numcols, fignum
+plt.plot(mort_data.Year, mort_data.mortality_rate_median)  # MAP data
 plt.plot(model_years, mort.mort_rate)  # model
 plt.title("Malaria Mortality Rate")
 plt.xlabel("Year")
 plt.xticks(rotation=90)
 plt.ylabel("Mortality rate")
-# plt.gca().set_xlim(start_date, end_date)
-# plt.legend(["Data", "Model"])
+plt.gca().set_xlim(start_date, end_date)
+plt.legend(["Data", "Model"])
 plt.show()
+
