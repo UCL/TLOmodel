c4aaa0f7af4798aa1542f84f05a0b11774469a7b
Jonathan Roberts <j.roberts18@imperial.ac.uk>
2020-01-02 14:26:11 +0000

    Directory paths and example paracetamol data frame added
    Resource and output file paths added to the script and an example data
    frame for paracetamol tablets has been added to the resources directory
    so that the script can now generate an example plot with data points.
    
    
    Former-commit-id: ea800b543d206ad5d4f70bbf0a3485b799c8ab0c

--- src/scripts/consumable_resource_analyses/map_plotting_example.py
+++ src/scripts/consumable_resource_analyses/map_plotting_example.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as plt
 import shapefile as shp
+import pandas as pd
+from pathlib import Path
 
 # Example plotting script for administrative district boundaries in Malawi
 # More details available in the cookbook: https://github.com/UCL/TLOmodel/wiki/Cookbook#plotting-maps
 
-# read in the shape file
-sf = shp.Reader('../../../resources/ResourceFile_mwi_admbnda_adm2_nso_20181016.shp')
-# note: at the moment this script uses it's directory as the working directory
+# assume that the present working directory is the top level TLOmodel directory
+resourcefilepath = Path("./resources")
+outputfilepath = Path("./outputs")
+
+# read in the shape file for district level maps
+sf = shp.Reader(shp=open(Path(resourcefilepath) / 'ResourceFile_mwi_admbnda_adm2_nso_20181016.shp', 'rb'),
+                dbf=open(Path(resourcefilepath) / 'ResourceFile_mwi_admbnda_adm2_nso_20181016.dbf', 'rb'),
+                shx=open(Path(resourcefilepath) / 'ResourceFile_mwi_admbnda_adm2_nso_20181016.shx', 'rb'))
 
 # create a figure
 plt.figure()
@@ -21,24 +28,26 @@ for shape in sf.shapeRecords():
             i_end = shape.shape.parts[i + 1]
         x = [i[0] for i in shape.shape.points[i_start:i_end]]
         y = [i[1] for i in shape.shape.points[i_start:i_end]]
-        plt.plot(x, y, color='k', linewidth=0.1)
+        plt.plot(x, y, color='k', linewidth=0.5)
 
 # remove figure axes and set the aspect ratio to equal so that the map isn't stretched
 plt.axis('off')
 plt.gca().set_aspect('equal')
 
 # example of how to add data to the map using a colour map:
-
-# paracetamol_df = pd.read_csv('paracetamol_df.csv')
-# stock_out_days = paracetamol_df['Stock Out Days']
-# eastings = paracetamol_df['Eastings']
-# northings = paracetamol_df['Northings']
-# cm = plt.cm.get_cmap('Purples')
-# sc = plt.scatter(eastings, northings, c=stock_out_days, cmap=cm, s=4)
-# plt.colorbar(sc, fraction=0.01, pad=0.01, label="Stock Out Days")
+paracetamol_df = pd.read_csv(Path(resourcefilepath) / 'ResourceFile_Example_Paracetamol_DataFrame.csv')
+stock_out_days = paracetamol_df['Stock Out Days']
+eastings = paracetamol_df['Eastings']
+northings = paracetamol_df['Northings']
+cm = plt.cm.get_cmap('Purples')
+sc = plt.scatter(eastings, northings, c=stock_out_days, cmap=cm, s=4)
+plt.colorbar(sc, fraction=0.01, pad=0.01, label="Stock Out Days")
 
 # give the figure a title
-plt.title("Plot Title")
+plt.title("Paracetamol Stock Out Days Example")
+
+# save the figure
+plt.savefig(Path(outputfilepath) / 'Map_Paracetamol_Stock_Out_Days_Example.png', bbox_inches="tight", dpi=600)
 
 # display the figure in PyCharm's Plots window
 plt.show()
