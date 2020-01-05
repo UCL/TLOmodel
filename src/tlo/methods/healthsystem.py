5c463792a9a26ec62def34703c5ce22c226caa5e
Tim Hallett <timothy.hallett@imperial.ac.uk>
2020-01-05 10:54:10 +0000

    additional check on the declaration of symptoms
    Former-commit-id: d9db7ee2551afbc548c7ca6b4c16b172becd9868

--- src/tlo/methods/healthsystem.py
+++ src/tlo/methods/healthsystem.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 import tlo
 from tlo import DateOffset, Module, Parameter, Property, Types
-from tlo.events import PopulationScopeEventMixin, RegularEvent, Event
+from tlo.events import Event, PopulationScopeEventMixin, RegularEvent
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
