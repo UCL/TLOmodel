9abdec026730b6e63ad8570606546f7015b024f4
Tara Mangal <t.mangal@imperial.ac.uk>
2020-01-16 17:51:53 +0000

    added tests

--- tests/test_malaria.py
+++ tests/test_malaria.py
@@ -22,89 +22,46 @@ from tlo.methods import (
 )
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2012, 1, 1)
+end_date = Date(2011, 1, 1)
 popsize = 50
 
-outputpath = './src/scripts/outputs/'
-datestamp = datetime.date.today().strftime("__%Y_%m_%d")
+try:
+    resourcefilepath = Path(os.path.dirname(__file__)) / '../resources'
+except NameError:
+    # running interactively
+    resourcefilepath = 'resources'
 
 
-@pytest.fixture(autouse=True)
-def disable_logging():
-    logging.disable(logging.INFO)
+# @pytest.fixture(autouse=True)
+# def disable_logging():
+#     logging.disable(logging.DEBUG)
 
 
-@pytest.fixture(scope='module')
-def simulation():
-    # Establish the logger
-    logfile = outputpath + 'LogFile' + datestamp + '.log'
-
-    if os.path.exists(logfile):
-        os.remove(logfile)
-    fh = logging.FileHandler(logfile)
-    fr = logging.Formatter("%(levelname)s|%(name)s|%(message)s")
-    fh.setFormatter(fr)
-    logging.getLogger().addHandler(fh)
-
-    service_availability = ["*"]
-    malaria_strat = 0  # levels: 0 = national; 1 = district
-
-    sim = Simulation(start_date=start_date)
-
-    resourcefilepath = Path("./resources")
-
-    sim.register(demography.Demography(resourcefilepath=resourcefilepath))
-    sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
-                                           service_availability=service_availability,
-                                           mode_appt_constraints=0,
-                                           ignore_cons_constraints=True,
-                                           ignore_priority=True,
-                                           capabilities_coefficient=1.0))
-    sim.register(symptommanager.SymptomManager(resourcefilepath=resourcefilepath))
-    sim.register(healthseekingbehaviour.HealthSeekingBehaviour())
-    sim.register(dx_algorithm_child.DxAlgorithmChild())
-    sim.register(dx_algorithm_adult.DxAlgorithmAdult())
-    sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
-    sim.register(contraception.Contraception(resourcefilepath=resourcefilepath))
-    sim.register(enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath))
-    sim.register(malaria.Malaria(resourcefilepath=resourcefilepath,
-                                 level=malaria_strat))
-    return sim
-
-
-def test_run(simulation):
-    simulation.make_initial_population(n=popsize)
-    simulation.simulate(end_date=end_date)
-
-
-def test_dtypes(simulation):
+def check_dtypes(simulation):
     # check types of columns
     df = simulation.population.props
     orig = simulation.population.new_row
     assert (df.dtypes == orig.dtypes).all()
 
 
-def test_run_no_hsi():
-    # There should be no HSI events run or scheduled
-
-    # Establish the simulation object
-    sim = Simulation(start_date=start_date)
-
-    # Establish the logger
-    logfile = outputpath + 'LogFile' + datestamp + '.log'
-
-    if os.path.exists(logfile):
-        os.remove(logfile)
-    fh = logging.FileHandler(logfile)
+# @pytest.fixture(scope='module')
+def test_no_hsi(tmpdir):
+    # temporary log-file
+    f = tmpdir.mkdir("malaria").join("dummy.log")
+    fh = logging.FileHandler(f)
     fr = logging.Formatter("%(levelname)s|%(name)s|%(message)s")
     fh.setFormatter(fr)
+
+    # disable logging to stdout
+    logging.getLogger().handlers.clear()
+
     logging.getLogger().addHandler(fh)
 
-    service_availability = []
-    malaria_strat = 0  # levels: 0 = national; 1 = district
-    resourcefilepath = Path("./resources")
+    service_availability = ["*"]
+    malaria_strat = 1  # levels: 0 = national; 1 = district
 
-    # Register the appropriate modules
+    sim = Simulation(start_date=start_date)
+
     sim.register(demography.Demography(resourcefilepath=resourcefilepath))
     sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
                                            service_availability=service_availability,
@@ -122,34 +79,20 @@ def test_run_no_hsi():
     sim.register(malaria.Malaria(resourcefilepath=resourcefilepath,
                                  level=malaria_strat))
 
-    sim.seed_rngs(0)
-
     # Run the simulation and flush the logger
     sim.make_initial_population(n=popsize)
     sim.simulate(end_date=end_date)
-    test_dtypes(sim)
+    check_dtypes(sim)
 
     # read the results
     fh.flush()
-    output = parse_log_file(logfile)
+    output = parse_log_file(f)
     fh.close()
 
     # check no-one has been treated
-    assert output['tlo.methods.malaria']['tx_coverage'] == 0
+    assert (output['tlo.methods.malaria']['tx_coverage']['number_treated'] == 0).all()
 
-
-def __check_deaths(simulation):
-    # deaths only occur in severe malaria cases
-    # check types of columns
-    df = simulation.population.props
-
+    # check scheduled malaria deaths occurring only due to severe malaria (not clinical or asym)
+    df = sim.population.props
     assert not (
-            (df.ma_date_death) & ((df.ma_specific_symptoms == 'clinical') | (df.ma_specific_symptoms == 'none'))).any()
-
-
-if __name__ == '__main__':
-    t0 = time.time()
-    simulation = simulation()
-    test_run(simulation)
-    t1 = time.time()
-    print('Time taken', t1 - t0)
+        (df.ma_date_death) & ((df.ma_specific_symptoms == 'clinical') | (df.ma_specific_symptoms == 'none'))).any()
