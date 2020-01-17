9d466c2471a26bdb099ac226a3ce84f71489d22e
Tim Hallett <timothy.hallett@imperial.ac.uk>
2020-01-17 09:22:56 +0000

    making error message about 'predictors variables not in df' give the names of what is missing
    Former-commit-id: 24e20e5aca9d7b6f3363e73098fa42c203fdf677

--- src/tlo/lm.py
+++ src/tlo/lm.py
@@ -166,6 +166,14 @@ class LinearModel(object):
                 new_columns[f'__{column_name}__'] = kwargs[column_name]
             df = df.assign(**new_columns)
 
+
+        indicator_property_names_not_in_df = [p.property_name
+                    for p in self.predictors
+                    if (p.property_name is not None) and (p.property_name not in df.columns)]
+
+        assert not indicator_property_names_not_in_df,\
+            f"Predictor variables not in df: {indicator_property_names_not_in_df}"
+
         assert all([p.property_name in df.columns
                     for p in self.predictors
                     if p.property_name is not None]), f"Predictor variables not in df"
