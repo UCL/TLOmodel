80dc455384b6210bbbd47c31b218edc80c9d58c3
Asif Tamuri <tamuri@gmail.com>
2020-02-17 17:16:47 +0000

    Revert files unrelated to PR back to master
    Former-commit-id: 9080350424f171cad8e83c28e51c2a34a419c5a8
    Former-commit-id: cdbb6f1cc8fa5d33ad5e04495cd8eb0ac0b5b8b4

--- tests/test_healthburden.py
+++ tests/test_healthburden.py
@@ -16,10 +16,10 @@ from tlo.methods import (
 )
 
 try:
-    resourcefilepath = Path(os.path.dirname(__file__)) / "../resources"
+    resourcefilepath = Path(os.path.dirname(__file__)) / '../resources'
 except NameError:
     # running interactively
-    resourcefilepath = "resources"
+    resourcefilepath = 'resources'
 
 start_date = Date(2010, 1, 1)
 end_date = Date(2012, 1, 1)
@@ -48,14 +48,10 @@ def test_run_with_healthburden_with_dummy_diseases(tmpdir):
 
     # Register the appropriate modules
     sim.register(demography.Demography(resourcefilepath=resourcefilepath))
-    sim.register(
-        healthsystem.HealthSystem(
-            resourcefilepath=resourcefilepath,
-            service_availability=service_availability,
-            capabilities_coefficient=0.0,
-            mode_appt_constraints=0,
-        )
-    )
+    sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
+                                           service_availability=service_availability,
+                                           capabilities_coefficient=0.0,
+                                           mode_appt_constraints=0))
     sim.register(symptommanager.SymptomManager(resourcefilepath=resourcefilepath))
     sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
     sim.register(enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath))
@@ -75,26 +71,25 @@ def test_run_with_healthburden_with_dummy_diseases(tmpdir):
 
     # Do the checks
     # correctly configured index (outputs on 31st december in each year of simulation for each age/sex group)
-    dalys = output["tlo.methods.healthburden"]["DALYS"]
-    age_index = sim.modules["Demography"].AGE_RANGE_CATEGORIES
-    sex_index = ["M", "F"]
+    dalys = output['tlo.methods.healthburden']['DALYS']
+    age_index = sim.modules['Demography'].AGE_RANGE_CATEGORIES
+    sex_index = ['M', 'F']
     year_index = list(range(start_date.year, end_date.year + 1))
-    correct_multi_index = pd.MultiIndex.from_product(
-        [sex_index, age_index, year_index], names=["sex", "age_range", "year"]
-    )
-    dalys["year"] = pd.to_datetime(dalys["date"]).dt.year
-    assert (pd.to_datetime(dalys["date"]).dt.month == 12).all()
-    assert (pd.to_datetime(dalys["date"]).dt.day == 31).all()
-    output_multi_index = dalys.set_index(["sex", "age_range", "year"]).index
+    correct_multi_index = pd.MultiIndex.from_product([sex_index, age_index, year_index],
+                                                     names=['sex', 'age_range', 'year'])
+    dalys['year'] = pd.to_datetime(dalys['date']).dt.year
+    assert (pd.to_datetime(dalys['date']).dt.month == 12).all()
+    assert (pd.to_datetime(dalys['date']).dt.day == 31).all()
+    output_multi_index = dalys.set_index(['sex', 'age_range', 'year']).index
     assert output_multi_index.equals(correct_multi_index)
 
     # check that there is a YLD for each module registered
     yld_colnames = list()
     for colname in list(dalys.columns):
-        if "YLD" in colname:
+        if 'YLD' in colname:
             yld_colnames.append(colname)
 
     module_names_in_output = set()
     for yld_colname in yld_colnames:
-        module_names_in_output.add(yld_colname.split("_", 2)[1])
-    assert module_names_in_output == {"Mockitis", "ChronicSyndrome"}
+        module_names_in_output.add(yld_colname.split('_', 2)[1])
+    assert module_names_in_output == {'Mockitis', 'ChronicSyndrome'}
