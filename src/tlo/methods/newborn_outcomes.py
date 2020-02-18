79483cc7cd9cfdaa8bbe9cb36f30eeff68073ae2
Asif Tamuri <tamuri@gmail.com>
2020-02-18 10:17:22 +0000

    Fix import order

--- src/tlo/methods/newborn_outcomes.py
+++ src/tlo/methods/newborn_outcomes.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from tlo import DateOffset, Module, Parameter, Property, Types, logging
 from tlo.events import Event, IndividualScopeEventMixin, PopulationScopeEventMixin, RegularEvent
