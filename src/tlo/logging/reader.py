4a6c66b8788459206683bad602ca2bc4e7c8d5fc
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-02-27 10:13:32 +0000

    Use Packet class to interact with each json row
    Former-commit-id: 5f4db7d45bd6f4651d77c9e22132a61d8d512be5

--- src/tlo/logging/reader.py
+++ src/tlo/logging/reader.py
@@ -1,37 +1,52 @@
-from collections import defaultdict
-
 import json
+from collections import defaultdict
+from typing import DefaultDict, Dict
+
 import pandas as pd
 
 
+class Packet:
+    """Convenience class for interacting with packet of json log data"""
+    is_header = False
+
+    def __init__(self, line):
+        log_data = json.loads(line)
+        self.key = log_data['key']
+        self.module = log_data['module']
+        self.log_id = f'{self.module}_{self.key}'
+
+        if 'level' in log_data.keys():
+            self.is_header = True
+            self.level = log_data['level']
+            self.header_line = {'header': log_data, 'values': [], 'dates': []}
+        else:
+            self.date = log_data['date']
+            self.values = log_data['values']
+
 
 def parse_structured_output(log_lines, level):
-    # TODO: class of packets
     allowed_logs = set()
-    parsed_logs = defaultdict(dict)
-    output_logs = defaultdict(dict)
+    parsed_logs: DefaultDict[str, Dict[str, Dict[str, list]]] = defaultdict(dict)
+    output_logs: DefaultDict[str, Dict[str, pd.DataFrame]] = defaultdict(dict)
 
     for line in log_lines:
         # only parse json entities
         if line.startswith('{'):
-            packet = json.loads(line)
+            packet = Packet(line)
             # new header line, if this is the right level, then add module and key to log with header and blank data
-            if 'level' in packet.keys():
-                if packet['level'] == level:
-                    allowed_logs.add(f'{packet["module"]}_{packet["key"]}')
-                    parsed_logs[packet['module']][packet['key']] = {'header': packet, 'values': [], 'dates': []}
-                    continue
+            if packet.is_header:
+                if packet.level == level:
+                    allowed_logs.add(packet.log_id)
+                    parsed_logs[packet.module][packet.key] = packet.header_line
             # log data row if we allow this logger
-            if f'{packet["module"]}_{packet["key"]}' in allowed_logs:
-                parsed_logs[packet['module']][packet['key']]['values'].append(packet['values'])
-                parsed_logs[packet['module']][packet['key']]['dates'].append(pd.Timestamp(packet['date']))
+            elif packet.log_id in allowed_logs:
+                parsed_logs[packet.module][packet.key]['dates'].append(packet.date)
+                parsed_logs[packet.module][packet.key]['values'].append(packet.values)
 
-    # convert dictionaries to dataframe
-    for module, keys in parsed_logs.items():
-        for key, data in keys.items():
-            dates = pd.DataFrame(data["dates"], columns=['date'], dtype=pd.Timestamp)
-            output_logs[module][key] = pd.DataFrame(
-                data['values'], columns=data['header']['columns'].keys(), index=data['dates']
-            )
+    # convert dictionaries to dataframes
+    for module, log_keys in parsed_logs.items():
+        for key, data in log_keys.items():
+            output_logs[module][key] = pd.DataFrame(data['values'], columns=data['header']['columns'].keys())
+            output_logs[module][key].insert(0, "date", pd.Series(data["dates"], dtype='datetime64[ns]'))
 
     return output_logs
