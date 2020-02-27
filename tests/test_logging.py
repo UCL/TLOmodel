54ab147d02a4341fe201c34003ac504292741c02
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-02-27 14:01:15 +0000

    Add tests for structured logging filtering
    Former-commit-id: 80b8c42b3e15626f7ad4e1d397bf81bad6a9082f

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,6 +1,15 @@
-import pytest
+import os
+from pathlib import Path
 
-from tlo import logging
+import json
+import pytest
+import pandas as pd
+
+from tlo import logging, Simulation, Date
+from tlo.methods import demography, enhanced_lifestyle
+
+start_date = Date(2010, 1, 1)
+popsize = 500
 
 
 @pytest.fixture(scope='function')
@@ -13,6 +22,23 @@ def basic_configuration(tmpdir):
     yield file_handler, file_name
 
     file_handler.close()
+
+
+@pytest.fixture(scope='function')
+def simulation_configuration(tmpdir):
+    resourcefilepath = Path(os.path.dirname(__file__)) / '../resources'
+
+    sim = Simulation(start_date=start_date)
+
+    sim.register(demography.Demography(resourcefilepath=resourcefilepath))
+    sim.register(enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath))
+    f = sim.configure_logging("log", directory=tmpdir)
+
+    yield sim.output_file, f
+
+    sim.output_file.close()
+    logger = logging.getLogger('tlo.test.logger')
+    logger.keys.clear()
 
 
 def read_file(file_handler, file_name):
@@ -28,25 +54,40 @@ def read_file(file_handler, file_name):
     return lines
 
 
-def log_message(message_level, logger_level, message):
+def log_message(message_level, logger_level, message, logger_name='tlo.test.logger', structured_logging=False):
     """
     Sets up logger level, and writes message at the message level
 
     :param message_level: level that the message will be added as
     :param logger_level: level that the logger is set to
     :param message: message to be written to log
+    :param logger_name: name of the logger
+    :param structured_logging:
+
     """
-    logger = logging.getLogger("tlo.test.logger")
+    logger = logging.getLogger(logger_name)
     logger.setLevel(logger_level)
 
-    if message_level == 'logging.DEBUG':
-        logger.debug(message)
-    elif message_level == 'logging.INFO':
-        logger.info(message)
-    elif message_level == 'logging.WARNING':
-        logger.warning(message)
-    elif message_level == 'logging.CRITICAL':
-        logger.critical(message)
+    if structured_logging:
+        if message_level == 'logging.DEBUG':
+            logger.debug(key='tlo.test.logger', data=message)
+        elif message_level == 'logging.INFO':
+            logger.info(key='tlo.test.logger', data=message)
+        elif message_level == 'logging.WARNING':
+            logger.warning(key='tlo.test.logger', data=message)
+        elif message_level == 'logging.CRITICAL':
+            logger.critical(key='tlo.test.logger', data=message)
+    else:
+        if message_level == 'logging.DEBUG':
+            logger.debug(message)
+        elif message_level == 'logging.INFO':
+            logger.info(message)
+        elif message_level == 'logging.WARNING':
+            logger.warning(message)
+        elif message_level == 'logging.CRITICAL':
+            logger.critical(message)
+
+
 
 
 @pytest.mark.parametrize("message_level", ["logging.DEBUG", "logging.INFO", "logging.WARNING", "logging.CRITICAL"])
@@ -91,4 +132,58 @@ def test_disable(basic_configuration, message_level):
     lines = read_file(*basic_configuration)
 
     # messages should not be written to log
+    assert [] == lines
+
+
+@pytest.mark.parametrize("message_level", ["logging.DEBUG", "logging.INFO", "logging.WARNING", "logging.CRITICAL"])
+def test_structlog_messages_same_level(simulation_configuration, message_level):
+    # given that messages are at the same level as the logger
+    logger_level = eval(message_level)
+    message = {"message": pd.Series([12.5])[0]}
+    file_handler, file_path = simulation_configuration
+    log_message(message_level, logger_level, message, structured_logging=True)
+
+    lines = read_file(file_handler, file_path)
+    header_json = json.loads(lines[0])
+    data_json = json.loads(lines[1])
+
+    # message should be written to log
+    assert len(lines) == 2
+    assert header_json['level'] == message_level.lstrip("logging.")
+    assert 'message' in header_json['columns']
+    assert header_json['columns']['message'] == 'float64'
+    assert data_json['values'] == [12.5]
+
+
+@pytest.mark.parametrize("message_level", ["logging.DEBUG", "logging.INFO", "logging.WARNING", "logging.CRITICAL"])
+def test_structlog_messages_higher_level(simulation_configuration, message_level):
+    # given that messages are a higher level than the logger
+    logger_level = eval(message_level) + 1
+    message = {"message": pd.Series([12.5])[0]}
+    file_handler, file_path = simulation_configuration
+    log_message(message_level, logger_level, message, structured_logging=True)
+
+    lines = read_file(file_handler, file_path)
+    header_json = json.loads(lines[0])
+    data_json = json.loads(lines[1])
+
+    # message should be written to log
+    assert len(lines) == 2
+    assert header_json['level'] == message_level.lstrip("logging.")
+    assert 'message' in header_json['columns']
+    assert header_json['columns']['message'] == 'float64'
+    assert data_json['values'] == [12.5]
+
+
+@pytest.mark.parametrize("message_level", ["logging.DEBUG", "logging.INFO", "logging.WARNING", "logging.CRITICAL"])
+def test_structlog_messages_lower_level(simulation_configuration, message_level):
+    # given that messages are at a lower level than logger
+    logger_level = eval(message_level) - 1
+    message = {"message": pd.Series([12.5])[0]}
+    file_handler, file_path = simulation_configuration
+    log_message(message_level, logger_level, message, structured_logging=True)
+
+    lines = read_file(file_handler, file_path)
+
+    # message should be written to log
     assert [] == lines
