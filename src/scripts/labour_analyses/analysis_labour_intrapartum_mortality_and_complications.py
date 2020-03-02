ba1f13d524b4a993e5fd58aefece1ef40222b292
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2020-03-02 14:16:35 +0000

    New branch with agreed changes to HSI structure
    Former-commit-id: af79ba85f60bbfe0033e9b77daa973dcfbd0f78d

--- src/scripts/labour_analyses/analysis_labour_intrapartum_mortality_and_complications.py
+++ src/scripts/labour_analyses/analysis_labour_intrapartum_mortality_and_complications.py
@@ -33,7 +33,7 @@ sim = Simulation(start_date=start_date)
 sim.register(demography.Demography(resourcefilepath=resourcefilepath))
 sim.register(enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath))
 sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
-sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath))
+sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath, disable=True))
 sim.register(contraception.Contraception(resourcefilepath=resourcefilepath))
 sim.register(labour.Labour(resourcefilepath=resourcefilepath))
 sim.register(newborn_outcomes.NewbornOutcomes(resourcefilepath=resourcefilepath))
@@ -48,13 +48,26 @@ sim.simulate(end_date=end_date)
 # Get the output from the logfile
 output = parse_log_file(logfile)
 
+# =====================================================================================================================
+# Live births
+live_births = output['tlo.methods.labour']['live_births']
+live_births['date'] = pd.to_datetime(live_births['date'])
+live_births['year'] = live_births['date'].dt.year
+live_births_by_year = live_births.groupby(['year'])['child'].size()
+
+# All births
+all_births_df = output['tlo.methods.demography']['on_birth']
+all_births_df['date'] = pd.to_datetime(all_births_df['date'])
+all_births_df['year'] = all_births_df['date'].dt.year
+all_births_by_year = all_births_df.groupby(['year'])['child'].size()
+
 # All cause Intrapartum Maternal Deaths
+
 all_cause_deaths = output['tlo.methods.demography']['death']
 all_cause_deaths['date'] = pd.to_datetime(all_cause_deaths['date'])
 all_cause_deaths['year'] = all_cause_deaths['date'].dt.year
 intrapartum_deaths_df = all_cause_deaths.loc[all_cause_deaths.cause == 'labour']
 # Above will change we when append causes onto instantaneous death logging
-
 intrapartum_deaths_by_year = intrapartum_deaths_df.groupby(['year'])['person_id'].size()
 
 death_by_cause = intrapartum_deaths_by_year.reset_index()
@@ -66,44 +79,59 @@ death_by_cause.plot.bar(stacked=True)
 plt.title("Total Intrapartum Deaths per Year")
 plt.show()
 
-
 # Intrapartum MMR
+mmr_df = pd.concat((death_by_cause, live_births_by_year), axis=1)
+mmr_df.columns = ['maternal_deaths', 'live_births']
+mmr_df['MMR'] = mmr_df['maternal_deaths']/mmr_df['live_births'] * 100000
+
+mmr_df.plot.bar(y='MMR', stacked=True)
+plt.title("Yearly Intrapartum Maternal Mortality Rate")
+plt.show()
+
+# ======================================= COMPLICATION INCIDENCE ======================================================
 
 
-# All births dataframe
-all_births_df = output['tlo.methods.demography']['on_birth']
-all_births_df['date'] = pd.to_datetime(all_births_df['date'])
-all_births_df['year'] = all_births_df['date'].dt.year
-birth_by_year = all_births_df.groupby(['year'])['child'].size()
-
-
-def incidence_analysis(complication):
+def incidence_analysis(complication, birth_denominator):
     dataframe = output['tlo.methods.labour'][f'{complication}']
     dataframe['date'] = pd.to_datetime(dataframe['date'])
     dataframe['year'] = dataframe['date'].dt.year
     complication_per_year = dataframe.groupby(['year'])['person_id'].size()
+    if ~complication_per_year.empty:
 
-    complication_df = pd.concat((complication_per_year, birth_by_year), axis=1)
-    complication_df.columns = ['maternal_sepsis_cases', 'all_births']
-    complication_df[f'{complication}'] = complication_df['complication_cases']/complication_df['all_births'] * 1000
+        complication_df = pd.concat((complication_per_year, all_births_by_year), axis=1)
+        complication_df.columns = ['maternal_sepsis_cases', 'all_births']
+        complication_df[f'{complication}'] = complication_df['complication_cases']/complication_df['all_births'] * \
+                                             birth_denominator
 
-    complication_df.plot.bar(y='sepsis_incidence', stacked=True)
-    plt.title(f"Yearly {complication} Incidence")
-    plt.show()
+        complication_df.plot.bar(y='sepsis_incidence', stacked=True)
+        plt.title(f"Yearly {complication} Incidence")
+        plt.show()
 
+    else:
+        print(f'no cases of {complication} in this simulation run')
+
+
+   # TODO: maternal deaths by each contributing cause
+
+
+
+# TODO: determine correct denominators
 # Incidence of Obstructed Labour
-incidence_analysis('obstructed_labour')
+
+
+incidence_analysis('obstructed_labour', 100)
 
 # Incidence of Uterine Rupture
-incidence_analysis('uterine_rupture')
+incidence_analysis('uterine_rupture', 100)
 
 # Incidence of Antepartum Haemorrhage
-incidence_analysis('antepartum_haemorrhage')
+incidence_analysis('antepartum_haem', 100)
 
 # Incidence of Intrapartum Eclampsia
-incidence_analysis('')
+incidence_analysis('eclampsia', 1000)
 
 # Incidence of Intrapartum direct maternal sepsis
+incidence_analysis('sepsis', 100)
 
 # Intrapartum Stillbirths
 # Intrapartum SBR
