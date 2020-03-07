c12cc757732019174400d7b9722ff5b66901bc75
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2020-03-07 08:53:46 +0000

    .

--- src/scripts/depression_analyses/analyses_depression_run_me.py
+++ src/scripts/depression_analyses/analyses_depression_run_me.py
@@ -32,7 +32,7 @@ resourcefilepath = Path("./resources")
 
 start_date = Date(2010, 1, 1)
 end_date = Date(2014, 7, 1)
-popsize = 100000
+popsize = 10000
 
 sim = Simulation(start_date=start_date)
 sim.seed_rngs(0)
