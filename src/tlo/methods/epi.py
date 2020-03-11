eb5d0912d828d3dc1f844275f95058d337bdea68
tdm32 <t.mangal@imperial.ac.uk>
2020-03-11 16:28:21 +0000

    epi module created
    Former-commit-id: 66491ba16d7700d6db483974ab6d2dbfc7393a28
    Former-commit-id: bce068d84684fcd48554e635a8082474176c9a14

--- src/tlo/methods/epi.py
+++ src/tlo/methods/epi.py
@@ -61,7 +61,11 @@ class Epi(Module):
 
         :param population: the population of individuals
         """
-        pass
+        df = population.props
+        p = self.parameters
+
+        # Set default for properties
+        df.loc[df.is_alive, "ep_bcg"] = False
 
     def initialise_simulation(self, sim):
 
@@ -69,9 +73,10 @@ class Epi(Module):
         sim.schedule_event(EpiLoggingEvent(self), sim.date + DateOffset(months=1))
 
     def on_birth(self, mother_id, child_id):
-        """Initialise our properties for a newborn individual.
+        """Initialise our properties for a newborn individual
 
-        This is called by the simulation whenever a new person is born.
+        all vaccinations are scheduled to occur with a probability dependent on the year and district
+        birth doses occur within 24 hours of delivery
 
         :param mother_id: the ID for the mother for this child
         :param child_id: the ID for the new child
@@ -79,186 +84,24 @@ class Epi(Module):
         df = self.sim.population.props  # shortcut to the population props dataframe
 
         # Initialise all the properties that this module looks after:
-        df.at[child_id, "cs_has_cs"] = False
-        df.at[child_id, "cs_status"] = "N"
-        df.at[child_id, "cs_date_acquired"] = pd.NaT
-        df.at[child_id, "cs_scheduled_date_death"] = pd.NaT
-        df.at[child_id, "cs_date_cure"] = pd.NaT
+        df.at[child_id, "ep_bcg"] = False
 
     def on_hsi_alert(self, person_id, treatment_id):
         """
         This is called whenever there is an HSI event commissioned by one of the other disease modules.
         """
-
-        logger.debug(
-            "This is ChronicSyndrome, being alerted about a health system interaction "
-            "person %d for: %s",
-            person_id,
-            treatment_id,
-        )
-
-        # To simulate a "piggy-backing" appointment, whereby additional treatment and test are done
-        # for another disease, schedule another appointment (with smaller resources than a full appointmnet)
-        # and set it to priority 0 (to give it highest possible priority).
-
-        if treatment_id == "Mockitis_TreatmentMonitoring":
-            piggy_back_dx_at_appt = HSI_ChronicSyndrome_SeeksEmergencyCareAndGetsTreatment(
-                self, person_id
-            )
-            piggy_back_dx_at_appt.TREATMENT_ID = "ChronicSyndrome_PiggybackAppt"
-
-            # Arbitrarily reduce the size of appt footprint to reflect that this is a piggy back appt
-            for key in piggy_back_dx_at_appt.EXPECTED_APPT_FOOTPRINT:
-                piggy_back_dx_at_appt.EXPECTED_APPT_FOOTPRINT[key] = (
-                    piggy_back_dx_at_appt.EXPECTED_APPT_FOOTPRINT[key] * 0.25
-                )
-
-            self.sim.modules["HealthSystem"].schedule_hsi_event(
-                piggy_back_dx_at_appt, priority=0, topen=self.sim.date, tclose=None
-            )
+        # TODO: consider here how early interventions are bundled
+        # TODO: routine infant check-ups may occur alongside vaccinations
 
     def report_daly_values(self):
-        # This must send back a pd.Series or pd.DataFrame that reports on the average daly-weights that have been
-        # experienced by persons in the previous month. Only rows for alive-persons must be returned.
-        # The names of the series of columns is taken to be the label of the cause of this disability.
-        # It will be recorded by the healthburden module as <ModuleName>_<Cause>.
+        """ epi module returns dalys=0 for all persons alive """
 
-        logger.debug("This is chronicsyndrome reporting my health values")
+        logger.debug("This is epi reporting my health values")
 
         df = self.sim.population.props  # shortcut to population properties dataframe
 
-        health_values_df = pd.DataFrame(index=df.index[df.is_alive])
-
-        for symptom, daly_wt in self.parameters["daly_wts"].items():
-            health_values_df.loc[
-                self.sim.modules["SymptomManager"].who_has(symptom), symptom
-            ] = daly_wt
-
-        health_values_df.fillna(0, inplace=True)
-
-        return health_values_df
-
-
-class ChronicSyndromeEvent(RegularEvent, PopulationScopeEventMixin):
-
-    # This event is occuring regularly at one monthly intervals
-
-    def __init__(self, module):
-        super().__init__(module, frequency=DateOffset(months=1))
-        assert isinstance(module, ChronicSyndrome)
-
-    def apply(self, population):
-        df = population.props
-        p = self.module.parameters
-        rng: np.random.RandomState = self.module.rng
-
-        # 1. get (and hold) index of currently infected and uninfected individuals
-        # currently_cs = df.index[df.cs_has_cs & df.is_alive]
-        currently_not_cs = df.index[~df.cs_has_cs & df.is_alive]
-
-        # 2. handle new cases
-        p_aq = p["p_acquisition_per_year"] / 12.0
-        now_acquired = rng.random_sample(size=len(currently_not_cs)) < p_aq
-
-        # if any are new cases
-        if now_acquired.sum():
-            newcases_idx = currently_not_cs[now_acquired]
-
-            death_years_ahead = rng.exponential(scale=20, size=now_acquired.sum())
-            death_td_ahead = pd.to_timedelta(death_years_ahead, unit="y")
-
-            df.loc[newcases_idx, "cs_has_cs"] = True
-            df.loc[newcases_idx, "cs_status"].values[:] = "C"
-            df.loc[newcases_idx, "cs_date_acquired"] = self.sim.date
-            df.loc[newcases_idx, "cs_scheduled_date_death"] = (
-                self.sim.date + death_td_ahead
-            )
-            df.loc[newcases_idx, "cs_date_cure"] = pd.NaT
-
-            # schedule death events for new cases
-            for person_index in newcases_idx:
-                death_event = ChronicSyndromeDeathEvent(self.module, person_index)
-                self.sim.schedule_event(
-                    death_event, df.at[person_index, "cs_scheduled_date_death"]
-                )
-
-            # Assign symptoms:
-            for symp in self.module.parameters["prob_of_symptoms"]:
-                # persons who will have symptoms (each can occur independently)
-                persons_id_with_symp = np.array(newcases_idx)[
-                    self.module.rng.rand(len(newcases_idx))
-                    < self.module.parameters["prob_of_symptoms"][symp]
-                ]
-
-                self.sim.modules["SymptomManager"].change_symptom(
-                    person_id=list(persons_id_with_symp),
-                    symptom_string=symp,
-                    add_or_remove="+",
-                    disease_module=self.module,
-                )
-
-        # 3) Handle progression to severe symptoms
-        curr_cs_but_not_craving_sandwiches = list(
-            set(df.index[df.cs_has_cs & df.is_alive])
-            - set(self.sim.modules["SymptomManager"].who_has("em_craving_sandwiches"))
-        )
-
-        become_severe = (
-            self.module.rng.random_sample(size=len(curr_cs_but_not_craving_sandwiches))
-            < p["prob_dev_severe_symptoms_per_year"] / 12
-        )
-        become_severe_idx = np.array(curr_cs_but_not_craving_sandwiches)[become_severe]
-
-        self.sim.modules["SymptomManager"].change_symptom(
-            person_id=list(become_severe_idx),
-            symptom_string="em_craving_sandwiches",
-            add_or_remove="+",
-            disease_module=self.module,
-        )
-
-
-class ChronicSyndromeDeathEvent(Event, IndividualScopeEventMixin):
-    def __init__(self, module, person_id):
-        super().__init__(module, person_id=person_id)
-
-    def apply(self, person_id):
-        df = self.sim.population.props  # shortcut to the dataframe
-
-        # Apply checks to ensure that this death should occur
-        if df.at[person_id, "cs_status"] == "C":
-            # Fire the centralised death event:
-            death = InstantaneousDeath(self.module, person_id, cause="ChronicSyndrome")
-            self.sim.schedule_event(death, self.sim.date)
-
-
-class ChronicSyndrome_LaunchOutreachEvent(Event, PopulationScopeEventMixin):
-    """
-    This is the event that is run by ChronicSyndrome and it is the Outreach Event.
-    It will now submit the individual HSI events that occur when each individual is met.
-    (i.e. Any large campaign that involves contct with individual is composed of many individual outreach events).
-    """
-
-    def __init__(self, module):
-        super().__init__(module)
-        assert isinstance(module, ChronicSyndrome)
-
-    def apply(self, population):
-        df = self.sim.population.props
-
-        # Find the person_ids who are going to get the outreach
-        gets_outreach = df.index[(df["is_alive"]) & (df["sex"] == "F")]
-        for person_id in gets_outreach:
-            # make the outreach event (let this disease module be alerted about it, and also Mockitis)
-            outreach_event_for_individual = HSI_ChronicSyndrome_Outreach_Individual(
-                self.module, person_id=person_id
-            )
-
-            self.sim.modules["HealthSystem"].schedule_hsi_event(
-                outreach_event_for_individual,
-                priority=1,
-                topen=self.sim.date,
-                tclose=None,
-            )
+        health_values = pd.Series(index=df.index[df.is_alive], data=0)
+        return health_values  # returns the series
 
 
 # ---------------------------------------------------------------------------------
