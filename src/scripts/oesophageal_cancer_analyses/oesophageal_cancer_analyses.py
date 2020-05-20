942b7f9fe95e3ff4f06ccd10b5662bcd892ee24f
Tim Hallett <timothy.hallett@imperial.ac.uk>
2020-05-20 16:57:57 +0100

    starting work on the analysis script

--- src/scripts/oesophageal_cancer_analyses/oesophageal_cancer_analyses.py
+++ src/scripts/oesophageal_cancer_analyses/oesophageal_cancer_analyses.py
@@ -28,74 +28,122 @@ resourcefilepath = Path("./resources")
 # Set parameters for the simulation
 start_date = Date(2010, 1, 1)
 end_date = Date(2015, 1, 1)
-popsize = 10000
+popsize = 5000
 
-# Establish the simulation object and set the seed
-sim = Simulation(start_date=start_date)
-sim.seed_rngs(0)
+def run_sim(service_availability):
 
-# Register the appropriate modules
-sim.register(demography.Demography(resourcefilepath=resourcefilepath),
-             contraception.Contraception(resourcefilepath=resourcefilepath),
-             enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
-             healthsystem.HealthSystem(resourcefilepath=resourcefilepath, disable=True),
-             symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
-             healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
-             healthburden.HealthBurden(resourcefilepath=resourcefilepath),
-             labour.Labour(resourcefilepath=resourcefilepath),
-             pregnancy_supervisor.PregnancySupervisor(resourcefilepath=resourcefilepath),
-             oesophagealcancer.OesophagealCancer(resourcefilepath=resourcefilepath)
-             )
+    # Establish the simulation object and set the seed
+    sim = Simulation(start_date=start_date)
+    sim.seed_rngs(0)
 
-# Manipulate parameters in order that there is a high burden of oes_cancer in order to do the checking:
-# Initial prevalence of cancer:
-sim.modules['OesophagealCancer'].parameters['init_prop_oes_cancer_stage'] = [0.2, 0.1, 0.1, 0.05, 0.05, 0.025]
+    # Register the appropriate modules
+    sim.register(demography.Demography(resourcefilepath=resourcefilepath),
+                 contraception.Contraception(resourcefilepath=resourcefilepath),
+                 enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
+                 healthsystem.HealthSystem(resourcefilepath=resourcefilepath, service_availability=service_availability),
+                 symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
+                 healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
+                 healthburden.HealthBurden(resourcefilepath=resourcefilepath),
+                 labour.Labour(resourcefilepath=resourcefilepath),
+                 pregnancy_supervisor.PregnancySupervisor(resourcefilepath=resourcefilepath),
+                 oesophagealcancer.OesophagealCancer(resourcefilepath=resourcefilepath)
+                 )
 
-# Rate of cancer onset per 3 months:
-sim.modules['OesophagealCancer'].parameters['r_low_grade_dysplasia_none'] = 0.05
+    # Manipulate parameters in order that there is a high burden of oes_cancer in order to do the checking:
+    # Initial prevalence of cancer:
+    sim.modules['OesophagealCancer'].parameters['init_prop_oes_cancer_stage'] = [0.2, 0.1, 0.1, 0.05, 0.05, 0.025]
 
-# Rates of cancer progression per 3 months:
-sim.modules['OesophagealCancer'].parameters['r_high_grade_dysplasia_low_grade_dysp'] *= 5
-sim.modules['OesophagealCancer'].parameters['r_stage1_high_grade_dysp'] *= 5
-sim.modules['OesophagealCancer'].parameters['r_stage2_stage1'] *= 5
-sim.modules['OesophagealCancer'].parameters['r_stage3_stage2'] *= 5
-sim.modules['OesophagealCancer'].parameters['r_stage4_stage3'] *= 5
+    # Rate of cancer onset per 3 months:
+    sim.modules['OesophagealCancer'].parameters['r_low_grade_dysplasia_none'] = 0.05
 
-# Establish the logger
-logfile = sim.configure_logging(filename="LogFile")
+    # Rates of cancer progression per 3 months:
+    sim.modules['OesophagealCancer'].parameters['r_high_grade_dysplasia_low_grade_dysp'] *= 5
+    sim.modules['OesophagealCancer'].parameters['r_stage1_high_grade_dysp'] *= 5
+    sim.modules['OesophagealCancer'].parameters['r_stage2_stage1'] *= 5
+    sim.modules['OesophagealCancer'].parameters['r_stage3_stage2'] *= 5
+    sim.modules['OesophagealCancer'].parameters['r_stage4_stage3'] *= 5
 
-# Run the simulation
-sim.make_initial_population(n=popsize)
-sim.simulate(end_date=end_date)
+    # Establish the logger
+    logfile = sim.configure_logging(filename="LogFile")
 
+    # Run the simulation
+    sim.make_initial_population(n=popsize)
+    sim.simulate(end_date=end_date)
 
-# %% TODO: Demonstrate the burden and the interventions
-output = parse_log_file(logfile)
-
-# PREVALENCE wrt dx/ treated/ palliative
-s = output['tlo.methods.oesophagealcancer']['summary_stats']
-s['date'] = pd.to_datetime(s['date'])
-s = s.set_index('date', drop=True)
-
-# Total prevalence
-s.plot(y=['total_low_grade_dysplasia', 'total_high_grade_dysplasia', 'total_stage1', 'total_stage2', 'total_stage3', 'total_stage4'])
-plt.show()
-
-# Numbers diagnosed, treated, palliated
-
-s.plot()
-plt.show()
-
-# DALYS wrt age
-h = output['tlo.methods.healthburden']['DALYS']
-h['date'] = pd.to_datetime(h['date'])
-h = h.set_index('date', drop=True)
-
-h.groupby(by=['age_range']).sum().reset_index().plot.bar(x='age_range', y=['YLD_OesophagealCancer_0'], stacked=True)
-plt.show()
-
-# DEATHS wrt age and time
+    return logfile
 
 
-# %% TODO: Demonstrate the impact of the interventions
+def make_set_of_plots(logfile):
+    output = parse_log_file(logfile)
+
+    ## TOTAL COUNTS BY STAGE OVER TIME
+    s = output['tlo.methods.oesophagealcancer']['summary_stats']
+    s['date'] = pd.to_datetime(s['date'])
+    s = s.set_index('date', drop=True)
+
+    # Total prevalence over time
+    s.plot(y=['total_low_grade_dysplasia', 'total_high_grade_dysplasia', 'total_stage1', 'total_stage2', 'total_stage3',
+              'total_stage4'])
+    plt.show()
+
+
+    ## PROGRESSION THROUGH CARE CASCADE OVER TIME (SUMMED ACROSS TYPES OF CANCER)
+    def get_cols_excl_none(allcols, stub):
+        cols = allcols[allcols.str.startswith(stub)]
+        cols_not_none = [s for s in cols if ("none" not in s)]
+        return cols_not_none
+
+    summary = {
+        'total': s[get_cols_excl_none(s.columns, 'total_')].sum(axis=1),
+        'udx': s[get_cols_excl_none(s.columns, 'undiagnosed_')].sum(axis=1),
+        'dx': s[get_cols_excl_none(s.columns, 'diagnosed_')].sum(axis=1),
+        'tr': s[get_cols_excl_none(s.columns, 'treatment_')].sum(axis=1),
+        'pc': s[get_cols_excl_none(s.columns, 'palliative_')].sum(axis=1)
+    }
+    counts_by_cascade = pd.DataFrame(summary)
+
+    # assert (df['total'] == (df['udx'] + df['dx'])).all()  # todo - fix
+
+    counts_by_cascade.plot.bar(y=['udx', 'dx', 'tr', 'pc'])
+    plt.show()
+
+    # DALYS wrt age
+    h = output['tlo.methods.healthburden']['DALYS']
+    h['date'] = pd.to_datetime(h['date'])
+    h = h.set_index('date', drop=True)
+
+    h.groupby(by=['age_range']).sum().reset_index().plot.bar(x='age_range', y=['YLD_OesophagealCancer_0'], stacked=True)
+    plt.show()
+
+    # DEATHS wrt age and time
+    d = output['tlo.methods.demography']['death']
+    d['date'] = pd.to_datetime(d['date'])
+    d = d.set_index('date', drop=True)
+
+    d['age_group'] = d['age'].map(demography.Demography(resourcefilepath=resourcefilepath).AGE_RANGE_LOOKUP)
+    d['year'] = d.index.year
+
+    oes_cancer_deaths = pd.DataFrame(d.loc[d.cause == 'OesophagealCancer'].groupby(by=['age_group']).size())
+    oes_cancer_deaths.plot.bar()
+    plt.show()
+
+    return {
+        'total_counts_by_stage_over_time': s,
+        'counts_by_cascade': counts_by_cascade,
+        'oes_cancer_deaths': oes_cancer_deaths
+    }
+
+
+# %% Run the simulation with and without service availabilty
+
+# Without:
+logfile_no_healthsystem = run_sim(service_availability=[])
+results_no_healthsystem = make_set_of_plots(logfile_no_healthsystem)
+
+# With:
+logfile_with_healthsystem = run_sim(service_availability=['*'])
+results_with_healthsystem = make_set_of_plots(logfile_with_healthsystem)
+
+
+# Compare results:
 
