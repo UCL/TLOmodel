691d001562d608913a71900ecf08dbb3247748f1
Tim Hallett <tbhallett@gmail.com>
2020-05-27 16:45:33 +0100

    fixes to test_find_event_for_person_in_event_queue
    Former-commit-id: 5cb14633269aaaf0f148841d476fccdfbec42d34

--- tests/test_find_events_for_person_in_event_queue.py
+++ tests/test_find_events_for_person_in_event_queue.py
@@ -11,9 +11,6 @@ from tlo.methods import (
     dx_algorithm_child, labour, pregnancy_supervisor, newborn_outcomes)
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2011, 1, 1)
-popsize = 50
-
 
 try:
     resourcefilepath = Path(os.path.dirname(__file__)) / '../resources'
@@ -23,44 +20,45 @@ except NameError:
 
 
 def test_can_look_at_future_events():
-
     sim = Simulation(start_date=start_date)
-    sim.register(demography.Demography(resourcefilepath=resourcefilepath))
-    sim.register(contraception.Contraception(resourcefilepath=resourcefilepath))
-    sim.register(enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath))
-    sim.register(symptommanager.SymptomManager(resourcefilepath=resourcefilepath))
-    sim.register(healthseekingbehaviour.HealthSeekingBehaviour())
-    sim.register(dx_algorithm_child.DxAlgorithmChild())
-    sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
-    sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
-                                           disable=False)
+    sim.register(demography.Demography(resourcefilepath=resourcefilepath),
+                 contraception.Contraception(resourcefilepath=resourcefilepath),
+                 enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
+                 symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
+                 healthseekingbehaviour.HealthSeekingBehaviour(),
+                 dx_algorithm_child.DxAlgorithmChild(),
+                 healthburden.HealthBurden(resourcefilepath=resourcefilepath),
+                 healthsystem.HealthSystem(resourcefilepath=resourcefilepath),
+                 labour.Labour(resourcefilepath=resourcefilepath),
+                 newborn_outcomes.NewbornOutcomes(resourcefilepath=resourcefilepath),
+                 pregnancy_supervisor.PregnancySupervisor(resourcefilepath=resourcefilepath),
+                 mockitis.Mockitis(),
+                 chronicsyndrome.ChronicSyndrome()
                  )
-    sim.register(labour.Labour(resourcefilepath=resourcefilepath))
-    sim.register(newborn_outcomes.NewbornOutcomes(resourcefilepath=resourcefilepath))
-    sim.register(pregnancy_supervisor.PregnancySupervisor(resourcefilepath=resourcefilepath))
-    sim.register()
-    sim.register(mockitis.Mockitis())
-    sim.register(chronicsyndrome.ChronicSyndrome())
+
     sim.seed_rngs(0)
-    sim.make_initial_population(n=popsize)
-    sim.simulate(end_date=end_date)
+    sim.make_initial_population(n=10)
 
     # check can look at future events in the sim.event_queue and HSI_EVENT_QUEUE:
-    for person_id in sim.population.props.index.values:
-        events = sim.event_queue.find_events_for_person(person_id=person_id)
-        hsi_events = sim.modules['HealthSystem'].find_events_for_person(person_id=person_id)
+    person_id = 0  # select a random person
 
-        # Schedule some events for this person
-        dummy_event = mockitis.MockitisDeathEvent(sim.modules['Mockitis'], person_id)
-        dummy_hsi = chronicsyndrome.HSI_ChronicSyndrome_SeeksEmergencyCareAndGetsTreatment(
-            sim.modules['ChronicSyndrome'], person_id=person_id)
+    # Schedule some events for this person
+    # event queue
+    dummy_event = mockitis.MockitisDeathEvent(sim.modules['Mockitis'], person_id)
+    sim.schedule_event(dummy_event, sim.date)
 
-        sim.schedule_event(dummy_event, sim.date)
-        sim.modules['HealthSystem'].schedule_hsi_event(dummy_hsi, priority=0, topen=sim.date,
-                                                       tclose=sim.date + pd.DateOffset(days=1))
 
-    # TODO: commented out pre PR as cant get it to run correctly - Asif informed
-    # assert len(events) > 0
-    # assert len(hsi_events) > 0
+    # hsi event queue
+    dummy_hsi = chronicsyndrome.HSI_ChronicSyndrome_SeeksEmergencyCareAndGetsTreatment(
+        sim.modules['ChronicSyndrome'], person_id=person_id)
+    sim.modules['HealthSystem'].schedule_hsi_event(dummy_hsi, priority=0, topen=sim.date,
+                                                   tclose=sim.date + pd.DateOffset(days=1))
+
+    # Query the queue of events for this person:
+    events = sim.event_queue.find_events_for_person(person_id=person_id)
+    hsi_events = sim.modules['HealthSystem'].find_events_for_person(person_id=person_id)
+
+    assert len(events) > 0
+    assert len(hsi_events) > 0
 
     # TODO: nice way to combine and sort these two lists.
