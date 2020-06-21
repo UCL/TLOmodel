48da47fbf5a6163f7c5cbd181544357e3cd2e0c8
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2020-06-21 17:28:22 +0100

    initial running
    Former-commit-id: 2960204b4f270caa625b5f5433921ad4d6ec354f

--- src/scripts/breast_cancer_analyses/breast_cancer_analyses.py
+++ src/scripts/breast_cancer_analyses/breast_cancer_analyses.py
@@ -43,7 +43,7 @@ resourcefilepath = Path("./resources")
 # Set parameters for the simulation
 start_date = Date(2010, 1, 1)
 end_date = Date(2020, 1, 1)
-popsize = 100
+popsize = 10000
 
 
 def run_sim(service_availability):
