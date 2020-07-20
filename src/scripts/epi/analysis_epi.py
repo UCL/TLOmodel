3c0bd09c87dd97a198db66a8f98a71df2f352da3
tdm32 <t.mangal@imperial.ac.uk>
2020-07-20 14:07:52 +0100

    edited ResourceFile_EPI_WHO_estimates.xlsx values to range 0-1

--- src/scripts/epi/analysis_epi.py
+++ src/scripts/epi/analysis_epi.py
@@ -33,7 +33,7 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 resourcefilepath = Path("./resources")
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2020, 12, 31)
+end_date = Date(2015, 12, 31)
 popsize = 500
 
 log_config = {
