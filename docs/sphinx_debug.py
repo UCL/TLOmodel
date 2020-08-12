d1930f6edf6d1cc24e923391646bb6fec58161db
Matthew Gillman <m.gillman@ucl.ac.uk>
2020-08-12 10:20:05 +0100

    Added apidoc call.

--- docs/sphinx_debug.py
+++ docs/sphinx_debug.py
@@ -4,8 +4,18 @@ from pathlib import Path
 from typing import List
 
 from sphinx.cmd import build
+from sphinx.ext import apidoc
 
 root_dir = Path(__file__).resolve().parents[1]
+
+
+def run_sphinx_apidoc(input_dir, output_dir):
+    """
+    Generate sphinx sources from module
+    :param input_dir: module path
+    :param output_dir: output sources
+    """
+    apidoc.main(["-f", "-o", output_dir, input_dir])
 
 
 def run_sphinx_build(goal, input_dir, output_dir, args: List[str] = None):
@@ -24,9 +34,12 @@ def run_sphinx_build(goal, input_dir, output_dir, args: List[str] = None):
 
 
 if __name__ == '__main__':
-    input_dir = f"{root_dir}/docs"
-    output_dir = f"{root_dir}/dist/docs"
+    module_path = f"{root_dir}/src/tlo"
+    output_sources = f"{root_dir}/docs/reference"
+    sphinx_sources = f"{root_dir}/docs"
+    output_html = f"{root_dir}/dist/docs"
 
-    run_sphinx_build(goal="html", input_dir=input_dir, output_dir=output_dir, args=["-E"])
-    run_sphinx_build(goal="linkcheck", input_dir=input_dir, output_dir=output_dir)
+    run_sphinx_apidoc(input_dir=module_path, output_dir=output_sources)
+    run_sphinx_build(goal="html", input_dir=sphinx_sources, output_dir=output_html, args=["-E"])
+    run_sphinx_build(goal="linkcheck", input_dir=sphinx_sources, output_dir=output_html)
 
