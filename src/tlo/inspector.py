d7141b33e6890cff3fab54be60761e95ce531d55
Matthew Gillman <m.gillman@ucl.ac.uk>
2020-08-18 10:42:25 +0100

    Added os.walk() and more documentation.

--- src/tlo/inspector.py
+++ src/tlo/inspector.py
@@ -4,7 +4,51 @@
 # and Script path
 # /Users/matthewgillman/PycharmProjects/TLOmodel/src/tlo/inspector.py
 import inspect
-from tlo.methods import mockitis
+from os import walk
+
+MODULE_DIR = "./src/tlo/methods"
+
+# Use this so we can dynamically import, and not need to hard-code it.
+exec("from tlo.methods import mockitis")
+
+
+def generate_module_list(dir_string):
+    '''Obtain all Python module files in this directory
+    e.g. mockitis.py, hiv.py, healthsystem.py...
+    in an alphabetically-sorted list.
+
+    :param dir_string: string containing path to directory to process.
+
+    For each directory in the tree, walk yields a 3-tuple
+    (dirpath, dirnames, filenames)
+    Use break to prevent recursive search.
+
+    Reference:
+    https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+    '''
+
+    for (dirpath, dirnames, filenames) in walk(dir_string):
+        #if '__pycache__' in dirnames:
+        #    dirnames.remove('__pycache__')
+        # print(f"dirpath = {dirpath}")
+        # print(f"dirnames = {dirnames}")
+        # print(f"filenames = {filenames}")
+        if '__init__.py' in filenames:
+            filenames.remove('__init__.py')
+        break  # In this version we don't traverse any subdirectories
+
+    return sorted(filenames)
+
+
+def extract_required_members(module, exclusions):
+    '''Which class members do we wish to include in
+    the .rst file we will write?
+
+    Skip over the ones we don't want.'''
+    pass
+
+def write_rst_file(filename):
+    pass
 
 if __name__ == '__main__':
 
@@ -22,6 +66,7 @@ if __name__ == '__main__':
     leader = "tlo.methods.mockitis"
     for name, obj in stuff:
         if leader in str(obj) and inspect.isclass(obj):
-            print (obj)
+            print (obj)  # e.g. <class 'tlo.methods.mockitis.MockitisEvent'>
 
-
+    modules = generate_module_list(MODULE_DIR)
+    print (modules)
