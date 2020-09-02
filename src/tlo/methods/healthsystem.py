851a4064ccf42995910e6fcfa41a0a50dae51a84
Tim Hallett <timothy.hallett@imperial.ac.uk>
2020-09-02 11:27:22 +0100

    streamlined handling of consumables requests

--- src/tlo/methods/healthsystem.py
+++ src/tlo/methods/healthsystem.py
@@ -1255,11 +1255,47 @@ class HSI_Event:
         self.apply(self.target, squeeze_factor)
         self.post_apply_hook()
 
-    def get_all_consumables(self, item_code):
+    def get_all_consumables(self, item_codes=None, pkg_codes=None):
         """Helper function to allow for getting and checking of entire set of consumables.
         It accepts a footprint, or an item_code, or a package_code, and returns True/False for whether all the items
-         are available"""
-        pass
+         are available. It avoids the use of consumables 'footprints'. cons_item_code_availability_today"""
+
+        # Turn the input arguments into the usual consumables footprint:
+
+        # Item Codes provided:
+        if item_codes is not None:
+            if not isinstance(item_codes, list):
+                item_codes = [item_codes]
+            # turn into 'consumable footprint':
+            footprint_items = dict(zip(item_codes, [1]*len(item_codes)))
+        else:
+            footprint_items = {}
+
+        # Package Codes provided:
+        if pkg_codes is not None:
+            if not isinstance(pkg_codes, list):
+                pkg_codes = [pkg_codes]
+            footprint_pkgs = dict(zip(pkg_codes, [1]*len(pkg_codes)))
+        else:
+            footprint_pkgs = {}
+
+        # Make the total footprint
+        footprint = {
+            'Item_Code': footprint_items,
+            'Intervention_Package_Code': footprint_pkgs,
+        }
+
+        # Check availability of consumables
+        rtn_from_health_system = self.sim.modules['HealthSystem'].request_consumables(self, footprint)
+
+        all_available = all(
+            rtn_from_health_system['Intervention_Package_Code'].values()
+        ) and all(
+            rtn_from_health_system['Item_Code'].values()
+        )
+
+        return all_available
+
 
 class HSIEventWrapper(Event):
     """This is wrapper that contains an HSI event.
