45d3636c2ed87220d74307f89bd8e9eecd9cefa6
tdm32 <t.mangal@imperial.ac.uk>
2020-09-09 16:14:57 +0100

    brought in dx_algorithm_child.py from master with diarrhoea updates

--- src/tlo/methods/dx_algorithm_child.py
+++ src/tlo/methods/dx_algorithm_child.py
@@ -1,103 +1,162 @@
 """
-An example of a diagnostic algorithm that is called during an HSI Event.
+This is the place for all the stuff to do with diagnosing a child that presents for care.
+It is expected that the pieces of logic and data that go here will be shared across multiple modules so they
+are put here rather than the individual disease modules.
+
+There should be a method here to respond to every symptom that a child could present with. Currently this module is
+served by the following disease modules:
+* Diarrhoea
+
+
 """
-from tlo import Module, logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-
-# ---------------------------------------------------------------------------------------------------------
-#   MODULE DEFINITIONS
-# ---------------------------------------------------------------------------------------------------------
+from tlo import Module
+from tlo.methods.diarrhoea import (
+    HSI_Diarrhoea_Dysentery,
+    HSI_Diarrhoea_Non_Severe_Persistent_Diarrhoea,
+    HSI_Diarrhoea_Severe_Persistent_Diarrhoea,
+    HSI_Diarrhoea_Treatment_PlanA,
+    HSI_Diarrhoea_Treatment_PlanB,
+    HSI_Diarrhoea_Treatment_PlanC,
+)
+from tlo.methods.dxmanager import DxTest
 
 
 class DxAlgorithmChild(Module):
     """
-    This is an example/placeholder to show how a diagnostic algorithm can be used.
-    The module contains parameters and a function 'diagnose(...)' which is called by a HSI (usually a Generic HSI)
-    and returns a 'diagnosis'.
+    The module contains parameters and functions to 'diagnose(...)' children.
+    These functions are called by an HSI (usually a Generic HSI)
     """
 
-    # Define parameters
     PARAMETERS = {}
-
-    # No Properties to define
     PROPERTIES = {}
 
     def __init__(self, name=None, resourcefilepath=None):
-        # NB. Parameters passed to the module can be inserted in the __init__ definition.
-
         super().__init__(name)
         self.resourcefilepath = resourcefilepath
 
     def read_parameters(self, data_folder):
-        """Read parameter values from file, if required.
-        To access files use: Path(self.resourcefilepath) / file_name
-        """
         pass
 
     def initialise_population(self, population):
         pass
 
     def initialise_simulation(self, sim):
-        pass
+        """
+        Define the Diagnostics Tests that will be used
+        """
+
+        # Test for the visual inspection of 'Danger signs' for a child who is dehydrated
+        if 'Diarrhoea' in self.sim.modules:
+            self.sim.modules['HealthSystem'].dx_manager.register_dx_test(
+                danger_signs_visual_inspection=DxTest(
+                    property='gi_last_diarrhoea_dehydration',
+                    target_categories=['severe'],
+                    sensitivity=0.90,
+                    specificity=0.80
+                )
+            )
 
     def on_birth(self, mother_id, child_id):
         pass
 
-    def diagnose(self, person_id, hsi_event):
+    def do_when_diarrhoea(self, person_id, hsi_event):
         """
-        This will diagnose the condition of the person. It is being called from inside an HSI Event.
+        This routine is called when Diarrhoea is reported.
 
-        :param person_id: The person is to be diagnosed
-        :param hsi_event: The calling hsi_event.
-        :return: a string representing the diagnosis
+        It diagnoses the condition of the child and schedules HSI Events appropriate to the condition.
+
+        See this report https://apps.who.int/iris/bitstream/handle/10665/104772/9789241506823_Chartbook_eng.pdf
+        (page 3).
+        NB:
+            * Provisions for cholera are not included
+            * The danger signs are classified collectively and are based on the result of a DxTest representing the
+              ability of the clinician to correctly determine the true value of the
+              property 'gi_current_severe_dehydration'
         """
+        # Create some short-cuts:
+        schedule_hsi = self.sim.modules['HealthSystem'].schedule_hsi_event
         df = self.sim.population.props
-        hs = self.sim.modules["HealthSystem"]
 
-        diagnosis_str = "unknown"
+        def run_dx_test(test):
+            return self.sim.modules['HealthSystem'].dx_manager.run_dx_test(dx_tests_to_run=test, hsi_event=hsi_event)
 
-        # get the symptoms of the person:
-        # num_of_symptoms = sum(symptoms.apply(lambda symp: symp != set()))
-        # symptoms = df.loc[person_id, df.columns.str.startswith('sy_')]
+        symptoms = self.sim.modules['SymptomManager'].has_what(person_id)
 
-        if "fever" in self.sim.modules["SymptomManager"].has_what(person_id):
+        # Gather information that can be reported:
+        # 1) Get duration of diarrhoea to date
+        duration_in_days = (self.sim.date - df.at[person_id, 'gi_last_diarrhoea_date_of_onset']).days
+        assert duration_in_days >= 0
 
-            # call the DxTest RDT to diagnose malaria
-            dx_result = hs.dx_manager.run_dx_test(
-                dx_tests_to_run='malaria_rdt',
-                hsi_event=hsi_event
-            )
+        # 2) Get type of diarrhoea
+        blood_in_stool = df.at[person_id, 'gi_last_diarrhoea_type'] == 'bloody'
 
-            if dx_result:
+        # 3) Get status of dehydration
+        dehydration = 'dehydration' in symptoms
 
-                # severe malaria
-                if df.at[person_id, "ma_inf_type"] == "severe":
-                    diagnosis_str = "severe_malaria"
+        # Gather information that cannot be reported:
+        # 1) Assessment of danger signs
+        danger_signs = run_dx_test('danger_signs_visual_inspection')
 
-                    logger.debug(
-                        "dx_algorithm_child diagnosing severe malaria for person %d on date %s",
-                        person_id,
-                        self.sim.date,
-                    )
+        # Apply the algorithms:
+        # --------   Classify Extent of Dehydration   ---------
+        if not dehydration:
+            # Those who do NOT have DEHYDRATION
 
-                # clinical malaria
-                elif df.at[person_id, "ma_inf_type"] == "clinical":
+            # Treatment Plan A for uncomplicated diarrhoea (no dehydration and no danger signs)
+            schedule_hsi(hsi_event=HSI_Diarrhoea_Treatment_PlanA(person_id=person_id,
+                                                                 module=self.sim.modules['Diarrhoea']),
+                         priority=0,
+                         topen=self.sim.date,
+                         tclose=None
+                         )
 
-                    diagnosis_str = "clinical_malaria"
+            if duration_in_days >= 14:
+                # 'Non_Severe_Persistent_Diarrhoea'
+                schedule_hsi(hsi_event=HSI_Diarrhoea_Non_Severe_Persistent_Diarrhoea(person_id=person_id,
+                                                                                     module=self.sim.modules[
+                                                                                         'Diarrhoea']),
+                             priority=0,
+                             topen=self.sim.date,
+                             tclose=None
+                             )
 
-                    logger.debug(
-                        "dx_algorithm_child diagnosing clinical malaria for person %d on date %s",
-                        person_id,
-                        self.sim.date,
-                    )
+        else:
+            # Those who do have DEHYDRATION
 
+            # Given that there is dehydration - schedule an HSI if the duration of diarrhoea has been long
+            if duration_in_days >= 14:
+                # 'Severe_Persistent_Diarrhoea'
+                schedule_hsi(hsi_event=HSI_Diarrhoea_Severe_Persistent_Diarrhoea(person_id=person_id,
+                                                                                 module=self.sim.modules['Diarrhoea']),
+                             priority=0,
+                             topen=self.sim.date,
+                             tclose=None
+                             )
+
+            if not danger_signs:
+                # Treatment Plan B for some dehydration diarrhoea but not danger signs
+                # TODO:add "...and not other severe classification from other disease modules (measles, pneumonia, etc)"
+                schedule_hsi(hsi_event=HSI_Diarrhoea_Treatment_PlanB(person_id=person_id,
+                                                                     module=self.sim.modules['Diarrhoea']),
+                             priority=0,
+                             topen=self.sim.date,
+                             tclose=None
+                             )
             else:
-                diagnosis_str = "negative_malaria_test"
+                # Danger sign for 'Severe_Dehydration'
+                schedule_hsi(hsi_event=HSI_Diarrhoea_Treatment_PlanC(person_id=person_id,
+                                                                     module=self.sim.modules['Diarrhoea']),
+                             priority=0,
+                             topen=self.sim.date,
+                             tclose=None
+                             )
 
-        logger.debug(f"{person_id} diagnosis is {diagnosis_str}")
-
-        # return the diagnosis as a string
-        return diagnosis_str
+        # --------  Classify Whether Dysentery or Not  --------
+        if blood_in_stool:
+            # 'Dysentery'
+            schedule_hsi(hsi_event=HSI_Diarrhoea_Dysentery(person_id=person_id, module=self.sim.modules['Diarrhoea']),
+                         priority=0,
+                         topen=self.sim.date,
+                         tclose=None
+                         )
+        # -----------------------------------------------------
