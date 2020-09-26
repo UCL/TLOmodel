5210a64b19b94375b698762efdc08f770bd15788
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2020-09-26 17:57:04 +0100

    .

--- src/scripts/bladder_cancer_analyses/bladder_cancer_analyses_logging.py
+++ src/scripts/bladder_cancer_analyses/bladder_cancer_analyses_logging.py
@@ -32,7 +32,7 @@ resourcefilepath = Path("./resources")
 
 start_date = Date(2010, 1, 1)
 end_date = Date(2020,  1, 1)
-popsize = 19000
+popsize = 1900000
 
 # Establish the simulation object
 log_config = {
