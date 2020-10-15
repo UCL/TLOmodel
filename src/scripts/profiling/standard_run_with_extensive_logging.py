8fb8d13b838379a6fc2fe176f712e170e948ee51
Tim Hallett <timothy.hallett@imperial.ac.uk>
2020-10-15 12:17:24 +0100

    a script that includes the logging

---
+++ src/scripts/profiling/standard_run_with_extensive_logging.py
@@ -0,0 +1,85 @@
+"""
+A run of the model at scale using all disease modules currently included in Master - including logging
+* All logging
+* Script including parsing logfile
+
+For use in profiling.
+"""
+
+from pathlib import Path
+
+import pandas as pd
+
+from tlo import Date, Simulation, logging
+from tlo.analysis.utils import parse_log_file
+from tlo.methods import (
+    contraception,
+    demography,
+    depression,
+    dx_algorithm_child,
+    enhanced_lifestyle,
+    healthburden,
+    healthseekingbehaviour,
+    healthsystem,
+    labour,
+    pregnancy_supervisor,
+    symptommanager,
+    oesophagealcancer,
+    malaria,
+    epi,
+    epilepsy,
+    dx_algorithm_adult,
+)
+
+# Key parameters about the simulation:
+start_date = Date(2010, 1, 1)
+end_date = start_date + pd.DateOffset(years=30)
+
+popsize = int(5e5)
+
+# The resource files
+resourcefilepath = Path("./resources")
+
+log_config = {
+    "filename": "for_profiling",
+    "directory": "./outputs",
+    "custom_levels": {"*": logging.INFO}
+}
+
+sim = Simulation(start_date=start_date, seed=0, log_config=log_config)
+
+# Register the appropriate modules
+sim.register(
+    # Standard modules:
+    demography.Demography(resourcefilepath=resourcefilepath),
+    enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
+    healthsystem.HealthSystem(resourcefilepath=resourcefilepath),
+    symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
+    healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
+    healthburden.HealthBurden(resourcefilepath=resourcefilepath),
+    contraception.Contraception(resourcefilepath=resourcefilepath),
+    labour.Labour(resourcefilepath=resourcefilepath),
+    pregnancy_supervisor.PregnancySupervisor(resourcefilepath=resourcefilepath),
+    dx_algorithm_child.DxAlgorithmChild(resourcefilepath=resourcefilepath),
+    dx_algorithm_adult.DxAlgorithmAdult(resourcefilepath=resourcefilepath),
+    #
+    # Disease modules considered complete:
+    malaria.Malaria(resourcefilepath=resourcefilepath),
+    epi.Epi(resourcefilepath=resourcefilepath),
+    depression.Depression(resourcefilepath=resourcefilepath),
+    oesophagealcancer.OesophagealCancer(resourcefilepath=resourcefilepath),
+    epilepsy.Epilepsy(resourcefilepath=resourcefilepath)
+)
+
+# Run the simulation
+sim.make_initial_population(n=popsize)
+sim.simulate(end_date=end_date)
+
+# Parse the log-file
+log_df = parse_log_file(sim.log_filepath)
+
+
+
+
+
+
