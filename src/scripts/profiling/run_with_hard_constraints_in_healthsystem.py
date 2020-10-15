8cb0444df083f54d151df67cdfd00b862cd00252
Tim Hallett <timothy.hallett@imperial.ac.uk>
2020-10-15 12:13:21 +0100

    edits to standard_run.py

---
+++ src/scripts/profiling/run_with_hard_constraints_in_healthsystem.py
@@ -0,0 +1,93 @@
+"""
+A run of the model that uses a lot of health system functionality -
+
+* Allow appoints to be rolled over if not capacity and runs with reduced capacity
+
+For use in profiling.
+"""
+
+
+"""
+A run of the model at scale using all disease modules currently included in Master - with no logging
+
+For use in profiling.
+"""
+
+from pathlib import Path
+
+import pandas as pd
+
+from tlo import Date, Simulation, logging
+from tlo.methods import (
+    contraception,
+    demography,
+    depression,
+    dx_algorithm_child,
+    enhanced_lifestyle,
+    healthburden,
+    healthseekingbehaviour,
+    healthsystem,
+    labour,
+    pregnancy_supervisor,
+    symptommanager,
+    oesophagealcancer,
+    malaria,
+    epi,
+    epilepsy,
+    dx_algorithm_adult,
+)
+
+# Key parameters about the simulation:
+start_date = Date(2010, 1, 1)
+end_date = start_date + pd.DateOffset(years=30)
+
+popsize = int(5e5)
+
+# The resource files
+resourcefilepath = Path("./resources")
+
+log_config = {
+    "filename": "for_profiling",
+    "directory": "./outputs",
+    "custom_levels": {"*": logging.WARNING}
+}
+
+sim = Simulation(start_date=start_date, seed=0, log_config=log_config)
+
+# Register the appropriate modules
+sim.register(
+    # Standard modules:
+    demography.Demography(resourcefilepath=resourcefilepath),
+    enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
+    healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
+                              mode_appt_constraints=2,
+                              capabilities_coefficient=0.01
+                              ),
+    symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
+    healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
+    healthburden.HealthBurden(resourcefilepath=resourcefilepath),
+    contraception.Contraception(resourcefilepath=resourcefilepath),
+    labour.Labour(resourcefilepath=resourcefilepath),
+    pregnancy_supervisor.PregnancySupervisor(resourcefilepath=resourcefilepath),
+    dx_algorithm_child.DxAlgorithmChild(resourcefilepath=resourcefilepath),
+    dx_algorithm_adult.DxAlgorithmAdult(resourcefilepath=resourcefilepath),
+    #
+    # Disease modules considered complete:
+    malaria.Malaria(resourcefilepath=resourcefilepath),
+    epi.Epi(resourcefilepath=resourcefilepath),
+    depression.Depression(resourcefilepath=resourcefilepath),
+    oesophagealcancer.OesophagealCancer(resourcefilepath=resourcefilepath),
+    epilepsy.Epilepsy(resourcefilepath=resourcefilepath)
+)
+
+# Run the simulation
+sim.make_initial_population(n=popsize)
+sim.simulate(end_date=end_date)
+
+
+
+
+
+
+
+
--- src/scripts/profiling/run_with_heavy_hsi_usage.py
+++
@@ -1,9 +0,0 @@
-"""
-A run of the model that uses a lot of health system functionality
-
-For use in profiling.
-"""
-
-
-
-
