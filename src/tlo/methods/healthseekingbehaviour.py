51868cbc81b166f2f529bb77420ee5572eb4edb9
Asif Tamuri <tamuri@gmail.com>
2020-10-23 15:44:47 +0100

    Improve performance of health seeking poll - Slowest procedure on large 500k pop. sim. run for 2 yrs - Move lookups on dataframe outside of person loop     - baseline probability odds, symptoms and is_child for all persons - Use iterrows to loop over person data - Remove nested-functions and move calculations inline - Remove unnecessary assertions

--- src/tlo/methods/healthseekingbehaviour.py
+++ src/tlo/methods/healthseekingbehaviour.py
@@ -87,6 +87,14 @@ class HealthSeekingBehaviour(Module):
         super().__init__(name)
         self.resourcefilepath = resourcefilepath
 
+        self.hsb = dict()
+        self.odds_ratio_health_seeking_in_children = dict()
+        self.odds_ratio_health_seeking_in_adults = dict()
+        self.no_healthcareseeking_in_children = set()
+        self.emergency_in_children = set()
+        self.no_healthcareseeking_in_adults = set()
+        self.emergency_in_adults = set()
+
         # "force_any_symptom_to_lead_to_healthcareseeking"=True will mean that probability of health care seeking is 1.0
         # for anyone with newly onset symptoms
         assert isinstance(force_any_symptom_to_lead_to_healthcareseeking, bool)
@@ -104,7 +112,7 @@ class HealthSeekingBehaviour(Module):
 
         # Define the LinearModel for health seeking behaviour for the 'average symptom'
         p = self.parameters
-        self.hsb = dict()
+
         self.hsb['children'] = LinearModel(
             LinearModelType.LOGISTIC,
             p['baseline_odds_of_healthcareseeking_children'],
@@ -145,29 +153,20 @@ class HealthSeekingBehaviour(Module):
         sim.schedule_event(HealthSeekingBehaviourPoll(self), sim.date)
 
         # Assemble the health-care seeking information from the symptoms that have been registered
-        self.no_healthcareseeking_in_children = set()
-        self.emergency_in_children = set()
-        self.odds_ratio_health_seeking_in_children = dict()
-        self.no_healthcareseeking_in_adults = set()
-        self.emergency_in_adults = set()
-        self.odds_ratio_health_seeking_in_adults = dict()
-
         for symptom in self.sim.modules['SymptomManager'].all_registered_symptoms:
             # Children:
             if symptom.no_healthcareseeking_in_children:
-                self.no_healthcareseeking_in_children = self.no_healthcareseeking_in_children.union({symptom.name})
-
+                self.no_healthcareseeking_in_children.add(symptom.name)
             elif symptom.emergency_in_children:
-                self.emergency_in_children = self.emergency_in_children.union({symptom.name})
+                self.emergency_in_children.add(symptom.name)
             else:
                 self.odds_ratio_health_seeking_in_children[symptom.name] = symptom.odds_ratio_health_seeking_in_children
 
             # Adults:
             if symptom.no_healthcareseeking_in_adults:
-                self.no_healthcareseeking_in_adults = self.no_healthcareseeking_in_adults.union({symptom.name})
-
+                self.no_healthcareseeking_in_adults.add(symptom.name)
             elif symptom.emergency_in_adults:
-                self.emergency_in_adults = self.emergency_in_adults.union({symptom.name})
+                self.emergency_in_adults.add(symptom.name)
             else:
                 self.odds_ratio_health_seeking_in_adults[symptom.name] = symptom.odds_ratio_health_seeking_in_adults
 
@@ -184,7 +183,6 @@ class HealthSeekingBehaviour(Module):
 class HealthSeekingBehaviourPoll(RegularEvent, PopulationScopeEventMixin):
     """This event occurs every day and determines if persons with newly onset symptoms will seek care.
     """
-
     def __init__(self, module):
         """Initialise the HealthSeekingBehaviourPoll
         :param module: the module that created this event
@@ -198,112 +196,93 @@ class HealthSeekingBehaviourPoll(RegularEvent, PopulationScopeEventMixin):
         :param population: the current population
         """
         m = self.module
+        df = population.props
+        symptom_manager = self.sim.modules["SymptomManager"]
+        health_system = self.sim.modules["HealthSystem"]
 
-        # get the list of person_ids who have onset generic acute symptoms in the last day, extracting any person_ids
-        #    that have died (since the onset of symptoms)
-        alive_person_ids = list(self.sim.population.props.index[self.sim.population.props.is_alive])
-        person_ids_with_new_symptoms = list(
-            m.sim.modules['SymptomManager'].persons_with_newly_onset_symptoms.intersection(alive_person_ids)
-        )
+        # get rows of alive persons with new symptoms
+        alive_with_symptoms = df.loc[sorted(symptom_manager.persons_with_newly_onset_symptoms), 'is_alive']
+        selected_persons = df.loc[alive_with_symptoms[alive_with_symptoms].index]
 
-        # clear the list of person_ids with newly onset symptoms
-        m.sim.modules['SymptomManager'].persons_with_newly_onset_symptoms = set()
+        # clear the list of persons with newly onset symptoms
+        symptom_manager.persons_with_newly_onset_symptoms.clear()
 
-        def make_generic_emergency_first_appt(person_id):
-            """Schedule a generic emergency first appt for the current date."""
-            hsi_genericemergencyfirstappt = HSI_GenericEmergencyFirstApptAtFacilityLevel1(self.module,
-                                                                                          person_id=person_id)
-            self.sim.modules['HealthSystem'].schedule_hsi_event(hsi_genericemergencyfirstappt,
-                                                                priority=0,
-                                                                topen=self.sim.date,
-                                                                tclose=None)
+        # calculate the baseline probability for adults and children to seek care & put them in single series
+        # (index remains the person_id)
+        baseline_prob_child = m.hsb['children'].predict(selected_persons[selected_persons.age_years < 15])
+        baseline_prob_adult = m.hsb['adults'].predict(selected_persons[selected_persons.age_years >= 15])
+        baseline_prob = baseline_prob_child.append(baseline_prob_adult, verify_integrity=True).rename('baseline_prob')
 
-        def make_generic_non_emergency_first_appt_at_facility_level1(person_id):
-            """Schedule a generic non-emergency appointment.
-            Occurs after a delay of 0-4 days, or immediately if using 'force_any_symptom_to_lead_to_healthcareseeking'.
-            """
-            if self.module.force_any_symptom_to_lead_to_healthcareseeking:
-                date_of_seeking_care = self.sim.date
-            else:
-                max_days_delay = self.module.parameters['max_days_delay_to_generic_HSI_after_symptoms']
-                date_of_seeking_care = self.sim.date + DateOffset(days=m.rng.randint(0, max_days_delay))
+        # this replaces calls to `symptom_manager.has_what(person_id)` inside the loop. get all the symptoms for
+        # all persons and skip the checks/options in the `has_what()` method
+        # todo: move this to symptom manager i.e. have_what(person_ids) (but better name)
+        persons_symptoms = selected_persons.apply(
+            lambda p: [s for s in symptom_manager.symptom_names if p[f'sy_{s}'] > 0], axis=1
+        ).rename('symptoms')
 
-            hsi_genericfirstappt = HSI_GenericFirstApptAtFacilityLevel1(m, person_id=person_id)
-            self.sim.modules['HealthSystem'].schedule_hsi_event(hsi_genericfirstappt,
-                                                                priority=0,
-                                                                topen=date_of_seeking_care,
-                                                                tclose=None)
+        # make dataframe for processing below:
+        # person_id (index), baseline_prob, age_years < 15 (i.e. is child), symptoms (list object)
+        persons = pd.concat([baseline_prob, selected_persons.age_years < 15, persons_symptoms], axis=1)
 
-        def get_joint_odds_ratio_of_all_symptoms(symptoms_list, is_child):
-            """For a given symptoms list (of symptoms that do causes care seeking), get the associated odds ratio for
-            health seeking behaviour: this is the product of the odds ratio associated with each symptom."""
-            odds_ratio_list = []
-            for s in symptoms_list:
-                if is_child:
-                    assert s not in m.emergency_in_children
-                    assert s not in m.no_healthcareseeking_in_children
-                    assert s in m.odds_ratio_health_seeking_in_children
-                    odds_ratio_list.append(m.odds_ratio_health_seeking_in_children[s])
-                else:
-                    assert s not in m.emergency_in_adults
-                    assert s not in m.no_healthcareseeking_in_adults
-                    assert s in m.odds_ratio_health_seeking_in_adults
-                    odds_ratio_list.append(m.odds_ratio_health_seeking_in_adults[s])
+        # For each individual person_id, with at least one new onset symptom,
+        # look at the symptoms and determine if will seek care.
+        # This is run looking at all symptoms even if only one is newly onset.
+        for person_id, person in persons.iterrows():
+            symptoms = person['symptoms']
+            is_child = person['age_years']
 
-            return np.array(odds_ratio_list).prod()
+            # determine whether symptoms cause person to seek care
+            seek_emergency_care = False
+            seek_non_emergency_care = False
 
-        def compute_prob_from_baseline_prob_and_odds_ratio(baseline_prob, odds_ratio):
-            """For a baseline probability and an effect odds ratio, compute probability that is implied"""
-            baseline_odds = baseline_prob / (1 - baseline_prob)
-            odds = baseline_odds * odds_ratio
-            return odds / (1 + odds)
+            # Look at joint effect on health-care seeking of all symptoms. For a given symptoms list (of symptoms
+            # that do causes care seeking), get the associated odds ratio for health seeking behaviour: this is the
+            # product of the odds ratio associated with each symptom.
+            care_seeking_odds = 1
 
-        for person_id in person_ids_with_new_symptoms:
-            # For each individual person_id, with at least one new onset symptom, look at the symptoms and determine if
-            # will seek care.
-            # This is run looking at all symptoms even if only one is newly onset.
-
-            symptoms = m.sim.modules['SymptomManager'].has_what(person_id)
-            is_child = population.props.at[person_id, 'age_years'] < 15
-
-            # sort the symptoms into the different categories:
-            emergency_care_seeking_symptoms = []
-            non_care_seeking_symptoms = []
-            care_seeking_symptoms = []
-
-            for symptom in symptoms:
-                if is_child:
+            if is_child:
+                for symptom in symptoms:
                     if symptom in m.emergency_in_children:
-                        emergency_care_seeking_symptoms.append(symptom)
-                    elif symptom in m.no_healthcareseeking_in_children:
-                        non_care_seeking_symptoms.append(symptom)
-                    else:
-                        care_seeking_symptoms.append(symptom)
-
-                if not is_child:
+                        seek_emergency_care = True
+                    elif symptom not in m.no_healthcareseeking_in_children:
+                        seek_non_emergency_care = True
+                        care_seeking_odds *= m.odds_ratio_health_seeking_in_children[symptom]
+            else:
+                for symptom in symptoms:
                     if symptom in m.emergency_in_adults:
-                        emergency_care_seeking_symptoms.append(symptom)
-                    elif symptom in m.no_healthcareseeking_in_adults:
-                        non_care_seeking_symptoms.append(symptom)
-                    else:
-                        care_seeking_symptoms.append(symptom)
+                        seek_emergency_care = True
+                    elif symptom not in m.no_healthcareseeking_in_adults:
+                        seek_non_emergency_care = True
+                        care_seeking_odds *= m.odds_ratio_health_seeking_in_adults[symptom]
 
-            assert len(symptoms) == (
-                len(emergency_care_seeking_symptoms) + len(non_care_seeking_symptoms) + len(care_seeking_symptoms)
-            )
-
-            # if any of the symptom is an emergency - generate an emergency HSI:
-            if len(emergency_care_seeking_symptoms) > 0:
-                make_generic_emergency_first_appt(person_id)
+            # Generate an emergency HSI, if any of the symptoms is an emergency
+            if seek_emergency_care:
+                health_system.schedule_hsi_event(
+                    HSI_GenericEmergencyFirstApptAtFacilityLevel1(m, person_id=person_id),
+                    priority=0,
+                    topen=self.sim.date,
+                    tclose=None
+                )
 
             # if any non-emergency symptoms - consider making a generic non-emergency HSI:
-            if len(care_seeking_symptoms) > 0:
-                # Look at joint effect on health-care seeking of all symptoms
-                if is_child:
-                    baseline_prob = m.hsb['children'].predict(population.props.loc[[person_id]]).values[0]
-                else:
-                    baseline_prob = m.hsb['adults'].predict(population.props.loc[[person_id]]).values[0]
-                odds_ratio = get_joint_odds_ratio_of_all_symptoms(care_seeking_symptoms, is_child)
-                prob_hsb = compute_prob_from_baseline_prob_and_odds_ratio(baseline_prob, odds_ratio)
-                if (m.rng.rand() < prob_hsb) or self.module.force_any_symptom_to_lead_to_healthcareseeking:
-                    make_generic_non_emergency_first_appt_at_facility_level1(person_id)
+            if seek_non_emergency_care:
+                # For a baseline probability and an effect odds ratio, compute probability that is implied
+                baseline_odds = person['baseline_prob'] / (1 - person['baseline_prob'])
+                odds = baseline_odds * care_seeking_odds
+                prob_hsb = odds / (1 + odds)
+
+                if (m.rng.rand() < prob_hsb) or m.force_any_symptom_to_lead_to_healthcareseeking:
+                    # Schedule a generic non-emergency appointment. Occurs after a delay of 0-4 days, or immediately
+                    # if using 'force_any_symptom_to_lead_to_healthcareseeking'.
+                    if m.force_any_symptom_to_lead_to_healthcareseeking:
+                        date_of_seeking_care = self.sim.date
+                    else:
+                        max_days_delay = m.parameters['max_days_delay_to_generic_HSI_after_symptoms']
+                        date_of_seeking_care = self.sim.date + DateOffset(days=m.rng.randint(0, max_days_delay))
+
+                    health_system.schedule_hsi_event(
+                        HSI_GenericFirstApptAtFacilityLevel1(m, person_id=person_id),
+                        priority=0,
+                        topen=date_of_seeking_care,
+                        tclose=None
+                    )
