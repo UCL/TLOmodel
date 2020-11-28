054f80031222b8973af5c696a8c64d13107e3cba
Andrew Phillips <andrew.phillips@ucl.ac.uk>
2020-11-28 12:25:13 +0000

    .

--- src/scripts/breast_cancer_analyses_logging.py
+++ src/scripts/breast_cancer_analyses_logging.py
@@ -32,7 +32,7 @@ resourcefilepath = Path("./resources")
 
 start_date = Date(2010, 1, 1)
 end_date = Date(2021,  1, 1)
-popsize = 100
+popsize = 1450000
 # 1450000
 
 # Establish the simulation object
