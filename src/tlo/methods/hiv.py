3e0fb9fa22dd3566922fec50992f41f4c686b516
Asif Tamuri <tamuri@gmail.com>
2021-02-07 23:49:30 +0000

    lint imports

--- src/tlo/methods/hiv.py
+++ src/tlo/methods/hiv.py
@@ -34,7 +34,7 @@ import os
 import numpy as np
 import pandas as pd
 
-from tlo import DateOffset, Module, Parameter, Property, Types, logging, DAYS_IN_YEAR
+from tlo import DAYS_IN_YEAR, DateOffset, Module, Parameter, Property, Types, logging
 from tlo.events import Event, IndividualScopeEventMixin, PopulationScopeEventMixin, RegularEvent
 from tlo.lm import LinearModel, LinearModelType, Predictor
 from tlo.methods import Metadata, demography
