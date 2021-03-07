cf880e982c56397aee7340a3d42381f4851dd7b4
Timothy Colbourn <timcolbourn@hotmail.com>
2021-03-07 11:31:15 +0000

    comment out remove_beddays_footprint reference in demography as this version of health system doesn't have that (tried to merge in new healthsystem from Master but it didn't seem to work even though said it had merged)

--- src/tlo/methods/demography.py
+++ src/tlo/methods/demography.py
@@ -357,8 +357,8 @@ class InstantaneousDeath(Event, IndividualScopeEventMixin):
             df.at[individual_id, 'cause_of_death'] = self.cause
 
             # release any beds-days that would be used by this person:
-            if 'HealthSystem' in self.sim.modules:
-                self.sim.modules['HealthSystem'].remove_beddays_footprint(person_id=individual_id)
+            #if 'HealthSystem' in self.sim.modules:
+            #    self.sim.modules['HealthSystem'].remove_beddays_footprint(person_id=individual_id)
 
         logger.debug(key='official_death',
                      data=(f"*******************************************The person {individual_id} "
