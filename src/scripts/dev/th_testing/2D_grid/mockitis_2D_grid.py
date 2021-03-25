aad980ae3a1460335ae23a620dfdf6eff7ef3508
Asif Tamuri <tamuri@gmail.com>
2021-03-25 14:53:12 +0000

    Move make_grid into base class

--- src/scripts/dev/th_testing/2D_grid/mockitis_2D_grid.py
+++ src/scripts/dev/th_testing/2D_grid/mockitis_2D_grid.py
@@ -7,7 +7,6 @@ Run on the batch system using:
 """
 
 import numpy as np
-import pandas as pd
 
 from tlo import Date, logging
 from tlo.methods import (
@@ -60,10 +59,7 @@ class MockitisBatch(BaseScenario):
 
     def draw_parameters(self, draw_number, rng):
         grid = self.make_grid(
-            {
-                'p_infection': np.linspace(0, 1.0, 3),
-                'p_cure': [0.25, 0.5]
-            }
+            {'p_infection': np.linspace(0, 1.0, 3), 'p_cure': [0.25, 0.5]}
         )
 
         return {
@@ -72,34 +68,6 @@ class MockitisBatch(BaseScenario):
                 'p_cure': grid['p_cure'][draw_number]
             },
         }
-
-    def make_grid(self, ranges: dict) -> pd.DataFrame:
-        """Utility function to flatten an n-dimension grid of parameters for use in scenarios
-
-        Typically used in draw_parameters determining a set of parameters for a draw. This function will check that the
-        number of draws of the scenario is equal to the number of coordinates in the grid.
-
-        Parameter 'ranges' is a dictionary of { string key: iterable }, where iterable can be, for example, an np.array
-        or list. The function will return a DataFrame where each key is a column and each row represents a single
-        coordinate in the grid.
-
-        Usage (in draw_parameters):
-
-            grid = self.make_grid({'p_one': np.linspace(0, 1.0, 5), 'p_two': np.linspace(3.0, 4.0, 2)})
-            return {
-                'Mockitis': {
-                    grid['p_one'][draw_number],
-                    grid['p_two'][draw_number]
-                }
-            }
-        """
-        grid = np.meshgrid(*ranges.values())
-        flattened = [g.ravel() for g in grid]
-        positions = np.stack(flattened, axis=1)
-        grid_lookup = pd.DataFrame(positions, columns=ranges.keys())
-        assert self.number_of_draws == len(grid_lookup), f"{len(grid_lookup)} coordinates in grid, " \
-                                                         f"but number_of_draws is {self.number_of_draws}."
-        return grid_lookup
 
 
 if __name__ == '__main__':
