a7b97295e5dfbb09180862e98727dd7b61fc3f66
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2021-04-09 15:56:02 +0100

    linting

--- src/scripts/other_adult_cancers_analyses/other_adult_cancers_analyses.py
+++ src/scripts/other_adult_cancers_analyses/other_adult_cancers_analyses.py
@@ -46,7 +46,7 @@ resourcefilepath = Path("./resources")
 # Set parameters for the simulation
 start_date = Date(2010, 1, 1)
 end_date = Date(2015, 1, 1)
-popsize = 1000
+popsize = 10000
 
 
 def run_sim(service_availability):
