e937e6fc45e131a44cb5d6ec87435f2b57acb53f
Timothy Colbourn <timcolbourn@hotmail.com>
2021-04-29 15:41:31 +0100

    adjusted for runs for v0 paper (need to increase birth rate still for calibration)

--- src/scripts/demography/analysis_demography_calibrations.py
+++ src/scripts/demography/analysis_demography_calibrations.py
@@ -49,8 +49,8 @@ def run():
 
     # Basic arguments required for the simulation
     start_date = Date(2010, 1, 1)
-    end_date = Date(2050, 1, 2)
-    pop_size = 200
+    end_date = Date(2099, 12, 31)
+    pop_size = 20000
 
     # This creates the Simulation instance for this run. Because we"ve passed the `seed` and
     # `log_config` arguments, these will override the default behaviour.
