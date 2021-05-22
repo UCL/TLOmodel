025f4ae5cab5f1dea80ae18bc99767c7ca80475b
Ines Lin <ines.lin@ucl.ac.uk>
2021-05-22 17:43:21 +0100

    ready for PR

--- src/scripts/undernutrition_analyses/wasting_run_test.py
+++ src/scripts/undernutrition_analyses/wasting_run_test.py
@@ -44,7 +44,7 @@ sim = Simulation(start_date=start_date, seed=seed, log_config=log_config)
 # run the simulation
 sim.register(demography.Demography(resourcefilepath=resourcefilepath))
 sim.register(enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath))
-sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath, disable=True))  # service_availability=service_availability
+sim.register(healthsystem.HealthSystem(resourcefilepath=resourcefilepath, disable=True))
 sim.register(healthburden.HealthBurden(resourcefilepath=resourcefilepath))
 # sim.register(simplified_births.SimplifiedBirths(resourcefilepath=resourcefilepath))
 sim.register(contraception.Contraception(resourcefilepath=resourcefilepath))
