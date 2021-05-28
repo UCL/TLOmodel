98e7d171e5555243cd7ec976ec49f0b7ef6c2c08
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2021-05-28 15:21:03 +0100

    linting

--- src/scripts/bladder_cancer_analyses/bladder_cancer_analyses.py
+++ src/scripts/bladder_cancer_analyses/bladder_cancer_analyses.py
@@ -26,8 +26,8 @@ from tlo.methods import (
     healthsystem,
     labour,
     newborn_outcomes,
-    pregnancy_supervisor,
     postnatal_supervisor,
+    pregnancy_supervisor,
     symptommanager,
 )
 
--- src/scripts/bladder_cancer_analyses/bladder_cancer_analyses_single_run.py
+++ src/scripts/bladder_cancer_analyses/bladder_cancer_analyses_single_run.py
@@ -18,8 +18,8 @@ from tlo.methods import (
     healthsystem,
     labour,
     newborn_outcomes,
-    pregnancy_supervisor,
     postnatal_supervisor,
+    pregnancy_supervisor,
     symptommanager,
 )
 
