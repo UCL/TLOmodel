61e7ec870fc7100087e9e6ab72fcc2c510f14dac
Tim Hallett <timothy.hallett@imperial.ac.uk>
2021-06-09 12:28:02 +0100

    edits to make run shorter

--- src/scripts/calibration_analyses/long_run/long_run.py
+++ src/scripts/calibration_analyses/long_run/long_run.py
@@ -47,9 +47,9 @@ class LongRun(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2030, 12, 31)
-        self.pop_size = 50_000
+        self.pop_size = 1_000
         self.number_of_draws = 1        # <- one scenario
-        self.runs_per_draw = 10          # <- repeated this many times
+        self.runs_per_draw = 3          # <- repeated this many times
 
     def log_configuration(self):
         return {
