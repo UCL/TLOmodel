6925e5924f0248d53953a6740a498b5b5b619d18
tdm32 <t.mangal@imperial.ac.uk>
2021-06-28 18:12:54 +0100

    plot outputs from azure grid search

--- src/scripts/hiv/hiv_tb_analyses/analysing_baseline_scenario.py
+++ src/scripts/hiv/hiv_tb_analyses/analysing_baseline_scenario.py
@@ -10,6 +10,7 @@ import numpy as np
 from tlo.analysis.utils import (
     extract_params,
     extract_results,
+    get_grid,
     get_scenario_info,
     get_scenario_outputs,
     load_pickled_dataframes,
@@ -32,15 +33,61 @@ info = get_scenario_info(results_folder)
 # 1) Extract the parameters that have varied over the set of simulations
 params = extract_params(results_folder)
 
+# ---------------------- EXTRACT HIV PREVALENCE ---------------------- #
 # 2) Extract a specific logged output for all runs, e.g. prevalence:
 extracted = extract_results(results_folder,
                             module="tlo.methods.hiv",
                             key="summary_inc_and_prev_for_adults_and_children_and_fsw",
-                            column="hiv_prev_adult_15plus",
+                            column="hiv_prev_adult_1549",
                             index="date")
 
-# 3) Get summary of the results for that log-element
-prevalence = summarize(extracted)
+# 3) Get summary of the results for that log-element (only mean and the value at then of the simulation)
+res = summarize(extracted, only_mean=True).iloc[-1]
+res.name = 'z'
+
+# 4) Create a heatmap:
+
+grid = get_grid(params, res)
+fig, ax = plt.subplots()
+c = ax.pcolormesh(
+    grid['Hiv:beta'],
+    grid['Tb:transmission_rate'],
+    grid['z'],
+    shading='nearest'
+)
+ax.set_title('HIV prevalence 2019')
+plt.xlabel('Hiv: transmission rate')
+plt.ylabel('Tb: transmission rate')
+fig.colorbar(c, ax=ax)
+plt.show()
+
+# ---------------------- EXTRACT TB PREVALENCE ---------------------- #
+# Extract a specific logged output for all runs
+extracted_tb = extract_results(results_folder,
+                               module="tlo.methods.tb",
+                               key="tb_prevalence",
+                               column="tbPrevActive",
+                               index="date")
+
+# Get summary of the results for that log-element (only mean and the value at then of the simulation)
+tb_prev = summarize(extracted_tb, only_mean=True).iloc[-1]
+tb_prev.name = 'z'
+
+# 4) Create a heatmap:
+
+grid = get_grid(params, res)
+fig, ax = plt.subplots()
+c = ax.pcolormesh(
+    grid['Hiv:beta'],
+    grid['Tb:transmission_rate'],
+    grid['z'],
+    shading='nearest'
+)
+ax.set_title('TB prevalence 2019')
+plt.xlabel('Hiv: transmission rate')
+plt.ylabel('Tb: transmission rate')
+fig.colorbar(c, ax=ax)
+plt.show()
 
 # plot the output values for 2019 (last year for which data are available)
 # grid plot with hiv and tb transmission rates with (i) hiv prevalence and (ii) tb prevalence
@@ -55,4 +102,3 @@ prevalence = summarize(extracted)
 # incidence in adults
 
 # ART coverage
-
