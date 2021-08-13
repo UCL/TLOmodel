b3d21d116c7fd13a33ff2894667caafde846b1e8
Ines Lin <ines.lin@ucl.ac.uk>
2021-08-13 18:47:48 +0100

    test HSI

--- tests/test_wasting.py
+++ tests/test_wasting.py
@@ -54,7 +54,7 @@ start_date = Date(2010, 1, 1)
 
 
 def get_sim(tmpdir):
-    """Return simulation objection with Alri and other necessary modules registered."""
+    """Return simulation objection with Wasting and other necessary modules registered."""
     sim = Simulation(start_date=start_date, seed=0, show_progress_bar=False, log_config={
         'filename': 'tmp',
         'directory': tmpdir,
@@ -86,159 +86,6 @@ def check_dtypes(sim):
     df = sim.population.props
     orig = sim.population.new_row
     assert (df.dtypes == orig.dtypes).all()
-
-
-# def test_integrity_of_linear_models(tmpdir):
-#     """Run the models to make sure that is specified correctly and can run."""
-#     sim = get_sim(tmpdir)
-#     sim.make_initial_population(n=5000)
-#     alri = sim.modules['Alri']
-#     df = sim.population.props
-#     person_id = 0
-#
-#     # make the models
-#     models = Models(alri)
-#
-#     # --- compute_risk_of_acquisition & incidence_equations_by_pathogen:
-#     # if no vaccine and very high risk:
-#     # make risk of vaccine high:
-#     for patho in alri.all_pathogens:
-#         models.p[f'base_inc_rate_ALRI_by_{patho}'] = [0.5] * len(models.p[f'base_inc_rate_ALRI_by_{patho}'])
-#     models.make_model_for_acquisition_risk()
-#
-#     # ensure no one has the relevant vaccines:
-#     df['va_pneumo_all_doses'] = False
-#     df['va_hib_all_doses'] = False
-#
-#     for pathogen in alri.all_pathogens:
-#         res = models.compute_risk_of_aquisition(
-#             pathogen=pathogen,
-#             df=df.loc[df.is_alive & (df.age_years < 5)]
-#         )
-#         assert (res > 0).all() and (res <= 1.0).all()
-#         assert not res.isna().any()
-#         assert 'float64' == res.dtype.name
-#
-#     # pneumococcal vaccine: if efficacy of vaccine is perfect, whomever has vaccine should have no risk of infection
-#     # from Strep_pneumoniae_PCV13
-#     models.p['rr_infection_strep_with_pneumococcal_vaccine'] = 0.0
-#     df['va_pneumo_all_doses'] = True
-#     assert (0.0 == models.compute_risk_of_aquisition(
-#         pathogen='Strep_pneumoniae_PCV13',
-#         df=df.loc[df.is_alive & (df.age_years < 5)])
-#             ).all()
-#
-#     # pneumococcal vaccine: if efficacy of vaccine is perfect, whomever has vaccine should have no risk of infection
-#     # from Strep_pneumoniae_PCV13
-#     models.p['rr_infection_hib_haemophilus_vaccine'] = 0.0
-#     df['va_hib_all_doses'] = True
-#     assert (0.0 == models.compute_risk_of_aquisition(
-#         pathogen='Hib',
-#         df=df.loc[df.is_alive & (df.age_years < 5)])
-#             ).all()
-#
-#     # --- determine_disease_type
-#     # set efficacy of pneumococcal vaccine to be 100% (i.e. 0 relative risk of infection)
-#     models.p['rr_infection_strep_with_pneumococcal_vaccine'] = 0.0
-#     for patho in alri.all_pathogens:
-#         for age in range(0, 100):
-#             for va_pneumo_all_doses in [True, False]:
-#                 disease_type, bacterial_coinfection = \
-#                     models.determine_disease_type_and_secondary_bacterial_coinfection(
-#                         age=age,
-#                         pathogen=patho,
-#                         va_pneumo_all_doses=va_pneumo_all_doses
-#                     )
-#
-#                 assert disease_type in alri.disease_types
-#
-#                 if patho in alri.pathogens['bacterial']:
-#                     assert pd.isnull(bacterial_coinfection)
-#                 elif patho in alri.pathogens['fungal']:
-#                     assert pd.isnull(bacterial_coinfection)
-#                 else:
-#                     # viral primary infection- may have a bacterial coinfection or may not:
-#                     assert pd.isnull(bacterial_coinfection) or bacterial_coinfection in alri.pathogens['bacterial']
-#                     # check that if has had pneumococcal vaccine they are not coinfected with `Strep_pneumoniae_PCV13`
-#                     if va_pneumo_all_doses:
-#                         assert bacterial_coinfection != 'Strep_pneumoniae_PCV13'
-#
-#     # --- complications
-#     for patho in alri.all_pathogens:
-#         for coinf in (alri.pathogens['bacterial'] + [np.nan]):
-#             for disease_type in alri.disease_types:
-#                 df.loc[person_id, [
-#                     'ri_primary_pathogen',
-#                     'ri_secondary_bacterial_pathogen',
-#                     'ri_disease_type']
-#                 ] = (
-#                     patho,
-#                     coinf,
-#                     disease_type
-#                 )
-#                 res = models.complications(person_id)
-#
-#                 assert isinstance(res, set)
-#                 assert all([c in alri.complications for c in res])
-#
-#     # --- delayed_complications
-#     for ri_complication_sepsis in [True, False]:
-#         for ri_complication_pneumothorax in [True, False]:
-#             for ri_complication_respiratory_failure in [True, False]:
-#                 for ri_complication_lung_abscess in [True, False]:
-#                     for ri_complication_empyema in [True, False]:
-#                         df.loc[person_id, [
-#                             'ri_complication_sepsis',
-#                             'ri_complication_pneumothorax',
-#                             'ri_complication_respiratory_failure',
-#                             'ri_complication_lung_abscess',
-#                             'ri_complication_empyema']
-#                         ] = (
-#                             ri_complication_sepsis,
-#                             ri_complication_pneumothorax,
-#                             ri_complication_respiratory_failure,
-#                             ri_complication_lung_abscess,
-#                             ri_complication_empyema
-#                         )
-#                         res = models.delayed_complications(person_id=person_id)
-#                         assert isinstance(res, set)
-#                         assert all([c in ['sepsis', 'respiratory_failure'] for c in res])
-#
-#     # --- symptoms_for_disease
-#     for disease_type in alri.disease_types:
-#         res = models.symptoms_for_disease(disease_type)
-#         assert isinstance(res, set)
-#         assert all([s in sim.modules['SymptomManager'].symptom_names for s in res])
-#
-#     # --- symptoms_for_complication
-#     for complication in alri.complications:
-#         res = models.symptoms_for_complication(complication)
-#         assert isinstance(res, set)
-#         assert all([s in sim.modules['SymptomManager'].symptom_names for s in res])
-#
-#     # --- death
-#     for disease_type in alri.disease_types:
-#         df.loc[person_id, [
-#             'ri_disease_type',
-#             'age_years',
-#             'ri_complication_sepsis',
-#             'ri_complication_respiratory_failure',
-#             'ri_complication_meningitis',
-#             'hv_inf',
-#             'un_clinical_acute_malnutrition',
-#             'nb_low_birth_weight_status']
-#         ] = (
-#             disease_type,
-#             0,
-#             False,
-#             False,
-#             False,
-#             True,
-#             'SAM',
-#             'low_birth_weight'
-#         )
-#         res = models.death(person_id)
-#         assert isinstance(res, bool)
 
 
 # def check_configuration_of_properties(sim):
@@ -917,7 +764,7 @@ def test_use_of_HSI(tmpdir):
     assert not pd.isnull(person['un_am_recovery_date'])
     assert pd.isnull(person['un_sam_death_date'])
 
-
+# TODO: do the HSI tests for SAM, with complication, witout complcations and respective HSIs
 
     # # Check that there is a ProgressionSevereWastingEvent scheduled for this person:
     # progression_event_tuple = [event_tuple for event_tuple in sim.find_events_for_person(person_id) if
