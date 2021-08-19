0af72b47bbd226395e27c4ba79cdefb40e3d9e14
Asif Tamuri <tamuri@gmail.com>
2021-08-19 13:16:18 +0100

    Rollback

--- src/tlo/analysis/utils.py
+++ src/tlo/analysis/utils.py
@@ -43,9 +43,6 @@ def _parse_line(line):
     try:
         parsed = literal_eval(parts[4])
     except ValueError:
-        # When playing around with the capabilities coefficient, I had a ValueError here from there not being any
-        # mapping for inf, created the mapping here
-        # parsed = eval(parts[4], {'Timestamp': pd.Timestamp, 'nan': np.nan, 'NaT': pd.NaT, 'inf': float('Inf')})
         parsed = eval(parts[4], {'Timestamp': pd.Timestamp, 'nan': np.nan, 'NaT': pd.NaT})
 
     info = {
