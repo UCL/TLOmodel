b296a7f0c177ec157eaebf7f36b5fded73537304
tdm32 <t.mangal@imperial.ac.uk>
2021-08-25 13:51:47 +0100

    edit make_plot function to include additional arguments

--- src/scripts/tb/plots_from_calibration.py
+++ src/scripts/tb/plots_from_calibration.py
@@ -5,8 +5,11 @@ The results of the batchrun were put into the 'outputs' results_folder
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+
 import numpy as np
 import pandas as pd
+import datetime
 
 from tlo.analysis.utils import (
     extract_params,
@@ -17,6 +20,8 @@ from tlo.analysis.utils import (
     load_pickled_dataframes,
     summarize,
 )
+resourcefilepath = Path("./resources")
+datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 
 outputspath = Path('./outputs/t.mangal@imperial.ac.uk')
 
@@ -113,9 +118,16 @@ params = extract_params(results_folder)
 # %% Function to make standard plot to compare model and data
 def make_plot(
     model=None,
+    model_low=None,
+    model_high=None,
+    data_name=None,
     data_mid=None,
     data_low=None,
     data_high=None,
+    xlim=None,
+    ylim=None,
+    xlab=None,
+    ylab=None,
     title_str=None
 ):
     assert model is not None
@@ -123,20 +135,39 @@ def make_plot(
 
     # Make plot
     fig, ax = plt.subplots()
-    ax.plot(model.index, model.values, '-', color='r')
+    ax.plot(model.index, model.values, '-', color='C3')
+    if (model_low is not None) and (model_high is not None):
+        ax.fill_between(model_low.index,
+                        model_low,
+                        model_high,
+                        color='C3',
+                        alpha=0.2)
 
     if data_mid is not None:
-        ax.plot(data_mid.index, data_mid.values, '-')
+        ax.plot(data_mid.index, data_mid.values, '-', color='g')
     if (data_low is not None) and (data_high is not None):
         ax.fill_between(data_low.index,
                         data_low,
                         data_high,
+                        color='g',
                         alpha=0.2)
+
+    if xlim is not None:
+        ax.set_xlim(xlim)
+
+    if ylim is not None:
+        ax.set_xlim(ylim)
+
+    if xlab is not None:
+        ax.set_xlabel(xlab)
+
+    if ylab is not None:
+        ax.set_xlabel(ylab)
+
     plt.title(title_str)
-    plt.legend(['Model', 'Data'])
+    plt.legend(['Model', data_name])
     plt.gca().set_ylim(bottom=0)
-    # plt.savefig(outputpath / (title_str.replace(" ", "_") + datestamp + ".pdf"), format='pdf')
-    # plt.show()
+    plt.savefig(outputspath / (title_str.replace(" ", "_") + datestamp + ".pdf"), format='pdf')
 
 
 # %% extract results
@@ -152,30 +183,51 @@ model_hiv_adult_prev = summarize(extract_results(results_folder,
                       )
 model_hiv_adult_prev.index = model_hiv_adult_prev.index.year
 
-# HIV - prevalence among in adults aged 15+
-fig, ax = plt.subplots()
-# model
-ax.plot(model_hiv_adult_prev.index, model_hiv_adult_prev['mean'] * 100,
-        label='TLO (mean)', color='C3')
-ax.fill_between(model_hiv_adult_prev.index,
-                model_hiv_adult_prev['lower'] * 100,
-                model_hiv_adult_prev['upper'] * 100,
-                color='C3',
-                alpha=0.2,
-                zorder=5
-                )
-# data: UNAIDS
-ax.plot(model_hiv_adult_prev.index, data_hiv_unaids['prevalence_age15_49'] * 100,
-        label='UNAIDS', color='g')
-ax.fill_between(data_hiv_unaids.index,
-                data_hiv_unaids['prevalence_age15_49_lower'] * 100,
-                data_hiv_unaids['prevalence_age15_49_upper'] * 100,
-                color='g',
-                alpha=0.2,
-                zorder=5
-                )
+model_hiv_adult_inc = summarize(extract_results(results_folder,
+                                      module="tlo.methods.hiv",
+                                      key="summary_inc_and_prev_for_adults_and_children_and_fsw",
+                                      column="hiv_adult_inc_1549",
+                                      index="date",
+                                      do_scaling=False
+                                      ),
+                      collapse_columns=True
+                      )
+model_hiv_adult_inc.index = model_hiv_adult_inc.index.year
+
+model_hiv_child_prev = summarize(extract_results(results_folder,
+                                      module="tlo.methods.hiv",
+                                      key="summary_inc_and_prev_for_adults_and_children_and_fsw",
+                                      column="hiv_prev_child",
+                                      index="date",
+                                      do_scaling=False
+                                      ),
+                      collapse_columns=True
+                      )
+model_hiv_child_prev.index = model_hiv_child_prev.index.year
+
+
+
+
+# %% make plots
+# HIV - prevalence among in adults aged 15-49
+
+make_plot(
+    title_str="HIV Prevalence in Adults Aged 15-49 (%)",
+    model=model_hiv_adult_prev['mean'] * 100,
+    model_low=model_hiv_adult_prev['lower'] * 100,
+    model_high=model_hiv_adult_prev['upper'] * 100,
+    data_name='UNAIDS',
+    data_mid=data_hiv_unaids['prevalence_age15_49'],
+    data_low=data_hiv_unaids['prevalence_age15_49_lower'],
+    data_high=data_hiv_unaids['prevalence_age15_49_upper'],
+    xlim=[2010, 2020],
+    ylim=[0, 15],
+    xlab="Year",
+    ylab="HIV prevalence (%)"
+)
+
 # data: MPHIA
-ax.plot(model_hiv_adult_prev.index[6], data_hiv_mphia_prev.loc[
+plt.plot(model_hiv_adult_prev.index[6], data_hiv_mphia_prev.loc[
     data_hiv_mphia_prev.age == "Total 15-49", "total percent hiv positive"].values[0], 'gx')
 
 # data: DHS
@@ -186,19 +238,116 @@ y_lower = abs(y_values - (data_hiv_dhs_prev.loc[
                 (data_hiv_dhs_prev.Year >= 2010), "HIV prevalence among general population 15-49 lower"]))
 y_upper = abs(y_values - (data_hiv_dhs_prev.loc[
                 (data_hiv_dhs_prev.Year >= 2010), "HIV prevalence among general population 15-49 upper"]))
-ax.errorbar(x_values, y_values,
+plt.errorbar(x_values, y_values,
              yerr=[y_lower, y_upper], fmt='o')
 
-ax.set_title("HIV Prevalence in Adults Aged 15-49 (%)")
-ax.set_xlabel("Year")
-ax.set_ylabel("HIV prevalence (%)")
-ax.set_xlim(2010, 2020)
-ax.set_ylim(0, 15)
-ax.legend()
-fig.tight_layout()
-plt.savefig(make_graph_file_name("HIV_prev_adults"))
+# handles for legend
+red_line = mlines.Line2D([], [], color='C3',
+                         markersize=15, label='TLO')
+blue_line = mlines.Line2D([], [], color='C0',
+                          markersize=15, label='UNAIDS')
+green_cross = mlines.Line2D([], [], linewidth=0, color='g', marker='x',
+                          markersize=7, label='MPHIA')
+orange_ci = mlines.Line2D([], [], color='C1', marker='.',
+                          markersize=15, label='DHS')
+plt.legend(handles=[red_line, blue_line, green_cross, orange_ci])
+
 plt.show()
 
 
+# ---------------------------------------------------------------------- #
 
+# HIV Incidence 15-49 per 100 population
+fig, ax = plt.subplots()
+# model
+ax.plot(model_hiv_adult_inc.index, model_hiv_adult_inc['mean'] * 100,
+        label='TLO (mean)', color='C3')
+ax.fill_between(model_hiv_adult_inc.index,
+                model_hiv_adult_inc['lower'] * 100,
+                model_hiv_adult_inc['upper'] * 100,
+                color='C3',
+                alpha=0.2,
+                zorder=5
+                )
+# data: UNAIDS
+ax.plot(model_hiv_adult_prev.index, data_hiv_unaids['incidence_per_1000'] / 10,
+        label='UNAIDS', color='g')
+ax.fill_between(data_hiv_unaids.index,
+                data_hiv_unaids['incidence_per_1000_lower'] / 10,
+                data_hiv_unaids['incidence_per_1000_upper'] / 10,
+                color='g',
+                alpha=0.2,
+                zorder=5
+                )
+# MPHIA
+plt.errorbar(model_hiv_adult_inc.index[6], data_hiv_mphia_inc_estimate,
+             yerr=[[data_hiv_mphia_inc_yerr[0]], [data_hiv_mphia_inc_yerr[1]]], fmt='o')
+
+ax.set_title("HIV Incidence in Adults Aged 15-49 per 100 population")
+ax.set_xlabel("Year")
+ax.set_ylabel("HIV incidence per 100 population")
+ax.set_xlim(2010, 2020)
+ax.set_ylim(0, 6)
+
+# handles for legend
+red_line = mlines.Line2D([], [], color='C3',
+                         markersize=15, label='TLO')
+blue_line = mlines.Line2D([], [], color='C0',
+                          markersize=15, label='UNAIDS')
+orange_ci = mlines.Line2D([], [], color='C1', marker='.',
+                          markersize=15, label='MPHIA')
+plt.legend(handles=[red_line, blue_line, orange_ci])
+
+fig.tight_layout()
+plt.savefig(make_graph_file_name("HIV_inc_adults"))
+plt.show()
+
+
+# ---------------------------------------------------------------------- #
+
+# HIV Prevalence Children
+# HIV Incidence 15-49
+fig, ax = plt.subplots()
+# model
+ax.plot(model_hiv_child_prev.index, model_hiv_child_prev['mean'] * 100,
+        label='TLO (mean)', color='C3')
+ax.fill_between(model_hiv_child_prev.index,
+                model_hiv_child_prev['lower'] * 100,
+                model_hiv_child_prev['upper'] * 100,
+                color='C3',
+                alpha=0.2,
+                zorder=5
+                )
+# data: UNAIDS
+ax.plot(data_hiv_aidsinfo.index, data_hiv_aidsinfo['prevalence_0_14'] * 100,
+        label='UNAIDS', color='g')
+ax.fill_between(data_hiv_aidsinfo.index,
+                data_hiv_aidsinfo['prevalence_0_14_lower'] * 100,
+                data_hiv_aidsinfo['prevalence_0_14_upper'] * 100,
+                color='g',
+                alpha=0.2,
+                zorder=5
+                )
+# MPHIA
+plt.plot(model_hiv_child_prev.index[6], data_hiv_mphia_prev.loc[
+    data_hiv_mphia_prev.age == "Total 0-14", "total percent hiv positive"].values[0], 'gx')
+
+ax.set_title("HIV Prevalence in Children 0-14 (%)")
+ax.set_xlabel("Year")
+ax.set_ylabel("HIV prevalence (%)")
+ax.set_xlim(2010, 2020)
+ax.set_ylim(0, 5)
+
+# handles for legend
+red_line = mlines.Line2D([], [], color='C3',
+                         markersize=15, label='TLO')
+blue_line = mlines.Line2D([], [], color='C0',
+                          markersize=15, label='UNAIDS')
+green_cross = mlines.Line2D([], [], linewidth=0, color='g', marker='x',
+                          markersize=7, label='MPHIA')
+plt.legend(handles=[red_line, blue_line, green_cross])
+
+fig.tight_layout()
+plt.savefig(make_graph_file_name("HIV_prev_children"))
+plt.show()
 
