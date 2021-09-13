a1008d3b27782954b0ca0112010471f8e3c6f657
Britta Jewell <britta.jewell@gmail.com>
2021-09-13 12:32:26 +0100

    adding possibility to avert death through treatment for conditions

--- src/tlo/methods/cardio_metabolic_disorders.py
+++ src/tlo/methods/cardio_metabolic_disorders.py
@@ -790,8 +790,15 @@ class CardioMetabolicDisorders_MainPollingEvent(RegularEvent, PopulationScopeEve
                     idx_selected_to_die = eligible_population[eligible_population].index
 
                 for person_id in idx_selected_to_die:
-                    schedule_death_to_occur_before_next_poll(person_id, condition,
-                                                             m.parameters['interval_between_polls'])
+                    if df.at[person_id, f'nc_{condition}_on_medication']:
+                        treatmentworks = self.module.rng.rand() < self.module.parameters[
+                            f'{condition}_hsi'].pr_treatment_works
+                        if not treatmentworks:
+                            schedule_death_to_occur_before_next_poll(person_id, condition,
+                                                                     m.parameters['interval_between_polls'])
+                    else:
+                        schedule_death_to_occur_before_next_poll(person_id, condition,
+                                                                 m.parameters['interval_between_polls'])
 
         # add the new incidence numbers to tracker
         self.module.df_incidence_tracker = self.module.df_incidence_tracker.add(current_incidence_df)
