e0283b8c85edba2ebfc5acd062e04b73d4099ca1
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2021-09-20 14:44:29 +0100

    removed most of the analysis rti scripts from this PR

--- src/scripts/rti/analysis_rti_single_injury_model_vs_multiple_injury_analyse_logs.py
+++
@@ -1,8 +0,0 @@
-from scripts.rti.rti_create_graphs import create_rti_graphs
-
-create_rti_graphs('outputs/single_injury_model_vs_multiple_injury/single_injury',
-                  'C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/SingleVsMultipleInjury/Single_Injury',
-                  'Single_Injury')
-create_rti_graphs('outputs/single_injury_model_vs_multiple_injury/multiple_injury',
-                  'C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/SingleVsMultipleInjury/Multiple_Injury',
-                  'Multiple_Injury')
