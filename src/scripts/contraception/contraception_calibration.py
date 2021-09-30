bc5726f6bc19b5046b3a2c55708aa8597c14cbd1
Tim Hallett <timothy.hallett@imperial.ac.uk>
2021-09-30 11:20:48 +0100

    script to check assumptions and the match to wpp

--- src/scripts/contraception/contraception_calibration.py
+++ src/scripts/contraception/contraception_calibration.py
@@ -1,4 +1,3 @@
-import datetime
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -6,6 +5,7 @@ import numpy as np
 import pandas as pd
 
 from tlo import Date, Simulation
+from tlo.analysis.utils import make_calendar_period_lookup
 from tlo.methods import (
     care_of_women_during_pregnancy,
     contraception,
@@ -20,7 +20,7 @@ from tlo.methods import (
 )
 from tlo.methods.hiv import DummyHivModule
 
-# Create simulation object
+#%% Create dummy simulation object
 resourcefilepath = Path('resources')
 start_date = Date(2010, 1, 1)
 sim = Simulation(start_date=start_date, seed=0)
@@ -45,82 +45,131 @@ sim.register(
 )
 sim.make_initial_population(n=50000)
 
-# %% Examine trend in contraceptive use
+# %% Simulate the changes in contraceptive use 'manually'
 
 # Run the ContraceptivePoll (with the option `run_do_pregnancy`=False to prevent pregnancies)
 states = sim.modules['Contraception'].all_contraception_states
 poll = contraception.ContraceptionPoll(module=sim.modules['Contraception'], run_do_pregnancy=False)
 age_update_event = demography.AgeUpdateEvent(sim.modules['Demography'], sim.modules['Demography'].AGE_RANGE_LOOKUP)
-results1549 = pd.DataFrame(index=range(100), columns=states)
-results1519 = pd.DataFrame(index=range(100), columns=states)
-results3034 = pd.DataFrame(index=range(100), columns=states)
-results_by_age = dict()
 
+usage_by_age = dict()
 
-for i in range(12*20):
-    results1549.loc[i] = sim.population.props.loc[(sim.population.props.sex == 'F') & (
-        sim.population.props.age_years.between(15, 49)), 'co_contraception'].value_counts()
-    results1519.loc[i] = sim.population.props.loc[(sim.population.props.sex == 'F') & (
-        sim.population.props.age_years.between(15, 19)), 'co_contraception'].value_counts()
-    results3034.loc[i] = sim.population.props.loc[(sim.population.props.sex == 'F') & (
-        sim.population.props.age_years.between(30, 34)), 'co_contraception'].value_counts()
+for date in pd.date_range(sim.date, Date(2029, 12, 31), freq=pd.DateOffset(months=1)):
 
-    results_by_age[sim.date] = sim.population.props.loc[(sim.population.props.sex == 'F') & (
+    sim.date = date
+    age_update_event.apply(sim.population)
+
+    usage_by_age[date] = sim.population.props.loc[(sim.population.props.sex == 'F') & (
         sim.population.props.age_years.between(15, 49))].groupby(by=['co_contraception', 'age_range']).size()
 
-    sim.date += pd.DateOffset(months=1)
-    age_update_event.apply(sim.population)
     poll.apply(sim.population)
 
 
-def plot(df, title=''):
-    spacing = (np.arange(len(df)) % 24) == 0
-    df.loc[spacing].plot.bar(stacked=True)
-    plt.title(title)
-    plt.xlabel('Month')
-    plt.ylabel('Number')
-    plt.show()
-
-plot(results1549, 'All Ages')
-plot(results1519, '15-19')
-plot(results3034, '30-34')
-
-
-# %% Compare average rates of pregnancy with rates of birth in WPP
-# todo this!!! and this use this to get correct number of births in 2010 (change fertility levels) and in future years (change rates of contraceptive use)
+# %% Declare useful formatting functions and load WPP data
 AGE_RANGE_CATEGORIES, AGE_RANGE_LOOKUP = sim.modules['Demography'].AGE_RANGE_CATEGORIES, sim.modules[
     'Demography'].AGE_RANGE_LOOKUP
-p = sim.modules['Contraception'].processed_params
-use = p['initial_method_use']
 
-# Get the "intrinsic fertility rates" by contraception status
+_, period_lookup = make_calendar_period_lookup()
+
+def format_usage_results(df):
+    return df.unstack().T.apply(lambda row: row/row.sum(), axis=1).dropna()
+
+# %% Describe patterns of contraceptive usage over time
+
+def plot(df, title=''):
+    spacing = (np.arange(len(df)) % 24) == 0
+
+    fig, ax = plt.subplots()
+    df.loc[spacing].apply(lambda row: row/row.sum(), axis=1).plot.bar(stacked=True, ax=ax, legend=False)
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('Proportion')
+    fig.legend(loc=7)
+
+    fig.tight_layout()
+    fig.subplots_adjust(right=0.65)
+    plt.show()
+
+for age_grp in ['15-19', '20-24', '25-29', '30-34', '35-39', '40-44', '45-49']:
+    plot(pd.DataFrame.from_dict({d: usage_by_age[d].unstack()[age_grp] for d in usage_by_age}, orient='index'), age_grp)
+
+
+#%% Check that initial usage by age matches the input assumption (initial_method_use)
+actual_init_use = format_usage_results(usage_by_age[Date(2010, 1, 1)])
+
+assumption_init_use = sim.modules['Contraception'].processed_params['initial_method_use'].copy()
+assumption_init_use.index = assumption_init_use.index.map(AGE_RANGE_LOOKUP)
+assumption_init_use = assumption_init_use.groupby(by=assumption_init_use.index).mean()
+
+fig, ax = plt.subplots(2, 4)
+ax = ax.reshape(-1)
+for i, agegrp in enumerate(actual_init_use.index):
+    ax[i].plot(actual_init_use.loc[agegrp].index, actual_init_use.loc[agegrp].values, '--', label='actual')
+    ax[i].plot(assumption_init_use.loc[agegrp].index, actual_init_use.loc[agegrp].values, '-', label='assumption')
+    ax[i].set_title(f"{agegrp}")
+plt.show()
+
+
+# %% Get the "intrinsic fertility" rates by contraception status assumed in the TLO model
 fert = pd.DataFrame(index=range(15, 50), columns=states)
 fert['not_using'] = 1.0 - np.exp(-p['p_pregnancy_no_contraception_per_month'] * 12)
 fert.loc[:, fert.columns.drop('not_using')] = 1.0 - np.exp(-p['p_pregnancy_with_contraception_per_month'] * 12)
+fert.index = fert.index.map(AGE_RANGE_LOOKUP)
+fert = fert.groupby(by=fert.index).mean()
 
-# Compute average fertility, grouped by age, for specific comparison dates
-dates = [
-    datetime.date(2010, 1, 1),
-    datetime.date(2020, 1, 1),
-    datetime.date(2030, 1, 1)
-]
 
-for date in dates:
+# %% Compare Age-Specific Fertility Rates to 'approx average fertility' of women in the TLO model output in 2010
+# (given patterns of contraceptive use and intrinsic fertility and the assumption of the proportion of pregnancy that
+# result in live births).
 
-    average_fert = (use * fert).sum(axis=1)
-    agegrps = average_fert.index.map(AGE_RANGE_LOOKUP)
-    average_fert_5y = average_fert.groupby(by=agegrps).mean().rename_axis('Age_Grp')
+# Load WPP data on live births (age-specific fertility rates)
+wpp = pd.read_csv(resourcefilepath / 'demography' / 'ResourceFile_ASFR_WPP.csv')
 
-    # Compare to WPP ASFR (=live births, so expect to be a bit higher than model due to losses and mortality)
-    wpp = pd.read_csv(resourcefilepath / 'demography' / 'ResourceFile_ASFR_WPP.csv')
-    wpp_fert = \
-    wpp.loc[(wpp.Period == '2010-2014') & (wpp.Variant == 'WPP_Estimates'), ['Age_Grp', 'asfr']].set_index('Age_Grp')[
-        'asfr']
+# Make assumption about the proportion of pregnancy that lead to live births (in the TLO model)
+prop_preg_to_live_birth = 0.67
 
-    f = pd.concat({
-        'wpp': wpp_fert,
-        'model': average_fert_5y},
-        axis=1
-    )
+fig, ax = plt.subplots()
+wpp_fert = wpp.loc[(wpp.Period == period_lookup[2010]) & (wpp.Variant == 'WPP_Estimates'), ['Age_Grp', 'asfr']].set_index('Age_Grp')
+model_fert = (format_usage_results(usage_by_age[Date(2010, 1, 1)]) * fert).sum(axis=1)
+ax.plot(model_fert.index, model_fert.values, 'b-', label='Model Pregnancy')
+ax.plot(model_fert.index, model_fert.values * prop_preg_to_live_birth, 'b--', label='Approx Model Live Births')
+ax.plot(wpp_fert.index, wpp_fert.values, 'r-', label='WPP Live Births')
+ax.set_title(f"{2010}")
+ax.set_ylim([0, 0.60])
+plt.setp(ax.get_xticklabels(), rotation=90, ha='right')
+plt.legend()
+plt.tight_layout()
+plt.show()
 
-    ratio = average_fert_5y / wpp_fert
+# %% Examine trends in fertility over time.
+
+# Compute the 'approx average fertility' of women in the TLO model over time.
+av_fert_by_period = dict()
+for date in usage_by_age:
+    usage = format_usage_results(usage_by_age[date])
+    av_fert_by_period[period_lookup[date.year]] = (usage * fert).sum(axis=1)
+av_fert_by_period = pd.DataFrame(av_fert_by_period)
+
+# Produce plot, normalising to the period 2010-2014
+fig, ax = plt.subplots(2, 4)
+ax = ax.reshape(-1)
+for i, agegrp in enumerate(av_fert_by_period.index):
+
+    # Get WPP fertility:
+    wpp_fert = wpp.loc[wpp.Period.isin(av_fert_by_period.columns) & (wpp.Variant.isin(['WPP_Estimates', 'WPP_Medium variant'])) & (wpp.Age_Grp == agegrp), ['Period', 'asfr']].set_index('Period')
+
+    # Get average fertility in the model:
+    model_fert = av_fert_by_period.loc[agegrp]
+
+    # Comparative plot:
+    ax[i].plot(model_fert.index, model_fert.values / model_fert.values[0] , 'b-', label='Trend in Model Pregnancy')
+    ax[i].plot(wpp_fert.index, wpp_fert.values / wpp_fert.values[0], 'r-', label='Trend in WPP Live Births')
+
+    ax[i].set_title(f"{agegrp}")
+    ax[i].set_ylim([0.2, 5.0])
+    ax[i].set_yscale('log')
+    plt.setp(ax[i].get_xticklabels(), rotation=90, ha='right')
+
+ax[-1].set_axis_off()
+plt.tight_layout()
+plt.show()
