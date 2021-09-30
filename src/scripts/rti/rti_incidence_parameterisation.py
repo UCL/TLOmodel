5ece07cefe1b31fbfe7cfd29aa117728c0abebc0
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2021-09-30 09:50:24 +0100

    set up incidence parameterisation script and created analysis script

--- src/scripts/rti/rti_incidence_parameterisation.py
+++ src/scripts/rti/rti_incidence_parameterisation.py
@@ -55,8 +55,8 @@ class TestScenario(BaseScenario):
 # are given out and once where we allow multiple injuries
 
     def draw_parameters(self, draw_number, rng):
-        base_rate_max = 0.0072620235369823 - 0.005
-        base_rate_min = 0.0072620235369823 + 0.005
+        base_rate_max = 0.0072620235369823 - 0.002
+        base_rate_min = 0.0072620235369823 + 0.002
         parameter_range = np.linspace(base_rate_min, base_rate_max, num=self.number_of_samples_in_parameter_range)
         return {
             'RTI': {'base_rate_injrti': parameter_range[draw_number]},
---
+++ src/scripts/rti/rti_incidence_parameterisation_plots.py
@@ -0,0 +1,88 @@
+"""This file uses the results of the batch file to make some summary statistics.
+The results of the bachrun were put into the 'outputs' results_folder
+"""
+
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from tlo.analysis.utils import (
+    extract_params,
+    extract_results,
+    get_scenario_info,
+    get_scenario_outputs,
+    load_pickled_dataframes,
+    summarize,
+)
+
+outputspath = Path('./outputs/')
+
+# %% Analyse results of runs when doing a sweep of a single parameter:
+
+# 0) Find results_folder associated with a given batch_file and get most recent
+results_folder = get_scenario_outputs('rti_incidence_parameterisation.py', outputspath)[-1]
+
+# look at one log (so can decide what to extract)
+log = load_pickled_dataframes(results_folder)
+
+# get basic information about the results
+info = get_scenario_info(results_folder)
+
+# 1) Extract the parameters that have varied over the set of simulations
+params = extract_params(results_folder)
+
+# 2) Extract a specific log series for all runs:
+extracted = extract_results(results_folder,
+                            module="tlo.methods.rti",
+                            key="summary_1m",
+                            column="incidence of rti per 100,000",
+                            index="date")
+
+# 3) Get summary of the results for that log-element
+incidence = summarize(extracted)
+# If only interested in the means
+incidence_onlymeans = summarize(extracted, only_mean=True)
+# get per parameter summaries
+mean_incidence_overall = incidence.mean()
+# get upper and lower estimates
+mean_incidence_lower = mean_incidence_overall.loc[:, "lower"]
+mean_incidence_upper = mean_incidence_overall.loc[:, "upper"]
+lower_upper = np.array(list(zip(
+    mean_incidence_lower.to_list(),
+    mean_incidence_upper.to_list()
+))).transpose()
+# name of parmaeter that varies
+param_name = 'RTI:base_rate_injrti'
+# find the values that fall within our accepted range of incidence based on results of the GBD study
+gbd_data = pd.read_csv('resources/gbd/ResourceFile_RTI_Deaths_and_Incidence.csv')
+incidence_data = gbd_data.loc[gbd_data['measure'] == 'Incidence']
+incidence_data = incidence_data.loc[incidence_data['metric'] == 'Rate']
+incidence_data = incidence_data.loc[incidence_data['year'] > 2009]
+expected_incidence_upper = incidence_data['upper'].mean()
+expected_incidence_lower = incidence_data['lower'].mean()
+per_param_average_incidence = mean_incidence_overall[:, 'mean'].values
+yerr = abs(lower_upper - per_param_average_incidence)
+in_accepted_range = np.where((per_param_average_incidence > expected_incidence_lower) &
+                             (per_param_average_incidence < expected_incidence_upper))
+xvals = range(info['number_of_draws'])
+colors = ['lightsteelblue' if i not in in_accepted_range[0] else 'lightsalmon' for i in xvals]
+xlabels = [
+    round(params.loc[(params.module_param == param_name)][['value']].loc[draw].value, 3)
+    for draw in range(info['number_of_draws'])
+]
+fig, ax = plt.subplots()
+ax.bar(
+    x=xvals,
+    height=mean_incidence_overall[:, 'mean'].values,
+    yerr=yerr,
+    color=colors
+)
+ax.set_xticks(xvals)
+ax.set_xticklabels(xlabels)
+plt.xlabel(param_name)
+plt.ylabel('Incidence of RTI per 100,000')
+plt.title('Calibration of the base rate of rti injury which determines\n the overall incidence of RTI')
+plt.savefig(f"C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/Calibration/base_rate_rti_{min(xlabels)}_"
+            f"{max(xlabels)}.png")
