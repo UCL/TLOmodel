29ffd2e794c342d9ae92f47c8d7cda5b57c9f511
Tim Hallett <timothy.hallett@imperial.ac.uk>
2021-10-01 16:00:50 +0100

    fixed dependency in malaria

--- src/tlo/methods/malaria.py
+++ src/tlo/methods/malaria.py
@@ -40,6 +40,8 @@ class Malaria(Module):
 
     OPTIONAL_INIT_DEPENDENCIES = {'HealthBurden'}
 
+    ADDITIONAL_DEPENDENCIES = {'DxAlgorithmAdult'}
+
     METADATA = {
         Metadata.DISEASE_MODULE,
         Metadata.USES_HEALTHSYSTEM,
