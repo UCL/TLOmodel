2a52b717ca7ac5fc7c327be6044c7caee31ef273
andrew-phillips-1 <39617310+andrew-phillips-1@users.noreply.github.com>
2021-10-01 18:35:46 +0100

    .

--- src/scripts/epilepsy_analyses/analysis_epilepsy.py
+++ src/scripts/epilepsy_analyses/analysis_epilepsy.py
@@ -26,7 +26,7 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 resourcefilepath = Path("./resources")
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2021,  1, 1)
+end_date = Date(2014,  1, 1)
 popsize = 1000
 
 # Establish the simulation object
