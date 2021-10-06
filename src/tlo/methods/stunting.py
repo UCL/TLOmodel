5646796da338dfe8994067103fe7fc943266bfef
Matt Graham <matthew.m.graham@gmail.com>
2021-10-06 14:56:09 +0100

    Add placeholder stunting and wasting modules

---
+++ src/tlo/methods/stunting.py
@@ -0,0 +1,42 @@
+"""Placeholder for childhood stunting module."""
+
+from tlo import Module, Property, Types, logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class Stunting(Module):
+    """Placeholder for childhood stunting module.
+
+    Provides dummy values for properties required by other modules.
+    """
+
+    INIT_DEPENDENCIES = {'Demography'}
+
+    PROPERTIES = {
+        'un_HAZ_category':
+        Property(
+            Types.CATEGORICAL,
+            'temporary property',
+            categories=['HAZ<-3', '-3<=HAZ<-2', 'HAZ>=-2']
+        ),
+    }
+
+    def __init__(self, name=None):
+        super().__init__(name)
+
+    def read_parameters(self, data_folder):
+        pass
+
+    def initialise_population(self, population):
+        df = population.props
+        df.loc[df.is_alive, 'un_HAZ_category'] = 'HAZ>=-2'
+
+    def initialise_simulation(self, sim):
+        pass
+
+    def on_birth(self, mother, child):
+        df = self.sim.population.props
+        df.at[child, 'un_HAZ_category'] = 'HAZ>=-2'
---
+++ src/tlo/methods/wasting.py
@@ -0,0 +1,40 @@
+"""Placeholder for childhood wasting module."""
+
+from tlo import Module, Property, Types, logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class Wasting(Module):
+    """Placeholder for childhood wasting module.
+
+    Provides dummy values for properties required by other modules.
+    """
+
+    INIT_DEPENDENCIES = {'Demography'}
+
+    PROPERTIES = {
+        'un_clinical_acute_malnutrition':
+        Property(
+            Types.CATEGORICAL, 'temporary property', categories=['MAM', 'SAM', 'well']
+        ),
+    }
+
+    def __init__(self, name=None):
+        super().__init__(name)
+
+    def read_parameters(self, data_folder):
+        pass
+
+    def initialise_population(self, population):
+        df = population.props
+        df.loc[df.is_alive, 'un_clinical_acute_malnutrition'] = 'well'
+
+    def initialise_simulation(self, sim):
+        pass
+
+    def on_birth(self, mother, child):
+        df = self.sim.population.props
+        df.at[child, 'un_clinical_acute_malnutrition'] = 'well'
