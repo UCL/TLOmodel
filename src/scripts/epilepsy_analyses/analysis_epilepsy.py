7365df86f24ac2fa7755201209ce9253d814ddb9
andrew-phillips-1 <39617310+andrew-phillips-1@users.noreply.github.com>
2021-10-11 18:25:27 +0100

    .

--- src/scripts/epilepsy_analyses/analysis_epilepsy.py
+++ src/scripts/epilepsy_analyses/analysis_epilepsy.py
@@ -63,19 +63,11 @@ sim.simulate(end_date=end_date)
 # %% read the results
 output = parse_log_file(sim.log_filepath)
 
-prop_seiz_stat_0 = pd.Series(
- output['tlo.methods.epilepsy']['epilepsy_logging']['prop_seiz_stat_0'].values,
-    index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
-prop_seiz_stat_0.plot()
-plt.title('Proportion of people with epilepsy seizure status 0')
-plt.ylim(0.95, 1)
-plt.show()
-
 prop_seiz_stat_1 = pd.Series(
  output['tlo.methods.epilepsy']['epilepsy_logging']['prop_seiz_stat_1'].values,
     index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
 prop_seiz_stat_1.plot()
-plt.title('Proportion of people with epilepsy seizure status 1')
+plt.title('Proportion of people with epilepsy but no current seizures')
 plt.ylim(0, 0.02)
 plt.show()
 
@@ -83,7 +75,7 @@ prop_seiz_stat_2 = pd.Series(
  output['tlo.methods.epilepsy']['epilepsy_logging']['prop_seiz_stat_2'].values,
     index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
 prop_seiz_stat_2.plot()
-plt.title('Proportion of people with epilepsy seizure status 2')
+plt.title('Proportion of people with infrequent epilepsy seizures')
 plt.ylim(0, 0.02)
 plt.show()
 
@@ -91,7 +83,71 @@ prop_seiz_stat_3 = pd.Series(
  output['tlo.methods.epilepsy']['epilepsy_logging']['prop_seiz_stat_3'].values,
     index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
 prop_seiz_stat_3.plot()
-plt.title('Proportion of people with epilepsy seizure status 3')
+plt.title('Proportion of people with frequent epilepsy seizures')
 plt.ylim(0, 0.005)
+plt.show()
+
+n_seiz_stat_1_3 = pd.Series(
+ output['tlo.methods.epilepsy']['epilepsy_logging']['n_seiz_stat_1_3'].values,
+    index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
+n_seiz_stat_1_3.plot()
+plt.title('Number with epilepsy (past or current)')
+plt.ylim(0, 3000)
+plt.show()
+
+n_seiz_stat_2_3 = pd.Series(
+ output['tlo.methods.epilepsy']['epilepsy_logging']['n_seiz_stat_2_3'].values,
+    index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
+n_seiz_stat_2_3.plot()
+plt.title('Number with epilepsy (infrequent or frequent seizures)')
+plt.ylim(0, 3000)
+plt.show()
+
+prop_antiepilep_seiz_stat_1 = pd.Series(
+ output['tlo.methods.epilepsy']['epilepsy_logging']['prop_antiepilep_seiz_stat_1'].values,
+    index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
+prop_antiepilep_seiz_stat_1.plot()
+plt.title('Proportion on antiepileptics amongst people with epilepsy but no current seizures')
+plt.ylim(0, 1)
+plt.show()
+
+prop_antiepilep_seiz_stat_2 = pd.Series(
+ output['tlo.methods.epilepsy']['epilepsy_logging']['prop_antiepilep_seiz_stat_2'].values,
+    index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
+prop_antiepilep_seiz_stat_2.plot()
+plt.title('Proportion on antiepileptics amongst people with infrequent epilepsy seizures')
+plt.ylim(0, 1)
+plt.show()
+
+prop_antiepilep_seiz_stat_3 = pd.Series(
+ output['tlo.methods.epilepsy']['epilepsy_logging']['prop_antiepilep_seiz_stat_3'].values,
+    index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
+prop_antiepilep_seiz_stat_3.plot()
+plt.title('Proportion on antiepileptics amongst people with frequent epilepsy seizures')
+plt.ylim(0, 1)
+plt.show()
+
+n_epi_death = pd.Series(
+ output['tlo.methods.epilepsy']['epilepsy_logging']['n_epi_death'].values,
+    index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
+n_epi_death.plot()
+plt.title('Number of deaths from epilepsy')
+plt.ylim(0, 50)
+plt.show()
+
+n_antiep = pd.Series(
+ output['tlo.methods.epilepsy']['epilepsy_logging']['n_antiep'].values,
+    index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
+n_antiep.plot()
+plt.title('Number of people on antiepileptics')
+plt.ylim(0, 5000)
+plt.show()
+
+epi_death_rate = pd.Series(
+ output['tlo.methods.epilepsy']['epilepsy_logging']['epi_death_rate'].values,
+    index=output['tlo.methods.epilepsy']['epilepsy_logging']['date'])
+epi_death_rate.plot()
+plt.title('Rate of epilepsy death in people with seizures')
+plt.ylim(0, 1)
 plt.show()
 
