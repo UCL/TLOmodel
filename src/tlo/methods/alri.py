7f4cceb80b2fffc4b23ba4f3191f2a673223b46b
Matt Graham <matthew.m.graham@gmail.com>
2021-11-19 16:41:25 +0000

    Sort any sets iterated over to ensure determnism

--- src/tlo/methods/alri.py
+++ src/tlo/methods/alri.py
@@ -131,7 +131,7 @@ class Alri(Module):
     }
 
     # Make set of all pathogens combined:
-    all_pathogens = set(chain.from_iterable(pathogens.values()))
+    all_pathogens = sorted(set(chain.from_iterable(pathogens.values())))
 
     # Declare Causes of Death
     CAUSES_OF_DEATH = {
@@ -146,20 +146,19 @@ class Alri(Module):
     }
 
     # Declare the disease types:
-    disease_types = {
-        'pneumonia', 'bronchiolitis', 'other_alri'
-    }
+    disease_types = sorted({'pneumonia', 'bronchiolitis', 'other_alri'})
 
     # Declare the Alri complications:
-    complications = {'pneumothorax',
-                     'pleural_effusion',
-                     'empyema',
-                     'lung_abscess',
-                     'sepsis',
-                     'meningitis',
-                     'respiratory_failure',
-                     'hypoxia'  # <-- Low implies Sp02<93%'
-                     }
+    complications = sorted({
+        'pneumothorax',
+        'pleural_effusion',
+        'empyema',
+        'lung_abscess',
+        'sepsis',
+        'meningitis',
+        'respiratory_failure',
+        'hypoxia'  # <-- Low implies Sp02<93%'
+    })
 
     PARAMETERS = {
         # Incidence rate by pathogens  -----
@@ -669,7 +668,7 @@ class Alri(Module):
         # ---- The underlying Alri condition ----
         'ri_disease_type':
             Property(Types.CATEGORICAL, 'If infected, what disease type is the person currently suffering from.',
-                     categories=list(disease_types)
+                     categories=disease_types
                      ),
 
         # ---- Treatment Status ----
@@ -748,7 +747,7 @@ class Alri(Module):
             'respiratory_distress', 'danger_signs'
         }
 
-        for symptom_name in all_symptoms:
+        for symptom_name in sorted(all_symptoms):
             if symptom_name not in self.sim.modules['SymptomManager'].generic_symptoms:
                 self.sim.modules['SymptomManager'].register_symptom(
                     Symptom(name=symptom_name)
@@ -1004,7 +1003,7 @@ class Alri(Module):
     def impose_symptoms_for_complication(self, complication, person_id):
         """Impose symptoms for a complication."""
         symptoms = self.models.symptoms_for_complication(complication=complication)
-        for symptom in symptoms:
+        for symptom in sorted(symptoms):
             self.sim.modules['SymptomManager'].change_symptom(
                 person_id=person_id,
                 symptom_string=symptom,
@@ -1498,7 +1497,7 @@ class AlriIncidentCase(Event, IndividualScopeEventMixin):
         m = self.module
         models = m.models
 
-        for symptom in models.symptoms_for_disease(disease_type=disease_type):
+        for symptom in sorted(models.symptoms_for_disease(disease_type=disease_type)):
             m.sim.modules['SymptomManager'].change_symptom(
                 person_id=person_id,
                 symptom_string=symptom,
@@ -1521,7 +1520,7 @@ class AlriIncidentCase(Event, IndividualScopeEventMixin):
         # Consider delayed-onset of complications and schedule events accordingly
         date_of_onset_delayed_complications = random_date(self.sim.date, date_of_outcome, m.rng)
         delayed_complications = models.delayed_complications(person_id=person_id)
-        for delayed_complication in delayed_complications:
+        for delayed_complication in sorted(delayed_complications):
             self.sim.schedule_event(
                 AlriDelayedOnsetComplication(person_id=person_id,
                                              complication=delayed_complication,
@@ -1763,8 +1762,7 @@ class Tracker():
         # Check and store parameters
         self.pathogens = pathogens
         self.age_grps_lookup = age_grps
-        self.unique_age_grps = list(set(self.age_grps_lookup.values()))
-        self.unique_age_grps.sort()
+        self.unique_age_grps = sorted(set(self.age_grps_lookup.values()))
 
         # Initialise Tracker
         self.tracker = None
