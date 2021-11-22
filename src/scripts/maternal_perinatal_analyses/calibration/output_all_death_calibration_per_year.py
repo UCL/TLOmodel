cfe10b3a8ecf33b62e377f237d31b76236c24fe4
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2021-11-22 13:20:15 +0000

    parameter update

--- src/scripts/maternal_perinatal_analyses/calibration/output_all_death_calibration_per_year.py
+++ src/scripts/maternal_perinatal_analyses/calibration/output_all_death_calibration_per_year.py
@@ -14,7 +14,7 @@ scenario_filename = 'standard_mph_calibration.py'  # <-- update this to look at 
 
 # %% Declare usual paths:
 outputspath = Path('./outputs/sejjj49@ucl.ac.uk/')
-graph_location = 'output_graphs_30k10runs_standard_mph_calibration-2021-11-16T172838Z/death'
+graph_location = 'output_graphs_30k_standard_mph_calibration-2021-11-18T095751Z/death'
 rfp = Path('./resources')
 
 # Find results folder (most recent run generated using that scenario_filename)
@@ -146,7 +146,7 @@ def simple_line_chart(model_rate, target_rate, x_title, y_title, title, file_nam
     plt.ylabel(y_title)
     plt.title(title)
     plt.legend()
-    ##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/{file_name}.png')
+    plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/{file_name}.png')
     plt.show()
 
 
@@ -158,7 +158,7 @@ def simple_line_chart_two_targets(model_rate, target_rate_one, target_rate_two, 
     plt.ylabel(y_title)
     plt.title(title)
     plt.legend()
-    ##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/{file_name}.png')
+    plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/{file_name}.png')
     plt.show()
 
 
@@ -171,7 +171,7 @@ def simple_bar_chart(model_rates, x_title, y_title, title, file_name):
     plt.ylabel(y_title)
     plt.title(title)
     plt.legend()
-    ##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/{file_name}.png')
+    plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/{file_name}.png')
     plt.show()
 
 def line_graph_with_ci_and_target_rate(mean_list, lq_list, uq_list, target_rate, x_label, y_label, title, file_name):
@@ -183,7 +183,7 @@ def line_graph_with_ci_and_target_rate(mean_list, lq_list, uq_list, target_rate,
     plt.ylabel(y_label)
     plt.title(title)
     plt.legend()
-    ##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/{file_name}.png')
+    plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/{file_name}.png')
     plt.show()
 
 
@@ -251,7 +251,7 @@ plt.xlabel('Year')
 plt.ylabel("Deaths per 100,000 live births")
 plt.title('Maternal Mortality Ratio per Year')
 plt.legend()
-##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/mmr.png')
+plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/mmr.png')
 plt.show()
 
 # ==============================================  DEATHS... ======================================================
@@ -294,7 +294,7 @@ plt.ylabel('Total Deaths Maternal Deaths (scaled)')
 plt.title('Yearly Modelled Maternal Deaths Compared to GBD')
 plt.xticks(ind + width / 2, (years))
 plt.legend(loc='best')
-##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/deaths_gbd_comparison.png')
+plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/deaths_gbd_comparison.png')
 plt.show()
 
 # do WHO estiamte also
@@ -389,7 +389,7 @@ def pie_prop_cause_of_death(values, years, labels, title):
     plt.legend(labels, loc='center left', bbox_to_anchor=(1, 0.5))
     # Equal aspect ratio ensures that pie is drawn as a circle.
     plt.title(f'Proportion of total maternal deaths by cause ({title}) {years}')
-    ##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/mat_death_by_cause_{title}_{years}.png', bbox_inches="tight")
+    plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/mat_death_by_cause_{title}_{years}.png', bbox_inches="tight")
     plt.show()
 
 props_df = pd.DataFrame(data=proportions_dicts)
@@ -589,7 +589,7 @@ plt.xlabel('Year')
 plt.ylabel("Rate per 1000 births")
 plt.title('Neonatal Mortality Rate per Year')
 plt.legend()
-##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/nmr.png')
+plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/nmr.png')
 plt.show()
 
 # TOTAL DEATHS
@@ -627,7 +627,7 @@ plt.ylabel('Total Deaths Neonatal Deaths (scaled)')
 plt.title('Yearly Modelled Neonatal Deaths Compared to GBD')
 plt.xticks(ind + width / 2, (years))
 plt.legend(loc='best')
-##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/deaths_gbd_comparison_neo.png')
+plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/deaths_gbd_comparison_neo.png')
 plt.show()
 
 # todo: force colours for each complication in each year to be the same
@@ -645,7 +645,7 @@ for year, dictionary in zip(sim_years, list_of_proportions_dicts_nb):
     ax.set_position([box.x0, box.y0, box.width * 0.5, box.height])
     plt.legend(labels, loc='center left', bbox_to_anchor=(1, 0.5))
     plt.title(f'Proportion of total neonatal deaths by cause in {year}')
-    ##(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/neo_death_by_cause_{year}.png', bbox_inches="tight")
+    plt.savefig(f'./outputs/sejjj49@ucl.ac.uk/{graph_location}/neo_death_by_cause_{year}.png', bbox_inches="tight")
     plt.show()
 
 # CASE FATALITY PER COMPLICATION
@@ -755,7 +755,7 @@ for cause, tr in zip(simplified_causes, trs):
     if (cause == 'encephalopathy') or (cause == 'neonatal_respiratory_depression'):
         deaths = get_mean_and_quants_from_str_df(death_results, cause)[0]
 
-    elif cause == 'neonatal_sepsis':
+    elif cause == 'neonatal_sepsis':  # TODO: ERROR HERE( also early_onset_sepsis and late_onset_neonatal_sepsis) labels
         early = get_mean_and_quants_from_str_df(death_results, 'early_onset_neonatal_sepsis')[0]
         late = get_mean_and_quants_from_str_df(death_results, 'late_onset_sepsis')[0]
         deaths = [x + y for x, y in zip(early, late)]
@@ -786,12 +786,16 @@ for cause, tr in zip(simplified_causes, trs):
 
 # add in GBD?
 
+def get_dalys(df_):
+    df_.drop(columns='date').groupby(['year']).sum().stack()
+
+
 dalys_stacked = extract_results(
         results_folder,
         module="tlo.methods.healthburden",
                 key="dalys_stacked",
                 custom_generate_series=(
-                    lambda df_: df_.drop(
+                    lambda df: df.drop(
                         columns='date'
                     ).groupby(['year']).sum().stack()
                 ),
