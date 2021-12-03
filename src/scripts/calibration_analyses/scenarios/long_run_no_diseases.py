b09d3f32f861e361a9b1844325930a4ef9ddccc2
Tim Hallett <timothy.hallett@imperial.ac.uk>
2021-12-03 00:32:42 +0000

    with longer time horizon

--- src/scripts/calibration_analyses/scenarios/long_run_no_diseases.py
+++ src/scripts/calibration_analyses/scenarios/long_run_no_diseases.py
@@ -20,7 +20,7 @@ class LongRun(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2029, 12, 31)
+        self.end_date = Date(2049, 12, 31)
         self.pop_size = 50_000  # <- recommended population size for the runs is 50k
         self.number_of_draws = 1  # <- one scenario
         self.runs_per_draw = 5  # <- repeated this many times
