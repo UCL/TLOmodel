18ab263e71a8fdbe0e210bfe1d8f72273f2d7003
Tim Hallett <timothy.hallett@imperial.ac.uk>
2021-12-09 11:29:14 +0000

    small pop size

--- src/scripts/healthsystem/hsi_in_typical_run/scenario_hsi_in_typical_run.py
+++ src/scripts/healthsystem/hsi_in_typical_run/scenario_hsi_in_typical_run.py
@@ -51,7 +51,7 @@ class LongRun(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2014, 12, 31)
-        self.pop_size = 20_000
+        self.pop_size = 5_000
         self.number_of_draws = 1
         self.runs_per_draw = 1
 
