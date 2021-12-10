7dc912746bf78a3abd3ee86f43db69131763cc64
Bingling <bshe@ic.ac.uk>
2021-12-10 16:40:47 +0000

    Update the file plotting health system daily capabilities

--- src/scripts/healthsystem/descriptions_of_input_data/analysis_describe_healthsystem_capabilities.py
+++ src/scripts/healthsystem/descriptions_of_input_data/analysis_describe_healthsystem_capabilities.py
@@ -1,45 +1,27 @@
 """
-This file produces a nice plot of the capabilities of the healthsystem in terms of the hours available for
-different cadres of healthcare workers.
+This file produces histograms of the healthsystem capabilities \
+in terms of staff allocation and daily capabilities in minutes per cadre per facility level.
 """
 
-# %%
-
 from pathlib import Path
-
 import pandas as pd
 from matplotlib import pyplot as plt
+from matplotlib.ticker import ScalarFormatter
 
-resourcefilepath = Path("./resources")
+# Get the path of the folder that stores the data - three scenarios: actual, funded, funded_plus
+workingpath = Path('./resources/healthsystem/human_resources/funded_plus')
 
-# %%
+# Define the path of output histograms - three scenarios: actual, funded, funded_plus
+outputpath = Path('./outputs/healthsystem/human_resources/funded_plus')
 
-outputpath = Path("./outputs")  # folder for convenience of storing outputs
+# Read data
+data = pd.read_csv(workingpath / 'ResourceFile_Daily_Capabilities.csv')
 
 
-data = pd.read_csv(
-    Path(resourcefilepath) / "ResourceFile_Daily_Capabilities.csv"
-)
-
-# [['Total_Minutes_Per_Day','Officer_Type','District']]
-
-data = data.dropna()
-# data['District'] = data['District'].fillna('National')
-
-# do some re-grouping to make a more manageable number of health cadres:
-data['Officer_Type'] = data['Officer_Type'].replace('DCSA', 'CHW')
-data['Officer_Type'] = data['Officer_Type'].replace(['Lab Officer', 'Lab Technician', 'Lab Assistant'], 'Lab Support')
-data['Officer_Type'] = data['Officer_Type'].replace(['Radiographer', 'Radiography Technician'], 'Radiography')
-data['Officer_Type'] = data['Officer_Type'].replace(['Nurse Officer', 'Nutrition Staff', 'Med. Assistant'], 'Nurse')
-data['Officer_Type'] = data['Officer_Type'].replace('Nurse Midwife Technician', 'MidWife')
-data['Officer_Type'] = data['Officer_Type'].replace(['Pharmacist', 'Pharm Technician', 'Pharm Assistant'], 'Pharmacy')
-data['Officer_Type'] = data['Officer_Type'].replace(['Medical Officer / Specialist', 'Clinical Officer / Technician'],
-                                                    'Clinician')
-data['Officer_Type'] = data['Officer_Type'].replace(['Dental Therapist'], 'Dentist')
-
-# MINUTES PER HEALTH OFFICER TYPE BY DISTRICT:
-dat = pd.DataFrame(data.groupby(['District', 'Officer_Type'], as_index=False)['Total_Minutes_Per_Day'].sum())
-tab = dat.pivot(index='District', columns='Officer_Type', values='Total_Minutes_Per_Day')
+# MINUTES PER HEALTH OFFICER CATEGORY BY DISTRICT
+data_districts = data.dropna(inplace=False)
+dat = pd.DataFrame(data_districts.groupby(['District', 'Officer_Category'], as_index=False)['Total_Mins_Per_Day'].sum())
+tab = dat.pivot(index='District', columns='Officer_Category', values='Total_Mins_Per_Day')
 ax = tab.plot.bar(stacked=True)
 plt.ylabel('Minutes per day')
 plt.xlabel('District')
@@ -48,6 +30,128 @@ ax.legend(ncol=3, bbox_to_anchor=(0, 1),
           loc='lower left', fontsize='small')
 
 plt.savefig(outputpath / 'health_officer_minutes_per_district.pdf', bbox_inches='tight')
-plt.show()
 
-# %%
+# STAFF COUNTS PER HEALTH OFFICER CATEGORY BY DISTRICT
+data_districts = data.dropna(inplace=False)
+dat = pd.DataFrame(data_districts.groupby(['District', 'Officer_Category'], as_index=False)['Staff_Count'].sum())
+tab = dat.pivot(index='District', columns='Officer_Category', values='Staff_Count')
+ax = tab.plot.bar(stacked=True)
+plt.ylabel('Staff counts')
+plt.xlabel('District')
+
+ax.legend(ncol=3, bbox_to_anchor=(0, 1),
+          loc='lower left', fontsize='small')
+
+plt.savefig(outputpath / 'staff_allocation_per_district.pdf', bbox_inches='tight')
+
+
+# MINUTES PER HEALTH OFFICER CATEGORY BY LEVEL
+dat = pd.DataFrame(data.groupby(['Facility_Level', 'Officer_Category'], as_index=False)['Total_Mins_Per_Day'].sum())
+tab = dat.pivot(index='Facility_Level', columns='Officer_Category', values='Total_Mins_Per_Day')
+ax = tab.plot.bar(stacked=True)
+# ax = tab.plot.bar(stacked=True, log=True)
+plt.ylabel('Minutes per day')
+plt.xlabel('Facility level')
+
+ax.tick_params(axis='x', rotation=0)
+
+formatter = ScalarFormatter()
+formatter.set_scientific(False)
+ax.yaxis.set_major_formatter(formatter)
+
+ax.legend(ncol=3, bbox_to_anchor=(0, 1),
+          loc='lower left', fontsize='small')
+
+plt.savefig(outputpath / 'health_officer_minutes_per_level.pdf', bbox_inches='tight')
+
+# STAFF COUNTS PER HEALTH OFFICER CATEGORY BY LEVEL
+dat = pd.DataFrame(data.groupby(['Facility_Level', 'Officer_Category'], as_index=False)['Staff_Count'].sum())
+tab = dat.pivot(index='Facility_Level', columns='Officer_Category', values='Staff_Count')
+ax = tab.plot.bar(stacked=True)
+# ax = tab.plot.bar(stacked=True, log=True)
+plt.ylabel('Staff counts')
+plt.xlabel('Facility level')
+
+ax.tick_params(axis='x', rotation=0)
+
+ax.legend(ncol=3, bbox_to_anchor=(0, 1),
+          loc='lower left', fontsize='small')
+
+plt.savefig(outputpath / 'staff_allocation_per_level.pdf', bbox_inches='tight')
+
+
+# MINUTES PER HEALTH OFFICER CATEGORY BY LEVEL
+
+# Level 0
+data_level = data.loc[data['Facility_Level'] == '0', :]
+tab = data_level.pivot(index='District', columns='Officer_Category', values='Total_Mins_Per_Day')
+ax = tab.plot.bar(stacked=True)
+plt.ylabel('Minutes per day at level 0')
+plt.xlabel('District')
+
+ax.legend(ncol=3, bbox_to_anchor=(0, 1),
+          loc='lower left', fontsize='small')
+
+plt.savefig(outputpath / 'health_officer_minutes_per_district_level_0.pdf', bbox_inches='tight')
+
+# Level 1a
+data_level = data.loc[data['Facility_Level'] == '1a', :]
+tab = data_level.pivot(index='District', columns='Officer_Category', values='Total_Mins_Per_Day')
+ax = tab.plot.bar(stacked=True)
+plt.ylabel('Minutes per day at level 1a')
+plt.xlabel('District')
+
+ax.legend(ncol=3, bbox_to_anchor=(0, 1),
+          loc='lower left', fontsize='small')
+
+plt.savefig(outputpath / 'health_officer_minutes_per_district_level_1a.pdf', bbox_inches='tight')
+
+# Level 1b
+data_level = data.loc[data['Facility_Level'] == '1b', :]
+tab = data_level.pivot(index='District', columns='Officer_Category', values='Total_Mins_Per_Day')
+ax = tab.plot.bar(stacked=True)
+plt.ylabel('Minutes per day at level 1b')
+plt.xlabel('District')
+
+ax.legend(ncol=3, bbox_to_anchor=(0, 1),
+          loc='lower left', fontsize='small')
+
+plt.savefig(outputpath / 'health_officer_minutes_per_district_level_1b.pdf', bbox_inches='tight')
+
+# Level 2
+data_level = data.loc[data['Facility_Level'] == '2', :]
+tab = data_level.pivot(index='District', columns='Officer_Category', values='Total_Mins_Per_Day')
+ax = tab.plot.bar(stacked=True)
+plt.ylabel('Minutes per day at level 2')
+plt.xlabel('District')
+
+ax.legend(ncol=3, bbox_to_anchor=(0, 1),
+          loc='lower left', fontsize='small')
+
+plt.savefig(outputpath / 'health_officer_minutes_per_district_level_2.pdf', bbox_inches='tight')
+
+# Level 3
+data_level = data.loc[data['Facility_Level'] == '3', :]
+tab = data_level.pivot(index='Region', columns='Officer_Category', values='Total_Mins_Per_Day')
+ax = tab.plot.bar(stacked=True)
+plt.ylabel('Minutes per day at level 3')
+plt.xlabel('Regional Referral Hospital')
+ax.tick_params(axis='x', rotation=0)
+
+ax.legend(ncol=3, bbox_to_anchor=(0, 1),
+          loc='lower left', fontsize='small')
+
+plt.savefig(outputpath / 'health_officer_minutes_per_district_level_3.pdf', bbox_inches='tight')
+
+# Level 4
+data_level = data.loc[data['Facility_Level'] == '4', :]
+tab = data_level.pivot(index='Facility_Name', columns='Officer_Category', values='Total_Mins_Per_Day')
+ax = tab.plot.bar(stacked=True, width=0.1)
+plt.ylabel('Minutes per day at level 4')
+plt.xlabel('National resource hospital')
+ax.tick_params(axis='x', rotation=0)
+
+ax.legend(ncol=3, bbox_to_anchor=(0, 1),
+          loc='lower left', fontsize='small')
+
+plt.savefig(outputpath / 'health_officer_minutes_per_district_level_4.pdf', bbox_inches='tight')
