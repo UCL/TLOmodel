483d130d456f5da75865d3895e13dc8279f53c04
Bingling <bshe@ic.ac.uk>
2021-12-13 19:59:48 +0000

    Updating existing plots

--- src/scripts/healthsystem/hsi_in_typical_run/analysis_hsi_in_typical_run.py
+++ src/scripts/healthsystem/hsi_in_typical_run/analysis_hsi_in_typical_run.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 # import numpy as np
 import pandas as pd
+from matplotlib.cm import get_cmap
 
 from tlo.analysis.utils import get_scenario_outputs, load_pickled_dataframes
 
@@ -38,14 +39,14 @@ frac_time_used_2016 = frac_time_used.loc['2015-12-31':'2017-01-01']
 
 # Plot:
 frac_time_used_2014_2018.plot()
-plt.title("Fraction of Total Healthcare Worker Time Used 2014-2018")
+plt.title("Fraction of Total Healthcare Worker Time Used (year 2014-2018)")
 plt.xlabel("Date")
 plt.tight_layout()
 plt.savefig(make_graph_file_name('HSI_Frac_time_used_2014_2018'))
 plt.show()
 
 frac_time_used_2016.plot()
-plt.title("Fraction of Total Healthcare Worker Time Used 2016")
+plt.title("Fraction of Total Healthcare Worker Time Used (year 2016)")
 plt.xlabel("Date")
 plt.tight_layout()
 plt.savefig(make_graph_file_name('HSI_Frac_time_used_2016'))
@@ -58,7 +59,7 @@ hsi["date"] = pd.to_datetime(hsi["date"])
 hsi["month"] = hsi["date"].dt.month
 
 # Number of HSI that are taking place by originating module, by month
-year = 2018
+year = 2016
 hsi["Module"] = hsi["TREATMENT_ID"].str.split('_').apply(lambda x: x[0])
 evs = hsi.loc[hsi.date.dt.year == year]\
     .groupby(by=['month', 'Module'])\
@@ -66,17 +67,30 @@ evs = hsi.loc[hsi.date.dt.year == year]\
     .pivot_table(index='month', columns='Module', values='count', fill_value=0)
 
 # Plot:
-evs.plot.bar(stacked=True)
+# Use colormap tab20 so that each module has a unique color
+color_tab20 = get_cmap('tab20_r')
+evs.plot.bar(stacked=True, color=color_tab20.colors)
 plt.title(f"HSI by Module, per Month (year {year})")
 plt.ylabel('Total per month')
 plt.tight_layout()
+plt.legend(ncol=3, loc='center', fontsize='xx-small')
 plt.savefig(make_graph_file_name('HSI_per_module_per_month'))
 plt.show()
 
-# Plot the breakdown of all HSI, over all the years
-evs = hsi.groupby(by=['Module']).size()
-evs.plot.pie()
-plt.title("HSI by Module")
+# Plot the breakdown of all HSI, over all the years 2010-2018
+evs = pd.DataFrame(hsi.groupby(by=['Module']).size())
+# Calculate the fraction
+evs[1] = 100*evs[0]/evs[0].sum()
+patches, texts = plt.pie(evs[0], colors=color_tab20.colors)
+labels = ['{0} - {1:1.2f} %'.format(i, j) for i, j in zip(evs.index, evs[1])]
+# Sort legend
+sort_legend = True
+if sort_legend:
+    patches, labels, dummy = zip(*sorted(zip(patches, labels, evs[0]),
+                                         key=lambda x: x[2],
+                                         reverse=True))
+plt.legend(patches, labels, ncol=3, loc='lower center', fontsize='xx-small')
+plt.title("HSI by Module (year 2010-2018)")
 plt.tight_layout()
 plt.savefig(make_graph_file_name('HSI_per_module'))
 plt.show()
