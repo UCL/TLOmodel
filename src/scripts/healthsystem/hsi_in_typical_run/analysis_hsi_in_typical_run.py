19aaf5bdc60522ed679db626df7532d1d837e42c
Bingling <bshe@ic.ac.uk>
2021-12-15 19:05:24 +0000

    Rename weird modules

--- src/scripts/healthsystem/hsi_in_typical_run/analysis_hsi_in_typical_run.py
+++ src/scripts/healthsystem/hsi_in_typical_run/analysis_hsi_in_typical_run.py
@@ -61,9 +61,12 @@ hsi["month"] = hsi["date"].dt.month
 # Number of HSI that are taking place by originating module, by month
 year = 2016
 hsi["Module"] = hsi["TREATMENT_ID"].str.split('_').apply(lambda x: x[0])
-# todo: Rename Module HSI to Generic First Appointment? \
-#  (Q: The treatment_id for module HSI seems not related to Generic First Appt; \
-#  Besides, there are similar modules called GenericEmergency... and GenericFirstAppt...)
+# Rename Module 'HSI' to 'CareOfWomenDuringPregnancy'
+hsi["Module"] = hsi["Module"].replace("HSI", "CareOfWomenDuringPregnancy")
+# Rename Module 'GenericEmergency...' and 'GenericFirstAppt...' to 'HealthSeekingBehaviour' (or 'GenericFirstAppt')
+hsi["Module"] = hsi["Module"].replace("GenericFirstApptAtFacilityLevel0", "HealthSeekingBehaviour")
+hsi["Module"] = hsi["Module"].replace("GenericEmergencyFirstApptAtFacilityLevel1", "HealthSeekingBehaviour")
+
 evs = hsi.loc[hsi.date.dt.year == year]\
     .groupby(by=['month', 'Module'])\
     .size().reset_index().rename(columns={0: 'count'})\
@@ -71,12 +74,12 @@ evs = hsi.loc[hsi.date.dt.year == year]\
 
 # Plot:
 # Use colormap tab20 so that each module has a unique color
-color_tab20 = get_cmap('tab20_r')
+color_tab20 = get_cmap('tab20')
 evs.plot.bar(stacked=True, color=color_tab20.colors)
 plt.title(f"HSI by Module, per Month (year {year})")
 plt.ylabel('Total per month')
 plt.tight_layout()
-plt.legend(ncol=3, loc='center', fontsize='xx-small')
+plt.legend(ncol=3, loc='center', fontsize='x-small')
 plt.savefig(make_graph_file_name('HSI_per_module_per_month'))
 plt.show()
 
@@ -84,6 +87,7 @@ plt.show()
 evs = pd.DataFrame(hsi.groupby(by=['Module']).size())
 # Calculate the fraction
 evs[1] = 100*evs[0]/evs[0].sum()
+color_tab20 = get_cmap('tab20_r')
 patches, texts = plt.pie(evs[0], colors=color_tab20.colors)
 labels = ['{0} - {1:1.2f} %'.format(i, j) for i, j in zip(evs.index, evs[1])]
 # Sort legend
@@ -92,7 +96,7 @@ if sort_legend:
     patches, labels, dummy = zip(*sorted(zip(patches, labels, evs[0]),
                                          key=lambda x: x[2],
                                          reverse=True))
-plt.legend(patches, labels, ncol=3, loc='lower center', fontsize='xx-small')
+plt.legend(patches, labels, ncol=3, loc='lower center', fontsize='x-small')
 plt.title("HSI by Module (year 2010-2018)")
 plt.tight_layout()
 plt.savefig(make_graph_file_name('HSI_per_module'))
@@ -108,7 +112,17 @@ appts_by_treatment_id = \
 
 # Todo: Since the resulted appts_by_treatment_id deleted many hsi events \
 #  (i.e., the hsi list is much shorter than in num_hsi_by_treatment_id), \
-#  will try delete empty entries first then apply drop_duplicates().
+#  need to regenerate this table.
+# appts_by_treatment_id_alt = hsi[["TREATMENT_ID", 'Number_By_Appt_Type_Code']]
+# # drop rows that have empty entries
+# null_appt_idx = appts_by_treatment_id_alt[appts_by_treatment_id_alt['Number_By_Appt_Type_Code'] == {}].index
+# appts_by_treatment_id_alt = appts_by_treatment_id_alt.drop(index=null_appt_idx).copy()
+# # set index
+# appts_by_treatment_id_alt.set_index('TREATMENT_ID', inplace=True)
+# # turn to series
+# appts_by_treatment_id_alt = appts_by_treatment_id_alt.squeeze('columns')
+# # drop repetitive rows that have same index and value
+# # appts_by_treatment_id_alt = appts_by_treatment_id_alt.drop_duplicates()
 
 
 # Plot...
