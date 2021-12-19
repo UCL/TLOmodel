ef81375cd6a81280bb1f7df399578d4acbac0fd8
tdm32 <t.mangal@imperial.ac.uk>
2021-12-19 19:53:06 +0000

    test runs on laptop to return deviance with run 2010-2020 it works!!!

--- src/scripts/hiv/deviance_for_calibration/analysis_logged_deviance.py
+++ src/scripts/hiv/deviance_for_calibration/analysis_logged_deviance.py
@@ -38,7 +38,7 @@ resourcefilepath = Path("./resources")
 # %% Run the simulation
 start_date = Date(2010, 1, 1)
 end_date = Date(2020, 1, 1)
-popsize = 500
+popsize = 150000
 
 # set up the log config
 log_config = {
