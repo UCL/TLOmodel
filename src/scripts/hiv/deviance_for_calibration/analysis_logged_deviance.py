c18cf02de0ff9439c15cf5adc7f9d0aa2d44386f
tdm32 <t.mangal@imperial.ac.uk>
2022-03-15 11:11:20 +0000

    fix pickle issue

--- src/scripts/hiv/deviance_for_calibration/analysis_logged_deviance.py
+++ src/scripts/hiv/deviance_for_calibration/analysis_logged_deviance.py
@@ -33,10 +33,8 @@ resourcefilepath = Path("./resources")
 
 # %% Run the simulation
 start_date = Date(2010, 1, 1)
-end_date = Date(2012, 1, 1)
-popsize = 1000
-
-scenario = 1
+end_date = Date(2013, 1, 1)
+popsize = 2000
 
 # set up the log config
 log_config = {
@@ -48,7 +46,7 @@ log_config = {
         "tlo.methods.hiv": logging.INFO,
         "tlo.methods.tb": logging.INFO,
         "tlo.methods.demography": logging.INFO,
-        # "tlo.methods.healthsystem": logging.INFO,
+        "tlo.methods.healthsystem": logging.INFO,
     },
 }
 
@@ -65,7 +63,7 @@ sim.register(
         resourcefilepath=resourcefilepath,
         service_availability=["*"],  # all treatment allowed
         mode_appt_constraints=0,  # mode of constraints to do with officer numbers and time
-        cons_availability="default",  # mode for consumable constraints (if ignored, all consumables available)
+        cons_availability="all",  # mode for consumable constraints (if ignored, all consumables available)
         ignore_priority=True,  # do not use the priority information in HSI event to schedule
         capabilities_coefficient=1.0,  # multiplier for the capabilities of health officers
         disable=False,  # disables the healthsystem (no constraints and no logging) and every HSI runs
@@ -81,11 +79,6 @@ sim.register(
     # deviance_measure.Deviance(resourcefilepath=resourcefilepath),
 )
 
-# todo change
-sim.modules["Tb"].parameters["scenario"] = scenario
-# sim.modules["Tb"].parameters["scenario_start_date"] = "01/07/2010"
-
-
 # Run the simulation and flush the logger
 sim.make_initial_population(n=popsize)
 sim.simulate(end_date=end_date)
@@ -96,4 +89,4 @@ output = parse_log_file(sim.log_filepath)
 # save the results, argument 'wb' means write using binary mode. use 'rb' for reading file
 with open(outputpath / "default_run.pickle", "wb") as f:
     # Pickle the 'data' dictionary using the highest protocol available.
-    pickle.dump(output, f, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(dict(output), f, pickle.HIGHEST_PROTOCOL)
