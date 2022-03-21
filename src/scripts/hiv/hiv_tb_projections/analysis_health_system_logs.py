2d5ce7d88497cb899bf4404cad2da0fdd79f5172
Leila-Uwais <leila.uwais17@imperial.ac.uk>
2022-03-21 20:31:16 +0000

    merge tara_tb_hiv_healthsystem_logs changes

---
+++ src/scripts/hiv/hiv_tb_projections/analysis_health_system_logs.py
@@ -0,0 +1,102 @@
+"""
+Run the HIV/TB modules with health system outputs aggregated annually
+"""
+
+import datetime
+import pickle
+from pathlib import Path
+
+import pandas as pd
+from tlo import Date, Simulation, logging
+from tlo.analysis.utils import parse_log_file
+from tlo.methods import (
+    demography,
+    enhanced_lifestyle,
+    epi,
+    healthburden,
+    healthseekingbehaviour,
+    healthsystem,
+    hiv,
+    simplified_births,
+    symptommanager,
+    tb,
+)
+
+# Where will outputs go
+outputpath = Path("./outputs")  # folder for convenience of storing outputs
+
+# date-stamp to label log files and any other outputs
+datestamp = datetime.date.today().strftime("__%Y_%m_%d")
+
+# The resource files
+resourcefilepath = Path("./resources")
+
+# %% Run the simulation
+start_date = Date(2010, 1, 1)
+end_date = Date(2025, 1, 1)
+popsize = 50000
+
+# set up the log config
+log_config = {
+    "filename": "test_runs",
+    "directory": outputpath,
+    "custom_levels": {
+        "*": logging.WARNING,
+        "tlo.methods.hiv": logging.INFO,
+        "tlo.methods.tb": logging.INFO,
+        "tlo.methods.demography": logging.INFO,
+        "tlo.methods.healthsystem.summary": logging.INFO,
+    },
+}
+
+# Register the appropriate modules
+# need to call epi before tb to get bcg vax
+# seed = random.randint(0, 50000)
+seed = 14  # set seed for reproducibility
+sim = Simulation(start_date=start_date, seed=seed, log_config=log_config, show_progress_bar=True)
+sim.register(
+    demography.Demography(resourcefilepath=resourcefilepath),
+    simplified_births.SimplifiedBirths(resourcefilepath=resourcefilepath),
+    enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
+    healthsystem.HealthSystem(
+        resourcefilepath=resourcefilepath,
+        service_availability=["*"],  # all treatment allowed
+        mode_appt_constraints=0,  # mode of constraints to do with officer numbers and time
+        cons_availability="all",  # mode for consumable constraints (if ignored, all consumables available)
+        ignore_priority=True,  # do not use the priority information in HSI event to schedule
+        capabilities_coefficient=1.0,  # multiplier for the capabilities of health officers
+        disable=False,  # disables the healthsystem (no constraints and no logging) and every HSI runs
+        disable_and_reject_all=False,  # disable healthsystem and no HSI runs
+        store_hsi_events_that_have_run=False,  # convenience function for debugging
+    ),
+    symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
+    healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
+    healthburden.HealthBurden(resourcefilepath=resourcefilepath),
+    epi.Epi(resourcefilepath=resourcefilepath),
+    hiv.Hiv(resourcefilepath=resourcefilepath),
+    tb.Tb(resourcefilepath=resourcefilepath),
+    # deviance_measure.Deviance(resourcefilepath=resourcefilepath),
+)
+
+# Run the simulation and flush the logger
+sim.make_initial_population(n=popsize)
+sim.simulate(end_date=end_date)
+
+# parse the results
+output = parse_log_file(sim.log_filepath)
+
+# save the results, argument 'wb' means write using binary mode. use 'rb' for reading file
+# with open(outputpath / "default_run.pickle", "wb") as f:
+#     # Pickle the 'data' dictionary using the highest protocol available.
+#     pickle.dump(dict(output), f, pickle.HIGHEST_PROTOCOL)
+
+for key, dfs in output.items():
+    if key.startswith("tlo."):
+        with open(outputpath / f"{key}.pickle", "wb") as f:
+            print(f)
+            pickle.dump(dfs, f)
+
+tmp = output["tlo.methods.healthsystem.summary"]["health_system_annual_logs"]
+new = tmp[['date', 'treatment_counts']].copy()
+new2 = pd.DataFrame(new['treatment_counts'].to_list())
+
