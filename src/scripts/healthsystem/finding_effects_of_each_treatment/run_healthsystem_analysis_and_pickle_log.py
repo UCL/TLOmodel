32174ab23e9ef16209f7cbc964db771b1b5ab98f
Tim Hallett <timothy.hallett@imperial.ac.uk>
2022-03-23 21:46:13 +0000

    update module and treatment_ids

--- src/scripts/healthsystem/finding_effects_of_each_treatment/run_healthsystem_analysis_and_pickle_log.py
+++ src/scripts/healthsystem/finding_effects_of_each_treatment/run_healthsystem_analysis_and_pickle_log.py
@@ -2,6 +2,7 @@
 A run of the model with logging so as to allow for descriptions of overall Health Burden and usage of the Health System.
 TODO -- have turned off spurious_symptoms to make it go faster, but this should be on really!
 """
+
 import pickle
 from datetime import datetime
 from pathlib import Path
@@ -26,7 +27,9 @@ from tlo.methods import (
     malaria,
     oesophagealcancer,
     pregnancy_supervisor,
-    symptommanager,
+    symptommanager, rti, prostate_cancer, other_adult_cancers, breast_cancer, bladder_cancer,
+    cardio_metabolic_disorders, measles, wasting, stunting, alri, postnatal_supervisor, newborn_outcomes,
+    care_of_women_during_pregnancy,
 )
 
 # Define path and filenames
@@ -41,14 +44,13 @@ end_date = start_date + pd.DateOffset(years=4)
 pop_size = 20_000
 
 # The resource files
-rfp = Path("./resources")
+resourcefilepath = Path("./resources")
 
 log_config = {
     "filename": "for_profiling",
     "directory": "./outputs",
     "custom_levels": {
         "*": logging.WARNING,
-        "tlo.methods.healthsystem": logging.INFO,
         "tlo.methods.healthburden": logging.INFO,
         "tlo.methods.demography": logging.INFO
     }
@@ -61,24 +63,58 @@ def run_sim(service_availability):
     # Register the appropriate modules
     sim.register(
         # Standard modules:
-        demography.Demography(resourcefilepath=rfp),
-        enhanced_lifestyle.Lifestyle(resourcefilepath=rfp),
-        healthsystem.HealthSystem(resourcefilepath=rfp, service_availability=service_availability),
-        symptommanager.SymptomManager(resourcefilepath=rfp, spurious_symptoms=False),
-        healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=rfp),
-        healthburden.HealthBurden(resourcefilepath=rfp),
-        contraception.Contraception(resourcefilepath=rfp),
-        labour.Labour(resourcefilepath=rfp),
-        pregnancy_supervisor.PregnancySupervisor(resourcefilepath=rfp),
-        #
-        # Disease modules considered complete:
-        diarrhoea.Diarrhoea(resourcefilepath=rfp),
-        malaria.Malaria(resourcefilepath=rfp),
-        hiv.Hiv(resourcefilepath=rfp),
-        epi.Epi(resourcefilepath=rfp),
-        depression.Depression(resourcefilepath=rfp),
-        oesophagealcancer.OesophagealCancer(resourcefilepath=rfp),
-        epilepsy.Epilepsy(resourcefilepath=rfp)
+        demography.Demography(resourcefilepath=resourcefilepath),
+        enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
+        symptommanager.SymptomManager(
+            resourcefilepath=resourcefilepath,
+            spurious_symptoms=False,
+        ),
+        healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
+        healthburden.HealthBurden(resourcefilepath=resourcefilepath),
+
+        # HealthSystem
+        healthsystem.HealthSystem(
+            resourcefilepath=resourcefilepath,
+            service_availability=service_availability
+        ),
+        epi.Epi(resourcefilepath=resourcefilepath),
+
+        # Modules for birth/labour/newborns
+        contraception.Contraception(resourcefilepath=resourcefilepath),
+        pregnancy_supervisor.PregnancySupervisor(resourcefilepath=resourcefilepath),
+        care_of_women_during_pregnancy.CareOfWomenDuringPregnancy(resourcefilepath=resourcefilepath),
+        labour.Labour(resourcefilepath=resourcefilepath),
+        newborn_outcomes.NewbornOutcomes(resourcefilepath=resourcefilepath),
+        postnatal_supervisor.PostnatalSupervisor(resourcefilepath=resourcefilepath),
+
+        # Diseases of childhood
+        alri.Alri(resourcefilepath=resourcefilepath),
+        diarrhoea.Diarrhoea(resourcefilepath=resourcefilepath),
+        stunting.Stunting(resourcefilepath=resourcefilepath),
+        wasting.Wasting(resourcefilepath=resourcefilepath),
+
+        # Major Infectious Diseases
+        hiv.Hiv(resourcefilepath=resourcefilepath),
+        malaria.Malaria(resourcefilepath=resourcefilepath),
+        measles.Measles(resourcefilepath=resourcefilepath),
+
+        # Chronic Conditions
+        #  - Cardio-metabolic
+        cardio_metabolic_disorders.CardioMetabolicDisorders(resourcefilepath=resourcefilepath),
+
+        # - Cancers
+        bladder_cancer.BladderCancer(resourcefilepath=resourcefilepath),
+        breast_cancer.BreastCancer(resourcefilepath=resourcefilepath),
+        oesophagealcancer.OesophagealCancer(resourcefilepath=resourcefilepath),
+        other_adult_cancers.OtherAdultCancer(resourcefilepath=resourcefilepath),
+        prostate_cancer.ProstateCancer(resourcefilepath=resourcefilepath),
+
+        # - Mental Health
+        depression.Depression(resourcefilepath=resourcefilepath),
+        epilepsy.Epilepsy(resourcefilepath=resourcefilepath),
+
+        # Other
+        rti.RTI(resourcefilepath=resourcefilepath)
     )
     # Run the simulation
     sim.make_initial_population(n=pop_size)
@@ -96,58 +132,102 @@ def run_sim(service_availability):
 # (todo - or consider letting the gating be at the level of the disease module or not rely on the naming)
 
 treatment_ids = [
-    'CareOfWomenDuringPregnancy_PresentsForFirstAntenatalCareVisit',
-    'CareOfWomenDuringPregnancy_PresentsForSubsequentAntenatalCareVisit',
-    'CareOfWomenDuringPregnancy_EmergencyTreatment',
-    'CareOfWomenDuringPregnancy_PresentsForPostAbortionCare',
-    'CareOfWomenDuringPregnancy_TreatmentFollowingAntepartumStillbirth',
+    "Alri_GenericTreatment",
     "BladderCancer_Investigation_Following_blood_urine",
     "BladderCancer_Investigation_Following_pelvic_pain",
-    "BladderCancer_StartTreatment",
     "BladderCancer_MonitorTreatment",
     "BladderCancer_PalliativeCare",
-    'Depression_TalkingTherapy',
-    'Depression_Antidepressant_Start',
-    'Depression_Antidepressant_Refill',
-    'Diarrhoea_Treatment_PlanA',
-    'Diarrhoea_Treatment_PlanB',
-    'Diarrhoea_Treatment_PlanC',
-    'Diarrhoea_Severe_Persistent_Diarrhoea',
-    'Diarrhoea_Non_Severe_Persistent_Diarrhoea',
-    'Diarrhoea_Dysentery',
-    "Epi_bcg",
-    "Epi_opv",
+    "BladderCancer_StartTreatment",
+    "BreastCancer_Investigation_Following_breast_lump_discernible",
+    "BreastCancer_MonitorTreatment",
+    "BreastCancer_PalliativeCare",
+    "breastCancer_StartTreatment",
+    "CardioMetabolicDisorders_CommunityTestingForHypertension",
+    "CardioMetabolicDisorders_InvestigationNotFollowingSymptoms",
+    "CardioMetabolicDisorders_Investigation_Following_Symptoms",
+    "CardioMetabolicDisorders_Refill_Medication",
+    "CardioMetabolicDisorders_SeeksEmergencyCareAndGetsTreatment",
+    "CardioMetabolicDisorders_StartWeightLossAndMedication",
+    "CareOfWomenDuringPregnancy_AntenatalOutpatientFollowUp",
+    "CareOfWomenDuringPregnancy_AntenatalOutpatientManagementOfAnaemia",
+    "CareOfWomenDuringPregnancy_AntenatalWardInpatientCare",
+    "CareOfWomenDuringPregnancy_EighthAntenatalCareContact",
+    "CareOfWomenDuringPregnancy_FifthAntenatalCareContact",
+    "CareOfWomenDuringPregnancy_FirstAntenatalCareContact",
+    "CareOfWomenDuringPregnancy_FourthAntenatalCareContact",
+    "CareOfWomenDuringPregnancy_MaternalEmergencyAssessment",
+    "CareOfWomenDuringPregnancy_PostAbortionCaseManagement",
+    "CareOfWomenDuringPregnancy_PresentsForInductionOfLabour",
+    "CareOfWomenDuringPregnancy_SecondAntenatalCareContact",
+    "CareOfWomenDuringPregnancy_SeventhAntenatalCareContact",
+    "CareOfWomenDuringPregnancy_SixthAntenatalCareContact",
+    "CareOfWomenDuringPregnancy_ThirdAntenatalCareContact",
+    "CareOfWomenDuringPregnancy_TreatmentForEctopicPregnancy",
+    "Contraception_FamilyPlanningAppt",
+    "Depression_Antidepressant_Refill",
+    "Depression_Antidepressant_Start",
+    "Depression_TalkingTherapy",
+    "Diarrhoea_Treatment_Inpatient",
+    "Diarrhoea_Treatment_Outpatient",
     "Epi_DtpHibHep",
-    "Epi_Rota",
     "Epi_MeaslesRubella",
-    "Epi_hpv",
+    "Epi_Pneumo",
+    "Epi_Rota",
     "Epi_Td",
-    'Epilepsy_Start_Anti-Epilpetics',
-    "Hiv_TestAndRefer",
+    "Epi_bcg",
+    "Epi_hpv",
+    "Epi_opv",
+    "Epilepsy_Start_Anti - Epilpetics",
+    # "GenericEmergencyFirstApptAtFacilityLevel1",
+    # "GenericFirstApptAtFacilityLevel0",
     "Hiv_Circumcision",
     "Hiv_StartOrContinueOnPrep",
+    "Hiv_TestAndRefer",
     "Hiv_Treatment_InitiationOrContinuation",
-    'GenericFirstApptAtFacilityLevel1',
-    'GenericFirstApptAtFacilityLevel0',
-    'GenericEmergencyFirstApptAtFacilityLevel1',
-    'Labour_PresentsForSkilledAttendanceInLabour',
-    'Labour_ReceivesCareForPostpartumPeriod',
-    'Labour_CaesareanSection',
-    'Labour_ReceivesBloodTransfusion',
-    'Labour_SurgeryForLabourComplicationsFacilityLevel1',
+    "Labour_ReceivesCareFollowingCaesareanSection",
+    "Labour_ReceivesComprehensiveEmergencyObstetricCare",
+    "Labour_ReceivesSkilledBirthAttendanceDuringLabour",
+    "Labour_ReceivesSkilledBirthAttendanceFollowingLabour",
+    "Malaria_IPTp",
     "Malaria_RDT",
+    "Malaria_treatment_adult",
     "Malaria_treatment_child0_5",
     "Malaria_treatment_child5_15",
-    "Malaria_treatment_adult",
-    "Malaria_treatment_complicated_child",
     "Malaria_treatment_complicated_adult",
-    "Malaria_IPTp",
-    'NewbornOutcomes_ReceivesSkilledAttendance',
-    'NewbornOutcomes_NeonateInpatientDay',
+    "Malaria_treatment_complicated_child",
+    "Measles_Treatment",
+    "NewbornOutcomes_CareOfTheNewbornBySkilledAttendant",
     "OesophagealCancer_Investigation_Following_Dysphagia",
-    "OesophagealCancer_StartTreatment",
     "OesophagealCancer_MonitorTreatment",
-    "OesophagealCancer_PalliativeCare"
+    "OesophagealCancer_PalliativeCare",
+    "OesophagealCancer_StartTreatment",
+    "OtherAdultCancer_Investigation_Following_other_adult_ca_symptom",
+    "OtherAdultCancer_MonitorTreatment",
+    "OtherAdultCancer_PalliativeCare",
+    "OtherAdultCancer_StartTreatment",
+    "PostnatalSupervisor_NeonatalWardInpatientCare",
+    "PostnatalSupervisor_PostnatalCareContactOne",
+    "PostnatalSupervisor_PostnatalCareContactTwoMaternal",
+    "PostnatalSupervisor_PostnatalWardInpatientCare",
+    "PostnatalSupervisor_TreatmentForObstetricFistula",
+    "ProstateCancer_Investigation_Following_blood_urine",
+    "ProstateCancer_Investigation_Following_pelvic_pain",
+    "ProstateCancer_Investigation_Following_psa_positive",
+    "ProstateCancer_MonitorTreatment",
+    "ProstateCancer_PalliativeCare",
+    "ProstateCancer_StartTreatment",
+    "RTI_Acute_Pain_Management",
+    "RTI_Burn_Management",
+    "RTI_Fracture_Cast",
+    "RTI_Imaging_Event",
+    "RTI_Major_Surgeries",
+    "RTI_MedicalIntervention",
+    "RTI_Minor_Surgeries",
+    "RTI_Open_Fracture_Treatment",
+    "RTI_Shock_Treatment",
+    "RTI_Suture",
+    "RTI_Tetanus_Vaccine",
+    "Complementary_feeding_for_stunting"
 ]
 
 stubs = {s.split('_')[0] for s in treatment_ids}
@@ -159,6 +239,7 @@ scenarios = {
 
 # create scenarios in which only one of the stubs is permitted.
 # Note that 'wildcard is needed to allow all Treatment_IDs with that stub.
+# todo - turn this around so that it's about the lost effect when that thing is NOT running.
 for s in stubs:
     scenarios[f"Only {s}"] = [f"{st}*" for st in stubs if st == s]
 
