664653b4d224fc4cd6a1feebfde48342eb98dc61
tdm32 <t.mangal@imperial.ac.uk>
2022-04-06 16:30:11 +0100

    fixes to failing tests caused by tb not being reigstered

--- tests/test_demography.py
+++ tests/test_demography.py
@@ -29,6 +29,7 @@ from tlo.methods import (
     pregnancy_supervisor,
     prostate_cancer,
     symptommanager,
+    tb
 )
 from tlo.methods.causes import Cause
 from tlo.methods.demography import AgeUpdateEvent
@@ -135,6 +136,7 @@ def test_cause_of_death_being_registered(tmpdir, seed):
         depression.Depression(resourcefilepath=rfp),
         diarrhoea.Diarrhoea(resourcefilepath=rfp),
         hiv.Hiv(resourcefilepath=rfp),
+        tb.Tb(resourcefilepath=rfp),
         malaria.Malaria(resourcefilepath=rfp),
         cardio_metabolic_disorders.CardioMetabolicDisorders(resourcefilepath=rfp),
         oesophagealcancer.OesophagealCancer(resourcefilepath=rfp),
