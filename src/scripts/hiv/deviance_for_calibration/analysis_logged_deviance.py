432a6327defbd649cd9de03340086c53466d9e74
tdm32 <t.mangal@imperial.ac.uk>
2022-04-06 13:38:23 +0100

    fixes to test_hiv.py

--- src/scripts/hiv/deviance_for_calibration/analysis_logged_deviance.py
+++ src/scripts/hiv/deviance_for_calibration/analysis_logged_deviance.py
@@ -34,7 +34,7 @@ resourcefilepath = Path("./resources")
 # %% Run the simulation
 start_date = Date(2010, 1, 1)
 end_date = Date(2025, 1, 1)
-popsize = 20000
+popsize = 30000
 
 # set up the log config
 log_config = {
