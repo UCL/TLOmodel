325861ce8a6a0bcb4f2bab30b9190b4a3a260158
Tim Hallett <timothy.hallett@imperial.ac.uk>
2022-04-13 13:42:09 +0100

    add test_fullmodel.py as the beginning of a set of tests done on the whole model

---
+++ tests/test_fullmodel.py
@@ -0,0 +1,31 @@
+"""A set of tests on the models with all the disease modules registered."""
+
+import os
+from pathlib import Path
+
+import pandas as pd
+
+from scripts.profiling import shared
+from tlo import Date, Simulation
+from tlo.methods.fullmodel import fullmodel
+
+resourcefilepath = Path(os.path.dirname(__file__)) / '../resources'
+start_date = Date(2010, 1, 1)
+
+
+def check_dtypes(simulation):
+    df = simulation.population.props
+    orig = simulation.population.new_row
+    assert (df.dtypes == orig.dtypes).all()
+
+
+def test_dtypes_and_checksum(seed):
+    """Check that types of all properties are as expected and that the checksum can be generated."""
+
+    for _use_simplified_births in (True, False):
+        sim = Simulation(start_date=start_date, seed=seed)
+        sim.register(*fullmodel(resourcefilepath=resourcefilepath, use_simplified_births=_use_simplified_births))
+        sim.make_initial_population(n=1000)
+        sim.simulate(end_date=start_date + pd.DateOffset(months=1))
+        check_dtypes(sim)
+        shared.print_checksum(sim)
