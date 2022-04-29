efd50fd33d4b7d7c5383ddc4da4684ce01b2825f
Tara <37845078+tdm32@users.noreply.github.com>
2022-04-29 17:01:30 +0100

    Update src/tlo/methods/malaria.py
    additional comment on risk with IPTp
    
    Co-authored-by: Tim Hallett <39991060+tbhallett@users.noreply.github.com>

--- src/tlo/methods/malaria.py
+++ src/tlo/methods/malaria.py
@@ -319,7 +319,7 @@ class Malaria(Module):
         df.loc[alive & df.age_exact_years.between(0.5, 1), "ma_age_edited"] = 0.5
         df.loc[alive_over_one, "ma_age_edited"] = df.loc[alive_over_one, "age_years"].astype(float)
 
-        # select new infections, not currently on IPTp
+        # select new infections, (persons on IPTp are not at risk of infection)
         alive_uninfected = alive & ~df.ma_is_infected & ~df.ma_iptp
         now_infected = _draw_incidence_for("monthly_prob_inf", alive_uninfected)
         df.loc[now_infected, "ma_is_infected"] = True
