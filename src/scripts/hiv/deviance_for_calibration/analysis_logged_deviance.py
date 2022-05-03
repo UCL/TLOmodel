01dfbb46aa97ba1cb5b92dc4e1eba5e3d6c89d1a
tdm32 <t.mangal@imperial.ac.uk>
2022-05-03 17:38:17 +0100

    update tb microscopy consumable availability

--- src/scripts/hiv/deviance_for_calibration/analysis_logged_deviance.py
+++ src/scripts/hiv/deviance_for_calibration/analysis_logged_deviance.py
@@ -33,7 +33,7 @@ resourcefilepath = Path("./resources")
 
 # %% Run the simulation
 start_date = Date(2010, 1, 1)
-end_date = Date(2020, 1, 1)
+end_date = Date(2040, 1, 1)
 popsize = 5000
 
 # set up the log config
