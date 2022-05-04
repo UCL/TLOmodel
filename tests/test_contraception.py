a94877ec8f5f8141e4c360778b3e38426f1cdc32
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2022-05-04 15:20:22 +0100

    soften test

--- tests/test_contraception.py
+++ tests/test_contraception.py
@@ -626,7 +626,7 @@ def test_correct_number_of_live_births_created(tmpdir, seed):
     assert np.isclose(
         actual_num_births_in_Oct2010,
         expected_births,
-        atol=3.0 * np.sqrt(expected_births)  # ~99% confidence interval for poisson process (a rough approximation!)
+        atol=4.0 * np.sqrt(expected_births)  # Rough guide of allowable tolerance
     )
 
 
