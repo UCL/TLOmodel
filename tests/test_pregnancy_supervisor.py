6a39f149b4ba6669c0d268214bec92de48ed5f55
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2022-05-04 09:10:35 +0100

    remove call to pandas.tseries.offsets.DateOffset

--- tests/test_pregnancy_supervisor.py
+++ tests/test_pregnancy_supervisor.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import pytest
-from pandas.tseries.offsets import DateOffset
 
 from tlo import Date, Simulation
 from tlo.analysis.utils import parse_log_file
