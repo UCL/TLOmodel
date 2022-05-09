14cbb3ee182c8f8124aafd7e86c0038cca6936f4
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2022-05-09 14:42:33 +0100

    Created output from batch runs

--- src/scripts/epilepsy_analyses/analysis_epilepsy_large_population_size_plots.py
+++ src/scripts/epilepsy_analyses/analysis_epilepsy_large_population_size_plots.py
@@ -3,6 +3,9 @@ The results of the bachrun were put into the 'outputs' results_folder
 """
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 from tlo.analysis.utils import (
     extract_params,
     extract_results,
@@ -11,12 +14,12 @@ from tlo.analysis.utils import (
     load_pickled_dataframes,
     summarize,
 )
+
 # NOTE THAT THIS FILE PATH IS UNIQUE EACH INDIVIDUAL AND WILL BE DIFFERENT FOR EACH USER
 outputspath = Path('./outputs/rmjlra2@ucl.ac.uk/')
 
 # %% Analyse results of runs when doing a sweep of a single parameter:
 
-# 0) Find results_folder associated with a given batch_file and get most recent
 # 0) Find results_folder associated with a given batch_file and get most recent
 results_folder = get_scenario_outputs('analysis_epilepsy_large_population_size.py', outputspath)[-1]
 
@@ -41,5 +44,26 @@ inc_death = extract_results(results_folder,
                             column="epi_death_rate",  # <-- the column in the dataframe
                             index="date")  # <-- optional index
 
-inc_summary = summarize(inc, only_mean=True).mean()
-inc_death_summary = summarize(inc_death, only_mean=True).mean()
+inc_summary = summarize(inc, only_mean=True)
+inc_mean = summarize(inc, only_mean=True).mean()
+
+inc_death_summary = summarize(inc_death, only_mean=True)
+inc_death_mean = summarize(inc_death, only_mean=True).mean()
+
+gbd_inc = 47.26
+gbd_inc_death = 1.88
+plt.tight_layout()
+plt.subplot(2, 1, 1)
+plt.plot(inc_summary.index, inc_summary.values, color='lightsteelblue', label='Inc')
+plt.plot(inc_death_summary.index, inc_death_summary.values, color='lightsalmon', label='Inc death')
+plt.xlabel('Date')
+plt.ylabel('Incidence per 100,000 p.y.')
+plt.legend()
+plt.subplot(2, 1, 2)
+plt.bar(np.arange(2), [inc_mean.values[0], inc_death_mean.values[0]], color='rebeccapurple', width=0.4, label='Model')
+plt.bar(np.arange(2) + 0.4, [gbd_inc, gbd_inc_death], color='royalblue', width=0.4, label='GBD')
+plt.xticks(np.arange(2) + 0.2, ['Mean incidence', 'Mean incidence\nof death'])
+plt.legend()
+plt.ylabel('Incidence per 100,000 p.y.')
+plt.savefig("C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/Epilepsy/large_pop_size_inc_summary.png",
+            bbox_inches='tight')
