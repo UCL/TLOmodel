1f10e510537fd514c469b4cf7f0836864789e77f
tdm32 <t.mangal@imperial.ac.uk>
2022-05-27 16:51:41 +0100

    edit scenario scripts

--- src/scripts/hiv/hiv_tb_projections_may2022/scenario3.py
+++ src/scripts/hiv/hiv_tb_projections_may2022/scenario3.py
@@ -13,8 +13,8 @@ tlo batch-job tlo_q1_demo-123 --tasks
 Download result files for a completed job:
 tlo batch-download scenario1-2022-04-20T112503Z
 
-21st March
-Job ID: scenario1-2022-04-20T112503Z
+Job ID: scenario3-2022-05-27T155123Z
+
 
 """
 
