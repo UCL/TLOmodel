205c91dfa49b0b7a5e6c4c4f4571e5aa5c7947fa
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2022-05-31 16:09:24 +0100

    update scenario

--- src/scripts/healthsystem/finding_effects_of_each_treatment/scenario_effect_of_each_treatment.py
+++ src/scripts/healthsystem/finding_effects_of_each_treatment/scenario_effect_of_each_treatment.py
@@ -25,11 +25,11 @@ class EffectOfEachTreatment(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2010, 3, 31)
-        self.pop_size = 5_000
+        self.end_date = Date(2019, 12, 31)
+        self.pop_size = 20_000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
-        self.runs_per_draw = 1  # <- repeated this many times (per draw)
+        self.runs_per_draw = 3  # <- repeated this many times (per draw)
 
     def log_configuration(self):
         return {
