208ce694d4e74c671c5f1d40d476a6b87deff776
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2022-06-09 16:24:23 +0100

    linting

--- src/tlo/analysis/utils.py
+++ src/tlo/analysis/utils.py
@@ -392,7 +392,6 @@ def create_pickles_locally(scenario_output_dir, compressed_file_name_prefix=None
                 with open(logfile.parent / f"{key}.pickle", "wb") as f:
                     pickle.dump(output, f)
 
-
     def uncompress_and_save_logfile(compressed_file) -> Path:
         """Uncompress and save a log file and return its path."""
         target = compressed_file.parent / str(compressed_file.name[0:-3])
@@ -412,11 +411,12 @@ def create_pickles_locally(scenario_output_dir, compressed_file_name_prefix=None
             if compressed_file_name_prefix is None:
                 logfile = [x for x in os.listdir(run_folder) if x.endswith('.log')][0]
             else:
-                compressed_file_name = [x for x in os.listdir(run_folder) if x.startswith(compressed_file_name_prefix)][0]
+                compressed_file_name = [
+                    x for x in os.listdir(run_folder) if x.startswith(compressed_file_name_prefix)
+                ][0]
                 logfile = uncompress_and_save_logfile(Path(run_folder) / compressed_file_name)
 
             turn_log_into_pickles(logfile)
-
 
 
 def compare_number_of_deaths(logfile: Path, resourcefilepath: Path):
