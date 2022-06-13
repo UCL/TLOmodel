c40070f018345fd5f8bf2c61fcbb3989399d5426
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2022-06-13 19:11:41 +0100

    update scenario file

--- src/scripts/healthsystem/finding_effects_of_each_treatment/analysis_effect_of_each_treatment.py
+++ src/scripts/healthsystem/finding_effects_of_each_treatment/analysis_effect_of_each_treatment.py
@@ -8,20 +8,26 @@ import squarify
 from matplotlib import pyplot as plt
 
 from tlo import Date
-from tlo.analysis.utils import extract_results, get_scenario_outputs, load_pickled_dataframes, make_age_grp_lookup, \
-    make_age_grp_types, summarize
+from tlo.analysis.utils import (
+    extract_results,
+    get_scenario_outputs,
+    make_age_grp_lookup,
+    make_age_grp_types,
+    summarize,
+)
 
 # %% Declare the name of the file that specified the scenarios used in this run.
 scenario_filename = 'scenario_effect_of_each_treatment.py'
 
-# %% Declare usual paths:
+# Declare usual paths:
 outputspath = Path('./outputs/tbh03@ic.ac.uk')
 rfp = Path('./resources')
 
 # Find results folder (most recent run generated using that scenario_filename)
 results_folder = get_scenario_outputs(scenario_filename, outputspath)[-1]
 
-# Declare path for output graphs from this script
+# %% Definitions of general helper functions
+
 make_graph_file_name = lambda stub: results_folder / f"{stub}.png"  # noqa: E731
 
 TARGET_PERIOD = (Date(2010, 1, 1), Date(2019, 12, 31))
@@ -43,12 +49,13 @@ def drop_outside_period(_df):
     return _df.drop(index=_df.index[~_df['date'].between(*TARGET_PERIOD)])
 
 
-def get_counts_of_hsi_by_treatment_id(_df):
-    return _df\
-        .loc[pd.to_datetime(_df['date']).between(*TARGET_PERIOD), 'TREATMENT_ID']\
-        .apply(pd.Series)\
-        .sum().\
-        astype(int)
+def set_param_names_as_column_index_level_0(_df):
+    """Set the columns index (level 0) as the param_names."""
+    ordered_param_names_no_prefix = {i: x for i, x in enumerate(param_names)}
+    names_of_cols_level0 = [ordered_param_names_no_prefix.get(col) for col in _df.columns.levels[0]]
+    assert len(names_of_cols_level0) == len(_df.columns.levels[0])
+    _df.columns = _df.columns.set_levels(names_of_cols_level0, level=0)
+    return _df
 
 
 def get_colors(x):
@@ -56,29 +63,40 @@ def get_colors(x):
     return [cmap(i) for i in np.arange(0, 1, 1.0 / len(x))]
 
 
+def find_difference_relative_to_comparison(_ser: pd.Series, comparison: str, scaled=False):
+    """Find the percentage difference in the values in a pd.Series with a multi-index, between the draws (level 0)
+    within the runs (level 1). Drop the comparison entries."""
+    return _ser \
+        .unstack() \
+        .apply(lambda x: (x - x[comparison]) / (x[comparison] if scaled else 1.0), axis=0) \
+        .drop(index=[comparison]) \
+        .stack()
+    # todo - is this wrong way around?!?!?!!? Better commenting needed about how we're estimating "deaths averted"
+
+
+def find_mean_difference_in_appts_relative_to_comparison(_df: pd.DataFrame, comparison: str):
+    """Find the mean difference in the number of appointments between each draw and the comparison draw (within each
+    run)."""
+    return pd.concat({
+        _idx: row.unstack().apply(lambda x: (x[comparison] - x), axis=0).mean(axis=1)
+        for _idx, row in _df.iterrows()
+    }, axis=1).T.drop(columns=comparison)
+
+
 # %% Define parameter names
 param_names = get_parameter_names_from_scenario_file()
-
-# Find the difference in the number of deaths between each draw and a comparison, comparing with the run
-comparator_draw_num = [i for i, x in enumerate(param_names) if x == 'Everything'][0]
 
 
 # %% Quantify the health associated with each TREATMENT_ID (short) (The difference in deaths and DALYS between each
 # scenario and the 'Everything' scenario.)
 
-def set_param_names_as_column_index_level_0(_df):
-    """Set the columns index (level 0) as the param_names. Suppress the 'No' prefix and '*' suffix."""
-    param_names_no_prefix = [x.lstrip("No ").rstrip("*") for x in param_names]
-    _df.columns = _df.columns.set_levels(param_names_no_prefix, level=0)
-    return _df
-
-
 def num_deaths_by_age_group(_df):
     """Return total number of deaths (total by age-group within the TARGET_PERIOD)"""
     return _df \
-        .loc[pd.to_datetime(_df.date).between(*TARGET_PERIOD)]\
-        .groupby(_df['age'].map(age_grp_lookup).astype(make_age_grp_types()))\
+        .loc[pd.to_datetime(_df.date).between(*TARGET_PERIOD)] \
+        .groupby(_df['age'].map(age_grp_lookup).astype(make_age_grp_types())) \
         .size()
+
 
 def num_dalys_by_cause(_df):
     """Return total number of DALYS (Stacked) (total by age-group within the TARGET_PERIOD)"""
@@ -87,6 +105,7 @@ def num_dalys_by_cause(_df):
         .drop(columns=['date', 'sex', 'age_range', 'year']) \
         .sum()
 
+
 num_deaths = extract_results(
     results_folder,
     module='tlo.methods.demography',
@@ -94,6 +113,8 @@ num_deaths = extract_results(
     custom_generate_series=num_deaths_by_age_group,
     do_scaling=True
 ).pipe(set_param_names_as_column_index_level_0).sum()  # (Summing across age-groups)
+# todo - stochastic variability!? Look at differences which exceed stochastic variation somehow???
+# todo - run bigger and on "coarser" set and remove FirstAttendance* as a scenario ****
 
 num_dalys = extract_results(
     results_folder,
@@ -103,22 +124,12 @@ num_dalys = extract_results(
     do_scaling=True
 ).pipe(set_param_names_as_column_index_level_0).sum()  # (Summing across causes)
 
-
-def find_percentage_difference_relative_to_comparison(_ser: pd.Series, comparison: str):
-    """Find the percentage difference in the values in a pd.Series with a multi-index, between the draws (level 0)
-    within the runs (level 1). Drop the comparison entries."""
-    return _ser\
-        .unstack()\
-        .apply(lambda x: 100.0 * (x[comparison] - x) / x[comparison], axis=0)\
-        .drop(index=[comparison])\
-        .stack()
-
 fig, ax = plt.subplots()
 name_of_plot = 'Deaths Averted by Each TREATMENT_ID (Short)'
 pc_deaths_averted = summarize(
     pd.DataFrame(
-        find_percentage_difference_relative_to_comparison(num_deaths, comparison='Everything')).T
-).iloc[0].unstack().sort_values(by='mean', ascending=False)
+        find_difference_relative_to_comparison(num_deaths, comparison='Everything')).T
+).iloc[0].unstack().sort_values(by='mean', ascending=True)
 pc_deaths_averted['mean'].plot.barh(ax=ax)
 ax.set_title(name_of_plot)
 ax.set_ylabel('TREATMENT_ID (Short)')
@@ -130,13 +141,12 @@ fig.tight_layout()
 fig.savefig(make_graph_file_name(name_of_plot.replace(' ', '_')))
 fig.show()
 
-
 fig, ax = plt.subplots()
 name_of_plot = 'DALYS Averted by Each TREATMENT_ID (Short)'
 pc_dalys_averted = summarize(
     pd.DataFrame(
-        find_percentage_difference_relative_to_comparison(num_dalys, comparison='Everything')).T
-).iloc[0].unstack().sort_values(by='mean', ascending=False)
+        find_difference_relative_to_comparison(num_dalys, comparison='Everything')).T
+).iloc[0].unstack().sort_values(by='mean', ascending=True)
 pc_dalys_averted['mean'].plot.barh(ax=ax)
 ax.set_title(name_of_plot)
 ax.set_ylabel('TREATMENT_ID (Short)')
@@ -149,9 +159,34 @@ fig.savefig(make_graph_file_name(name_of_plot.replace(' ', '_')))
 fig.show()
 
 
-
 # %% Quantify the healthcare system resources used with each TREATMENT_ID (short) (The difference in the number of
 # appointments between each scenario and the 'Everything' scenario.)
+
+def get_counts_of_hsi_by_treatment_id(_df):
+    return _df \
+        .loc[pd.to_datetime(_df['date']).between(*TARGET_PERIOD), 'TREATMENT_ID'] \
+        .apply(pd.Series) \
+        .sum() \
+        .astype(int)
+
+
+def find_mean_appts(_df: pd.DataFrame):
+    """Find the mean difference in the number of appointments between each draw and the comparison draw (within each
+    run)."""
+    return pd.concat({
+        _idx: row.unstack().mean(axis=1)
+        for _idx, row in _df.iterrows()
+    }, axis=1).T
+
+
+def get_counts_of_appts(_df):
+    """Get the counts of appointments of each type being used."""
+    return _df \
+        .loc[pd.to_datetime(_df['date']).between(*TARGET_PERIOD), 'Number_By_Appt_Type_Code'] \
+        .apply(pd.Series) \
+        .sum() \
+        .astype(int)
+
 
 counts_of_hsi_by_treatment_id = extract_results(
     results_folder,
@@ -159,21 +194,20 @@ counts_of_hsi_by_treatment_id = extract_results(
     key='HSI_Event',
     custom_generate_series=get_counts_of_hsi_by_treatment_id,
     do_scaling=True
-)
+).pipe(set_param_names_as_column_index_level_0).fillna(0.0).sort_index()
 
-# 1) Examine the appointments that are occuring
-for i, scenario_name in enumerate(param_names):
-    average_num_hsi = counts_of_hsi_by_treatment_id.loc[:, (i, slice(None))].mean(axis=1)
-    average_num_hsi = average_num_hsi.loc[average_num_hsi > 0]
+mean_appts = find_mean_appts(counts_of_hsi_by_treatment_id)
 
-    # todo: find a way to fail gracefully here when one draw (=20) fails....
-    # todo: find why nan's are appearing anywhere in `counts_of_hsi_treatment_id`
+# 1) Examine the HSI that are occurring by TREATMENT_ID
+for scenario_name, _counts in mean_appts.T.iterrows():
+    _counts_non_zero = _counts[_counts > 0]
+
     fig, ax = plt.subplots()
-    name_of_plot = f'HSI Events Occurring With Service Availability = {scenario_name}'
+    name_of_plot = f'HSI Events Occurring: {scenario_name}'
     squarify.plot(
-        sizes=average_num_hsi.values,
-        label=average_num_hsi.index,
-        color=get_colors(average_num_hsi.values),
+        sizes=_counts_non_zero.values,
+        label=_counts_non_zero.index,
+        color=get_colors(_counts_non_zero.values),
         alpha=1,
         pad=True,
         ax=ax,
@@ -184,8 +218,27 @@ for i, scenario_name in enumerate(param_names):
     fig.savefig(make_graph_file_name(name_of_plot.replace(' ', '_')))
     fig.show()
 
+# Look at differences in terms of TREATMENT_IDS
+delta = find_mean_difference_in_appts_relative_to_comparison(counts_of_hsi_by_treatment_id, comparison='Everything')
+big_difference = delta.apply(lambda x: np.isclose(1.0, x))
 
-# 2) Look at differences...
+# 2) Examine the Difference in the number/type of appointments occurring
 
-# todo ....
+counts_of_appts = extract_results(
+    results_folder,
+    module='tlo.methods.healthsystem.summary',
+    key='HSI_Event',
+    custom_generate_series=get_counts_of_appts,
+    do_scaling=False
+).pipe(set_param_names_as_column_index_level_0).fillna(0.0).sort_index()
 
+delta_appts = find_mean_difference_in_appts_relative_to_comparison(counts_of_appts, comparison='Everything')
+
+for scenario_name, _appt_counts in delta_appts.T.iterrows():
+    fig, ax = plt.subplots()
+    name_of_plot = f'Difference in Appointments: {scenario_name}*'
+    _appt_counts.plot.bar(ax=ax)
+    ax.set_title(name_of_plot, {'size': 12, 'color': 'black'})
+    fig.tight_layout()
+    fig.savefig(make_graph_file_name(name_of_plot.replace(' ', '_')))
+    fig.show()
