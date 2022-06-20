a3623af42f569813990823c31ca313375c928a8e
tdm32 <t.mangal@imperial.ac.uk>
2022-06-20 17:05:36 +0100

    edit scenario set up

--- src/scripts/hiv/hiv_tb_projections_may2022/scenario1.py
+++ src/scripts/hiv/hiv_tb_projections_may2022/scenario1.py
@@ -33,10 +33,10 @@ class TestScenario(BaseScenario):
         super().__init__()
         self.seed = 5
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2050, 12, 31)
-        self.pop_size = 100000
+        self.end_date = Date(2035, 12, 31)
+        self.pop_size = 50000
         self.number_of_draws = 5
-        self.runs_per_draw = 5
+        self.runs_per_draw = 3
 
     def log_configuration(self):
         return {
