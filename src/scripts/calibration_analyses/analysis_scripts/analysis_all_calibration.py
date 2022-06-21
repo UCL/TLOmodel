c516413f14b78c0ec0a9d244ede97d7dc6767715
Bingling <b.she@imperial.ac.uk>
2022-06-21 10:56:46 +0100

    add scripts/functions to analysis_all_calibration

--- src/scripts/calibration_analyses/analysis_scripts/analysis_all_calibration.py
+++ src/scripts/calibration_analyses/analysis_scripts/analysis_all_calibration.py
@@ -4,6 +4,8 @@ from scripts.calibration_analyses.analysis_scripts import (
     analysis_cause_of_death_and_disability_calibrations,
     analysis_demography_calibrations,
     analysis_hsi_descriptions,
+    analysis_get_simulated_monthly_usage_by_appt_type,
+    analysis_compare_appt_usage_real_and_simulation
 )
 from tlo.analysis.utils import get_scenario_outputs
 
@@ -21,9 +23,15 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
     analysis_hsi_descriptions.apply(
         results_folder=results_folder, output_folder=results_folder, resourcefilepath=rfp)
 
+    analysis_get_simulated_monthly_usage_by_appt_type.apply(
+        results_folder=results_folder, output_folder=results_folder)  # this much run before analysis_compare_
+
+    analysis_compare_appt_usage_real_and_simulation.apply(
+        results_folder=results_folder, output_folder=results_folder, resourcefilepath=rfp)
+
 
 if __name__ == "__main__":
-    outputspath = Path('./outputs/tbh03@ic.ac.uk')
+    outputspath = Path('./outputs/bshe@ic.ac.uk')  # tbh03
     rfp = Path('./resources')
 
     # Find results folder (most recent run generated using that scenario_filename)
