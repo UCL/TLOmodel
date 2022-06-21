e475311d1133171e2b7a4c635cbff03902f5d7c0
Bingling <b.she@imperial.ac.uk>
2022-06-21 10:42:13 +0100

    define apply function

--- src/scripts/calibration_analyses/analysis_scripts/analysis_get_simulated_monthly_usage_by_appt_type.py
+++ src/scripts/calibration_analyses/analysis_scripts/analysis_get_simulated_monthly_usage_by_appt_type.py
@@ -16,95 +16,94 @@ import pandas as pd
 from tlo import Date
 from tlo.analysis.utils import (
     extract_results,
-    get_scenario_outputs,
+    # get_scenario_outputs,
     summarize,
     # unflatten_flattened_multi_index_in_logging,
     # create_pickles_locally,
     # load_pickled_dataframes,
 )
 
-# %% Declare the name of the file that specified the scenarios used in this run.
-scenario_filename = 'long_run_all_diseases.py'
 
-# %% Declare usual paths.
-# path of model output
-outputspath = Path('./outputs/bshe@ic.ac.uk')
-# path of real appointment usage and relevant data
-rfp = Path('./resources')
+def apply(results_folder: Path, output_folder: Path):
+    # %% Declare the name of the file that specified the scenarios used in this run.
+    # scenario_filename = 'long_run_all_diseases.py'
 
-# Find results folder (most recent run generated using that scenario_filename)
-results_folder = get_scenario_outputs(scenario_filename, outputspath)[-1]
+    # %% Declare usual paths.
+    # path of model output
+    # outputspath = Path('./outputs/bshe@ic.ac.uk')
+    # path of real appointment usage and relevant data
+    # rfp = Path('./resources')
 
-# create_pickles_locally(results_folder, compressed_file_name_prefix="long_run")  # <-- sometimes needed after download
-# log = load_pickled_dataframes(results_folder, 0, 4)['tlo.methods.healthsystem.summary']
-#
-# hsi = log['HSI_Event']
-#
-# appt_usage = hsi.join(hsi['Number_By_Appt_Type_Code'].apply(pd.Series).fillna(0.0)).drop(
-#     columns='Number_By_Appt_Type_Code')
+    # Find results folder (most recent run generated using that scenario_filename)
+    # results_folder = get_scenario_outputs(scenario_filename, outputspath)[-1]
 
-# Declare period for which the results will be generated (defined inclusively)
-TARGET_PERIOD = (Date(2015, 1, 1), Date(2019, 12, 31))
+    # create_pickles_locally(results_folder, compressed_file_name_prefix="long_run")
+    # <-- sometimes needed after download
+    # log = load_pickled_dataframes(results_folder, 0, 4)['tlo.methods.healthsystem.summary']
+    #
+    # hsi = log['HSI_Event']
+    #
+    # appt_usage = hsi.join(hsi['Number_By_Appt_Type_Code'].apply(pd.Series).fillna(0.0)).drop(
+    #     columns='Number_By_Appt_Type_Code')
 
-# Declare path for output graphs from this script
-# make_graph_file_name = lambda stub: results_folder / f"{stub}.png"
+    # Declare period for which the results will be generated (defined inclusively)
+    TARGET_PERIOD = (Date(2015, 1, 1), Date(2019, 12, 31))
 
+    # Declare path for output graphs from this script
+    # make_graph_file_name = lambda stub: results_folder / f"{stub}.png"
 
-# %% Declare helper functions
-def drop_outside_period(_df):
-    """Return a dataframe which only includes for which the date is within the limits defined by TARGET_PERIOD"""
-    return _df.drop(index=_df.index[~_df['date'].between(*TARGET_PERIOD)])
+    # %% Declare helper functions
+    def drop_outside_period(_df):
+        """Return a dataframe which only includes for which the date is within the limits defined by TARGET_PERIOD"""
+        return _df.drop(index=_df.index[~_df['date'].between(*TARGET_PERIOD)])
 
+    def formatting_hsi_df(_df):
+        """Standard formatting for the HSI_Event log."""
 
-def formatting_hsi_df(_df):
-    """Standard formatting for the HSI_Event log."""
+        # Remove entries for those HSI that did not run
+        _df = drop_outside_period(_df) \
+            .drop(_df.index[~_df.did_run]) \
+            .reset_index(drop=True) \
+            .drop(columns=['Person_ID', 'Squeeze_Factor', 'did_run', 'TREATMENT_ID', 'Facility_Level'])
 
-    # Remove entries for those HSI that did not run
-    _df = drop_outside_period(_df) \
-        .drop(_df.index[~_df.did_run]) \
-        .reset_index(drop=True) \
-        .drop(columns=['Person_ID', 'Squeeze_Factor', 'did_run', 'TREATMENT_ID', 'Facility_Level'])
+        # Unpack the dictionary in `Number_By_Appt_Type_Code`.
+        _df = _df.join(_df['Number_By_Appt_Type_Code'].apply(pd.Series).fillna(0.0)).drop(
+            columns='Number_By_Appt_Type_Code')
 
-    # Unpack the dictionary in `Number_By_Appt_Type_Code`.
-    _df = _df.join(_df['Number_By_Appt_Type_Code'].apply(pd.Series).fillna(0.0)).drop(
-        columns='Number_By_Appt_Type_Code')
+        return _df
 
-    return _df
+    # hsi = log['HSI_Event']
+    # hsi = formatting_hsi_df(hsi)
 
+    def get_counts_of_appt_type(_df):
+        # long format
+        _df = formatting_hsi_df(_df).melt(id_vars=['date', 'Facility_ID'], var_name='Appt_Type', value_name='Num')
 
-# hsi = log['HSI_Event']
-# hsi = formatting_hsi_df(hsi)
+        # creat month and year
+        _df['Year'] = _df['date'].dt.year
+        _df['Month'] = _df['date'].dt.month
 
+        # group by Year, Month, Appt_Type, Facility_ID
+        _df = _df.drop(columns='date').groupby(by=['Year', 'Month', 'Facility_ID', 'Appt_Type'])['Num'].sum()
 
-def get_counts_of_appt_type(_df):
-    # long format
-    _df = formatting_hsi_df(_df).melt(id_vars=['date', 'Facility_ID'], var_name='Appt_Type', value_name='Num')
+        return _df
 
-    # creat month and year
-    _df['Year'] = _df['date'].dt.year
-    _df['Month'] = _df['date'].dt.month
+    # hsi = log['HSI_Event']
+    # hsi = get_counts_of_appt_type(hsi)
 
-    # group by Year, Month, Appt_Type, Facility_ID
-    _df = _df.drop(columns='date').groupby(by=['Year', 'Month', 'Facility_ID', 'Appt_Type'])['Num'].sum()
+    # counts of appointments
+    counts_of_appt_type = summarize(
+        extract_results(
+            results_folder,
+            module='tlo.methods.healthsystem',
+            key='HSI_Event',
+            custom_generate_series=get_counts_of_appt_type,
+            do_scaling=True
+        ),
+        only_mean=True,  # the mean of different runs; if False, uncertainty will be considered
+        collapse_columns=True,  # squeeze dimensions of draws and runs
+    ).reset_index()
 
-    return _df
-
-
-# hsi = log['HSI_Event']
-# hsi = get_counts_of_appt_type(hsi)
-
-# counts of appointments
-counts_of_appt_type = summarize(
-    extract_results(
-        results_folder,
-        module='tlo.methods.healthsystem',
-        key='HSI_Event',
-        custom_generate_series=get_counts_of_appt_type,
-        do_scaling=True
-    ),
-    only_mean=True,  # the mean of different runs; if False, uncertainty will be considered
-    collapse_columns=True,  # squeeze dimensions of draws and runs
-).reset_index()
-
-counts_of_appt_type_2015_2019 = counts_of_appt_type.copy()
-counts_of_appt_type_2015_2019.to_csv(results_folder / 'Simulated appt usage between 2015 and 2019.csv', index=False)
+    counts_of_appt_type_2015_2019 = counts_of_appt_type.copy()
+    counts_of_appt_type_2015_2019.to_csv(
+        output_folder / 'Simulated appt usage between 2015 and 2019.csv', index=False)
