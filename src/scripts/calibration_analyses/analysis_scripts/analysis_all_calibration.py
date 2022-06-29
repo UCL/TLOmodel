ecebfed2464b29b316efd04a48161c479935ce21
Asif Tamuri <tamuri@gmail.com>
2022-06-29 21:27:04 +0000

    create html file to present plots

--- src/scripts/calibration_analyses/analysis_scripts/analysis_all_calibration.py
+++ src/scripts/calibration_analyses/analysis_scripts/analysis_all_calibration.py
@@ -1,8 +1,11 @@
+import glob
+import zipfile
 from pathlib import Path
 
 import analysis_cause_of_death_and_disability_calibrations
 import analysis_demography_calibrations
 import analysis_hsi_descriptions
+
 from tlo.analysis.utils import get_scenario_outputs
 
 
@@ -18,6 +21,25 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
 
     analysis_hsi_descriptions.apply(
         results_folder=results_folder, output_folder=output_folder, resourcefilepath=resourcefilepath)
+
+    # make html page to present results
+    html = "<html><body>"
+
+    for filename in sorted(glob.glob(str(output_folder / "*.png"))):
+        html += f"<p style='text-align: center; font-size: 130%'><a href='{filename}'>{filename}</a></p>"
+        html += f"<img style='max-width:100%; display:block; margin-left:auto; margin-right:auto' src='{filename}'/>"
+        html += "<br><br>"
+
+    with zipfile.ZipFile(output_folder / "images.zip", mode="w") as archive:
+        for filename in glob.glob("*.png"):
+            archive.write(filename)
+
+    html += """<hr><p><a href='images.zip'>images.zip</a> <a href='stdout.txt'>stdout.txt</a>
+            <a href='stderr.txt'>stderr.txt</a> <a href='task.txt'>task.txt</a></p>"""
+    html += "</body></html>"
+
+    with open(output_folder / "index.html", "w") as output_file:
+        output_file.write(html)
 
 
 if __name__ == "__main__":
