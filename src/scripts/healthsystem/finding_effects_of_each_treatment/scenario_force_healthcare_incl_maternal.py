68044e6fea95db7300ddb87710f392fc0aa673e2
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2022-06-30 17:45:21 +0100

    move and rename file and put to same population size as others

--- src/scripts/healthsystem/finding_effects_of_each_treatment/scenario_force_healthcare_incl_maternal.py
+++ src/scripts/healthsystem/finding_effects_of_each_treatment/scenario_force_healthcare_incl_maternal.py
@@ -29,7 +29,7 @@ class EffectOfEachTreatment(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2014, 12, 31)
-        self.pop_size = 100_000
+        self.pop_size = 50_000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 3  # <- repeated this many times (per draw)
