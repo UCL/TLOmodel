bba5af04be8de7e7dfa882408b22e2330bf91d89
Asif Tamuri <tamuri@gmail.com>
2022-07-03 19:28:50 +0100

    reset pop size to 50k before merging to master

--- src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
+++ src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
@@ -21,7 +21,7 @@ class LongRun(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2019, 12, 31)
-        self.pop_size = 1000  # <- recommended population size for the runs
+        self.pop_size = 50_000  # <- recommended population size for the runs
         self.number_of_draws = 1  # <- one scenario
         self.runs_per_draw = 10  # <- repeated this many times
 
