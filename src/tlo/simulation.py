579d87cee4174042469017e2f35e51aa954b3e2d
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2022-08-03 16:46:43 +0100

    (Easily) control order of events during the day (#654)
    Co-authored-by: Asif Tamuri <tamuri@gmail.com>

--- src/tlo/simulation.py
+++ src/tlo/simulation.py
@@ -12,7 +12,7 @@ import numpy as np
 
 from tlo import Date, Population, logging
 from tlo.dependencies import check_dependencies_present, topologically_sort_modules
-from tlo.events import IndividualScopeEventMixin
+from tlo.events import Event, IndividualScopeEventMixin
 from tlo.progressbar import ProgressBar
 
 logger = logging.getLogger(__name__)
@@ -254,7 +254,6 @@ class Simulation:
 
         :param event: the Event to schedule
         :param date: when the event should happen
-        :param force_over_from_healthsystem: allows an HSI event to enter the scheduler
         """
         assert date >= self.date, 'Cannot schedule events in the past'
 
@@ -262,8 +261,9 @@ class Simulation:
             'This looks like an HSI event. It should be handed to the healthsystem scheduler'
         assert (event.__str__().find('HSI_') < 0), \
             'This looks like an HSI event. It should be handed to the healthsystem scheduler'
+        assert isinstance(event, Event)
 
-        self.event_queue.schedule(event, date)
+        self.event_queue.schedule(event=event, date=date)
 
     def fire_single_event(self, event, date):
         """Fires the event once for the given date
@@ -297,7 +297,7 @@ class Simulation:
         """
         person_events = list()
 
-        for date, counter, event in self.event_queue.queue:
+        for date, _, _, event in self.event_queue.queue:
             if isinstance(event, IndividualScopeEventMixin):
                 if event.target == person_id:
                     person_events.append((date, event))
@@ -323,8 +323,7 @@ class EventQueue:
         :param event: the event to schedule
         :param date: when it should happen
         """
-
-        entry = (date, next(self.counter), event)
+        entry = (date, event.priority, next(self.counter), event)
         heapq.heappush(self.queue, entry)
 
     def next_event(self):
@@ -332,7 +331,7 @@ class EventQueue:
 
         :returns: an (event, date) pair
         """
-        date, count, event = heapq.heappop(self.queue)
+        date, _, _, event = heapq.heappop(self.queue)
         return event, date
 
     def __len__(self):
