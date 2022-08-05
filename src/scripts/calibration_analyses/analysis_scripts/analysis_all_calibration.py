28306654b98459d0020c0d24f1fb1a1c144d8a9c
Timothy Colbourn <timcolbourn@hotmail.com>
2022-08-05 11:13:49 +0100

    changing line 366 of contraception.py back to             _init_over_time_modification_by_age = 1.0 / expand_to_age_years([1.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0], _ages) (the 2.0s had been set to 0.5)

--- src/scripts/calibration_analyses/analysis_scripts/analysis_all_calibration.py
+++ src/scripts/calibration_analyses/analysis_scripts/analysis_all_calibration.py
@@ -60,11 +60,17 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
 
 
 if __name__ == "__main__":
-    outputspath = Path('./outputs/sejjtec@ucl.ac.uk')
+    outputspath = Path('./outputs/sejjtec@ucl.ac.uk/')
 
     # Find results folder (most recent run generated using that scenario_filename)
     scenario_filename = 'long_run_no_diseases-2022-08-04T195703Z'
     results_folder = get_scenario_outputs(scenario_filename, outputspath)[-1]
+
+    # Test dataset:
+    # results_folder = Path('/Users/timothycolbourn/PycharmProjects/TLOmodel/outputs/sejjtec@ucl.ac.uk/long_run_no_diseases-2022-08-04T195703Z')
+
+    # If needed -- in the case that pickles were not created remotely during batch
+    # create_pickles_locally(results_folder)
 
     # Run all the calibrations
     apply(results_folder=results_folder, output_folder=results_folder, resourcefilepath=Path('./resources'))
