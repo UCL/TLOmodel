0f1bf1b38777085e46bedeacc97821f729a15a17
tdm32 <t.mangal@imperial.ac.uk>
2022-08-15 12:21:11 +0100

    plot tb treatment delays

--- src/scripts/hiv/hiv_tb_projections_may2022/plots_for_paper.py
+++ src/scripts/hiv/hiv_tb_projections_may2022/plots_for_paper.py
@@ -1137,3 +1137,216 @@ plt.legend(bbox_to_anchor=(-0.4,0.4), loc="center left", facecolor="white")
 fig.savefig(outputspath / "Tb_treated.png")
 
 plt.show()
+
+
+# ---------------------------------- Treatment delays -------------------------------------
+
+def extract_tx_delay(results_folder: Path,
+                     module: str,
+                     key: str,
+                     column: str = None,
+                     ):
+    """Utility function to unpack results
+    edited version for utils.py
+    """
+
+    # get number of draws and numbers of runs
+    info = get_scenario_info(results_folder)
+
+    # Collect results from each draw/run
+    res = dict()
+    for draw in range(info['number_of_draws']):
+        for run in range(info['runs_per_draw']):
+
+            draw_run = (draw, run)
+
+            try:
+                df: pd.DataFrame = load_pickled_dataframes(results_folder, draw, run, module)[module][key]
+                test = df[column]
+                test2 = test.apply(pd.to_numeric, errors="coerce")
+                res[draw_run] = test2
+
+            except KeyError:
+                # Some logs could not be found - probably because this run failed.
+                res[draw_run] = None
+
+    return res
+
+
+tb_tx_delay_adult_sc0_dict = extract_tx_delay(results_folder=results0,
+                                              module="tlo.methods.tb",
+                                              key="tb_treatment_delays",
+                                              column="tbTreatmentDelayAdults")
+
+tb_tx_delay_adult_sc1_dict = extract_tx_delay(results_folder=results1,
+                                              module="tlo.methods.tb",
+                                              key="tb_treatment_delays",
+                                              column="tbTreatmentDelayAdults")
+
+tb_tx_delay_adult_sc2_dict = extract_tx_delay(results_folder=results2,
+                                              module="tlo.methods.tb",
+                                              key="tb_treatment_delays",
+                                              column="tbTreatmentDelayAdults")
+
+tb_tx_delay_adult_sc3_dict = extract_tx_delay(results_folder=results3,
+                                              module="tlo.methods.tb",
+                                              key="tb_treatment_delays",
+                                              column="tbTreatmentDelayAdults")
+
+tb_tx_delay_adult_sc4_dict = extract_tx_delay(results_folder=results4,
+                                              module="tlo.methods.tb",
+                                              key="tb_treatment_delays",
+                                              column="tbTreatmentDelayAdults")
+
+# convert dict to dataframe
+tb_tx_delay_adult_sc0 = pd.DataFrame(tb_tx_delay_adult_sc0_dict.items())
+tb_tx_delay_adult_sc1 = pd.DataFrame(tb_tx_delay_adult_sc1_dict.items())
+tb_tx_delay_adult_sc2 = pd.DataFrame(tb_tx_delay_adult_sc2_dict.items())
+tb_tx_delay_adult_sc3 = pd.DataFrame(tb_tx_delay_adult_sc3_dict.items())
+tb_tx_delay_adult_sc4 = pd.DataFrame(tb_tx_delay_adult_sc4_dict.items())
+
+# need to collapse all draws/runs together
+# set up empty list with columns for each year
+# values will be variable length lists of delays
+years = list((range(2010, 2035, 1)))
+
+
+def summarise_tx_delay(treatment_delay_df):
+    """
+    extract all treatment delays from all draws/runs
+    for each scenario and collapse into lists, with
+    one list per year
+    """
+    list_delays = [[] for i in range(25)]
+
+    # for each row of tb_tx_delay_adult_sc0 0-14 [draws, runs]:
+    for i in range(treatment_delay_df.shape[0]):
+
+        # separate each row into its arrays 0-25 [years]
+        tmp = treatment_delay_df.loc[i, 1]
+
+        # combine them into a list, with items separated from array
+        # e.g. tmp[0] has values for 2010
+        for j in range(25):
+            tmp2 = tmp[j]
+
+            list_delays[j] = [*list_delays[j], *tmp2]
+
+    return list_delays
+
+
+list_tx_delay0 = summarise_tx_delay(tb_tx_delay_adult_sc0)
+list_tx_delay1 = summarise_tx_delay(tb_tx_delay_adult_sc1)
+list_tx_delay2 = summarise_tx_delay(tb_tx_delay_adult_sc2)
+list_tx_delay3 = summarise_tx_delay(tb_tx_delay_adult_sc3)
+list_tx_delay4 = summarise_tx_delay(tb_tx_delay_adult_sc4)
+
+# replace nan with negative number (false positive)
+list_tx_delay0 = [[-99 if np.isnan(j) else j for j in i] for i in list_tx_delay0]
+list_tx_delay1 = [[-99 if np.isnan(j) else j for j in i] for i in list_tx_delay1]
+list_tx_delay2 = [[-99 if np.isnan(j) else j for j in i] for i in list_tx_delay2]
+list_tx_delay3 = [[-99 if np.isnan(j) else j for j in i] for i in list_tx_delay3]
+list_tx_delay4 = [[-99 if np.isnan(j) else j for j in i] for i in list_tx_delay4]
+
+# convert lists to df
+# todo note nans are fillers for dataframe
+delay0 = pd.DataFrame(list_tx_delay0).T
+delay0.columns = years
+# convert wide to long format
+delay0 = delay0.reset_index()
+delay0_scatter = pd.melt(delay0, id_vars='index', value_vars=years)
+delay0_scatter['value_weeks'] = round(delay0_scatter.value / 7)
+delay0_scatter.loc[delay0_scatter['value_weeks'] >= 10, 'value_weeks'] = 10
+
+delay1 = pd.DataFrame(list_tx_delay1).T
+delay1.columns = years
+# convert wide to long format
+delay1 = delay1.reset_index()
+delay1_scatter = pd.melt(delay1, id_vars='index', value_vars=years)
+delay1_scatter['value_weeks'] = round(delay1_scatter.value / 7)
+delay1_scatter.loc[delay1_scatter['value_weeks'] >= 10, 'value_weeks'] = 10
+
+delay2 = pd.DataFrame(list_tx_delay2).T
+delay2.columns = years
+# convert wide to long format
+delay2 = delay2.reset_index()
+delay2_scatter = pd.melt(delay2, id_vars='index', value_vars=years)
+delay2_scatter['value_weeks'] = round(delay2_scatter.value / 7)
+delay2_scatter.loc[delay2_scatter['value_weeks'] >= 10, 'value_weeks'] = 10
+
+delay3 = pd.DataFrame(list_tx_delay3).T
+delay3.columns = years
+# convert wide to long format
+delay3 = delay3.reset_index()
+delay3_scatter = pd.melt(delay3, id_vars='index', value_vars=years)
+delay3_scatter['value_weeks'] = round(delay3_scatter.value / 7)
+delay3_scatter.loc[delay3_scatter['value_weeks'] >= 10, 'value_weeks'] = 10
+
+delay4 = pd.DataFrame(list_tx_delay4).T
+delay4.columns = years
+# convert wide to long format
+delay4 = delay4.reset_index()
+delay4_scatter = pd.melt(delay4, id_vars='index', value_vars=years)
+delay4_scatter['value_weeks'] = round(delay4_scatter.value / 7)
+delay4_scatter.loc[delay4_scatter['value_weeks'] >= 10, 'value_weeks'] = 10
+
+
+## plots
+
+
+# scenario 1 delays 2023-2035
+# aggregate values over 10 weeks
+delay0_hist = delay0_scatter.loc[delay0_scatter['variable'] >= 2023]
+delay0_hist = delay0_hist.loc[delay0_hist['value_weeks'] >= 1]  # exclude negative values (false +ve)
+
+delay1_hist = delay1_scatter.loc[delay1_scatter['variable'] >= 2023]
+delay1_hist = delay1_hist.loc[delay1_hist['value_weeks'] >= 1]
+
+delay2_hist = delay2_scatter.loc[delay2_scatter['variable'] >= 2023]
+delay2_hist = delay2_hist.loc[delay2_hist['value_weeks'] >= 1]
+
+delay3_hist = delay3_scatter.loc[delay3_scatter['variable'] >= 2023]
+delay3_hist = delay3_hist.loc[delay3_hist['value_weeks'] >= 1]
+
+delay4_hist = delay4_scatter.loc[delay4_scatter['variable'] >= 2023]
+delay4_hist = delay4_hist.loc[delay4_hist['value_weeks'] >= 1]
+
+
+counts, bins, bars = plt.hist(delay0_hist.value_weeks, bins=range(0,11))
+
+colours = [berry[5], berry[4], berry[3], berry[2], berry[1]]
+bins = range(1, 12)
+labels = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+## plot
+fig, ax = plt.subplots()
+
+plt.hist([list(delay0_hist.value_weeks),
+                               list(delay1_hist.value_weeks),
+                               list(delay2_hist.value_weeks),
+                               list(delay3_hist.value_weeks),
+                               list(delay4_hist.value_weeks),
+          ],
+         bins=bins,
+         align='right',
+         color=colours,
+         density=True)
+
+ax.set_xticks(bins)
+ax.set_xticklabels(labels)
+
+plt.ylabel("Density")
+plt.ylim((0, 1.0))
+# plt.xlim((1, 11.0))
+
+plt.title("TB treatment delays")
+plt.legend(labels=["Scenario 0", "Scenario 1", "Scenario 2", "Scenario 3", "Scenario 4"])
+plt.show()
+
+
+
+
+
+
+
+
