b72d712c08372f5dee4b1c7c7a549c6883f59104
Dimitra Salmanidou <d.salmanidou.12@ucl.ac.uk>
2022-10-04 16:22:55 +0100

    adds two test functions for random_date

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -251,10 +251,23 @@ def test_get_person_id_to_inherit_from(rng: np.random.RandomState):
         assert population_dataframe.loc[inherit_from_id].is_alive
 
 
-def test_random_date_returns_date(rng):
+def test_random_date_returns_date_sequential(rng):
+    # start_date < end_date - sequential order
     num_iter = 20
-    for year_init, year_fin in rng.randint(1900, 2050, size=(num_iter, 2)):
-        start_date, end_date = Date(year_init, 1, 1), Date(year_fin, 1, 1) 
+    for year_init in rng.randint(1900, 2050, size=num_iter):
+        year_fin = year_init + rng.randint(1, 100)
+        start_date, end_date = Date(year_init, 1, 1), Date(year_fin, 1, 1)
         random_date = tlo.util.random_date(start_date, end_date, rng)
         assert isinstance(random_date, Date)
         assert start_date <= random_date < end_date
+
+
+def test_random_date_returns_date_nonsequential(rng):
+    # start_date >= end_date - nonsequential order
+    num_iter = 20
+    for year_init in rng.randint(1900, 2050, size=num_iter):
+        year_fin = year_init - rng.randint(0, 100)
+        start_date, end_date = Date(year_init, 1, 1), Date(year_fin, 1, 1)
+        random_date = tlo.util.random_date(start_date, end_date, rng)
+        assert isinstance(random_date, Date)
+        assert start_date == random_date
