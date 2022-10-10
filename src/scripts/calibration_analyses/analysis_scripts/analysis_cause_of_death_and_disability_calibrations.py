48465501bd4c46cc25420254ace9856707e23778
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2022-10-10 14:37:25 +0100

    Updates to the Routine Calibration Plots (#708)

--- src/scripts/calibration_analyses/analysis_scripts/analysis_cause_of_death_and_disability_calibrations.py
+++ src/scripts/calibration_analyses/analysis_scripts/analysis_cause_of_death_and_disability_calibrations.py
@@ -5,8 +5,6 @@ This uses the results of the Scenario defined in: src/scripts/long_run/long_run.
 results (change 'scenario_filename').
 """
 
-# todo - use GBD all ages numbers for some outputs (correct uncertainty bounds)
-
 from pathlib import Path
 
 import numpy as np
@@ -16,11 +14,14 @@ from matplotlib import pyplot as plt
 from tlo.analysis.utils import (
     extract_results,
     format_gbd,
+    get_color_cause_of_death_label,
+    get_scenario_outputs,
     load_pickled_dataframes,
     make_age_grp_lookup,
     make_age_grp_types,
     make_calendar_period_lookup,
     make_calendar_period_type,
+    order_of_cause_of_death_label,
     summarize,
 )
 
@@ -112,7 +113,6 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
 
         # todo - the grouping should be inside the function for the extraction like done in
         #  `analysis_effect_of_each_treatment`...?
-        # todo - factorize this.
         # todo - use colormap
 
         # %% Load the cause-of-deaths mappers and use them to populate the 'label' for gbd outputs
@@ -146,46 +146,47 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
                 by=['sex', 'age_grp', 'label']
             )[['mean', 'lower', 'upper']].sum().unstack(fill_value=0.0)
 
-        # %% Make figures of overall summaries of outcomes by cause
-        # todo - improve formatting of this one
-
+        # %% Define useful things for the plotting:
         dats = ['GBD', 'Model']
         sexes = ['F', 'M']
+
+        all_causes = sorted(list(results.index.levels[3]), key=order_of_cause_of_death_label)
+
         sexname = lambda x: 'Females' if x == 'F' else 'Males'  # noqa: E731
+        reformat_cause = lambda x: x.replace(' / ', '_')  # noqa: E731
 
-        fig, axes = plt.subplots(ncols=2, nrows=2, sharey=True, sharex=True, figsize=(40, 40))
+        # %% Make figures of overall summaries of outcomes by cause
 
-        for col, sex in enumerate(sexes):
+        for _i, sex in enumerate(sexes):
+
+            fig, axes = plt.subplots(ncols=2, nrows=1, sharey=True, sharex=True)
             for row, dat in enumerate(dats):
-                ax = axes[row][col]
+                ax = axes[row]
                 df = outcome_by_age_pt[dat].loc[sex].loc[:, pd.IndexSlice['mean']] / 1e3
+                df = df[reversed(order_of_cause_of_death_label(df.columns))]  # Reverse the standard order so that
+                #                                                               'Other' is at base
 
-                xs = np.arange(len(df.index))
-                df.plot.bar(stacked=True, ax=ax, fontsize=30)
-                ax.set_xlabel('Age Group', fontsize=40)
-                ax.set_title(f"{sexname(sex)}: {dat}", fontsize=60)
+                df.plot.bar(stacked=True, ax=ax,
+                            color=[get_color_cause_of_death_label(_label) for _label in df.columns])
+                ax.set_xlabel('Age Group')
+                ax.set_ylabel(f"{what} per year\n(thousands)")
+                ax.set_title(f"{dat}")
                 ax.get_legend().remove()
+                ax.grid(axis='y')
 
-        # add a big axis, hide frame
-        bigax = fig.add_subplot(111, frameon=False)
+            # Create figure legend and remove duplicated entries
+            handles, labels = ax.get_legend_handles_labels()
+            lgd = dict(zip(labels, handles))
+            fig.legend(reversed(lgd.values()), reversed(lgd.keys()), loc="center right", ncol=1, fontsize=6)
 
-        # hide tick and tick label of the "big axis"
-        bigax.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
-        bigax.set_ylabel(f"{what} per year (thousands)", fontsize=40)
+            fig.suptitle(f'{sexname(sex)}', fontsize=18)
+            fig.tight_layout()
 
-        fig.legend(loc="center right", fontsize=15)
-        fig.tight_layout()
-        plt.savefig(make_graph_file_name(f"{what}_{period}_StackedBars_ModelvsGBD"))
-        plt.show()
-        plt.close(fig)
+            fig.savefig(make_graph_file_name(f"{what}_{period}_{sex}_StackedBars_ModelvsGBD"))
+            fig.show()
+            plt.close(fig)
 
         # %% Plots of age-breakdown of outcomes patten for each cause:
-
-        sexes = ['F', 'M']
-        dats = ['GBD', 'Model']
-
-        all_causes = list(results.index.levels[3])
-        reformat_cause = lambda x: x.replace(' / ', '_')  # noqa: E731
 
         for cause in all_causes:
             try:
@@ -222,7 +223,7 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
 
                 fig.tight_layout()
                 plt.savefig(make_graph_file_name(
-                    f"{what}_{period}_Scatter_Plot_{reformat_cause(cause)}")
+                    f"{what}_{period}_AgeAndSexSpecificLineGraph_{reformat_cause(cause)}")
                 )
                 plt.show()
                 plt.close(fig)
@@ -232,19 +233,25 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
 
         # %% Plots comparing between model and actual across all ages and sexes:
 
-        # - TLO Model:
-        tot_outcomes_by_cause = pd.concat({
-            'Model': summarize(results.groupby(by=['label']).sum(), collapse_columns=True).unstack(),
-            'GBD': gbd.loc[gbd.period == period].groupby(['label']).sum()[['mean', 'lower', 'upper']].unstack()
-        }, axis=1)
-        # todo - for GBD, instead use all ages and all sex numbers to get correct uncertainity bounds (the addition of
-        #  the bounds for the sub-categories is not correct)
+        tot_outcomes_by_cause = pd.concat(
+            {
+                dat: outcome_by_age_pt[dat].sum(axis=0) for dat in outcome_by_age_pt.keys()
+            }, axis=1
+        )
+        # todo N.B. For GBD, should really use all ages and all sex numbers from GBD to get correct uncertainty bounds
+        #  (the addition of the bounds for the sub-categories - as done here - is not strictly correct.)
+        #  ... OR use formula to make my own explicit assumption about correlation of uncertainty in different age-grps.
 
-        select_labels = ['AIDS', 'Childhood Diarrhoea', 'Other']
+        select_labels = ['AIDS', 'Cancer (Other)', 'Measles', 'Other']
 
         fig, ax = plt.subplots()
-        xylim = tot_outcomes_by_cause.loc[('mean', slice(None))].max().max() / 1e3
-        for cause in tot_outcomes_by_cause.index.levels[1]:
+        xylim = tot_outcomes_by_cause.loc[('upper', slice(None))].max().max() / 1e3
+        line_x = np.linspace(0, xylim)
+        ax.plot(line_x, line_x, 'k--')
+        ax.fill_between(line_x, line_x*0.9, line_x*1.1, color='grey', alpha=0.5)
+        ax.set(xlim=(0, xylim), ylim=(0, xylim))
+
+        for cause in all_causes:
 
             vals = tot_outcomes_by_cause.loc[(slice(None), cause), ] / 1e3
 
@@ -259,7 +266,7 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
                 vals.at[('upper', cause), 'Model'] - y
             ]).reshape(2, 1)
 
-            ax.errorbar(x=x, y=y, xerr=xerr, yerr=yerr, label=cause)
+            ax.errorbar(x=x, y=y, xerr=xerr, yerr=yerr, label=cause, color=get_color_cause_of_death_label(cause))
 
             # add labels to selected points
             if cause in select_labels:
@@ -270,19 +277,50 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
                             ha='center'
                             )
 
-        line_x = np.linspace(0, xylim)
-        ax.plot(line_x, line_x, 'r')
-        ax.set(xlim=(0, xylim), ylim=(0, xylim))
         ax.set_xlabel('GBD (thousands)')
         ax.set_ylabel('Model (thousands)')
         ax.set_title(f'{what} per year by Cause {period}')
+        ax.legend(ncol=1, prop={'size': 8}, loc='lower right')
         plt.savefig(make_graph_file_name(f"{what}_{period}_Scatter_Plot"))
+        plt.show()
+        plt.close(fig)
+
+        # %% Assess the "coverage" of the model: i.e. the fraction of deaths/dalys that are causes that are represnted
+        # the model.
+        outcomes = outcome_by_age_pt['GBD'][("mean")]
+        fraction_causes_modelled = 1.0 - outcomes['Other'] / outcomes.sum(axis=1)
+        fig, axs = plt.subplots(nrows=1, ncols=2, sharey=True)
+        for sex, ax in zip(sexes, axs):
+            fraction_causes_modelled.loc[(sex, slice(None))].droplevel(0).plot(
+                ax=ax, color=get_color_cause_of_death_label('Other'), lw=5)
+            ax.set_ylim(0, 1.0)
+            xticks = fraction_causes_modelled.index.levels[1]
+            ax.set_xticks(range(len(xticks)))
+            ax.set_xticklabels(xticks, rotation=90)
+            ax.grid(axis='y')
+            ax.set_xlabel('Age-Group')
+            ax.set_ylabel('Fraction')
+            ax.set_title(f"{sexname(sex)}")
+        fig.suptitle(f"Fraction of {what} Represented in the Model", fontsize=16)
+        fig.tight_layout()
+        plt.savefig(make_graph_file_name(f"{what}_{period}_coverage"))
         plt.show()
         plt.close(fig)
 
     # %% Make graphs for each of Deaths and DALYS for a specific period
     make_std_graphs(what='Deaths', period='2010-2014')
-    make_std_graphs(what='DALYs', period='2010-2014')
+    # make_std_graphs(what='DALYs', period='2010-2014')  # <-- todo colormapping and order for DALYS
 
-    make_std_graphs(what='Deaths', period='2015-2019')
-    make_std_graphs(what='DALYs', period='2015-2019')
+    # make_std_graphs(what='Deaths', period='2015-2019')
+    # make_std_graphs(what='DALYs', period='2015-2019')  # <-- todo colormapping and order for DALYS
+
+
+if __name__ == "__main__":
+    outputspath = Path('./outputs/tbh03@ic.ac.uk')
+    rfp = Path('./resources')
+
+    # Find results folder (most recent run generated using that scenario_filename)
+    scenario_filename = 'long_run_all_diseases.py'
+    results_folder = get_scenario_outputs(scenario_filename, outputspath)[-1]
+
+    apply(results_folder=results_folder, output_folder=results_folder, resourcefilepath=rfp)
