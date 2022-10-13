92076375883c40a3b39ed841dfa77436a9e2ca93
Timothy Colbourn <timcolbourn@hotmail.com>
2022-10-13 12:01:36 +0100

    set interventions off in contraception.py (analysis_contraception.py script updated for intervention calibration)

--- src/tlo/methods/contraception.py
+++ src/tlo/methods/contraception.py
@@ -121,7 +121,7 @@ class Contraception(Module):
                                             )
     }
 
-    def __init__(self, name=None, resourcefilepath=None, use_healthsystem=True, use_interventions=True):
+    def __init__(self, name=None, resourcefilepath=None, use_healthsystem=True, use_interventions=False):
         super().__init__(name)
         self.resourcefilepath = resourcefilepath
 
