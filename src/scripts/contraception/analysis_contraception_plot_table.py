19fd7475f902740d1c05e1aa3c3df5d2139e79cf
Eva Janouskova <janouskova.eva.012@gmail.com>
2022-10-17 23:52:03 +0100

    co analysis: speed up; measure running time; to plot set True for all Figs; not open plots

--- src/scripts/contraception/analysis_contraception_plot_table.py
+++ src/scripts/contraception/analysis_contraception_plot_table.py
@@ -2,29 +2,33 @@ import numpy as np
 
 import fnc_analyse_contraception as a_co
 import pandas as pd
-import collections
+# import collections
+import time
+
+time_start = time.time()
 
 ################################################################################
 # TO SET:
 # for the output figures
-datestamp_without = '2022_09_10'
-datestamp_with = '2022_09_10'  # TODO: need data
-# datestamp1_log = '2022-09-14T105623'
-datestamp_without_log = '2022-09-10T181844'
-datestamp_with_log = '2022-09-10T181844'  # TODO: need data
-logFile_without = 'contraception_analysis__' + datestamp_without_log + '.log'
-logFile_with = 'contraception_analysis__' + datestamp_with_log + '.log'
+datestamp_without = '2022_09_14_v1'
+datestamp_with = '2022_09_14_v2'  # TODO: need data
+datestamp_without_log = '2022-09-14T105623'
+datestamp_with_log = '2022-09-14T105620'  # TODO: need data
+logFile_without = 'run_analysis_contraception__' + datestamp_without_log + '.log'
+logFile_with = 'run_analysis_contraception__' + datestamp_with_log + '.log'
 # which years we want to summarise for the table of use and costs
 TimePeriods_starts = [2022, 2031, 2041, 2051]
 # order of contraceptives for the table
 contraceptives_order = ['pill', 'IUD', 'injections', 'implant', 'male_condom',
                         'female_sterilization', 'other_modern']
-# ##### '2022-09-14T105623' or '2022-09-14T105623' (datestamp1 = .._v1 or ..v2)
-# outputFile = 'run_analysis_contraception__' + datestamp1_log + '.log'
+# ##### '2022-09-14T105623' or '2022-09-14T105620' (datestamp = .._v1 or ..v2)
+# logFile_xx = 'run_analysis_contraception__' + datestamp_xx_log + '.log'
 # => cons_availability = "all"
 # => start_date = Date(2010, 1, 1); end_date = Date(2099, 12, 31)
 # seed = 2022
-# and pop_size = 50000 => running time: cca 11:02-
+# and pop_size = 50000
+# => running time: 7.15e-06 s until analysis, 27.7 s loading the log, 22 mins
+# each analysis; total time: 42.5 mins
 # ##### '2022-09-10T181844'
 # outputFile = 'contraception_analysis__' + datestamp1_log + '.log'
 # => cons_availability = "all"
@@ -47,21 +51,29 @@ def fullprint(in_to_print):  # TODO: remove
         print(in_to_print)
 
 
+time_before_analysis = time.time()
+print("time until analysis:")
+print(time_before_analysis - time_start)
+
 # Use and Consumables Costs of Contraception methods Over time
 # WITHOUT interventions:
 use_without_df, percentage_use_without_df, costs_without_df =\
     a_co.analyse_contraception(
         datestamp_without, logFile_without,
         # %% Plot Contraception Use Over time?
-        False,
+        True,
         # %% Plot Contraception Use By Method Over time?
-        False,
+        True,
         # %% Plot Pregnancies Over time?
-        False,
+        True,
         # Calculate Use and Consumables Costs of Contraception methods within
         # some time periods?
         True, TimePeriods_starts
     )
+
+time_after_analysis = time.time()
+print("time of analysis performance only:")
+print(time_after_analysis - time_before_analysis)
 
 print("COSTS")
 print(costs_without_df)
@@ -76,23 +88,23 @@ fullprint(percentage_use_without_df)
 print(list(percentage_use_without_df.columns))
 
 # # Use and Consumables Costs of Contraception methods Over time
-# # WITH interventions:  # TODO: need the data
-# use_with_df, percentage_use_with_df, costs_with_df =\
-#     a_co.analyse_contraception(
-#         datestamp_with, outputFile_with,
-#         # %% Plot Contraception Use Over time?
-#         False,
-#         # %% Plot Contraception Use By Method Over time?
-#         False,
-#         # %% Plot Pregnancies Over time?
-#         False,
-#         # Calculate Use and Consumables Costs of Contraception methods within
-#         # some time periods?
-#         True, TimePeriods_starts
-#     )
-use_with_df = use_without_df
-percentage_use_with_df = percentage_use_without_df
-costs_with_df = costs_without_df
+# WITH interventions:  # TODO: need the data
+use_with_df, percentage_use_with_df, costs_with_df =\
+    a_co.analyse_contraception(
+        datestamp_with, logFile_with,
+        # %% Plot Contraception Use Over time?
+        True,
+        # %% Plot Contraception Use By Method Over time?
+        True,
+        # %% Plot Pregnancies Over time?
+        True,
+        # Calculate Use and Consumables Costs of Contraception methods within
+        # some time periods?
+        True, TimePeriods_starts
+    )
+# use_with_df = use_without_df
+# percentage_use_with_df = percentage_use_without_df
+# costs_with_df = costs_without_df
 
 # %% Plot Use and Consumables Costs of Contraception methods Over time
 # with and without intervention:
@@ -110,38 +122,12 @@ if not ('use_output' in locals() or 'use_output' in globals()):
 
 # DF.rename_axis('Contraception method', axis=1)
 
-
-def join_two_df_second_in_brackets(in_df1, in_df2):
-    """
-    Creates a data frame where are values from in_df1 followed by values from
-    in_df2 in brackets.
-    :param in_df1: a data frame - values added before the bracket,
-    :param in_df2: a data frame - values added inside the bracket.
-
-    :return: A data frame with joined values from both inputs, second of them
-        in brackets.
-    """
-    assert collections.Counter(in_df1) == collections.Counter(in_df2)  # TODO: try to break it, whether it will warn me
-    assert all(in_df1.index == in_df2.index)  # TODO: try to break it, whether it will warn me
-    data = []
-    for c in in_df1.columns:
-        data_c = []
-        for r in in_df1.index:
-            data_c.append(str(str(in_df1.loc[r, c]) + " (" +
-                              str(in_df2.loc[r, c]) + ")"))
-            # print(data_c[len(data_c) - 1])
-        data.append(data_c)
-    joined_df = pd.DataFrame(data, index=in_df1.columns, columns=in_df1.index).transpose()
-    return joined_df
-
-
-# TODO: remove the below
 use_without_val_perc_df =\
-    join_two_df_second_in_brackets(use_without_df.round(2),
-                                   percentage_use_without_df.round(2))
-
-use_with_val_perc_df = join_two_df_second_in_brackets(use_with_df.round(2),
-                                                      percentage_use_with_df.round(2))
+    use_without_df.round(2).astype(str) +\
+    " (" + percentage_use_without_df.round(2).astype(str) + ")"
+use_with_val_perc_df =\
+    use_with_df.round(2).astype(str) +\
+    " (" + percentage_use_with_df.round(2).astype(str) + ")"
 
 
 def combine_use_costs_with_without_interv(
@@ -203,3 +189,7 @@ use_costs_table_df.to_excel(writer, index_label=use_costs_table_df.columns.name)
 
 # reverse_mean_percentage_use.round(2)
 writer.save()
+
+time_end = time.time()
+print("total time incl. two analysis, all plots for both data & creating the table:")
+print(time_end - time_start)
