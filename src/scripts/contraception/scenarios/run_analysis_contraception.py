ec950530ed0c132de848b0a16052acf4606dfc5a
Timothy Colbourn <timcolbourn@hotmail.com>
2022-11-10 09:17:01 +0000

    add "tlo.methods.demography": logging.INFO  to custom logs in run_analysis_contraception.py

--- src/scripts/contraception/scenarios/run_analysis_contraception.py
+++ src/scripts/contraception/scenarios/run_analysis_contraception.py
@@ -29,7 +29,8 @@ class RunAnalysisCo(BaseScenario):
             'custom_levels': {
                 '*': logging.WARNING,
                 "tlo.methods.contraception": logging.INFO,
-                "tlo.methods.healthsystem": logging.INFO
+                "tlo.methods.healthsystem": logging.INFO,
+                "tlo.methods.demography": logging.INFO
             }
         }
 
