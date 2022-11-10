09e7f924c7a2b2a19a8145c5d36d98a1aac45c99
Timothy Colbourn <timcolbourn@hotmail.com>
2022-11-10 08:59:43 +0000

    added pop_intervention_cost and ppfp_intervention_cost parameters to contraception.py

--- src/scripts/contraception/scenarios/run_analysis_contraception.py
+++ src/scripts/contraception/scenarios/run_analysis_contraception.py
@@ -46,7 +46,7 @@ class RunAnalysisCo(BaseScenario):
 
             # - Contraception and replacement for Labour etc.
             contraception.Contraception(resourcefilepath=self.resources,
-                                        use_interventions=False,
+                                        use_interventions=True,
                                         interventions_start_date=Date(2016, 1, 1),  # if needs to be changed
                                         # the default date is Date(2023, 1, 1)
                                         use_healthsystem=True),  # <-- using HealthSystem
