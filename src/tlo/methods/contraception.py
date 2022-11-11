683ed9b2ab8c70be75cea93013409ac4b72bae83
Timothy Colbourn <timcolbourn@hotmail.com>
2022-11-11 11:58:11 +0000

    contraception interventions on, starting 2023, for long runs
    diff --git src/tlo/methods/contraception.py src/tlo/methods/contraception.py
    index d2625d0e5..21f2517d4 100644
    --- src/tlo/methods/contraception.py
    +++ src/tlo/methods/contraception.py
    @@ -131,8 +131,8 @@ class Contraception(Module):
                                                 )
         }
    
    -    def __init__(self, name=None, resourcefilepath=None, use_healthsystem=True, use_interventions=False,
    -                 interventions_start_date=Date(2016, 1, 1), run_update_contraceptive=True):
    +    def __init__(self, name=None, resourcefilepath=None, use_healthsystem=True, use_interventions=True,
    +                 interventions_start_date=Date(2023, 1, 1), run_update_contraceptive=True):
             super().__init__(name)
             self.resourcefilepath = resourcefilepath

--- src/tlo/methods/contraception.py
+++ src/tlo/methods/contraception.py
@@ -131,8 +131,8 @@ class Contraception(Module):
                                             )
     }
 
-    def __init__(self, name=None, resourcefilepath=None, use_healthsystem=True, use_interventions=False,
-                 interventions_start_date=Date(2016, 1, 1), run_update_contraceptive=True):
+    def __init__(self, name=None, resourcefilepath=None, use_healthsystem=True, use_interventions=True,
+                 interventions_start_date=Date(2023, 1, 1), run_update_contraceptive=True):
         super().__init__(name)
         self.resourcefilepath = resourcefilepath
 
