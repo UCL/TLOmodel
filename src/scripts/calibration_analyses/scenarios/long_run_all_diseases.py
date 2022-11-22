98d8851157c53c35c09fc2016e2aa2322f9c39b5
Timothy Colbourn <timcolbourn@hotmail.com>
2022-11-22 12:14:48 +0000

    health system logging off, 2099 end date for births calibration - contraception interventions OFF
    diff --git src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
    index 2cc49be6e..3292e747d 100644
    --- src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
    +++ src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
    @@ -21,7 +21,7 @@ class LongRun(BaseScenario):
             super().__init__()
             self.seed = 0
             self.start_date = Date(2010, 1, 1)
    -        self.end_date = self.start_date + pd.DateOffset(years=20)
    +        self.end_date = Date(2099, 12, 31)
             self.pop_size = 20_000
             self.number_of_draws = 1
             self.runs_per_draw = 1  # <- repeated this many times
    diff --git src/tlo/methods/contraception.py src/tlo/methods/contraception.py
    index 21f2517d4..3df4ee19a 100644
    --- src/tlo/methods/contraception.py
    +++ src/tlo/methods/contraception.py
    @@ -131,7 +131,7 @@ class Contraception(Module):
                                                 )
         }
    
    -    def __init__(self, name=None, resourcefilepath=None, use_healthsystem=True, use_interventions=True,
    +    def __init__(self, name=None, resourcefilepath=None, use_healthsystem=True, use_interventions=False,
                      interventions_start_date=Date(2023, 1, 1), run_update_contraceptive=True):
             super().__init__(name)
             self.resourcefilepath = resourcefilepath

--- src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
+++ src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
@@ -21,7 +21,7 @@ class LongRun(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = self.start_date + pd.DateOffset(years=20)
+        self.end_date = Date(2099, 12, 31)
         self.pop_size = 20_000
         self.number_of_draws = 1
         self.runs_per_draw = 1  # <- repeated this many times
