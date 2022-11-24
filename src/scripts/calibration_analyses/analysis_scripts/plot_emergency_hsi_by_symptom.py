492da3316c5ff65d6785a99408513b8e6de46396
Bingling <b.she@imperial.ac.uk>
2022-11-24 12:51:05 +0000

    counts of each group of symptoms

--- src/scripts/calibration_analyses/analysis_scripts/plot_emergency_hsi_by_symptom.py
+++ src/scripts/calibration_analyses/analysis_scripts/plot_emergency_hsi_by_symptom.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.cm import get_cmap
 
+from tlo import Date
 from tlo.analysis.utils import get_scenario_outputs, load_pickled_dataframes
 
 scenario_filename = 'long_run_all_diseases.py'
@@ -20,5 +21,15 @@ rfp = Path('./resources')
 results_folder = get_scenario_outputs(scenario_filename, outputspath)[-1]
 print(f"Results folder is: {results_folder}")
 
+TARGET_PERIOD = (Date(2015, 1, 1), Date(2019, 12, 31))
+
 # Extract results
 log = load_pickled_dataframes(results_folder)['tlo.methods.hsi_generic_first_appts']
+symptom = log['symptoms_of_person_at_emergency_hsi'].copy()
+symptom = symptom.drop(index=symptom.index[~symptom['date'].between(*TARGET_PERIOD)])
+
+# groupby and get avg annual counts of each group of symptoms
+symptom['year'] = symptom.date.dt.year.copy()
+symptom.drop(columns='date', inplace=True)
+symptom = symptom.groupby(['year', 'message']).size().reset_index(name='count')
+symptom = symptom.groupby('message')['count'].mean().reset_index()
