cde1984d1b4dd2108d96a1cd590637b1b214e6fd
Asif Tamuri <tamuri@gmail.com>
2022-11-29 13:18:43 +0000

    Update function to parse all log files and create pickles for scenario run

--- src/tlo/analysis/utils.py
+++ src/tlo/analysis/utils.py
@@ -408,41 +408,19 @@ def format_gbd(gbd_df: pd.DataFrame):
     return gbd_df
 
 
-def create_pickles_locally(scenario_output_dir, compressed_file_name_prefix=None):
+def create_pickles_locally(scenario_dir, log_name_prefix):
     """For a run from the Batch system that has not resulted in the creation of the pickles, reconstruct the pickles
      locally."""
-
-    def turn_log_into_pickles(logfile):
-        print(f"Opening {logfile}")
-        outputs = parse_log_file(logfile)
-        for key, output in outputs.items():
-            if key.startswith("tlo."):
-                print(f" - Writing {key}.pickle")
-                with open(logfile.parent / f"{key}.pickle", "wb") as f:
-                    pickle.dump(output, f)
-
-    def uncompress_and_save_logfile(compressed_file) -> Path:
-        """Uncompress and save a log file and return its path."""
-        target = compressed_file.parent / str(compressed_file.name[0:-3])
-        with open(target, "wb") as t:
-            with gzip.open(compressed_file, 'rb') as s:
-                t.write(s.read())
-        return target
-
-    draw_folders = [f for f in os.scandir(scenario_output_dir) if f.is_dir()]
+    draw_folders = [f for f in os.scandir(scenario_dir) if f.is_dir()]
     for draw_folder in draw_folders:
         run_folders = [f for f in os.scandir(draw_folder) if f.is_dir()]
         for run_folder in run_folders:
-            # Find the original log-file written by the simulation
-            if compressed_file_name_prefix is None:
-                logfile = [x for x in os.listdir(run_folder) if x.endswith('.log')][0]
-            else:
-                compressed_file_name = [
-                    x for x in os.listdir(run_folder) if x.startswith(compressed_file_name_prefix)
-                ][0]
-                logfile = uncompress_and_save_logfile(Path(run_folder) / compressed_file_name)
-
-            turn_log_into_pickles(logfile)
+            log_filename = [x for x in os.listdir(run_folder) if x.startswith(log_name_prefix)]
+            assert len(log_filename) == 1, f"Found {len(log_filename)} files starting with {log_name_prefix}"
+            logs = parse_log_file(Path(run_folder) / str(log_filename[0]))
+            # we only produce pickles on access - do them all now
+            for module, log in logs.items():
+                print(f"Found {len(log.keys())} keys in {module} log.")
 
 
 def compare_number_of_deaths(logfile: Path, resourcefilepath: Path):
