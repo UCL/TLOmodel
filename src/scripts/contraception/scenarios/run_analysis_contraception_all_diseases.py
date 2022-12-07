6bc4b72e0b21a818860b2899fed909647ca436a8
Eva Janouskova <janouskova.eva.012@gmail.com>
2022-12-07 10:31:43 +0000

    run_an._co_all_dis.: 50K pop

--- src/scripts/contraception/scenarios/run_analysis_contraception_all_diseases.py
+++ src/scripts/contraception/scenarios/run_analysis_contraception_all_diseases.py
@@ -50,7 +50,7 @@ class RunAnalysisCo(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2099, 12, 31)
-        self.pop_size = 2_000  # <- recommended population size for the runs
+        self.pop_size = 50_000  # <- recommended population size for the runs
         self.number_of_draws = 1  # <- one scenario
         self.runs_per_draw = 1  # <- repeated this many times
 
