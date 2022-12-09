ceb43484c30f73839410627a77373ebec287019b
Eva Janouskova <janouskova.eva.012@gmail.com>
2022-12-09 17:30:49 +0000

    sim: 2010-2099

--- src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
+++ src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
@@ -17,7 +17,7 @@ class RunAnalysisCo(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2011, 12, 31)
+        self.end_date = Date(2099, 12, 31)
         self.pop_size = 2_000  # <- recommended population size for the runs is 50k
         self.number_of_draws = 1  # <- one scenario
         self.runs_per_draw = 1  # <- repeated this many times
