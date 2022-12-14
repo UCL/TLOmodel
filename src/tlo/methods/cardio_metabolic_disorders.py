e46dd09fe5b534c154a79e9db96c3e3e4cd30192
Bingling <b.she@imperial.ac.uk>
2022-12-14 16:04:50 +0000

    Revert "change emergency symptom properties"
    This reverts commit f129fcd7bf834f3359d83122874d4a2bd32aee5d.

--- src/tlo/methods/cardio_metabolic_disorders.py
+++ src/tlo/methods/cardio_metabolic_disorders.py
@@ -339,12 +339,9 @@ class CardioMetabolicDisorders(Module):
         # Register symptoms from events and make them emergencies
         for event in self.events:
             self.sim.modules['SymptomManager'].register_symptom(
-                Symptom(name=f'{event}_damage',
-                        odds_ratio_health_seeking_in_adults=0.2,  # low probability to seek health care
-                        prob_seeks_emergency_appt_in_adults=1,    # if seeking care, then 100% emergency health care
-                        odds_ratio_health_seeking_in_children=0.0,  # this symptom is not for children
-                        prob_seeks_emergency_appt_in_children=0.0,  # this symptom is not for children
-                        ),
+                Symptom.emergency(
+                    name=f'{event}_damage', which='adults'
+                ),
             )
 
     def initialise_population(self, population):
