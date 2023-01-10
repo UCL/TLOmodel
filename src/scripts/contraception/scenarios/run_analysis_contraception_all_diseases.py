2787ca905afadd8510f2e0d645b1db650f96ebda
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-01-10 17:18:24 +0000

    analy_co_all_dis scenario: 50K pop, with interv, (2010-2099)

--- src/scripts/contraception/scenarios/run_analysis_contraception_all_diseases.py
+++ src/scripts/contraception/scenarios/run_analysis_contraception_all_diseases.py
@@ -19,7 +19,7 @@ class RunAnalysisCo(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2099, 12, 31)
-        self.pop_size = 1_000  # <- recommended population size for the runs
+        self.pop_size = 50_000  # <- recommended population size for the runs
         self.number_of_draws = 1  # <- one scenario
         self.runs_per_draw = 1  # <- repeated this many times
 
@@ -37,7 +37,10 @@ class RunAnalysisCo(BaseScenario):
     def modules(self):
         return fullmodel(
             resourcefilepath=self.resources,
-            module_kwargs={"Healthsystem": {"cons_availability": "all"}}
+            module_kwargs={
+                "Healthsystem": {"cons_availability": "all"},
+                "Contraception": {"use_interventions": True}
+            }
         )
 
     def draw_parameters(self, draw_number, rng):
