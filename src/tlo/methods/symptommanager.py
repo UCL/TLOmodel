fd4329ca874e36c9092b80c4309e042119b72d4c
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2023-01-16 20:38:51 +0000

    linting

--- src/tlo/methods/symptommanager.py
+++ src/tlo/methods/symptommanager.py
@@ -40,8 +40,8 @@ class Symptom:
       (`odds_ratio_health_seeking_in_`)
      * the probability that emergency care is sought, if care is sought at all (`prob_seeks_emergency_appt_in_`).
 
-    The default behaviour is for a symptom that causes healthcare-seeking for non-emergency care with the same probability
-    as the "average symptom".
+    The default behaviour is for a symptom that causes healthcare-seeking for non-emergency care with the same
+    probability as the "average symptom".
 
     The in-built method `emergency_symptom_with_automatic_healthcareseeking` produces another common type of symptom,
     which gives a very high probability that emergency care is sought.
