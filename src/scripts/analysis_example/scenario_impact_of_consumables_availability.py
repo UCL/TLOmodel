b83379368714c16d1671d7ead43d10719caf0f9b
Matt Graham <matthew.m.graham@gmail.com>
2023-01-23 23:06:49 +0000

    Small spacing tweaks and add warning filter

--- src/scripts/analysis_example/scenario_impact_of_consumables_availability.py
+++ src/scripts/analysis_example/scenario_impact_of_consumables_availability.py
@@ -1,14 +1,16 @@
 """
-This file defines a batch run of a large population for a long time with all disease modules and full use of HSIs
-It's used for calibrations (demographic patterns, health burdens and healthsytstem usage)
+This file defines an example scenario for analysing the impact of consumable availability.
 
-Run on the batch system using:
-```tlo batch-submit src/scripts/analysis_example/scenario_impact_of_consumables_availability.py```
+It can be submitted on Azure Batch by running:
+
+    tlo batch-submit src/scripts/analysis_example/scenario_impact_of_consumables_availability.py
 
 or locally using:
-    ```tlo scenario-run src/scripts/analysis_example/scenario_impact_of_consumables_availability.py```
 
+    tlo scenario-run src/scripts/analysis_example/scenario_impact_of_consumables_availability.py
 """
+import warnings
+
 import pandas as pd
 
 from tlo import Date, logging
@@ -16,7 +18,14 @@ from tlo.methods.fullmodel import fullmodel
 from tlo.scenario import BaseScenario
 
 
+# Ignore warnings to avoid cluttering output from simulation - generally you do not
+# need (and generally shouldn't) do this as warnings can contain useful information but
+# we will do so here for the purposes of this example to keep things simple.
+warnings.simplefilter("ignore", (UserWarning, RuntimeWarning))
+
+
 class ImpactOfConsumablesAvailability(BaseScenario):
+
     def __init__(self):
         super().__init__()
         self.seed = 0
@@ -44,11 +53,12 @@ class ImpactOfConsumablesAvailability(BaseScenario):
         return {
             'HealthSystem': {
                 'cons_availability': ['default', 'all'][draw_number]
-                }
+            }
         }
 
 
 if __name__ == '__main__':
+
     from tlo.cli import scenario_run
 
     scenario_run([__file__])
