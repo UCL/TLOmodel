1b03d2de2f3fb111e8442734328bb5d1b6cefaec
Bingling <b.she@imperial.ac.uk>
2023-01-31 17:20:23 +0000

    fix inappropriate format

--- src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
+++ src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
@@ -47,7 +47,7 @@ class LongRun(BaseScenario):
     def draw_parameters(self, draw_number, rng):
         return {
             'Stunting': {
-                'prob_stunting_diagnosed_at_generic_appt': [0.01][draw_number]
+                'prob_stunting_diagnosed_at_generic_appt': 0.01
             }
         }
 
