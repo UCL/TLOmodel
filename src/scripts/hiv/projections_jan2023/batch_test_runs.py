e903d463fdac2705d7c785bf58de7c6024f07aff
tdm32 <t.mangal@imperial.ac.uk>
2023-02-03 14:12:26 +0000

    flake8

--- src/scripts/hiv/projections_jan2023/batch_test_runs.py
+++ src/scripts/hiv/projections_jan2023/batch_test_runs.py
@@ -11,7 +11,7 @@ or execute a single run:
 tlo scenario-run src/scripts/hiv/deviance_for_calibration/calibration_script.py --draw 1 0
 
 Run on the batch system using:
-tlo batch-submit src/scripts/hiv/deviance_for_calibration/calibration_script.py
+tlo batch-submit src/scripts/hiv/projections_jan2023/batch_test_runs.py
 
 Display information about a job:
 tlo batch-job tlo_q1_demo-123 --tasks
