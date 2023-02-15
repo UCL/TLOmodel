150436017db882bf7a8fd02cdb69bae001c0aa4d
tdm32 <t.mangal@imperial.ac.uk>
2023-02-15 10:55:01 +0000

    set up calibration_script.py

--- src/scripts/hiv/projections_jan2023/calibration_script.py
+++ src/scripts/hiv/projections_jan2023/calibration_script.py
@@ -5,13 +5,13 @@ check the batch configuration gets generated without error:
 tlo scenario-run --draw-only src/scripts/hiv/projections_jan2023/calibration_script.py
 
 Test the scenario starts running without problems:
-tlo scenario-run src/scripts/hiv/deviance_for_calibration/calibration_script.py
+tlo scenario-run src/scripts/hiv/projections_jan2023/calibration_script.py
 
 or execute a single run:
 tlo scenario-run src/scripts/hiv/deviance_for_calibration/calibration_script.py --draw 1 0
 
 Run on the batch system using:
-tlo batch-submit src/scripts/hiv/deviance_for_calibration/calibration_script.py
+tlo batch-submit src/scripts/hiv/projections_jan2023/calibration_script.py
 
 Display information about a job:
 tlo batch-job tlo_q1_demo-123 --tasks
