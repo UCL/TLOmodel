7329c9c7a79babc9459f296d123c055247481117
Bingling <b.she@imperial.ac.uk>
2023-02-20 11:08:08 +0000

    scale run on more values of draw_parameters

--- src/scripts/hsi_calibrations/depression_scale_run.py
+++ src/scripts/hsi_calibrations/depression_scale_run.py
@@ -23,7 +23,7 @@ class LongRun(BaseScenario):
         self.start_date = Date(2010, 1, 1)
         self.end_date = self.start_date + pd.DateOffset(years=10)
         self.pop_size = 20_000
-        self.number_of_draws = 3
+        self.number_of_draws = 6
         self.runs_per_draw = 10
 
     def log_configuration(self):
@@ -47,9 +47,10 @@ class LongRun(BaseScenario):
     def draw_parameters(self, draw_number, rng):
         return {
             'Depression': {
-                'pr_assessed_for_depression_in_generic_appt_level1': [0.5, 0.1, 0.1][draw_number],
-                'pr_assessed_for_depression_for_perinatal_female': [1.0, 0.2, 0.2][draw_number],
-                'sensitivity_of_assessment_of_depression': [0.6, 0.6, 0.5][draw_number]
+                'pr_assessed_for_depression_in_generic_appt_level1': [0.1, 0.1, 0.1, 0.1, 0.1, 0.1][draw_number],
+                'pr_assessed_for_depression_for_perinatal_female': [0.1, 0.1, 0.1, 0.1, 0.1, 0.1][draw_number],
+                'init_pr_ever_diagnosed_depression': [0.2, 0.2, 0.2, 0.1, 0.1, 0.1][draw_number],
+                'sensitivity_of_assessment_of_depression': [0.75, 0.6, 0.5, 0.75, 0.6, 0.5][draw_number]
             }
         }
 
