b7963c3f0956e1669ecf6fe5c004ec72e87b8c0e
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-02-22 23:55:19 +0000

    run_analysis_co_no_dis: test for 1K pop till 2099, co. logging INFO & use_interventions=True

--- src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
+++ src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
@@ -21,7 +21,7 @@ class RunAnalysisCo(BaseScenario):
         super().__init__(
             seed=0,
             start_date=Date(2010, 1, 1),
-            end_date=Date(2050, 12, 31),
+            end_date=Date(2099, 12, 31),
             initial_population_size=1_000,  # selected size for the Tim C at al. paper: 250K
             number_of_draws=1,  # <- one scenario
             runs_per_draw=1,  # <- repeated this many times
