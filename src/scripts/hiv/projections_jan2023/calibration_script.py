b6e3d5ee5e5741a296beb500ee8827aa0ca8bbbb
tdm32 <t.mangal@imperial.ac.uk>
2023-02-22 22:10:10 +0000

    set up full calibration run

--- src/scripts/hiv/projections_jan2023/calibration_script.py
+++ src/scripts/hiv/projections_jan2023/calibration_script.py
@@ -34,10 +34,8 @@ import os
 # Ignore warnings to avoid cluttering output from simulation
 warnings.simplefilter("ignore", (UserWarning, RuntimeWarning))
 
-# number_of_draws = 30
-# runs_per_draw = 3
-number_of_draws = 1
-runs_per_draw = 1
+number_of_draws = 30
+runs_per_draw = 3
 
 
 class TestScenario(BaseScenario):
@@ -48,7 +46,7 @@ class TestScenario(BaseScenario):
             seed=0,
             start_date=Date(2010, 1, 1),
             end_date=Date(2023, 1, 1),
-            initial_population_size=5000,
+            initial_population_size=100000,
             number_of_draws=number_of_draws,
             runs_per_draw=runs_per_draw,
         )
