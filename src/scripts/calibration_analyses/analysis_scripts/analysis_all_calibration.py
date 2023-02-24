5cb5d88794669c3686b7ad54a26f4f11717b050f
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-02-24 15:14:40 +0000

    analysis_all_calibration: back to original

--- src/scripts/calibration_analyses/analysis_scripts/analysis_all_calibration.py
+++ src/scripts/calibration_analyses/analysis_scripts/analysis_all_calibration.py
@@ -12,8 +12,7 @@ from scripts.calibration_analyses.analysis_scripts import (
     analysis_compare_appt_usage_real_and_simulation,
     plot_appt_use_by_hsi,
 )
-
-# from tlo.analysis.utils import get_scenario_outputs
+from tlo.analysis.utils import get_scenario_outputs
 
 
 def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = None):
@@ -61,20 +60,11 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
 
 
 if __name__ == "__main__":
-    outputspath = Path('./outputs/sejjtec@ucl.ac.uk/')
+    outputspath = Path('./outputs/tbh03@ic.ac.uk')
 
     # Find results folder (most recent run generated using that scenario_filename)
-    scenario_filename = 'run_analysis_contraception_all_diseases.py'
-    # results_folder = get_scenario_outputs(scenario_filename, outputspath)[-1]
-
-    # Test dataset:
-    results_folder = Path(
-        '/Users/timothycolbourn/PycharmProjects/TLOmodel/outputs/sejjtec@ucl.ac.uk/' +
-        'run_analysis_contraception_all_diseases-2022-12-02T162502Z'
-    )
-
-    # If needed -- in the case that pickles were not created remotely during batch
-    # create_pickles_locally(results_folder)
+    scenario_filename = 'long_run_all_diseases.py'
+    results_folder = get_scenario_outputs(scenario_filename, outputspath)[-1]
 
     # Run all the calibrations
     apply(results_folder=results_folder, output_folder=results_folder, resourcefilepath=Path('./resources'))
