e596f0d22d30870b2874a4d018823566b864595b
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-02-25 00:23:04 +0000

    analysis_impact_of_cons_avail & stunting_analysis_plots: back to original

--- src/scripts/healthsystem/impact_of_cons_availability/analysis_impact_of_consumables_availability.py
+++ src/scripts/healthsystem/impact_of_cons_availability/analysis_impact_of_consumables_availability.py
@@ -19,7 +19,7 @@ from tlo.analysis.utils import (
     summarize,
 )
 
-outputspath = Path('./outputs/sejjtec@ucl.ac.uk')
+outputspath = Path('./outputs/tbh03@ic.ac.uk')
 
 
 # %% Gathering basic information
