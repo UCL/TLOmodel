30e30a68bf46afeb9f002ca8daff94a408cee956
tdm32 <t.mangal@imperial.ac.uk>
2023-03-02 16:16:18 +0000

    set up parameter sheet for scaled parameters in resourcefile_tb

--- src/tlo/methods/tb.py
+++ src/tlo/methods/tb.py
@@ -381,6 +381,12 @@ class Tb(Module):
             Types.STRING,
             "sub-set of scenarios used for sensitivity analysis"
         ),
+        # ------------------ sacle-up parameters for scenario analysis ------------------ #
+        "scaleup_parameters": Parameter(
+            Types.DATA_FRAME,
+            "list of parameters and values changed in scenario analysis",
+        ),
+
     }
 
     def read_parameters(self, data_folder):
@@ -410,6 +416,8 @@ class Tb(Module):
         p["tb_high_risk_distr"] = workbook["all_districts"]
 
         p["ipt_coverage"] = workbook["ipt_coverage"]
+
+        p["scaleup_parameters"] = workbook["scaleup_parameters"]
 
         self.district_list = (
             self.sim.modules["Demography"]
@@ -1404,17 +1412,11 @@ class ScenarioSetupEvent(RegularEvent, PopulationScopeEventMixin):
     """ This event exists to change parameters or functions
     depending on the scenario for projections which has been set
     * scenario 0 is the default which uses baseline parameters
-    * scenario 1 optimistic, achieving all program targets
-    * scenario 2 optimistic with program constraints
-    * scenario 3 optimistic with program constraints and additional measures to reduce incidence
-    * scenario 4 optimistic and additional measures to reduce incidence
+    * scenario 1 achieves all program targets with consumables constraints
+    * scenario 2 achieves all program targets without consumables constraints
 
     It only occurs once at param: scenario_start_date,
     called by initialise_simulation
-
-    the sensitivity analysis is determined by parameter scenario_SI which redacts one intervention at a time
-    using parameter values "a"-"i"
-    currently this is only called for scenario 4 runs, otherwise the default scenario_SI value is "z"
     """
 
     def __init__(self, module):
@@ -1424,6 +1426,7 @@ class ScenarioSetupEvent(RegularEvent, PopulationScopeEventMixin):
 
         p = self.module.parameters
         scenario = p["scenario"]
+        scaled_params = p["scaleup_parameters"]
 
         logger.debug(
             key="message", data=f"ScenarioSetupEvent: scenario {scenario}"
@@ -1435,61 +1438,65 @@ class ScenarioSetupEvent(RegularEvent, PopulationScopeEventMixin):
 
         # scenario 1 or 2 scale-up all HIV/TB program activities
         if scenario > 0:
-
             # HIV
             # reduce risk of HIV - applies to whole adult population
-            self.sim.modules["Hiv"].parameters["beta"] = self.sim.modules["Hiv"].parameters["beta"] * 0.9
+            self.sim.modules["Hiv"].parameters["beta"] = self.sim.modules["Hiv"].parameters["beta"] * scaled_params[
+                "hiv_beta"]
 
             # increase PrEP coverage for FSW after HIV test
-            self.sim.modules["Hiv"].parameters["prob_prep_for_fsw_after_hiv_test"] = 0.5
+            self.sim.modules["Hiv"].parameters["prob_prep_for_fsw_after_hiv_test"] = scaled_params[
+                "prob_prep_for_fsw_after_hiv_test"]
 
             # prep poll for AGYW - target to the highest risk
             # increase retention to 75% for FSW and AGYW
-            self.sim.modules["Hiv"].parameters["prob_prep_for_agyw"] = 0.1
-            self.sim.modules["Hiv"].parameters["probability_of_being_retained_on_prep_every_3_months"] = 0.75
+            self.sim.modules["Hiv"].parameters["prob_prep_for_agyw"] = scaled_params["prob_prep_for_agyw"]
+            self.sim.modules["Hiv"].parameters["probability_of_being_retained_on_prep_every_3_months"] = scaled_params[
+                "probability_of_being_retained_on_prep_every_3_months"]
 
             # increase probability of VMMC after hiv test
-            self.sim.modules["Hiv"].parameters["prob_circ_after_hiv_test"] = 0.25
+            self.sim.modules["Hiv"].parameters["prob_circ_after_hiv_test"] = scaled_params["prob_circ_after_hiv_test"]
 
             # increase testing/diagnosis rates, default 2020 0.03/0.25 -> 93% dx
-            self.sim.modules["Hiv"].parameters["hiv_testing_rates"]["annual_testing_rate_children"] = 0.1
-            self.sim.modules["Hiv"].parameters["hiv_testing_rates"]["annual_testing_rate_adults"] = 0.3
+            self.sim.modules["Hiv"].parameters["hiv_testing_rates"]["annual_testing_rate_children"] = scaled_params[
+                "annual_testing_rate_children"]
+            self.sim.modules["Hiv"].parameters["hiv_testing_rates"]["annual_testing_rate_adults"] = scaled_params[
+                "annual_testing_rate_adults"]
 
             # ANC testing - value for mothers and infants testing
-            self.sim.modules["Hiv"].parameters["prob_anc_test_at_delivery"] = 0.95
+            self.sim.modules["Hiv"].parameters["prob_hiv_test_at_anc_or_delivery"] = scaled_params[
+                "prob_hiv_test_at_anc_or_delivery"]
+            self.sim.modules["Hiv"].parameters["prob_hiv_test_for_newborn_infant"] = scaled_params[
+                "prob_hiv_test_for_newborn_infant"]
 
             # prob ART start if dx, this is already 95% at 2020
-            # self.sim.modules["Hiv"].parameters["prob_start_art_after_hiv_test"] = 0.95
+            # self.sim.modules["Hiv"].parameters["prob_start_art_after_hiv_test"] = scaled_params[
+            #   "prob_start_art_after_hiv_test"]
 
             # viral suppression rates
             # adults already at 95% by 2020
             # change all column values
-            self.sim.modules["Hiv"].parameters["prob_start_art_or_vs"]["virally_suppressed_on_art"] = 95
+            self.sim.modules["Hiv"].parameters["prob_start_art_or_vs"]["virally_suppressed_on_art"] = scaled_params[
+                "virally_suppressed_on_art"]
 
             # TB
             # use NTP treatment rates
-            self.sim.modules["Tb"].parameters["rate_testing_active_tb"]["treatment_coverage"] = 90
+            self.sim.modules["Tb"].parameters["rate_testing_active_tb"]["treatment_coverage"] = scaled_params[
+                "tb_treatment_coverage"]
 
             # increase tb treatment success rates
-            self.sim.modules["Tb"].parameters["prob_tx_success_ds"] = 0.9
-            self.sim.modules["Tb"].parameters["prob_tx_success_mdr"] = 0.9
-            self.sim.modules["Tb"].parameters["prob_tx_success_0_4"] = 0.9
-            self.sim.modules["Tb"].parameters["prob_tx_success_5_14"] = 0.9
-            self.sim.modules["Tb"].parameters["prob_tx_success_shorter"] = 0.9
+            self.sim.modules["Tb"].parameters["prob_tx_success_ds"] = scaled_params["tb_prob_tx_success_ds"]
+            self.sim.modules["Tb"].parameters["prob_tx_success_mdr"] = scaled_params["tb_prob_tx_success_mdr"]
+            self.sim.modules["Tb"].parameters["prob_tx_success_0_4"] = scaled_params["tb_prob_tx_success_0_4"]
+            self.sim.modules["Tb"].parameters["prob_tx_success_5_14"] = scaled_params["tb_prob_tx_success_5_14"]
+            self.sim.modules["Tb"].parameters["prob_tx_success_shorter"] = scaled_params["tb_prob_tx_success_shorter"]
 
             # change first-line testing for TB to xpert
-            p["first_line_test"] = "xpert"
-            p["second_line_test"] = "sputum"
-
-            # change IPT eligibility for TB contacts to all years
-            p["age_eligibility_for_ipt"] = 100
+            p["first_line_test"] = scaled_params["first_line_test"]
+            p["second_line_test"] = scaled_params["second_line_test"]
 
             # increase coverage of IPT
-            p["ipt_coverage"]["coverage_plhiv"] = 0.6
-            p["ipt_coverage"]["coverage_paediatric"] = 80  # this will apply to contacts of all ages
-
-            # retention on IPT (PLHIV)
-            self.sim.modules["Tb"].parameters["prob_retained_ipt_6_months"] = 0.99
+            p["ipt_coverage"]["coverage_plhiv"] = scaled_params["ipt_coverage_plhiv"]
+            p["ipt_coverage"]["coverage_paediatric"] = scaled_params["ipt_coverage_paediatric"]
 
         # remove consumables constraints, all cons available
         if scenario == 2:
