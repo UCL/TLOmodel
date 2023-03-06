b0e702a53df09d37ff26f63f223d8806609a8e5f
Bingling <b.she@imperial.ac.uk>
2023-03-06 09:14:22 +0000

    schedule follow-up appts, not reschedule

--- src/tlo/methods/hiv.py
+++ src/tlo/methods/hiv.py
@@ -2222,14 +2222,14 @@ class HSI_Hiv_Circ(HSI_Event, IndividualScopeEventMixin):
                 # Schedule follow-up appts
                 # schedule first follow-up appt, 3 days from procedure;
                 self.sim.modules["HealthSystem"].schedule_hsi_event(
-                    self,
+                    HSI_Hiv_Circ(person_id=person_id, module=self.module),
                     topen=self.sim.date + DateOffset(days=3),
                     tclose=None,
                     priority=0,
                 )
                 # schedule second follow-up appt, 7 days from procedure;
                 self.sim.modules["HealthSystem"].schedule_hsi_event(
-                    self,
+                    HSI_Hiv_Circ(person_id=person_id, module=self.module),
                     topen=self.sim.date + DateOffset(days=7),
                     tclose=None,
                     priority=0,
