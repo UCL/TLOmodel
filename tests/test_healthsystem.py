10535d8a8424885f3f81639e0d971a95ad0dddae
Matt Graham <matthew.m.graham@gmail.com>
2023-03-31 17:50:20 +0100

    Change use of deprecated `iteritems` method to `items` (#904)

--- tests/test_healthsystem.py
+++ tests/test_healthsystem.py
@@ -834,7 +834,7 @@ def test_two_loggers_in_healthsystem(seed, tmpdir):
     # Check the count of appointment type (total) matches the count split by level
     counts_of_appts_by_level = pd.concat(
         {idx: pd.DataFrame.from_dict(mydict)
-         for idx, mydict in summary_hsi_event['Number_By_Appt_Type_Code_And_Level'].iteritems()
+         for idx, mydict in summary_hsi_event['Number_By_Appt_Type_Code_And_Level'].items()
          }).unstack().fillna(0.0).astype(int)
 
     assert summary_hsi_event['Number_By_Appt_Type_Code'].apply(pd.Series).sum().to_dict() == \
