e8c37bd3211bec16a976b9799662a6c489005559
Matt Graham <matthew.m.graham@gmail.com>
2023-04-16 16:39:33 +0100

    Avoid summing non-numeric columns (#941)

--- src/tlo/methods/rti.py
+++ src/tlo/methods/rti.py
@@ -2019,7 +2019,7 @@ class RTI(Module):
 
         daly_change = selected_for_rti_inj.applymap(
             lambda code: self.ASSIGN_INJURIES_AND_DALY_CHANGES[code][1]
-        ).sum(axis=1)
+        ).sum(axis=1, numeric_only=True)
         df.loc[injured_index, 'rt_disability'] += daly_change
 
         # Store the true sum of DALY weights in the df
