1ea5fa573124d6575fcb61fabd50bbc57629f886
Dimitra Salmanidou <40604877+dimitrasal@users.noreply.github.com>
2023-04-19 16:25:37 +0100

    Reduce clumping of HSI events on first day of month (#754)
    * "scenario run to investigate HSI clumping"
    
    * "analyse and fix HSI clumping"
    
    * "Analysis by date"
    
    * "modify analysis by date script to take results folder as argument"
    
    * "styling corrections"
    
    * "styling corrections"
    
    * "prefix change"
    
    * "prefix change"
    
    * "styling fix"
    
    * "changes import order"
    
    * "removes test run file"
    
    * removes comments
    
    Co-authored-by: Matt Graham <matthew.m.graham@gmail.com>
    
    * removes comments
    
    Co-authored-by: Matt Graham <matthew.m.graham@gmail.com>
    
    * removes comments
    
    Co-authored-by: Matt Graham <matthew.m.graham@gmail.com>
    
    * uncomments parts of the code
    
    Co-authored-by: Matt Graham <matthew.m.graham@gmail.com>
    
    * uncomments parts of the code
    
    Co-authored-by: Matt Graham <matthew.m.graham@gmail.com>
    
    * styling change
    
    ---------
    
    Co-authored-by: Matt Graham <matthew.m.graham@gmail.com>

--- src/tlo/methods/malaria.py
+++ src/tlo/methods/malaria.py
@@ -15,6 +15,7 @@ from tlo.methods.causes import Cause
 from tlo.methods.dxmanager import DxTest
 from tlo.methods.healthsystem import HSI_Event
 from tlo.methods.symptommanager import Symptom
+from tlo.util import random_date
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -670,7 +671,6 @@ class MalariaScheduleTesting(RegularEvent, PopulationScopeEventMixin):
 
     def apply(self, population):
         df = population.props
-        now = self.sim.date
         p = self.module.parameters
 
         # select people to go for testing (and subsequent tx)
@@ -686,7 +686,8 @@ class MalariaScheduleTesting(RegularEvent, PopulationScopeEventMixin):
             self.sim.modules["HealthSystem"].schedule_hsi_event(
                 HSI_Malaria_rdt(self.module, person_id=person_index),
                 priority=1,
-                topen=now, tclose=None
+                topen=random_date(self.sim.date, self.sim.date + self.frequency, self.module.rng),
+                tclose=None
             )
 
 
