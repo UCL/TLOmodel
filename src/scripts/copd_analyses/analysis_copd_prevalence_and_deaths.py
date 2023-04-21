4a9338c56095ce93773fd066f9952b2c0c8bad58
mnjowe <emmanuelmnjowe@gmail.com>
2023-04-21 14:14:34 +0200

    sorting imports

--- src/scripts/copd_analyses/analysis_copd_prevalence_and_deaths.py
+++ src/scripts/copd_analyses/analysis_copd_prevalence_and_deaths.py
@@ -24,7 +24,6 @@ from tlo.methods import (
     symptommanager,
 )
 
-
 # todo @Emmmanuel....
 #  First thing will be to run the model with Copd registered for 30 years. Then,
 #  for (1) In the test file I've written an example of how to get this out of the log. We want stacked bars (that sum
