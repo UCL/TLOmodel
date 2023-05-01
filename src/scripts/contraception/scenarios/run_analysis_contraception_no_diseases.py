5e6bdcf773163f8ba685ad937000be9f1ea102a0
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-05-01 14:30:46 +0100

    run_analysis_co_no_dis: update script description

--- src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
+++ src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
@@ -23,17 +23,18 @@ logging above warning level:
     - DEBUG if tabel from analysis_contraception_plot_table required.
 * demography: INFO.
 NB. For analysis_all_calibration this is enough only if analysis_hsi_descriptions are not required, and the analysis
-needs to be changed accordingly to run properly.
+needs to be changed accordingly to run properly. We use an adjusted analysis_all_calibration script, stored in the
+EvaJ/contraception_2023-02_inclPR807/AnalysisAllCalib_Contraception branch.
 
 
 CONTRACEPTION PAPER (Tim C et al. 2023):
 ---------------------------------------
-- we decided on 1 draw & 1 run/per draw with 250K initial_population_size
+- 1 draw & 1 run/per draw with 250K initial_population_size
 - use_interventions=False/True according to what we need
 - for analysis_all_calibration with analysis_hsi_descriptions excluded (Fig 2):
-    2010-2099 with contraception logging at level INFO (a job to simulate so many years with DEBUG logging fails)
+    2010-2099 with contraception logging at the level INFO (a job to simulate that many years with DEBUG logging fails)
 - for analysis_contraception_plot_table (Fig 3, Fig 4, Tab 4, and Fig A6.1):
-    2010-2050 with contraception logging at level DEBUG
+    2010-2050 with contraception logging at the level DEBUG
 """
 
 from tlo import Date, logging
