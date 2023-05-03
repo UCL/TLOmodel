2cd87ec12ef7aecb437d1bf13774b9561735e6a4
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-05-03 14:10:18 +0100

    fnc_analyse_co: datestamps update

--- src/scripts/contraception/fnc_analyse_contraception.py
+++ src/scripts/contraception/fnc_analyse_contraception.py
@@ -256,6 +256,9 @@ def analyse_contraception(in_id: str, in_log_file: str, in_suffix: str,
             elif in_log_file == 'run_analysis_contraception_no_diseases__2023-04-26T142508.log':
                 # with interv, 250K, till 2050; final costs update EHP & OHT + pregn test to initiate co
                 results_folder_name = 'run_analysis_contraception_no_diseases-2023-04-26T142300Z'
+            elif in_log_file == 'run_analysis_contraception_no_diseases__2023-05-01T154930.log':
+                # with interv, 250K till 2050; final costs update EHP & OHT + pregn test + interv costs
+                results_folder_name = 'run_analysis_contraception_no_diseases-2023-05-01T154641Z'
             elif in_log_file == 'run_analysis_contraception_no_diseases__2023-04-26T141435.log':
                 # without interv, 2K till 2099, final costs update EHP & OHT + pregn test to initiate co
                 results_folder_name = 'run_analysis_contraception_no_diseases-2023-04-26T141159Z'
