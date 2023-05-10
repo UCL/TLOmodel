00f724640981a5c001010e8370dc819293e79cd1
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2023-05-10 11:30:45 +0100

    make the symptom list into a list

--- src/tlo/methods/malaria.py
+++ src/tlo/methods/malaria.py
@@ -943,7 +943,7 @@ class MalariaUpdateEvent(RegularEvent, PopulationScopeEventMixin):
             & (df.ma_date_symptoms == now)].index
 
         # assign clinical symptoms
-        symptom_list = {"fever", "headache", "vomiting", "stomachache"}
+        symptom_list = ["fever", "headache", "vomiting", "stomachache"]
         self.sim.modules["SymptomManager"].change_symptom(
             person_id=new_symptomatic_clinical,
             symptom_string=symptom_list,
