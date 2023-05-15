b59f5eba64bb58f49a52c5494f881f902a41132d
nicx503 <nic503@york.ac.uk>
2023-05-15 10:58:06 +0100

    revised the draw parameters

--- src/scripts/hiv/projections_jan2023/analysis_full_model.py
+++ src/scripts/hiv/projections_jan2023/analysis_full_model.py
@@ -67,7 +67,6 @@ sim.register(*fullmodel(
 # set the scenario
 #sim.modules["Tb"].parameters["probability_community_chest_xray"] = 0.6
 sim.modules["Tb"].parameters["scenario"] =1
-sim.modules["Tb"].parameters["second_line_test"] =[]
 sim.modules["Tb"].parameters["scenario_start_date"] =start_date
 
 # Run the simulation and flush the logger
