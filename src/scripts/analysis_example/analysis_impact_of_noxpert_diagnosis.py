5ead44711fd460c3ef6ef90bf61e2e29a24a2567
nicx503 <nic503@york.ac.uk>
2023-05-16 20:32:42 +0100

    revised the draw parameters

--- src/scripts/analysis_example/analysis_impact_of_noxpert_diagnosis.py
+++ src/scripts/analysis_example/analysis_impact_of_noxpert_diagnosis.py
@@ -22,18 +22,33 @@ from tlo.analysis.utils import (
 
 from tlo import Date
 
-results_folder = Path("./outputs")
-results_folder = Path("./outputs/nic503@york.ac.uk")
+#results_folder = Path("./outputs")
+outputspath = Path("./outputs/nic503@york.ac.uk")
+
+
+# collecting basic information associated with scenario
+
+# Find results_folder associated with a given batch_file and get most recent
+results_folder = get_scenario_outputs('scenario_impact_noXpert_diagnosis.py', outputspath)[-1]
+
+# look at one log (so can decide what to extract)
+log = load_pickled_dataframes(results_folder)
+
+# get basic information about the results
+info = get_scenario_info(results_folder)
+
+# 1) Extract the parameters that have varied over the set of simulations
+params = extract_params(results_folder)
 
 num_deaths =extract_results(
-        outputpath,
+        outputspath,
         module='tlo.methods.demography',
         key='death',
         custom_generate_series=get_num_deaths,
         do_scaling=True
     )
 num_dalys = extract_results(
-    outputpath,
+    outputspath,
     module='tlo.methods.healthburden',
     key='dalys_stacked',
     custom_generate_series=get_num_dalys(),
