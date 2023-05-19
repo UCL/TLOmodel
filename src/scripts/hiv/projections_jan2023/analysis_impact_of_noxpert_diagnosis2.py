6af97ba70180251de0c32d860070062737df7a1b
nicx503 <nic503@york.ac.uk>
2023-05-19 09:06:47 +0100

    revised the draw parameters

--- src/scripts/hiv/projections_jan2023/analysis_impact_of_noxpert_diagnosis2.py
+++ src/scripts/hiv/projections_jan2023/analysis_impact_of_noxpert_diagnosis2.py
@@ -1,5 +1,5 @@
 """Analyse the results of scenario to test impact of Noxpert diagnosis."""
-# python src/scripts/hiv/projections_jan2023/analysis_impact_of_noxpert_diagnosis2.py --scenario-outputs-folder outputs/nic503@york.ac.uk --show-figures
+# python src/scripts/hiv/projetions_jan2023/analysis_impact_of_noxpert_diagnosis2.py --scenario-outputs-folder outputs/nic503@york.ac.uk --show-figures
 import argparse
 import datetime
 from pathlib import Path
@@ -18,16 +18,8 @@ from tlo.analysis.utils import (
 resourcefilepath = Path("./resources")
 datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 outputspath = Path("./outputs/nic503@york.ac.uk")
-results_folder = get_scenario_outputs('scenario_impact_noXpert_diagnosis.py', outputspath)[-1]
-
-# Load log (useful for checking what can be extracted)
-log = load_pickled_dataframes(results_folder)
-
-# Get basic information about the results
-scenario_info = get_scenario_info(results_folder)
 
 def extract_total_deaths(results_folder):
-
     def extract_deaths_total(df: pd.DataFrame) -> pd.Series:
         return pd.Series({"Total": len(df)})
 
@@ -38,7 +30,11 @@ def extract_total_deaths(results_folder):
         custom_generate_series=extract_deaths_total,
         do_scaling=True
     )
-    sum_deaths.to_excel(outputspath/"total_deaths.xlsx", index=True)
+    if sum_deaths is not None:
+        sum_deaths.to_excel(outputspath / "total_deaths.xlsx", index=True)
+    else:
+        print("Error: Unable to extract total deaths.")
+
 def extract_total_dalys(results_folder):
 
     def extract_dalys_total(df: pd.DataFrame) -> pd.Series:
@@ -72,13 +68,14 @@ def make_plot(summarized_total_deaths, param_strings):
     ax.set_ylabel("Total number of deaths")
     fig.tight_layout()
     return fig, ax
-
 def compute_difference_in_deaths_across_runs(total_deaths, scenario_info):
     deaths_difference_by_run = [
-        total_deaths[0][run_number]["Total"] - total_deaths[1][run_number]["Total"]
+        total_deaths[0][run_number]["total_deaths"] - total_deaths[1][run_number]["total_deaths"]
         for run_number in range(scenario_info["runs_per_draw"])
     ]
     return np.mean(deaths_difference_by_run)
+    deaths_difference_by_run.to_excel(outputspath/"total_dalys.xlsx", index=True)
+
 
 if __name__ == "__main__":
 
@@ -107,12 +104,17 @@ if __name__ == "__main__":
     # Find results_folder associated with a given batch_file and get most recent
     results_folder = get_scenario_outputs("scenario_impact_noXpert_diagnosis.py", outputspath)[-1]
 
+    # Load log (useful for checking what can be extracted)
+    log = load_pickled_dataframes(results_folder)
+
+    # Get basic information about the results
+    scenario_info = get_scenario_info(results_folder)
+
     # Get the parameters that have varied over the set of simulations
     params = extract_params(results_folder)
 
     # Create a list of strings summarizing the parameter values in the different draws
     param_strings = [f"{row.module_param}={row.value}" for _, row in params.iterrows()]
-
     # extracts deaths from runs
     total_deaths = extract_total_deaths(results_folder)
 
@@ -124,6 +126,7 @@ if __name__ == "__main__":
 
     # Plot the total deaths across the two scenario draws as a bar plot with error bars
     fig_1, ax_1 = make_plot(summarize(total_deaths), param_strings)
+    #fig_2, ax_1 = make_plot(summarize(sum_dalys), param_strings)
 
     # Show Matplotlib figure windows
     if args.show_figures:
