973ee8ec1bf9d6c9198f0cd4f420e1d012e70125
nicx503 <nic503@york.ac.uk>
2023-05-24 06:55:19 +0100

    revising scripts so it extracts output from the batch run

--- src/scripts/hiv/projections_jan2023/output_BoD_1.py
+++ src/scripts/hiv/projections_jan2023/output_BoD_1.py
@@ -94,31 +94,31 @@ cons_available .to_excel(outputpath / "cons_available_NoXpert.xlsx")
 
 print(f" expected ylds{output['tlo.methods.healthburden']['yld_by_causes_of_disability']}")
 ylds = output['tlo.methods.healthburden']['yld_by_causes_of_disability'].drop(columns=[])
-ylds.to_excel(outputpath / "ylds_NoXpert.xlsx")
+ylds.to_excel(outputpath / "ylds_NoXpertxx.xlsx")
 print(f"expected ylls{output['tlo.methods.healthburden']['yll_by_causes_of_death_stacked']}")
 yll_output = output['tlo.methods.healthburden']['yll_by_causes_of_death_stacked'].drop(columns=[])
-yll_output.to_excel(outputpath / "sample_yll_NoXpert.xlsx")
+yll_output.to_excel(outputpath / "sample_yll_NoXpertxx.xlsx")
 
 # Exports TB program indicators
 print(f"projected TB incidence{output['tlo.methods.tb']['tb_incidence']}")
 TB_incidence= output['tlo.methods.tb']['tb_incidence'].drop(columns=[])
-TB_incidence.to_excel(outputpath / "new_TB_cases_NoXpert.xlsx")
+TB_incidence.to_excel(outputpath / "new_TB_cases_NoXpertxx.xlsx")
 
 print(f"projected treatment coverage {output['tlo.methods.tb']['tb_incidence']}")
 TB_treatment_cov= output['tlo.methods.tb']['tb_treatment'].drop(columns=[])
-TB_treatment_cov.to_excel(outputpath / "TB_treatment_NoXpert.xlsx")
+TB_treatment_cov.to_excel(outputpath / "TB_treatment_NoXpertxx.xlsx")
 
 # output DALYs
 print(f"expected dalys{output['tlo.methods.healthburden']['dalys_stacked']}")
 #sample_dalys= output['tlo.methods.healthburden']['dalys_stacked'].groupby(['cause', 'sex']).size()
 sample_dalys= output['tlo.methods.healthburden']['dalys_stacked'].drop(columns=[])
-sample_dalys.to_excel(outputpath / "sample_dalys_NoXpert.xlsx")
+sample_dalys.to_excel(outputpath / "sample_dalys_NoXpertxx.xlsx")
 
 # output serialises mortality patterns
 print(f"expected deaths {output['tlo.methods.demography']['death']}")
 sample_deaths = output['tlo.methods.demography']['death'].groupby(['date','cause', 'sex']).size()
 #sample_deaths = output['tlo.methods.demography']['death'].drop(columns=[])
-sample_deaths.to_excel(outputpath / "sample_mortality_NoXpert.xlsx")
+sample_deaths.to_excel(outputpath / "sample_mortality_NoXpertxx.xlsx")
 
 
 # # results_folder = Path("./outputs")
