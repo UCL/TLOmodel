709120c59db24363e23366ba3a04b262e529d239
Margherita Molaro <48129834+marghe-molaro@users.noreply.github.com>
2023-06-02 13:49:22 +0100

    Increase population size for Azure submission, run for 5 years

--- src/scripts/healthsystem/impact_of_policy/scenario_impact_of_policy.py
+++ src/scripts/healthsystem/impact_of_policy/scenario_impact_of_policy.py
@@ -28,7 +28,7 @@ class ImpactOfHealthSystemMode(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = self.start_date + pd.DateOffset(years=5)
-        self.pop_size = 100
+        self.pop_size = 100_000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 1
