cd19cfbc8f0e5d16d42a9f3f26f2b063b0c88616
nicx503 <nic503@york.ac.uk>
2023-06-06 12:24:34 +0100

    revising scripts so it extracts output from the batch run

--- src/scripts/hiv/projections_jan2023/analysis_full_model.py
+++ src/scripts/hiv/projections_jan2023/analysis_full_model.py
@@ -24,7 +24,7 @@ resourcefilepath = Path("./resources")
 # %% Run the simulation
 start_date =Date(2010, 1, 1)
 end_date = Date(2012, 12, 31)
-popsize = 600
+popsize = 1000
 scenario = 1
 
 # set up the log config
