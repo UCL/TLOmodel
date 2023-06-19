ae5fc3ab52bfd7ca15031409267924fffbff5acd
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2023-06-19 18:07:42 +0100

    roll back change to resource file and cap year of testing to the last year for which data is provided

--- src/tlo/methods/malaria.py
+++ src/tlo/methods/malaria.py
@@ -401,12 +401,11 @@ class Malaria(Module):
         df = population.props
         rng = self.rng
         p = self.parameters
-        now = self.sim.date
-        year = now.year if now.year <= 2023 else 2023
 
         # extract annual testing rates from NMCP reports
         # this is the # rdts issued divided by population size
         test_rates = p['rdt_testing_rates']
+        year = min(test_rates.Year.max(), self.sim.date.year)  # year of the simulation, or the latest year of data
         rdt_rate = test_rates.loc[(test_rates.Year == year), 'Rate_rdt_testing'].values[0] / 12
         # adjust rdt usage reported rate to reflect consumables availability
         rdt_rate = rdt_rate * p['scaling_factor_for_rdt_availability']
