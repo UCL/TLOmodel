b271d3c476b6c52a8c3ea9ce7e12a0cfa0ab1f0b
nicx503 <nic503@york.ac.uk>
2023-06-21 22:38:47 +0100

    created scenario file and analysis scripts for the TB module

--- src/scripts/hiv/projections_jan2023/output_BoD_1.py
+++ src/scripts/hiv/projections_jan2023/output_BoD_1.py
@@ -20,13 +20,11 @@ resourcefilepath = Path("./resources")
 outputpath = Path("./outputs")  # folder for convenience of storing outputs
 #outputpath = Path("./outputs/nic503@york.ac.uk")
 datestamp = datetime.date.today().strftime("__%Y_%m_%d")
-# with open(outputpath / "default_run.pickle", "rb") as f:
-#     output = pickle.load(f)
 
 with open(outputpath / "default_run.pickle", "rb") as f:
     output = pickle.load(f)
 
-TARGET_PERIOD = (Date(2010, 1, 1), Date(2012, 12, 31))
+TARGET_PERIOD = (Date(2010, 1, 1), Date(2013, 12, 31))
 
 def get_num_deaths(_df):
     """Return total number of Deaths (total within the TARGET_PERIOD)
@@ -88,7 +86,7 @@ print(output.keys())
 #print(output(tlo.methods.healthsystem.summary).keys())
 # output serialises availability of  CXR consumables
 cons_available = output['tlo.methods.healthsystem.summary']['Consumables'].drop(columns=[])
-cons_available .to_excel(outputpath / "cons_available_base.xlsx")
+cons_available .to_excel(outputpath / "cons_available_xpert.xlsx")
 
 # output YLLs and YLDs
 
@@ -102,7 +100,7 @@ cons_available .to_excel(outputpath / "cons_available_base.xlsx")
 # Exports TB program indicators
 print(f"projected TB incidence{output['tlo.methods.tb']['tb_incidence']}")
 TB_incidence= output['tlo.methods.tb']['tb_incidence'].drop(columns=[])
-TB_incidence.to_excel(outputpath / "new_TB_cases_base.xlsx")
+TB_incidence.to_excel(outputpath / "new_TB_cases_xpert.xlsx")
 #
 # print(f"projected treatment coverage {output['tlo.methods.tb']['tb_incidence']}")
 # TB_treatment_cov= output['tlo.methods.tb']['tb_treatment'].drop(columns=[])
@@ -112,60 +110,11 @@ TB_incidence.to_excel(outputpath / "new_TB_cases_base.xlsx")
 print(f"expected dalys{output['tlo.methods.healthburden']['dalys_stacked']}")
 #sample_dalys= output['tlo.methods.healthburden']['dalys_stacked'].groupby(['cause', 'sex']).size()
 sample_dalys= output['tlo.methods.healthburden']['dalys_stacked'].drop(columns=[])
-sample_dalys.to_excel(outputpath / "sample_dalys_base.xlsx")
+sample_dalys.to_excel(outputpath / "sample_dalys_xpert.xlsx")
 
 # output serialises mortality patterns
 print(f"expected deaths {output['tlo.methods.demography']['death']}")
 sample_deaths = output['tlo.methods.demography']['death'].groupby(['date','cause', 'sex']).size()
 #sample_deaths = output['tlo.methods.demography']['death'].drop(columns=[])
-sample_deaths.to_excel(outputpath / "expected_mortality_base.xlsx")
+sample_deaths.to_excel(outputpath / "expected_mortality_xpert.xlsx")
 
-
-# # results_folder = Path("./outputs")
-# # results_folder = Path("./outputs/nic503@york.ac.uk")
-#
-# num_deaths =extract_results(
-#         outputpath,
-#         module='tlo.methods.demography',
-#         key='death',
-#         custom_generate_series=get_num_deaths,
-#         do_scaling=True
-#     )
-# num_dalys = extract_results(
-#     outputpath,
-#     module='tlo.methods.healthburden',
-#     key='dalys_stacked',
-#     custom_generate_series=get_num_dalys(),
-#     do_scaling=True
-# )
-#
-# num_deaths_summarized = summarize(num_deaths).loc[0].unstack()
-# num_dalys_summarized = summarize(num_dalys).loc[0].unstack()
-#
-# #     # # TB deaths will exclude TB/HIV
-# #     # # keep if cause = TB
-# #     # keep = (deaths.cause == "TB")
-# #     # deaths_TB = deaths.loc[keep].copy()
-# #     # deaths_TB["year"] = deaths_TB.index.year  # count by year
-# #     # tot_tb_non_hiv_deaths = deaths_TB.groupby(by=["year"]).size()
-# #     # tot_tb_non_hiv_deaths.index = pd.to_datetime(tot_tb_non_hiv_deaths.index, format="%Y")
-#
-# # Plot for total number of DALYs from the scenario
-# name_of_plot = f'Total DALYS, {target_period()}'
-# fig, ax = make_plot(num_dalys_summarized / 1e6)
-# ax.set_title(name_of_plot)
-# ax.set_ylabel('DALYS (Millions)')
-# fig.tight_layout()
-# fig.savefig("DALY_graph.png")
-# plt.show()
-#
-# # plot of total number of deaths from the scenario
-# name_of_plot= f'Total Deaths, {target_period()}'
-# fig, ax = make_plot(num_deaths_summarized / 1e6)
-# ax.set_title(name_of_plot)
-# ax.set_ylabel('Deaths (Millions)')
-# fig.tight_layout()
-# fig.savefig("Mortality_graph.png")
-# plt.show()
-#
-#
