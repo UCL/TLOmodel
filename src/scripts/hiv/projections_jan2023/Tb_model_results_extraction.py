9ff9ddeaae45ed549f404aefdb43eb3d1012def6
nicx503 <nic503@york.ac.uk>
2023-06-22 19:21:39 +0100

    created scenario file and analysis scripts for the TB module

--- src/scripts/hiv/projections_jan2023/Tb_model_results_extraction.py
+++ src/scripts/hiv/projections_jan2023/Tb_model_results_extraction.py
@@ -21,7 +21,7 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 # with open(outputpath / "default_run.pickle", "rb") as f:
 #     output = pickle.load(f)
 
-with open(outputpath / "default_run_xpert1", "rb") as f:
+with open(outputpath / "default_run_xpert1.pickle", "rb") as f:
     output = pickle.load(f)
 
 TARGET_PERIOD = (Date(2010, 1, 1), Date(2013, 12, 31))
