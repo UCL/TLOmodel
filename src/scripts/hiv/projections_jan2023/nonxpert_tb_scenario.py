8dbd2f14c64c8a2edc6f8b8dfed6407e77751267
nicx503 <nic503@york.ac.uk>
2023-06-22 15:56:23 +0100

    created scenario file and analysis scripts for the TB module

--- src/scripts/hiv/projections_jan2023/nonxpert_tb_scenario.py
+++ src/scripts/hiv/projections_jan2023/nonxpert_tb_scenario.py
@@ -43,7 +43,7 @@ class ImpactOfNoXpert(BaseScenario):
         super().__init__(
             seed=2025,
             start_date=Date(2010, 1, 1),
-            end_date=Date(2012, 12, 31),
+            end_date=Date(2013, 12, 31),
             initial_population_size=1200,
             number_of_draws=1,
             runs_per_draw=2,
