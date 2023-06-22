cc7dbdd81304b11855672545b311a8588bc749fc
nicx503 <nic503@york.ac.uk>
2023-06-22 16:30:47 +0100

    created scenario file and analysis scripts for the TB module

--- src/scripts/hiv/projections_jan2023/noncxr_tb_scenario.py
+++ src/scripts/hiv/projections_jan2023/noncxr_tb_scenario.py
@@ -42,8 +42,8 @@ class ImpactOfnoCXR(BaseScenario):
         super().__init__(
             seed=2025,
             start_date=Date(2010, 1, 1),
-            end_date=Date(2033, 12, 31),
-            initial_population_size=50000,
+            end_date=Date(2012, 12, 31),
+            initial_population_size=1200,
             number_of_draws=1,
             runs_per_draw=2,
         )
