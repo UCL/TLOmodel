7ac9b38c824526a027f68fa7b4582769f5211629
nicx503 <nic503@york.ac.uk>
2023-06-26 23:49:52 +0100

    Revised scenarios for TB diagnostics availability-- all scenarios will run from one branch

--- src/scripts/hiv/projections_jan2023/revised_analysis_impact_of_Tb_DAH.py
+++ src/scripts/hiv/projections_jan2023/revised_analysis_impact_of_Tb_DAH.py
@@ -24,7 +24,7 @@ datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 
 # Get basic information about the results
 #results_folder = (outputspath / 'impact_of_Tb_DAH_baseline-2023-06-14T133033Z')
-results_folder = get_scenario_outputs("noncxr_tb_scenario-2023-06-22T153114Z", outputspath) [-1]
+results_folder = get_scenario_outputs("baseline_tb_services_scenario-2023-06-23T213051Z", outputspath) [-1]
 # look at one log (so can decide what to extract)
 log = load_pickled_dataframes(results_folder)
 # get basic information about the results
@@ -32,7 +32,7 @@ info = get_scenario_info(results_folder)
 
 # Extract the parameters that have varied over the set of simulations
 params = extract_params(results_folder)
-
+draw = 0
 # Defines functions to extract relevant results
 tb_death_count = extract_results(
     results_folder,
@@ -44,7 +44,7 @@ tb_death_count = extract_results(
     do_scaling=False,
 )
 summary_deaths = summarize(tb_death_count)
-summary_deaths.to_excel(outputspath / "summary_death_nocxr.xlsx")
+summary_deaths.to_excel(outputspath / "summary_death_baseline.xlsx")
 
 #prints dictionary keys for the TB module
 print(f"Keys of log['tlo.methods.tb']: {log['tlo.methods.tb'].keys()}")
@@ -63,7 +63,7 @@ tb_hiv_prop = summarize(
 
 tb_hiv_prop.index = tb_hiv_prop.index.year
 tb_hiv_prop_with_year = pd.DataFrame(tb_hiv_prop)
-tb_hiv_prop.to_excel(outputspath / "PLHIV_tb_nocxr.xlsx")
+tb_hiv_prop.to_excel(outputspath / "PLHIV_tb_baseline.xlsx")
 
 #MDR TB cases
 mdr_tb_cases = summarize(
@@ -79,10 +79,11 @@ mdr_tb_cases = summarize(
 )
 mdr_tb_cases.index = mdr_tb_cases.index.year
 mdr_tb = pd.DataFrame(mdr_tb_cases)
-mdr_tb.to_excel(outputspath / "mdr_tb_nocxr.xlsx")
+mdr_tb.to_excel(outputspath / "mdr_tb_baseline.xlsx")
+
 
 # TB treatment coverage
-tb_tx = summarize(
+tb_treatment = summarize(
         extract_results(
             results_folder,
             module="tlo.methods.tb",
@@ -93,12 +94,14 @@ tb_tx = summarize(
         ),
         collapse_columns=True,
     )
-tb_tx.index = tb_tx.index.year,
-tb_tx_coverage = pd.DataFrame(tb_tx)
-tb_tx_coverage.to_excel(outputspath / "tb_tx_coverage_cxr.xlsx")
+#print("Before loading tb_tx")
+#print(tb_treatment)
+#tb_treatment.index = tb_treatment.index.year,
+tb_treatment_cov = pd.DataFrame(tb_treatment)
+tb_treatment_cov.to_excel(outputspath / "tb_tx_coverage_baseline.xlsx")
 
-   ###summarizing incidence and mortality########################
-    # computing person years to used as denominators for mortality rate and incidence
+###summarizing incidence and mortality                  ########################
+# computing person years to used as denominators for mortality rate and incidence
 def get_person_years(_df):
     """ extract person-years for each draw/run
     sums across men and women
@@ -115,13 +118,19 @@ def get_person_years(_df):
     py.index = pd.to_datetime(years, format="%Y")
     return py
 
-pyears = extract_results(
+pyears = summarize (
+    extract_results(
     results_folder,
     module="tlo.methods.demography",
     key="person_years",
     custom_generate_series=get_person_years,
     do_scaling=False
 )
+)
+#print(pyears)
+print(pyears.columns)
+tb_pyears = pd.DataFrame(pyears)
+tb_pyears.to_excel(outputspath / "pyears_baseline.xlsx")
 
 #TB Incidence
 tb_inc = summarize(
@@ -135,78 +144,185 @@ tb_inc = summarize(
     ),
     collapse_columns=True,
 )
-tb_inc.index = tb_inc.index.year
-activeTB_inc_rate = (tb_inc.divide(pyears["mean"].values, axis=0)) * 100000
-activeTB_inc_rate_df = pd.DataFrame(activeTB_inc_rate)
+print(tb_inc)
+tb_incidence = pd.DataFrame(tb_inc)
+tb_incidence.to_excel(outputspath / "tb_incidence_baseline.xlsx")
 
-# write files to excel
-active_tb_cases = "tb_inc_activeTB_inc_rate_xpert.xlsx"
-with pd.ExcelWriter(active_tb_cases) as writer:
-    tb_inc.to_excel(writer, sheet_name="tb_inc")
-    activeTB_inc_rate_df.to_excel(writer, sheet_name="activeTB_inc_rate_nocxr")
+tb_inc.index = tb_inc.index.year
+print(tb_inc.head())
+print(pyears.head())
+tb_inc = tb_inc.reset_index(drop=True)
+#pyears = pyears.reset_index(drop=True)
+pyears = pyears.reset_index(drop=True)
+
+
+# activeTB_inc_rate = (tb_inc["mean"] / pyears["mean"]) * 100000
+# activeTB_inc_rate= pd.DataFrame(activeTB_inc_rate)
+# activeTB_inc_rate.to_excel(outputspath / "tb_incidence_rate_baseline.xlsx")
+#
+# # Calculate the rates
+# activeTB_inc_rate = (tb_inc["mean"] / pyears["mean"]) * 100000
+# upper_activeTB_inc_rate = (tb_inc["upper"] / pyears["upper"]) * 100000
+# lower_activeTB_inc_rate = (tb_inc["lower"] / pyears["lower"]) * 100000
+#
+# # Create a new dataframe with the calculated values
+# summarized_incidence = pd.DataFrame({
+#     "TB_inc_rate": activeTB_inc_rate,
+#     "upper": upper_activeTB_inc_rate,
+#     "lower": lower_activeTB_inc_rate
+# })
+#
+# # Output the combined table to an Excel file
+# summarized_incidence .to_excel(outputspath / "baseline_incidence_rate.xlsx")
 
 #######################################################################
-# number of deaths and mortality rate
+#number of TB deaths and mortality rate
+results_deaths = extract_results(
+    results_folder,
+    module="tlo.methods.demography",
+    key="death",
+    custom_generate_series=(
+        lambda df: df.assign(year=df["date"].dt.year).groupby(
+            ["year", "cause"])["person_id"].count()
+    ),
+    do_scaling=False,
+)
+
+# Removes multi-index
+results_deaths = results_deaths.reset_index()
+print(results_deaths)
+
+tb_deaths = results_deaths.loc[results_deaths.cause.isin(["AIDS_non_TB", "AIDS_TB", "TB"])]
+AIDS_TB = results_deaths.loc[results_deaths.cause == "AIDS_non_TB"]
+AIDS_non_TB = results_deaths.loc[results_deaths.cause == "AIDS_TB"]
+TB = results_deaths.loc[results_deaths.cause == "TB"]
+
+combined_tb_table = pd.concat([AIDS_non_TB, AIDS_TB, TB])
+#print(combined_table)
+combined_tb_table.to_excel(outputspath / "combined_ttb_able_baseline.xlsx")
+scaling_factor_key = log['tlo.methods.demography']['scaling_factor']
+
+print(f"Keys of log['tlo.methods.demography']: {log['tlo.methods.demography'].keys()}")
+print("Scaling Factor Key:", scaling_factor_key)
+#########################################################################################
+
 # deaths due to TB (not including TB-HIV)
-def summarise_tb_deaths(results_folder, person_years):
-    results_deaths = extract_results(
+
+def tb_mortality_rate(results_folder, pyears):
+    tb_deaths = extract_results(
         results_folder,
         module="tlo.methods.demography",
         key="death",
         custom_generate_series=(
-            lambda df: df.assign(year=df["date"].dt.year).groupby(
-                ["year", "cause"])["person_id"].count()
+            lambda df: df.assign(year=df["date"].dt.year).groupby(["year", "cause"])["person_id"].count()
         ),
         do_scaling=False,
     )
-    # removes multi-index
-    results_deaths = results_deaths.reset_index()
-    # select only cause AIDS_TB and AIDS_non_TB
-    tmp = results_deaths.loc[results_deaths.cause == "TB"]
-    # group deaths by year
-    tmp2 = pd.DataFrame(tmp.groupby(["year"]).sum())
-    # divide each draw/run by the respective person-years from that run
-    # need to reset index as they don't match exactly (date format)
-    tmp3 = tmp2.reset_index(drop=True) / (pyears.reset_index(drop=True))
+
+    # Print the first few rows of tb_deaths DataFrame
+    # Select only causes AIDS_TB, AIDS_non_TB, and TB
+    tb_deaths1 = results_deaths.loc[(results_deaths.cause == "AIDS_TB") | \
+    (results_deaths.cause == "AIDS_non_TB") | (results_deaths.cause == "AIDS_non_TB")
+        ]
+
+    # Group deaths by year
+    tb_deaths2 = pd.DataFrame(tb_deaths1.groupby(["year"]).sum())
+
+    # Divide each draw/run by the respective person-years from that run
+    # Need to reset index as they don't match exactly (date format)
+    tb_deaths3 = tb_deaths2.reset_index(drop=True) / pyears.reset_index(drop=True)
+
+    tb_mortality_rate = {}  # empty dict
+    tb_mortality_rate["year"] = tb_deaths2.index
+    tb_mortality_rate["median_tb_deaths_rate_100kpy"] = tb_deaths3.quantile(0.5, axis=1) * 100000
+    tb_mortality_rate["lower_tb_deaths_rate_100kpy"] = tb_deaths3.quantile(0.025, axis=1) * 100000
+    tb_mortality_rate["upper_tb_deaths_rate_100kpy"] = tb_deaths3.quantile(0.975, axis=1) * 100000
+    # Convert tb_mortality_rate dictionary into a DataFrame
+
+    return tb_mortality_rate
+
+# Call the function with appropriate arguments
+mortality_rates = tb_mortality_rate(results_folder, pyears)
+mortality_rates_summary = pd.DataFrame.from_dict(mortality_rates)
+
+# Print the resulting mortality rates
+mortality_rates_summary.to_excel(outputspath / "mortality_rates_summary.xlsx",index=False)
+print(mortality_rates_summary)
 
 
-# Summarizing TB DALYs
-    # Define function to calculate number of DALYs
-TARGET_PERIOD = (Date(2010, 1, 1), Date(2015, 1, 1))
-def num_dalys(_df):
-    """Return total number of DALYS (Stacked) (total by age-group within the TARGET_PERIOD)"""
-    return _df \
-        .loc[_df.year.between(*[i.year for i in TARGET_PERIOD])] \
-        .drop(columns=['date', 'sex', 'age_range', 'cause', 'year']) \
-        .sum()
 
-# Define function to summarize TB DALYs
-def tb_daly_summary(results_folder):
-    dalys = extract_results(
-        results_folder,
-        module='tlo.methods.healthburden',
-        key='dalys_stacked',
-        custom_generate_series=num_dalys,
-        do_scaling=False
-    )
-    dalys.columns = dalys.columns.get_level_values(0)
-    dalys.loc['TB (non-AIDS)'] = dalys.loc['TB (non-AIDS)'] + dalys.loc['non_AIDS_TB']
-    dalys.drop(['non_AIDS_TB'], inplace=True)
-    tb_daly = pd.DataFrame()
-    tb_daly['median'] = dalys.median(axis=1).round(decimals=-3).astype(int)
-    tb_daly['lower'] = dalys.quantile(q=0.025, axis=1).round(decimals=-3).astype(int)
-    tb_daly['upper'] = dalys.quantile(q=0.975, axis=1).round(decimals=-3).astype(int)
-    return tb_daly
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Summarizing TB DALYs
+#     # Define function to calculate number of DALYs
+# TARGET_PERIOD = (Date(2010, 1, 1), Date(2015, 1, 1))
+# def num_dalys(_df):
+#     """Return total number of DALYS (Stacked) (total by age-group within the TARGET_PERIOD)"""
+#     return _df \
+#         .loc[_df.year.between(*[i.year for i in TARGET_PERIOD])] \
+#         .drop(columns=['date', 'sex', 'age_range', 'cause', 'year']) \
+#         .sum()
+#
+# # Define function to summarize TB DALYs
+# def tb_daly_summary(results_folder):
+#     dalys = extract_results(
+#         results_folder,
+#         module='tlo.methods.healthburden',
+#         key='dalys_stacked',
+#         custom_generate_series=num_dalys,
+#         do_scaling=False
+#     )
+#     dalys.columns = dalys.columns.get_level_values(0)
+#     dalys.loc['TB (non-AIDS)'] = dalys.loc['TB (non-AIDS)'] + dalys.loc['non_AIDS_TB']
+#     dalys.drop(['non_AIDS_TB'], inplace=True)
+#     tb_daly = pd.DataFrame()
+#     tb_daly['median'] = dalys.median(axis=1).round(decimals=-3).astype(int)
+#     tb_daly['lower'] = dalys.quantile(q=0.025, axis=1).round(decimals=-3).astype(int)
+#     tb_daly['upper'] = dalys.quantile(q=0.975, axis=1).round(decimals=-3).astype(int)
+#     return tb_daly
 
 # Call the function to get the results
-tb_dalys = tb_daly_summary(results_folder)
+# tb_dalys = tb_daly_summary(results_folder)
+#
+# # Export results to Excel
+# tb_dalys.index = tb_dalys.index.year,
+# summary_dalys = pd.DataFrame(tb_dalys)
+# summary_dalys.to_excel(outputspath / "summary_tb_dalys_nocxr.xlsx")
 
-# Export results to Excel
-tb_dalys.index = tb_dalys.index.year,
-summary_dalys = pd.DataFrame(tb_dalys)
-summary_dalys.to_excel(outputspath / "summary_tb_dalys_nocxr.xlsx")
+if __name__ == "__main__":
 
-
+    parser = argparse.ArgumentParser(
+        "Analyse scenario results for noxpert diagnosis pathway"
+    )
+    parser.add_argument(
+        "--scenario-outputs-folder",
+        type=Path,
+        required=True,
+        help="Path to folder containing scenario outputs",
+    )
+    parser.add_argument(
+        "--show-figures",
+        action="store_true",
+        help="Whether to interactively show generated Matplotlib figures",
+    )
+    parser.add_argument(
+        "--save-figures",
+        action="store_true",
+        help="Whether to save generated Matplotlib figures to results folder",
+    )
+    args = parser.parse_args()
 
 
 
