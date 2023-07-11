df88c89c05b61737ad58ca22e80d24156ffef993
nicx503 <nic503@york.ac.uk>
2023-07-11 20:31:26 +0100

    Analysis script for extracting outputs from the TB DAH scenario runs

--- src/scripts/hiv/projections_jan2023/tb_DAH_scenarios.py
+++ src/scripts/hiv/projections_jan2023/tb_DAH_scenarios.py
@@ -45,7 +45,7 @@ class ImpactOfTbDaH(BaseScenario):
     def log_configuration(self):
         return {
             'filename': 'Tb_DAH_scenarios_test_run01',
-           # 'directory': Path('./outputs')
+           'directory': Path('./outputs/nic503@york.ac.uk'),
             'custom_levels': {
                 '*': logging.WARNING,
                 'tlo.methods.demography': logging.INFO,
