febaf65d3c7726aeb32934793d5407f3fd3c332e
tdm32 <t.mangal@imperial.ac.uk>
2023-07-13 15:05:34 +0100

    flake8 checks

--- src/tlo/methods/epi.py
+++ src/tlo/methods/epi.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from tlo import Date, DateOffset, Module, Parameter, Property, Types, logging
-from tlo.events import Event, IndividualScopeEventMixin, PopulationScopeEventMixin, RegularEvent
+from tlo.events import IndividualScopeEventMixin, PopulationScopeEventMixin, RegularEvent
 from tlo.methods import Metadata
 from tlo.methods.healthsystem import HSI_Event
 
