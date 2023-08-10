4fca000e35e2e8004f8df161e546eaf38ff35682
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-08-11 00:35:34 +0100

    run_analysis_co_no_dis: 2010-2050, 2K pop, co-logging at DEBUG level

--- src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
+++ src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
@@ -49,8 +49,8 @@ class RunAnalysisCo(BaseScenario):
         super().__init__(
             seed=0,
             start_date=Date(2010, 1, 1),
-            end_date=Date(2099, 12, 31),
-            initial_population_size=250_000,  # selected size for the Tim C at al. 2023 paper: 250K
+            end_date=Date(2050, 12, 31),
+            initial_population_size=2_000,  # selected size for the Tim C at al. 2023 paper: 250K
             number_of_draws=1,  # <- one scenario
             runs_per_draw=1,  # <- repeated this many times
         )
@@ -61,7 +61,7 @@ class RunAnalysisCo(BaseScenario):
             'directory': './outputs',  # <- (specified only for local running)
             'custom_levels': {
                 '*': logging.WARNING,
-                "tlo.methods.contraception": logging.INFO,
+                "tlo.methods.contraception": logging.DEBUG,
                 "tlo.methods.demography": logging.INFO
             }
         }
