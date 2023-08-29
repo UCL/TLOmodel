310546e3e6c24071a3af6384923944a21c3b04e0
nicx503 <nic503@york.ac.uk>
2023-08-29 14:26:50 +0100

    In the commit, I have revised the HSI_Tb_Xray_level1b and 2 to add the probability of accessing chest xray. The probability parameter was defined yet not used within the code to define probability of accessing chest Xray

--- src/tlo/methods/tb.py
+++ src/tlo/methods/tb.py
@@ -2595,9 +2595,7 @@ class HSI_Tb_CommunityXray(HSI_Event, IndividualScopeEventMixin):
 
         self.TREATMENT_ID = "Tb_Test_ScreeningOutreach"
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({"ConWithDCSA": 1})
-       # self.ACCEPTED_FACILITY_LEVEL = '0'
-        self.ACCEPTED_FACILITY   #[Is this defined in the health system]
-
+        self.ACCEPTED_FACILITY_LEVEL = '0'
 
     def apply(self, person_id, squeeze_factor):
         print(f'"STARTING COMMUNITY CHEST XRAY SCREENING"')
