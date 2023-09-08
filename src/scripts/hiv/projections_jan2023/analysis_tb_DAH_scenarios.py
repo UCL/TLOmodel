50e38e0b09e8d076525fed7fbea342d22548af9e
nicx503 <nic503@york.ac.uk>
2023-09-08 21:14:39 +0100

    added an additional CXR scale -up scenario at 20%

--- src/scripts/hiv/projections_jan2023/analysis_tb_DAH_scenarios.py
+++ src/scripts/hiv/projections_jan2023/analysis_tb_DAH_scenarios.py
@@ -24,7 +24,7 @@ outputspath = Path("./outputs/nic503@york.ac.uk")
 datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 
 # Get basic information about the results
-results_folder = get_scenario_outputs("Tb_DAH_scenarios_test_run06_partial-2023-09-08T133643Z", outputspath)[-1]
+results_folder = get_scenario_outputs("Tb_DAH_scenarios_test_run06_partial-2023-09-08T163846Z", outputspath)[-1]
 log = load_pickled_dataframes(results_folder)
 info = get_scenario_info(results_folder)
 print(info)
