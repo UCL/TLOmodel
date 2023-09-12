03e3f09fdcbaf3a33c039798054e804d44ab2ce8
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-09-12 19:15:44 +0100

    brc, co: used_equipment renamed to EQUIPMENT; if equip always same for HSI - set in __init__, otherwise updated in apply
    diff --git src/tlo/methods/breast_cancer.py src/tlo/methods/breast_cancer.py
    index 2529911d8..f19ec5358 100644
    --- src/tlo/methods/breast_cancer.py
    +++ src/tlo/methods/breast_cancer.py
    @@ -647,6 +647,8 @@ class HSI_BreastCancer_Investigation_Following_breast_lump_discernible(HSI_Event
             self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({"Over5OPD": 1, "Mammography": 1})
             self.ACCEPTED_FACILITY_LEVEL = '3'  # Biopsy only available at level 3 and above.
             # TODO: but the appt footprints suggests mammography to be provided
    +        self.EQUIPMENT = {'Slice Master sample processing Unit', 'Paraffin Dispense', 'Whatever used with biopsy',
    +                          'Mammography maybe?'}  # biopsy and ?mammography always performed with this HSI
    
         def apply(self, person_id, squeeze_factor):
             df = self.sim.population.props
    @@ -760,6 +762,10 @@ class HSI_BreastCancer_StartTreatment(HSI_Event, IndividualScopeEventMixin):
             df.at[person_id, "brc_date_treatment"] = self.sim.date
             df.at[person_id, "brc_stage_at_which_treatment_given"] = df.at[person_id, "brc_status"]
    
    +        # Update equipment used with treatment
    +        # NB. read only with HSI run and healthsystem.summary logger set at the level INFO or higher
    +        self.EQUIPMENT.update({'Anything used for mastectomy as I guess this is about'})
    +
             # Schedule a post-treatment check for 12 months:
             hs.schedule_hsi_event(
                 hsi_event=HSI_BreastCancer_PostTreatmentCheck(
    diff --git src/tlo/methods/contraception.py src/tlo/methods/contraception.py
    index 88736c98c..35db50580 100644
    --- src/tlo/methods/contraception.py
    +++ src/tlo/methods/contraception.py
    @@ -1255,7 +1255,8 @@ class HSI_Contraception_FamilyPlanningAppt(HSI_Event, IndividualScopeEventMixin)
             items_all = {**items_essential, **items_optional}
    
             # Determine whether the contraception is administrated (ie all essential items are available),
    -        # if so do log the availability of all items, if not set the contraception to "not_using":
    +        # if so do log the availability of all items and record used equipment if any, if not set the contraception to
    +        # "not_using":
             co_administrated = all(v for k, v in cons_available.items() if k in items_essential)
    
             if co_administrated:
    @@ -1279,6 +1280,14 @@ class HSI_Contraception_FamilyPlanningAppt(HSI_Event, IndividualScopeEventMixin)
                                  )
    
                 _new_contraceptive = self.new_contraceptive
    +
    +            # Update equipment when needed
    +            # NB. read only with HSI run and healthsystem.summary logger set at the level of logger.INFO or higher
    +            if _new_contraceptive == 'female_sterilization':
    +                self.EQUIPMENT.update({'Smt used to sterilize a woman'})
    +            elif _new_contraceptive == 'IUD':
    +                self.EQUIPMENT.update({'Equipment used when performing IUD'})
    +
             else:
                 _new_contraceptive = "not_using"

--- src/tlo/methods/breast_cancer.py
+++ src/tlo/methods/breast_cancer.py
@@ -647,6 +647,8 @@ class HSI_BreastCancer_Investigation_Following_breast_lump_discernible(HSI_Event
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({"Over5OPD": 1, "Mammography": 1})
         self.ACCEPTED_FACILITY_LEVEL = '3'  # Biopsy only available at level 3 and above.
         # TODO: but the appt footprints suggests mammography to be provided
+        self.EQUIPMENT = {'Slice Master sample processing Unit', 'Paraffin Dispense', 'Whatever used with biopsy',
+                          'Mammography maybe?'}  # biopsy and ?mammography always performed with this HSI
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -759,6 +761,10 @@ class HSI_BreastCancer_StartTreatment(HSI_Event, IndividualScopeEventMixin):
         # Record date and stage of starting treatment
         df.at[person_id, "brc_date_treatment"] = self.sim.date
         df.at[person_id, "brc_stage_at_which_treatment_given"] = df.at[person_id, "brc_status"]
+
+        # Update equipment used with treatment
+        # NB. read only with HSI run and healthsystem.summary logger set at the level INFO or higher
+        self.EQUIPMENT.update({'Anything used for mastectomy as I guess this is about'})
 
         # Schedule a post-treatment check for 12 months:
         hs.schedule_hsi_event(
