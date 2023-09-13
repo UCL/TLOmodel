c4d59a970961ac670ee5077173a441803afd2409
nicx503 <nic503@york.ac.uk>
2023-09-13 18:51:38 +0100

    added an additional CXR scale -up scenario at 20%

--- src/tlo/methods/tb.py
+++ src/tlo/methods/tb.py
@@ -1482,7 +1482,7 @@ class ScenarioSetupEvent(RegularEvent, PopulationScopeEventMixin):
         #increases probability of accessing chest xray by 10%
         if scenario == 3:
             self.sim.modules['HealthSystem'].override_availability_of_consumables(
-                 {175: 0.53})
+                 {175: 0.43})
 # ######################################################################
 class TbActiveCasePoll(RegularEvent, PopulationScopeEventMixin):
     """The Tb Regular Poll Event for assigning active infections
