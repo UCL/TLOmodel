9783ddae90e34cd728c5f388a49b039f21f8aab7
Matt Graham <matthew.m.graham@gmail.com>
2023-09-25 10:12:50 +0100

    Add option to check differences to script

--- src/scripts/automation/update_citation.py
+++ src/scripts/automation/update_citation.py
@@ -1,4 +1,7 @@
+"""Update citation file from contributors list or check if fill will be updated."""
+
 import argparse
+import difflib
 from pathlib import Path
 
 import yaml
@@ -54,6 +57,14 @@ if __name__ == "__main__":
         default=root_directory / "CITATION.cff",
         help="Path to existing citation file to update",
     )
+    parser.add_argument(
+        "--check",
+        action="store_true",
+        help=(
+            "Whether to check if citation file will be updated without modifying file "
+            "and raise an error if so."
+        ),
+    )
     args = parser.parse_args()
     assert args.contributors_file_path.exists()
     with open(args.contributors_file_path, "r") as f:
@@ -61,12 +72,31 @@ if __name__ == "__main__":
     assert args.citation_file_path.exists()
     with open(args.citation_file_path, "r") as f:
         citation = yaml.safe_load(f)
+        f.seek(0)
+        original_citation_lines = f.readlines()
     citation["authors"] = strip_and_filter_contributors(contributors)
-    with open(args.citation_file_path, "w") as f:
-        comment = (
-            "# Author list automatically generated from contributors.yaml by running\n"
-            "#     python src/scripts/automation/update_citation.py\n"
-            "# Any manual updates to author list here may be overwritten\n"
+    comment = (
+        "# Author list automatically generated from contributors.yaml by running\n"
+        "#     python src/scripts/automation/update_citation.py\n"
+        "# Any manual updates to author list here may be overwritten\n"
+    )
+    yaml_string = yaml.dump(citation, sort_keys=False, allow_unicode=True, indent=2)
+    updated_citation_lines = (comment + yaml_string).splitlines(keepends=True)
+    if args.check:
+        citation_diff = "".join(
+            difflib.unified_diff(
+                original_citation_lines,
+                updated_citation_lines,
+                fromfile=f"original ({args.citation_file_path})",
+                tofile="updated",
+            )
         )
-        yaml_string = yaml.dump(citation, sort_keys=False, allow_unicode=True, indent=2)
-        f.write(comment + yaml_string)
+        if citation_diff != "":
+            raise RuntimeError(
+                f"Citation file at {args.citation_file_path} would be updated:\n\n"
+                + citation_diff
+                + f"\nRe-run script {__file__} to update."
+            )
+    else:
+        with open(args.citation_file_path, "w") as f:
+            f.writelines(updated_citation_lines)
