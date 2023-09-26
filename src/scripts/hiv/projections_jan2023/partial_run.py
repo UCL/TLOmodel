9c042797c3d5966fb6ec7d806576be9ca6b66d6f
nicx503 <nic503@york.ac.uk>
2023-09-26 04:33:00 +0100

    added additional scenarios for sensitivity analysis

--- src/scripts/hiv/projections_jan2023/partial_run.py
+++ src/scripts/hiv/projections_jan2023/partial_run.py
@@ -26,10 +26,10 @@ warnings.simplefilter("ignore", (UserWarning, RuntimeWarning))
 class ImpactOfTbDaHx(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = random.randint(0, 50000)
+        self.seed = 2023 #random.randint(0, 50000)
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2013, 12, 31)
-        self.pop_size = 500
+        self.pop_size = 1000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 4
