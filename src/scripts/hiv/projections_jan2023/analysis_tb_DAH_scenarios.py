6ab4a1b113aeabe575448e534730e73acdd64232
nicx503 <nic503@york.ac.uk>
2023-09-27 09:34:21 +0100

    added additional scenarios for sensitivity analysis

--- src/scripts/hiv/projections_jan2023/analysis_tb_DAH_scenarios.py
+++ src/scripts/hiv/projections_jan2023/analysis_tb_DAH_scenarios.py
@@ -24,7 +24,7 @@ outputspath = Path("./outputs/newton.chagoma@york.ac.uk")
 datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 
 # Get basic information about the results
-results_folder = get_scenario_outputs("Tb_DAH_impact_scenarios-2023-09-26T113542Z", outputspath)[-1]
+results_folder = get_scenario_outputs("Tb_DAH_impact_scenarios-2023-09-26T225054Z", outputspath)[-1]
 log = load_pickled_dataframes(results_folder)
 info = get_scenario_info(results_folder)
 print(info)
