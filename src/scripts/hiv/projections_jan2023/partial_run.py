1b6ada125b2fbaf8ec1dda3212727e3aa6cdb053
nicx503 <nic503@york.ac.uk>
2023-10-04 11:39:21 +0100

    added additional scenarios for sensitivity analysis

--- src/scripts/hiv/projections_jan2023/partial_run.py
+++ src/scripts/hiv/projections_jan2023/partial_run.py
@@ -87,7 +87,7 @@ class ImpactOfTbDaHx(BaseScenario):
             "No CXR Available": {
                 'Tb': {
                     'scenario': 2,
-                    'scaling_factor_WHO': 1.8,
+                    'scaling_factor_WHO': 1.95,
                 },
             },
             "CXR scale_up": {
