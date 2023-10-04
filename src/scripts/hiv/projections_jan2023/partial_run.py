08ff6583278267d0eb5f1c913a2e8b4c49200135
nicx503 <nic503@york.ac.uk>
2023-10-04 13:34:02 +0100

    added additional scenarios for sensitivity analysis

--- src/scripts/hiv/projections_jan2023/partial_run.py
+++ src/scripts/hiv/projections_jan2023/partial_run.py
@@ -35,7 +35,7 @@ class ImpactOfTbDaHx(BaseScenario):
         self.runs_per_draw = 3
     def log_configuration(self):
         return {
-            'filename': 'Tb_DAH_scenarios_test_run27_partial',
+            'filename': 'Tb_DAH_scenarios_test_run28_partial',
             'directory': Path('./outputs/nic503@york.ac.uk'),
             'custom_levels': {
                 '*': logging.WARNING,
@@ -93,6 +93,7 @@ class ImpactOfTbDaHx(BaseScenario):
             "CXR scale_up": {
                  'Tb': {
                      'scenario': 3,
+                     'scaling_factor_WHO': 1.3,
                  }
             },
             "Outreach services": {
