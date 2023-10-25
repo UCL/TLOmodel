0018579c970847d5bb9811277107cf577988d1bd
nicx503 <nic503@york.ac.uk>
2023-10-25 10:35:19 +0100

    Revised scenario for assessing impact

--- src/scripts/hiv/projections_jan2023/partial_run.py
+++ src/scripts/hiv/projections_jan2023/partial_run.py
@@ -74,7 +74,7 @@ class ImpactOfTbDaHx(BaseScenario):
             "Baseline": {
                 'Tb': {
                     'scenario': 0,
-                    'scaling_factor_WHO': 1.5,
+                    'scaling_factor_WHO': 1.6,
                 },
             },
         # overrides availability of Xpert
@@ -94,7 +94,7 @@ class ImpactOfTbDaHx(BaseScenario):
             "CXR scale_up": {
                  'Tb': {
                      'scenario': 3,
-                    'scaling_factor_WHO': 0.9,
+                    'scaling_factor_WHO': 0.95,
                  }
             },
                  "Outreach services": {
