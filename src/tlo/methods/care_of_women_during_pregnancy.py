ddfeefe5c011733ef5327a36bab6af9384e63790
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-10-25 18:49:13 +0100

    ac: empty declaration of equipment

--- src/tlo/methods/care_of_women_during_pregnancy.py
+++ src/tlo/methods/care_of_women_during_pregnancy.py
@@ -1442,6 +1442,7 @@ class HSI_CareOfWomenDuringPregnancy_FirstAntenatalCareContact(HSI_Event, Indivi
         self.TREATMENT_ID = 'AntenatalCare_Outpatient'
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'AntenatalFirst': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -1537,6 +1538,7 @@ class HSI_CareOfWomenDuringPregnancy_SecondAntenatalCareContact(HSI_Event, Indiv
         self.TREATMENT_ID = 'AntenatalCare_Outpatient'
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'ANCSubsequent': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -1632,6 +1634,7 @@ class HSI_CareOfWomenDuringPregnancy_ThirdAntenatalCareContact(HSI_Event, Indivi
         self.TREATMENT_ID = 'AntenatalCare_Outpatient'
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'ANCSubsequent': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -1711,6 +1714,7 @@ class HSI_CareOfWomenDuringPregnancy_FourthAntenatalCareContact(HSI_Event, Indiv
         self.TREATMENT_ID = 'AntenatalCare_Outpatient'
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'ANCSubsequent': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -1786,6 +1790,7 @@ class HSI_CareOfWomenDuringPregnancy_FifthAntenatalCareContact(HSI_Event, Indivi
         self.TREATMENT_ID = 'AntenatalCare_Outpatient'
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'ANCSubsequent': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -1859,6 +1864,7 @@ class HSI_CareOfWomenDuringPregnancy_SixthAntenatalCareContact(HSI_Event, Indivi
         self.TREATMENT_ID = 'AntenatalCare_Outpatient'
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'ANCSubsequent': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -1927,6 +1933,7 @@ class HSI_CareOfWomenDuringPregnancy_SeventhAntenatalCareContact(HSI_Event, Indi
         self.TREATMENT_ID = 'AntenatalCare_Outpatient'
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'ANCSubsequent': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -1989,6 +1996,7 @@ class HSI_CareOfWomenDuringPregnancy_EighthAntenatalCareContact(HSI_Event, Indiv
         self.TREATMENT_ID = 'AntenatalCare_Outpatient'
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'ANCSubsequent': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -2045,6 +2053,7 @@ class HSI_CareOfWomenDuringPregnancy_FocusedANCVisit(HSI_Event, IndividualScopeE
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({('AntenatalFirst' if (self.visit_number == 1)
                                                                   else 'ANCSubsequent'): 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
 
@@ -2161,6 +2170,7 @@ class HSI_CareOfWomenDuringPregnancy_PresentsForInductionOfLabour(HSI_Event, Ind
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({})
         self.BEDDAYS_FOOTPRINT = self.make_beddays_footprint({'maternity_bed': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -2201,6 +2211,7 @@ class HSI_CareOfWomenDuringPregnancy_AntenatalWardInpatientCare(HSI_Event, Indiv
 
         beddays = self.module.calculate_beddays(person_id)
         self.BEDDAYS_FOOTPRINT = self.make_beddays_footprint({'maternity_bed': beddays})
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -2458,6 +2469,7 @@ class HSI_CareOfWomenDuringPregnancy_AntenatalOutpatientManagementOfAnaemia(HSI_
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'Over5OPD': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
         self.ALERT_OTHER_DISEASES = []
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -2507,6 +2519,7 @@ class HSI_CareOfWomenDuringPregnancy_AntenatalOutpatientManagementOfGestationalD
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'Over5OPD': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1a'
         self.ALERT_OTHER_DISEASES = []
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -2593,6 +2606,7 @@ class HSI_CareOfWomenDuringPregnancy_PostAbortionCaseManagement(HSI_Event, Indiv
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({})
         self.ACCEPTED_FACILITY_LEVEL = '1b'  # any hospital?
         self.BEDDAYS_FOOTPRINT = self.make_beddays_footprint({'maternity_bed': 3})  # todo: check with TC
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -2683,6 +2697,7 @@ class HSI_CareOfWomenDuringPregnancy_TreatmentForEctopicPregnancy(HSI_Event, Ind
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({'MajorSurg': 1})
         self.ACCEPTED_FACILITY_LEVEL = '1b'
         self.BEDDAYS_FOOTPRINT = self.make_beddays_footprint({'maternity_bed': 5})  # todo: check with TC
+        self.EQUIPMENT = set()
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
