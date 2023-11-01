87a75e84610698fe23b8ef8ad2fef2e82ea75eb9
nicx503 <nic503@york.ac.uk>
2023-11-01 22:54:55 +0000

    Revised scenario for assessing impact

--- src/tlo/methods/tb.py
+++ src/tlo/methods/tb.py
@@ -1471,16 +1471,16 @@ class ScenarioSetupEvent(RegularEvent, PopulationScopeEventMixin):
             return
         # sets availability of xpert to nil
         if scenario == 1:
-            self.sim.modules['HealthSystem'].override_availability_of_consumables(
-                {187: 0.0})
+            # self.sim.modules['HealthSystem'].override_availability_of_consumables(
+            #     {187: 0.0})
             self.sim.modules['HealthSystem'].get_item_codes_from_package_name({187: 0})
 
             # self.sim.modules['HealthSystem'].override_availability_of_consumables(
             #     {175: 0.53})
         # sets availability of xray to nil
         if scenario == 2:
-           self.sim.modules['HealthSystem'].override_availability_of_consumables(
-                 {175: 0.0})
+           # self.sim.modules['HealthSystem'].override_availability_of_consumables(
+           #       {175: 0.0})
            self.sim.modules['HealthSystem'].get_item_codes_from_package_name({175: 0})
            # self.sim.modules['HealthSystem'].override_availability_of_consumables(
            #        {187: 0.08})
