c7a4e6a48bf392054d8923ba8e65177c074d297f
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2023-11-03 12:25:03 +0000

    update population size to match others

--- src/scripts/overview_paper/B_finding_effects_of_each_treatment/scenario_effect_of_each_treatment.py
+++ src/scripts/overview_paper/B_finding_effects_of_each_treatment/scenario_effect_of_each_treatment.py
@@ -36,7 +36,7 @@ class EffectOfEachTreatment(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2020, 1, 1)
-        self.pop_size = 75_000
+        self.pop_size = 100_000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 5
