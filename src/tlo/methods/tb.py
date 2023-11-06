c204e64d9059ada7293dbb72b560e88670a278a5
nicx503 <nic503@york.ac.uk>
2023-11-06 16:11:01 +0000

    Revised scenario for assessing impact

--- src/tlo/methods/tb.py
+++ src/tlo/methods/tb.py
@@ -1471,8 +1471,8 @@ class ScenarioSetupEvent(RegularEvent, PopulationScopeEventMixin):
             return
         # sets availability of xpert to nil
         if scenario == 1:
-            self.sim.modules['HealthSystem'].override_availability_of_consumables({187: 0.0})
-           #self.sim.modules['HealthSystem'].get_item_codes_from_package_name({187: 0})
+            #self.sim.modules['HealthSystem'].override_availability_of_consumables({187: 0.0})
+           self.sim.modules['HealthSystem'].get_item_codes_from_package_name({187: 0})
 
             # self.sim.modules['HealthSystem'].override_availability_of_consumables(
             #     {175: 0.53})
@@ -1484,8 +1484,8 @@ class ScenarioSetupEvent(RegularEvent, PopulationScopeEventMixin):
            #        {187: 0.08})
         #increases probability of accessing chest xray by 10%
         if scenario == 3:
-          self.sim.modules['HealthSystem'].override_availability_of_consumables({175: 0.63})
-          #self.sim.modules['HealthSystem'].get_item_codes_from_package_name({175: 0.8})
+          #self.sim.modules['HealthSystem'].override_availability_of_consumables({175: 0.63})
+          self.sim.modules['HealthSystem'].get_item_codes_from_package_name({175: 0.8})
 
         # if scenario == 4:
         #         self.sim.modules['HealthSystem'].override_availability_of_consumables(
