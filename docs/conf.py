5acec8156597e4dfc2a93bfb7cdce524673b223d
Matt Graham <matthew.m.graham@gmail.com>
2023-11-07 12:31:48 +0000

    Get documentation version from installed tlo metadata (#1180)

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ project = 'TLOmodel'
 year = '2021'
 author = 'The TLOmodel Team'
 copyright = '{0}, {1}'.format(year, author)
-release = get_version('setuptools_scm')
+release = get_version('tlo')
 version = ".".join(release.split('.')[:2])
 
 pygments_style = 'trac'
