7dbc22f629342aa4ee391fbc9cf1a7037a9c5c94
nicx503 <nic503@york.ac.uk>
2023-11-11 19:06:36 +0000

    Revised scenario for assessing impact

--- src/scripts/hiv/projections_jan2023/partial_run.py
+++ src/scripts/hiv/projections_jan2023/partial_run.py
@@ -29,7 +29,7 @@ class ImpactOfTbDaHx(BaseScenario):
         self.seed = 0 #random.randint(0, 50000)
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2015, 12, 31)
-        self.pop_size =2000
+        self.pop_size =5000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 3
