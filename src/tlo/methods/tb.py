6a2b28b91dac810ab2fc775045f2771bc640a5ad
nicx503 <nic503@york.ac.uk>
2023-11-15 19:44:32 +0000

    Revised scenario for assessing impact

--- src/tlo/methods/tb.py
+++ src/tlo/methods/tb.py
@@ -1492,21 +1492,21 @@ class ScenarioSetupEvent(RegularEvent, PopulationScopeEventMixin):
            self.sim.modules['HealthSystem'].override_availability_of_consumables({175: 0.61})
            self.sim.modules['HealthSystem'].override_availability_of_consumables({187: 0.85})
            #.sim.modules["Tb"].parameters["probability_community_chest_xray"] = 0.0
-           # self.sim.modules["Tb"].parameters["WHO scaling factor"] = 1.
+           self.sim.modules["Tb"].parameters["WHO scaling factor"] = 1.55
 
         # increase CXR by 20%
         if scenario == 4:
-            #self.sim.modules["Tb"].parameters["probability_community_chest_xray"] = 0.0
+            self.sim.modules["Tb"].parameters["probability_community_chest_xray"] = 0.0
             self.sim.modules['HealthSystem'].override_availability_of_consumables({175: 0.71})
             self.sim.modules['HealthSystem'].override_availability_of_consumables({187: 0.85})
-            # self.sim.modules["Tb"].parameters["WHO scaling factor"] = 1.0
+            self.sim.modules["Tb"].parameters["WHO scaling factor"] = 1.50
 
         # increase CXR by 30%
         if scenario == 5:
             self.sim.modules["Tb"].parameters["probability_community_chest_xray"] = 0.0
             self.sim.modules['HealthSystem'].override_availability_of_consumables({175: 0.81})
             self.sim.modules['HealthSystem'].override_availability_of_consumables({187: 0.85})
-            # self.sim.modules["Tb"].parameters["WHO scaling factor"] = 1.0
+            self.sim.modules["Tb"].parameters["WHO scaling factor"] = 1.45
 
         # Introduce community Xray
         if scenario == 6:
