d13450775c94ec2c28cc4da45d4458c9b9aaf482
nicx503 <nic503@york.ac.uk>
2023-11-15 22:58:16 +0000

    Revised scenario for assessing impact

--- src/scripts/hiv/projections_jan2023/partial_run02.py
+++ src/scripts/hiv/projections_jan2023/partial_run02.py
@@ -1,12 +1,12 @@
 """  This script partially run the tb code as running the full model might take longer
- tlo scenario-run src/scripts/hiv/projections_jan2023/partial_run.py
+ tlo scenario-run src/scripts/hiv/projections_jan2023/partial_run02.py
   execute a single run:"""
 import warnings
 from typing import Dict
 from tlo.scenario import BaseScenario
 import random
 from pathlib import Path
-from tlo import Date,  logging
+from tlo import Date, logging
 from tlo.methods import (
     demography,
     tb,
@@ -26,13 +26,14 @@ warnings.simplefilter("ignore", (UserWarning, RuntimeWarning))
 class ImpactOfTbDaHx(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = 0 #random.randint(0, 50000)
+        self.seed = 0  # random.randint(0, 50000)
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2012, 12, 31)
-        self.pop_size = 3000
+        self.pop_size = 1000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
-        self.runs_per_draw = 3
+        self.runs_per_draw = 2
+
     def log_configuration(self):
         return {
             'filename': 'Tb_DAH_scenarios_test_run31_partial',
@@ -47,6 +48,7 @@ class ImpactOfTbDaHx(BaseScenario):
                 'tlo.methods.hiv': logging.INFO,
             }
         }
+
     def modules(self):
         return [
             demography.Demography(resourcefilepath=self.resources),
@@ -67,43 +69,70 @@ class ImpactOfTbDaHx(BaseScenario):
         else:
             return None
 
+class ImpactOfTbDaHx:
     def _get_scenarios(self) -> Dict[str, Dict]:
         """Return the Dict with values for the parameters that are changed, keyed by a name for the scenario."""
         return {
-        # baseline scenario
+            # baseline scenario
             "Baseline": {
                 'Tb': {
                     'scenario': 0,
+                    'probability_community_chest_xray': 0.0,
                 },
+                **({'HealthSystem': {'cons_availability': {'item_code': {157: [0.51]}}}} if 0 == 0 else {}),
             },
-        # overrides availability of Xpert
+            # overrides availability of Xpert to nil
             "No Xpert Available": {
                 'Tb': {
                     'scenario': 1,
-                   # 'scaling_factor_WHO': 1.95,
+                    'probability_community_chest_xray': 0.0,
                 },
+                **({'HealthSystem': {'cons_availability': {'item_code': {187: ['none']}}}} if 1 == 0 else {}),
             },
-        # overrides availability of CXR
+            # overrides availability of CXR to nil
             "No CXR Available": {
                 'Tb': {
                     'scenario': 2,
-                    'scaling_factor_WHO': 1.9,
+                    'probability_community_chest_xray': 0.0,
                 },
+                **({'HealthSystem': {'cons_availability': {'item_code': {175: ['none']}}}} if 2 == 0 else {}),
             },
-            "CXR scale_up": {
-                 'Tb': {
-                     'scenario': 3,
-                     'scaling_factor_WHO': 1.25,
-                 }
+            # increase CXR by 10 percentage points
+            "CXR_scale_up10": {
+                'Tb': {
+                    'scenario': 3,
+                    'probability_community_chest_xray': 0.0,
+                },
+                **({'HealthSystem': {'cons_availability': {'item_code': {175: 0.61}}}} if 3 == 0 else {}),
             },
+
+            # increase CXR by 20 percentage points
+            "CXR_scale_up20": {
+                'Tb': {
+                    'scenario': 4,
+                    'probability_community_chest_xray': 0.0,
+                },
+                **({'HealthSystem': {'cons_availability': {'item_code': {175: 0.71}}}} if 4 == 0 else {}),
+            },
+
+            # increase CXR by 30 percentage points
+            "CXR_scale_up30": {
+                'Tb': {
+                    'scenario': 5,
+                    'probability_community_chest_xray': 0.0,
+                },
+                **({'HealthSystem': {'cons_availability': {'item_code': {175: 0.81}}}} if 5 == 0 else {}),
+            },
+
+            # introduce outreach services
             "Outreach services": {
                 'Tb': {
-                     'scenario': 0,
-                     'probability_community_chest_xray': 0.1,
-                     'scaling_factor_WHO': 0.89,
+                    'scenario': 6,
+                    'probability_community_chest_xray': 0.1,
                 }
             },
         }
+
 if __name__ == '__main__':
     from tlo.cli import scenario_run
     scenario_run([__file__])
