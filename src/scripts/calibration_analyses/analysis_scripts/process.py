86ac92900a7b62514af53fc8c7ef70544cdc08a4
Mosè Giordano <mose@gnu.org>
2023-11-15 11:56:31 +0000

    [CI] Run test jobs on self-hosted runners with `test` label
    This is to avoid running test jobs on dev machines.

--- src/scripts/calibration_analyses/analysis_scripts/process.py
+++ src/scripts/calibration_analyses/analysis_scripts/process.py
@@ -1,4 +1,3 @@
-import os
 import sys
 from pathlib import Path
 
@@ -14,4 +13,6 @@ if __name__ == '__main__':
     _output_dir_for_processed = Path(sys.argv[1])
     _run_directory_name = sys.argv[2]
     _resources_dir = Path(sys.argv[3])
-    process(_output_dir_for_processed, _output_dir_for_processed.parent / _run_directory_name, _resources_dir)
+    process(_output_dir_for_processed,
+            _output_dir_for_processed.parent / _run_directory_name,
+            _resources_dir)
