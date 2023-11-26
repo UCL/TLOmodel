704a183d210a9f1631e39a547a50262631dd9033
nicx503 <nic503@york.ac.uk>
2023-11-26 13:55:50 +0000

    Revised scenario for assessing impact

--- src/scripts/hiv/projections_jan2023/tb_DAH_impact.py
+++ src/scripts/hiv/projections_jan2023/tb_DAH_impact.py
@@ -53,7 +53,7 @@ class ImpactOfTbDaH(BaseScenario):
 
     def log_configuration(self):
         return {
-            'filename': 'Tb_DAH_impactx44',
+            'filename': 'Tb_DAH_impactx45',
             'directory': Path('./outputs/nic503@york.ac.uk'),
             'custom_levels': {
                 '*': logging.WARNING,
