7dd8ce32dbcd6f3b46d17213d430d8c9d65f23fa
nicx503 <nic503@york.ac.uk>
2023-11-28 16:58:45 +0000

    Revised scenario for assessing impact

--- src/scripts/hiv/projections_jan2023/tb_DAH_impact.py
+++ src/scripts/hiv/projections_jan2023/tb_DAH_impact.py
@@ -46,14 +46,14 @@ class ImpactOfTbDaH(BaseScenario):
        # self.seed = random.randint(0, 50000),
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2012, 12, 31)
-        self.pop_size = 5000
+        self.pop_size = 10000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 2
 
     def log_configuration(self):
         return {
-            'filename': 'Tb_DAH_impactx56',
+            'filename': 'Tb_DAH_impactx57',
             'directory': Path('./outputs/nic503@york.ac.uk'),
             'custom_levels': {
                 '*': logging.WARNING,
@@ -119,7 +119,7 @@ class ImpactOfTbDaH(BaseScenario):
             "Outreach services": {
                 'Tb': {
                     'scenario': 4,
-                    'probability_community_chest_xray': 0.30,
+                    'probability_community_chest_xray': 0.15,
                 }
             },
         }
