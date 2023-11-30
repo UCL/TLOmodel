b69b20764a8d3b67579f5a0a479d02b9d058d0e4
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-11-30 18:03:21 +0000

    hs: back to set() as default

--- src/tlo/methods/healthsystem.py
+++ src/tlo/methods/healthsystem.py
@@ -183,7 +183,7 @@ class HSI_Event:
         self._received_info_about_bed_days = None
         self.expected_time_requests = {}
         self.facility_info = None
-        self.EQUIPMENT = None
+        self.EQUIPMENT = set()
 
     @property
     def bed_days_allocated_to_this_event(self):
@@ -401,7 +401,7 @@ class HSI_Event:
             beddays_footprint=tuple(
                 sorted((k, v) for k, v in self.BEDDAYS_FOOTPRINT.items() if v > 0)
             ),
-            equipment=(tuple(sorted(self.EQUIPMENT))) if self.EQUIPMENT is not None else None
+            equipment=(tuple(sorted(self.EQUIPMENT)))
         )
 
 
