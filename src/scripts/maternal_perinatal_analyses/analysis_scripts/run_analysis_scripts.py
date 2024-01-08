89e7e61bd3255043c6831c1020cceb7472b2f33b
joehcollins <joseph.h.collins@ucl.ac.uk>
2024-01-08 16:31:25 +0000

    update priority policy resource file to include new treatment ids

--- src/scripts/maternal_perinatal_analyses/analysis_scripts/run_analysis_scripts.py
+++ src/scripts/maternal_perinatal_analyses/analysis_scripts/run_analysis_scripts.py
@@ -4,20 +4,20 @@ import maternal_newborn_health_thesis_analysis
 scenario_dict1 = {'Status Quo': 'baseline_scenario',
                   'Intervention 1': 'increased_anc_scenario',
                   'Intervention 2': 'anc_scenario_plus_cons_and_qual',
-                  'Sensitivity (min)': 'min_anc_sensitivity_scenario',
-                  'Sensitivity (max)': 'max_anc_sensitivity_scenario'}
+                  'Sensitivity 1': 'min_anc_sensitivity_scenario',
+                  'Sensitivity 2': 'max_anc_sensitivity_scenario'}
 
 scenario_dict2 = {'Status Quo': 'baseline_scenario',
                   'Intervention 1': 'bemonc',
                   'Intervention 2': 'cemonc',
-                  'Sensitivity (min)': 'min_sba_sensitivity_analysis',
-                  'Sensitivity (max)': 'max_sba_sensitivity_analysis'}
+                  'Sensitivity 1': 'min_sba_sensitivity_analysis',
+                  'Sensitivity 2': 'max_sba_sensitivity_analysis'}
 
 scenario_dict3 = {'Status Quo': 'baseline_scenario',
                   'Intervention 1': 'increased_pnc_scenario',
                   'Intervention 2': 'pnc_scenario_plus_cons',
-                  'Sensitivity (min)': 'min_pnc_sensitivity_analysis',
-                  'Sensitivity (max)': 'max_pnc_sensitivity_analysis'}
+                  'Sensitivity 1': 'min_pnc_sensitivity_analysis',
+                  'Sensitivity 2': 'max_pnc_sensitivity_analysis'}
 
 
 # define key variables used within the analysis scripts
@@ -30,7 +30,7 @@ for scenario_dict, service, colours in zip([scenario_dict1, scenario_dict2, scen
                                            ['anc', 'sba', 'pnc'],
                                            [['#8c510a', '#d73027', '#8073ac', '#01665e', '#c51b7d'],
                                             ['#8c510a', '#de77ae', '#5aae61', '#e08214',  '#4575b4'],
-                                            ['#8c510a', '#b2e2e2', '#1b7837', '#f46d43', '#80cdc1']]):
+                                            ['#8c510a', '#dfc27dtg', '#1b7837', '#f46d43', '#80cdc1']]):
 
     service_of_interest = service
     scen_colours = colours
