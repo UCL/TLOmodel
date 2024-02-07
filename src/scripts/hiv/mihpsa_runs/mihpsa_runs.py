6ae49e5ff2f3663d529f982119924ee1410f682f
tdm32 <t.mangal@imperial.ac.uk>
2024-02-07 13:05:52 +0000

    set up batch runs for mihpsa

--- src/scripts/hiv/mihpsa_runs/mihpsa_runs.py
+++ src/scripts/hiv/mihpsa_runs/mihpsa_runs.py
@@ -55,10 +55,10 @@ class TestScenario(BaseScenario):
         super().__init__(
             seed=0,
             start_date=Date(2010, 1, 1),
-            end_date=Date(2014, 1, 1),
-            initial_population_size=1000,
+            end_date=Date(2046, 1, 1),
+            initial_population_size=150_000,
             number_of_draws=1,
-            runs_per_draw=1,
+            runs_per_draw=5,
         )
 
     def log_configuration(self):
