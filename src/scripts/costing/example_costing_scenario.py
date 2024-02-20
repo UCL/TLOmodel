b8fd8fc966da59055feaaeaaa4245f878050f656
sm2511 <sakshi.mohan@york.ac.uk>
2024-02-20 19:06:42 +0000

    create example scenario for costing script

---
+++ src/scripts/costing/example_costing_scenario.py
@@ -0,0 +1,48 @@
+'''
+
+
+Run on the batch system using:
+```tlo batch-submit src/scripts/costing/example_costing_scenario.py```
+
+or locally using:
+    ```tlo batch-submit src/scripts/costing/example_costing_scenario.py```
+
+'''
+
+from tlo import Date, logging
+from tlo.methods.fullmodel import fullmodel
+from tlo.scenario import BaseScenario
+
+class SampleCostingScenario(BaseScenario):
+    def __init__(self):
+        super().__init__()
+        self.seed = 0
+        self.start_date = Date(2010, 1, 1)
+        self.end_date = Date(2020, 1, 1)
+        self.pop_size = 100  # <- recommended population size for the runs
+        self.number_of_draws = 1  # <- one scenario
+        self.runs_per_draw = 3  # <- repeated this many times
+
+    def log_configuration(self):
+        return {
+            'filename': 'example_costing_scenario',
+            'directory': './outputs',  # <- (specified only for local running)
+            'custom_levels': {
+                '*': logging.WARNING,
+                'tlo.methods.demography': logging.INFO,
+                'tlo.methods.healthsystem': logging.INFO,
+                'tlo.methods.healthsystem.summary': logging.INFO,
+            }
+        }
+
+    def modules(self):
+        return fullmodel(resourcefilepath=self.resources)
+
+    def draw_parameters(self, draw_number, rng):
+        return get_parameters_for_status_quo()
+
+
+if __name__ == '__main__':
+    from tlo.cli import scenario_run
+
+    scenario_run([__file__])
