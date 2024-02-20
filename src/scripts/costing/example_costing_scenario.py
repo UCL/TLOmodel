6b730ea8d7b796a040e5bba03898939ab36878d7
sm2511 <sakshi.mohan@york.ac.uk>
2024-02-20 19:44:48 +0000

    reduce scenario size

--- src/scripts/costing/example_costing_scenario.py
+++ src/scripts/costing/example_costing_scenario.py
@@ -18,10 +18,10 @@ class SampleCostingScenario(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2020, 1, 1)
+        self.end_date = Date(2013, 1, 1)
         self.pop_size = 100  # <- recommended population size for the runs
         self.number_of_draws = 2  # <- one scenario
-        self.runs_per_draw = 3  # <- repeated this many times
+        self.runs_per_draw = 2 # <- repeated this many times
 
     def log_configuration(self):
         return {
