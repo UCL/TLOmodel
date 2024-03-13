2f16e280dc3e99e65473711141ba541a078ea64b
Emmanuel Mnjowe <32415622+mnjowe@users.noreply.github.com>
2024-03-13 14:19:48 +0200

    Updates to Lifestyle Module (#545)
    Co-authored-by: Asif Tamuri <tamuri@gmail.com>
    Co-authored-by: Tim Hallett <39991060+tbhallett@users.noreply.github.com>
    Co-authored-by: Matt Graham <matthew.m.graham@gmail.com>
    Co-authored-by: joehcollins <joseph.h.collins@ucl.ac.uk>

--- tests/test_labour.py
+++ tests/test_labour.py
@@ -230,8 +230,9 @@ def test_event_scheduling_for_care_seeking_during_home_birth(seed):
     mni[mother_id]['sought_care_labour_phase'] = 'none'
     sim.date = sim.date + pd.DateOffset(days=5)
 
-    # force a complication
+    # force a complication and care seeking
     params['prob_pph_uterine_atony'] = 1.0
+    params['prob_careseeking_for_complication_pn'] = 1.0
 
     # run postpartum home event
     home_birth_pp = labour.BirthAndPostnatalOutcomesEvent(mother_id=mother_id, module=sim.modules['Labour'])
