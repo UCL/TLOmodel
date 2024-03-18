b7b1e0829d788219c68db9513e567dd168d4ae04
willGraham01 <1willgraham@gmail.com>
2024-03-18 11:47:06 +0000

    Lint pass and type-check updates

--- src/tlo/methods/labour.py
+++ src/tlo/methods/labour.py
@@ -2298,7 +2298,7 @@ class Labour(Module):
         self,
         patient_id: int,
         patient_details: NamedTuple = None,
-        random_state: RandomState = None,
+        random_state: "RandomState" = None,
         **kwargs,
     ) -> Tuple[List[Tuple["HSI_Event", Dict[str, Any]]], Dict[str, Any]]:
         event_info = []
