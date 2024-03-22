1be4c0d46fd73abee932bbe59fbe2569847fea99
tdm32 <t.mangal@imperial.ac.uk>
2024-03-22 14:02:11 +0000

    test runs

--- src/scripts/malaria/impact_analysis/analysis_scenarios.py
+++ src/scripts/malaria/impact_analysis/analysis_scenarios.py
@@ -40,7 +40,7 @@ class EffectOfProgrammes(BaseScenario):
     def __init__(self):
         super().__init__()
         self.seed = 0
-        self.start_date = Date(2010, 1, 1)  # todo update these for full runs
+        self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2014, 1, 1)  # todo update these for full runs
         self.pop_size = 15_000  # todo update these for full runs
         self.number_of_draws = 2  # todo update these for full runs
