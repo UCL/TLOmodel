9dd58d870f13be9af6f221a6cbd6c52c578d113f
tdm32 <t.mangal@imperial.ac.uk>
2024-03-22 18:50:02 +0000

    set up full runs

--- src/scripts/malaria/impact_analysis/analysis_scenarios.py
+++ src/scripts/malaria/impact_analysis/analysis_scenarios.py
@@ -41,9 +41,9 @@ class EffectOfProgrammes(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2014, 1, 1)  # todo update these for full runs
-        self.pop_size = 15_000  # todo update these for full runs
-        self.number_of_draws = 1  # todo update these for full runs
+        self.end_date = Date(2020, 1, 1)  # todo update these for full runs
+        self.pop_size = 100_000  # todo update these for full runs
+        self.number_of_draws = 9  # todo update these for full runs
         self.runs_per_draw = 1  # todo update these for full runs
 
         self.treatment_effects = pd.read_excel(
@@ -105,23 +105,20 @@ class EffectOfProgrammes(BaseScenario):
 
         return {
             'HealthSystem': {
-                # 'Service_Availability': [service_availability['Everything'], service_availability['Everything'],
-                #                          service_availability['Everything'], service_availability['Everything'],
-                #                          service_availability['Everything'],
-                #                          service_availability['No_Hiv_*'], service_availability['No_Tb_*'],
-                #                          service_availability['No_Malaria_*'], service_availability['No_HTM']][
-                #     draw_number],
-                'Service_Availability': service_availability['Everything'],
+                'Service_Availability': [service_availability['Everything'], service_availability['Everything'],
+                                         service_availability['Everything'], service_availability['Everything'],
+                                         service_availability['Everything'],
+                                         service_availability['No_Hiv_*'], service_availability['No_Tb_*'],
+                                         service_availability['No_Malaria_*'], service_availability['No_HTM']][
+                    draw_number],
                 'use_funded_or_actual_staffing': 'funded',
                 'mode_appt_constraints': 1,
                 'policy_name': 'Naive',
             },
-            # 'Hiv': {
-            #     'scenario': [0, 1, 2, 3, 5, 0, 0, 3, 3][draw_number],
-            # },
             'Hiv': {
-                'scenario': 2,
+                'scenario': [0, 1, 2, 3, 5, 0, 0, 3, 3][draw_number],
             },
+
         }
 
 
