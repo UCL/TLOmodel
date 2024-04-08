2f372b191d916f4def8bfecbf7e3daa7ca1da1a9
Margherita Molaro <48129834+marghe-molaro@users.noreply.github.com>
2024-04-08 09:15:22 +0100

    Submit remaining 8/10 runs for const growth capabilities expansion scenarios

--- src/scripts/healthsystem/impact_of_const_capabilities_expansion/scenario_impact_of_const_capabilities_expansion.py
+++ src/scripts/healthsystem/impact_of_const_capabilities_expansion/scenario_impact_of_const_capabilities_expansion.py
@@ -27,13 +27,13 @@ from tlo.scenario import BaseScenario
 class ImpactOfHealthSystemMode(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = 0
+        self.seed = 2
         self.start_date = Date(2010, 1, 1)
         self.end_date = self.start_date + pd.DateOffset(years=31)
         self.pop_size = 100_000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
-        self.runs_per_draw = 2
+        self.runs_per_draw = 8
 
     def log_configuration(self):
         return {
