740531099d5028c68ac0a81ceaa6622c4a2adfc9
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2024-04-23 17:33:16 +0100

    temporarily make the scale-run small

--- src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
+++ src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
@@ -21,10 +21,10 @@ class LongRun(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2031, 1, 1)  # The simulation will stop before reaching this date.
-        self.pop_size = 20_000
+        self.end_date = Date(2016, 1, 1)  # The simulation will stop before reaching this date.
+        self.pop_size = 10_000
         self.number_of_draws = 1
-        self.runs_per_draw = 10
+        self.runs_per_draw = 2
 
     def log_configuration(self):
         return {
