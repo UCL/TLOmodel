4dcf29cd549ea7e56abd8f9fc7f393527ea4b1a8
joehcollins <joseph.h.collins@ucl.ac.uk>
2024-04-30 17:07:36 +0100

    update analysis file

--- src/scripts/maternal_perinatal_analyses/analysis_scripts/run_analysis_scripts.py
+++ src/scripts/maternal_perinatal_analyses/analysis_scripts/run_analysis_scripts.py
@@ -19,6 +19,9 @@ scenario_dict3 = {'Status Quo': 'baseline_scenario',
                   'PN min.': 'min_pnc_sensitivity_analysis',
                   'PN max.': 'max_pnc_sensitivity_analysis'}
 
+scenario_dict4 = {'Status Quo': 'baseline_scenario',
+                  'All services coverage\nand qual': 'uhc_cov_qual',
+                  'All services max.': 'max_uhc'}
 
 # define key variables used within the analysis scripts
 intervention_years = list(range(2023, 2031))
@@ -26,12 +29,14 @@ sim_years = list(range(2012, 2031))
 output_path = './outputs/sejjj49@ucl.ac.uk/'
 
 
-for scenario_dict, service, colours in zip([scenario_dict1, scenario_dict2, scenario_dict3],
-                                           ['anc', 'sba', 'pnc'],
-                                           [['#8c510a', '#d73027', '#8073ac', '#01665e', '#c51b7d'],
-                                            ['#8c510a', '#de77ae', '#5aae61', '#e08214',  '#4575b4'],
-                                            ['#8c510a', '#dfc27d', '#1b7837', '#f46d43', '#80cdc1']]):
-
+# for scenario_dict, service, colours in zip([scenario_dict1, scenario_dict2, scenario_dict3],
+#                                            ['anc', 'sba', 'pnc'],
+#                                            [['#8c510a', '#d73027', '#8073ac', '#01665e', '#c51b7d'],
+#                                             ['#8c510a', '#de77ae', '#5aae61', '#e08214',  '#4575b4'],
+#                                             ['#8c510a', '#dfc27d', '#1b7837', '#f46d43', '#80cdc1']]):
+for scenario_dict, service, colours in zip([scenario_dict4],
+                                            ['uhc'],
+                                            [['#8c510a', '#d73027', '#8073ac']]):
     service_of_interest = service
     scen_colours = colours
 
