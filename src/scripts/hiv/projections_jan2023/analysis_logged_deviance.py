043783d8fab3eebb93489045d3f68ca3417dbee2
tdm32 <t.mangal@imperial.ac.uk>
2024-05-07 12:55:15 +0100

    fix errors

--- src/scripts/hiv/projections_jan2023/analysis_logged_deviance.py
+++ src/scripts/hiv/projections_jan2023/analysis_logged_deviance.py
@@ -34,7 +34,7 @@ resourcefilepath = Path("./resources")
 
 # %% Run the simulation
 start_date = Date(2010, 1, 1)
-end_date = Date(2051, 1, 1)
+end_date = Date(2015, 1, 1)
 popsize = 3000
 
 # scenario = 1
