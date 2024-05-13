a194a8f26105032ed9b0280493af3481686aab69
tdm32 <t.mangal@imperial.ac.uk>
2024-05-13 09:18:38 +0100

    switch off MDA and run

--- src/scripts/schistosomiasis/schisto_calibration_check.py
+++ src/scripts/schistosomiasis/schisto_calibration_check.py
@@ -44,9 +44,9 @@ equal_allocation_by_district = True
 popsize = 500  # if equal_allocation_by_district=True then this is the popsize in each district
 
 
-def run_simulation(popsize=popsize, mda_execute=True):
+def run_simulation(popsize=popsize, mda_execute=False):
     start_date = Date(2010, 1, 1)
-    end_date = Date(2041, 1, 1)
+    end_date = Date(2061, 1, 1)
 
     # For logging, set all modules to WARNING threshold, then alters `Schisto` to level "INFO"
     custom_levels = {
