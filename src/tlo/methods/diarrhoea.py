a96785095b72f539d588a2e8f57b4aeea2816b2b
joehcollins <joseph.h.collins@ucl.ac.uk>
2024-05-14 15:57:38 +0100

    diarrhoea.py consumable units

--- src/tlo/methods/diarrhoea.py
+++ src/tlo/methods/diarrhoea.py
@@ -646,20 +646,21 @@ class Diarrhoea(Module):
         """Look up and store the consumables item codes used in each of the HSI."""
         ic = self.sim.modules['HealthSystem'].get_item_code_from_item_name
 
-        self.consumables_used_in_hsi['ORS'] = {ic('ORS, sachet'): 1}
+        self.consumables_used_in_hsi['ORS'] = {ic('ORS, sachet'): 2}
 
         self.consumables_used_in_hsi['Treatment_Severe_Dehydration'] = \
-            {ic('ORS, sachet'): 1,
+            {ic('ORS, sachet'): 2,
              ic('Giving set iv administration + needle 15 drops/ml_each_CMST'): 1,
-             ic("ringer's lactate (Hartmann's solution), 1000 ml_12_IDA"): 1}
+             ic("ringer's lactate (Hartmann's solution), 1000 ml_12_IDA"): 1000}
 
-        self.consumables_used_in_hsi['Zinc_Under6mo'] = {ic('Zinc, tablet, 20 mg'): 1}
+        self.consumables_used_in_hsi['Zinc'] = ic('Zinc, tablet, 20 mg')
 
-        self.consumables_used_in_hsi['Zinc_Over6mo'] = {ic('Zinc, tablet, 20 mg'): 1}
-
+        # For weight based treatment for children under five, we've averaged the median weight for each for years
+        # 0-5 as 12kg.
+        # So for cipro/para - 10mg/kg 12 hrly for 7 days = ((10*12)*2) * 7 (same dose reccomended)
         self.consumables_used_in_hsi['Antibiotics_for_Dysentery'] = \
-            {ic('Ciprofloxacin 250mg_100_CMST'): 1,
-             ic("Paracetamol syrup 120mg/5ml_0.0119047619047619_CMST"): 1}
+            {ic('Ciprofloxacin 250mg_100_CMST'): 1680,
+             ic("Paracetamol syrup 120mg/5ml_0.0119047619047619_CMST"): 1680}
 
     def do_when_presentation_with_diarrhoea(self, person_id, hsi_event):
         """This routine is called when Diarrhoea is a symptom for a child attending a Generic HSI Appointment. It
@@ -732,9 +733,10 @@ class Diarrhoea(Module):
 
         # ** Implement the procedure for treatment **
         # STEP ZERO: Get the Zinc consumable (happens irrespective of whether child will die or not)
+        # Dose is 10mg 24hrly for 10 days <6months or 20m for >6mnths
+        dose = 100 if person.age_exact_years < 0.5 else 200
         gets_zinc = hsi_event.get_consumables(
-            item_codes=self.consumables_used_in_hsi[
-                'Zinc_Under6mo' if person.age_exact_years < 0.5 else 'Zinc_Over6mo']
+            item_codes={self.consumables_used_in_hsi['Zinc']: dose}
         )
 
         # STEP ONE: Aim to alleviate dehydration:
