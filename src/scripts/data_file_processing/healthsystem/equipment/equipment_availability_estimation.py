c59fdea1d75a24b38c01dc3c11be136720f890d4
sm2511 <sakshi.mohan@york.ac.uk>
2024-05-16 11:31:21 +0100

    extrapolate values from other survey responses

--- src/scripts/data_file_processing/healthsystem/equipment/equipment_availability_estimation.py
+++ src/scripts/data_file_processing/healthsystem/equipment/equipment_availability_estimation.py
@@ -48,8 +48,30 @@ new_column_names = {'date-last-calibrated': 'date_last_calibrated', 'previous-pr
                     'today': 'available', 'today-functional': 'functional_today'}
 unique_equipment_df_for_model = unique_equipment_df_for_model.rename(columns=new_column_names)
 
-# TODO Mark equipment as avaiable if there is info only on functional, calibrated, prepared etc., similary functional if clibrated or preapred
+# Extrapolate values from other variables
+available_missing_before_extrapolation = len(unique_equipment_df_for_model[unique_equipment_df_for_model.available.isna()])
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['functional'].isin(["Yes", "No"]),'available'] = "Yes" # if there is a valid response to "functional" the equipment must be available
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['functional_today'].isin(['AVAILABLE  AND  FUNCTIONAL', 'AVAILABLE  NOT  FUNCTIONAL', "AVAILABLE DON'T KNOW IF  FUNCTIONAL", 'Yes']),'available'] = "Yes" # if equipment is functional then it is available
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['functional_today'].isin(['NOT AVAILABLE', 'No']),'available'] = "No" # if equipment is functional then it is available
+available_missing_after_extrapolation = len(unique_equipment_df_for_model[unique_equipment_df_for_model.available.isna()])
+print((available_missing_before_extrapolation - available_missing_after_extrapolation)/len(unique_equipment_df_for_model) * 100, "% values for 'available' extrapolated from other survey questions")
 
-# TODO Assign values to Yes, No, Functional, Don't know etc.
-unique_combinations = hhfa_equipment[['equipment', 'availability_var']].drop_duplicates()
-print(unique_combinations)
+functional_missing_before_extrapolation = len(unique_equipment_df_for_model[unique_equipment_df_for_model.functional.isna()])
+unique_equipment_df_for_model.loc[(unique_equipment_df_for_model['functional_today'].isin(['AVAILABLE  AND  FUNCTIONAL', 'Yes']) & unique_equipment_df_for_model['functional'].isna()),'functional'] = "Yes" # seperate question asking whether equipment was available today
+unique_equipment_df_for_model.loc[(unique_equipment_df_for_model['functional_today'].isin(['AVAILABLE  NOT  FUNCTIONAL', 'No', 'NOT AVAILABLE']) & unique_equipment_df_for_model['functional'].isna()),'functional'] = "No" # seperate question asking whether equipment was available today
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['available'].isin(['AT LEAST ONE VALID', 'Available and functional']),'functional'] = "Yes" # information under the available question captures whether equipment is functional
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['available'].isin(['AVAILABLE NON VALID', 'Available not functional']),'functional'] = "No"  # information under the available question captures whether equipment is functional
+functional_missing_after_extrapolation = len(unique_equipment_df_for_model[unique_equipment_df_for_model.functional.isna()])
+print((functional_missing_before_extrapolation - functional_missing_after_extrapolation)/len(unique_equipment_df_for_model) * 100, "% values for 'functional' extrapolated from other survey questions")
+
+# Sense check values - if equipment is calibrated or prepared, then it must be functional
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['prepared'] == "Yes", 'functional'] = "Yes"
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['calibrated'] == "Yes", 'functional'] = "Yes"
+
+# Convert to binary values
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['available'].isin(['AT LEAST ONE VALID', 'Available and functional', 'AVAILABLE NON VALID', 'Available not functional', "REPORTED  AVAILABLE BUT NOT SEEN", "Yes"]),'available'] = 1
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['available'].isin(['NOT AVAILABLE TODAY', 'Not available', 'NEVER AVAILABLE', "Not observed", "No"]),'available'] = 0
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['functional'] == '3','functional'] = np.nan # 3 implies "Don't know"
+unique_equipment_df_for_model.loc[unique_equipment_df_for_model['functional'] == 'Yes','functional'] = 1
+unique_equipment_df_for_model.loc[(unique_equipment_df_for_model['functional'] == 'No')|(unique_equipment_df_for_model['available'] == 0),'functional'] = 0
+
