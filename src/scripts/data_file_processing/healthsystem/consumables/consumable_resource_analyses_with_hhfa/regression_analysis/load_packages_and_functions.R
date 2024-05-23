eedb2e8c60828cc1e5a63a1f3010a313e0ba0c20
sm2511 <sakshi.mohan@york.ac.uk>
2024-05-23 11:27:06 +0100

    move file paths to main.R

--- src/scripts/data_file_processing/healthsystem/consumables/consumable_resource_analyses_with_hhfa/regression_analysis/load_packages_and_functions.R
+++ src/scripts/data_file_processing/healthsystem/consumables/consumable_resource_analyses_with_hhfa/regression_analysis/load_packages_and_functions.R
@@ -53,13 +53,6 @@ pacman::p_load(magrittr, # for %>% to work
                fastDummies # to create dummies from categorical variable
                )
 
-# Set file paths
-path_to_local_repo <- "/Users/sm2511/PycharmProjects/TLOmodel/" # Change this if different user
-path_to_dropbox <- "/Users/sm2511/Dropbox/Thanzi la Onse/" # Change this if different user
-path_to_files_in_dropbox <- paste0(path_to_dropbox, "05 - Resources/Module-healthsystem/consumables raw files/")
-path_to_data <- paste0(path_to_dropbox, "07 - Data/HHFA_2018-19/2 clean/")
-path_to_scripts <-  paste0(path_to_local_repo, "src/scripts/data_file_processing/healthsystem/consumables/consumable_resource_analyses_with_hhfa/regression_analysis/")
-
 # Set paths to store outputs
 dir.create(file.path(path_to_local_repo, "outputs/", "regression_analysis"))
 path_to_outputs <- paste0(path_to_local_repo, "outputs/", "regression_analysis/")
--- src/scripts/data_file_processing/healthsystem/consumables/consumable_resource_analyses_with_hhfa/regression_analysis/main.R
+++ src/scripts/data_file_processing/healthsystem/consumables/consumable_resource_analyses_with_hhfa/regression_analysis/main.R
@@ -1,5 +1,12 @@
 # This script sources other scripts needed for the regression analysis and controls the workflow.
 
+# Set file paths
+path_to_local_repo <- "/Users/sm2511/PycharmProjects/TLOmodel/" # Change this if different user
+path_to_dropbox <- "/Users/sm2511/Dropbox/Thanzi la Onse/" # Change this if different user
+path_to_files_in_dropbox <- paste0(path_to_dropbox, "05 - Resources/Module-healthsystem/consumables raw files/")
+path_to_data <- paste0(path_to_dropbox, "07 - Data/HHFA_2018-19/2 clean/")
+path_to_scripts <-  paste0(path_to_local_repo, "src/scripts/data_file_processing/healthsystem/consumables/consumable_resource_analyses_with_hhfa/regression_analysis/")
+
 # Load Libraries and Functions
 source(paste0(path_to_scripts, "load_packages_and_functions.R"))
 
