038f94581123083d9fd69a5e77a1015162c75590
Matt Graham <matthew.m.graham@gmail.com>
2024-05-30 20:31:01 +0100

    Exclude GenericFirstApptsModule from enumerations

--- tests/test_module_dependencies.py
+++ tests/test_module_dependencies.py
@@ -30,7 +30,12 @@ simulation_initial_population = 1000
 
 
 module_class_map = get_module_class_map(
-    excluded_modules={'Module', 'Skeleton', 'SimplifiedPregnancyAndLabour'}
+    excluded_modules={
+        "Module",
+        "GenericFirstApptModule",
+        "Skeleton",
+        "SimplifiedPregnancyAndLabour",
+    }
 )
 
 
