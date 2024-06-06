243690f205dd18cbc67f4da58dfade38d28c2f74
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2024-06-06 22:39:35 +0100

    reduce number of draws

--- src/scripts/comparison_of_horizontal_and_vertical_programs/scenario_comparison_of_horizontal_and_vertical_programs.py
+++ src/scripts/comparison_of_horizontal_and_vertical_programs/scenario_comparison_of_horizontal_and_vertical_programs.py
@@ -27,7 +27,7 @@ class HorizontalAndVerticalPrograms(BaseScenario):
         self.pop_size = 100_000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
-        self.runs_per_draw = 5
+        self.runs_per_draw = 3  # <--- todo: N.B. Very small number of repeated run, to be efficient for now
 
     def log_configuration(self):
         return {
