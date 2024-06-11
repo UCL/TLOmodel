b78d5a1c84611fa721f1a5accca8986ab89694c9
sm2511 <sakshi.mohan@york.ac.uk>
2024-06-11 21:37:48 +0100

    update scenario for large run

--- src/scripts/consumables_analyses/scenario_impact_of_consumable_scenarios.py
+++ src/scripts/consumables_analyses/scenario_impact_of_consumable_scenarios.py
@@ -1,14 +1,17 @@
 """
 This file defines a batch run to calculate the health effect of updated consumable availability estimates
 as a result of a supply chain intervention. The following scenarios are considered:
-1. 'scenario1' - all facilities set to 1b,
-2. 'scenario2' - all facility ownership set to CHAM,
-3. 'scenario3' - all facilities have functional computers,
-4. 'scenario4' - all facility drug stocks are managed by pharmacists or pharmacist technicians,
-5. 'scenario5' - all facilities have a functional emergency vehicle,
-6. 'scenario6' - all facilities provide diagnostic services,
-7. 'scenario7' - all facilities are within 10 kms from the relevant DHO,
-8. 'scenario8' - all facilities are within 10 kms from the relevant Regional medical Store (Warehouse),
+1. 'default' - this is the benchmark scenario with 2018 levels of consumable availability
+2. 'scenario1' : All items perform as well as consumables other than drugs/diagnostic tests
+3. 'scenario2' : 1 + All items perform as well as consumables classified as 'Vital' in the Essential Medicines List
+4. 'scenario3' : 2 + All facilities perform as well as those in which consumables stock is managed by pharmacists
+5. 'scenario4' : 3 + Level 1a facilities perform as well as level 1b
+6. 'scenario5' : 4 + All facilities perform as well as CHAM facilities
+7. 'scenario6' : All facilities have the same probability of consumable availability as the 75th percentile best performing facility for each individual item
+8. 'scenario7' : All facilities have the same probability of consumable availability as the 90th percentile best performing facility for each individual item
+9. 'scenario8' : All facilities have the same probability of consumable availability as the 99th percentile best performing facility for each individual item
+10. 'all': all consumable are always available - provides the theoretical maximum health gains which can be made through improving consumable supply
+
 The batch runs are for a large population for a long time with all disease modules and full use of HSIs.
 Run on the batch system using:
 ```tlo batch-submit src/scripts/consumables_analyses/scenario_impact_of_consumable_scenarios.py```
@@ -29,10 +32,10 @@ class ImpactOfConsumablesScenarios(BaseScenario):
         super().__init__()
         self.seed = 99
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2011, 12, 31)
-        self.pop_size = 50 # large population size for final simulation
-        self.number_of_draws = 2  # <- 10 scenarios (10)
-        self.runs_per_draw = 2  # <- repeated this many times
+        self.end_date = Date(2019, 12, 31)
+        self.pop_size = 100_000 # large population size for final simulation
+        self.number_of_draws = 10  # <- 10 scenarios (10)
+        self.runs_per_draw = 5  # <- repeated this many times
 
     def log_configuration(self):
         return {
@@ -52,10 +55,12 @@ class ImpactOfConsumablesScenarios(BaseScenario):
     def draw_parameters(self, draw_number, rng):
         return {
             'HealthSystem': {
-                'cons_availability': ['default', 'scenario1'][draw_number] # , 'scenario2', 'scenario3', 'scenario4', 'scenario5', 'scenario6', 'scenario7', 'scenario8', 'all'
+                'cons_availability': ['default',
+                                      'scenario1', 'scenario2', 'scenario3', 'scenario4',
+                                      'scenario5', 'scenario6', 'scenario7', 'scenario8',
+                                      'all'][draw_number]
                }
         }
-
 
 if __name__ == '__main__':
     from tlo.cli import scenario_run
