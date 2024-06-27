92d03169694e698ba91e6c340ebb910d00bae8c0
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2024-06-27 16:59:34 +0100

    refacotring

--- src/scripts/comparison_of_horizontal_and_vertical_programs/analysis_hss_elements.py
+++ src/scripts/comparison_of_horizontal_and_vertical_programs/analysis_hss_elements.py
@@ -40,7 +40,7 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
 
     def get_parameter_names_from_scenario_file() -> Tuple[str]:
         """Get the tuple of names of the scenarios from `Scenario` class used to create the results."""
-        from scripts.comparison_of_horizontal_and_vertical_programs.scenario_comparison_of_horizontal_and_vertical_programs import \
+        from scripts.comparison_of_horizontal_and_vertical_programs.scenario_hss_elements import \
             HorizontalAndVerticalPrograms
         e = HorizontalAndVerticalPrograms()
         return tuple(e._scenarios.keys())
