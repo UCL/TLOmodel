cb639de09d1a3037559242e46267fc64eb5df8da
RachelMurray-Watson <rmurwat@gmail.com>
2024-07-04 14:40:13 +0100

    Added check in healthsystem.py hsi logger to see if the appointment footprint was blank. If it is, then the HSI is not logged.

--- src/tlo/methods/healthsystem.py
+++ src/tlo/methods/healthsystem.py
@@ -1725,7 +1725,7 @@ class HealthSystem(Module):
             },
             description="record of each HSI event"
         )
-        if did_run:
+        if did_run and event_details.appt_footprint:
             if self._hsi_event_count_log_period is not None:
                 # Do logging for HSI Event using counts of each 'unique type' of HSI event (as defined by
                 # `HSIEventDetails`).
