c8a7cf964c40b61174b5741c7c5ed49341c98354
RachelMurray-Watson <rmurwat@gmail.com>
2024-07-10 09:30:33 +0100

    Reformatted imports in line with results of checks

--- src/tlo/analysis/probability_premature_death.py
+++ src/tlo/analysis/probability_premature_death.py
@@ -9,10 +9,9 @@ from pathlib import Path
 from typing import Dict, Tuple
 
 import pandas as pd
+from tlo.analysis.life_expectancy import _aggregate_person_years_by_age, _num_deaths_by_age_group
 
 from tlo.analysis.utils import get_scenario_info, summarize
-
-from tlo.analysis.life_expectancy import _aggregate_person_years_by_age, _num_deaths_by_age_group
 
 def _calculate_probability_of_dying_before_70(
     _person_years_at_risk: pd.Series,
