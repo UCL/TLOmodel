3e99e7f0b0a5dc0e5ff47a175f056da68767ddee
RachelMurray-Watson <rmurwat@gmail.com>
2024-07-19 14:01:25 +0100

    changed params to run locally

--- src/scripts/longterm_projections/test_azure_longterm_trends.py
+++ src/scripts/longterm_projections/test_azure_longterm_trends.py
@@ -30,10 +30,10 @@ class LongRun(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2020, 1, 12)#Date(2099, 12, 31)
+        self.end_date = Date(2021, 1, 12)#Date(2099, 12, 31)
         self.pop_size = 20_000
         self.number_of_draws = 1
-        self.runs_per_draw = 10
+        self.runs_per_draw = 1
 
     def log_configuration(self):
         return {
