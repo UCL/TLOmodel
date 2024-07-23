b50bb5d444b50fff53f32a88ffff124e3402ee7c
RachelMurray-Watson <rmurwat@gmail.com>
2024-07-23 09:51:10 +0100

    Spacing

--- src/scripts/longterm_projections/test_azure_longterm_trends.py
+++ src/scripts/longterm_projections/test_azure_longterm_trends.py
@@ -9,11 +9,13 @@ from tlo.methods import (
     alri, bladder_cancer, breast_cancer, cardio_metabolic_disorders, care_of_women_during_pregnancy,
     contraception, copd, demography, depression, diarrhoea, enhanced_lifestyle, epi,
     epilepsy, healthburden, healthseekingbehaviour, healthsystem, hiv, labour,
-    malaria, measles, newborn_outcomes, oesophagealcancer, other_adult_cancers, pregnancy_supervisor, postnatal_supervisor,
+    malaria, measles, newborn_outcomes, oesophagealcancer, other_adult_cancers, pregnancy_supervisor,
+    postnatal_supervisor,
     prostate_cancer, rti, schisto, simplified_births, stunting, symptommanager, tb, wasting, fullmodel
 )
 
 from tlo.methods.scenario_switcher import ImprovedHealthSystemAndCareSeekingScenarioSwitcher
+
 
 # Start time for the simulation
 #start_time = time.time()
@@ -30,7 +32,7 @@ class LongRun(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2019, 1, 12)#Date(2099, 12, 31)
+        self.end_date = Date(2019, 1, 12)  #Date(2099, 12, 31)
         self.pop_size = 1_000
         self.number_of_draws = 1
         self.runs_per_draw = 2
@@ -38,11 +40,11 @@ class LongRun(BaseScenario):
     def log_configuration(self):
         return {
             'filename': 'longterm_trends_all_diseases',
-            'directory':  './outputs',
+            'directory': './outputs',
             'custom_levels': {
-               # '*': logging.WARNING,
-               # "*": logging.DEBUG,
-               # "*": logging.FATAL,
+                # '*': logging.WARNING,
+                # "*": logging.DEBUG,
+                # "*": logging.FATAL,
                 'tlo.methods.demography': logging.INFO,
                 'tlo.methods.demography.detail': logging.WARNING,
                 'tlo.methods.healthburden': logging.INFO,
@@ -53,32 +55,33 @@ class LongRun(BaseScenario):
         }
 
     def modules(self):
-         fullmodel_instance = fullmodel.fullmodel(resourcefilepath=self.resources, use_simplified_births=False, module_kwargs = {"HealthSystem": {
-            "mode_appt_constraints":0,
-            "cons_availability":"all",
-            "ignore_priority":False,
-            "beds_availability":'all',
-            "equip_availability":'all',
-            "capabilities_coefficient":1.0,
-            "use_funded_or_actual_staffing":"funded",
-            "disable":False,
-            "disable_and_reject_all":False}})
+        fullmodel_instance = fullmodel.fullmodel(resourcefilepath=self.resources, use_simplified_births=False,
+                                                 module_kwargs={"HealthSystem": {
+                                                     "mode_appt_constraints": 0,
+                                                     "cons_availability": "all",
+                                                     "ignore_priority": False,
+                                                     "beds_availability": 'all',
+                                                     "equip_availability": 'all',
+                                                     "capabilities_coefficient": 1.0,
+                                                     "use_funded_or_actual_staffing": "funded",
+                                                     "disable": False,
+                                                     "disable_and_reject_all": False}})
 
-         switcher = ImprovedHealthSystemAndCareSeekingScenarioSwitcher(resourcefilepath=self.resources)
+        switcher = ImprovedHealthSystemAndCareSeekingScenarioSwitcher(resourcefilepath=self.resources)
 
-         switcher.parameters = {
-                "year_of_switch": 2010,
-                "max_healthsystem_function": [True, True],
-                "max_healthcare_seeking": [True, True]
-            }
-         fullmodel_instance =  fullmodel_instance + [switcher]
-         return fullmodel_instance
+        switcher.parameters = {
+            "year_of_switch": 2010,
+            "max_healthsystem_function": [True, True],
+            "max_healthcare_seeking": [True, True]
+        }
+        fullmodel_instance = fullmodel_instance + [switcher]
+        return fullmodel_instance
 
     def draw_parameters(self, draw_number, rng):
         return
+
 
 if __name__ == '__main__':
     from tlo.cli import scenario_run
 
     scenario_run([__file__])
-
