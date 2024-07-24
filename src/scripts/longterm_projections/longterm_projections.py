e2c2cced13586e576061cf8a546314193fcb4d07
RachelMurray-Watson <rmurwat@gmail.com>
2024-07-24 20:26:38 +0100

    The scenario switcher doesn't work...

--- src/scripts/longterm_projections/longterm_projections.py
+++ src/scripts/longterm_projections/longterm_projections.py
@@ -29,9 +29,6 @@ class LongRun(BaseScenario):
             'filename': 'longterm_trends_all_diseases',
             'directory': './outputs',
             'custom_levels': {
-                # '*': logging.WARNING,
-                # "*": logging.DEBUG,
-                # "*": logging.FATAL,
                 'tlo.methods.demography': logging.INFO,
                 'tlo.methods.demography.detail': logging.WARNING,
                 'tlo.methods.healthburden': logging.INFO,
@@ -42,21 +39,32 @@ class LongRun(BaseScenario):
         }
 
     def modules(self):
-        return (fullmodel(resourcefilepath=self.resources, use_simplified_births=False,)
-        + [ImprovedHealthSystemAndCareSeekingScenarioSwitcher(resourcefilepath=self.resources)])
+        return (
+            fullmodel(resourcefilepath=self.resources)
+            + [ImprovedHealthSystemAndCareSeekingScenarioSwitcher(resourcefilepath=self.resources)]
+        )
 
 
     def draw_parameters(self, draw_number, rng):
         if draw_number < len(self._scenarios):
+            print(list(self._scenarios.values())[draw_number])
             return list(self._scenarios.values())[draw_number]
 
 
     def _get_scenarios(self) -> Dict[str, Dict]:
         """Return the Dict with values for the parameters that are changed, keyed by a name for the scenario."""
         # Load helper class containing the definitions of the elements of all the scenarios
-        return {
-            "Baseline":
-                self._baseline()}
+        return {"FULL PACKAGE":
+                mix_scenarios(
+                    self._baseline(),
+                    {
+                        'ImprovedHealthSystemAndCareSeekingScenarioSwitcher': {
+                            'max_healthsystem_function': [True, True],
+                            'max_healthcare_seeking': [True, True],
+                            'year_of_switch': self.YEAR_OF_CHANGE
+                        },
+                    },
+                ),}
 
     def _baseline(self) -> Dict:
         """Return the Dict with values for the parameter changes that define the baseline scenario. """
@@ -65,21 +73,19 @@ class LongRun(BaseScenario):
             {"HealthSystem":{
                     "mode_appt_constraints": 1,
                     "mode_appt_constraints_postSwitch": 1,
-                    "year_mode_switch": self.YEAR_OF_CHANGE,
                     "cons_availability": "all",
                     "beds_availability": 'all',
                     "equip_availability": 'all',
                     "use_funded_or_actual_staffing": "funded_plus",
-                    },
-                    'ImprovedHealthSystemAndCareSeekingScenarioSwitcher': {
-                            'max_healthsystem_function': [True, True],  # <-- switch from False to True mid-way
-                            'max_healthcare_seeking': [True, True],  # <-- switch from False to True mid-way
-                            'year_of_switch': self.YEAR_OF_CHANGE
-                            }
+                    },})
+    #                'ImprovedHealthSystemAndCareSeekingScenarioSwitcher': {
+    #                        'max_healthsystem_function': [True, True],
+    #                        'max_healthcare_seeking': [True, True],
+    #                        'year_of_switch': self.YEAR_OF_CHANGE
+    #                       }
 
-            },
-        )
-
+    #        },
+    #    )
 
 if __name__ == '__main__':
     from tlo.cli import scenario_run
