be2a68dba910d11b95e36fc325cc3f9e576b131c
RachelMurray-Watson <rmurwat@gmail.com>
2024-07-24 19:09:00 +0100

    Updated scenario so that params are entered in the draw parameters as part of a baseline scenario

--- src/scripts/longterm_projections/longterm_projections.py
+++ src/scripts/longterm_projections/longterm_projections.py
@@ -1,11 +1,13 @@
 import datetime
 import time
 from pathlib import Path
+from typing import Dict
 
 from tlo import Date, logging
 from tlo.scenario import BaseScenario
 from tlo.methods import ( fullmodel
 )
+from tlo.analysis.utils import get_parameters_for_status_quo, mix_scenarios
 
 from tlo.methods.scenario_switcher import ImprovedHealthSystemAndCareSeekingScenarioSwitcher
 
@@ -19,6 +21,9 @@ class LongRun(BaseScenario):
         self.pop_size = 100_000
         self.number_of_draws = 1
         self.runs_per_draw = 2
+        self.YEAR_OF_CHANGE = 2010
+        self._scenarios = self._get_scenarios()
+        self.number_of_draws = len(self._scenarios)
 
     def log_configuration(self):
         return {
@@ -38,30 +43,43 @@ class LongRun(BaseScenario):
         }
 
     def modules(self):
-        fullmodel_instance = fullmodel.fullmodel(resourcefilepath=self.resources, use_simplified_births=False,
-                                                 module_kwargs={"HealthSystem": {
-                                                     "mode_appt_constraints": 0,
-                                                     "cons_availability": "all",
-                                                     "ignore_priority": False,
-                                                     "beds_availability": 'all',
-                                                     "equip_availability": 'all',
-                                                     "capabilities_coefficient": 1.0,
-                                                     "use_funded_or_actual_staffing": "funded",
-                                                     "disable": False,
-                                                     "disable_and_reject_all": False}})
+        return (fullmodel.fullmodel(resourcefilepath=self.resources, use_simplified_births=False,)
+        + [ImprovedHealthSystemAndCareSeekingScenarioSwitcher(resourcefilepath=self.resources)])
 
-        switcher = ImprovedHealthSystemAndCareSeekingScenarioSwitcher(resourcefilepath=self.resources)
-
-        switcher.parameters = {
-            "year_of_switch": 2010,
-            "max_healthsystem_function": [True, True],
-            "max_healthcare_seeking": [True, True]
-        }
-        fullmodel_instance = fullmodel_instance + [switcher]
-        return fullmodel_instance
 
     def draw_parameters(self, draw_number, rng):
-        return
+        if draw_number < len(self._scenarios):
+            return list(self._scenarios.values())[draw_number]
+
+
+    def _get_scenarios(self) -> Dict[str, Dict]:
+        """Return the Dict with values for the parameters that are changed, keyed by a name for the scenario."""
+        # Load helper class containing the definitions of the elements of all the scenarios
+        return {
+            "Baseline":
+                self._baseline()}
+
+    def _baseline(self) -> Dict:
+        """Return the Dict with values for the parameter changes that define the baseline scenario. """
+        return mix_scenarios(
+            get_parameters_for_status_quo(),
+            {"HealthSystem":{
+                    "mode_appt_constraints": 1,
+                    "mode_appt_constraints_postSwitch": 1,
+                    "year_mode_switch": self.YEAR_OF_CHANGE,
+                    "cons_availability": "all",
+                    "beds_availability": 'all',
+                    "equip_availability": 'all',
+                    "use_funded_or_actual_staffing": "funded_plus",
+                    },
+                    'ImprovedHealthSystemAndCareSeekingScenarioSwitcher': {
+                            'max_healthsystem_function': [False, True],  # <-- switch from False to True mid-way
+                            'max_healthcare_seeking': [False, True],  # <-- switch from False to True mid-way
+                            'year_of_switch': self.YEAR_OF_CHANGE
+                            }
+
+            },
+        )
 
 
 if __name__ == '__main__':
