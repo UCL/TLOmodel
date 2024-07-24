e4e8bff0952c029fe6004e9627894bb3850a2a87
RachelMurray-Watson <rmurwat@gmail.com>
2024-07-24 09:17:04 +0100

    Fixed indentation

--- tests/test_blank_footprint.py
+++ tests/test_blank_footprint.py
@@ -157,26 +157,26 @@ def test_outputs_to_log_no_blank(tmpdir):
     )
 
     # Edit the `initialise_simulation` method of DummyModule so that, during the simulation, an HSI is run.
-    def schedule_hsi(sim):
-        """Drop-in replacement for `initialise_simulation` in the DummyModule module."""
-        # Make the district for person_id=0 such that the HSI will be served by facility_id=0
-       sim = set_person_district_id(sim)
+def schedule_hsi(sim):
+    """Drop-in replacement for `initialise_simulation` in the DummyModule module."""
+    # Make the district for person_id=0 such that the HSI will be served by facility_id=0
+    sim = set_person_district_id(sim)
 
-        # Schedule the HSI event with a blank footprint for person_id=0
-        sim.modules['HealthSystem'].schedule_hsi_event(
-            hsi_event=get_dummy_hsi_event_instance(module=sim.modules['DummyModule'], facility_id=0, blank = blank_footprint),
-            topen=sim.start_date,
-            tclose=None,
-            priority=0
-        )
+    # Schedule the HSI event with a blank footprint for person_id=0
+    sim.modules['HealthSystem'].schedule_hsi_event(
+        hsi_event=get_dummy_hsi_event_instance(module=sim.modules['DummyModule'], facility_id=0, blank = blank_footprint),
+        topen=sim.start_date,
+        tclose=None,
+        priority=0
+    )
 
 
     sim.modules['DummyModule'].initialise_simulation = schedule_hsi
 
-    # Simulate for one day
+        # Simulate for one day
     sim.simulate(end_date=sim.start_date + pd.DateOffset(days=1))
 
-    # Check that log is created and the content is as expected.
+        # Check that log is created and the content is as expected.
     hsi_log = parse_log_file(sim.log_filepath)['tlo.methods.healthsystem.summary']['hsi_event_details']
     assert hsi_log.iloc[0,1]['0']['appt_footprint'] == []
 
