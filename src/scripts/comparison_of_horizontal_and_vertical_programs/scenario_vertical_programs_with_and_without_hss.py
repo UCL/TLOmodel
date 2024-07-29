b1a5726ac45f95ba1d0639b73cd4572cc4ebecba
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2024-07-29 12:08:49 +0100

    add loggers for HIV, TB, Malaria in the scenario_vertical_programs_with_and_without_hss.py

--- src/scripts/comparison_of_horizontal_and_vertical_programs/scenario_vertical_programs_with_and_without_hss.py
+++ src/scripts/comparison_of_horizontal_and_vertical_programs/scenario_vertical_programs_with_and_without_hss.py
@@ -43,6 +43,9 @@ class HTMWithAndWithoutHSS(BaseScenario):
                 'tlo.methods.healthburden': logging.INFO,
                 'tlo.methods.healthsystem': logging.WARNING,
                 'tlo.methods.healthsystem.summary': logging.INFO,
+                'tlo.methods.hiv': logging.INFO,
+                'tlo.methods.tb': logging.INFO,
+                'tlo.methods.malaria': logging.INFO,
             }
         }
 
