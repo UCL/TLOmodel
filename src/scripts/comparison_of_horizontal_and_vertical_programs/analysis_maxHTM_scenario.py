ee32ab2a5e455060878befd3343cc3eff8dfcb2f
tdm32 <t.mangal@imperial.ac.uk>
2024-07-29 13:01:50 +0100

    comment out plots in analysis_maxHTM_scenario.py

--- src/scripts/comparison_of_horizontal_and_vertical_programs/analysis_maxHTM_scenario.py
+++ src/scripts/comparison_of_horizontal_and_vertical_programs/analysis_maxHTM_scenario.py
@@ -128,109 +128,109 @@ if __name__ == '__main__':
 
 # %% Produce some figures and summary info
 
-# Find results_folder associated with a given batch_file (and get most recent [-1])
-results_folder = get_scenario_outputs("scaleup_tests-", outputspath)[-1]
-
-# get basic information about the results
-info = get_scenario_info(results_folder)
-
-# 1) Extract the parameters that have varied over the set of simulations
-params = extract_params(results_folder)
-
-
-# DEATHS
-
-
-def get_num_deaths_by_cause_label(_df):
-    """Return total number of Deaths by label within the TARGET_PERIOD
-    values are summed for all ages
-    df returned: rows=COD, columns=draw
-    """
-    return _df \
-        .loc[pd.to_datetime(_df.date).between(*TARGET_PERIOD)] \
-        .groupby(_df['label']) \
-        .size()
-
-
-TARGET_PERIOD = (Date(scaleup_start_year, 1, 1), end_date)
-
-# produce df of total deaths over scale-up period
-num_deaths_by_cause_label = extract_results(
-        results_folder,
-        module='tlo.methods.demography',
-        key='death',
-        custom_generate_series=get_num_deaths_by_cause_label,
-        do_scaling=True
-    )
-
-
-def summarise_deaths_for_one_cause(results_folder, label):
-    """ returns mean deaths for each year of the simulation
-    values are aggregated across the runs of each draw
-    for the specified cause
-    """
-
-    results_deaths = extract_results(
-        results_folder,
-        module="tlo.methods.demography",
-        key="death",
-        custom_generate_series=(
-            lambda df: df.assign(year=df["date"].dt.year).groupby(
-                ["year", "label"])["person_id"].count()
-        ),
-        do_scaling=True,
-    )
-    # removes multi-index
-    results_deaths = results_deaths.reset_index()
-
-    # select only cause specified
-    tmp = results_deaths.loc[
-        (results_deaths.label == label)
-    ]
-
-    # group deaths by year
-    tmp = pd.DataFrame(tmp.groupby(["year"]).sum())
-
-    # get mean for each draw
-    mean_deaths = pd.concat({'mean': tmp.iloc[:, 1:].groupby(level=0, axis=1).mean()}, axis=1).swaplevel(axis=1)
-
-    return mean_deaths
-
-
-aids_deaths = summarise_deaths_for_one_cause(results_folder, 'AIDS')
-tb_deaths = summarise_deaths_for_one_cause(results_folder, 'TB (non-AIDS)')
-malaria_deaths = summarise_deaths_for_one_cause(results_folder, 'Malaria')
-
-
-draw_labels = ['No scale-up', 'HIV scale-up', 'TB scale-up', 'Malaria scale-up', 'HTM scale-up']
-colours = ['blue', 'green', 'red', 'purple', 'orange']
-
-# Create subplots
-fig, axs = plt.subplots(3, 1, figsize=(10, 10))
-# Plot for df1
-for i, col in enumerate(aids_deaths.columns):
-    axs[0].plot(aids_deaths.index, aids_deaths[col], label=draw_labels[i],
-                color=colours[i])
-axs[0].set_title('HIV/AIDS')
-axs[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))  # Legend to the right of the plot
-axs[0].axvline(x=scaleup_start_year, color='gray', linestyle='--')
-
-# Plot for df2
-for i, col in enumerate(tb_deaths.columns):
-    axs[1].plot(tb_deaths.index, tb_deaths[col], color=colours[i])
-axs[1].set_title('TB')
-axs[1].axvline(x=scaleup_start_year, color='gray', linestyle='--')
-
-# Plot for df3
-for i, col in enumerate(malaria_deaths.columns):
-    axs[2].plot(malaria_deaths.index, malaria_deaths[col], color=colours[i])
-axs[2].set_title('Malaria')
-axs[2].axvline(x=scaleup_start_year, color='gray', linestyle='--')
-
-for ax in axs:
-    ax.set_xlabel('Years')
-    ax.set_ylabel('Number deaths')
-
-plt.tight_layout(rect=[0, 0, 0.85, 1])  # Adjust layout to make space for legend
-plt.show()
-
+# # Find results_folder associated with a given batch_file (and get most recent [-1])
+# results_folder = get_scenario_outputs("scaleup_tests-", outputspath)[-1]
+#
+# # get basic information about the results
+# info = get_scenario_info(results_folder)
+#
+# # 1) Extract the parameters that have varied over the set of simulations
+# params = extract_params(results_folder)
+#
+#
+# # DEATHS
+#
+#
+# def get_num_deaths_by_cause_label(_df):
+#     """Return total number of Deaths by label within the TARGET_PERIOD
+#     values are summed for all ages
+#     df returned: rows=COD, columns=draw
+#     """
+#     return _df \
+#         .loc[pd.to_datetime(_df.date).between(*TARGET_PERIOD)] \
+#         .groupby(_df['label']) \
+#         .size()
+#
+#
+# TARGET_PERIOD = (Date(scaleup_start_year, 1, 1), end_date)
+#
+# # produce df of total deaths over scale-up period
+# num_deaths_by_cause_label = extract_results(
+#         results_folder,
+#         module='tlo.methods.demography',
+#         key='death',
+#         custom_generate_series=get_num_deaths_by_cause_label,
+#         do_scaling=True
+#     )
+#
+#
+# def summarise_deaths_for_one_cause(results_folder, label):
+#     """ returns mean deaths for each year of the simulation
+#     values are aggregated across the runs of each draw
+#     for the specified cause
+#     """
+#
+#     results_deaths = extract_results(
+#         results_folder,
+#         module="tlo.methods.demography",
+#         key="death",
+#         custom_generate_series=(
+#             lambda df: df.assign(year=df["date"].dt.year).groupby(
+#                 ["year", "label"])["person_id"].count()
+#         ),
+#         do_scaling=True,
+#     )
+#     # removes multi-index
+#     results_deaths = results_deaths.reset_index()
+#
+#     # select only cause specified
+#     tmp = results_deaths.loc[
+#         (results_deaths.label == label)
+#     ]
+#
+#     # group deaths by year
+#     tmp = pd.DataFrame(tmp.groupby(["year"]).sum())
+#
+#     # get mean for each draw
+#     mean_deaths = pd.concat({'mean': tmp.iloc[:, 1:].groupby(level=0, axis=1).mean()}, axis=1).swaplevel(axis=1)
+#
+#     return mean_deaths
+#
+#
+# aids_deaths = summarise_deaths_for_one_cause(results_folder, 'AIDS')
+# tb_deaths = summarise_deaths_for_one_cause(results_folder, 'TB (non-AIDS)')
+# malaria_deaths = summarise_deaths_for_one_cause(results_folder, 'Malaria')
+#
+#
+# draw_labels = ['No scale-up', 'HIV scale-up', 'TB scale-up', 'Malaria scale-up', 'HTM scale-up']
+# colours = ['blue', 'green', 'red', 'purple', 'orange']
+#
+# # Create subplots
+# fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+# # Plot for df1
+# for i, col in enumerate(aids_deaths.columns):
+#     axs[0].plot(aids_deaths.index, aids_deaths[col], label=draw_labels[i],
+#                 color=colours[i])
+# axs[0].set_title('HIV/AIDS')
+# axs[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))  # Legend to the right of the plot
+# axs[0].axvline(x=scaleup_start_year, color='gray', linestyle='--')
+#
+# # Plot for df2
+# for i, col in enumerate(tb_deaths.columns):
+#     axs[1].plot(tb_deaths.index, tb_deaths[col], color=colours[i])
+# axs[1].set_title('TB')
+# axs[1].axvline(x=scaleup_start_year, color='gray', linestyle='--')
+#
+# # Plot for df3
+# for i, col in enumerate(malaria_deaths.columns):
+#     axs[2].plot(malaria_deaths.index, malaria_deaths[col], color=colours[i])
+# axs[2].set_title('Malaria')
+# axs[2].axvline(x=scaleup_start_year, color='gray', linestyle='--')
+#
+# for ax in axs:
+#     ax.set_xlabel('Years')
+#     ax.set_ylabel('Number deaths')
+#
+# plt.tight_layout(rect=[0, 0, 0.85, 1])  # Adjust layout to make space for legend
+# plt.show()
+#
