79f708a26799a91186486857a1deacc068e12a18
RachelMurray-Watson <rmurwat@gmail.com>
2024-07-31 09:30:24 +0100

    changed years of interest

--- src/scripts/longterm_projections/longterm_projections.py
+++ src/scripts/longterm_projections/longterm_projections.py
@@ -13,7 +13,7 @@ class LongRun(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2099, 1, 12)  #Date(2099, 12, 31)
+        self.end_date = Date(2080, 1, 12)  #Date(2099, 12, 31)
         self.pop_size = 100_000
         self.number_of_draws = 1
         self.runs_per_draw = 10
