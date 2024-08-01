cdd490cd9c9c326ef14251121f845f94a9874379
RachelMurray-Watson <rmurwat@gmail.com>
2024-08-01 14:36:53 +0100

    added function to report the prevalence of depression for that month across all individuals

--- src/tlo/methods/depression.py
+++ src/tlo/methods/depression.py
@@ -554,6 +554,20 @@ class Depression(Module, GenericFirstAppointmentsMixin):
 
         return av_daly_wt_last_month
 
+    def report_prevalence(self):
+        # This reports on the prevalence of HIV for all individuals
+
+        df = self.sim.population.props
+        any_depr_in_the_last_month = (df['is_alive']) & (
+            ~pd.isnull(df['de_date_init_most_rec_depr']) & (df['de_date_init_most_rec_depr'] <= self.sim.date)
+        ) & (
+                                         pd.isnull(df['de_date_depr_resolved']) |
+                                         (df['de_date_depr_resolved'] >= (self.sim.date - DateOffset(months=1)))
+                                     )
+        total_prev = len(
+            any_depr_in_the_last_month
+        ) / len(df[df.is_alive])
+        return total_prev
     def _check_for_suspected_depression(
         self, symptoms: List[str], treatment_id: str, has_even_been_diagnosed: bool
     ):
