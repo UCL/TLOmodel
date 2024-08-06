1e1bbfe64ea8727427e6f8a1ae6565b3238ffee8
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2024-08-06 11:14:14 +0100

    update analysis script

--- src/scripts/impact_of_historical_changes_in_hr/examining_data_historic_changes_in_hr.py
+++ src/scripts/impact_of_historical_changes_in_hr/examining_data_historic_changes_in_hr.py
@@ -10,47 +10,43 @@ path_to_dropbox = Path(  # <-- point to the TLO dropbox locally
     '/Users/tbh03/Library/CloudStorage/OneDrive-SharedLibraries-ImperialCollegeLondon/TLOModel - WP - Documents/'
 )
 
+# File provided by Dominic Nkhoma (29th July 2024)
+df = pd.read_excel(
+    path_to_dropbox / '07 - Data' / 'Historical_Changes_in_HR' / 'Yearly Employees Data Consolidated (Vote 006).xlsx',
+    sheet_name='Sheet1')
 
-# File provided by Dominic Nkhoma
-f = path_to_dropbox / '07 - Data' / 'Historical_Changes_in_HR' / 'Monthly Staff Totals Consolidated.xlsx'
-wb = pd.read_excel(f, sheet_name=None)
-
-def process_sheet(sheet_name):
-    df = wb[sheet_name].set_index('District').stack()
-    df.index = df.index.set_names(['District', 'Month'])
-    df.name = int(sheet_name)
-    return df
-
-df = pd.concat([process_sheet(sheet) for sheet in wb.keys() if sheet in (str(y) for y in (2017, 2018, 2019, 2020, 2021, 2022,))], axis=1).stack()
-# NB. Not importing 2024 as the data are incomplete
-# NB. Not importing 2023 as Dominic informs us it is incorrect
-
-df.index = df.index.set_names(['District', 'Month', 'Year'])
+num_employees = df.set_index(['District', 'Month', 'Year'])['Emp_Totals']
 
 
 # Summarise trend over years (taking average by month and summing over District)
-year_by_year = df.groupby(by=['Year', 'District']).mean().groupby(by='Year').sum()
+year_by_year = num_employees.groupby(by=['Year', 'District']).mean().groupby(by='Year').sum()
+
+# Summarise trend over years alt. (taking sum over the districts, and then averaging over months)
+year_by_year2 = num_employees.groupby(by=['Year', 'Month']).sum().groupby(by='Year').mean()
+
 
 fig, ax = plt.subplots()
-year_by_year .plot(ax=ax, legend=False)
-ax.set_title('Trend in Healthcare Workers, 2017-2022', fontweight='bold', fontsize=10)
+year_by_year.plot(ax=ax, legend=False)
+# year_by_year2.plot(ax=ax, legend=False)
+ax.set_title('Trend in Healthcare Workers, 2017-2024', fontweight='bold', fontsize=10)
 ax.set_ylabel('Number of HCW')
-ax.set_ylim(0, 200_000)
+ax.set_ylim(0, 30_000)
 fig.tight_layout()
 fig.show()
 
 
 # Summarise trend over years, in each district
 fig, ax = plt.subplots(figsize=(6, 4), layout='constrained')
-df.groupby(by=['Year', 'District']).mean().unstack().plot(ax=ax, legend=False)
-ax.set_title('Trend in Healthcare Workers by District, 2017-2022', fontweight='bold', fontsize=10)
+num_employees.groupby(by=['Year', 'District']).mean().unstack().plot(ax=ax, legend=False)
+ax.set_title('Trend in Healthcare Workers by District, 2017-2024', fontweight='bold', fontsize=10)
 ax.set_ylabel('Number of HCW')
-ax.set_ylim([0, 30_000])
+ax.set_ylim([0, 5_000])
 fig.legend(loc="outside lower center", ncols=5, fontsize='small')
 fig.show()
 
 # difference vs 2017
 diff_since_2017 = year_by_year - year_by_year.at[2017]
+
 
 #%% Plot to explain setup of Scenario
 year_on_year_trend_normalised = year_by_year / year_by_year[2017]
@@ -75,9 +71,5 @@ plt.plot(x, [1.0] * len(x), '-gx', label='No Scale-up Counterfactual')
 plt.legend()
 plt.grid()
 # plt.axhline(y=1.0, color='k')
-plt.ylim(0.95, 2.0)
+plt.ylim(0.95, 1.3)
 plt.show()
-
-
-
-
