bc6d4290069d18aa6520016dc8aec4c835dc4ce3
Bingling <b.she@imperial.ac.uk>
2024-08-13 00:41:32 +0100

    calculate scale up factor outside healthsystem module

--- src/scripts/healthsystem/impact_of_hcw_capabilities_expansion/create_hr_minute_salary_by_officer_type_facility_id.py
+++ src/scripts/healthsystem/impact_of_hcw_capabilities_expansion/create_hr_minute_salary_by_officer_type_facility_id.py
@@ -44,3 +44,47 @@ four_cadres_cost = staff_cost.loc[
     staff_cost.Officer_Category.isin(['Clinical', 'DCSA', 'Nursing_and_Midwifery', 'Pharmacy'])].reset_index(drop=True)
 four_cadres_cost['cost_frac'] = four_cadres_cost['annual_cost'] / four_cadres_cost['annual_cost'].sum()
 assert four_cadres_cost.cost_frac.sum() == 1
+
+
+# calculate hr scale up factor for years 2020-2030 (10 years in total) outside the healthsystem module
+
+def calculate_hr_scale_up_factor(extra_budget_frac, yr, scenario) -> pd.DataFrame:
+    """This function calculates the yearly hr scale up factor for Clinical, DCSA, Nursing_and_Midwifery,
+    and Pharmacy cadres for a year yr, given a fraction of an extra budget allocated to each cadre and
+    a yearly budget growth rate of 4.2%.
+    Parameter extra_budget_frac (list) is a list of four floats, representing the fractions.
+    Parameter yr (int) is a year between 2020 and 2030.
+    Parameter scenario (string) is a column name in the extra budget fractions resource file.
+    Output dataframe stores scale up factors and relevant for the year yr.
+    """
+    # get data of previous year
+    prev_year = yr - 1
+    prev_data = scale_up_factor_dict[scenario][prev_year].copy()
+
+    # calculate and update scale_up_factor
+    prev_data['extra_budget_frac'] = extra_budget_frac
+    prev_data['extra_budget'] = 0.042 * prev_data.annual_cost.sum() * prev_data.extra_budget_frac
+    prev_data['extra_staff'] = prev_data.extra_budget / prev_data.Annual_Salary_USD
+    prev_data['scale_up_factor'] = (prev_data.Staff_Count + prev_data.extra_staff) / prev_data.Staff_Count
+
+    # store the updated data for the year yr
+    new_data = prev_data[['Officer_Category', 'Annual_Salary_USD', 'scale_up_factor']].copy()
+    new_data['Staff_Count'] = prev_data.Staff_Count + prev_data.extra_staff
+    new_data['annual_cost'] = prev_data.annual_cost + prev_data.extra_budget
+
+    return new_data
+
+
+# calculate scale up factors for all defined scenarios and years
+extra_budget_frac_data = pd.read_csv(resourcefilepath
+                                     / 'healthsystem' / 'human_resources' / 'scaling_capabilities'
+                                     / 'ResourceFile_HR_expansion_by_officer_type_given_extra_budget.csv'
+                                     ).set_index('Officer_Category')
+four_cadres_cost['scale_up_factor'] = 1
+scale_up_factor_dict = {s: {y: {} for y in range(2019, 2030)} for s in extra_budget_frac_data.columns}
+for s in extra_budget_frac_data.columns:
+    # for the initial/current year of 2019
+    scale_up_factor_dict[s][2019] = four_cadres_cost.drop(columns='cost_frac').copy()
+    # for the years with scaled up hr
+    for y in range(2020, 2030):
+        scale_up_factor_dict[s][y] = calculate_hr_scale_up_factor(list(extra_budget_frac_data[s]), y, s)
