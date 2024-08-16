7cc8d02a91f72fba99a8e54fef611f2ee021fef3
RachelMurray-Watson <rmurwat@gmail.com>
2024-08-16 10:49:34 +0100

    removed calculations of maternal and newborn mortality pending help

--- tests/test_record_prevalence_healthburden_class.py
+++ tests/test_record_prevalence_healthburden_class.py
@@ -51,21 +51,21 @@ def test_run_with_healthburden_with_dummy_diseases(tmpdir, seed):
     prevalence_malaria_log = output['tlo.methods.malaria']["prevalence"]["clinical_prev"]
 
     # Maternal deaths
-    maternal_deaths_function = prevalence['maternal_deaths']
+    #maternal_deaths_function = prevalence['maternal_deaths']
     death_df = output['tlo.methods.demography']['death']
-    properties_deceased = output['tlo.methods.demography.detail']["properties_of_deceased_persons"]
+    #properties_deceased = output['tlo.methods.demography.detail']["properties_of_deceased_persons"]
 
-    direct_deaths = len(death_df[death_df['cause'] == 'Maternal Disorders'])
-    indirect_deaths_non_hiv = len(properties_deceased.loc[
-        (properties_deceased['is_pregnant'] | properties_deceased['la_is_postpartum']) &
-        (properties_deceased['cause_of_death'].str.contains('Malaria|Suicide|ever_stroke|diabetes|chronic_ischemic_hd|ever_heart_attack|chronic_kidney_disease') |
-         (properties_deceased['cause_of_death'] == 'TB'))])
-    hiv_pd = len(properties_deceased.loc[
-        (properties_deceased['is_pregnant'] | properties_deceased['la_is_postpartum']) &
-        (properties_deceased['cause_of_death'].str.contains('AIDS_non_TB|AIDS_TB'))])
+   #direct_deaths = len(death_df[death_df['cause'] == 'Maternal Disorders'])
+    #indirect_deaths_non_hiv = len(properties_deceased.loc[
+    #    (properties_deceased['is_pregnant'] | properties_deceased['la_is_postpartum']) &
+    #    (properties_deceased['cause_of_death'].str.contains('Malaria|Suicide|ever_stroke|diabetes|chronic_ischemic_hd|ever_heart_attack|chronic_kidney_disease') |
+    #     (properties_deceased['cause_of_death'] == 'TB'))])
+    #hiv_pd = len(properties_deceased.loc[
+    #    (properties_deceased['is_pregnant'] | properties_deceased['la_is_postpartum']) &
+    #    (properties_deceased['cause_of_death'].str.contains('AIDS_non_TB|AIDS_TB'))])
 
-    hiv_indirect_maternal_deaths = hiv_pd * 0.3
-    maternal_deaths_log = direct_deaths + indirect_deaths_non_hiv + hiv_indirect_maternal_deaths
+    #hiv_indirect_maternal_deaths = hiv_pd * 0.3
+    #maternal_deaths_log = direct_deaths + indirect_deaths_non_hiv + hiv_indirect_maternal_deaths
 
     # Newborn deaths
     prevalence_newborn_deaths_function = prevalence['newborn_deaths']
@@ -97,6 +97,12 @@ def test_run_with_healthburden_with_dummy_diseases(tmpdir, seed):
     #    .count()
     #)
 
+    # Cardiometablis disorders
+    diabetes_prevalence_function = prevalence['diabetes']
+    diabetes_prevalence_log = output['tlo.methods.cardio_metabolic_disorders']["diabetes_prevalence"]
+
+    hypertension_prevalence_function = prevalence['hypertension']
+    hypertension_prevalence_log = output['tlo.methods.cardio_metabolic_disorders']["hypertension_prevalence"]
     results = {
         'prevalence_newborn_deaths_log': prevalence_newborn_deaths_log,
         'prevalence_newborn_deaths_function': prevalence_newborn_deaths_function,
