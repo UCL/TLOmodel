3962fa6df8fd7f0ced991861234c050caf62107e
RachelMurray-Watson <rmurwat@gmail.com>
2024-08-19 12:10:23 +0100

    Added check of "must have died in the last month"

--- src/tlo/methods/demography.py
+++ src/tlo/methods/demography.py
@@ -382,23 +382,23 @@ class Demography(Module):
             maternal_deaths = None
         else:
             df = self.sim.population.props
-            if len(df[df['age_days'] < 29]) == 0:
+            if len(df[(df['age_days'] < 29) & (df['date_of_death'] >= (self.sim.date - DateOffset(months=1)))]) == 0:
                 neonatal_deaths = 0
                 maternal_deaths = 0
             else:
-                neonatal_deaths = (len(df[(df['age_days'] < 29) & (df['age_years'] == 0) & ~(df['is_alive'])]) / len(df[df['age_days'] < 29])) * 1000
+                neonatal_deaths = (len(df[(df['age_days'] < 29) & (df['age_years'] == 0) & ~(df['is_alive']) & (df['date_of_death'] >= (self.sim.date - DateOffset(months=1)))]) / len(df[df['age_days'] < 29])) * 1000
                 number_births = len(df[df['age_days'] < 29])
                 maternal_direct_deaths = len(df.loc[(df['cause_of_death'] == 'Maternal Disorders')])
                 indirect_deaths_non_hiv = len(df.loc[
                                                   (df['is_pregnant'] | df['la_is_postpartum']) &
                                                   (df['cause_of_death'].str.contains(
                                                       'Malaria|Suicide|ever_stroke|diabetes|chronic_ischemic_hd|ever_heart_attack|chronic_kidney_disease') |
-                                                   (df['cause_of_death'] == 'TB'))
+                                                   (df['cause_of_death'] == 'TB')) & (df['date_of_death'] >= (self.sim.date - DateOffset(months=1)))
                                                   ])
 
                 direct_deaths_non_hiv = len(df.loc[
                                                 (df['is_pregnant'] | df['la_is_postpartum']) &
-                                                df['cause_of_death'].str.contains('AIDS_non_TB|AIDS_TB')
+                                                df['cause_of_death'].str.contains('AIDS_non_TB|AIDS_TB') & (df['date_of_death'] >= (self.sim.date - DateOffset(months=1)))
                                                 ])
                 direct_deaths_non_hiv = direct_deaths_non_hiv*0.3 # https://www.who.int/publications/i/item/9789240068759
                 maternal_deaths = maternal_direct_deaths + indirect_deaths_non_hiv + direct_deaths_non_hiv
