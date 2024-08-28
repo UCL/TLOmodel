d205aa456d7bab4f688f3c833adf2570ad53a45a
Bingling <b.she@imperial.ac.uk>
2024-08-28 11:54:13 +0100

    name logger elements consistently

--- src/tlo/methods/healthsystem.py
+++ src/tlo/methods/healthsystem.py
@@ -3043,8 +3043,8 @@ class HRExpansionByOfficerType(Event, PopulationScopeEventMixin):
                                         'Clinical, DCSA, Nursing_and_Midwifery, Pharmacy - '
                                         'given fractions of an extra budget',
                             data={
-                                'Scale up factor': [sf_clinical, sf_dcsa, sf_nursing, sf_pharmacy],
-                                'Year of scaling up': self.sim.date.year,
+                                'scale_up_factor': [sf_clinical, sf_dcsa, sf_nursing, sf_pharmacy],
+                                'year_of_scale_up': self.sim.date.year,
                             }
                             )
 
