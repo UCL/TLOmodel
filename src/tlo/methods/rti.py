f18c1197d9985fbbdd911f65f5e49376c15f910a
RachelMurray-Watson <rmurwat@gmail.com>
2024-09-04 12:26:01 +0100

    Now using (df['rt_inj_severity'] != 'none') again to record RTI injuries

--- src/tlo/methods/rti.py
+++ src/tlo/methods/rti.py
@@ -2443,7 +2443,7 @@ class RTI(Module, GenericFirstAppointmentsMixin):
         # This returns dataframe that reports on the prevalence of RTIs for all individuals
         df = self.sim.population.props
         total_prev = len(
-            df[(df['is_alive']) & (df['rt_road_traffic_inc'])]
+            df[(df['is_alive']) & (df['rt_inj_severity'] != 'none')]
         ) / len(df[df['is_alive']])
 
         return total_prev
