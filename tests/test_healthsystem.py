6ced85b6f3e8cff36c94f4a171c3e3b7cf4075a7
Bingling <b.she@imperial.ac.uk>
2024-09-06 10:58:35 +0100

    add comments in test

--- tests/test_healthsystem.py
+++ tests/test_healthsystem.py
@@ -2604,6 +2604,10 @@ def test_HR_expansion_by_officer_type(seed, tmpdir):
         params['end_year_HR_expansion_by_officer_type'] = end_year  # last update happens on 1 Jan (end_year - 1)
         params['HR_expansion_by_officer_type'] = HR_expansion_by_officer_type.to_dict()
 
+        # for testing _rescale_capabilities_to_capture_effective_capability
+        # params['year_mode_switch'] = 2011
+        # params['scale_to_effective_capabilities'] = True
+
         popsize = 100
         sim.make_initial_population(n=popsize)
 
