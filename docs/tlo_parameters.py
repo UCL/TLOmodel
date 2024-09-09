220b01be8cc76309ca70d0bcacba58039f5e9927
Matt Graham <matthew.m.graham@gmail.com>
2024-09-09 16:42:45 +0100

    Add option to create RST as well as MD output

--- docs/tlo_parameters.py
+++ docs/tlo_parameters.py
@@ -3,6 +3,7 @@
 import argparse
 from collections import defaultdict
 from collections.abc import Iterable
+from functools import partial
 from pathlib import Path
 from typing import TypeAlias, get_args
 import numpy
@@ -74,6 +75,8 @@ def get_parameter_tables(
     overriden_parameters: dict[str, dict[str, ParameterValue]],
     excluded_modules: set[str],
     excluded_parameters: dict[str, set[str]],
+    escape_characters: callable,
+    format_internal_link: callable,
     max_inline_parameter_length: int = 10,
 ) -> tuple[ModuleParameterTablesDict, ModuleStructuredParametersDict]:
     module_parameter_tables = {}
@@ -97,8 +100,8 @@ def get_parameter_tables(
             if value is None:
                 continue
             record = {
-                "Name": parameter_name,
-                "Description": parameter.description,
+                "Name": escape_characters(parameter_name),
+                "Description": escape_characters(parameter.description),
                 "Type": _TYPE_TO_DESCRIPTION[type(value)],
             }
             if (
@@ -111,7 +114,7 @@ def get_parameter_tables(
                 assert len(value) == 1
                 record["Value"] = str(value[0])
             else:
-                record["Value"] = internal_link_with_backlink_anchor(
+                record["Value"] = format_internal_link(
                     "...", parameter_id(module.name, parameter_name)
                 )
                 module_structured_parameters[module.name][parameter_name] = (
@@ -128,84 +131,159 @@ def parameter_id(module_name, parameter_name):
     return f"{module_name}-{parameter_name}"
 
 
-def markdown_table_or_html_summary(dataframe, rows_threshold=10):
-    if len(dataframe) > rows_threshold:
-        return dataframe._repr_html_()
-    else:
-        return dataframe.to_markdown(index=False)
+def dataframe_as_table(dataframe, rows_threshold=None, tablefmt="pipe"):
+    summarize = rows_threshold is not None and len(dataframe) > rows_threshold
+    if summarize:
+        original_rows = len(dataframe)
+        dataframe = dataframe[1:rows_threshold]
+    table_string = dataframe.to_markdown(index=False, tablefmt=tablefmt)
+    if summarize:
+        table_string += (
+            f"\n\n*Only first {rows_threshold} rows of {original_rows} are shown.*\n"
+        )
+    return table_string
 
 
-def anchor_tag(id: str) -> str:
+def md_anchor_tag(id: str) -> str:
     return f"<a id='{id}'></a>"
 
 
-def title(text: str) -> str:
-    return f"% {text}\n\n"
-
-
-def heading(text: str, level: int) -> str:
-    return "#" * level + " " + text + "\n\n"
-
-
-def list_item(text: str, bullet: str = "-", indent_level: int = 0) -> str:
+def md_list_item(text: str, bullet: str = "-", indent_level: int = 0) -> str:
     return "  " * indent_level + f"{bullet} {text}\n"
 
 
-def hyperlink(link_text: str, url: str) -> str:
+def md_hyperlink(link_text: str, url: str) -> str:
     return f"[{link_text}]({url})"
 
 
-def internal_link_with_backlink_anchor(
+def md_internal_link_with_backlink_anchor(
     link_text: str, id: str, suffix: str = "backlink"
 ):
-    return anchor_tag(f"{id}-{suffix}") + hyperlink(link_text, f"#{id}")
+    return md_anchor_tag(f"{id}-{suffix}") + md_hyperlink(link_text, f"#{id}")
 
 
-def anchor_and_backlink(id: str, suffix: str = "backlink"):
-    return anchor_tag(id) + hyperlink("↩", f"#{id}-{suffix}")
+def rst_internal_link(link_text: str, id: str):
+    return f":ref:`{link_text}<{id}>`"
 
 
-def write_parameters_markdown_file(
+def escape_rst_markup_characters(text: str):
+    return text.replace("_", "\_").replace("*", "\*")
+
+
+def md_anchor_and_backlink(id: str, suffix: str = "backlink"):
+    return md_anchor_tag(id) + md_hyperlink("↩", f"#{id}-{suffix}")
+
+
+def md_table_of_contents(module_names):
+    return "\n".join(
+        [
+            md_list_item(
+                md_internal_link_with_backlink_anchor(module_name, module_name.lower())
+            )
+            for module_name in module_names
+        ]
+    )
+
+
+def rst_table_of_contents(_module_names):
+    return ".. contents::\n   :local:\n   :depth: 1\n   :backlinks: entry\n\n"
+
+
+def md_header(text: str, level: int) -> str:
+    return ("#" * level if level > 0 else "%") + " " + text + "\n\n"
+
+
+def rst_header(title: str, level: int = 0) -> str:
+    separator_character = '*=-^"'[level]
+    line = separator_character * len(title)
+    return (line + "\n" if level == 0 else "") + title + "\n" + line + "\n\n"
+
+
+def md_module_header(module_name):
+    return md_header(f"{module_name} " + md_anchor_and_backlink(module_name.lower()), 1)
+
+
+def rst_module_header(module_name):
+    return rst_header(module_name, 1)
+
+
+def md_structured_parameter_header(parameter_name, module_name):
+    return md_header(
+        f"{parameter_name} "
+        + md_anchor_and_backlink(parameter_id(module_name, parameter_name)),
+        2,
+    )
+
+
+def rst_structured_parameter_header(parameter_name, module_name):
+    return f".. _{parameter_id(module_name, parameter_name)}:\n\n" + rst_header(
+        parameter_name, 2
+    )
+
+
+_formatters = {
+    ".md": {
+        "header": md_header,
+        "table_of_contents": md_table_of_contents,
+        "module_header": md_module_header,
+        "structured_parameter_header": md_structured_parameter_header,
+        "dataframe_as_table": partial(dataframe_as_table, tablefmt="pipe"),
+        "internal_link": md_internal_link_with_backlink_anchor,
+        "character_escaper": lambda x: x,
+    },
+    ".rst": {
+        "header": rst_header,
+        "table_of_contents": rst_table_of_contents,
+        "module_header": rst_module_header,
+        "structured_parameter_header": rst_structured_parameter_header,
+        "dataframe_as_table": partial(dataframe_as_table, tablefmt="grid"),
+        "internal_link": rst_internal_link,
+        "character_escaper": escape_rst_markup_characters,
+    },
+}
+
+
+def write_parameters_file(
     output_file_path: Path,
     module_parameter_tables: ModuleParameterTablesDict,
     module_structured_parameters: ModuleStructuredParametersDict,
+    summarization_rows_threshold: int = 10,
 ) -> None:
+    formatter = _formatters[output_file_path.suffix]
     with output_file_path.open("w") as output_file:
-        output_file.write(title("Parameters"))
-        for module_name in module_parameter_tables.keys():
-            output_file.write(
-                list_item(
-                    internal_link_with_backlink_anchor(module_name, module_name.lower())
-                )
-            )
+        output_file.write(formatter["header"]("Parameters", 0))
+        output_file.write("Default parameter values used in simulations.\n\n")
+        output_file.write(
+            formatter["table_of_contents"](module_parameter_tables.keys())
+        )
         output_file.write("\n")
         for module_name, parameter_table in module_parameter_tables.items():
-            output_file.write(
-                heading(f"{module_name} " + anchor_and_backlink(module_name.lower()), 2)
-            )
-            output_file.write(parameter_table.to_markdown(index=False))
+            output_file.write(formatter["module_header"](module_name))
+            output_file.write(formatter["dataframe_as_table"](parameter_table))
             output_file.write("\n\n")
             for (
                 parameter_name,
                 structured_parameter,
             ) in module_structured_parameters[module_name].items():
                 output_file.write(
-                    heading(
-                        f"{parameter_name} "
-                        + anchor_and_backlink(
-                            parameter_id(module_name, parameter_name)
-                        ),
-                        3,
+                    formatter["structured_parameter_header"](
+                        parameter_name, module_name
                     )
                 )
                 if isinstance(structured_parameter, dict):
                     for key, dataframe in structured_parameter.items():
-                        output_file.write(heading(key, 4))
-                        output_file.write(markdown_table_or_html_summary(dataframe))
-                        output_file.write("\n")
+                        output_file.write(formatter["header"](key, 3))
+                        output_file.write(
+                            formatter["dataframe_as_table"](
+                                dataframe, summarization_rows_threshold
+                            )
+                        )
+                        output_file.write("\n\n")
                 else:
                     output_file.write(
-                        markdown_table_or_html_summary(structured_parameter)
+                        formatter["dataframe_as_table"](
+                            structured_parameter, summarization_rows_threshold
+                        )
                     )
                     output_file.write("\n")
                 output_file.write("\n")
@@ -228,12 +306,16 @@ if __name__ == "__main__":
     )
     status_quo_parameters = get_parameters_for_status_quo()
     simulation.register(*fullmodel.fullmodel(args.resource_file_path))
+    internal_link_formatter = _formatters[args.output_file_path.suffix]["internal_link"]
+    character_escaper = _formatters[args.output_file_path.suffix]["character_escaper"]
     module_parameter_tables, module_structured_parameters = get_parameter_tables(
         simulation.modules.values(),
         status_quo_parameters,
         {"HealthBurden", "Wasting"},
-        {"Demography": {"gbd_causes_of_death_data"}, "Tb": {"who_incidence_estimates"}}
+        {"Demography": {"gbd_causes_of_death_data"}, "Tb": {"who_incidence_estimates"}},
+        character_escaper,
+        internal_link_formatter,
     )
-    write_parameters_markdown_file(
+    write_parameters_file(
         args.output_file_path, module_parameter_tables, module_structured_parameters
     )
