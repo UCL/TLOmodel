2d298af1c02e55e439505f647f79f20908e159b0
RachelMurray-Watson <rmurwat@gmail.com>
2024-09-11 14:13:27 +0100

    Changed to test new logs

--- src/scripts/longterm_projections/read_log_file.py
+++ src/scripts/longterm_projections/read_log_file.py
@@ -1,8 +1,9 @@
 from tlo.analysis.utils import create_pickles_locally, parse_log_file
 
 # File paths
-input_file = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-04T082106Z/0/0/longterm_trends_all_diseases__2024-09-04T082345.log'
-
+input_file = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-02T093957Z/0/2/longterm_trends_all_diseases__2024-09-02T094225.log'
+input_file = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-02T093957Z/0/4/longterm_trends_all_diseases__2024-09-02T094225.log'
+input_file = '/Users/rem76/Documents/longterm_trends_all_diseases__2024-09-04T172323.log'
 #overall_results = '/Users/rem76/PycharmProjects/TLOmodel/outputs/tbh03@ic.ac.uk/longterm_trends_all_diseases-2024-08-02T111316Z/0/0'
 
 # Get the results folder from the scenario outputs
@@ -10,7 +11,9 @@ input_file = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longt
 results = parse_log_file(input_file)
 
 
-scenario_output_dir = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-04T082106Z'
+scenario_output_dir = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-02T093957Z'
+scenario_output_dir = '/Users/rem76/Documents/'
+
 
 # # get the pickled files if not generated at the batch run
 create_pickles_locally(scenario_output_dir = scenario_output_dir, compressed_file_name_prefix='longterm_trends_all_diseases__')
