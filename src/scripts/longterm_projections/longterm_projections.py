2a396277842375bac5e4cc3f704bc2ba30f91cc5
RachelMurray-Watson <rmurwat@gmail.com>
2024-09-11 21:12:53 +0100

    return to actual params

--- src/scripts/longterm_projections/longterm_projections.py
+++ src/scripts/longterm_projections/longterm_projections.py
@@ -12,9 +12,9 @@ class LongRun(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2010, 1, 12)
-        self.pop_size = 1000
-        self.number_of_draws = 1
+        self.end_date = Date(2060, 1, 12)
+        self.pop_size = 100_000
+        self.number_of_draws = 10
         self.runs_per_draw = 1
         self.YEAR_OF_CHANGE = 2020
         self._scenarios = self._get_scenarios()
