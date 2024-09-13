9ca7381e497c35ee352930855778bf3b6a489eab
RachelMurray-Watson <rmurwat@gmail.com>
2024-09-13 10:38:57 +0100

    Changed it back to be the first part of treatment id (as those are the short code treatment ids)

--- src/scripts/longterm_projections/read_log_file.py
+++ src/scripts/longterm_projections/read_log_file.py
@@ -1,7 +1,11 @@
 from tlo.analysis.utils import create_pickles_locally, parse_log_file
 
 # File paths
+
+input_file = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-02T093957Z/0/0/longterm_trends_all_diseases__2024-09-02T094225.log'
+input_file = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-02T093957Z/0/1/longterm_trends_all_diseases__2024-09-02T094225.log'
 input_file = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-02T093957Z/0/2/longterm_trends_all_diseases__2024-09-02T094225.log'
+input_file = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-02T093957Z/0/3/longterm_trends_all_diseases__2024-09-02T094225.log'
 input_file = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-02T093957Z/0/4/longterm_trends_all_diseases__2024-09-02T094225.log'
 input_file = '/Users/rem76/Documents/longterm_trends_all_diseases__2024-09-04T172323.log'
 #overall_results = '/Users/rem76/PycharmProjects/TLOmodel/outputs/tbh03@ic.ac.uk/longterm_trends_all_diseases-2024-08-02T111316Z/0/0'
@@ -12,7 +16,6 @@ results = parse_log_file(input_file)
 
 
 scenario_output_dir = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-02T093957Z'
-scenario_output_dir = '/Users/rem76/Documents/'
 
 
 # # get the pickled files if not generated at the batch run
