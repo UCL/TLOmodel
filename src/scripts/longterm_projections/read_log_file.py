9afedd57781c2b62bea35a4bf563fa3e4927b3aa
RachelMurray-Watson <rmurwat@gmail.com>
2024-09-30 12:30:13 +0100

    Accidentally deleted scenario directory

--- src/scripts/longterm_projections/read_log_file.py
+++ src/scripts/longterm_projections/read_log_file.py
@@ -5,7 +5,7 @@ from tlo.analysis.utils import create_pickles_locally, parse_log_file
 
 # Parse the log file
 
-
+scenario_output_dir = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/longterm_trends_all_diseases-2024-09-12T084811Z/'
 
 # # get the pickled files if not generated at the batch run
 create_pickles_locally(scenario_output_dir = scenario_output_dir, compressed_file_name_prefix='longterm_trends_all_diseases__')
