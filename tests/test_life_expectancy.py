b55b93a1469718eebdef595f29ce7cee4dcb3392
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2024-10-02 10:27:16 +0100

    update test: * inequality should be < not <= * read of the picklefile can streamlined * check should be equality of the estimates of the two methods * linting * update docstring

--- tests/test_life_expectancy.py
+++ tests/test_life_expectancy.py
@@ -3,6 +3,7 @@ import os
 import pickle
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from tlo.analysis.life_expectancy import (
@@ -40,19 +41,18 @@ def test_get_life_expectancy():
     assert rtn_full.columns.levels[1].to_list() == [0, 1]
 
 
-def test_probability_premature_death(tmpdir, age_before_which_death_is_defined_as_premature: int =70):
+def test_probability_premature_death(tmpdir, age_before_which_death_is_defined_as_premature: int = 70):
     """
     Test the calculation of the probability of premature death from a simulated cohort.
 
     This function loads results from a dummy cohort (N = 100, with 37 F and 63 M) simulation where all individuals start at age 0.
     The simulation was then run for 70 years (2010 - 2080), during which individuals could die but nobody could be born.
-    In this dummy data set, 7 F die and 24 M die, giving a probability of premature death as 0.189 and 0.381 respectively.
+    In this dummy data set, 6 F die and 23 M die prematurely, giving a probability of premature death as 0.16 and 0.37, respectively.
     The premature deaths amongst these individuals is then the number that have died before the age of 70 (default value).
     This test uses the calculates the probability of premature death separately for males and females using the
     data from this simulated run and the function get_probability_of_premature_death.
     It then compares these simulated probabilities against the total number of deaths before the age of 70 (default)
     that occurred in the simulated cohort.
-
     """
     # load results from a dummy cohort where everyone starts at age 0.
     target_period = (datetime.date(2010, 1, 1), datetime.date(2080, 12, 31))
@@ -60,41 +60,32 @@ def test_probability_premature_death(tmpdir, age_before_which_death_is_defined_a
     results_folder_dummy_results = Path(os.path.dirname(__file__)) / 'resources' / 'probability_premature_death'
     pickled_file = os.path.join(results_folder_dummy_results, '0', '0', 'tlo.methods.demography.pickle')
 
+    # - Compute 'manually' from raw data
     with open(pickled_file, 'rb') as file:
         demography_data = pickle.load(file)
-                # test parsing when log level is INFO
-    initial_female = demography_data['population']['female'][0]
-    initial_male = demography_data['population']['male'][0]
+    initial_popsize = {'F':  demography_data['population']['female'][0], 'M': demography_data['population']['male'][0]}
+    deaths_total = demography_data['death'][['sex', 'age']]
+    num_premature_deaths = deaths_total.loc[deaths_total['age'] < age_before_which_death_is_defined_as_premature] \
+                                       .groupby('sex') \
+                                       .size() \
+                                       .to_dict()
+    prob_premature_death = {s: num_premature_deaths[s] / initial_popsize[s] for s in ("M", "F")}
 
-    death_sex = extract_results(
-        results_folder_dummy_results,
-        module="tlo.methods.demography",
-        key="death",
-        column="sex",
-        do_scaling=False
-    )
-    death_age = extract_results(
-        results_folder_dummy_results,
-        module="tlo.methods.demography",
-        key="death",
-        column="age",
-        do_scaling=False
-    )
-    deaths_total= pd.DataFrame({
-    'Sex': death_sex[0][0],
-    'Age': death_age[0][0]}, index = range(len(death_sex)))
-
-    probability_premature_death_sim_F = len(deaths_total[
-    (deaths_total['Sex'] == 'F') &
-    (deaths_total['Age'].astype(int) <= age_before_which_death_is_defined_as_premature)])/ initial_female
-    probability_premature_death_sim_M = len(deaths_total[(deaths_total['Sex'] == 'M') & (
-            deaths_total['Age'].astype(int) <= age_before_which_death_is_defined_as_premature)]) / initial_male
-
+    # - Compute using utility function
     probability_premature_death_summary = get_probability_of_premature_death(
         results_folder=results_folder_dummy_results,
         target_period=target_period,
-        summary=True,)
+        summary=True,
+    )
 
-
-    assert probability_premature_death_summary[0]['lower'][0] < probability_premature_death_sim_F > probability_premature_death_summary[0]['lower'][0]
-    assert probability_premature_death_summary[0]['lower'][1] < probability_premature_death_sim_M > probability_premature_death_summary[0]['lower'][1]
+    # Confirm both methods gives the same answer
+    assert np.isclose(
+        probability_premature_death_summary.loc["F"].loc[(0, 'mean')],
+        prob_premature_death['F'],
+        atol=0.01
+    )
+    assert np.isclose(
+        probability_premature_death_summary.loc["M"].loc[(0, 'mean')],
+        prob_premature_death['M'],
+        atol=0.01
+    )
