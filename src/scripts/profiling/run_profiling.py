eeb8a7ba6378df85c8b7cec27ef69ea050902487
Matt Graham <matthew.m.graham@gmail.com>
2024-10-04 15:04:47 +0100

    Lower profiling % duration threshold to show more frames

--- src/scripts/profiling/run_profiling.py
+++ src/scripts/profiling/run_profiling.py
@@ -307,7 +307,7 @@ def run_profiling(
             color=True,
             flat=True,
             flat_time="total",
-            processor_options={"show_regex": ".*/tlo/.*", "hide_regex": ".*/pandas/.*"}
+            processor_options={"show_regex": ".*/tlo/.*", "hide_regex": ".*/pandas/.*", "filter_threshold": 1e-3}
         )
         converter = Ansi2HTMLConverter(title=output_name)
         print(f"Writing {output_html_file}", end="...", flush=True)
