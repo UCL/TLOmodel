0cbfbb3470d37339524b0746f0700c1dbd081211
RachelMurray-Watson <rmurwat@gmail.com>
2024-10-10 12:53:44 +0100

    Functions and process for unzipping file directories and extracting netCDF files NB - can only work for ssp1_1_9 at the moment

---
+++ src/scripts/climate_change/process_CMIP6_data.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+import zipfile
+
+
+def unzip_all_in_directory(directory):
+    """
+    Unzips all .zip files in the specified directory, extracting each into a separate folder.
+    Parameters:
+        directory (str): The path to the folder containing the .zip files.
+    """
+    for filename in os.listdir(directory):
+        if filename.endswith('.zip'):
+            file_path = os.path.join(directory, filename)
+            extract_dir = os.path.join(directory, filename[:-4])
+            os.makedirs(extract_dir, exist_ok=True)
+
+            with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                zip_ref.extractall(extract_dir)
+
+
+def extract_nc_files_from_unzipped_folders(directory):
+    """
+    Searches for .nc files in the specified directory and all its subfolders,
+    and copies them to the output directory, maintaining the folder structure.
+
+    Parameters:
+        directory (str): The path to the folder containing the unzipped folders.
+        output_directory (str): The path to the folder where .nc files should be copied.
+    """
+    output_directory = os.path.join(directory, 'nc_files')
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith('.nc'):
+                source_file_path = os.path.join(root, filename)
+                # Copy the .nc file to the 'nc_files' directory
+                shutil.copy2(source_file_path, output_directory)
+
+
+
+# unzip files and extract the netCDF files
+
+base_dir = "/Users/rem76/Desktop/Climate_change_health/Data/Precipitation_data/"
+
+scenarios = ["ssp1_1_9"] #,"ssp1_2_6", "ssp4_3_4", "ssp5_3_4OS", "ssp2_4_5", "ssp4_6_0", "ssp3_7_0", "ssp5_8_5"]
+for scenario in scenarios:
+    scenario_directory = os.path.join(base_dir, scenario)
+    unzip_all_in_directory(scenario_directory)
+    extract_nc_files_from_unzipped_folders(scenario_directory)
