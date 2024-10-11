9b5d27bbd86b1744c5f679354aa32e10ec3c9229
sakshimohan <sakshi.mohan@york.ac.uk>
2024-10-11 06:59:10 +0100

    divide staff count by number of years and add assertion

--- src/scripts/costing/analysis_scenario_test_hr_logger.py
+++ src/scripts/costing/analysis_scenario_test_hr_logger.py
@@ -32,6 +32,9 @@ print('Script Start', datetime.datetime.now().strftime('%H:%M'))
 outputfilepath = Path('./outputs/')
 resourcefilepath = Path("./resources")
 results_folder = get_scenario_outputs('scenario_test_hr_logger.py', outputfilepath)[0]
+log = load_pickled_dataframes(results_folder)
+final_year_of_simulation = max(log['tlo.simulation']['info']['date']).year
+first_year_of_simulation = min(log['tlo.simulation']['info']['date']).year
 
 def expand_capacity_by_officer_type_and_facility_level(_df: pd.Series) -> pd.Series:
     """Summarise the parsed logged-key results for one draw (as dataframe) into a pd.Series."""
@@ -54,9 +57,10 @@ staff_count_by_cadre_and_level= staff_count_by_cadre_and_level.reset_index()
 staff_count_by_cadre_and_level.columns = ['_'.join([str(i) for i in col]).strip() for col in staff_count_by_cadre_and_level.columns]
 staff_count_by_cadre_and_level['Facility_Level'] = staff_count_by_cadre_and_level['index_'].str.split('_').str[1]
 staff_count_by_cadre_and_level['Cadre'] = staff_count_by_cadre_and_level['index_'].str.split('_').str[3]
-total_staff_count_by_cadre = staff_count_by_cadre_and_level.groupby('Cadre')['0_0'].sum()
+total_staff_count_by_cadre = staff_count_by_cadre_and_level.groupby('Cadre')['0_0'].sum()/(final_year_of_simulation-first_year_of_simulation)
 
 # Staff count as per the resourcefile
-actual_staff_count = pd.read_csv(resourcefilepath / 'healthsystem' / 'human_resources' / 'actual' /
+actual_staff_count = pd.read_csv(resourcefilepath / 'healthsystem' / 'human_resources' / 'funded_plus' /
                 'ResourceFile_Daily_Capabilities.csv')
 total_staff_count_by_cadre_actual = actual_staff_count.groupby('Officer_Category')['Staff_Count'].sum()
+assert total_staff_count_by_cadre_actual['Clinical'] == total_staff_count_by_cadre['Clinical']
