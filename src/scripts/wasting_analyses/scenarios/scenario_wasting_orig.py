a6de785a5eeb2285c57edceda1d07e4e6b44b9b4
Eva Janouskova <janouskova.eva.012@gmail.com>
2024-10-12 00:08:36 +0100

    scenarios wast: names udpated in comments

--- src/scripts/wasting_analyses/scenarios/scenario_wasting_orig.py
+++ src/scripts/wasting_analyses/scenarios/scenario_wasting_orig.py
@@ -3,11 +3,11 @@ This file defines a scenario for wasting analysis.
 
 It can be submitted on Azure Batch by running:
 
-    tlo batch-submit src/scripts/wasting_analyses/scenario_wasting.py
+    tlo batch-submit src/scripts/wasting_analyses/scenarios/scenario_wasting_orig.py
 
 or locally using:
 
-    tlo scenario-run src/scripts/wasting_analyses/scenario_wasting.py
+    tlo scenario-run src/scripts/wasting_analyses/scenarios/scenario_wasting_orig.py
 """
 import warnings
 
