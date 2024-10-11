2eb0ce731a0bb1df8f9c3cd39f957f02d2383e7c
RachelMurray-Watson <rmurwat@gmail.com>
2024-10-11 11:48:57 +0100

    Creating dictionaries of data by model and grid

--- src/scripts/climate_change/process_CMIP6_data.py
+++ src/scripts/climate_change/process_CMIP6_data.py
@@ -1,8 +1,10 @@
+import glob
 import os
 import shutil
 import zipfile
-
-
+import glob
+from netCDF4 import Dataset
+import pandas as pd
 def unzip_all_in_directory(directory):
     """
     Unzips all .zip files in the specified directory, extracting each into a separate folder.
@@ -49,4 +51,32 @@ for scenario in scenarios:
     scenario_directory = os.path.join(base_dir, scenario)
     unzip_all_in_directory(scenario_directory)
     extract_nc_files_from_unzipped_folders(scenario_directory)
+
+
+# Put all into one csv file
+
+#### Multiple files
+file_list = glob.glob(os.path.join(base_dir, "*.nc"))
+data_by_model_and_grid = {}
+models = ["cams_csm1_0", "ipsl_cm6a_lr", "miroc6","miroc_es2l", "mri_esm2_0", "canesm5", "cnrm_esm2_1", "ec_earth3", "ec_earth3_veg_lr", "fgoals_g3", "gfdl_esm4", "ukesm1_0_ll"]
+model = 0
+for file in glob.glob(os.path.join(base_dir, "*.nc")):
+    data_per_model  = Dataset(file, mode='r')
+    pr_data = data_per_model.variables['pr'][:]  # in kg m-2 s-1 = mm s-1 x 86400 to get to day
+    lat_data = data_per_model.variables['lat'][:]
+    long_data = data_per_model.variables['lon'][:]
+    grid_dictionary = {}
+    grid = 0
+    for i in range(len(long_data)):
+        for j in range(len(lat_data)):
+            precip_data_for_grid = pr_data[:,i,j] # across all time points
+            precip_data_for_grid = precip_data_for_grid * 86400 # to get from per second to per day
+            grid_dictionary[grid] = precip_data_for_grid
+            grid += 1
+    data_by_model_and_grid[models[model]] = grid_dictionary
+    model += 1
+
+
+
+
 
