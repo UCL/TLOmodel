25bd34c01c44ba741c262156e8494edd418a86c6
RachelMurray-Watson <rmurwat@gmail.com>
2024-10-14 10:08:23 +0100

    Dropped division by metric as unneeded

--- src/scripts/climate_change/reporting_and_weather_data.py
+++ src/scripts/climate_change/reporting_and_weather_data.py
@@ -10,22 +10,6 @@ from sklearn.metrics import r2_score
 # Reporting rate is expected reporting vs actual reporting
 reporting_data = pd.read_csv('/Users/rem76/Desktop/Climate_change_health/Data/Reporting_Rate/Reporting_Rate_Central_Hospital_2000_2024.csv')
 
-# Divide dataset based on what is being reported
-# get metrics recorded
-all_columns = reporting_data.columns
-metrics = set([col.split(" - Reporting rate")[0] for col in all_columns])
-metrics = {metric for metric in metrics if not metric.startswith("organisation")} # inlcude only reporting data
-
-
-monthly_reporting_data_by_metric =  {}
-
-for metric in metrics:
-    columns_of_interest = [reporting_data.columns[1]] + reporting_data.columns[reporting_data.columns.str.startswith(metric)].tolist()
-    data_of_interest = reporting_data[columns_of_interest]
-    data_of_interest.columns = [col.replace(metric, "") for col in data_of_interest.columns]
-    data_of_interest.columns = [col.replace(" - Reporting rate ", "") for col in data_of_interest.columns]
-    monthly_reporting_data_by_metric[metric] = data_of_interest
-
 ### Actually don't want by metric - instead look across all dates for a given row and average (i.e. want to average by month by facility, not by metric by facility)
 ### But need to drop mental health, as that is only relevant for the  Zomba Mental Hospital and otherwise brings down averages
 # extract mental health data
