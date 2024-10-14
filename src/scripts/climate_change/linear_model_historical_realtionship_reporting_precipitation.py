a8ed9c62e9b3d0ce218626e63d10f0d463950f5e
RachelMurray-Watson <rmurwat@gmail.com>
2024-10-14 13:47:59 +0100

    Two versions of linear regression - one by facility (year and month as fixed effects) and one using all data, with facility as a fixed effect
    Give very different result s

--- src/scripts/climate_change/linear_model_historical_realtionship_reporting_precipitation.py
+++ src/scripts/climate_change/linear_model_historical_realtionship_reporting_precipitation.py
@@ -1,9 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy as sp
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import make_pipeline
 
 # data is from 2011 - 2024
 monthly_reporting_by_facility = pd.read_csv("/Users/rem76/Desktop/Climate_change_health/Data/monthly_reporting_by_facility_lm.csv", index_col=0)
@@ -54,35 +57,57 @@ plt.tight_layout()
 #plt.show()
 
 
-## Linear regression
+
+## Linear regression - flattened
 # year as a fixed effect
 year = range(2014, 2024, 1) # year as a fixed effect
 year_repeated = [y for y in year for _ in range(12)]
 year = year_repeated[:-4]
+year_flattened = year*len(weather_data_historical.columns) # to get flattened data
+
 # add month as a fixed effect
 month = range(12)
 month_repeated = [m for m in month for _ in range(2014, 2024, 1)]
 month = month_repeated[:-4]
+month_flattened = month*len(weather_data_historical.columns)
 
-X = weather_data_historical.values
-y = monthly_reporting_by_facility.values
-if X.ndim == 1:
-    X = X.reshape(-1, 1)
-if y.ndim == 1:
-    y = y.reshape(-1, 1)
-print(len(year))
-print(len(X))
-print(len(month))
-X = np.column_stack((X, year, month))
+# facility as fixed effect
+facility_flattened = list(range(len(weather_data_historical.columns))) * len(month)
+# location as a fixed effect
 
-# Perform linear regression
+# linear regression - flatten for more data points
+X = weather_data_historical.values.flatten().reshape(-1, 1)
+y = monthly_reporting_by_facility.values.flatten()
+
+#X = np.column_stack((X)) #, year_flattened, month_flattened, facility_flattened))
+print("X shape:", X.shape)
+print("y shape:", y.shape)
+# # Perform linear regression
 model = LinearRegression()
 model.fit(X, y)
 y_pred = model.predict(X)
 
-# Evaluate the model
 r2 = r2_score(y, y_pred)
-print(f'R-squared: {r2:.2f}')
+print(r2 )
 print(model.coef_)
-print(model.intercept_)
 
+
+# ## Linear regression - by facility
+results_list = []
+
+for facility in monthly_reporting_by_facility.columns:
+    y = monthly_reporting_by_facility[facility].values
+    X = weather_data_historical.values
+    X = np.column_stack((X, year, month))
+
+    model = LinearRegression()
+    model.fit(X, y)
+    y_pred = model.predict(X)
+
+    r2 = r2_score(y, y_pred)
+    coefficients = model.coef_
+
+    results_list.append({'Facility': facility, 'R2': r2, 'Coefficients': coefficients})
+
+results = pd.DataFrame(results_list)
+print(results)
