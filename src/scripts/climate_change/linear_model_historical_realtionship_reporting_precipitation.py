0c077a9559b1a8dfdc5550546026d2f722e1b40e
RachelMurray-Watson <rmurwat@gmail.com>
2024-10-14 10:53:52 +0100

    Added labels to graph removed September 2024 (bad quality data)

--- src/scripts/climate_change/linear_model_historical_realtionship_reporting_precipitation.py
+++ src/scripts/climate_change/linear_model_historical_realtionship_reporting_precipitation.py
@@ -4,14 +4,18 @@ from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 import pandas as pd
 
-monthly_reporting_by_facility = pd.read_csv_csv("/Users/rem76/Desktop/Climate_change_health/Data/monthly_reporting_by_facility_lm.csv")
-weather_data_historical = pd.read_csv_csv("/Users/rem76/Desktop/Climate_change_health/Data/historical_weather_by_facility_lm.csv")
+# data is from 2011 - 2024
+monthly_reporting_by_facility = pd.read_csv("/Users/rem76/Desktop/Climate_change_health/Data/monthly_reporting_by_facility_lm.csv", index_col=0)
+weather_data_historical = pd.read_csv("/Users/rem76/Desktop/Climate_change_health/Data/historical_weather_by_facility_lm.csv", index_col=0)
 plt.figure(figsize=(12, 6))
 
 # Plot each facility's reporting data against weather data
 for facility in weather_data_historical.columns:
     plt.plot(weather_data_historical.index, monthly_reporting_by_facility[facility], label=facility)
-
+months = weather_data_historical.index
+year_labels = range(2011, 2025, 1)
+year_ticks = range(0, len(months), 12)
+plt.xticks(year_ticks, year_labels, rotation=90)
 plt.xlabel('Weather Data')
 plt.ylabel('Reporting')
 plt.title('Reporting vs. Weather Data by Facility')
@@ -20,6 +24,10 @@ plt.grid()
 plt.tight_layout()
 plt.show()
 
+## Drop Mental Hospital - no reporting before 2011 maybe
+weather_data_historical = weather_data_historical.drop("Zomba Mental Hospital", axis=1)
+monthly_reporting_by_facility = monthly_reporting_by_facility.drop("Zomba Mental Hospital", axis=1)
+
 X = weather_data_historical.values
 y = monthly_reporting_by_facility.values
 if X.ndim == 1:
@@ -27,6 +35,9 @@ if X.ndim == 1:
 if y.ndim == 1:
     y = y.reshape(-1, 1)
 
+# Remove September 2024, bad quality data
+X = X[0:len(X) - 1]
+y = y[0:len(y) - 1]
 
 # Perform linear regression
 model = LinearRegression()
