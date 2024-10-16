441af24d23743bdd03fc677721b33e819707f033
RachelMurray-Watson <rmurwat@gmail.com>
2024-10-16 08:55:15 +0100

    removed double request for days updated directory

--- src/scripts/climate_change/data_retrieval_ERA5_reanalysis_daily.py
+++ src/scripts/climate_change/data_retrieval_ERA5_reanalysis_daily.py
@@ -3,7 +3,7 @@ import os
 import cdsapi
 years = ["2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021",
          "2022", "2023", "2024"]
-base_dir = "/Users/rem76/Desktop/Climate_change_health/Data/Precipitation_data/Historical"
+base_dir = "/Users/rem76/Desktop/Climate_change_health/Data/Precipitation_data/Historical/daily_maximum"
 
 for year in years:
         year_dir = os.path.join(base_dir, year)
@@ -20,19 +20,6 @@ for year in years:
                 "04", "05", "06",
                 "07", "08", "09",
                 "10", "11", "12"
-            ],
-            "day": [
-                "01", "02", "03",
-                "04", "05", "06",
-                "07", "08", "09",
-                "10", "11", "12",
-                "13", "14", "15",
-                "16", "17", "18",
-                "19", "20", "21",
-                "22", "23", "24",
-                "25", "26", "27",
-                "28", "29", "30",
-                "31"
             ],
             "day": [
                 "01", "02", "03",
