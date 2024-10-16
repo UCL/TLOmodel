1ce484ffc81fb920641c42ea7d4ac6094b3a3f8f
RachelMurray-Watson <rmurwat@gmail.com>
2024-10-16 12:39:10 +0100

    Added new data for finer grain on facilities

--- src/scripts/climate_change/linear_model_historical_realtionship_reporting_precipitation.py
+++ src/scripts/climate_change/linear_model_historical_realtionship_reporting_precipitation.py
@@ -5,22 +5,27 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import statsmodels.api as sm
 
-# data is from 2011 - 2024
-monthly_reporting_by_facility = pd.read_csv("/Users/rem76/Desktop/Climate_change_health/Data/monthly_reporting_by_facility_lm.csv", index_col=0)
-weather_data_historical = pd.read_csv("/Users/rem76/Desktop/Climate_change_health/Data/historical_weather_by_facility_lm.csv", index_col=0)
-#
+# # data is from 2011 - 2024 - for facility
+# monthly_reporting_by_facility = pd.read_csv("/Users/rem76/Desktop/Climate_change_health/Data/monthly_reporting_by_facility_lm.csv", index_col=0)
+# weather_data_historical = pd.read_csv("/Users/rem76/Desktop/Climate_change_health/Data/historical_weather_by_facility_lm.csv", index_col=0)
+
+monthly_reporting_by_facility = pd.read_csv("/Users/rem76/Desktop/Climate_change_health/Data/monthly_reporting_by_DHO_lm.csv", index_col=0)
+weather_data_historical = pd.read_csv("/Users/rem76/Desktop/Climate_change_health/Data/historical_weather_by_DHO_lm.csv", index_col=0)
+print(len(monthly_reporting_by_facility))
+print(len(weather_data_historical))
+# weather data is from 2011 - but report
 # Plot each facility's reporting data against weather data
 plt.figure(figsize=(12, 6))
 
 for facility in weather_data_historical.columns:
-    plt.plot(weather_data_historical.index, monthly_reporting_by_facility[facility], label=facility)
+    plt.plot(weather_data_historical.index, monthly_reporting_by_facility, label=facility)
 months = weather_data_historical.index
 year_labels = range(2011, 2025, 1)
 year_ticks = range(0, len(months), 12)
 plt.xticks(year_ticks, year_labels, rotation=90)
-plt.xlabel('Weather Data')
-plt.ylabel('Reporting')
-plt.title('Reporting vs. Weather Data by Facility')
+plt.xlabel('Year')
+plt.ylabel('Reporting %')
+plt.title('Reporting by Facility')
 plt.legend(title='Facilities', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.grid()
 plt.tight_layout()
@@ -29,23 +34,20 @@ plt.tight_layout()
 ## Drop Mental Hospital - bad reporting generally
 weather_data_historical = weather_data_historical.drop("Zomba Mental Hospital", axis=1)
 monthly_reporting_by_facility = monthly_reporting_by_facility.drop("Zomba Mental Hospital", axis=1)
-## Drop Queen Elizabeth - bad reporting generally, also have one represented for Lilongwe
-weather_data_historical = weather_data_historical.drop("Queen Elizabeth Central Hospital", axis=1)
-monthly_reporting_by_facility = monthly_reporting_by_facility.drop("Queen Elizabeth Central Hospital", axis=1)
+## Drop MOH MALAWI Govt
+weather_data_historical = weather_data_historical.drop("MOH MALAWI Govt", axis=1)
+monthly_reporting_by_facility = monthly_reporting_by_facility.drop("MOH MALAWI Govt", axis=1)
 ## Drop September 2024
 weather_data_historical = weather_data_historical.drop(weather_data_historical.index[-1])
 monthly_reporting_by_facility = monthly_reporting_by_facility.drop(monthly_reporting_by_facility.index[-1])
-## Drop before 2016?  12*6
-weather_data_historical = weather_data_historical.drop(weather_data_historical.index[0:72]).reset_index(drop=True)
-monthly_reporting_by_facility = monthly_reporting_by_facility.drop(monthly_reporting_by_facility.index[0:72]).reset_index(drop=True)
 
 # Plot each facility's reporting data against weather data
 plt.figure(figsize=(12, 6))
 
 for facility in weather_data_historical.columns:
-    plt.plot(weather_data_historical.index, monthly_reporting_by_facility[facility], label=facility)
+    plt.plot(weather_data_historical.index, monthly_reporting_by_facility, label=facility)
 months = weather_data_historical.index
-year_labels = range(2017, 2025, 1)
+year_labels = range(2011, 2025, 1)
 year_ticks = range(0, len(months), 12)
 plt.xticks(year_ticks, year_labels, rotation=90)
 plt.xlabel('Weather Data')
@@ -60,14 +62,14 @@ plt.tight_layout()
 
 ## Linear regression - flattened
 # year
-year = range(2016, 2024, 1) # year as a fixed effect
+year = range(2011, 2025, 1) # year as a fixed effect
 year_repeated = [y for y in year for _ in range(12)]
 year = year_repeated[:-4]
 year_flattened = year*len(weather_data_historical.columns) # to get flattened data
 
 # month
 month = range(12)
-month_repeated = [m for m in month for _ in range(2016, 2024, 1)]
+month_repeated = [m for m in month for _ in range(2011, 2025, 1)]
 month = month_repeated[:-4]
 month_flattened = month*len(weather_data_historical.columns)
 
@@ -77,6 +79,10 @@ facility_flattened = list(range(len(weather_data_historical.columns))) * len(mon
 # linear regression - flatten for more data points
 weather_data = weather_data_historical.values.flatten()
 y = monthly_reporting_by_facility.values.flatten()
+print(len(weather_data))
+print(len(year_flattened))
+print(len(month_flattened))
+print(len(facility_flattened))
 X = pd.DataFrame({
     'weather_data': weather_data,
     'year': year_flattened,
@@ -88,7 +94,7 @@ X = pd.DataFrame({
 facility_encoded = pd.get_dummies(X['facility'])
 
 X = np.column_stack((X[['weather_data', 'year', 'month']], facility_encoded))
-
+print(len(y))
 model = sm.OLS(y,X)
 results = model.fit()
 
