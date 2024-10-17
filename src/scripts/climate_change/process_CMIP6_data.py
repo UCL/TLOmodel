cad17be6af903e66654b06e48e64a5c12fb691ef
RachelMurray-Watson <rmurwat@gmail.com>
2024-10-17 10:46:06 +0100

    tidied

--- src/scripts/climate_change/process_CMIP6_data.py
+++ src/scripts/climate_change/process_CMIP6_data.py
@@ -40,8 +40,8 @@ def extract_nc_files_from_unzipped_folders(directory):
         for filename in files:
             if filename.endswith('.nc'):
                 source_file_path = os.path.join(root, filename)
-                # Copy the .nc file to the 'nc_files' directory
                 shutil.copy2(source_file_path, output_directory)
+
 
 
 
@@ -57,13 +57,17 @@ for scenario in scenarios:
 
 
 # Put all into one csv file
+base_dir = "/Users/rem76/Desktop/Climate_change_health/Data/Precipitation_data/"
 
+scenario = "ssp1_1_9"
+scenario_directory = os.path.join(base_dir, scenario)
+nc_file_directory = os.path.join(scenario_directory, 'nc_files')
 #### Multiple files
-file_list = glob.glob(os.path.join(base_dir, "*.nc"))
+file_list = glob.glob(os.path.join(nc_file_directory, "*.nc"))
 data_by_model_and_grid = {}
 models = ["cams_csm1_0", "ipsl_cm6a_lr", "miroc6","miroc_es2l", "mri_esm2_0", "canesm5", "cnrm_esm2_1", "ec_earth3", "ec_earth3_veg_lr", "fgoals_g3", "gfdl_esm4", "ukesm1_0_ll"]
 model = 0
-for file in glob.glob(os.path.join(base_dir, "*.nc")):
+for file in file_list:
     data_per_model  = Dataset(file, mode='r')
     pr_data = data_per_model.variables['pr'][:]  # in kg m-2 s-1 = mm s-1 x 86400 to get to day
     lat_data = data_per_model.variables['lat'][:]
@@ -72,14 +76,15 @@ for file in glob.glob(os.path.join(base_dir, "*.nc")):
     grid = 0
     for i in range(len(long_data)):
         for j in range(len(lat_data)):
-            precip_data_for_grid = pr_data[:,i,j] # across all time points
+            precip_data_for_grid = pr_data[:,j,i] # across all time points
             precip_data_for_grid = precip_data_for_grid * 86400 # to get from per second to per day
             grid_dictionary[grid] = precip_data_for_grid
+
             grid += 1
     data_by_model_and_grid[models[model]] = grid_dictionary
     model += 1
-print(data_by_model_and_grid)
 data_by_model_and_grid = pd.DataFrame.from_dict(data_by_model_and_grid)
+
 data_by_model_and_grid.to_csv(Path(scenario_directory)/"data_by_model_and_grid.csv")
 
 
