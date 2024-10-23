ec94bbe26827d5a33df736ce7674fc7c976e8b5a
mmsuarezcosta <mmsuarezcosta@gmail.com>
2024-10-23 10:05:40 +0200

    comment out hash

--- src/scripts/cervical_cancer_analyses/cervical_cancer_analyses.py
+++ src/scripts/cervical_cancer_analyses/cervical_cancer_analyses.py
@@ -95,14 +95,20 @@ def run_sim(service_availability):
 
     sim.make_initial_population(n=popsize)
     sim.simulate(end_date=end_date)
-    df_hash_population_props = hash_dataframe(sim.population.props)
-
-    print(f"Hash: {df_hash_population_props}")
-
-    # Save hash to a file
-    with open('/Users/marianasuarez/Downloads/TLOmodelTest/df_hash_test.txt', 'w') as f:
-        f.write(df_hash_population_props)
-
+    # df_hash_population_props = hash_dataframe(sim.population.props)
+    #
+    # print(f"Hash: {df_hash_population_props}")
+    #
+    # # Save hash to a file
+    # with open('/Users/marianasuarez/Downloads/TLOmodelTest/df_hash_test.txt', 'w') as f:
+    #     f.write(df_hash_population_props)
+    # df_hash_population_props = hash_dataframe(sim.population.props)
+    #
+    # print(f"Hash: {df_hash_population_props}")
+    #
+    # # Save hash to a file
+    # with open('/Users/marianasuarez/Downloads/TLOmodelTest/df_hash_test.txt', 'w') as f:
+    #     f.write(df_hash_population_props)
     # parse the simulation logfile to get the output dataframes
     log_df = parse_log_file(sim.log_filepath)
     df_hash_population_props = hash_dataframe(sim.population.props)
