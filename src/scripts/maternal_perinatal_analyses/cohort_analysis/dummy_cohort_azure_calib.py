c8905bb163a808170883277a5a6b2268f03341e8
joehcollins <joseph.h.collins@ucl.ac.uk>
2024-10-31 11:12:49 +0000

    edits

--- src/scripts/maternal_perinatal_analyses/cohort_analysis/dummy_cohort_azure_calib.py
+++ src/scripts/maternal_perinatal_analyses/cohort_analysis/dummy_cohort_azure_calib.py
@@ -9,7 +9,7 @@ from tlo.analysis.utils import extract_results, get_scenario_outputs, summarize
 
 outputspath = './outputs/sejjj49@ucl.ac.uk/'
 
-scenario = 'block_intervention_test-2024-10-17T161423Z'
+scenario = 'block_intervention_test-2024-10-18T113429Z'
 
 results_folder= get_scenario_outputs(scenario, outputspath)[-1]
 
@@ -36,15 +36,27 @@ def get_data_frames(key, results_folder):
 
     return [results_df, results_df_summ]
 
+results = {k:get_data_frames(k, results_folder)[0] for k in
+           ['mat_comp_incidence', 'nb_comp_incidence', 'deaths_and_stillbirths','service_coverage',
+            'yearly_mnh_counter_dict']}
+
 results_sum = {k:get_data_frames(k, results_folder)[1] for k in
                ['mat_comp_incidence', 'nb_comp_incidence', 'deaths_and_stillbirths','service_coverage',
                 'yearly_mnh_counter_dict']}
 
-results = {k:get_data_frames(k, results_folder)[0] for k in
-               ['mat_comp_incidence', 'nb_comp_incidence', 'deaths_and_stillbirths','service_coverage',
-                'yearly_mnh_counter_dict']}
+def get_data(df, draw):
+    return (df.loc['direct_mmr', (draw, 'lower')],
+            df.loc['direct_mmr', (draw, 'mean')],
+            df.loc['direct_mmr', (draw, 'upper')])
 
-baseline = results['deaths_and_stillbirths'].loc['direct_mmr', (0, slice(0, 19))].droplevel(0)
+mmrs = {'baseline':get_data(results_sum['deaths_and_stillbirths'], 0),
+           'blood_transfusion_min':get_data(results_sum['deaths_and_stillbirths'], 1),
+           'blood_transfusion_max': get_data(results_sum['deaths_and_stillbirths'], 2),
+           'pph_treatment_uterotonics_min':get_data(results_sum['deaths_and_stillbirths'], 3),
+           'pph_treatment_uterotonics_max': get_data(results_sum['deaths_and_stillbirths'], 4),
+           'sepsis_treatment_min':get_data(results_sum['deaths_and_stillbirths'], 5),
+           'sepsis_treatment_max': get_data(results_sum['deaths_and_stillbirths'], 6),
+           }
 
 def get_mmr_diffs(df, draws):
     diff_results = {}
@@ -53,7 +65,7 @@ def get_mmr_diffs(df, draws):
     for draw in draws:
         # diff_df = ((results['deaths_and_stillbirths'][draw] - baseline)/baseline) * 100
         diff_df = results['deaths_and_stillbirths'][draw] - baseline
-        diff_df.columns = pd.MultiIndex.from_tuples([(0, v) for v in range(len(diff_df.columns))],
+        diff_df.columns = pd.MultiIndex.from_tuples([(draw, v) for v in range(len(diff_df.columns))],
                                                     names=['draw', 'run'])
         results_diff = summarize(diff_df)
         results_diff.fillna(0)
@@ -61,25 +73,19 @@ def get_mmr_diffs(df, draws):
 
     return diff_results
 
-diff_results = get_mmr_diffs(results, range(1,4))
+diff_results = get_mmr_diffs(results, range(1,7))
 
-def get_data(df, draw):
-    return (df.loc['direct_mmr', (draw, 'lower')],
-            df.loc['direct_mmr', (draw, 'mean')],
-            df.loc['direct_mmr', (draw, 'upper')])
 
-results = {'baseline':get_data(results['deaths_and_stillbirths'], 0),
-           'blood_transfusion':get_data(results['deaths_and_stillbirths'], 1),
-           'pph_treatment_uterotonics':get_data(results['deaths_and_stillbirths'], 2),
-           'sepsis_treatment':get_data(results['deaths_and_stillbirths'], 3)}
-
-results_diff = {'blood_transfusion':get_data(diff_results[1], 1),
-           'pph_treatment_uterotonics':get_data(diff_results[2], 2),
-           'sepsis_treatment':get_data(diff_results[3], 3)}
+results_diff = {'blood_transfusion_min':get_data(diff_results[1], 1),
+                'blood_transfusion_max':get_data(diff_results[2], 2),
+                'pph_treatment_uterotonics_min':get_data(diff_results[3], 3),
+                'pph_treatment_uterotonics_max': get_data(diff_results[4], 4),
+                'sepsis_treatment_mins':get_data(diff_results[5], 5),
+                'sepsis_treatment_max': get_data(diff_results[6], 6)}
 
 # todo: compare deaths with demography logging...
 
-results = data
+data = mmrs
 
 # Extract means and errors
 labels = data.keys()
@@ -91,43 +97,15 @@ errors = [lower_errors, upper_errors]
 # Create bar chart with error bars
 fig, ax = plt.subplots()
 ax.bar(labels, means, yerr=errors, capsize=5, alpha=0.7, ecolor='black')
-ax.set_ylabel('Values')
-ax.set_title('Bar Chart with Error Bars')
+ax.set_ylabel('MMR')
+ax.set_title('Average MMR under each scenario')
 
 # Adjust label size
-plt.xticks(rotation=45, fontsize=8)
+plt.xticks(fontsize=8)
 plt.tight_layout()
 plt.show()
 
-# scenario_filename = 'cohort_test-2024-10-09T130546Z'
-# # scenario_filename2 = 'cohort_test-2024-10-15T122825Z'
-# scenario_filename2 = 'cohort_test-2024-10-16T071357Z'
-#
-# results_folder_old = get_scenario_outputs(scenario_filename, outputspath)[-1]
-# results_folder_new = get_scenario_outputs(scenario_filename2, outputspath)[-1]
-#
-# def get_data_frames(key, results_folder):
-#     def sort_df(_df):
-#         _x = _df.drop(columns=['date'], inplace=False)
-#         return _x.iloc[0]
-#
-#     results_df = summarize (extract_results(
-#                 results_folder,
-#                 module="tlo.methods.pregnancy_supervisor",
-#                 key=key,
-#                 custom_generate_series=sort_df,
-#                 do_scaling=False
-#             ))
-#
-#     return results_df
-#
-# results_old = {k:get_data_frames(k, results_folder_old) for k in
-#                ['mat_comp_incidence', 'nb_comp_incidence', 'deaths_and_stillbirths','service_coverage',
-#                 'yearly_mnh_counter_dict']}
-#
-# results_new = {k:get_data_frames(k, results_folder_new) for k in
-#                ['mat_comp_incidence', 'nb_comp_incidence', 'deaths_and_stillbirths', 'service_coverage',
-#                 'yearly_mnh_counter_dict']}
+
 #
 # import matplotlib.pyplot as plt
 # import numpy as np
