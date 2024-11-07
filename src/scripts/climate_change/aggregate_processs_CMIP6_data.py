e0383fa77c0b5ba1064e105efec8944df317dc87
RachelMurray-Watson <rmurwat@gmail.com>
2024-11-07 14:40:17 +0000

    Changed how data were aggregated (do not need to search over model names, can just extract from file)

--- src/scripts/climate_change/aggregate_processs_CMIP6_data.py
+++ src/scripts/climate_change/aggregate_processs_CMIP6_data.py
@@ -2,9 +2,8 @@
 
 import glob
 import os
-import shutil
-import zipfile
 from pathlib import Path
+import re
 
 import pandas as pd
 from netCDF4 import Dataset
@@ -26,12 +25,11 @@ elif scenario == "ssp2_4_5":
                      "mri_esm2_0", "noresm2_mm", "canesm5", "cesm2", "cmcc_cm2_sr5", "cnrm_cm6_1", "cnrm_esm2_1",
                      "ec_earth3_cc", "ec_earth3_veg_lr", "fgoals_g3",
                      "gfdl_esm4", "inm_cm4_8", "kace_1_0_g", "mpi_esm1_2_lr", "nesm3", "noresm2_lm", "ukesm1_0_ll"]
-
-model = 0
+print(scenario)
 nc_file_directory = os.path.join(scenario_directory, 'nc_files')
 for file in glob.glob(os.path.join(nc_file_directory, "*.nc")):
+    model = re.search(r'pr_day_(.*?)_ssp245', file).group(1)
     data_per_model  = Dataset(file, mode='r')
-    print(data_per_model.variables)
     pr_data = data_per_model.variables['pr'][:]  # in kg m-2 s-1 = mm s-1 x 86400 to get to day
     lat_data = data_per_model.variables['lat'][:]
     long_data = data_per_model.variables['lon'][:]
@@ -43,8 +41,8 @@ for file in glob.glob(os.path.join(nc_file_directory, "*.nc")):
             precip_data_for_grid = precip_data_for_grid * 86400 # to get from per second to per day
             grid_dictionary[grid] = precip_data_for_grid
             grid += 1
-    data_by_model_and_grid[models[model]] = grid_dictionary
-    model += 1
+            print(grid_dictionary)
+    data_by_model_and_grid[model] = grid_dictionary
 print(data_by_model_and_grid)
 data_by_model_and_grid = pd.DataFrame.from_dict(data_by_model_and_grid)
-data_by_model_and_grid.to_csv(Path(scenario_directory)/"data_by_model_and_grid_CMIP6_projections.csv")
+#data_by_model_and_grid.to_csv(Path(scenario_directory)/"data_by_model_and_grid_CMIP6_projections.csv")
