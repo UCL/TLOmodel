6d3d6f416163b6669d320ac999fd918670b82c99
sakshimohan <sakshi.mohan@york.ac.uk>
2024-11-07 18:04:33 +0000

    update xticklabels to letters

--- src/scripts/costing/cost_analysis_htm_with_and_without_hss.py
+++ src/scripts/costing/cost_analysis_htm_with_and_without_hss.py
@@ -9,6 +9,7 @@ import os
 import textwrap
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 from matplotlib.ticker import FuncFormatter
 import numpy as np
 import pandas as pd
@@ -81,6 +82,13 @@ htm_scenarios = {0:"Baseline", 1: "HSS PACKAGE: Perfect", 2: "HSS PACKAGE: Reali
 7: "TB Programs Scale-up WITH FULL HSS PACKAGE", 8: "TB Programs Scale-up WITH REALISTIC HSS PACKAGE", 9: "Malaria Programs Scale-up WITHOUT HSS PACKAGE",
 10: "Malaria Programs Scale-up WITH FULL HSS PACKAGE", 11: "Malaria Programs Scale-up WITH REALISTIC HSS PACKAGE", 12: "HTM Programs Scale-up WITHOUT HSS PACKAGE",
 13: "HTM Programs Scale-up WITH FULL HSS PACKAGE", 14: "HTM Programs Scale-up WITH REALISTIC HSS PACKAGE", 15: "HTM Programs Scale-up WITH SUPPLY CHAINS", 16: "HTM Programs Scale-up WITH HRH"}
+
+htm_scenarios_substitutedict = {0:"0", 1: "1", 2: "A", 3: "B",
+4: "4", 5: "C", 6: "D",
+7: "7", 8: "E", 9: "F",
+10: "10", 11: "G", 12: "H",
+13: "13", 14: "I", 15: "J", 16: "K"}
+
 # Subset of scenarios included in analysis
 htm_scenarios_for_gf_report = [0, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 16]
 
@@ -160,6 +168,80 @@ def do_bar_plot_with_ci(_df, annotations=None, xticklabels_horizontal_and_wrappe
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     fig.tight_layout()
+
+    return fig, ax
+
+def do_standard_bar_plot_with_ci(_df, set_colors=None, annotations=None,
+                        xticklabels_horizontal_and_wrapped=False,
+                        put_labels_in_legend=True,
+                        offset=1e6):
+    """Make a vertical bar plot for each row of _df, using the columns to identify the height of the bar and the
+     extent of the error bar."""
+
+    substitute_labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    yerr = np.array([
+        (_df['mean'] - _df['lower']).values,
+        (_df['upper'] - _df['mean']).values,
+    ])
+# TODO should be above be 'median'
+    xticks = {(i + 0.5): k for i, k in enumerate(_df.index)}
+
+    if set_colors:
+        colors = [color_map.get(series, 'grey') for series in _df.index]
+    else:
+        cmap = sns.color_palette('Spectral', as_cmap=True)
+        rescale = lambda y: (y - np.min(y)) / (np.max(y) - np.min(y))  # noqa: E731
+        colors = list(map(cmap, rescale(np.array(list(xticks.keys()))))) if put_labels_in_legend else None
+
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.bar(
+        xticks.keys(),
+        _df['mean'].values,
+        yerr=yerr,
+        ecolor='black',
+        color=colors,
+        capsize=10,
+        label=xticks.values()
+    )
+
+    if annotations:
+        for xpos, (ypos, text) in zip(xticks.keys(), zip(_df['upper'].values.flatten(), annotations)):
+            annotation_y = ypos + offset
+
+            ax.text(
+                xpos,
+                annotation_y,
+                '\n'.join(text.split(' ', 1)),
+                horizontalalignment='center',
+                verticalalignment='bottom',  # Aligns text at the bottom of the annotation position
+                fontsize='x-small',
+                rotation='horizontal'
+            )
+
+    ax.set_xticks(list(xticks.keys()))
+
+    if put_labels_in_legend:
+        # Update xticks label with substitute labels
+        # Insert legend with updated labels that shows correspondence between substitute label and original label
+        xtick_values = [letter for letter, label in zip(substitute_labels, xticks.values())]
+        xtick_legend = [f'{letter}: {label}' for letter, label in zip(substitute_labels, xticks.values())]
+        h, legs = ax.get_legend_handles_labels()
+        ax.legend(h, xtick_legend, loc='center left', fontsize='small', bbox_to_anchor=(1, 0.5))
+        ax.set_xticklabels(list(xtick_values))
+    else:
+        if not xticklabels_horizontal_and_wrapped:
+            # xticklabels will be vertical and not wrapped
+            ax.set_xticklabels(list(xticks.values()), rotation=90)
+        else:
+            wrapped_labs = ["\n".join(textwrap.wrap(_lab, 20)) for _lab in xticks.values()]
+            ax.set_xticklabels(wrapped_labs)
+
+    ax.grid(axis="y")
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    fig.tight_layout(pad=2.0)
+    plt.subplots_adjust(left=0.15, right=0.85)  # Adjust left and right margins
 
     return fig, ax
 
@@ -477,7 +559,7 @@ plt.close(fig)
 # Plot incremental costs
 incremental_scenario_cost_summarized = summarize_cost_data(incremental_scenario_cost)
 name_of_plot = f'Incremental scenario cost relative to baseline {relevant_period_for_costing[0]}-{relevant_period_for_costing[1]}'
-fig, ax = do_bar_plot_with_ci(
+fig, ax = do_standard_bar_plot_with_ci(
     (incremental_scenario_cost_summarized / 1e6),
     annotations=[
         f"{round(row['mean'] / 1e6, 1)} \n ({round(row['lower'] / 1e6, 1)}- \n {round(row['upper'] / 1e6, 1)})"
@@ -507,7 +589,7 @@ input_costs_for_plot_summarized = input_costs_for_plot_summarized.melt(
     value_name='cost'
 )
 
-do_stacked_bar_plot_of_cost_by_category(_df = input_costs_for_plot_summarized, _cost_category = 'all', _year = list(range(2025, 2036)), _disaggregate_by_subgroup = False, _outputfilepath = figurespath, _scenario_dict = htm_scenarios)
+do_stacked_bar_plot_of_cost_by_category(_df = input_costs_for_plot_summarized, _cost_category = 'all', _year = list(range(2025, 2036)), _disaggregate_by_subgroup = False, _outputfilepath = figurespath, _scenario_dict = htm_scenarios_substitutedict)
 do_stacked_bar_plot_of_cost_by_category(_df = input_costs_for_plot_summarized, _cost_category = 'all', _year = [2025],  _disaggregate_by_subgroup = False, _outputfilepath = figurespath, _scenario_dict = htm_scenarios)
 do_stacked_bar_plot_of_cost_by_category(_df = input_costs_for_plot_summarized, _cost_category = 'human resources for health',  _disaggregate_by_subgroup = False, _outputfilepath = figurespath, _scenario_dict = htm_scenarios)
 do_stacked_bar_plot_of_cost_by_category(_df = input_costs_for_plot_summarized, _cost_category = 'medical consumables',  _disaggregate_by_subgroup = False, _outputfilepath = figurespath, _scenario_dict = htm_scenarios)
