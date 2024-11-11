ade941b9f9dbb6f967dc659407a2de39959e8e98
mmsuarezcosta <mmsuarezcosta@gmail.com>
2024-11-11 15:24:23 +0200

    update cancer consumables

--- src/tlo/methods/cancer_consumables.py
+++ src/tlo/methods/cancer_consumables.py
@@ -78,29 +78,22 @@ def get_consumable_item_codes_cancers(self) -> Dict[str, int]:
     elif 'CervicalCancer' == self.name:
         cons_dict['cervical_cancer_screening_via'] = \
             {get_item_code("Acetic acid, 5% dilute, 5 ml"): 1,
-             # get_item_code("Speculum"): 1,
              get_item_code("Disposables gloves, powder free, 100 pieces per box"): 1,
              get_item_code("Gauze, swabs 8-ply 10cm x 10cm_100_CMST"): 1}
 
         cons_dict['cervical_cancer_screening_xpert'] = \
         {get_item_code("Specimen container"): 1,
-        #     get_item_code("Xpert HPV test cartridge"): 1,
-        #      get_item_code("PCR machine"): 1,
-        #      get_item_code("Speculum"): 1,
+            get_item_code("Xpert"): 1,
              get_item_code("Disposables gloves, powder free, 100 pieces per box"): 1,
              get_item_code("Gauze, swabs 8-ply 10cm x 10cm_100_CMST"): 1}
 
         cons_dict['cervical_cancer_thermoablation'] = {
-            # {get_item_code("Thermoablation Device"): 1,
-            #  get_item_code("Thermoablation Probes"): 1,
-            # get_item_code("Speculum"): 1,
              get_item_code("Disposables gloves, powder free, 100 pieces per box"): 1,
              get_item_code("Gauze, swabs 8-ply 10cm x 10cm_100_CMST"): 1}
 
         cons_dict['cervical_cancer_cryotherapy'] = \
             {get_item_code("Cryotherapy unit with cryotips, use for one patient"): 1,
              get_item_code("Compressed gas, 25 kg cylinder"): 1,
-            # get_item_code("Speculum"): 1,
              get_item_code("Disposables gloves, powder free, 100 pieces per box"): 1,
              get_item_code("Gauze, swabs 8-ply 10cm x 10cm_100_CMST"): 1}
 
