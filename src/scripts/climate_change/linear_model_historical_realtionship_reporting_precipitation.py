37d0f92a3fb3f81433d79a100276446fbeb90398
RachelMurray-Watson <rmurwat@gmail.com>
2024-11-13 16:48:08 +0000

    exclude some outliers

--- src/scripts/climate_change/linear_model_historical_realtionship_reporting_precipitation.py
+++ src/scripts/climate_change/linear_model_historical_realtionship_reporting_precipitation.py
@@ -12,8 +12,8 @@ daily_total = False
 min_year_for_analyis = 2011
 absolute_min_year = 2011
 mask_threshold = 0
-five_day = True
-cumulative = True
+five_day = False
+cumulative = False
 model_fit_ANC_data = True
 model_fit_weather_data = True
 model_filename = (
@@ -72,13 +72,14 @@ else:
 
 def build_model(X, y, scale_y=False, beta=False, log_y=False, X_mask_mm=0):
     epsilon = 1e-5
+
     if log_y:
         y = np.log(np.clip(y, epsilon, None))  # Log-transform y with clipping for positivity
     if scale_y:
         y_scaled = np.clip(y / 100, epsilon, 1 - epsilon)
     else:
         y_scaled = y
-    mask = ~np.isnan(X).any(axis=1) & ~np.isnan(y_scaled) & (X[:, 0] >= X_mask_mm)
+    mask = (~np.isnan(X).any(axis=1) & ~np.isnan(y_scaled) & (X[:, 0] >= X_mask_mm) & (y <= 1e5))
     model = BetaModel(y_scaled[mask], X[mask]) if beta else sm.OLS(y_scaled[mask], X[mask])
     model_fit = model.fit()
     return model_fit, model_fit.predict(X[mask]), mask
@@ -99,47 +100,53 @@ def create_binary_feature(threshold, weather_data_df, recent_months):
 
     return binary_feature_list
 
-def stepwise_selection(X, y):
-    included = list(range(X.shape[1]))
+def stepwise_selection(X, y, log_y, p_value_threshold=0.05):
+    included = []
     current_aic = np.inf
 
     while True:
         changed = False
+
+        # Step 1: Try adding each excluded predictor and select the best one by AIC if significant
         excluded = list(set(range(X.shape[1])) - set(included))
         new_aic = pd.Series(index=excluded, dtype=float)
         for new_column in excluded:
             subset_X = X[:, included + [new_column]]
             results, _, _ = build_model(subset_X, y, log_y=log_y, X_mask_mm=mask_threshold)
-            new_aic[new_column] = results.aic
-        best_aic = new_aic.min()
-        if best_aic < current_aic:
+            if results.pvalues[-1] < p_value_threshold:
+                new_aic[new_column] = results.aic
+
+        # Add the predictor with the best AIC if it's better than the current model's AIC
+        if not new_aic.empty and new_aic.min() < current_aic:
             best_feature = new_aic.idxmin()
             included.append(best_feature)
-            current_aic = best_aic
+            current_aic = new_aic.min()
             changed = True
-        results, _, _ = build_model(X[:, included], y, log_y=log_y, X_mask_mm=mask_threshold)
-        pvalues = results.pvalues
-        pvalues = pd.Series(pvalues)
-        worst_pval = pvalues.max()
-        if worst_pval > 0.05:
-            worst_feature = pvalues.idxmax()
-            if worst_feature in included:
-                included.remove(worst_feature)
-                changed = True
 
+        # Step 2: Check significance of each included predictor and remove non-significant ones
+        results, _, _ = build_model(X[:, included], y, log_y=log_y, X_mask_mm=mask_threshold)
+        pvalues = pd.Series(results.pvalues, index=included)
+        for feature in pvalues[pvalues > p_value_threshold].index:
+            included.remove(feature)
+            changed = True
+
+        # Exit if no changes were made in this iteration
         if not changed:
             break
 
     return included
-###### Tidy data ############
-## Drop September 2024 -
+
+##############################################################################################
+########################## STEP 0: Tidy data ##########################
+##############################################################################################
+# Drop September 2024 -
 weather_data_historical = weather_data_historical.drop(weather_data_historical.index[-1])
 monthly_reporting_by_facility = monthly_reporting_by_facility.drop(monthly_reporting_by_facility.index[-1])
 
-## code if years need to be dropped
+# code if years need to be dropped
 weather_data_historical = weather_data_historical.iloc[(min_year_for_analyis-absolute_min_year)*12 :]
 monthly_reporting_by_facility = monthly_reporting_by_facility.iloc[(min_year_for_analyis-absolute_min_year)*12:]
-## Linear regression
+# Linear regression
 month_range = range(12)
 num_facilities = len(weather_data_historical.columns)
 year_range = range(min_year_for_analyis, 2025, 1) # year as a fixed effect
@@ -197,12 +204,15 @@ ftype_encoded = pd.get_dummies(ftype_info_each_month, drop_first=True)
 altitude = [float(x) for x in repeat_info(expanded_facility_info['A109__Altitude'], num_facilities, year_range)]
 # Lagged weather
 lag_1_month = weather_data_historical.shift(1).values.flatten()
+lag_2_month = weather_data_historical.shift(1).values.flatten()
 lag_3_month = weather_data_historical.shift(3).values.flatten()
+lag_4_month = weather_data_historical.shift(4).values.flatten()
 
 altitude = np.array(altitude)
 altitude = np.where(altitude < 0, np.nan, altitude)
 altitude = list(altitude)
 
+#print(np.nanmax(y) ) ## one y value is > 167,000. So should remove
 
 ##############################################################################################
 ########################## STEP 1: GENERATE PREDICTIONS OF ANC DATA ##########################
@@ -220,7 +230,7 @@ X = np.column_stack([
 ])
 
 if model_fit_ANC_data:
-    best_predictors = stepwise_selection(X, y)
+    best_predictors = stepwise_selection(X, y, log_y=log_y)
     X_best = X[:, best_predictors]
     results, y_pred, mask_ANC_data = build_model(X_best, y, log_y=log_y, X_mask_mm=mask_threshold)
     model_data = {
@@ -234,10 +244,8 @@ else:
     results = model_data['model']
     mask_ANC_data = model_data['mask']
     best_predictors = model_data['best_predictors']
-
-X = X[:, best_predictors]
-
-y_pred = results.predict(X[mask_ANC_data])
+    X = X[:, best_predictors]
+    y_pred = results.predict(X[mask_ANC_data])
 
 if log_y:
     residuals_percentage = (y[mask_ANC_data] - np.exp(y_pred))/y[mask_ANC_data] * 100
@@ -264,6 +272,7 @@ else:
             'y_filtered': y_filtered,
             'y_pred': np.exp(y_pred),
         })
+
 data_ANC_predictions = data_ANC_predictions.sort_values(by='Year_Month').reset_index(drop=True)
 fig, axs = plt.subplots(1, 2, figsize=(14, 6))
 
@@ -299,14 +308,16 @@ X = np.column_stack([
     owner_encoded[mask_ANC_data],
     ftype_encoded[mask_ANC_data],
     lag_1_month[mask_ANC_data],
+    lag_2_month[mask_ANC_data],
     lag_3_month[mask_ANC_data],
+    lag_4_month[mask_ANC_data],
     facility_encoded[mask_ANC_data],
     np.array(altitude)[mask_ANC_data]
 ])
 
 
 if model_fit_weather_data:
-    best_predictors = stepwise_selection(X, residuals_percentage)
+    best_predictors = stepwise_selection(X, residuals_percentage, log_y=log_y)
     X_best = X[:, best_predictors]
     results_of_weather_model, y_pred, mask_all_data = build_model(X_best, residuals_percentage, log_y=True, X_mask_mm=mask_threshold)
     model_data = {
@@ -319,6 +330,10 @@ else:
     model_data = joblib.load(model_filename_weather_model)
     results_of_weather_model = model_data['model']
     mask_all_data = model_data['mask']
+print(results_of_weather_model.summary())
+
+results_of_weather_model, y_pred, mask_all_data = build_model(X, residuals_percentage, log_y=True,
+                                                              X_mask_mm=mask_threshold)
 print(results_of_weather_model.summary())
 
 ##### Plot y_predic
