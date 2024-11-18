4e343757f9f83674cf624dad6f0dc30e6d6858c5
tdm32 <t.mangal@imperial.ac.uk>
2024-11-18 17:37:40 +0000

    set up scenario runs

--- src/scripts/hiv/projections_jan2023/analysis_logged_deviance.py
+++ src/scripts/hiv/projections_jan2023/analysis_logged_deviance.py
@@ -34,8 +34,8 @@ resourcefilepath = Path("./resources")
 
 # %% Run the simulation
 start_date = Date(2010, 1, 1)
-end_date = Date(2028, 1, 1)
-popsize = 10_000
+end_date = Date(2015, 1, 1)
+popsize = 40_000
 
 # scenario = 1
 
@@ -87,8 +87,8 @@ sim.register(
 )
 
 # set the scenario
-sim.modules["Hiv"].parameters["do_scaleup"] = False
-sim.modules["Hiv"].parameters["scaleup_start_year"] = 2019
+sim.modules["Hiv"].parameters["select_mihpsa_scenario"] = 1
+sim.modules["Hiv"].parameters["scaleup_start_year"] = 2011
 # sim.modules["Tb"].parameters["scenario"] = scenario
 # sim.modules["Tb"].parameters["scenario_start_date"] = Date(2010, 1, 1)
 # sim.modules["Tb"].parameters["scenario_SI"] = "z"
