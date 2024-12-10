ce630df23662fec689376ed8000f528c4dbab15c
nicx503 <nic503@york.ac.uk>
2024-12-10 10:12:10 +0000

    Revised scenario for assessing impact

--- src/scripts/hiv/projections_jan2023/tb_DAH_scenariosv2.py
+++ src/scripts/hiv/projections_jan2023/tb_DAH_scenariosv2.py
@@ -3,26 +3,27 @@ This file run scenarios for assesing unavailability of TB-related Development As
 
 
 It can be submitted on Azure Batch by running:
-tlo batch-submit src//scripts/hiv/projections_jan2023/tb_DAH_scenarios.py
+tlo batch-submit src//scripts/hiv/projections_jan2023/tb_DAH_scenariosv2.py
 
 or locally using:
 
- tlo scenario-run src/scripts/hiv/projections_jan2023/tb_DAH_scenarios.py
+ tlo scenario-run src/scripts/hiv/projections_jan2023/tb_DAH_scenariosv2.py
   execute a single run:
 
- tlo scenario-run src/scripts/hiv/projections_jan2023/tb_DAH_scenarios.py --draw 1 0
+ tlo scenario-run src/scripts/hiv/projections_jan2023/tb_DAH_scenariosv2.py --draw 1 0
 
  check the batch configuration gets generated without error:
-tlo scenario-run --draw-only src/scripts/hiv/projections_jan2023/tb_DAH_scenarios.py
+tlo scenario-run --draw-only src/scripts/hiv/projections_jan2023/tb_DAH_scenariosv2.py
 
  """
+
 import warnings
 from pathlib import Path
 from typing import Dict
 import random
 from tlo import Date, logging
 from tlo.scenario import BaseScenario
-from tlo.methods import(
+from tlo.methods import (
     demography,
     tb,
     symptommanager,
@@ -40,20 +41,22 @@ from tlo.methods import(
 # we will do so here for the purposes of this example to keep things simple.
 warnings.simplefilter("ignore", (UserWarning, RuntimeWarning))
 
-class ImpactOfTbDaH03(BaseScenario):
+
+class ImpactOfTbDaH04(BaseScenario):
     def __init__(self):
         super().__init__()
         self.seed = 2134
-       # self.seed = random.randint(0, 50000),
+        # self.seed = random.randint(0, 50000),
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2015, 12, 31)
         self.pop_size = 5000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
-        self.runs_per_draw = 2  #return to 5 runs in azure submission
+        self.runs_per_draw = 5
+
     def log_configuration(self):
         return {
-            'filename': 'tb_DAH_scenarios',
+            'filename': 'Tb_DAH_impact04',
             'directory': Path('./outputs/newton.chagoma@york.ac.uk'),
             'custom_levels': {
                 '*': logging.WARNING,
@@ -66,6 +69,7 @@ class ImpactOfTbDaH03(BaseScenario):
                 'tlo.methods.enhanced_lifestyle': logging.INFO
             }
         }
+
     def modules(self):
         return [
             demography.Demography(resourcefilepath=self.resources),
@@ -79,11 +83,21 @@ class ImpactOfTbDaH03(BaseScenario):
             enhanced_lifestyle.Lifestyle(resourcefilepath=self.resources),
             healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=self.resources),
         ]
+
     def draw_parameters(self, draw_number, rng):
+        # Adding health system constraints to every draw
+        health_system_constraints = {
+            'HealthSystem': {
+                'use_funded_or_actual_staffing': 'actual',
+                'cons_availability': 'default',
+            }
+        }
+
         if draw_number < self.number_of_draws:
-            return list(self._scenarios.values())[draw_number]
+            return list(self._scenarios.values())[draw_number] | health_system_constraints
         else:
             return
+
     def _get_scenarios(self) -> Dict[str, Dict]:
         """Return the Dict with values for the parameters that are changed, keyed by a name for the scenario."""
         return {
@@ -108,22 +122,24 @@ class ImpactOfTbDaH03(BaseScenario):
                     'probability_community_chest_xray': 0.0,
                 },
             },
-            #increase CXR by 90 percentage points
-             "CXR scaleup": {
-                 'Tb': {
-                     'scenario': 3,
-                     'scaling_factor_WHO': 1.4,
-                 },
-             },
-            #introduce outreach services
+            # increase CXR by 90 percentage points
+            "CXR scaleup": {
+                'Tb': {
+                    'scenario': 3,
+                    'probability_community_chest_xray': 0.1,
+                    'scaling_factor_WHO': 1.4,
+                },
+            },
+            # introduce outreach services
             "Outreach services": {
                 'Tb': {
                     'scenario': 4,
-                    'probability_community_chest_xray': 0.1,
-                    'scaling_factor_WHO': 1.44
+                    'scaling_factor_WHO': 1.44,
                 }
             },
         }
+
+
 if __name__ == '__main__':
     from tlo.cli import scenario_run
     scenario_run([__file__])
