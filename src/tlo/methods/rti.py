3ce6e9b416d9354f8351c15f5e1b751d4957ff3b
tdm32 <t.mangal@imperial.ac.uk>
2025-01-03 15:23:32 +0000

    add fix to rti to avoid error P133a not recognised

--- src/tlo/methods/rti.py
+++ src/tlo/methods/rti.py
@@ -2017,9 +2017,14 @@ class RTI(Module, GenericFirstAppointmentsMixin):
                (sum(df.loc[injured_index, 'rt_imm_death']) == 0)
         selected_for_rti_inj = df.loc[injured_index, RTI.INJURY_COLUMNS]
 
+        # todo remove
+        # daly_change = selected_for_rti_inj.applymap(
+        #     lambda code: self.ASSIGN_INJURIES_AND_DALY_CHANGES[code][1]
+        # ).sum(axis=1, numeric_only=True)
         daly_change = selected_for_rti_inj.applymap(
-            lambda code: self.ASSIGN_INJURIES_AND_DALY_CHANGES[code][1]
+            lambda code: self.ASSIGN_INJURIES_AND_DALY_CHANGES.get(code, [None, 0])[1]
         ).sum(axis=1, numeric_only=True)
+
         df.loc[injured_index, 'rt_disability'] += daly_change
 
         # Store the true sum of DALY weights in the df
