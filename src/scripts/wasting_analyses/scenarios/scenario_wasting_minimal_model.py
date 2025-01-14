ddad6dd051992699a0e3ea4ddfe01c7a48c42afe
Eva Janouskova <janouskova.eva.012@gmail.com>
2025-01-14 10:32:39 +0000

    scenario_wast_min_model: 1 year sim, 2K pop

--- src/scripts/wasting_analyses/scenarios/scenario_wasting_minimal_model.py
+++ src/scripts/wasting_analyses/scenarios/scenario_wasting_minimal_model.py
@@ -41,8 +41,8 @@ class WastingAnalysis(BaseScenario):
         super().__init__(
             seed=0,
             start_date=Date(year=2010, month=1, day=1),
-            end_date=Date(year=2031, month=1, day=1),
-            initial_population_size=30_000,
+            end_date=Date(year=2011, month=1, day=1),
+            initial_population_size=2_000,
             number_of_draws=1,
             runs_per_draw=1,
         )
