584ed45dea27ee80895729945ef067c6fac8c5bc
Joel Kumwenda <jkumwenda@gmail.com>
2025-01-15 13:37:49 +0300

    import Path in chronicsyndrome.py

--- src/tlo/methods/chronicsyndrome.py
+++ src/tlo/methods/chronicsyndrome.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, List, Optional
 
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
 from tlo import DAYS_IN_YEAR, DateOffset, Module, Parameter, Property, Types, logging
 from tlo.events import Event, IndividualScopeEventMixin, PopulationScopeEventMixin, RegularEvent
