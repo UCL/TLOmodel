f9e87d4e91d7be70ce39e838716eb87077b86eee
deploy.sh <tamuri@gmail.com>
2025-01-20 12:28:41 +0000

    publish: Add note about behaviour of asserts in batch jobs (#1573)
    generated from commit d500d75293d7fff5d796e5d2534d3768d3d038b4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev2508+gcb0f26f.d20250115',
+    VERSION: '0.1.dev2513+gd500d75',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
