2fcf1ec31ee8acc53af8c116ea177e7033b0215d
tdm32 <t.mangal@imperial.ac.uk>
2025-01-22 14:25:43 +0000

    update outputs and write to excel

--- src/scripts/hiv/mihpsa_runs/extract_batch_outputs.py
+++ src/scripts/hiv/mihpsa_runs/extract_batch_outputs.py
@@ -30,9 +30,6 @@ outputspath = Path("./outputs/t.mangal@imperial.ac.uk")
 # 0) Find results_folder associated with a given batch_file (and get most recent [-1])
 results_folder = get_scenario_outputs("mihpsa_runs.py", outputspath)[-1]
 
-# Declare path for output graphs from this script
-make_graph_file_name = lambda stub: results_folder / f"{stub}.png"  # noqa: E731
-
 # look at one log (so can decide what to extract)
 log = load_pickled_dataframes(results_folder)
 
@@ -45,119 +42,6 @@ params = extract_params(results_folder)
 scaling_factor = log['tlo.methods.population']['scaling_factor'].scaling_factor.values[0]
 
 # -----------------------------------------------------------------
-# # export one run
-# log0 = log['tlo.methods.hiv']['stock_variables']
-# # Select columns to be multiplied (excluding the first column 'First')
-# columns_to_multiply = log0.columns[1:]
-#
-# # Multiply selected columns by scaling factor
-# log0[columns_to_multiply] = (log0[columns_to_multiply] * scaling_factor).astype(int)
-# # log0.to_csv(outputspath / 'mihpsa_stock.csv')
-#
-#
-# log0F = log['tlo.methods.hiv']['flow_variables']
-# columns_to_multiply = log0F.columns[1:]
-# log0F[columns_to_multiply] = (log0F[columns_to_multiply] * scaling_factor).astype(int)
-#
-# # log0F.to_csv(outputspath / 'mihpsa_flow.csv')
-#
-#
-# # get test outputs
-#
-# # N_HIVTest_Facility_NEG_15_UP
-# # N_HIVTest_Facility_POS_15_UP
-# # N_HIVTest_Index_NEG_15_UP
-# # N_HIVTest_Index_POS_15_UP
-# # N_HIVTest_Community_NEG_15_UP
-# # N_HIVTest_Community_POS_15_UP
-# # N_HIVTest_SelfTest_POS_15_UP
-# # N_HIVTest_SelfTest_Dist
-#
-# # number tests age >=15, hiv_status=false
-# # Convert 'datetime' column to datetime type
-# log_tests = log['tlo.methods.hiv']['hiv_test']
-# log_tests['date'] = pd.to_datetime(log_tests['date'])
-#
-# # Filter for age 15 years and up
-# log_tests_filtered = log_tests[log_tests['adult'] == True]
-#
-# # Extract year from the datetime column
-# log_tests_filtered['year'] = log_tests_filtered['date'].dt.year
-#
-# # Group by 'hiv_status' and 'year', then count the number of entries
-# result = log_tests_filtered.groupby(['hiv_status', 'year']).size().reset_index()
-#
-# # scale to full population
-# result[0] = (result[0] * scaling_factor).astype(int)
-#
-# # result.to_csv(outputspath / 'mihpsa_tests.csv')
-#
-#
-# # get deaths
-#
-# # N_DeathsHIV_00_14_C
-# # N_DeathsHIV_15_UP_M
-# # N_DeathsHIV_15_UP_F
-# #
-# # N_DeathsAll_00_14_C
-# # N_DeathsAll_15_UP_M
-# # N_DeathsAll_15_UP_F
-#
-# deaths = log['tlo.methods.demography']['death']
-# deaths['date'] = pd.to_datetime(deaths['date'])
-# deaths['year'] = deaths['date'].dt.year
-#
-# # create new column adult=true/false
-# deaths['adult'] = deaths['age'] > 14
-# deaths['adult'] = deaths['adult'].astype(bool)
-#
-# deaths_hiv = deaths[deaths['label'] == 'AIDS']
-#
-# ### HIV DEATHS
-# # get HIV deaths, child
-# result_hiv = deaths_hiv.groupby(['adult', 'year']).size().reset_index()
-# result_hiv = deaths_hiv[deaths_hiv['adult'] == False].groupby('year').size().reset_index(name='count')
-# result_hiv["count"] = (result_hiv["count"] * scaling_factor).astype(int)
-# # result_hiv.to_csv(outputspath / 'mihpsa_hiv_deaths_child.csv')
-#
-#
-# # get HIV deaths, adult
-# # result_hiv_adult = deaths_hiv[deaths_hiv['adult'] == True]
-# result_hiv_adult = deaths_hiv[deaths_hiv['adult'] == True].groupby(['year', 'sex']).size().reset_index()
-# result_hiv_adult[0] = (result_hiv_adult[0] * scaling_factor).astype(int)
-# # result_hiv_adult.to_csv(outputspath / 'mihpsa_hiv_deaths_adult.csv')
-#
-#
-# ### ALL DEATHS
-# # get all deaths, child
-# result_all = deaths.groupby(['adult', 'year']).size().reset_index()
-# result_all[0] = (result_all[0] * scaling_factor).astype(int)
-# # result_all.to_csv(outputspath / 'mihpsa_all_deaths_child.csv')
-#
-#
-# # get all deaths, adult
-# result_all_adult = deaths[deaths['adult'] == True]
-# result_all_adult = deaths.groupby(['year', 'sex']).size().reset_index()
-# result_all_adult[0] = (result_all_adult[0] * scaling_factor).astype(int)
-# # result_all_adult.to_csv(outputspath / 'mihpsa_all_deaths_adult.csv')
-#
-#
-# # get deaths by single years of age, male and female
-# # male deaths
-# full_output = deaths_hiv.groupby(['age', 'year', 'sex']).size().reset_index()
-#
-# # convert to wide format
-# # Filter rows by sex=M first, then sex=F
-# full_output_sorted = full_output.sort_values(by='sex', ascending=False)
-#
-# # Pivot the DataFrame to wide format
-# pivot_df = full_output_sorted.pivot_table(index='age', columns='year', values=0, aggfunc='first')
-#
-# # Display the pivot DataFrame
-# print(pivot_df)
-
-# -----------------------------------------------------------------
-
 
 stock_variables = [
     "N_PLHIV_00_14_C",
@@ -255,6 +139,17 @@ for stock in stock_variables:
             stocks_output[draw] = pd.DataFrame()  # Initialise DataFrame for the draw if not exists
 
         stocks_output[draw][stock] = result[draw]
+
+with pd.ExcelWriter(results_folder/"stock_outputs_workbook.xlsx", engine='openpyxl') as writer:
+    # Iterate over the dictionary and write each DataFrame to a new sheet
+    for draw, df in stocks_output.items():
+        print(df)
+        df = df.T  # Switch rows and columns
+        print(df)
+
+        # Writing each draw's DataFrame to a new sheet named after the draw
+        df.to_excel(writer, sheet_name=f'Draw_{draw}', index=True)
+
 
 flows_output = {}
 
