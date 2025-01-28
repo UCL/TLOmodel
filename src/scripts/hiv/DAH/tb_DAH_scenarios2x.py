21d7367a97265b42f386bef970596f341f8493ac
nicx503 <nic503@york.ac.uk>
2025-01-28 12:16:38 +0000

    Added a component of xray screening so its targeted in people with Tb symptoms other than general pop. Also added loggers to print treatment IDs

--- src/scripts/hiv/DAH/tb_DAH_scenarios2x.py
+++ src/scripts/hiv/DAH/tb_DAH_scenarios2x.py
@@ -45,8 +45,8 @@ class ImpactOfTbDaH04(BaseScenario):
         self.seed = 2134
         #self.seed = random.randint(0, 50000),
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2015, 12, 31)
-        self.pop_size = 1000
+        self.end_date = Date(2011, 12, 31)
+        self.pop_size = 600
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 2
