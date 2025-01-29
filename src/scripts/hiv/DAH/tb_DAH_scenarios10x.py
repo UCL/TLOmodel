bf1c8c7c77cc0bfb5be5058dc8ae5434329e13d9
nicx503 <nic503@york.ac.uk>
2025-01-29 15:42:03 +0000

    added return statements and footprints and as treatment_id for Xrays, culture, clinical diagnosis not printed

--- src/scripts/hiv/DAH/tb_DAH_scenarios10x.py
+++ src/scripts/hiv/DAH/tb_DAH_scenarios10x.py
@@ -41,7 +41,7 @@ class ImpactOfTbDaH04(BaseScenario):
         #self.seed = random.randint(0, 50000),
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2012, 12, 31)
-        self.pop_size = 100
+        self.pop_size = 1000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 2
