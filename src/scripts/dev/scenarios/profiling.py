bad4a07bc64fda03524ba612a982a3683d5fcf2b
deploy.sh <tamuri@gmail.com>
2025-02-03 11:26:05 +0000

    A scenario suitable for profiling, with a module to hold profiling information.

---
+++ src/scripts/dev/scenarios/profiling.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+from tlo import Date, logging
+from tlo.analysis import performance
+from tlo.analysis.performance import PerformanceMonitor
+from tlo.methods import (
+    demography,
+    enhanced_lifestyle,
+    healthseekingbehaviour,
+    healthsystem,
+    symptommanager,
+)
+from tlo.methods import fullmodel
+from tlo.scenario import BaseScenario
+
+class Profiling(BaseScenario):
+    """getting scale_run setup as a scenario"""
+    def __init__(self):
+        super().__init__()
+        self.seed = 655123742
+        self.start_date = Date(2010, 1, 1)
+        self.end_date = Date(2030, 1, 1)
+        self.pop_size = 50000
+        self.number_of_draws = 1
+        self.runs_per_draw = 1
+
+    def log_configuration(self):
+        return {
+            "directory": Path("."),
+            "custom_levels": {"*": getattr(logging, "WARNING"), "tlo.profiling": logging.INFO},
+            "suppress_stdout": False
+        }
+
+    def modules(self):
+        fm = fullmodel.fullmodel(
+            resourcefilepath=Path("./resources"),
+            use_simplified_births=False,
+            module_kwargs={
+                "HealthSystem": {
+                    "disable": False,
+                    "mode_appt_constraints": 1,
+                    "capabilities_coefficient": None,
+                    "hsi_event_count_log_period": None
+                },
+                "SymptomManager": {"spurious_symptoms": True},
+            }
+        )
+        fm.append(PerformanceMonitor())
+        return fm
+
+if __name__ == '__main__':
+    from tlo.cli import scenario_run
+    scenario_run([__file__])
---
+++ src/tlo/analysis/performance.py
@@ -0,0 +1,84 @@
+import datetime
+from pathlib import Path
+
+import time
+
+from tlo import Module, Simulation, DateOffset, logging, Date
+from tlo.events import RegularEvent, PopulationScopeEventMixin
+from tlo.methods.healthsystem import HealthSystem
+
+try:
+    import psutil
+except ImportError:
+    psutil = None
+
+logger = logging.getLogger(__name__)
+
+class PerformanceMonitor(Module):
+
+    def read_parameters(self, data_folder: str | Path) -> None:
+        pass
+
+    def initialise_simulation(self, sim: Simulation) -> None:
+        sim.schedule_event(LogProgress(), sim.start_date)
+
+    def on_birth(self, mother_id: int, child_id: int) -> None:
+        pass
+
+    def on_simulation_end(self) -> None:
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        self.sim.save_to_pickle(Path(f"simulation-{timestamp}.pkl"))
+
+class LogProgress(RegularEvent, PopulationScopeEventMixin):
+    def __init__(self, module, frequency_months=3):
+        super().__init__(module, frequency=DateOffset(months=frequency_months))
+        self.time = time.time()
+
+    def apply(self, population):
+        df = population.props
+        now = time.time()
+        duration = (now - self.time) / 60  # minutes
+        self.time = now
+        sim: Simulation = population.sim
+        sim_queue_size = len(sim.event_queue)
+        health_system: "HealthSystem" = sim.modules["HealtSystem"]
+        hsi_queue_size = len(health_system.HSI_EVENT_QUEUE)
+
+        logger.info(
+            key="stats",
+            data={
+                "time": datetime.datetime.now().isoformat(),
+                "duration_minutes": duration,
+                "pop_df_number_alive": df.is_alive.sum(),
+                "pop_df_rows": len(df),
+                "pop_df_mem_MiB": df.memory_usage(index=True, deep=True).sum() / 2**20,
+                "sim_queue_size": sim_queue_size,
+                "hsi_queue_size": hsi_queue_size,
+                **memory_statistics(),
+            },
+        )
+
+def memory_statistics() -> dict[str, float]:
+    """
+    Extract memory usage statistics in current process using `psutil` if available.
+    Statistics are returned as a dictionary. If `psutil` not installed an empty dict is returned.
+
+    Key / value pairs are:
+    memory_rss_MiB: float
+        Resident set size in mebibytes. The non-swapped physical memory the process has used.
+    memory_vms_MiB: float
+        Virtual memory size in mebibytes. The total amount of virtual memory used by the process.
+    memory_uss_MiB: float
+        Unique set size in mebibytes. The memory which is unique to a process and which would be freed if the process
+        was terminated right now
+    """
+    if psutil is None:
+        return {}
+    process = psutil.Process()
+    memory_info = process.memory_full_info()
+    return {
+        "memory_rss_MiB": memory_info.rss / 2**20,
+        "memory_vms_MiB": memory_info.vms / 2**20,
+        "memory_uss_MiB": memory_info.uss / 2**20,
+    }
+
