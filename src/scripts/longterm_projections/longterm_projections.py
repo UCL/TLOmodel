498b7d6388ac7b41ac9888824e7b14579f1f903a
Asif Tamuri <tamuri@gmail.com>
2025-02-10 20:46:03 +0000

    only one draw/run needed

--- src/scripts/longterm_projections/longterm_projections.py
+++ src/scripts/longterm_projections/longterm_projections.py
@@ -15,10 +15,10 @@ class LongRun(BaseScenario):
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2020, 1, 12)
         self.pop_size = 100_000
-        self.runs_per_draw = 10
+        self.runs_per_draw = 1
         self.YEAR_OF_CHANGE = 2020
         self._scenarios = self._get_scenarios()
-        self.number_of_draws = len(self._scenarios)
+        self.number_of_draws = 1
 
     def log_configuration(self):
         return {
