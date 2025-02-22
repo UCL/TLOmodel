94a87eab95ca45c865649ce802abd6b6e79d6fdd
nicx503 <nic503@york.ac.uk>
2025-02-22 18:58:34 +0000

    added equipment for xray level1b and 2

--- src/tlo/methods/tb.py
+++ src/tlo/methods/tb.py
@@ -1534,7 +1534,7 @@ class ScenarioSetupEvent(RegularEvent, PopulationScopeEventMixin):
 
         # Introduce community Xray
         if scenario == 4:
-            self.sim.modules["Tb"].parameters["probability_community_chest_xray"] = 0.5
+            self.sim.modules["Tb"].parameters["probability_community_chest_xray"] = 0.15
             self.sim.modules['HealthSystem'].override_availability_of_consumables({175: 0.51})
             self.sim.modules['HealthSystem'].override_availability_of_consumables({187: 0.85})
             # Item code Solid culture and DST
