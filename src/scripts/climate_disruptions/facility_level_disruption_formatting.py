9b0dc6c4e80f8ea0612690958551dcab269a82fe
RachelMurray-Watson <rmurwat@gmail.com>
2025-03-07 11:31:16 +0000

    changed path to save files

--- src/scripts/climate_disruptions/facility_level_disruption_formatting.py
+++ src/scripts/climate_disruptions/facility_level_disruption_formatting.py
@@ -46,5 +46,5 @@ for scenario in scenarios:
             )
         projected_precip_disruptions['mean_all_service'] = \
         projected_precip_disruptions.groupby(['Facility_ID', 'month', 'year'])['disruption'].transform('mean')
-        projected_precip_disruptions.to_csv(f'/Users/rem76/Desktop/ResourceFiles_Disruptions/ResourceFile_Precipitation_Disruptions_{scenario}_{model}.csv')
+        projected_precip_disruptions.to_csv(f'/Users/rem76/PycharmProjects/TLOmodel/resources/ResourceFile_Climate/ResourceFile_Precipitation_Disruptions_{scenario}_{model}.csv')
 
