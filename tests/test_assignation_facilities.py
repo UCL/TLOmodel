818bededd0c569f82130a92c30b0e2f076c3f151
RachelMurray-Watson <rmurwat@gmail.com>
2025-03-10 11:16:42 +0000

    Now made abs value, which makes sampling easier

--- tests/test_assignation_facilities.py
+++ tests/test_assignation_facilities.py
@@ -1,62 +1,139 @@
 import os
 from pathlib import Path
+import pandas as pd
 
-import pytest
-from tlo.methods.healthsystem import HealthSystem, HealthSystemChangeParameters
-
-from tlo import DAYS_IN_MONTH, DAYS_IN_YEAR, Date, Module, Simulation, logging
-from tlo.methods import Metadata, demography
+from tlo import Date, Simulation, logging
 from tlo.methods import (
-    Metadata,
     chronicsyndrome,
     demography,
     enhanced_lifestyle,
-    epi,
     healthseekingbehaviour,
     healthsystem,
-    hiv,
     mockitis,
     simplified_births,
     symptommanager,
-    tb,
 )
-
-start_date = Date(2010, 1, 1)
-end_date = Date(2015, 1, 1)
-popsize = 500
+start_date = Date(2026, 1, 1)
+end_date = Date(2030, 1, 1)
+popsize = 5000
 
 resourcefilepath = Path(os.path.dirname(__file__)) / '../resources'
+def get_dataframe_of_run_events_count(_sim):
+    """Return a dataframe of event counts with info of treatment id, appointment footprint."""
+    count_df = pd.DataFrame(index=range(len(_sim.modules['HealthSystem'].hsi_event_counts)))
+    count_df['HSI_event'] = [event_details.event_name
+                             for event_details in _sim.modules['HealthSystem'].hsi_event_counts.keys()]
+    count_df['treatment_id'] = [event_details.treatment_id
+                                for event_details in _sim.modules['HealthSystem'].hsi_event_counts.keys()]
+    count_df['appt_footprint'] = [event_details.appt_footprint
+                                  for event_details in _sim.modules['HealthSystem'].hsi_event_counts.keys()]
+    count_df['count'] = [_sim.modules['HealthSystem'].hsi_event_counts[event_details]
+                         for event_details in _sim.modules['HealthSystem'].hsi_event_counts.keys()]
 
-def check_dtypes(simulation):
-    # check types of columns
-    df = simulation.population.props
-    orig = simulation.population.new_row
-    assert (df.dtypes == orig.dtypes).all()
+    return count_df
+# def check_dtypes(simulation):
+#     # check types of columns
+#     df = simulation.population.props
+#     orig = simulation.population.new_row
+#     assert (df.dtypes == orig.dtypes).all()
+#
+#
+# def test_setting_climate_disruptions(seed):
+#     """Check that the switches for turning on/off climate disruptions to healthcare access work"""
+#     sim = Simulation(start_date=start_date, seed=seed)
+#     sim.register(demography.Demography(resourcefilepath=resourcefilepath),
+#                  contraception.Contraception(resourcefilepath=resourcefilepath),
+#                  enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
+#                  healthburden.HealthBurden(resourcefilepath=resourcefilepath),
+#                  healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
+#                                            service_availability=['*'],
+#                                            cons_availability='all'),  # went set disable=true, cant check HSI queue,
+#                  newborn_outcomes.NewbornOutcomes(resourcefilepath=resourcefilepath),
+#                  pregnancy_supervisor.PregnancySupervisor(resourcefilepath=resourcefilepath),
+#                  care_of_women_during_pregnancy.CareOfWomenDuringPregnancy(resourcefilepath=resourcefilepath),
+#                  symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
+#                  labour.Labour(resourcefilepath=resourcefilepath),
+#                  postnatal_supervisor.PostnatalSupervisor(resourcefilepath=resourcefilepath),
+#                  healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
+#
+#                  hiv.DummyHivModule())
+#
+#     sim.make_initial_population(n=popsize)
+#     sim.simulate(end_date=end_date)
+#     assert sim.modules['HealthSystem'].services_affected_precip == 'none'
+#
+#
+# def test_setting_climate_disruptions(seed):
+#     """Check that the switches for turning on/off climate disruptions to healthcare access work"""
+#     sim = Simulation(start_date=start_date, seed=seed)
+#     sim.register(
+#         demography.Demography(resourcefilepath=resourcefilepath),
+#         healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
+#                                   climate_ssp = 'ssp126',
+#                                   climate_model_ensemble_model='mean',
+#                                   services_affected_precip = 'ANC')
+#     )
+#     sim.make_initial_population(n=popsize)
+#     sim.simulate(end_date=end_date)
+#     assert sim.modules['HealthSystem'].services_affected_precip == 'ANC'
 
 
-def test_setting_climate_disruptions(seed):
-    """Check that the switches for turning on/off climate disruptions to healthcare access work"""
-    sim = Simulation(start_date=start_date, seed=seed)
+def test_number_services(seed, tmpdir):
+    """Checks to see if the number of services under the climate disrupted scenario
+    are fewer than the no-climate disruption scenario"""
+    sim = Simulation(start_date=start_date,
+                     seed=seed,
+                     log_config={
+                     "filename": "log",
+                     "directory": tmpdir,
+                     "custom_levels": {
+                     "tlo.methods.healthsystem.summary": logging.DEBUG,
+            }})
     sim.register(
         demography.Demography(resourcefilepath=resourcefilepath),
-        healthsystem.HealthSystem(resourcefilepath=resourcefilepath)
-    )
-    sim.make_initial_population(n=popsize)
-    sim.simulate(end_date=end_date)
-    assert sim.modules['HealthSystem'].services_affected_precip == 'none'
+        enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
 
-
-def test_setting_climate_disruptions(seed):
-    """Check that the switches for turning on/off climate disruptions to healthcare access work"""
-    sim = Simulation(start_date=start_date, seed=seed)
-    sim.register(
-        demography.Demography(resourcefilepath=resourcefilepath),
         healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
-                                  climate_ssp = 'ssp126',
+                                  climate_ssp = 'ssp585',
                                   climate_model_ensemble_model='mean',
-                                  services_affected_precip = 'ANC')
+                                  services_affected_precip = 'ANC'),
+        symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
+        healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
+        mockitis.Mockitis(),
+        chronicsyndrome.ChronicSyndrome(),
+        simplified_births.SimplifiedBirths(resourcefilepath=resourcefilepath),
     )
     sim.make_initial_population(n=popsize)
     sim.simulate(end_date=end_date)
-    assert sim.modules['HealthSystem'].services_affected_precip == 'ANC'
+    #output_climate = parse_log_file(sim.log_filepath)
+    hsi_event_count_df_climate = get_dataframe_of_run_events_count(sim)
 
+    sim = Simulation(start_date=start_date,
+                     seed=seed,
+                     log_config={
+                     "filename": "log",
+                     "directory": tmpdir,
+                     "custom_levels": {
+                     "tlo.methods.healthsystem": logging.DEBUG,
+            }})
+    sim.register(
+        demography.Demography(resourcefilepath=resourcefilepath),
+        enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
+
+        healthsystem.HealthSystem(resourcefilepath=resourcefilepath,
+                                  climate_ssp = 'ssp585',
+                                  climate_model_ensemble_model='mean',
+                                  services_affected_precip = 'none'),
+        symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
+        healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
+        mockitis.Mockitis(),
+        chronicsyndrome.ChronicSyndrome(),
+        simplified_births.SimplifiedBirths(resourcefilepath=resourcefilepath),
+    )
+    sim.make_initial_population(n=popsize)
+    sim.simulate(end_date=end_date)
+    #output_no_climate = parse_log_file(sim.log_filepath)
+    hsi_event_count_df_no_climate = get_dataframe_of_run_events_count(sim)
+
+
+    assert sum(hsi_event_count_df_climate['count']) > sum(hsi_event_count_df_no_climate['count'])
