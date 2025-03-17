9d23328420b328f953c14a74ce041118490c95c7
RachelMurray-Watson <rmurwat@gmail.com>
2025-03-17 13:20:35 +0000

    Combined stacked bar chart across all scenarios

--- src/scripts/longterm_projections/prevalence_of_diseases_longterm_trends.py
+++ src/scripts/longterm_projections/prevalence_of_diseases_longterm_trends.py
@@ -18,9 +18,11 @@ from tlo.analysis.utils import (
 )
 
 min_year = 2020
-max_year = 2059
+max_year = 2069
 spacing_of_years = 1
 PREFIX_ON_FILENAME = '1'
+scenario_names = ["Baseline", "Perfect World", "HTM Scale-up", "Lifestyle: CMD", "Lifestyle: Cancer"]
+
 CONDITION_TO_COLOR_MAP_PREVALENCE = MappingProxyType(
     {
         '*': 'black',
@@ -60,22 +62,22 @@ CONDITION_TO_COLOR_MAP_PREVALENCE = MappingProxyType(
 )
 
 rename_dict = {  # For legend labels
-    'Alri': 'Lower respiratory infections',
-    'BladderCancer': 'Cancer (Bladder)',
-    'BreastCancer': 'Cancer (Breast)',
-    'Copd': 'COPD',
+    'ALRI': 'Lower respiratory infections',
+    'Bladder Cancer': 'Cancer (Bladder)',
+    'Breast Cancer': 'Cancer (Breast)',
+    'COPD': 'COPD',
     'Depression': 'Depression / Self-harm',
     'Diarrhoea': 'Diarrhoea',
     'Epilepsy': 'Epilepsy',
-    'Hiv': 'AIDS',
+    'HIV': 'AIDS',
     'Malaria': 'Malaria',
     'Measles': 'Measles',
-    'OesophagealCancer': 'Cancer (Oesophagus)',
-    'OtherAdultCancer': 'Cancer (Other)',
-    'ProstateCancer': 'Cancer (Prostate)',
+    'Oesophageal Cancer': 'Cancer (Oesophagus)',
+    'Other Adult Cancers': 'Cancer (Other)',
+    'Prostate Cancer': 'Cancer (Prostate)',
     'RTI': 'Transport Injuries',
     'Schisto': 'Schistosomiasis',
-    'Tb': 'TB',
+    'TB': 'TB',
     'chronic_ischemic_hd': 'Heart Disease',
     'chronic_kidney_disease': 'Kidney Disease',
     'chronic_lower_back_pain': 'Lower Back Pain',
@@ -88,129 +90,146 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
     """Produce standard set of plots describing the prevalence of each disease
     """
     # Set period of interest needed for helper functions
-    TARGET_PERIOD = (Date(min_year, 1, 1), Date(max_year, 12, 31))
-    # Definitions of general helper functions
-    make_graph_file_name = lambda stub: output_folder / f"{stub.replace('*', '_star_')}.png"  # noqa: E731
+    all_draws_prevalence_normalized = pd.DataFrame(columns = range(5)) # to save 2069 results
+    for draw in range(5):
+        TARGET_PERIOD = (Date(min_year, 1, 1), Date(max_year, 12, 31))
+        # Definitions of general helper functions
+        make_graph_file_name = lambda stub: output_folder / f"{stub.replace('*', '_star_')}_{draw}.png"  # noqa: E731
 
-    _, age_grp_lookup = make_age_grp_lookup()
+        _, age_grp_lookup = make_age_grp_lookup()
 
-    def _standardize_short_treatment_id(short_treatment_id):
-        return short_treatment_id.replace('_*', '*').rstrip('*') + '*'
+        def _standardize_short_treatment_id(short_treatment_id):
+            return short_treatment_id.replace('_*', '*').rstrip('*') + '*'
 
-    def get_color_cause_of_prevalence_label(prevalence_condition_label: str) -> str:
-        """Return the colour (as matplotlib string) assigned to this Prevalence Label.
+        def get_color_cause_of_prevalence_label(prevalence_condition_label: str) -> str:
+            """Return the colour (as matplotlib string) assigned to this Prevalence Label.
 
-        Returns `np.nan` if label is not recognised.
-        """
-        return CONDITION_TO_COLOR_MAP_PREVALENCE.get(_standardize_short_treatment_id(prevalence_condition_label),
-                                                     np.nan)
+            Returns `np.nan` if label is not recognised.
+            """
+            return CONDITION_TO_COLOR_MAP_PREVALENCE.get(_standardize_short_treatment_id(prevalence_condition_label),
+                                                         np.nan)
 
-    def get_prevalence_by_cause_label(_df):
-        """Return total number of Prevalence by label (total by age-group within the TARGET_PERIOD)
-        """
-        _df['date'] = pd.to_datetime(_df['date'])
-        # Filter the DataFrame based on the target period
-        filtered_df = _df.loc[_df['date'].between(*TARGET_PERIOD)]
-        prevalence_sum = filtered_df.sum(numeric_only=True)
-        return prevalence_sum
+        def get_prevalence_by_cause_label(_df):
+            """Return total number of Prevalence by label (total by age-group within the TARGET_PERIOD)
+            """
+            _df['date'] = pd.to_datetime(_df['date'])
+            # Filter the DataFrame based on the target period
+            filtered_df = _df.loc[_df['date'].between(*TARGET_PERIOD)]
+            prevalence_sum = filtered_df.sum(numeric_only=True)
+            return prevalence_sum
 
 
-    def get_population_for_year(_df):
-        """Returns the population in the year of interest"""
-        _df['date'] = pd.to_datetime(_df['date'])
+        def get_population_for_year(_df):
+            """Returns the population in the year of interest"""
+            _df['date'] = pd.to_datetime(_df['date'])
 
-        # Filter the DataFrame based on the target period
-        filtered_df = _df.loc[_df['date'].between(*TARGET_PERIOD)]
+            # Filter the DataFrame based on the target period
+            filtered_df = _df.loc[_df['date'].between(*TARGET_PERIOD)]
 
-        # Drop non-numeric columns (assume 'female' and 'male' are non-numeric)
-        numeric_df = filtered_df.drop(columns=['female', 'male'], errors='ignore')
+            # Drop non-numeric columns (assume 'female' and 'male' are non-numeric)
+            numeric_df = filtered_df.drop(columns=['female', 'male'], errors='ignore')
 
-        # Sum only numeric columns
-        population_sum = numeric_df.sum(numeric_only=True)
+            # Sum only numeric columns
+            population_sum = numeric_df.sum(numeric_only=True)
 
-        return population_sum
+            return population_sum
 
-    target_year_sequence = range(min_year, max_year, spacing_of_years)
-    make_graph_file_name = lambda stub: output_folder / f"{PREFIX_ON_FILENAME}_{stub}.png"  # noqa: E731
+        target_year_sequence = range(min_year, max_year, spacing_of_years)
+        make_graph_file_name = lambda stub: output_folder / f"{PREFIX_ON_FILENAME}_{stub}.png"  # noqa: E731
 
-    all_years_data_prevalence = {}
-    all_years_data_population = {}
-    for target_year in target_year_sequence:
-        TARGET_PERIOD = (
-            Date(target_year, 1, 1),
-            Date(target_year + spacing_of_years, 12, 31))
+        all_years_data_prevalence = {}
+        all_years_data_population = {}
+        for target_year in target_year_sequence:
+            TARGET_PERIOD = (
+                Date(target_year, 1, 1),
+                Date(target_year + spacing_of_years, 12, 31))
 
-        # Prevalence of diseases
-        result_data_deaths = summarize(extract_results(
-            results_folder,
-            module='tlo.methods.healthburden',
-            key='prevalence_of_diseases',
-            custom_generate_series=get_prevalence_by_cause_label,
-            do_scaling=True
-        ),
-            only_mean=True,
-            collapse_columns=True,
+            # Prevalence of diseases
+            result_data_deaths = summarize(extract_results(
+                results_folder,
+                module='tlo.methods.healthburden',
+                key='prevalence_of_diseases',
+                custom_generate_series=get_prevalence_by_cause_label,
+                do_scaling=True
+            ),
+                only_mean=True,
+                collapse_columns=True,
+            )[draw]
+            all_years_data_prevalence[target_year] = result_data_deaths['mean']
+            # Total population
+            result_data_population = summarize(extract_results(
+                results_folder,
+                module='tlo.methods.demography',
+                key='population',
+                custom_generate_series=get_population_for_year,
+                do_scaling=True
+            ),
+                only_mean=True,
+                collapse_columns=True,
+            )[draw]
+            all_years_data_population[target_year] = result_data_population['mean']
+        df_all_years_prevalence = pd.DataFrame(all_years_data_prevalence)
+
+        # Drop rows only if they exist
+        rows_to_drop = [
+            'live_births', 'population',
+            'PostnatalSupervisor', 'PregnancySupervisor', 'CardioMetabolicDisorders',
+            'NewbornOutcomes', 'Labour',
+            'Intrapartum stillbirth', 'Antenatal stillbirth', 'NMR', 'MMR'
+        ]
+        df_all_years_prevalence = df_all_years_prevalence.drop(index=rows_to_drop, errors='ignore')
+
+        # Rename index labels
+        df_all_years_prevalence = df_all_years_prevalence.rename(index=rename_dict)
+
+        # Plotting
+        fig, axes = plt.subplots(1, 2, figsize=(25, 10))
+        # Panel A: Prevalence - general - stacked
+
+        df_all_years_prevalence.T.plot.bar(stacked=True, ax=axes[0],
+                                           color=[get_color_cause_of_prevalence_label(_label) for _label in
+                                                  df_all_years_prevalence.index])
+        axes[0].set_title('Panel A: Prevalence by Condition')
+        axes[0].set_xlabel('Year')
+        axes[0].set_ylabel('Prevalence in population')
+        axes[0].grid(True)
+
+        axes[0].legend().set_visible(False)
+
+        # NORMALIZED Prevalence - normalized to 2010
+        df_all_years_prevalence = df_all_years_prevalence.rename(index=rename_dict)
+        df_all_years_prevalence_normalized = df_all_years_prevalence.div(df_all_years_prevalence.iloc[:, 0], axis=0)
+        for i, condition in enumerate(df_all_years_prevalence_normalized.index):
+            axes[1].plot(df_all_years_prevalence_normalized.columns, df_all_years_prevalence_normalized.loc[condition],
+                         marker='o',
+                         label=condition, color=[get_color_cause_of_prevalence_label(_label) for _label in
+                                                 df_all_years_prevalence_normalized.index][i])
+        axes[1].set_title('Panel B: Normalized Prevalence by Condition')
+        axes[1].set_xlabel('Year')
+        axes[1].set_ylabel('Fold change in deaths compared to 2020')
+
+        axes[1].legend(title='Condition', bbox_to_anchor=(1, 1), loc='upper left')
+        axes[1].grid(True)
+        axes[1].set_ylim(0, 4.5)
+        fig.tight_layout()
+        fig.savefig(make_graph_file_name(f'Trend_Prevalence_by_Condition_All_Years_Raw_and_Normalized_Panel_A_and_B_{draw}'))
+        plt.close(fig)
+        df_all_years_prevalence_normalized.to_csv(output_folder/f"Prevalence_by_condition_normalized_2020_{draw}.csv")
+        all_draws_prevalence_normalized[draw] = df_all_years_prevalence_normalized.iloc[:,-1]
+
+    fig, ax = plt.subplots(figsize=(15, 6))  # Single axis
+
+    all_draws_prevalence_normalized.T.plot.bar(
+            stacked=True, ax=ax,
+            color=[get_color_cause_of_prevalence_label(_label) for _label in all_draws_prevalence_normalized.index],
         )
-        all_years_data_prevalence[target_year] = result_data_deaths
-        # Total population
-        result_data_population = summarize(extract_results(
-            results_folder,
-            module='tlo.methods.demography',
-            key='population',
-            custom_generate_series=get_population_for_year,
-            do_scaling=True
-        ),
-            only_mean=True,
-            collapse_columns=True,
-        )
-        all_years_data_population[target_year] = result_data_population
-
-    # Convert the accumulated data into a DataFrame for plotting
-
-    df_all_years_prevalence = pd.DataFrame(all_years_data_prevalence)
-    df_all_years_prevalence = df_all_years_prevalence.drop([#'live_births',
-                                                            'population'], axis=0)  # extra data
-    df_all_years_prevalence = df_all_years_prevalence.drop(['PostnatalSupervisor', 'PregnancySupervisor',
-                                                            'CardioMetabolicDisorders', 'NewbornOutcomes', 'Labour'],
-                                                           axis=0)  # empty or duplicated with actual label
-    df_all_years_prevalence = df_all_years_prevalence.drop([#'NMR', 'MMR',
-                                                            'Intrapartum stillbirth', 'Antenatal stillbirth'],
-                                                           axis=0)  # not prevalence
-    df_all_years_prevalence = df_all_years_prevalence.rename(index=rename_dict)  # For labels
-
-    # Plotting
-    fig, axes = plt.subplots(1, 2, figsize=(25, 10))
-    # Panel A: Prevalence - general - stacked
-
-    df_all_years_prevalence.T.plot.bar(stacked=True, ax=axes[0],
-                                       color=[get_color_cause_of_prevalence_label(_label) for _label in
-                                              df_all_years_prevalence.index])
-    axes[0].set_title('Panel A: Prevalence by Condition')
-    axes[0].set_xlabel('Year')
-    axes[0].set_ylabel('Prevalence in population')
-    axes[0].grid(True)
-
-    axes[0].legend().set_visible(False)
-
-    # NORMALIZED Prevalence - normalized to 2010
-    df_all_years_prevalence = df_all_years_prevalence.rename(index=rename_dict)
-    df_all_years_prevalence_normalized = df_all_years_prevalence.div(df_all_years_prevalence.iloc[:, 0], axis=0)
-    for i, condition in enumerate(df_all_years_prevalence_normalized.index):
-        axes[1].plot(df_all_years_prevalence_normalized.columns, df_all_years_prevalence_normalized.loc[condition],
-                     marker='o',
-                     label=condition, color=[get_color_cause_of_prevalence_label(_label) for _label in
-                                             df_all_years_prevalence_normalized.index][i])
-    axes[1].set_title('Panel B: Normalized Prevalence by Condition')
-    axes[1].set_xlabel('Year')
-    axes[1].set_ylabel('Fold change in deaths compared to 2020')
-
-    axes[1].legend(title='Condition', bbox_to_anchor=(1, 1), loc='upper left')
-    axes[1].grid(True)
-    axes[1].set_ylim(0, 4.5)
+    ax.legend(bbox_to_anchor=(1.05, 1.05), ncol=1)
+    ax.set_ylabel('Fold change in condition prevalence compared to 2020')
+    ax.set_xlabel('Scenario')
+    ax.set_xticklabels(scenario_names, rotation=45)
     fig.tight_layout()
-    fig.savefig(make_graph_file_name('Trend_Prevalence_by_Condition_All_Years_Raw_and_Normalized_Panel_A_and_B'))
-    plt.close(fig)
-    df_all_years_prevalence_normalized.to_csv(output_folder/"Prevalence_by_condition_normalized_2020.csv")
+    fig.savefig(output_folder / "Prevalence_by_condition_normalized_combined.png")
+    plt.show()
 
 
 if __name__ == "__main__":
