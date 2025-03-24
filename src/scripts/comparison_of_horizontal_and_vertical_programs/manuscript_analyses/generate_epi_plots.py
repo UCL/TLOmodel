c8792f62973b4a889dc8274245534ac15e59c2a8
tdm32 <t.mangal@imperial.ac.uk>
2025-03-24 12:10:27 +0000

    epi outputs plot

--- src/scripts/comparison_of_horizontal_and_vertical_programs/manuscript_analyses/generate_epi_plots.py
+++ src/scripts/comparison_of_horizontal_and_vertical_programs/manuscript_analyses/generate_epi_plots.py
@@ -19,6 +19,7 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from scipy.ndimage import uniform_filter1d
 
 from tlo import Date
 from tlo.analysis.utils import (
@@ -133,6 +134,7 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
 
     # %% Extract the incidence
 
+    # this measure is per-person
     hiv_inc = compute_summary_statistics(extract_results(
         results_folder,
         module='tlo.methods.hiv',
@@ -143,6 +145,8 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
     ).pipe(set_param_names_as_column_index_level_0),
                                                 central_measure='median'
                                                 )
+    hiv_inc_per_1000py = hiv_inc * 1000
+    hiv_inc_per_1000py.index = hiv_inc_per_1000py.index.year
 
     # todo this needs to be divided by PY
     tb_inc = compute_summary_statistics(extract_results(
@@ -151,13 +155,44 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
         key='tb_incidence',
         column="num_new_active_tb",
         index='date',
-        do_scaling=True
+        do_scaling=False
     ).pipe(set_param_names_as_column_index_level_0),
                                                 central_measure='median'
                                                 )
 
+    def get_person_years(_df):
+        """ extract person-years for each draw/run
+        sums across men and women
+        will skip column if particular run has failed
+        """
+        years = pd.to_datetime(_df["date"]).dt.year
+        py = pd.Series(dtype="int64", index=years)
+        for year in years:
+            tot_py = (
+                (_df.loc[pd.to_datetime(_df["date"]).dt.year == year]["M"]).apply(pd.Series) +
+                (_df.loc[pd.to_datetime(_df["date"]).dt.year == year]["F"]).apply(pd.Series)
+            ).transpose()
+            py[year] = tot_py.sum().values[0]
 
-    mal_inc = compute_summary_statistics(extract_results(
+        py.index = pd.to_datetime(years, format="%Y")
+
+        return py
+
+    personyears = compute_summary_statistics(extract_results(
+        results_folder,
+        module="tlo.methods.demography",
+        key="person_years",
+        custom_generate_series=get_person_years,
+        do_scaling=False
+    ).pipe(set_param_names_as_column_index_level_0),
+                                                central_measure='median'
+                                                )
+
+    tb_inc_per_1000py = tb_inc.divide(personyears) * 1000
+    tb_inc_per_1000py.index = tb_inc_per_1000py.index.year
+    tb_inc_per_1000py = tb_inc_per_1000py.drop(tb_inc_per_1000py.index[0])  # drop first empty row
+
+    mal_inc_per_1000py = compute_summary_statistics(extract_results(
         results_folder,
         module='tlo.methods.malaria',
         key='incidence',
@@ -167,6 +202,7 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
     ).pipe(set_param_names_as_column_index_level_0),
                                                 central_measure='median'
                                                 )
+    mal_inc_per_1000py.index = mal_inc_per_1000py.index.year
 
     # %% Extract the treatment coverage
 
@@ -180,6 +216,8 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
     ).pipe(set_param_names_as_column_index_level_0),
                                                 central_measure='median'
                                                 )
+    hiv_tx = hiv_tx * 100
+    hiv_tx.index = hiv_tx.index.year
 
     tb_tx = compute_summary_statistics(extract_results(
         results_folder,
@@ -191,6 +229,8 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
     ).pipe(set_param_names_as_column_index_level_0),
                                                 central_measure='median'
                                                 )
+    tb_tx = tb_tx * 100
+    tb_tx.index = tb_tx.index.year
 
     mal_tx = compute_summary_statistics(extract_results(
         results_folder,
@@ -202,11 +242,141 @@ def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = No
     ).pipe(set_param_names_as_column_index_level_0),
                                                 central_measure='median'
                                                 )
+    mal_tx = mal_tx * 100
+    mal_tx.index = mal_tx.index.year
 
 
+    # %% Extract the numbers of deaths
+    def summarise_deaths_for_one_cause(results_folder, cause):
 
+        results_deaths = extract_results(
+            results_folder,
+            module="tlo.methods.demography",
+            key="death",
+            custom_generate_series=(
+                lambda df: df.assign(year=df["date"].dt.year).groupby(
+                    ["year", "cause"])["person_id"].count()
+            ),
+            do_scaling=True,
+        ).pipe(set_param_names_as_column_index_level_0)
 
+        # removes multi-index
+        results_deaths = results_deaths.reset_index()
 
+        # select only cause specified
+        if cause == 'HIV':
+            tmp = results_deaths.loc[
+                (results_deaths.cause == "AIDS_TB") | (results_deaths.cause == "AIDS_non_TB")
+                ]
+        else:
+            tmp = results_deaths.loc[
+                (results_deaths.cause == cause)
+            ]
+
+        # group deaths by year
+        tmp = pd.DataFrame(tmp.groupby(["year"]).sum())
+        # drop first column listing cause
+        tmp = tmp.drop('cause', axis=1)
+
+        # get median and UI
+        tmp2 = pd.concat({
+            'central': tmp.groupby(level=0, axis=1).median(0.5),
+            'lower': tmp.groupby(level=0, axis=1).quantile(0.025),
+            'upper': tmp.groupby(level=0, axis=1).quantile(0.975)
+        }, axis=1).swaplevel(axis=1)
+
+        return tmp2
+
+    aids_deaths = summarise_deaths_for_one_cause(results_folder, 'HIV') / 1000
+    tb_deaths = summarise_deaths_for_one_cause(results_folder, 'TB') / 1000
+    malaria_deaths = summarise_deaths_for_one_cause(results_folder, 'Malaria') / 1000
+
+    # %% Extract the numbers of deaths
+
+    # Function to apply smoothing using a moving average
+    def smooth_data(data, window_size=3):
+        return uniform_filter1d(data, size=window_size)
+
+    # Function to plot multiple dataframes efficiently
+    def plot_multiple_dfs(dfs, row_labels, titles, legend_text=None, legend_fontsize=10, row_label_fontsize=12, y_label_fontsize=10):
+        fig, axes = plt.subplots(3, 3, figsize=(12, 9), sharex=True)
+        sns.set_style("white")
+        name_of_plot = 'epi_outputs'
+
+        column_y_labels = [
+            'Incidence per 1000 person-years',  # Column 0
+            'Treatment coverage (%)',  # Column 1
+            'Numbers of deaths, thousands',  # Column 2
+        ]
+
+        if legend_text is None:
+            legend_text = Summary_scenarios
+
+        legend_lines = []
+
+        # Loop through the DataFrames and plot each one
+        for i, df in enumerate(dfs):
+            row, col = divmod(i, 3)
+            ax = axes[row, col]
+            ax.grid(False)  # Explicitly remove grid lines
+
+            # Plot each scenario in the DataFrame
+            for scenario in Summary_scenarios:
+                color = color_map.get(scenario, "#000000")  # Default to black if not in color_map
+
+                # Apply smoothing to the data
+                central_smoothed = smooth_data(df[(scenario, "central")].values)
+                lower_smoothed = smooth_data(df[(scenario, "lower")].values)
+                upper_smoothed = smooth_data(df[(scenario, "upper")].values)
+
+                line, = ax.plot(df.index, central_smoothed, label=scenario, color=color)
+
+                # Only the first time through should add the line to the legend
+                if col == 0 and row == 0:
+                    legend_lines.append(line)
+
+                ax.fill_between(df.index, lower_smoothed, upper_smoothed, color=color, alpha=0.2)
+
+            # Set titles and labels
+            if row == 0:
+                ax.set_title(titles[col])
+
+            # Add row labels only for the first column
+            if col == 0:  # Only add row label to the first column of each row
+                ax.annotate(row_labels[row], xy=(-0.3, 0.5), xycoords='axes fraction', fontsize=row_label_fontsize,
+                            ha='center', va='center', rotation=90, fontweight='bold')
+
+            # Add column-specific y-axis labels
+            ax.set_ylabel(column_y_labels[col], fontsize=y_label_fontsize, labelpad=10)  # y-axis label for each column
+
+            # Add custom y-axis limits for the second column if needed
+            if col == 1:
+                ax.set_ylim(0, 100)  # Constraint for second column
+
+            # Adjust x-tick labels to display every other year
+            if row == 2:
+                ax.set_xticks(df.index[::2])  # Label every other year
+                ax.set_xticklabels(df.index[::2], rotation=45)
+                ax.tick_params(axis='x', which='both', bottom=True)  # Ensure the tick marks are shown
+
+        axes[2, 2].legend(legend_lines, legend_text, loc="upper right", fontsize=legend_fontsize, frameon=False)
+
+        # Adjust the spacing for row labels and overall layout
+        plt.tight_layout(rect=[0, 1, 1, 1])  # Ensure that everything fits within the figure
+        plt.subplots_adjust(hspace=0.1, wspace=0.3)  # Increase vertical and horizontal space between subplots
+
+        # Save and display the plot
+        file_name = make_graph_file_name(name_of_plot.replace(' ', '_').replace(',', ''))
+        fig.savefig(file_name)
+        plt.show()
+
+    # Plot the epi outputs
+    dfs = [hiv_inc_per_1000py, hiv_tx, aids_deaths,
+           tb_inc_per_1000py, tb_tx, tb_deaths,
+           mal_inc_per_1000py, mal_tx, malaria_deaths]
+    plot_multiple_dfs(dfs, ["HIV", "Tuberculosis", "Malaria"],
+                      ["Incidence", "Treatment Coverage", "Numbers of deaths"],
+        legend_text=["HSS Package", "HTM Scale-up", "HTM Scale-up + HSS Package"])
 
 
 
