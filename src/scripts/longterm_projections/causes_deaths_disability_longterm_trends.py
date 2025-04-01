0868e75fd352564984724e21c915894c8690cd5d
RachelMurray-Watson <rmurwat@gmail.com>
2025-04-01 11:01:24 +0100

    Improve visibility of colour

--- src/scripts/longterm_projections/causes_deaths_disability_longterm_trends.py
+++ src/scripts/longterm_projections/causes_deaths_disability_longterm_trends.py
@@ -23,7 +23,7 @@ spacing_of_years = 1
 PREFIX_ON_FILENAME = '1'
 
 scenario_names = ["Baseline", "Perfect World", "HTM Scale-up", "Lifestyle: CMD", "Lifestyle: Cancer"]
-scenario_colours = ['#0081a7', '#00afb9', '#fdfcdc', '#fed9b7', '#f07167']
+scenario_colours = ['#0081a7', '#00afb9', '#FEB95F', '#fed9b7', '#f07167']
 def apply(results_folder: Path, output_folder: Path, resourcefilepath: Path = None):
     """Produce standard set of plots describing the effect of each TREATMENT_ID.
     - We estimate the epidemiological impact as the EXTRA deaths that would occur if that treatment did not occur.
