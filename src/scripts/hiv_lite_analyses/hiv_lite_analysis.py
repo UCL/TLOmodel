59e6f5aaf1b88daf9eae5beb6bcee49244aa831a
mnjowe <emmanuelmnjowe@gmail.com>
2025-04-10 06:16:52 +0300

    updated analyses file

--- src/scripts/hiv_lite_analyses/hiv_lite_analysis.py
+++ src/scripts/hiv_lite_analyses/hiv_lite_analysis.py
@@ -1,21 +1,40 @@
-from tlo import Simulation
+from matplotlib import pyplot as plt
+
+from tlo import Simulation, logging
 from pathlib import Path
 from tlo import Date
+from tlo.analysis.utils import parse_log_file
 
-from tlo.methods import demography, hivlite, simplified_births
+from tlo.methods import demography, hivlite
 
 outputpath = Path('./outputs')
 resourcefilepath = Path('./resources')
 
 start_date = Date(2010, 1, 1)
-end_date = Date(2011, 1, 1)
-log_config = {}
+end_date = Date(2020, 1, 1)
+
+log_config = {
+    "filename": "hiv_lite_logs",
+    "directory": outputpath,
+    "custom_levels": {
+        "*": logging.WARNING,
+        "tlo.methods.hivlite": logging.INFO,
+    },
+}
 
 sim = Simulation(start_date=start_date, seed=0, log_config=log_config)
 sim.register(demography.Demography(resourcefilepath=resourcefilepath),
-             simplified_births.SimplifiedBirths(resourcefilepath=resourcefilepath),
              hivlite.HivLite(resourcefilepath=resourcefilepath)
              )
-sim.make_initial_population(n=1000)
+sim.make_initial_population(n=20000)
 
-sim.simulate(end_date=end_date)
+# sim.simulate(end_date=end_date)
+logs = parse_log_file('outputs/hiv_lite_logs__2025-04-09T162555.log')
+df = logs['tlo.methods.hivlite']['hiv_infection_cases']
+
+df['year'] = df.date.dt.year
+df = df.set_index('year')
+df.drop('date', axis=1, inplace=True)
+df.plot.bar(stacked=True)
+plt.title('HIV cases by age group')
+plt.show()
--- src/tlo/methods/hivlite.py
+++ src/tlo/methods/hivlite.py
@@ -3,10 +3,12 @@ from pathlib import Path
 
 import pandas as pd
 
-from tlo import Module, Types, Parameter, Property, Population, Simulation
+from tlo import Module, Types, Parameter, Property, Population, Simulation, logging
 from tlo.events import RegularEvent, PopulationScopeEventMixin
 from tlo.methods import Metadata
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 class HivLite(Module):
     def __init__(self, resourcefilepath=None):
@@ -38,11 +40,11 @@ class HivLite(Module):
 
     def initialise_simulation(self, sim: Simulation) -> None:
         sim.schedule_event(HivInfectionEvent(self), sim.date + pd.DateOffset(months=1))
+        sim.schedule_event(HivLiteloogingevent(self), sim.date + pd.DateOffset(months=1))
 
-    def on_birth(self, mother_id: int, child_id: int) -> None:
+    def on_simulation_end(self) -> None:
         df = self.sim.population.props
-        df.at[child_id, 'hl_hiv_status'] = 'Non-Reactive'
-        df.at[child_id, 'hl_date_inf'] = pd.NaT
+        df.to_excel(Path('./outputs/aphrc.xlsx'), index=False)
 
 class HivInfectionEvent(RegularEvent, PopulationScopeEventMixin):
     def __init__(self, module):
@@ -60,7 +62,36 @@ class HivInfectionEvent(RegularEvent, PopulationScopeEventMixin):
         df.loc[inf_index, 'hl_hiv_status'] = 'Reactive'
         df.loc[inf_index, 'hl_date_inf'] = self.module.sim.date
 
-        print(df)
+class HivLiteloogingevent(RegularEvent, PopulationScopeEventMixin):
+    def __init__(self, module):
+        super().__init__(module, frequency=pd.DateOffset(months=12))
+
+    def apply(self, population):
+        df = population.props
+        alive_indv = df.loc[df.is_alive]
+
+        indiv_0_14 = alive_indv.loc[df.age_years.between(0, 14) & (df.hl_hiv_status == 'Reactive')]
+        indiv_15_24 = alive_indv.loc[df.age_years.between(15, 24) & (df.hl_hiv_status == 'Reactive')]
+        indiv_25_34 = alive_indv.loc[df.age_years.between(25, 34) & (df.hl_hiv_status == 'Reactive')]
+        indiv_35_44 = alive_indv.loc[df.age_years.between(35, 44) & (df.hl_hiv_status == 'Reactive')]
+        indiv_45_54 = alive_indv.loc[df.age_years.between(45, 54) & (df.hl_hiv_status == 'Reactive')]
+        indiv_55_64 = alive_indv.loc[df.age_years.between(55, 64) & (df.hl_hiv_status == 'Reactive')]
+        indiv_65 = alive_indv.loc[df.age_years >= 65 & (df.hl_hiv_status == 'Reactive')]
+
+        hiv_infections_cases_by_age_group = {
+            'indiv_0_14': len(indiv_0_14),
+            'indiv_15_24': len(indiv_15_24),
+            'indiv_25_34': len(indiv_25_34),
+            'indiv_35_44': len(indiv_35_44),
+            'indiv_45_54': len(indiv_45_54),
+            'indiv_55_64': len(indiv_55_64),
+            'indiv_65': len(indiv_65),
+        }
+
+        logger.info(key='hiv_infection_cases', data=hiv_infections_cases_by_age_group, description='hiv infection cases')
+
+
+
 
 
 
