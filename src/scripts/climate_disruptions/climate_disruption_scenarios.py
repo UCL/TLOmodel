6e0ad54bd1fa7eb375cdf0ddaf5b6327f26bf115
RachelMurray-Watson <rmurwat@gmail.com>
2025-04-23 14:27:55 +0100

    Reverted changes made to test if it's working

--- src/scripts/climate_disruptions/climate_disruption_scenarios.py
+++ src/scripts/climate_disruptions/climate_disruption_scenarios.py
@@ -13,8 +13,8 @@ class ClimateDisruptionScenario(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2030, 1, 12)
-        self.pop_size = 100
-        self.runs_per_draw = 1
+        self.pop_size = 10000
+        self.runs_per_draw = 5
         self.YEAR_OF_CHANGE = 2020
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
