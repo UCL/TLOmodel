59ce01626d649119a281fc14af972a9ce36872d5
joehcollins <joseph.h.collins@ucl.ac.uk>
2025-05-06 11:48:56 +0100

    results['mat_deaths']['crude'][0]

--- src/scripts/maternal_perinatal_analyses/cohort_analysis/int_analysis_script.py
+++ src/scripts/maternal_perinatal_analyses/cohort_analysis/int_analysis_script.py
@@ -100,7 +100,7 @@ def summarize_confidence_intervals(results: pd.DataFrame) -> pd.DataFrame:
 
     return summary
 
-scenario = 'testing_scenario_2244959'
+scenario = 'testing_scenario_2281340'
 results_folder= get_scenario_outputs(scenario, outputspath)[-1]
 
 # Create a folder to store graphs (if it hasn't already been created when ran previously)
@@ -109,24 +109,7 @@ g_path = f'{outputspath}graphs_{scenario}'
 if not os.path.isdir(g_path):
         os.makedirs(f'{outputspath}graphs_{scenario}')
 
-# interventions =['amtsl',
-#                 'pph_treatment_uterotonics',
-#                 'pph_treatment_mrrp',
-#                 'pph_treatment_surg',
-#                 'blood_transfusion',
-#                 'antihypertensives',
-#                 'mgso4',
-#                 'sepsis_treatment',
-#                 'birth_kit',
-#                 'caeasarean_section',
-#                 'iron_folic_acid',
-#                 'post_abortion_care',
-#                 'ectopic_pregnancy_treatment',
-#                 ]
-
-interventions =['caesarean_section_oth_surg']
-
-
+interventions =['neo_resus', 'kmc', 'neo_sepsis_treatment']
 int_analysis = ['baseline']
 
 for i in interventions:
@@ -157,44 +140,57 @@ results = {k:get_ps_data_frames(k, results_folder) for k in
            ['mat_comp_incidence', 'nb_comp_incidence', 'deaths_and_stillbirths','service_coverage',
             'yearly_mnh_counter_dict', 'intervention_coverage']}
 
-direct_deaths = extract_results(
+def get_deaths_demog(group, multiplier):
+    direct_deaths = extract_results(
+                results_folder,
+                module="tlo.methods.demography",
+                key="death",
+                custom_generate_series=(
+                    lambda df: df.loc[(df['label'] == f'{group} Disorders')].assign(
+                        year=df['date'].dt.year).groupby(['year'])['year'].count()),
+                do_scaling=False)
+
+    br = extract_results(
+                results_folder,
+                module="tlo.methods.demography",
+                key="on_birth",
+                custom_generate_series=(
+                    lambda df: df.assign(
+                        year=df['date'].dt.year).groupby(['year'])['year'].count()),
+                do_scaling=False
+            )
+
+    dd_sum = summarize_confidence_intervals(direct_deaths)
+    dd_mr = (direct_deaths/br) * multiplier
+    dd_mr_sum = summarize_confidence_intervals(dd_mr)
+
+    all_dalys_dfs = extract_results(
             results_folder,
-            module="tlo.methods.demography",
-            key="death",
+            module="tlo.methods.healthburden",
+            key="dalys_stacked",
             custom_generate_series=(
-                lambda df: df.loc[(df['label'] == 'Maternal Disorders')].assign(
-                    year=df['date'].dt.year).groupby(['year'])['year'].count()),
+                lambda df: df.drop(
+                    columns=['date', 'sex', 'age_range']).groupby(['year']).sum().stack()),
             do_scaling=False)
 
-br = extract_results(
-            results_folder,
-            module="tlo.methods.demography",
-            key="on_birth",
-            custom_generate_series=(
-                lambda df: df.assign(
-                    year=df['date'].dt.year).groupby(['year'])['year'].count()),
-            do_scaling=False
-        )
+    disorders_all = all_dalys_dfs.loc[(slice(None), f'{group} Disorders'), :]
 
-dd_sum = summarize_confidence_intervals(direct_deaths)
-dd_mmr = (direct_deaths/br) * 100_000
-dd_mr_sum = summarize_confidence_intervals(dd_mmr)
+    dalys_df = disorders_all.loc[2024]
+    dalys_df_sum = summarize_confidence_intervals(dalys_df)
 
-all_dalys_dfs = extract_results(
-        results_folder,
-        module="tlo.methods.healthburden",
-        key="dalys_stacked",
-        custom_generate_series=(
-            lambda df: df.drop(
-                columns=['date', 'sex', 'age_range']).groupby(['year']).sum().stack()),
-        do_scaling=False)
+    return [direct_deaths, dd_sum, dd_mr, dd_mr_sum, dalys_df, dalys_df_sum]
 
-mat_disorders_all = all_dalys_dfs.loc[(slice(None), 'Maternal Disorders'), :]
+mat_deaths_dalys = get_deaths_demog('Maternal', 100_000)
+neo_deaths_dalys = get_deaths_demog('Neonatal', 1000)
 
-mat_dalys_df = mat_disorders_all.loc[2024]
-mat_dalys_df_sum = summarize_confidence_intervals(mat_dalys_df)
-
-results.update({'dalys':{'crude': mat_dalys_df, 'summarised': mat_dalys_df_sum}})
+results.update({
+                'mat_deaths': {'crude': mat_deaths_dalys[0], 'summarised': mat_deaths_dalys[1]},
+                'neo_deaths': {'crude': neo_deaths_dalys[0], 'summarised': neo_deaths_dalys[1]},
+                'mmr': {'crude': mat_deaths_dalys[2], 'summarised': mat_deaths_dalys[3]},
+                'nmr': {'crude': neo_deaths_dalys[2], 'summarised': neo_deaths_dalys[3]},
+                'mat_dalys': {'crude': mat_deaths_dalys[4], 'summarised': mat_deaths_dalys[5]},
+                'neo_dalys': {'crude': neo_deaths_dalys[4], 'summarised': neo_deaths_dalys[5]},
+                })
 
 # Summarised results
 def get_data(df, key, draw):
@@ -202,13 +198,19 @@ def get_data(df, key, draw):
             df.loc[key, (draw, 'mean')],
             df.loc[key, (draw, 'upper')])
 
-dalys_by_scenario = {k: get_data(results['dalys']['summarised'], 'Maternal Disorders', d) for k, d in zip (
+mat_dalys_by_scenario = {k: get_data(results['mat_dalys']['summarised'], 'Maternal Disorders', d) for k, d in zip (
+    int_analysis, draws)}
+neo_dalys_by_scenario = {k: get_data(results['neo_dalys']['summarised'], 'Neonatal Disorders', d) for k, d in zip (
     int_analysis, draws)}
 
 mmr_by_scnario = {k: get_data(results['deaths_and_stillbirths']['summarised'], 'direct_mmr', d) for k, d in zip (
     int_analysis, draws)}
+nmr_by_scnario = {k: get_data(results['deaths_and_stillbirths']['summarised'], 'nmr', d) for k, d in zip (
+    int_analysis, draws)}
 
-mmr_by_scenario_oth_log = {k: get_data(dd_mr_sum, 2024, d) for k, d in zip (
+mmr_by_scenario_oth_log = {k: get_data(results['mat_deaths']['summarised'], 2024, d) for k, d in zip (
+    int_analysis, draws)}
+nmr_by_scenario_oth_log = {k: get_data(results['neo_deaths']['summarised'], 2024, d) for k, d in zip (
     int_analysis, draws)}
 
 def barcharts(data, y_label, title):
@@ -235,9 +237,11 @@ def barcharts(data, y_label, title):
     plt.savefig(f'{g_path}/{title}.png', bbox_inches='tight')
     plt.show()
 
-barcharts(dalys_by_scenario, 'DALYs', 'Total Maternal Disorders DALYs by scenario')
+barcharts(mat_dalys_by_scenario, 'DALYs', 'Total Maternal Disorders DALYs by scenario')
+barcharts(neo_dalys_by_scenario, 'DALYs', 'Total Neonatal Disorders DALYs by scenario')
+
 barcharts(mmr_by_scnario, 'MMR', 'Total MMR by scenario')
-barcharts(mmr_by_scnario, 'MMR', 'Total MMR by scenario')
+barcharts(nmr_by_scnario, 'MMR', 'Total NMR by scenario')
 
 # Difference results
 def get_diffs(df_key, result_key, ints, draws):
@@ -254,22 +258,32 @@ def get_diffs(df_key, result_key, ints, draws):
 
     return [diff_results, diff_df]
 
-diff_results = {}
-baseline = dd_mmr[0]
+def get_diffs_demog_log(mr_df):
+    diff_results = {}
+    baseline = mr_df['crude'][0]
 
-for draw, int in zip(draws, int_analysis):
-    diff_df = dd_mmr[draw] - baseline
-    diff_df.columns = pd.MultiIndex.from_tuples([(draw, v) for v in range(len(diff_df.columns))],
-                                                    names=['draw', 'run'])
-    results_diff = summarize_confidence_intervals(diff_df)
-    results_diff.fillna(0)
-    diff_results.update({int: results_diff.loc[2024].values})
+    for draw, int in zip(draws, int_analysis):
+        diff_df = mr_df['crude'][draw] - baseline
+        diff_df.columns = pd.MultiIndex.from_tuples([(draw, v) for v in range(len(diff_df.columns))],
+                                                        names=['draw', 'run'])
+        results_diff = summarize_confidence_intervals(diff_df)
+        results_diff.fillna(0)
+        diff_results.update({int: results_diff.loc[2024].values})
+
+    return diff_results
 
 
 mat_deaths = get_diffs('deaths_and_stillbirths', 'direct_maternal_deaths', int_analysis, draws)[0]
+neo_deaths = get_diffs('deaths_and_stillbirths', 'neonatal_deaths', int_analysis, draws)[0]
+
 mmr_diffs = get_diffs('deaths_and_stillbirths', 'direct_mmr', int_analysis, draws)[0]
-dalys_diffs = get_diffs('dalys', 'Maternal Disorders', int_analysis, draws)[0]
-mat_deaths_2 = diff_results
+nmr_diffs = get_diffs('deaths_and_stillbirths', 'nmr', int_analysis, draws)[0]
+
+mat_dalys_diffs = get_diffs('mat_dalys', 'Maternal Disorders', int_analysis, draws)[0]
+neo_dalys_diffs = get_diffs('neo_dalys', 'Neonatal Disorders', int_analysis, draws)[0]
+
+mat_deaths_2 = get_diffs_demog_log(results['mmr'])
+neo_deaths_2 = get_diffs_demog_log(results['nmr'])
 
 def get_diff_plots(data, outcome):
     categories = list(data.keys())
@@ -298,44 +312,16 @@ def get_diff_plots(data, outcome):
     plt.show()
 
 get_diff_plots(mmr_diffs, 'MMR')
-get_diff_plots(mat_deaths, 'Maternal Deaths (crude)')
-get_diff_plots(mat_deaths_2, 'MMR (demog log)')
-get_diff_plots(dalys_diffs, 'Maternal DALYs')
+get_diff_plots(nmr_diffs, 'NMR')
 
-# def get_tornado_plot(data, outcome):
-#     grouped_data = {}
-#     data.pop('baseline', None)
-#
-#     for key in mat_deaths_2.keys():
-#         base_key = key.rsplit('_', 1)[0] if key.endswith('_max') or key.endswith('_min') else key
-#
-#         if base_key not in grouped_data:
-#             grouped_data[base_key] = {'min': None, 'max': None}
-#         if 'min' in key:
-#             grouped_data[base_key]['min'] = data[key]
-#         elif 'max' in key:
-#             grouped_data[base_key]['max'] = data[key]
-#
-#     # Prepare data for plotting
-#     categories = list(grouped_data.keys())
-#     min_values = [np.mean(grouped_data[cat]['min']) for cat in categories]
-#     max_values = [np.mean(grouped_data[cat]['max']) for cat in categories]
-#
-#     # Plotting
-#     plt.figure(figsize=(10, 6))
-#     y_positions = np.arange(len(categories))
-#     plt.barh(y_positions, min_values, color='lightcoral', edgecolor='black', alpha=0.7,
-#             label='Min Effect')
-#     plt.barh(y_positions, max_values, color='skyblue', edgecolor='black', alpha=0.7,
-#              label='Max Effect')
-#     plt.axvline(0, color='black', linewidth=1, linestyle='--')  # Central zero line
-#     # Add labels
-#     plt.yticks(y_positions, categories)
-#     plt.xlabel(f'Difference in {outcome} from Status Quo')
-#     plt.title(f'Tornado Plot showing current and potenial impact of interventions on {outcome}')
-#     plt.legend()
-#     plt.savefig(f'{g_path}/{outcome}_tornado.png', bbox_inches='tight')
-#     plt.show(bbox_inches='tight')
+get_diff_plots(mat_deaths, 'Maternal Deaths (crude)')
+get_diff_plots(neo_deaths, 'Neonatal Deaths (crude)')
+
+get_diff_plots(mat_deaths_2, 'MMR (demog log)')
+get_diff_plots(neo_deaths_2, 'NMR (demog log)')
+
+get_diff_plots(mat_dalys_diffs, 'Maternal DALYs')
+get_diff_plots(neo_dalys_diffs, 'neonatal DALYs')
 
 
 def get_tornado_plot(data, outcome):
@@ -373,8 +359,8 @@ def get_tornado_plot(data, outcome):
     plt.figure(figsize=(10, 6))
     y_positions = np.arange(len(categories))
 
-    # bars_min = plt.barh(y_positions, min_values, color='lightcoral', edgecolor='black', alpha=0.7, label='Min Effect')
-    # bars_max = plt.barh(y_positions, max_values, color='skyblue', edgecolor='black', alpha=0.7, label='Max Effect')
+    bars_min = plt.barh(y_positions, min_values, color='lightcoral', edgecolor='black', alpha=0.7, label='Min Effect')
+    bars_max = plt.barh(y_positions, max_values, color='skyblue', edgecolor='black', alpha=0.7, label='Max Effect')
 
     # Add error bars for uncertainty intervals
     plt.errorbar(min_values, y_positions, xerr=min_errors, fmt='none', ecolor='darkred', capsize=5, alpha=0.9,
@@ -396,39 +382,7 @@ def get_tornado_plot(data, outcome):
 
 
 get_tornado_plot(mat_deaths_2, 'MMR')
-get_tornado_plot(dalys_diffs, 'DALYs')
+get_tornado_plot(neo_deaths_2, 'NMR')
 
-
-
-# # NORMALITY OF MMR ESTIMATES ACROSS RUNS (NOT DIFFERENCES)
-# for draw in draws:
-#     data = results['deaths_and_stillbirths']['crude'].loc['direct_mmr', draw].values
-#
-#     # Importing Shapiro-Wilk test for normality
-#     # Conducting Shapiro-Wilk test
-#     stat, p_value = shapiro(data)
-#     # Plotting histogram
-#     plt.hist(data, bins=15, density=True, alpha=0.6, color='skyblue', edgecolor='black')
-#
-#     # Overlay normal distribution (optional)
-#     mean, std = np.mean(data), np.std(data)
-#     xmin, xmax = plt.xlim()
-#     x = np.linspace(xmin, xmax, 100)
-#     p = norm.pdf(x, mean, std)
-#     plt.axvline(mean, color='green', linestyle='-', linewidth=2, label='Data Mean')
-#     plt.plot(x, p, 'r--', linewidth=2, label='Normal Curve')
-#
-#     # Adding labels and legend
-#     plt.title(f'MMR data Histogram with Normality Test (p-value = {p_value:.4f}) (Draw {draw})')
-#     plt.xlabel('Value')
-#     plt.ylabel('Density')
-#     plt.legend()
-#     # Show plot
-#     plt.show()
-#     # Printing Shapiro-Wilk test results
-#     print(f"Shapiro-Wilk Test Statistic: {stat:.4f}, p-value: {p_value:.4f}")
-#     if p_value > 0.05:
-#         print("Result: Data likely follows a normal distribution (p > 0.05).")
-#     else:
-#         print("Result: Data likely does not follow a normal distribution (p ≤ 0.05).")
-
+get_tornado_plot(mat_dalys_diffs, 'Maternal DALYs')
+get_tornado_plot(neo_dalys_diffs, 'Neonatal DALYs')
