d573aba76ffa48e4e6cbe928ed1b8b2455c3ec77
joehcollins <joseph.h.collins@ucl.ac.uk>
2025-05-06 13:24:33 +0100

    fix to error in contraception.pyn

--- src/tlo/methods/contraception.py
+++ src/tlo/methods/contraception.py
@@ -1320,7 +1320,7 @@ class StartInterventions(Event, PopulationScopeEventMixin):
         # Update module parameters to enable interventions
         if 'fp' in self.sim.modules['ServiceIntegration'].parameters['serv_int_screening']:
             after_birth = False
-        if self.sim.modules['ServiceIntegration'].parameters['serv_int_chronic']:
+        if 'fp' in self.sim.modules['ServiceIntegration'].parameters['serv_int_mch']:
             initiation = False
 
         self.module.processed_params = self.module.update_params_for_interventions(initiation=initiation,
