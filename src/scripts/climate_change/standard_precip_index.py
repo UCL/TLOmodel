23ef1089abe5feeb1458484756ac0b5176e240e1
RachelMurray-Watson <rmurwat@gmail.com>
2025-05-08 10:52:40 +0100

    Dropped final 3 rows as  # havent yet gotten data for oct/nov/dec 2024

--- src/scripts/climate_change/standard_precip_index.py
+++ src/scripts/climate_change/standard_precip_index.py
@@ -26,15 +26,21 @@ for facility in rainfall_data.columns[0:len(rainfall_data.columns) -1]: #unsure 
         'date',
         'precip',
         freq="M",
-        scale=3,
+        scale=1,
         fit_type="lmom",
         dist_type="gam"
     )
-    spi_time_facility.loc[:,facility] = df_spi.loc[:,'precip_scale_3_calculated_index']
-    spi_time_facility.rename(columns={'precip_scale_3_calculated_index':facility}, inplace=True)
+    spi_time_facility.loc[:,facility] = df_spi.loc[:,'precip_calculated_index']
+    spi_time_facility.rename(columns={'precip_calculated_index':facility}, inplace=True)
 
+# make sure 'date' is in datetime format
+spi_time_facility['date'] = pd.to_datetime(spi_time_facility['date'])
+
+spi_time_facility = spi_time_facility[
+    (spi_time_facility['date'].dt.year >= 2010) &
+    (spi_time_facility['date'].dt.year <= 2024)
+]
 spi_time_facility.drop(columns='date', inplace=True)
+spi_time_facility = spi_time_facility.iloc[:-3] # havent yet gotten data for oct/nov/dec 2024
 
-
-
-weather_data_monthly_original = spi_time_facility.to_csv(f"/Users/rem76/Desktop/Climate_change_health/Data/Drought_data/historical_drought_data_2010_2024.csv")
+spi_time_facility.to_csv(f"/Users/rem76/Desktop/Climate_change_health/Data/Drought_data/historical_drought_data_2010_2024.csv")
