09130584b262f1b441ded6eab1b9800d74424ee4
RachelMurray-Watson <rmurwat@gmail.com>
2025-05-23 12:44:02 +0100

    updated to larger population sizes

--- src/scripts/climate_disruptions/climate_disruption_scenarios.py
+++ src/scripts/climate_disruptions/climate_disruption_scenarios.py
@@ -12,9 +12,9 @@ class ClimateDisruptionScenario(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2030, 1, 12)
-        self.pop_size = 10000
-        self.runs_per_draw = 2
+        self.end_date = Date(2040, 1, 12)
+        self.pop_size = 50000
+        self.runs_per_draw = 10
         self.YEAR_OF_CHANGE = 2020
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
