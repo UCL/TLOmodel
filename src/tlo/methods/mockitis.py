0de905e0cc8a1e7b15d83ed90d8c274059614811
RachelMurray-Watson <rmurwat@gmail.com>
2025-05-29 09:48:16 +0100

    Added age and sex for mockitis

--- src/tlo/methods/mockitis.py
+++ src/tlo/methods/mockitis.py
@@ -297,9 +297,33 @@ class Mockitis(Module, GenericFirstAppointmentsMixin):
         logger.debug(key='debug', data='This is mockitis reporting my prevalence ')
 
         df = self.sim.population.props  # shortcut to population properties dataframe
-        total_prev = df.loc[df.is_alive, 'mi_is_infected'].sum() / len(df[df['is_alive']])
-        print(total_prev)
-        return {'Mockitis': total_prev}
+
+        # Select alive individuals with Mockitis infection
+        mockitis_df = df[(df['is_alive']) & (df['mi_is_infected'])]
+
+        prevalence_by_age_group_sex = {}
+
+        if mockitis_df.empty:
+            pass
+        else:
+            age_groups = {f'{start}-{start + 4}': (start, start + 4) for start in range(0, 100, 5)}
+            sexes = ['male', 'female']
+            total_alive = len(df[df['is_alive']])
+
+            for age_group in age_groups:
+                age_range = age_groups[age_group]
+                prevalence_by_age_group_sex[age_group] = {}
+
+                for sex in sexes:
+                    subset = mockitis_df[
+                        (mockitis_df['age_years'].between(age_range[0], age_range[1])) &
+                        (mockitis_df['sex'] == sex)
+                        ]
+
+                    total_prev = len(subset) / total_alive if total_alive > 0 else float('nan')
+                    prevalence_by_age_group_sex[age_group][sex] = total_prev
+
+        return {'Mockitis': prevalence_by_age_group_sex}
 
     def do_at_generic_first_appt_emergency(
         self,
