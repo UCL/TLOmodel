6ba13e179468d5a664d3e4475f3cd113351970b0
Eva Janouskova <janouskova.eva.012@gmail.com>
2025-05-30 16:18:10 +0100

    run_interv_analysis_wast & analysis_utility_fncs_wast: extract births and deaths data

--- src/scripts/wasting_analyses/analysis_utility_functions_wast.py
+++ src/scripts/wasting_analyses/analysis_utility_functions_wast.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import scipy.stats as st
 from matplotlib import pyplot as plt
 
@@ -9,325 +10,118 @@ plt.style.use('seaborn-darkgrid')
 """This file contains functions used for wasting analyses to extract data, derive outcomes and generate plots.
 (It is based on analysis_utility_function prepared by Joe for maternal_perinatal_analyses.)"""
 
-def extract_death_data_frames_and_outcomes(folder, birth_df):
-    print(f"{folder=}")
-    # All deaths occurring during the first 28 days of life are extracted
+def return_mean_95_CI_across_runs(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns a DataFrame with mean, lower CI, and upper CI for each year and each draw across runs.
+    The output DataFrame is structured with row index ['year'] and column index ['draw'], where each cell contains
+    a list of [mean, lower_ci, upper_ci].
+    """
+    result = pd.DataFrame(index=df.index, columns=df.columns.get_level_values('draw').unique())
 
-    # neonatal_mortality = extract_results(
-    #     folder,
-    #     module="tlo.methods.demography.detail",
-    #     key="properties_of_deceased_persons",
-    #     custom_generate_series=(
-    #         lambda df: df.loc[(df['age_days'] < 29)].assign(
-    #             year=df['date'].dt.year).groupby(['year'])['year'].count()),
-    #     do_scaling=True)
-    # neo_deaths = nd.fillna(0)
-    # neo_deaths_int = neo_deaths.loc[intervention_years[0]: intervention_years[-1]]
-    # return
+    for year in df.index:
+        row = df.loc[year]
+        ci = row.groupby(level='draw').apply(
+            lambda x: st.t.interval(0.95, len(x) - 1, loc=np.mean(x), scale=st.sem(x))
+        )
+        result.loc[year] = row.groupby(level='draw').mean().combine(
+            ci, lambda mean, ci_interval: [mean, ci_interval[0], ci_interval[1]]
+        )
 
-################################################### ABOVE my functions #################################################
-
-################################################### BELOW Joe's functions ###############################################
-
-
-def return_95_CI_across_runs(df, sim_years):
-    """Returns a list of lists from an outcome DF containing the mean and 95%CI values for that outcome over time.
-    The first list containins the mean value of a given outcome per year across runs, the second contains the smaller
-     value of the 95% CI and the third list contains the larger value of the 95% Ci"""
-
-    year_means = list()
-    lower_CI = list()
-    upper_CI = list()
-
-    for year in sim_years:
-        if year in df.index:
-            row = df.loc[year]
-            year_means.append(row.mean())
-            ci = st.t.interval(0.95, len(row) - 1, loc=np.mean(row), scale=st.sem(row))
-            lower_CI.append(ci[0])
-            upper_CI.append(ci[1])
-        else:
-            year_means.append(0)
-            lower_CI.append(0)
-            upper_CI.append(0)
-
-    return [year_means, lower_CI, upper_CI]
-
-
-def get_mean_95_CI_from_list(list_item):
-    """Returns the mean and 95% CI of data in a provided list"""
-    ci = st.t.interval(0.95, len(list_item) - 1, loc=np.mean(list_item), scale=st.sem(list_item))
-    result = [np.mean(list_item), ci[0], ci[1]]
     return result
 
+def extract_birth_data_frames_and_outcomes(folder, years_of_interest, intervention_years) -> dict:
+    """
+    :param folder: the folder from which the outcome data will be extracted
+    :param years_of_interest: years for which we want to extract the data
+    :param intervention_years: out of the years of interest, during which years was intervention implemented (if any)
+    :return: dictionary with list of lists with means, lower_CI, and upper_CI for all years of interest of
+        (1) total_births, (2) interv_births; and a dataframe (3) with births counts for years of interests as simulated
+        for draws and runs.
+    """
 
-def get_mean_from_columns(df, function):
-    """Returns mean value for each column in a provided data frame"""
-    values = list()
-    for col in df:
-        if function == 'avg':
-            values.append(np.mean(df[col]))
-        else:
-            values.append(sum(df[col]))
-    return values
-
-
-def line_graph_with_ci_and_target_rate(sim_years, data, target_data_dict, ylim, y_label, title,
-                                       graph_location, file_name):
-    """Outputs and saves line plot of an outcome over time, including uncertainty, in addition to a pre-determined
-    calibration target"""
-    fig, ax = plt.subplots()
-    ax.plot(sim_years, data[0], label="Model", color='deepskyblue')
-    ax.fill_between(sim_years, data[1], data[2], label="95% CI", color='b', alpha=.1)
-
-    if target_data_dict['double']:
-        plt.errorbar(target_data_dict['first']['year'], target_data_dict['first']['value'],
-                     label=target_data_dict['first']['label'], yerr=target_data_dict['first']['ci'],
-                     fmt='o', color='darkseagreen', ecolor='green', elinewidth=3, capsize=0)
-        plt.errorbar(target_data_dict['second']['year'], target_data_dict['second']['value'],
-                     label=target_data_dict['second']['label'], yerr=target_data_dict['second']['ci'],
-                     fmt='o', color='darkseagreen', ecolor='green', elinewidth=3, capsize=0)
-
-    elif not target_data_dict['double']:
-        plt.errorbar(target_data_dict['first']['year'], target_data_dict['first']['value'],
-                     label=target_data_dict['first']['label'], yerr=target_data_dict['first']['ci'],
-                     fmt='o', color='darkseagreen', ecolor='green', elinewidth=3, capsize=0)
-
-    plt.xlabel('Year')
-    plt.ylabel(y_label)
-    plt.title(title)
-    ax.set(ylim=(0, ylim))
-    plt.xticks(sim_years, labels=sim_years, rotation=45, fontsize=8)
-    plt.gca().set_ylim(bottom=0)
-    plt.legend()
-    plt.savefig(f'{graph_location}/{file_name}.png')
-    plt.show()
-
-
-def simple_line_chart_with_target(sim_years, model_rate, target_rate, y_title, title, file_name, graph_location):
-    """Outputs and saves line plot of an outcome over time in addition to a pre-determined
-        calibration target"""
-    plt.plot(sim_years, model_rate, 'o-g', label="Model", color='deepskyblue')
-    plt.plot(sim_years, target_rate, 'o-g', label="Target rate", color='darkseagreen')
-    plt.ylabel(y_title)
-    plt.xlabel('Year')
-    plt.title(title)
-    plt.gca().set_ylim(bottom=0)
-    plt.legend()
-    plt.savefig(f'{graph_location}/{file_name}.png')
-    plt.show()
-
-
-def simple_line_chart_with_ci(sim_years, data, y_title, title, file_name, graph_location):
-    """Outputs and saves line plot of an outcome over time, including uncertainty"""
-    fig, ax = plt.subplots()
-    ax.plot(sim_years, data[0], label="Model (mean)", color='deepskyblue')
-    ax.fill_between(sim_years, data[1], data[2], color='b', alpha=.1, label="95% CI")
-    plt.ylabel(y_title)
-    plt.xlabel('Year')
-    plt.title(title)
-    plt.legend()
-    plt.gca().set_ylim(bottom=0)
-    plt.grid(True)
-    plt.savefig(f'{graph_location}/{file_name}.png')
-    plt.show()
-
-
-def simple_bar_chart(model_rates, x_title, y_title, title, file_name, sim_years, graph_location):
-    """Outputs a simple bar chart over time"""
-    bars = sim_years
-    x_pos = np.arange(len(bars))
-    plt.bar(x_pos, model_rates, label="Model", color='thistle')
-    plt.xticks(x_pos, bars, rotation=90)
-    plt.xlabel(x_title)
-    plt.ylabel(y_title)
-    plt.title(title)
-    plt.legend()
-    plt.savefig(f'{graph_location}/{file_name}.png')
-    plt.show()
-
-
-def comparison_graph_multiple_scenarios_multi_level_dict(colours, intervention_years, data_dict, key, y_label, title,
-                                                         graph_location, save_name):
-    """Outputs and saves line plot of an outcome over time, including uncertainty, in addition to a pre-determined
-        calibration target. Data from the model is provided as a dictionary with multiple entries"""
-    fig, ax = plt.subplots()
-
-    for k, colour in zip(data_dict, colours):
-        ax.plot(intervention_years, data_dict[k][key][0], label=k, color=colour)
-        ax.fill_between(intervention_years, data_dict[k][key][1], data_dict[k][key][2], color=colour, alpha=.1)
-
-    plt.ylabel(y_label)
-    plt.xlabel('Year')
-    plt.title(title)
-
-    if 'nmr' in key:
-        plt.gca().set_ylim(bottom=0, top=25)
-    elif 'sbr' in key:
-        plt.gca().set_ylim(bottom=0, top=20)
-    else:
-        plt.gca().set_ylim(bottom=0)
-
-    plt.legend()
-    plt.xticks(intervention_years, labels=intervention_years, rotation=45, fontsize=8)
-    plt.savefig(f'./{graph_location}/{save_name}.png', bbox_inches='tight')
-    plt.show()
-
-
-def comparison_bar_chart_multiple_bars(data, dict_name, intervention_years, colours, y_title, title,
-                                       plot_destination_folder, save_name):
-    """Outputs a barchart comparing two data sources"""
-
-    N = len(intervention_years)
-    ind = np.arange(N)
-    if len(data.keys()) > 3:
-        width = 0.15
-    else:
-        width = 0.2
-
-    x_ticks = list()
-    for x in range(len(intervention_years)):
-        x_ticks.append(x)
-
-    for k, position, colour in zip(data, [ind - width, ind, ind + width, ind + width * 2, ind + width * 3],
-                                   colours):
-        ci = [(x - y) / 2 for x, y in zip(data[k][dict_name][2], data[k][dict_name][1])]
-        plt.bar(position, data[k][dict_name][0], width, label=k, yerr=ci, color=colour)
-
-    plt.ylabel(y_title)
-    plt.xlabel('Years')
-    plt.title(title)
-    plt.legend(loc='best')
-    plt.xticks(x_ticks, labels=intervention_years)
-    plt.savefig(f'{plot_destination_folder}/{save_name}.png')
-    plt.show()
-
-
-# =========================== FUNCTIONS RETURNING DATA FROM MULTIPLE SCENARIOS =======================================
-def get_modules_maternal_complication_dataframes(results_folder):
-    """Returns a dataframe from a scenario file which contains the number of maternal complications by type per year
-    for a given python script"""
-    comp_dfs = dict()
-
-    for module in ['pregnancy_supervisor', 'labour', 'postnatal_supervisor']:
-        c_df = extract_results(
-            results_folder,
-            module=f"tlo.methods.{module}",
-            key="maternal_complication",
-            custom_generate_series=(
-                lambda df_: df_.assign(year=df_['date'].dt.year).groupby(['year', 'type'])['person'].count()),
-            do_scaling=True
-        )
-        complications_df = c_df.fillna(0)
-
-        comp_dfs[module] = complications_df
-
-    return comp_dfs
-
-
-def get_modules_neonatal_complication_dataframes(results_folder):
-    """Returns a dataframe from a scenario file which contains the number of neonatal complications by type per year
-        for a given python script"""
-    comp_dfs = dict()
-
-    for module in ['newborn_outcomes', 'postnatal_supervisor']:
-        n_df = extract_results(
-            results_folder,
-            module=f"tlo.methods.{module}",
-            key="newborn_complication",
-            custom_generate_series=(
-                lambda df_: df_.assign(year=df_['date'].dt.year).groupby(['year', 'type'])['newborn'].count()),
-            do_scaling=True
-        )
-        complications_df = n_df.fillna(0)
-
-        comp_dfs[module] = complications_df
-
-    return comp_dfs
-
-
-def return_birth_data_from_multiple_scenarios(results_folders, sim_years, intervention_years):
-    """ Extracts data relating to births from a series of pre-specified scenario files"""
-
-    def extract_births(folder):
-        br = extract_results(
-            folder,
-            module="tlo.methods.demography",
-            key="on_birth",
-            custom_generate_series=(
-                lambda df: df.assign(
-                    year=df['date'].dt.year).groupby(['year'])['year'].count()),
-            do_scaling=True
-        )
-        births_results = br.fillna(0)
-        total_births_per_year = return_95_CI_across_runs(births_results, sim_years)
-
-        int_df = births_results.loc[intervention_years[0]: intervention_years[-1]]
-        int_births_per_year = return_95_CI_across_runs(int_df, intervention_years)
-
-        agg_births_data = get_mean_from_columns(births_results.loc[intervention_years[0]: intervention_years[-1]],
-                                                'agg')
-        agg_births = get_mean_95_CI_from_list(agg_births_data)
-
-        return {'total_births': total_births_per_year,
-                'int_births': int_births_per_year,
-                'agg_births': agg_births,
-                'births_data_frame': births_results}
-
-    return {k: extract_births(results_folders[k]) for k in results_folders}
-
-
-def return_pregnancy_data_from_multiple_scenarios(results_folders, sim_years, intervention_years):
-    """ Extracts data relating to pregnancies from a series of pre-specified scenario files"""
-
-    def extract_pregnancies(folder):
-        pr = extract_results(
-            folder,
-            module="tlo.methods.contraception",
-            key="pregnancy",
-            custom_generate_series=(
-                lambda df: df.assign(year=df['date'].dt.year).groupby(['year'])['year'].count()),
-            do_scaling=True
-        )
-        preg_results = pr.fillna(0)
-        total_pregnancies_per_year = return_95_CI_across_runs(preg_results, sim_years)
-        total_pregnancies_per_year_int = return_95_CI_across_runs(
-             preg_results.loc[intervention_years[0]: intervention_years[-1]], intervention_years)
-
-        agg_preg_data = get_mean_from_columns(preg_results.loc[intervention_years[0]: intervention_years[-1]], 'agg')
-        agg_preg = get_mean_95_CI_from_list(agg_preg_data)
-
-        return {'total_preg': total_pregnancies_per_year,
-                'int_preg': total_pregnancies_per_year_int,
-                'agg_preg': agg_preg,
-                'preg_data_frame': preg_results}
-
-    return {k: extract_pregnancies(results_folders[k]) for k in results_folders}
-
-
-def get_completed_pregnancies_from_multiple_scenarios(comps_df, birth_dict, results_folder):
-    """Sums the number of pregnancies that have ended in a given year including ectopic pregnancies,
-    abortions, stillbirths and births"""
-
-    ansb_df = extract_results(
-        results_folder,
-        module="tlo.methods.pregnancy_supervisor",
-        key="antenatal_stillbirth",
+    births_df = extract_results(
+        folder,
+        module="tlo.methods.demography",
+        key="on_birth",
         custom_generate_series=(
             lambda df: df.assign(
                 year=df['date'].dt.year).groupby(['year'])['year'].count()),
         do_scaling=True
-    )
+    ).fillna(0)
+    births_df = births_df.loc[years_of_interest[0]:years_of_interest[-1]]
 
-    eu = comps_df['pregnancy_supervisor'].loc[(slice(None), 'ectopic_unruptured'), slice(None)]
-    eu_f = eu.droplevel(1)
+    births_mean_ci_per_year_per_draw_df = return_mean_95_CI_across_runs(births_df)
 
-    ia = comps_df['pregnancy_supervisor'].loc[(slice(None), 'induced_abortion'), slice(None)]
-    ia_f = ia.droplevel(1)
+    interv_births_df = births_df.loc[intervention_years[0]:intervention_years[-1]]
+    interv_births_per_year_per_draw_df = return_mean_95_CI_across_runs(interv_births_df)
 
-    sa = comps_df['pregnancy_supervisor'].loc[(slice(None), 'spontaneous_abortion'), slice(None)]
-    sa_f = sa.droplevel(1)
+    return {'births_df': births_df,
+            'births_mean_ci_df': births_mean_ci_per_year_per_draw_df,
+            'interv_births_df': interv_births_df, # TODO: check when and how is this used, is it really worth to return this?
+            'interv_births_mean_ci_df': interv_births_per_year_per_draw_df}
 
-    comp_preg_data_frame = eu_f + ia_f + sa_f + ansb_df + birth_dict['births_data_frame']
+def extract_death_data_frames_and_outcomes(folder, births_df, years_of_interest, intervention_years):
+    # ### NEONATAL MORTALITY
+    # Extract all deaths occurring during the first 28 days of life
+    neonatal_deaths_df = extract_results(
+        folder,
+        module="tlo.methods.demography.detail",
+        key="properties_of_deceased_persons",
+        custom_generate_series=(
+            lambda df: df.loc[(df['age_days'] < 29)].assign(
+                year=df['date'].dt.year).groupby(['year'])['year'].count()),
+        do_scaling=True).fillna(0)
+    neonatal_deaths_df = neonatal_deaths_df.loc[years_of_interest[0]:years_of_interest[-1]]
 
-    return {'comp_preg_data_frame': comp_preg_data_frame}
+    neo_deaths_mean_ci_per_year_per_draw_df = return_mean_95_CI_across_runs(neonatal_deaths_df)
+
+    interv_neo_deaths_df = neonatal_deaths_df.loc[intervention_years[0]:intervention_years[-1]]
+    interv_neo_deaths_per_year_per_draw_df = return_mean_95_CI_across_runs(interv_neo_deaths_df)
+
+    # # TODO: rm prints when no longer needed
+    # print("\nYears, and (Draws, Runs) with no neonatal death:")
+    # no_neo_deaths = [(neonatal_deaths.index[row], neonatal_deaths.columns[col]) for row, col in
+    #                  zip(*np.where(neonatal_deaths == 0.0))]
+    # print(f"{no_neo_deaths}")
+    # #
+
+    # ### UNDER-5 MORTALITY
+    # Extract all deaths occurring during the first 5 years of life
+    under5_deaths_df = extract_results(
+        folder,
+        module="tlo.methods.demography.detail",
+        key="properties_of_deceased_persons",
+        custom_generate_series=(
+            lambda df: df.loc[(df['age_exact_years'] < 5)].assign(
+                year=df['date'].dt.year).groupby(['year'])['year'].count()),
+        do_scaling=True).fillna(0)
+    under5_deaths_df = under5_deaths_df.loc[years_of_interest[0]:years_of_interest[-1]]
+
+    under5_deaths_mean_ci_per_year_per_draw_df = return_mean_95_CI_across_runs(under5_deaths_df)
+
+    interv_under5_deaths_df = under5_deaths_df.loc[intervention_years[0]:intervention_years[-1]]
+    interv_under5_deaths_per_year_per_draw_df = return_mean_95_CI_across_runs(interv_under5_deaths_df)
+
+    # NEONATAL MORTALITY RATE (NMR), i.e. the number of deaths of infants under 28 days old per 1,000 live births
+    nmr_df = (neonatal_deaths_df / births_df) * 1000
+    nmr_per_year_per_draw_df = return_mean_95_CI_across_runs(nmr_df)
+
+    return {'neo_deaths_df': neonatal_deaths_df,
+            'neo_deaths_mean_ci_df': neo_deaths_mean_ci_per_year_per_draw_df,
+            'interv_neo_deaths_df': interv_neo_deaths_df, # TODO: check when and how is this used, is it really worth to return this?
+            'interv_neo_deaths_mean_ci_df': interv_neo_deaths_per_year_per_draw_df,
+            'under5_deaths_df': under5_deaths_df,
+            'under5_deaths_mean_ci_df': under5_deaths_mean_ci_per_year_per_draw_df,
+            'interv_under5_deaths_df': interv_under5_deaths_df,
+            'interv_under5_deaths_mean_ci_df': interv_under5_deaths_per_year_per_draw_df,
+            'neonatal_mort_rate_df': nmr_df,
+            'neo_mort_rate_mean_ci_df': nmr_per_year_per_draw_df}
+
+    # # TODO: rm prints when no longer needed
+    # print("\nYears, and (Draws, Runs) with no under 5 death:")
+    # no_under5_deaths = [(under5_deaths.index[row], under5_deaths.columns[col]) for row, col in
+    #                  zip(*np.where(under5_deaths == 0.0))]
+    # print(f"{no_under5_deaths}")
+    # #
+
+    # def plot_neonatal_mortality_multiple_scenario():
