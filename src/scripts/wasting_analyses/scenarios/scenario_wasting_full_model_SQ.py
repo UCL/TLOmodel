1e803c163fd30dcca70cdd9b67d41b482fcba3fe
Eva Janouskova <janouskova.eva.012@gmail.com>
2025-06-04 14:26:42 +0100

    scenario_wast_full_model(SQ|GM): new (to log deaths from all modules)

---
+++ src/scripts/wasting_analyses/scenarios/scenario_wasting_full_model_SQ.py
@@ -0,0 +1,59 @@
+"""
+This file defines a scenario for wasting analysis.
+
+It can be submitted on Azure Batch by running:
+
+    tlo batch-submit src/scripts/wasting_analyses/scenarios/scenario_wasting_full_model_SQ.py
+
+or locally using:
+
+    tlo scenario-run src/scripts/wasting_analyses/scenarios/scenario_wasting_full_model_SQ.py
+"""
+# import itertools
+import warnings
+
+from tlo import Date, logging
+from tlo.analysis.utils import get_parameters_for_status_quo
+from tlo.methods.fullmodel import fullmodel
+from tlo.scenario import BaseScenario
+
+# capture warnings during simulation run
+warnings.simplefilter('default', (UserWarning, RuntimeWarning))
+
+
+class WastingAnalysis(BaseScenario):
+
+    def __init__(self):
+        super().__init__(
+            seed=0,
+            start_date=Date(year=2010, month=1, day=1),
+            end_date=Date(year=2031, month=1, day=1),
+            initial_population_size=4_000,
+            number_of_draws=1,
+            runs_per_draw=10,
+        )
+
+    def log_configuration(self):
+        return {
+            'filename': 'wasting_analysis__minimal_model',
+            'directory': './outputs/wasting_analysis',
+            "custom_levels": {  # Customise the output of specific loggers
+                "tlo.methods.demography": logging.INFO,
+                "tlo.methods.population": logging.INFO,
+                "tlo.methods.wasting": logging.INFO,
+                '*': logging.WARNING
+            }
+        }
+
+    def modules(self):
+        return fullmodel(resourcefilepath=self.resources,
+                         module_kwargs=get_parameters_for_status_quo())
+
+    # Keeping Status Quo parameters
+    def draw_parameters(self, draw_number, rng):
+        return
+
+if __name__ == '__main__':
+    from tlo.cli import scenario_run
+
+    scenario_run([__file__])
