bbcd6367467b6b13d41345dc0a91faaa2de6f701
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2025-06-04 16:42:25 +0100

    iterate through models in reverse order to prevent people advancing through all stages

--- src/tlo/methods/cervical_cancer.py
+++ src/tlo/methods/cervical_cancer.py
@@ -849,7 +849,7 @@ class CervicalCancerMainPollingEvent(RegularEvent, PopulationScopeEventMixin):
         # determine if the person had a treatment during this stage of cancer (nb. treatment only has an effect on
         #  reducing progression risk during the stage at which is received.
 
-        for stage, lm in self.module.linear_models_for_progression_of_hpv_cc_status.items():
+        for stage, lm in reversed(list(self.module.linear_models_for_progression_of_hpv_cc_status.items())):
             gets_new_stage = lm.predict(df.loc[df.is_alive], rng)
 
             idx_gets_new_stage = gets_new_stage[gets_new_stage].index
