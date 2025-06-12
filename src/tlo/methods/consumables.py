ed81710903c681df33fe23c50b0caca8ade03612
Tim Hallett <39991060+tbhallett@users.noreply.github.com>
2025-06-12 18:35:59 +0300

    allow new scenarios for consumables (defined in differnet columns in the input data) to be picked up when changing consumables.availability.

--- src/tlo/methods/consumables.py
+++ src/tlo/methods/consumables.py
@@ -100,7 +100,10 @@ class Consumables:
         overriding the availability of specific consumables."""
 
         # Load the original read-in data (create copy so that edits do change the original)
-        self._prob_item_codes_available = self._processed_consumables_data.copy()
+        _, self._prob_item_codes_available = self._process_consumables_data(
+            availability_data=self._availability_data,
+            availability=availability
+        )
 
         # Load designations of the consumables
         item_code_designations = self._item_code_designations
@@ -111,7 +114,7 @@ class Consumables:
                             'scenario5', 'scenario6', 'scenario7', 'scenario8',
                             'scenario9', 'scenario10', 'scenario11', 'scenario12',
                             'scenario13', 'scenario14', 'scenario15'):
-            pass
+            pass  # change already picked up in `self._process_consumables_data()`
         elif availability == 'all':
             self.override_availability(dict(zip(self.item_codes, repeat(1.0))))
         elif availability == 'none':
@@ -148,6 +151,8 @@ class Consumables:
         Returns: (i) the set of all recognised item_codes; (ii) pd.Series of the availability of
         each consumable at each facility_id during each month.
         """
+        assert availability is not None, "This argument cannot be None."
+
         if availability in ('scenario1', 'scenario2', 'scenario3', 'scenario4',
                               'scenario5', 'scenario6', 'scenario7', 'scenario8',
                             'scenario9', 'scenario10', 'scenario11', 'scenario12',
