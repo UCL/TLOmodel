6a87da3a26139945ad8f727afc9700daaa34d036
tdm32 <t.mangal@imperial.ac.uk>
2025-07-23 14:02:34 +0100

    set up scenarios

--- src/scripts/hiv/program_simplification/analysis_hiv_program_simplification.py
+++ src/scripts/hiv/program_simplification/analysis_hiv_program_simplification.py
@@ -41,7 +41,7 @@ class HIV_Progam_Elements(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2035, 1, 1)
-        self.pop_size = 30_000
+        self.pop_size = 50_000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 1
