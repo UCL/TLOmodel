46744d3799b36070e76e021609a42c160c37dfca
Asif Tamuri <tamuri@gmail.com>
2025-08-01 22:56:57 +0100

    publish: Add New Paper to Publications (#1648)
    generated from commit ad00864bc34001425ea70dad56e409163056a029

---
+++ _sources/resources/healthsystem/consumables/README.md.rst.txt
@@ -0,0 +1,5 @@
+README (.md)
+============
+
+:download:`Download original .md file from GitHub <https://github.com/UCL/TLOmodel/raw/master/resources/healthsystem/consumables/README.md>`
+
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev2530+geb94b70',
+    VERSION: '0.1.dev2543+gad00864.d20250801',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
