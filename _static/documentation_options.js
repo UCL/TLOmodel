1be694bbc37045f40cfd60f2ac10a0d87c612abe
Asif Tamuri <tamuri@gmail.com>
2025-09-02 21:39:51 +0100

    publish: Update pubs - Chalkey et al and Molaro et al (#1686)
    generated from commit 49029e4e54136b381d14829d19c8a3094e0c7214

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev2543+gad00864.d20250801',
+    VERSION: '0.1.dev2548+g49029e4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
