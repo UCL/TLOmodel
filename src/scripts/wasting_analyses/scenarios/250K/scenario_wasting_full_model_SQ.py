bdb4b605110962cbc32180bb5fb22197e67040a1
Eva Janouskova <janouskova.eva.012@gmail.com>
2025-09-16 18:18:54 +0100

    [skip ci] scen_wast: add 250K scenarios

---
+++ src/scripts/wasting_analyses/scenarios/250K/scenario_wasting_full_model_SQ.py
@@ -0,0 +1,60 @@
+"""
+This file defines a scenario for wasting analysis.
+
+It can be submitted on Azure Batch by running:
+
+    tlo batch-submit src/scripts/wasting_analyses/scenarios/250K/scenario_wasting_full_model_SQ.py
+
+or locally using:
+
+    tlo scenario-run src/scripts/wasting_analyses/scenarios/250K/scenario_wasting_full_model_SQ.py
+"""
+# import itertools
+import warnings
+
+from tlo import Date, logging
+from tlo.analysis.utils import get_parameters_for_status_quo
+from tlo.methods.fullmodel import fullmodel
+from tlo.scenario import BaseScenario
+
+# capture warnings during simulation run
+warnings.simplefilter('default', (UserWarning, RuntimeWarning))
+
+
+class WastingAnalysis(BaseScenario):
+
+    def __init__(self):
+        super().__init__(
+            seed=0,
+            start_date=Date(year=2010, month=1, day=1),
+            end_date=Date(year=2031, month=1, day=2),
+            initial_population_size=250_000,
+            number_of_draws=1,
+            runs_per_draw=10,
+        )
+
+    def log_configuration(self):
+        return {
+            'filename': 'wasting_analysis__full_model_SQ',
+            'directory': './outputs/wasting_analysis',
+            "custom_levels": {  # Customise the output of specific loggers
+                "tlo.methods.demography": logging.INFO,
+                "tlo.methods.healthburden": logging.INFO,
+                'tlo.methods.healthsystem.summary': logging.INFO,
+                "tlo.methods.population": logging.INFO,
+                "tlo.methods.wasting": logging.DEBUG,
+                '*': logging.WARNING
+            }
+        }
+
+    def modules(self):
+        return fullmodel()
+
+    # Keeping Status Quo parameters
+    def draw_parameters(self, draw_number, rng):
+        return get_parameters_for_status_quo()
+
+if __name__ == '__main__':
+    from tlo.cli import scenario_run
+
+    scenario_run([__file__])
