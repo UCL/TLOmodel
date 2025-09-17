97c4d69d20e7fac4338d5bbfa848b754bf549f56
Eva Janouskova <janouskova.eva.012@gmail.com>
2025-09-17 11:40:01 +0100

    [skip ci] scen_wast: add 100K scenarios

---
+++ src/scripts/wasting_analyses/scenarios/100K/scenario_wasting_full_model_CS.py
@@ -0,0 +1,71 @@
+"""
+This file defines a scenario for wasting analysis.
+
+It can be submitted on Azure Batch by running:
+
+    tlo batch-submit src/scripts/wasting_analyses/scenarios/100K/scenario_wasting_full_model_CS.py
+
+or locally using:
+
+    tlo scenario-run src/scripts/wasting_analyses/scenarios/100K/scenario_wasting_full_model_CS.py
+"""
+# import itertools
+import warnings
+
+from tlo import Date, logging
+from tlo.analysis.utils import get_parameters_for_status_quo, mix_scenarios
+from tlo.methods.fullmodel import fullmodel
+from tlo.scenario import BaseScenario
+
+# capture warnings during simulation run
+warnings.simplefilter('default', (UserWarning, RuntimeWarning))
+
+
+class WastingAnalysis(BaseScenario):
+
+    def __init__(self):
+        super().__init__(
+            seed=0,
+            start_date=Date(year=2010, month=1, day=1),
+            end_date=Date(year=2031, month=1, day=2),
+            initial_population_size=100_000,
+            number_of_draws=1,
+            runs_per_draw=10,
+        )
+
+    def log_configuration(self):
+        return {
+            'filename': 'wasting_analysis__full_model_CS',
+            'directory': './outputs/wasting_analysis',
+            "custom_levels": {  # Customise the output of specific loggers
+                "tlo.methods.demography": logging.INFO,
+                "tlo.methods.healthburden": logging.INFO,
+                'tlo.methods.healthsystem.summary': logging.INFO,
+                "tlo.methods.population": logging.INFO,
+                "tlo.methods.wasting": logging.DEBUG,
+                '*': logging.WARNING
+            }
+        }
+
+    def modules(self):
+        return fullmodel()
+
+    # Scaling up care-seeking (CS) scenarios
+    def draw_parameters(self, draw_number, rng):
+        ### prob of symptoms awareness in MAM cases leading to care-seeking
+        # awareness_prob = [0.1, 0.3, 0.5, 1.0]
+        awareness_prob = [1.0, 1.0]
+
+        return mix_scenarios(
+            get_parameters_for_status_quo(),
+            {
+                'Wasting': {
+                    'interv_awareness_MAM_prob': awareness_prob[draw_number]
+                }
+            }
+        )
+
+if __name__ == '__main__':
+    from tlo.cli import scenario_run
+
+    scenario_run([__file__])
