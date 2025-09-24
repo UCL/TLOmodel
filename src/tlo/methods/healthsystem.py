00b8b97cc8ee3c354e59ce06cc19b6a813d1aa30
RachelMurray-Watson <rmurwat@gmail.com>
2025-09-24 11:33:44 +0100

    Restored actual checks etc for scenario file

--- src/tlo/methods/healthsystem.py
+++ src/tlo/methods/healthsystem.py
@@ -2532,7 +2532,7 @@ class HealthSystemScheduler(RegularEvent, PopulationScopeEventMixin):
                 climate_disrupted = False
 
                 # First, check for climate disruption
-                if year >= 2010: #2025 and self.module.parameters['services_affected_precip'] != 'none' and self.module.parameters['services_affected_precip'] is not None:
+                if year >= 2025 and self.module.parameters['services_affected_precip'] != 'none' and self.module.parameters['services_affected_precip'] is not None:
                     fac_level = item.hsi_event.facility_info.level
                     facility_used = self.sim.population.props.at[item.hsi_event.target, f'level_{fac_level}']
                     if facility_used in self.module.parameters['projected_precip_disruptions']['RealFacility_ID'].values:
@@ -2544,8 +2544,7 @@ class HealthSystemScheduler(RegularEvent, PopulationScopeEventMixin):
                             'disruption'
                         ]
                         prob_disruption = pd.DataFrame(prob_disruption)
-                        #prob_disruption = min(float(prob_disruption.iloc[0]) * self.module.parameters['rescaling_prob_disruption'], 1) # to account for some structural differences
-                        prob_disruption = 1
+                        prob_disruption = min(float(prob_disruption.iloc[0]) * self.module.parameters['rescaling_prob_disruption'], 1) # to account for some structural differences
                         if np.random.binomial(1, prob_disruption) == 1:
                             climate_disrupted = True
                             if self.sim.modules['HealthSeekingBehaviour'].force_any_symptom_to_lead_to_healthcareseeking:
