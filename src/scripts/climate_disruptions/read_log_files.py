f4df3893e975b395f4517be07019e1dc4990ef37
RachelMurray-Watson <rmurwat@gmail.com>
2025-10-09 14:38:30 +0100

    Create smaller log files from the one large log file

--- src/scripts/climate_disruptions/read_log_files.py
+++ src/scripts/climate_disruptions/read_log_files.py
@@ -1,13 +1,16 @@
 from tlo.analysis.utils import create_pickles_locally, parse_log_file
 import glob
 
+#
+# for draw in [0]:
+#     for run in range(5):
+#         log_pattern = f'/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/climate_scenario_runs-2025-09-26T144635Z/{draw}/{run}/climate_scenario_runs__2025-10-06*.log'
+#
+#         # Get all matching log files
+#         log_files = glob.glob(log_pattern)
+#
+#         for log_file in log_files:
+#             parse_log_file(log_file)
 
-for draw in [0]:
-    for run in range(5):
-        log_pattern = f'/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/climate_scenario_runs-2025-09-26T144635Z/{draw}/{run}/climate_scenario_runs__2025-10-06*.log'
-
-        # Get all matching log files
-        log_files = glob.glob(log_pattern)
-
-        for log_file in log_files:
-            parse_log_file(log_file)
+scenario_output_dir = '/Users/rem76/PycharmProjects/TLOmodel/outputs/rm916@ic.ac.uk/climate_scenario_runs-2025-09-26T144635Z'
+create_pickles_locally(scenario_output_dir = scenario_output_dir, compressed_file_name_prefix='climate_scenario_runs_')
