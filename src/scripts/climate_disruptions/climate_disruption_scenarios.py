ab79c54c1fa515d284c43bdc2761d0237533c031
RachelMurray-Watson <rmurwat@gmail.com>
2025-10-29 11:11:47 +0000

    Reset to correct parameters. It takes random draw from parameter grid, before was stepwise.

--- src/scripts/climate_disruptions/climate_disruption_scenarios.py
+++ src/scripts/climate_disruptions/climate_disruption_scenarios.py
@@ -23,7 +23,7 @@ class ClimateDisruptionScenario(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2011, 1, 12)
+        self.end_date = Date(2046, 1, 12)
         self.pop_size = 100_000
         self.runs_per_draw = 10
         self.YEAR_OF_CHANGE = 2020
