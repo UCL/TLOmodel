d12550496e89941e3b50c98bc92c5d45bd8ddf0b
RachelMurray-Watson <rmurwat@gmail.com>
2025-11-03 16:45:27 +0000

    For clarity deleted baseline
    Also added in other climate params that need to be updated in scenario as they were overwritten in parameter grid

--- src/scripts/climate_disruptions/climate_disruption_scenarios.py
+++ src/scripts/climate_disruptions/climate_disruption_scenarios.py
@@ -9,12 +9,26 @@ import numpy as np
 import random
 import json
 
+YEAR_OF_CHANGE = 2020
 full_grid = make_cartesian_parameter_grid({
     "HealthSystem": {
         "scale_factor_delay_in_seeking_care_weather": [float(x) for x in [0, 1, 2, 10, 20, 60]],
         "rescaling_prob_seeking_after_disruption": np.arange(0.01, 1.51, 0.5),
         "rescaling_prob_disruption": np.arange(0.0, 2.01, 0.5),
         "scale_factor_severity_disruption_and_delay": [float(x) for x in np.linspace(0.11, 1.0, 4)],
+        "mode_appt_constraints": [1],
+        "cons_availability": ["default"],
+        "cons_availability_postSwitch": ["all"],
+        "year_cons_availability_switch": [YEAR_OF_CHANGE],
+        "beds_availability": ["all"],
+        "equip_availability": ["default"],
+        "equip_availability_postSwitch": ["all"],
+        "year_equip_availability_switch": [YEAR_OF_CHANGE],
+        "use_funded_or_actual_staffing": ["funded_plus"],
+        "climate_ssp": ['ssp245'],
+        "climate_model_ensemble_model": ['mean'],
+        "services_affected_precip": ['all'],
+        "tclose_overwrite": [1000]
     }
 })
 
@@ -23,7 +37,7 @@ class ClimateDisruptionScenario(BaseScenario):
         super().__init__()
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2046, 1, 12)
+        self.end_date = Date(2041, 1, 12)
         self.pop_size = 100_000
         self.runs_per_draw = 10
         self.YEAR_OF_CHANGE = 2020
@@ -36,7 +50,6 @@ class ClimateDisruptionScenario(BaseScenario):
         #     }})
         self._parameter_grid = random.sample(full_grid, 9)
         print(self._parameter_grid)
-
         self.number_of_draws = len(self._parameter_grid)
 
         with open("selected_parameter_combinations.json", "w") as f:
@@ -68,48 +81,10 @@ class ClimateDisruptionScenario(BaseScenario):
 
     def _get_scenarios(self) -> Dict[str, Dict]:
         """Return the Dict with values for the parameters that are changed, keyed by a name for the scenario."""
-        return {#'Baseline': self._baseline(),
+        return {
                 'SSP 2.45 Mean': self._ssp245_mean(),
                 }
 
-    def _baseline(self) -> Dict:
-        """Return the Dict with values for the parameter changes that define the baseline scenario. """
-        return mix_scenarios(
-            get_parameters_for_status_quo(),
-            {'ImprovedHealthSystemAndCareSeekingScenarioSwitcher': {
-                'max_healthsystem_function': [False, True],
-                'max_healthcare_seeking': [False, False],
-                'year_of_switch': self.YEAR_OF_CHANGE
-                },
-             "HealthSystem": {
-                "mode_appt_constraints": 1,
-                "cons_availability": "default",
-                "cons_availability_postSwitch": "all",
-                "year_cons_availability_switch": self.YEAR_OF_CHANGE,
-                "beds_availability": "all",
-                "equip_availability": "default",
-                "equip_availability_postSwitch": "all",
-                "year_equip_availability_switch": self.YEAR_OF_CHANGE,
-                "use_funded_or_actual_staffing": "funded_plus",
-                 "climate_ssp": 'ssp245', #status quo
-                 "climate_model_ensemble_model": 'mean',
-                 "services_affected_precip": 'none', # ensures no disruption
-                 "tclose_overwrite": 1000 # days of t close overwrite
-                },
-                "Malaria": {
-                    'type_of_scaleup': 'max',
-                    'scaleup_start_year': self.YEAR_OF_CHANGE,
-                },
-                "Tb": {
-                        'type_of_scaleup': 'max',
-                        'scaleup_start_year': self.YEAR_OF_CHANGE,
-                    },
-                "Hiv": {
-                        'type_of_scaleup': 'max',
-                        'scaleup_start_year': self.YEAR_OF_CHANGE,
-                    }
-             },
-        )
     def _ssp245_mean(self) -> Dict:
         """Return the Dict with values for the parameter changes that define the baseline scenario. """
         return mix_scenarios(
