dc86116227c01ef447e00e430dc21089816afa38
RachelMurray-Watson <rmurwat@gmail.com>
2025-11-05 10:45:05 +0000

    SENSITIVITY ANALYSIS: uses guaranteed disruptions, and guaranteed reseeking healthcare
    if year >= 2010:
    p_disruption = 0
    will_seek_care = 0 (can't happen)
    
    changed to floats so will run

--- src/scripts/climate_disruptions/climate_disruption_scenarios.py
+++ src/scripts/climate_disruptions/climate_disruption_scenarios.py
@@ -12,10 +12,10 @@ import json
 YEAR_OF_CHANGE = 2020
 full_grid = make_cartesian_parameter_grid({
     "HealthSystem": {
-        "scale_factor_delay_in_seeking_care_weather": [1], #[float(x) for x in [0, 1, 2, 10, 20, 60]],
-        "rescaling_prob_seeking_after_disruption": [1], #np.arange(0.01, 1.51, 0.5),
-        "rescaling_prob_disruption": [1], #np.arange(0.0, 2.01, 0.5),
-        "scale_factor_severity_disruption_and_delay": [1], #[float(x) for x in np.linspace(0.11, 1.0, 4)],
+        "scale_factor_delay_in_seeking_care_weather": [1.0], #[float(x) for x in [0, 1, 2, 10, 20, 60]],
+        "rescaling_prob_seeking_after_disruption": [1.0], #np.arange(0.01, 1.51, 0.5),
+        "rescaling_prob_disruption": [1.0], #np.arange(0.0, 2.01, 0.5),
+        "scale_factor_severity_disruption_and_delay": [1.0], #[float(x) for x in np.linspace(0.11, 1.0, 4)],
         "mode_appt_constraints": [1],
         "cons_availability": ["default"],
         "cons_availability_postSwitch": ["all"],
@@ -71,7 +71,7 @@ class ClimateDisruptionScenario(BaseScenario):
         )
 
     def draw_parameters(self, draw_number, rng):
-        return self._parameter_grid#[draw_number]
+        return self._parameter_grid[0]#[draw_number]
 
     def _get_scenarios(self) -> Dict[str, Dict]:
         """Return the Dict with values for the parameters that are changed, keyed by a name for the scenario."""
