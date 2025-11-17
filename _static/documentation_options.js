4e1268a7b145d3a26ffb6eb0cc95d22641a38db0
Asif Tamuri <tamuri@gmail.com>
2025-11-17 09:14:40 +0000

    publish: hallett/update pubs nov 25 (#1752)
    generated from commit 39c577c39ad3e021b978eff67532f6808673c959

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev2558+g9b3fa2fb4',
+    VERSION: '0.1.dev2576+g39c577c39',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
