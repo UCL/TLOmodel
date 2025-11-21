94a9b35069c449ea80a31705de779a44038bf9a9
PempheroM <pempheromphamba@gmail.com>
2025-11-21 14:07:32 +0200

    reformat

--- src/scripts/nurses_analyses/nurses_scenario_analyses.py
+++ src/scripts/nurses_analyses/nurses_scenario_analyses.py
@@ -22,12 +22,14 @@ from tlo.scenario import BaseScenario
 class StaffingScenario(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed=12
-        self.start_date=Date(2010, 1, 1)
-        self.end_date=Date(2030, 1, 1)
-        self.initial_population_size=200
-        self.number_of_draws=2
-        self.runs_per_draw=2
+        self.seed = 0
+        self.start_date = Date(2010, 1, 1)
+        self.end_date = Date(2030, 1, 1)
+        self.initial_population_size = 200
+        self._scenarios = self._get_scenarios()
+        self.number_of_draws = len(self._scenarios)
+        self.number_of_draws = 2
+        self.runs_per_draw = 2
 
     def log_configuration(self):
         return {
@@ -43,7 +45,14 @@ class StaffingScenario(BaseScenario):
         }
 
     def modules(self):
-        return fullmodel(resourcefilepath=self.resources) + [ImprovedHealthSystemAndCareSeekingScenarioSwitcher(resourcefilepath=self.resources)]
+        return fullmodel(resourcefilepath=self.resources) + [
+            ImprovedHealthSystemAndCareSeekingScenarioSwitcher(resourcefilepath=self.resources)]
+
+    def draw_parameters(self, draw_number, rng):
+        if draw_number < self.number_of_draws:
+            return list(self._scenarios.values())[draw_number]
+        else:
+            return
 
     def _default_of_all_scenarios(self) -> Dict:
         return mix_scenarios(
@@ -57,7 +66,7 @@ class StaffingScenario(BaseScenario):
                     "year_mode_switch": 2020,
                     'cons_availability': 'default',
                     'cons_availability_postSwitch': "all",
-                   # 'year_cons_availability_switch': 2025,
+                    # 'year_cons_availability_switch': 2025,
                     'HR_budget_growth_rate': self.hr_budget[0],
                     'yearly_HR_scaling_mode': 'historical_scaling',  # for 5 years of 2020-2024; source data year 2019
                     'start_year_HR_expansion_by_officer_type': self.YEAR_OF_HRH_EXPANSION,
@@ -74,41 +83,86 @@ class StaffingScenario(BaseScenario):
             },
         )
 
-    def _baseline_scenario(self) -> Dict:
-        return mix_scenarios(
-            self._default_of_all_scenarios(),
-            {
-                'HealthSystem': {
-                    'ResourceFile_HR_scaling_by_level_and_officer_type': "historical_scaling",
-                    'mode_appt_constraints_postSwitch': 2,
-                    "use_funded_or_actual_staffing": "actual",
-                },
-            },
-        )
+    # def _baseline_scenario(self) -> Dict:
+    #     return mix_scenarios(
+    #         self._default_of_all_scenarios(),
+    #         {
+    #             'HealthSystem': {
+    #                 'ResourceFile_HR_scaling_by_level_and_officer_type': "historical_scaling",
+    #                 'mode_appt_constraints_postSwitch': 2,
+    #                 "use_funded_or_actual_staffing": "actual",
+    #             },
+    #         },
+    #     )
+    #
+    # def _improved_staffing_scenario(self) -> Dict:
+    #     return mix_scenarios(
+    #         self._default_of_all_scenarios(),
+    #         {
+    #             'HealthSystem': {
+    #                 'ResourceFile_HR_scaling_by_level_and_officer_type': "historical_scaling",
+    #                 'mode_appt_constraints_postSwitch': 2,
+    #                 "use_funded_or_actual_staffing": "funded_plus",
+    #             },
+    #         },
+    #     )
+    #
+    # def _worst_case_scenario(self) -> Dict:
+    #     return mix_scenarios(
+    #         self._default_of_all_scenarios(),
+    #         {
+    #             'HealthSystem': {
+    #                 'ResourceFile_HR_scaling_by_level_and_officer_type': "historical_scaling",
+    #                 'mode_appt_constraints_postSwitch': 2,
+    #                 "use_funded_or_actual_staffing": "actual",
+    #             },
+    #         },
+    #     )
+    ####################################################################
+    def _get_scenarios(self) -> Dict[str, Dict]:
+        """Return the Dict with values for the parameters that are changed, keyed by a name for the scenario.
+        """
+        return {
+            "Baseline":
+                mix_scenarios(
+                    self._default_of_all_scenarios(),
+                    {
+                        "HealthSystem": {
+                            'ResourceFile_HR_scaling_by_level_and_officer_type': "historical_scaling",
+                            'mode_appt_constraints_postSwitch': 2,
+                            "use_funded_or_actual_staffing": "actual",
+                        },
+                    }
+                ),
 
-    def _improved_staffing_scenario(self) -> Dict:
-        return mix_scenarios(
-            self._default_of_all_scenarios(),
-            {
-                'HealthSystem': {
-                    'ResourceFile_HR_scaling_by_level_and_officer_type': "historical_scaling",
-                    'mode_appt_constraints_postSwitch': 2,
-                    "use_funded_or_actual_staffing": "funded_plus",
-                },
-            },
-        )
+            "Improved Staffing":
+                mix_scenarios(
+                    self._default_of_all_scenarios(),
+                    {
+                        "HealthSystem": {
+                            'ResourceFile_HR_scaling_by_level_and_officer_type': "historical_scaling",
+                            'mode_appt_constraints_postSwitch': 2,
+                            "use_funded_or_actual_staffing": "funded_plus",
+                        },
+                    }
+                ),
 
-    def _worst_case_scenario(self) -> Dict:
-        return mix_scenarios(
-            self._default_of_all_scenarios(),
-            {
-                'HealthSystem': {
-                    'ResourceFile_HR_scaling_by_level_and_officer_type': "historical_scaling",
-                    'mode_appt_constraints_postSwitch': 2,
-                    "use_funded_or_actual_staffing": "actual",
-                },
-            },
-        )
+            "Worse Case":
+                mix_scenarios(
+                    self._default_of_all_scenarios(),
+                    {
+                        "HealthSystem": {
+                            'ResourceFile_HR_scaling_by_level_and_officer_type': "historical_scaling",
+                            'mode_appt_constraints_postSwitch': 2,
+                            "use_funded_or_actual_staffing": "actual",
+                        },
+                    }
+                ),
+        }
+
+
+
+
     # To be sensitivity analysis
     # def _baseline_scenario(self) -> Dict:
     #     return mix_scenarios(
@@ -125,112 +179,7 @@ class StaffingScenario(BaseScenario):
     #             },
     #         },
     #     )
-    def draw_parameters(self, draw_number, rng):
-        if draw_number < self.number_of_draws:
-            return list(self._scenarios.values())[draw_number]
-        else:
-            return
 
-        def _get_scenarios(self) -> Dict[str, Dict]:
-            """Return the Dict with values for the parameters that are changed, keyed by a name for the scenario.
-            """
-            return {
-                "Baseline":
-                    mix_scenarios(
-                        self._default_of_all_scenarios(),
-                        {
-                            "HealthSystem": {
-                                "ResourceFile_HR_scaling_by_level_and_officer_type": "default",
-                                "year_mode_switch": 2020,
-                                "mode_appt_constraints_postSwitch": 2,
-                                "scale_to_effective_capabilities": True,
-                                "policy_name": "Naive",
-                                "tclose_overwrite": 1,
-                                "tclose_days_offset_overwrite": 7,
-                                "use_funded_or_actual_staffing": "actual",
-                                "year_cons_availability_switch": 2025,
-                                "cons_availability_postSwitch": "all",
-                            },
-                        }
-                    ),
-
-                "Improved Staffing":
-                    mix_scenarios(
-                        get_parameters_for_status_quo(),
-                        {
-                            "HealthSystem": {
-                                "ResourceFile_HR_scaling_by_level_and_officer_type": "default",
-                                "year_mode_switch": 2020,
-                                "mode_appt_constraints_postSwitch": 2,
-                                "scale_to_effective_capabilities": True,
-                                "policy_name": "Naive",
-                                "tclose_overwrite": 1,
-                                "tclose_days_offset_overwrite": 7,
-                                "use_funded_or_actual_staffing": "funded_plus",
-                                "year_cons_availability_switch": 2025,
-                                "cons_availability_postSwitch": "all",
-                            },
-                        }
-                    ),
-
-                "Worst-case Scenario":
-                    mix_scenarios(
-                        get_parameters_for_status_quo(),
-                        {
-                            "HealthSystem": {
-                                "yearly_HR_scaling_mode": "historical_scaling",
-                                "year_mode_switch": 2020,
-                                "mode_appt_constraints_postSwitch": 2,
-                                "scale_to_effective_capabilities": True,
-                                "policy_name": "Naive",
-                                "tclose_overwrite": 1,
-                                "tclose_days_offset_overwrite": 7,
-                                "use_funded_or_actual_staffing": "actual",
-                                "year_cons_availability_switch": 2019,
-                                "cons_availability_postSwitch": "all",
-                            },
-                        }
-                    ),
-
-                "Demand Sensitivity":
-                    mix_scenarios(
-                        get_parameters_for_status_quo(),
-                        {
-                            "HealthSystem": {
-                                "yearly_HR_scaling_mode": "historical_scaling",
-                                "year_mode_switch": 2020,
-                                "mode_appt_constraints_postSwitch": 2,
-                                "scale_to_effective_capabilities": True,
-                                "policy_name": "Naive",
-                                "tclose_overwrite": 1,
-                                "tclose_days_offset_overwrite": 7,
-                                "use_funded_or_actual_staffing": "actual",
-                                "year_cons_availability_switch": 2019,
-                                "cons_availability_postSwitch": "all",
-                            },
-                        }
-                    ),
-
-#Look into doing sensitivity analyses in the model
-            "Time appointments Sensitivity":
-            mix_scenarios(
-                get_parameters_for_status_quo(),
-                {
-                    "HealthSystem": {
-                        "yearly_HR_scaling_mode": "historical_scaling",
-                        "year_mode_switch": 2020,
-                        "mode_appt_constraints_postSwitch": 2,
-                        "scale_to_effective_capabilities": True,
-                        "policy_name": "Naive",
-                        "tclose_overwrite": 1,
-                        "tclose_days_offset_overwrite": 7,
-                        "use_funded_or_actual_staffing": "actual",
-                        "year_cons_availability_switch": 2019,
-                        "cons_availability_postSwitch": "all",
-                    },
-                }
-            ),
-            }
 
 if __name__ == '__main__':
     from tlo.cli import scenario_run
