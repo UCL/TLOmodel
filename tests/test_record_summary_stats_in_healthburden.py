9cf0f2e6acfabcdba59bb0c5482f3ed5073469b0
RachelMurray-Watson <rmurwat@gmail.com>
2026-01-06 10:47:06 +0000

    Added report_disease_numbers method

--- tests/test_record_summary_stats_in_healthburden.py
+++ tests/test_record_summary_stats_in_healthburden.py
@@ -99,6 +99,18 @@ class DummyDisease(Module):
         self.stored_prevalence[self.sim.date] = proportion_infected
         self.stored_number_infected[self.sim.date] = infected_total
 
+    def report_disease_numbers(self):
+        """Return disease statistics for DiseaseNumbers module."""
+        df = self.sim.population.props
+        infected_total = df.loc[df.is_alive, 'dm_is_infected'].sum()
+        proportion_infected = infected_total / df.is_alive.sum()
+        # Store the values so we can compare later
+        self.stored_prevalence[self.sim.date] = proportion_infected
+        self.stored_number_infected[self.sim.date] = infected_total
+        return {
+            'number_infected': infected_total,
+            'proportion_infected': proportion_infected,
+        }
 
 
 class DummyDiseaseEvent(RegularEvent, PopulationScopeEventMixin):
