39e6d591d0fdbef7355656f2890979d84c326bdb
RachelMurray-Watson <rmurwat@gmail.com>
2026-01-19 10:54:15 +0000

    test

--- src/scripts/climate_disruptions/climate_disruption_scenarios_baseline.py
+++ src/scripts/climate_disruptions/climate_disruption_scenarios_baseline.py
@@ -51,7 +51,7 @@ class ClimateDisruptionScenario(BaseScenario):
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2025, 1, 12)
         self.pop_size = 10_000
-        self.runs_per_draw = 1
+        self.runs_per_draw = 10
         self.YEAR_OF_CHANGE = 2025
         self._scenarios = self._get_scenarios()
         self._parameter_grid = full_grid
