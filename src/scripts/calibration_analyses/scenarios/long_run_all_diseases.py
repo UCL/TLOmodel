36eacd6ae8440509d898b517dc04002398283c34
RachelMurray-Watson <rmurwat@gmail.com>
2026-01-21 11:34:00 +0000

    Add in DALYs by region

--- src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
+++ src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
@@ -24,7 +24,7 @@ class LongRun(BaseScenario):
         self.end_date = Date(2040, 1, 1)  # The simulation will stop before reaching this date.
         self.pop_size = 100_000
         self.number_of_draws = 1
-        self.runs_per_draw = 10
+        self.runs_per_draw = 11
 
     def log_configuration(self):
         return {
