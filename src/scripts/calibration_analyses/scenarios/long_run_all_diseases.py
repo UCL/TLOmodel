0becaadb65f459766dd2b569aaf2f4cf5f9785a7
RachelMurray-Watson <rmurwat@gmail.com>
2026-01-21 12:41:03 +0000

    Bigger pop

--- src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
+++ src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
@@ -22,13 +22,13 @@ class LongRun(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2040, 1, 1)  # The simulation will stop before reaching this date.
-        self.pop_size = 10_000
+        self.pop_size = 100_000
         self.number_of_draws = 1
         self.runs_per_draw = 10
 
     def log_configuration(self):
         return {
-            'filename': 'long_run_all_diseases_2040_test',  # <- (specified only for local running)
+            'filename': 'long_run_all_diseases_2040_test_100k',  # <- (specified only for local running)
             'directory': './outputs',  # <- (specified only for local running)
             'custom_levels': {
                 '*': logging.WARNING,
