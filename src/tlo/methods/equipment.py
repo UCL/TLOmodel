d8917d1505325b5cff6d6fce7f2ccc3494f203f2
Asif Tamuri <tamuri@gmail.com>
2026-02-24 13:21:07 +0000

    Update src/tlo/methods/equipment.py
    Co-authored-by: Tim Hallett <39991060+tbhallett@users.noreply.github.com>

--- src/tlo/methods/equipment.py
+++ src/tlo/methods/equipment.py
@@ -133,7 +133,7 @@ class Equipment:
 
         return dat
 
-    def parse_items(self, items: Union[int, str, Iterable[int], Iterable[str]]) -> Set[int]:
+    def parse_items(self, items: Union[int, str, Iterable[Union[int, str]]]) -> Set[int]:
         """Parse equipment items specified as an item_code (integer), an item descriptor (string), or an iterable of
          item_codes or descriptors (including a mix of the two), and return as a set of item_code (integers). For any
          item_code/descriptor not recognised, a ``UserWarning`` is issued."""
