0b7c8a40c1cf3e58b994ed3b4a33f0a3ea2c7ef0
thewati <watipasomul@gmail.com>
2026-04-20 11:09:13 +0200

    update for next run

--- src/scripts/nurses_analyses/nurses_scenario_analyses.py
+++ src/scripts/nurses_analyses/nurses_scenario_analyses.py
@@ -36,10 +36,10 @@ class StaffingScenario(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2035, 1, 1)
-        self.pop_size = 200
+        self.pop_size = 100000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
-        self.runs_per_draw = 2
+        self.runs_per_draw = 5
 
     def log_configuration(self):
         return {
