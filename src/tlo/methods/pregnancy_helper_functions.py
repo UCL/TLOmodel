0588c05f6470033e2535870357e18e966f14d15b
joehcollins <joseph.h.collins@ucl.ac.uk>
2026-06-05 15:52:52 +0100

    update name of resource file

--- src/tlo/methods/pregnancy_helper_functions.py
+++ src/tlo/methods/pregnancy_helper_functions.py
@@ -263,6 +263,7 @@ def check_int_deliverable(self, int_name, hsi_event, q_param=None, cons=None,
     l_params = self.sim.modules["Labour"].current_parameters
     c = self.sim.modules["PregnancySupervisor"].mnh_outcome_counter
 
+    print(int_name)
     assert int_name in p_params["all_interventions"]
     c[f"{int_name}_req"] += 1
 
