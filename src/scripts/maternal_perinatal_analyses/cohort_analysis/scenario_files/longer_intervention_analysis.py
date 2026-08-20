f37e3c806b537bca63c16e7f66a66371bebcae1f
joehcollins <joseph.h.collins@ucl.ac.uk>
2026-08-20 10:27:14 +0100

    fixes

--- src/scripts/maternal_perinatal_analyses/cohort_analysis/scenario_files/longer_intervention_analysis.py
+++ src/scripts/maternal_perinatal_analyses/cohort_analysis/scenario_files/longer_intervention_analysis.py
@@ -12,12 +12,12 @@ class InterventionLongScenario(BaseScenario):
         self.start_date = Date(2025, 1, 1)
         self.end_date = Date(2041, 1, 2)
         self.pop_size = 40_000
-        self.number_of_draws = 10
-        self.runs_per_draw = 20
+        self.number_of_draws = 1
+        self.runs_per_draw = 5
 
     def log_configuration(self):
         return {
-            'filename': 'emonc_interventions', 'directory': './outputs',
+            'filename': f'emonc_interventions_{self.pop_size/1000}K', 'directory': './outputs',
             "custom_levels": {
                 "*": logging.WARNING,
                 "tlo.methods.demography": logging.INFO,
@@ -40,54 +40,57 @@ class InterventionLongScenario(BaseScenario):
 
     def draw_parameters(self, draw_number, rng):
 
-        if draw_number == 0:
-            return {'PregnancySupervisor': {
-                    'analysis_year': 2025}}
-        else:
+        return {'PregnancySupervisor': {
+                'analysis_year': 2025}}
 
-             interventions_for_analysis = [# Ectopic case management & post - abortion case management
-                                           ["ectopic_pregnancy_treatment",
-                                            "post_abortion_care_core"],
-
-                                           # Maternal sepsis case management
-                                           ["sepsis_treatment"],
-
-                                           # Treatment of antepartum and postpartum hemorrhage
-                                           ["pph_treatment_uterotonics",
-                                            "pph_treatment_mrrp",
-                                            "blood_transfusion_pph",
-                                            "blood_transfusion_aph"],
-
-                                           # Management of obstructed labor
-                                           ["avd_ol"],
-
-                                           # Management of pre-eclampsia and eclampsia
-                                           ["iv_anti_htns_ec",
-                                            "mgso4_spe",
-                                            "mgso4_ec",
-                                            "avd_spe_ec"],
-
-                                           # Caesarean section (uncomplicated and complicated) & other surgery
-                                           ["caesarean_section_oth_surg_ip",
-                                            "caesarean_section_oth_surg_pp"],
-
-                                           # Newborn sepsis case management
-                                           ["neo_sepsis_treatment_all"],
-
-                                           # Essential care of preterm of sick newborn including KMC
-                                           ["kmc",
-                                            "neo_resus_preterm",
-                                            "neo_sepsis_treatment_preterm"],
-
-                                           # Newborn resuscitation
-                                           ["neo_resus_all"]]
-
-
-             return {'PregnancySupervisor': {
-                        'analysis_year': 2025,
-                        'interventions_analysis': True,
-                        'interventions_under_analysis': interventions_for_analysis[draw_number-1],
-                        'intervention_analysis_availability': 1.0}}
+        # if draw_number == 0:
+        #     return {'PregnancySupervisor': {
+        #             'analysis_year': 2025}}
+        # else:
+        #
+        #      interventions_for_analysis = [# Ectopic case management & post - abortion case management
+        #                                    ["ectopic_pregnancy_treatment",
+        #                                     "post_abortion_care_core"],
+        #
+        #                                    # Maternal sepsis case management
+        #                                    ["sepsis_treatment"],
+        #
+        #                                    # Treatment of antepartum and postpartum hemorrhage
+        #                                    ["pph_treatment_uterotonics",
+        #                                     "pph_treatment_mrrp",
+        #                                     "blood_transfusion_pph",
+        #                                     "blood_transfusion_aph"],
+        #
+        #                                    # Management of obstructed labor
+        #                                    ["avd_ol"],
+        #
+        #                                    # Management of pre-eclampsia and eclampsia
+        #                                    ["iv_anti_htns_ec",
+        #                                     "mgso4_spe",
+        #                                     "mgso4_ec",
+        #                                     "avd_spe_ec"],
+        #
+        #                                    # Caesarean section (uncomplicated and complicated) & other surgery
+        #                                    ["caesarean_section_oth_surg_ip",
+        #                                     "caesarean_section_oth_surg_pp"],
+        #
+        #                                    # Newborn sepsis case management
+        #                                    ["neo_sepsis_treatment_all"],
+        #
+        #                                    # Essential care of preterm of sick newborn including KMC
+        #                                    ["kmc",
+        #                                     "neo_resus_preterm",
+        #                                     "neo_sepsis_treatment_preterm"],
+        #
+        #                                    # Newborn resuscitation
+        #                                    ["neo_resus_all"]]
+        #
+        #
+        #      return {'PregnancySupervisor': {
+        #                 'analysis_year': 2025,
+        #                 'interventions_analysis': True,
+        #                 'interventions_under_analysis': interventions_for_analysis[draw_number-1],
+        #                 'intervention_analysis_availability': 1.0}}
 
 
 if __name__ == '__main__':
