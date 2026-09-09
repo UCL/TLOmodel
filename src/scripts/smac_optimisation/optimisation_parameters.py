afd352330c09b425f0c5e331a133966f7e55522d
Margherita Molaro <48129834+marghe-molaro@users.noreply.github.com>
2026-09-09 09:35:35 +0100

    Create global parameters for time periods and pop size

---
+++ src/scripts/smac_optimisation/optimisation_parameters.py
@@ -0,0 +1,4 @@
+YEAR_START_DATE = 2010 # Year in which overall (from suspend) tlo sim starts 
+YEAR_END_DATE = 2012 # Year in which overall (including resume) tlo sim ends
+CONFIG_YEAR_START_DATE = 2011 # Year in which configuration changes are enforced
+POP_SIZE = 1000 # Population size simulated
