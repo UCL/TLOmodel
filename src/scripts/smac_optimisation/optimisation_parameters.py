23906ddfe6309bf31615a566de30e0a6fa6718ab
Margherita Molaro <48129834+marghe-molaro@users.noreply.github.com>
2026-09-10 10:20:34 +0100

    Extend duration to have more than one year

--- src/scripts/smac_optimisation/optimisation_parameters.py
+++ src/scripts/smac_optimisation/optimisation_parameters.py
@@ -2,7 +2,7 @@
 # TLO / hyperparameters
 # --------------------------------------------------------------------------
 YEAR_START_DATE = 2010 # Year in which overall (from suspend) tlo sim starts
-YEAR_END_DATE = 2012 # Year in which overall (including resume) tlo sim ends
+YEAR_END_DATE = 2014 # Year in which overall (including resume) tlo sim ends
 CONFIG_YEAR_START_DATE = 2011 # Year in which configuration changes are enforced
 POP_SIZE = 1000 # Population size simulated
 START_FIRST_BOUNDARY = CONFIG_YEAR_START_DATE
