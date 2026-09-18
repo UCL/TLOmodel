2d12ead5f49287557a4ee4c56060b1efe42dd4a5
Margherita Molaro <48129834+marghe-molaro@users.noreply.github.com>
2026-09-18 15:28:10 +0100

    Expand

--- src/scripts/smac_optimisation/optimisation_parameters.py
+++ src/scripts/smac_optimisation/optimisation_parameters.py
@@ -39,7 +39,7 @@ CONVERGENCE_MIN_RELATIVE_IMPROVEMENT = 0.01
 # --------------------------------------------------------------------------
 USE_SUSPEND_RESUME = True
 SUBMIT_SUSPEND_PART = False
-VALID_CHECKPOINT_COMMITS: list[str] = ['ff2ce522d88a6844d8e7ea865e81d497755862b0']
+VALID_CHECKPOINT_COMMITS: list[str] = ['ff2ce522d88a6844d8e7ea865e81d497755862b0', '58d44ab52a811fb644b3a90be34b12167a1d6a47']
 
 # --------------------------------------------------------------------------
 # Baseline run (see submit_baseline_job(), compute_and_save_baseline_budgets())
@@ -49,7 +49,7 @@ SUBMIT_BASELINE_RUN = False
 # --------------------------------------------------------------------------
 # Initial design (see submit_initial_design_jobs())
 # --------------------------------------------------------------------------
-VALID_PRIOR_RUN_COMMITS: list[str] = ['ff2ce522d88a6844d8e7ea865e81d497755862b0']
+VALID_PRIOR_RUN_COMMITS: list[str] = ['ff2ce522d88a6844d8e7ea865e81d497755862b0', '58d44ab52a811fb644b3a90be34b12167a1d6a47']
 SUBMIT_INITIAL_DESIGN = True
 N_INIT = 5
 INITIAL_DESIGN_NEAR_BASELINE_FRACTION = 0.2
