cd5f15018f7b525e8393bd854232ac3fac872087
joehcollins <joseph.h.collins@ucl.ac.uk>
2026-09-23 15:48:07 +0100

    adding in met_need capture

--- src/scripts/maternal_perinatal_analyses/cohort_analysis/scenario_files/longer_intervention_analysis.py
+++ src/scripts/maternal_perinatal_analyses/cohort_analysis/scenario_files/longer_intervention_analysis.py
@@ -11,13 +11,13 @@ class InterventionLongScenario(BaseScenario):
         self.seed = 120598
         self.start_date = Date(2025, 1, 1)
         self.end_date = Date(2026, 1, 2)
-        self.pop_size = 100_000
+        self.pop_size = 150_000
         self.number_of_draws = 2
         self.runs_per_draw = 5
 
     def log_configuration(self):
         return {
-            'filename': 'testing_bigger_pop_100K', 'directory': './outputs',
+            'filename': 'testing_bigger_pop_150K', 'directory': './outputs',
             "custom_levels": {
                 "*": logging.WARNING,
                 "tlo.methods.demography": logging.INFO,
